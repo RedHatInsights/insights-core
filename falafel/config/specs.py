e5d15b9d5ae4f43f8c059c5f9476b61f8383db2a
Mike M. Clark <miclark@redhat.com>
2016-07-28 18:07:54 -0500

    Adding rhn_hibernate.conf for collection.

--- falafel/config/specs.py
+++ falafel/config/specs.py
@@ -139,6 +139,7 @@ static_specs = {
     "resolv.conf"               : SimpleFileSpec("etc/resolv.conf"),
     "rhn.conf"                  : SimpleFileSpec("etc/rhn/rhn.conf"),
     "rhn-entitlement-cert.xml"  : SimpleFileSpec("etc/sysconfig/rhn/rhn-entitlement-cert.xml"),
+    "rhn_hibernate.conf"        : SimpleSpecFile("usr/share/rhn/config-defaults/rhn_hibernate.conf"),
     "rhn_taskomatic_daemon.log" : SimpleFileSpec("var/log/rhn/rhn_taskomatic_daemon.log", large_content=True),
     "root_crontab"              : CommandSpec("/usr/bin/crontab -l -u root"),
     "rpm_-V_packages"           : CommandSpec("/bin/rpm -V coreutils procps procps-ng shadow-utils passwd sudo"),
