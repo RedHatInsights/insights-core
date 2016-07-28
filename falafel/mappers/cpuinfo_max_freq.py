82ab4e7d404af58c184e461f73fd2a5cd25ae1f6
Xiangce Liu <xiangceliu@redhat.com>
2016-07-28 11:34:33 +0800

    Add new shared mapper cpuinfo_max_freq

---
+++ falafel/mappers/cpuinfo_max_freq.py
@@ -0,0 +1,10 @@
+from falafel.core.plugins import mapper
+
+
+@mapper('cpuinfo_max_freq')
+def cpu_max_freq(context):
+    """
+    Returns an integer representing the maximum frequency in KHZ
+    - E.g.: 3200000
+    """
+    return int(context.content[0].strip())
---
+++ falafel/mappers/tests/test_cpuinfo_max_freq.py
@@ -0,0 +1,9 @@
+from falafel.tests import context_wrap
+from falafel.mappers.cpuinfo_max_freq import cpu_max_freq
+
+cpuinfo_content = '3200000'
+
+
+def test_cpu_max_freq():
+    result = cpu_max_freq(context_wrap(cpuinfo_content))
+    assert result == 3200000
