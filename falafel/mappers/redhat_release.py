3d39e0925cfbe4f92767c77e91f4e5b2b78e57cd
Kyle Lape <klape@redhat.com>
2016-08-04 14:23:11 -0500

    Update redhat-release mapper
    New computed fields:
    
    - major (int): RHEL major version
    - minor (int): RHEL minor version
    - version (float): Both major and minor version
    - is_rhel (bool): Whether or not it's RHEL
    - product (str)

--- falafel/mappers/redhat_release.py
+++ falafel/mappers/redhat_release.py
@@ -1,13 +1,47 @@
 from falafel.core.plugins import mapper
+from falafel.core import MapperOutput, computed
+
+
+def parse(data):
+    product, _, version_name = [v.strip() for v in data.partition("release")]
+    version, code_name = [v.strip() for v in version_name.split(None, 1)]
+    return {
+        "product": product,
+        "version": version,
+        "code_name": code_name.strip("()")
+    }
+
+
+class RedhatRelease(MapperOutput):
+
+    def __init__(self, data):
+        self.parsed = parse(data)
+        super(RedhatRelease, self).__init__(data)
+
+    @computed
+    def major(self):
+        return int(self.parsed["version"].split(".")[0])
+
+    @computed
+    def minor(self):
+        s = self.parsed["version"].split(".")
+        if len(s) > 1:
+            return int(s[1])
+
+    @computed
+    def version(self):
+        return float(self.parsed["version"])
+
+    @computed
+    def is_rhel(self):
+        return "Red Hat Enterprise Linux" in self.parsed["product"]
+
+    @computed
+    def product(self):
+        return self.parsed["product"]
 
 
 @mapper("redhat-release")
 def redhat_release(context):
-    """
-    As the content of file /etc/redhat-release is a string, we can return the content directly.
-    """
-    return {
-        'file_content': context.content[0],
-        'rhel_release': context.release,
-        'rhel_version': context.version
-    }
+    if len(context.content) == 1 and len(context.content[0]) > 0:
+        return RedhatRelease(context.content[0])
--- falafel/mappers/tests/test_redhat_release.py
+++ falafel/mappers/tests/test_redhat_release.py
@@ -2,17 +2,43 @@ from falafel.mappers import redhat_release
 from falafel.tests import context_wrap
 
 REDHAT_RELEASE1 = """
-Red Hat Enterprise Linux Server release 7.2 (Maipo)
-""".strip()
-
-REDHAT_RELEASE2 = """
 Red Hat Enterprise Linux Server release 6.7 (Santiago)
 """.strip()
 
+REDHAT_RELEASE2 = """
+Red Hat Enterprise Linux Server release 7.2 (Maipo)
+""".strip()
 
-def test_redhat_release():
+FEDORA = """
+Fedora release 23 (Twenty Three)
+""".strip()
+
+
+def test_rhe6():
     release = redhat_release.redhat_release(context_wrap(REDHAT_RELEASE1))
-    assert release.get("file_content") == "Red Hat Enterprise Linux Server release 7.2 (Maipo)"
+    assert release.data == REDHAT_RELEASE1
+    assert release.major == 6
+    assert release.minor == 7
+    assert release.version == 6.7
+    assert release.is_rhel is True, release.product
+    assert release.product == "Red Hat Enterprise Linux Server", release.product
 
+
+def test_rhe7():
     release = redhat_release.redhat_release(context_wrap(REDHAT_RELEASE2))
-    assert release.get("file_content") == "Red Hat Enterprise Linux Server release 6.7 (Santiago)"
+    assert release.data == REDHAT_RELEASE2
+    assert release.major == 7
+    assert release.minor == 2
+    assert release.version == 7.2
+    assert release.is_rhel is True
+    assert release.product == "Red Hat Enterprise Linux Server", release.product
+
+
+def test_fedora23():
+    release = redhat_release.redhat_release(context_wrap(FEDORA))
+    assert release.data == FEDORA
+    assert release.major == 23
+    assert release.minor is None
+    assert release.version == 23.0
+    assert release.is_rhel is False
+    assert release.product == "Fedora"
