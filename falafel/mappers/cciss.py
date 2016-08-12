334fe789003eecb741c470db6cbe1c04874b2d77
Jesse Jaggars <jjaggars@redhat.com>
2016-08-12 18:01:35 -0500

    Wrapping the new Cciss class with @mapper

--- falafel/mappers/cciss.py
+++ falafel/mappers/cciss.py
@@ -1,25 +1,64 @@
 import os
-import re
 from falafel.core.plugins import mapper
 from falafel.core import MapperOutput, computed
 
 
-CCISS_FIRMWARE_VERSION_REGEX = re.compile(r'\d+[.]*\d*')
-CCISS_MODEL_REGEX = re.compile(r'cciss\d+')
-
+@mapper('cciss')
 class Cciss(MapperOutput):
 
-    def __init__(self, content, path):
-        cciss_info = {
-            "device": os.path.basename(path)
+    @classmethod
+    def parse_context(cls, context):
+        '''
+        Returns a dict of cciss info.
+
+        Input
+        -----
+
+        cciss0: HP Smart Array P220i Controller
+        Board ID: 0x3355103c
+        Firmware Version: 3.42
+        IRQ: 82
+        Logical drives: 1
+        Sector size: 8192
+        Current Q depth: 0
+        Current # commands on controller: 0
+        Max Q depth since init: 84
+        Max # commands on controller since init: 111
+        Max SG entries since init: 128
+        Sequential access devices: 0
+
+        cciss/c0d0:  299.96GB   RAID 1(1+0)
+
+        Output
+        ------
+
+        {
+            "Sequential access devices": "0",
+            "Current Q depth": "0",
+            "cciss0": "HP Smart Array P220i Controller",
+            "Board ID": "0x3355103c",
+            "IRQ": "82",
+            "cciss/c0d0": "299.96GB   RAID 1(1+0)",
+            "Logical drives": "1",
+            "Current # commands on controller": "0",
+            "Sector size": "8192",
+            "Firmware Version": "3.42",
+            "Max # commands on controller since init": "111",
+            "Max SG entries since init": "128",
+            "Max Q depth since init": "84"
         }
-        for line in content:
+
+        '''
+
+        cciss_info = {
+            "device": os.path.basename(context.path)
+        }
+        for line in context.content:
             if line.strip():
                 key, val = line.split(":", 1)
                 cciss_info[key.strip()] = val.strip()
 
-        super(Cciss, self).__init__(cciss_info, path)
-
+        return cls(cciss_info, context.path)
 
     @computed
     def firmware_version(self):
@@ -28,45 +67,3 @@ class Cciss(MapperOutput):
     @computed
     def model(self):
         return self.data['device']
-
-
-@mapper('cciss')
-def get_cciss(context):
-    '''
-    Return a dict of cciss info. For example:
-
-    Input:
-    cciss0: HP Smart Array P220i Controller
-    Board ID: 0x3355103c
-    Firmware Version: 3.42
-    IRQ: 82
-    Logical drives: 1
-    Sector size: 8192
-    Current Q depth: 0
-    Current # commands on controller: 0
-    Max Q depth since init: 84
-    Max # commands on controller since init: 111
-    Max SG entries since init: 128
-    Sequential access devices: 0
-
-    cciss/c0d0:  299.96GB   RAID 1(1+0)
-
-    Output:
-    {
-    "Sequential access devices": "0",
-    "Current Q depth": "0",
-    "cciss0": "HP Smart Array P220i Controller",
-    "Board ID": "0x3355103c",
-    "IRQ": "82",
-    "cciss/c0d0": "299.96GB   RAID 1(1+0)",
-    "Logical drives": "1",
-    "Current # commands on controller": "0",
-    "Sector size": "8192",
-    "Firmware Version": "3.42",
-    "Max # commands on controller since init": "111",
-    "Max SG entries since init": "128",
-    "Max Q depth since init": "84"
-    }
-    '''
-
-    return Cciss(context.content, context.path)
--- falafel/mappers/tests/test_cciss.py
+++ falafel/mappers/tests/test_cciss.py
@@ -20,20 +20,19 @@ cciss/c0d0:  299.96GB   RAID 1(1+0)
 '''
 
 
-class TestCciss():
-    def test_get_cciss(self):
-        cciss_info = cciss.get_cciss(context_wrap(CCISS))
+def test_get_cciss():
+    cciss_info = cciss.Cciss.parse_context(context_wrap(CCISS))
 
-        assert cciss_info['cciss0'] == 'HP Smart Array P220i Controller'
-        assert cciss_info['Board ID'] == "0x3355103c"
-        assert cciss_info['Firmware Version'] == '3.42'
-        assert cciss_info['IRQ'] == '82'
-        assert cciss_info['Logical drives'] == '1'
-        assert cciss_info['Sector size'] == '8192'
-        assert cciss_info['Current Q depth'] == '0'
-        assert cciss_info['Current # commands on controller'] == '0'
-        assert cciss_info['Max Q depth since init'] == '84'
-        assert cciss_info['Max # commands on controller since init'] == '111'
-        assert cciss_info['Max SG entries since init'] == '128'
-        assert cciss_info['Sequential access devices'] == '0'
-        assert cciss_info['cciss/c0d0'] == '299.96GB   RAID 1(1+0)'
+    assert cciss_info['cciss0'] == 'HP Smart Array P220i Controller'
+    assert cciss_info['Board ID'] == "0x3355103c"
+    assert cciss_info['Firmware Version'] == '3.42'
+    assert cciss_info['IRQ'] == '82'
+    assert cciss_info['Logical drives'] == '1'
+    assert cciss_info['Sector size'] == '8192'
+    assert cciss_info['Current Q depth'] == '0'
+    assert cciss_info['Current # commands on controller'] == '0'
+    assert cciss_info['Max Q depth since init'] == '84'
+    assert cciss_info['Max # commands on controller since init'] == '111'
+    assert cciss_info['Max SG entries since init'] == '128'
+    assert cciss_info['Sequential access devices'] == '0'
+    assert cciss_info['cciss/c0d0'] == '299.96GB   RAID 1(1+0)'
