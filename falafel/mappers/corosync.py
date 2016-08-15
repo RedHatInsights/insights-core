67d2a21da1df072285f05d89dfa8cd87d59e6b30
Son Nguyen <songuyen@redhat.com>
2016-08-15 13:12:47 -0500

    Update corosync to CoroSyncConfig mapper output

--- falafel/mappers/corosync.py
+++ falafel/mappers/corosync.py
@@ -1,18 +1,23 @@
 from falafel.core.plugins import mapper
-
+from falafel.core import MapperOutput
 
 @mapper("corosync")
-def parse_corosync(context):
-    """
-    Parse /etc/sysconfig/corosync
-    return dict like {'COROSYNC_OPTIONS': '', 'COROSYNC_INIT_TIMEOUT': '60'}
-    """
-    corosync_dict = {}
-    for line in context.content:
-        line = line.strip()
-        if line.startswith("#") or line == "":
-            continue
-        if "=" in line:
-            (key, value) = line.split("=", 1)
-            corosync_dict[key.strip()] = value.strip().replace('"', "")
-    return corosync_dict
+class CoroSyncConfig(MapperOutput):
+
+    @classmethod
+    def parse_context(cls, context):
+
+        """
+        Parse /etc/sysconfig/corosync
+        return dict like {'COROSYNC_OPTIONS': '', 'COROSYNC_INIT_TIMEOUT': '60'}
+        """
+
+        corosync_dict = {}
+        for line in context.content:
+            line = line.strip()
+            if line.startswith("#") or line == "":
+                continue
+            if "=" in line:
+                (key, value) = line.split("=", 1)
+                corosync_dict[key.strip()] = value.strip().replace('"', "")
+        return cls(corosync_dict, context.path)
