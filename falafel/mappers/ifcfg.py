9dca4600223da28231675c3a1b2c6f5aa581f4c0
Jesse Jaggars <jjaggars@redhat.com>
2016-08-18 14:32:13 -0500

    tweaking ifcfg mapper to handle bonding opts

--- falafel/mappers/ifcfg.py
+++ falafel/mappers/ifcfg.py
@@ -15,18 +15,26 @@ def ifcfg(context):
     `TEAM_CONFIG`, `TEAM_PORT_CONFIG` will return a dict with user config dict
     `BONDING_OPTS` also will return a dict
     """
-    result = {}
+    result = {
+        "iface": context.path.rsplit("-", 1)[1]
+    }
     for line in get_active_lines(context.content):
-            key, _, value = line.partition("=")
-            key, value = (key.strip().strip(QUOTES), value.strip().strip(QUOTES))
-            if key in JSON_FIELDS:
-                value = json.loads(value.replace("\\", ""))
-            if key == "BONDING_OPTS":
-                value_map = {}
-                for key_value_pair in value.split():
-                    sub_key, _, sub_value = key_value_pair.partition("=")
-                    value_map[sub_key.strip()] = sub_value.strip()
-                value = value_map
-            result[key] = value
-    result["iface"] = context.path.rsplit("-", 1)[1]
+        key, _, value = line.partition("=")
+        key = key.strip().strip(QUOTES)
+
+        # In some cases we want to know what the actual value-side
+        # of the key is
+        if key == "BONDING_OPTS":
+            result["raw_bonding_value"] = value
+
+        value = value.strip().strip(QUOTES)
+        if key in JSON_FIELDS:
+            value = json.loads(value.replace("\\", ""))
+        if key == "BONDING_OPTS":
+            value_map = {}
+            for key_value_pair in value.split():
+                sub_key, _, sub_value = key_value_pair.partition("=")
+                value_map[sub_key.strip()] = sub_value.strip()
+            value = value_map
+        result[key] = value
     return result
