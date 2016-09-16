fc9d19160b8bf23cc3fbb86beba5ff0f7c513cb1
Paul Wayper <paulway@redhat.com>
2016-09-16 17:04:34 +1000

    Just use a single routine rather than a class - sets up the structure and parses the drive information for now

--- falafel/mappers/smartctl.py
+++ falafel/mappers/smartctl.py
@@ -1,14 +1,46 @@
 from falafel.core import MapperOutput
 from falafel.core.plugins import mapper
+import os
+import re
 
+"""
+    Mapper for output of smartctl -a for each drive in system.
+    This returns a dict with keys:
+     * 'device' - the name of the device after /dev/ - e.g. sda
+     * 'info' - the -i info (vendor, product, etc)
+     * 'health' - overall health assessment (-H)
+     * 'values' - the SMART values (-c) - SMART config on drive firmware
+     * 'attributes' - the SMART attributes (-A) - run time data
+"""
 @mapper('smartctl')
-class SMARTctl(MapperOutput):
+def SMARTctl(context):
     """
-        Mapper for output of smartctl -a for each drive in system.
-        Each drive is available separately - so sda is in SMARTctl.get('sda')
-        This returns a dict with keys:
-         * 'info' - the -i info (vendor, product, etc)
-         * 'health' - overall health assessment (-H)
-         * 'values' - the SMART values (-c) - configures SMART on drive
-         * 'attributes' - the SMART attributes (-A) - run time data
+        This function gets called once per detected file, so we don't have to
+        deal with file changes within this process.
     """
+    filename_re =   re.compile(r'smartctl_-a_\.dev\.(?P<device>\w+)$')
+    info_line_re =  re.compile(r'(?P<key>\w+(?:\s\w+)*):\s+(?P<value>\S.*?)\s*$')
+
+    match = filename_re.search(context.path)
+    if not match:
+        return {}
+
+    drive_info = {
+        'device'    : match.group('device'),
+        'info'      : {},
+        'health'    : 'not parsed',
+        'values'    : {},
+        'attributes': {},
+    }
+
+    for line in context.content:
+        # Parse info section until 'Error Counter logging not supported'
+        if line.startswith('Error Counter logging not supported'):
+            break
+        match = info_line_re.search(line)
+        if match:
+            drive_info['info'][match.group('key')] = match.group('value')
+
+    # Continue parsing the values and attributes sections
+
+    return drive_info
