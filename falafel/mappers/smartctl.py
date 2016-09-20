b470f97161a35ca872664f9130165768bb376b1f
Paul Wayper <paulway@redhat.com>
2016-09-20 10:18:06 +1000

    Many improvements - tests all now pass!
    First, I had to implement a per-list generator to go through the lines in the
    list without starting from the beginning again - this is not exactly pretty but
    makes the logic much easier to follow.  You don't want state machines in there,
    trust me.
    
    We now parse value names correctly using a simpler regex.
    
    We now parse attributes completely - everything's in there.

--- falafel/mappers/smartctl.py
+++ falafel/mappers/smartctl.py
@@ -1,7 +1,20 @@
 from falafel.core import MapperOutput
 from falafel.core.plugins import mapper
+
 import os
 import re
+
+# Make our own iterator across the content, which doesn't go back to the
+# start when we read it later...
+pos_for_list = {}
+def lines(l):
+    global pos_for_list
+    idl = id(l)
+    if idl not in pos_for_list:
+        pos_for_list[idl] = 0
+    while pos_for_list[idl] < len(l):
+        yield l[pos_for_list[idl]]
+        pos_for_list[idl] += 1
 
 """
     Mapper for output of smartctl -a for each drive in system.
@@ -20,7 +33,13 @@ def SMARTctl(context):
     """
     filename_re =   re.compile(r'smartctl_-a_\.dev\.(?P<device>\w+)$')
     info_line_re =  re.compile(r'(?P<key>\w+(?:\s\w+)*):\s+(?P<value>\S.*?)\s*$')
-    value_line_re = re.compile(r'(?P<key>\w[A-Za-z_.-]+(?:\s\w+)*):\s+\(\s*(?P<value>\S.*?)\)')
+    value_line_re = re.compile(r'(?P<key>\w[A-Za-z _.-]+):\s+\(\s*(?P<value>\S.*?)\)')
+    attr_line_re =  re.compile(r'^\s*(?P<id>\d+)\s(?P<name>\w+)\s+' +
+     r'(?P<flag>0x[0-9a-fA-F]{4})\s+(?P<value>\d{3})\s+(?P<worst>\d{3})\s+' + 
+     r'(?P<threshold>\d{3})\s+(?P<type>[A-Za-z_-]+)\s+(?P<updated>[A-Za-z_-]+)\s+' + 
+     r'(?P<when_failed>\S+)\s+(?P<raw_value>\S.*)$')
+    attr_keys = ['id', 'flag', 'value', 'worst', 'threshold', 'type', 'updated',
+     'when_failed', 'raw_value']
 
     match = filename_re.search(context.path)
     if not match:
@@ -35,14 +54,13 @@ def SMARTctl(context):
     }
 
     # Information section:
-    for line in context.content:
-        print "information section: line =", line
+    for line in lines(context.content):
         # Exit parsing information section if we go into the next section
-        if (line.startswith('Error Counter logging not supported') or
-           line.startswith('=== START OF READ SMART DATA SECTION ===')):
+        if line.startswith('=== START OF READ SMART DATA SECTION ==='):
             break
         match = info_line_re.search(line)
         if match:
+#            print "information matched key:", match.group('key'), "value:", match.group('value')
             drive_info['info'][match.group('key')] = match.group('value')
         else:
             # Translate some of the less structured information
@@ -55,11 +73,13 @@ def SMARTctl(context):
             elif line == 'Device does not support Self Test logging':
                 drive_info['info']['Self Test logging'] = 'Not supported'
             elif line == 'Temperature Warning Disabled or Not Supported':
-                drive_info['info']['Temperature Warning'] = 'Disabled or Not supported'
+                drive_info['info']['Temperature Warning'] = 'Disabled or Not Supported'
 
     # Values section:
     full_line = ''
-    for line in context.content:
+    for line in lines(context.content):
+        if line.startswith('=== START OF READ SMART DATA SECTION ==='):
+            continue
         if line.startswith('Vendor Specific SMART Attributes with Thresholds:'):
             break
         if line.startswith('SMART overall-health self-assessment test result'):
@@ -77,7 +97,6 @@ def SMARTctl(context):
         if full_line:
             full_line += ' '
         full_line += line.strip()
-        print "full_line =", full_line
         
         match = value_line_re.search(full_line)
         if match:
@@ -85,11 +104,21 @@ def SMARTctl(context):
             # with the previous line and values are in minutes.
             (key, value) = match.group('key', 'value')
             drive_info['values'][key] = value
-            print "... matched, key =", key, "value =", value
+#            print "... matched, key =", key, "value =", value
             full_line = ''
-            
-        
-    # Continue parsing the values and attributes sections
+
+    # Attributes sections
+    for line in lines(context.content):
+        if line.startswith('SMART Error Log Version:'):
+            break
+        if line.startswith('Vendor Specific SMART Attributes with Thresholds:'):
+            continue
+        if len(line) == 0:
+            continue
+        match = attr_line_re.match(line)
+        if match:
+            name = match.group('name')
+            drive_info['attributes'][name] = { key: match.group(key) for key in attr_keys }
 
     return drive_info
 
