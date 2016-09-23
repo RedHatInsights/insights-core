23e072e131fdafe6f402f6d7204189a05b6571fc
Xiangce Liu <xiangceliu@redhat.com>
2016-09-23 13:48:19 +0800

    Update all mappers used LogFileOutput to class mapper
    - Add "slave_interface" to Bond

--- falafel/mappers/catalina_out.py
+++ falafel/mappers/catalina_out.py
@@ -2,19 +2,5 @@ from .. import LogFileOutput, mapper
 
 
 @mapper('catalina.out')
-def catalina_out(context):
-    """
-    Returns an object in type of LogLineList which provide two methods:
-    - Usage:
-      in:
-        log = shared.get(tomcat_catalina_out)
-        if "Abort command issued" in log:
-            ...
-      get:
-        err_lines = log.get('KEY_WORDS')
-        for line in err_lines:
-            ...
-    -----------
-
-    """
-    return LogFileOutput(context.content, path=context.path)
+class CatalinaOut(LogFileOutput):
+    pass
--- falafel/mappers/libvirtd_log.py
+++ falafel/mappers/libvirtd_log.py
@@ -2,5 +2,5 @@ from .. import LogFileOutput, mapper
 
 
 @mapper('libvirtd.log')
-def parse_libvirtd_log(context):
-    return LogFileOutput(context.content, path=context.path)
+class LibVirtdLog(LogFileOutput):
+    pass
