d2f9931ca6c987299e76e39aee2028b69ba6c355
Paul Wayper <paulway@redhat.com>
2016-10-13 10:02:24 +1100

    Further improvements to make SMARTctl more conformant
    * Use class variables for the regular expressions.
    * Use named states (albeit still just numeric values)
    * Use the 'Mapper' base class
    * The device name is now in the 'device' property of the object, not
    in the info dict.
    * Do the parsing in parse_content and set the 'info' dict to the
    parsed drive info dict.  This makes the mapper more like the other
    mappers.

--- falafel/mappers/smartctl.py
+++ falafel/mappers/smartctl.py
@@ -1,10 +1,10 @@
-from falafel.core import MapperOutput
+from falafel.core import Mapper
 from falafel.core.plugins import mapper
 
 import re
 
 @mapper('smartctl')
-class SMARTctl(MapperOutput):
+class SMARTctl(Mapper):
     """
         Mapper for output of smartctl -a for each drive in system.
         This returns a dict with keys:
@@ -17,17 +17,29 @@ class SMARTctl(MapperOutput):
         deal with file changes within this process.
     """
 
-    def __init__(self, data, path):
+    INFO_LINE_RE =  re.compile(r'(?P<key>\w+(?:\s\w+)*):\s+' +
+     r'(?P<value>\S.*?)\s*$')
+    VALUE_LINE_RE = re.compile(r'(?P<key>\w[A-Za-z _.-]+):\s+' +
+     r'\(\s*(?P<value>\S.*?)\)')
+    ATTR_LINE_RE =  re.compile(r'^\s*(?P<id>\d+)\s(?P<name>\w+)\s+' +
+     r'(?P<flag>0x[0-9a-fA-F]{4})\s+(?P<value>\d{3})\s+' +
+     r'(?P<worst>\d{3})\s+(?P<threshold>\d{3})\s+' +
+     r'(?P<type>[A-Za-z_-]+)\s+(?P<updated>[A-Za-z_-]+)\s+' +
+     r'(?P<when_failed>\S+)\s+(?P<raw_value>\S.*)$')
+    ATTR_KEYS = ['id', 'flag', 'value', 'worst', 'threshold', 'type',
+     'updated', 'when_failed', 'raw_value']
+
+    def __init__(self, context):
+        super(SMARTctl, self).__init__(context)
         filename_re = re.compile(r'smartctl_-a_\.dev\.(?P<device>\w+)$')
-        match = filename_re.search(path)
+        match = filename_re.search(context.path)
+        self.data = {}
         if match:
-            data['device'] = match.group('device')
+            self.device = match.group('device')
         # Else warn?  fail?
 
-        super(SMARTctl, self).__init__(data, path)
 
-    @staticmethod
-    def parse_content(content):
+    def parse_content(self, content):
         drive_info = {
             'info'          : {},
             'health'        : 'not parsed',
@@ -36,34 +48,22 @@ class SMARTctl(MapperOutput):
             'full_line'     : '', # hack for persistent line storage
         }
 
-        info_line_re =  re.compile(r'(?P<key>\w+(?:\s\w+)*):\s+' +
-         r'(?P<value>\S.*?)\s*$')
-        value_line_re = re.compile(r'(?P<key>\w[A-Za-z _.-]+):\s+' +
-         r'\(\s*(?P<value>\S.*?)\)')
-        attr_line_re =  re.compile(r'^\s*(?P<id>\d+)\s(?P<name>\w+)\s+' +
-         r'(?P<flag>0x[0-9a-fA-F]{4})\s+(?P<value>\d{3})\s+' +
-         r'(?P<worst>\d{3})\s+(?P<threshold>\d{3})\s+' +
-         r'(?P<type>[A-Za-z_-]+)\s+(?P<updated>[A-Za-z_-]+)\s+' +
-         r'(?P<when_failed>\S+)\s+(?P<raw_value>\S.*)$')
-        attr_keys = ['id', 'flag', 'value', 'worst', 'threshold', 'type',
-         'updated', 'when_failed', 'raw_value']
-
         # Parsing using a state machine, sorry.  We use a state variable, and
         # functions to parse lines in each of the different states.  The function
         # returns the state as a result of reading that line, and we look up the
         # parse function out of an array based on the parse state.
-        # parse state 0 = Reading 'key: value' drive information
-        # parse state 1 = Reading freeform drive values
-        # parse state 2 = Reading attributes information
-        # parse state 3 = finished reading.
-        parse_state = 0
+        PARSE_FORMATTED_INFO = 0
+        PARSE_FREEFORM_INFO = 1
+        PARSE_ATTRIBUTE_INFO = 2
+        PARSE_COMPLETE = 3
+        parse_state = PARSE_FORMATTED_INFO
 
         # Information section:
         def parse_information(line):
             # Exit parsing information section if we go into the next section
             if line.startswith('=== START OF READ SMART DATA SECTION ==='):
-                return 1
-            match = info_line_re.search(line)
+                return PARSE_FREEFORM_INFO
+            match = self.INFO_LINE_RE.search(line)
             if match:
                 drive_info['info'][match.group('key')] = match.group('value')
             else:
@@ -79,53 +79,53 @@ class SMARTctl(MapperOutput):
                 elif line == 'Temperature Warning Disabled or Not Supported':
                     drive_info['info']['Temperature Warning'] =\
                         'Disabled or Not Supported'
-            return 0
+            return PARSE_FORMATTED_INFO
 
         # Values section:
         def parse_values(line):
             if line.startswith('=== START OF READ SMART DATA SECTION ==='):
-                return 1
+                return PARSE_FREEFORM_INFO
             if line.startswith('Vendor Specific SMART Attributes with Thresholds'):
-                return 2
+                return PARSE_ATTRIBUTE_INFO
             if line.startswith('SMART overall-health self-assessment test result'):
                 drive_info['health'] = ''.join((line.split(': '))[1:])
-                return 1
+                return PARSE_FREEFORM_INFO
             # Values section begins with this - ignore:
             if line.startswith('General SMART Values:'):
-                return 1
+                return PARSE_FREEFORM_INFO
 
             # Lines starting with a space are continuations of the commentary on
             # the previous setting - ignore
             if len(line) == 0 or line[0] == ' ' or line[0] == "\t":
-                return 1
+                return PARSE_FREEFORM_INFO
             # Otherwise, join this line to the full line
             if drive_info['full_line']:
                 drive_info['full_line'] += ' '
             drive_info['full_line'] += line.strip()
 
-            match = value_line_re.search(drive_info['full_line'])
+            match = self.VALUE_LINE_RE.search(drive_info['full_line'])
             if match:
                 # Handle the recommended polling time lines, which are joined
                 # with the previous line and values are in minutes.
                 (key, value) = match.group('key', 'value')
                 drive_info['values'][key] = value
                 drive_info['full_line'] = ''
-            return 1
+            return PARSE_FREEFORM_INFO
 
         # Attributes sections
         def parse_attributes(line):
             if line.startswith('SMART Error Log Version:'):
-                return 3
+                return PARSE_COMPLETE
             if line.startswith('Vendor Specific SMART Attributes with Thresholds'):
-                return 2
+                return PARSE_ATTRIBUTE_INFO
             if len(line) == 0:
-                return 2
-            match = attr_line_re.match(line)
+                return PARSE_ATTRIBUTE_INFO
+            match = self.ATTR_LINE_RE.match(line)
             if match:
                 name = match.group('name')
                 drive_info['attributes'][name] = {key: match.group(key)
-                    for key in attr_keys}
-            return 2
+                    for key in self.ATTR_KEYS}
+            return PARSE_ATTRIBUTE_INFO
 
         parse_for_state = [
             parse_information,
@@ -135,10 +135,10 @@ class SMARTctl(MapperOutput):
 
         for line in content:
             parse_state = parse_for_state[parse_state](line)
-            if parse_state == 3:
+            if parse_state == PARSE_COMPLETE:
                 break
 
         # Delete temporary full line storage
         del drive_info['full_line']
 
-        return drive_info
+        self.info = drive_info
