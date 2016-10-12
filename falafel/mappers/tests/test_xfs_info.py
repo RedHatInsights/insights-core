b5e54db57b84ef425448d38b9ebd3ffc619362b3
Paul Wayper <paulway@redhat.com>
2016-10-12 16:18:39 +1100

    Store parsing results in 'xfs', no space in front of data

--- falafel/mappers/tests/test_xfs_info.py
+++ falafel/mappers/tests/test_xfs_info.py
@@ -3,54 +3,54 @@ from falafel.tests import context_wrap
 
 class TestXFSInfo():
     def test_example_xfs_info(self):
-        xfs_info = xfs_info.XFSInfo(context_wrap("""
- meta-data=/dev/sda      isize=256    agcount=32, agsize=16777184 blks
-          =              sectsz=512   attr=2
- data     =              bsize=4096   blocks=536869888, imaxpct=5
-          =              sunit=32     swidth=128 blks
- naming   =version 2     bsize=4096
- log      =internal      bsize=4096   blocks=32768, version=2
-          =              sectsz=512   sunit=32 blks, lazy-count=1
- realtime =none          extsz=524288 blocks=0, rtextents=0
-        """.strip()))
+        xfs = xfs_info.XFSInfo(context_wrap("""
+meta-data=/dev/sda      isize=256    agcount=32, agsize=16777184 blks
+         =              sectsz=512   attr=2
+data     =              bsize=4096   blocks=536869888, imaxpct=5
+         =              sunit=32     swidth=128 blks
+naming   =version 2     bsize=4096
+log      =internal      bsize=4096   blocks=32768, version=2
+         =              sectsz=512   sunit=32 blks, lazy-count=1
+realtime =none          extsz=524288 blocks=0, rtextents=0
+        """.strip())).xfs_info
 
         # Section checks
-        assert 'meta-data' in xfs_info
-        assert 'data' in xfs_info
-        assert 'naming' in xfs_info
-        assert 'log' in xfs_info
-        assert 'realtime' in xfs_info
+        assert 'meta-data' in xfs
+        assert 'data' in xfs
+        assert 'naming' in xfs
+        assert 'log' in xfs
+        assert 'realtime' in xfs
 
         # Section specifier checks
-        assert xfs_info['meta-data']['specifier'] == '/dev/sda'
-        assert xfs_info['naming']['specifier'] == 'version'
-        assert xfs_info['naming']['specifier value'] == '2'
-        assert xfs_info['log']['specifier'] == 'internal'
-        assert xfs_info['realtime']['specifier'] == 'none'
+        assert xfs['meta-data']['specifier'] == '/dev/sda'
+        assert xfs['naming']['specifier'] == 'version'
+        assert xfs['naming']['specifier value'] == '2'
+        assert xfs['log']['specifier'] == 'internal'
+        assert xfs['realtime']['specifier'] == 'none'
 
         # Data checks
-        assert xfs_info['meta-data']['isize'] == 256
-        assert xfs_info['meta-data']['agcount'] == 32
-        assert xfs_info['meta-data']['agsize'] == '16777184 blks'
-        assert xfs_info['meta-data']['sectsz'] == 512
-        assert xfs_info['meta-data']['attr'] == 2
+        assert xfs['meta-data']['isize'] == 256
+        assert xfs['meta-data']['agcount'] == 32
+        assert xfs['meta-data']['agsize'] == '16777184 blks'
+        assert xfs['meta-data']['sectsz'] == 512
+        assert xfs['meta-data']['attr'] == 2
 
-        assert xfs_info['data']['bsize'] == 4096
-        assert xfs_info['data']['blocks'] == 536869888
-        assert xfs_info['data']['imaxpct'] == 5
-        assert xfs_info['data']['sunit'] == 32
-        assert xfs_info['data']['swidth'] == '128 blks'
+        assert xfs['data']['bsize'] == 4096
+        assert xfs['data']['blocks'] == 536869888
+        assert xfs['data']['imaxpct'] == 5
+        assert xfs['data']['sunit'] == 32
+        assert xfs['data']['swidth'] == '128 blks'
         
 
-        assert xfs_info['naming']['bsize'] == 4096
+        assert xfs['naming']['bsize'] == 4096
 
-        assert xfs_info['log']['bsize'] == 4096
-        assert xfs_info['log']['blocks'] == 32768
-        assert xfs_info['log']['version'] == 2
-        assert xfs_info['log']['sectsz'] == 512
-        assert xfs_info['log']['sunit'] == '32 blks'
-        assert xfs_info['log']['lazy-count'] == 1
+        assert xfs['log']['bsize'] == 4096
+        assert xfs['log']['blocks'] == 32768
+        assert xfs['log']['version'] == 2
+        assert xfs['log']['sectsz'] == 512
+        assert xfs['log']['sunit'] == '32 blks'
+        assert xfs['log']['lazy-count'] == 1
 
-        assert xfs_info['realtime']['extsz'] == 524288
-        assert xfs_info['realtime']['blocks'] == 0
-        assert xfs_info['realtime']['rtextents'] == 0
+        assert xfs['realtime']['extsz'] == 524288
+        assert xfs['realtime']['blocks'] == 0
+        assert xfs['realtime']['rtextents'] == 0
