78fba4904912dd0957d48137a16121e555e4d217
Paul Wayper <paulway@redhat.com>
2016-10-13 15:38:41 +1100

    Got flake8 tests working locally, finally

--- falafel/mappers/smartctl.py
+++ falafel/mappers/smartctl.py
@@ -3,6 +3,7 @@ from falafel.core.plugins import mapper
 
 import re
 
+
 @mapper('smartctl')
 class SMARTctl(Mapper):
     """
@@ -17,17 +18,20 @@ class SMARTctl(Mapper):
         deal with file changes within this process.
     """
 
-    INFO_LINE_RE =  re.compile(r'(?P<key>\w+(?:\s\w+)*):\s+' +
-     r'(?P<value>\S.*?)\s*$')
-    VALUE_LINE_RE = re.compile(r'(?P<key>\w[A-Za-z _.-]+):\s+' +
-     r'\(\s*(?P<value>\S.*?)\)')
-    ATTR_LINE_RE =  re.compile(r'^\s*(?P<id>\d+)\s(?P<name>\w+)\s+' +
-     r'(?P<flag>0x[0-9a-fA-F]{4})\s+(?P<value>\d{3})\s+' +
-     r'(?P<worst>\d{3})\s+(?P<threshold>\d{3})\s+' +
-     r'(?P<type>[A-Za-z_-]+)\s+(?P<updated>[A-Za-z_-]+)\s+' +
-     r'(?P<when_failed>\S+)\s+(?P<raw_value>\S.*)$')
+    INFO_LINE_STR = r'(?P<key>\w+(?:\s\w+)*):\s+' + \
+        r'(?P<value>\S.*?)\s*$'
+    INFO_LINE_RE = re.compile(INFO_LINE_STR)
+    VALUE_LINE_STR = r'(?P<key>\w[A-Za-z _.-]+):\s+' + \
+        r'\(\s*(?P<value>\S.*?)\)'
+    VALUE_LINE_RE = re.compile(VALUE_LINE_STR)
+    ATTR_LINE_STR = r'^\s*(?P<id>\d+)\s(?P<name>\w+)\s+' + \
+        r'(?P<flag>0x[0-9a-fA-F]{4})\s+(?P<value>\d{3})\s+' + \
+        r'(?P<worst>\d{3})\s+(?P<threshold>\d{3})\s+' + \
+        r'(?P<type>[A-Za-z_-]+)\s+(?P<updated>[A-Za-z_-]+)\s+' + \
+        r'(?P<when_failed>\S+)\s+(?P<raw_value>\S.*)$'
+    ATTR_LINE_RE = re.compile(ATTR_LINE_STR)
     ATTR_KEYS = ['id', 'flag', 'value', 'worst', 'threshold', 'type',
-     'updated', 'when_failed', 'raw_value']
+                 'updated', 'when_failed', 'raw_value']
 
     def __init__(self, context):
         super(SMARTctl, self).__init__(context)
@@ -38,20 +42,19 @@ class SMARTctl(Mapper):
             self.device = match.group('device')
         # Else warn?  fail?
 
-
     def parse_content(self, content):
         drive_info = {
-            'info'          : {},
-            'health'        : 'not parsed',
-            'values'        : {},
-            'attributes'    : {},
-            'full_line'     : '', # hack for persistent line storage
+            'info':         {},
+            'health':       'not parsed',
+            'values':       {},
+            'attributes':   {},
+            'full_line':    '',  # hack for persistent line storage
         }
 
         # Parsing using a state machine, sorry.  We use a state variable, and
-        # functions to parse lines in each of the different states.  The function
-        # returns the state as a result of reading that line, and we look up the
-        # parse function out of an array based on the parse state.
+        # functions to parse lines in each of the different states.  The
+        # function returns the state as a result of reading that line, and we
+        # look up the parse function out of an array based on the parse state.
         PARSE_FORMATTED_INFO = 0
         PARSE_FREEFORM_INFO = 1
         PARSE_ATTRIBUTE_INFO = 2
@@ -68,16 +71,17 @@ class SMARTctl(Mapper):
                 drive_info['info'][match.group('key')] = match.group('value')
             else:
                 # Translate some of the less structured information
-                if   line == 'Device does not support SMART':
+                if line == 'Device does not support SMART':
                     drive_info['info']['SMART support is'] = 'Not supported'
                 elif line == 'Device supports SMART and is Enabled':
                     drive_info['info']['SMART support is'] = 'Enabled'
                 elif line == 'Error Counter logging not supported':
-                    drive_info['info']['Error Counter logging'] = 'Not supported'
+                    drive_info['info']['Error Counter logging'] = \
+                        'Not supported'
                 elif line == 'Device does not support Self Test logging':
                     drive_info['info']['Self Test logging'] = 'Not supported'
                 elif line == 'Temperature Warning Disabled or Not Supported':
-                    drive_info['info']['Temperature Warning'] =\
+                    drive_info['info']['Temperature Warning'] = \
                         'Disabled or Not Supported'
             return PARSE_FORMATTED_INFO
 
@@ -85,17 +89,17 @@ class SMARTctl(Mapper):
         def parse_values(line):
             if line.startswith('=== START OF READ SMART DATA SECTION ==='):
                 return PARSE_FREEFORM_INFO
-            if line.startswith('Vendor Specific SMART Attributes with Thresholds'):
+            if line.startswith('Vendor Specific SMART Attributes with Thres'):
                 return PARSE_ATTRIBUTE_INFO
-            if line.startswith('SMART overall-health self-assessment test result'):
+            if line.startswith('SMART overall-health self-assessment test r'):
                 drive_info['health'] = ''.join((line.split(': '))[1:])
                 return PARSE_FREEFORM_INFO
             # Values section begins with this - ignore:
             if line.startswith('General SMART Values:'):
                 return PARSE_FREEFORM_INFO
 
-            # Lines starting with a space are continuations of the commentary on
-            # the previous setting - ignore
+            # Lines starting with a space are continuations of the commentary
+            # on the previous setting - ignore
             if len(line) == 0 or line[0] == ' ' or line[0] == "\t":
                 return PARSE_FREEFORM_INFO
             # Otherwise, join this line to the full line
@@ -116,15 +120,15 @@ class SMARTctl(Mapper):
         def parse_attributes(line):
             if line.startswith('SMART Error Log Version:'):
                 return PARSE_COMPLETE
-            if line.startswith('Vendor Specific SMART Attributes with Thresholds'):
+            if line.startswith('Vendor Specific SMART Attributes with Thres'):
                 return PARSE_ATTRIBUTE_INFO
             if len(line) == 0:
                 return PARSE_ATTRIBUTE_INFO
             match = self.ATTR_LINE_RE.match(line)
             if match:
                 name = match.group('name')
-                drive_info['attributes'][name] = {key: match.group(key)
-                    for key in self.ATTR_KEYS}
+                drive_info['attributes'][name] = {
+                    key: match.group(key) for key in self.ATTR_KEYS}
             return PARSE_ATTRIBUTE_INFO
 
         parse_for_state = [
