c80ca64bb275275dcfc575d815bcdc8d5dad14f7
Paul Wayper <paulway@redhat.com>
2016-10-13 16:08:42 +1100

    Change to a LogFileOutput class mapper

--- falafel/mappers/rhn_server_xmlrpc.py
+++ falafel/mappers/rhn_server_xmlrpc.py
@@ -1,5 +1,5 @@
 from falafel.core.plugins import mapper
-from falafel.core import MapperOutput, computed
+from falafel.core import LogFileOutput
 
 from datetime import datetime
 import re
@@ -8,32 +8,40 @@ import re
 2016/06/21 14:01:07 +01:00 29079 172.16.41.79: rhnServer/server_certificate.valid('Server id ID-1000014665 not found in database',)
 """
 
-line_re = re.compile(r"^(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2} [+-]?\d{2}:\d{2}) (?P<pid>\d+) (?P<client_ip>\S+): (?P<module>\w+)/(?P<function>[\w.-]+)(?:\((?:(?P<client_id>\d+), ?)?(?P<args>.*?),?\))?$")
-GROUPS =('timestamp', 'pid', 'client_ip', 'module', 'function', 'client_id', 'args')
 
-def parse_line(line):
-    """
-        Parse a log line using the XMLRPC regular expression into a dict.  
-        All data will be in fields, and the raw log line is stored in
-        'raw_log'.
-    """
-    msg_info = dict()
-    msg_info['raw_log'] = line
+@mapper('rhn_server_xmlrpc.log')
+class ServerXMLRPCLog(LogFileOutput):
 
-    match = line_re.search(line)
-    if match:
-        for group in GROUPS:
-            msg_info[group] = match.group(group)
-        try:
-            stamp = match.group('timestamp')
-            # Must remove : from timezone for strptime %z
-            msg_info['timestamp'] = datetime.strptime(stamp[0:23] + stamp[24:26], "%Y/%m/%d %H:%M:%S %z")
-        except:
-            pass
+    LINE_STR = r"^(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2} " + \
+        r"[+-]?\d{2}:\d{2}) (?P<pid>\d+) (?P<client_ip>\S+): " + \
+        r"(?P<module>\w+)/(?P<function>[\w.-]+)" + \
+        r"(?:\((?:(?P<client_id>\d+), ?)?(?P<args>.*?),?\))?$"
+    LINE_RE = re.compile(LINE_STR)
+    GROUPS = ('timestamp', 'pid', 'client_ip', 'module', 'function',
+              'client_id', 'args')
 
-    return msg_info
+    def parse_line(self, line):
+        """
+            Parse a log line using the XMLRPC regular expression into a dict.
+            All data will be in fields, and the raw log line is stored in
+            'raw_log'.
+        """
+        msg_info = dict()
+        msg_info['raw_log'] = line
 
-class LogLineList(MapperOutput):
+        match = self.LINE_RE.search(line)
+        if match:
+            for group in self.GROUPS:
+                msg_info[group] = match.group(group)
+            try:
+                stamp = match.group('timestamp')
+                # Must remove : from timezone for strptime %z
+                msg_info['timestamp'] = datetime.strptime(
+                    stamp[0:23] + stamp[24:26], "%Y/%m/%d %H:%M:%S %z")
+            except:
+                pass
+
+        return msg_info
 
     def __contains__(self, s):
         """
@@ -45,9 +53,8 @@ class LogLineList(MapperOutput):
         """
         Returns all lines that contain 's' and wrap them in a list
         """
-        return [parse_line(l) for l in self.data if s in l]
+        return [self.parse_line(l) for l in self.data if s in l]
 
-    @computed
     def last(self):
         """
         Returns the last complete log line
@@ -56,36 +63,9 @@ class LogLineList(MapperOutput):
         msg_info = dict()
         # Only check the last 2 lines, in that order
         for l in reversed(self.data[-2:]):
-            msg_info = parse_line(l)
+            msg_info = self.parse_line(l)
             # assume parse is successful if we got an IP address
             if msg_info['client_ip']:
                 return msg_info
         # Return the last one even if it didn't parse.
         return msg_info
-
-
-@mapper('rhn_server_xmlrpc.log')
-def server_xmlrpc_log(context):
-    """
-    Returns an object in type of LogLineList which provide 3 APIs:
-    - Usage:
-      in:
-        log = shared.get(taskomatic_daemon_log)
-        if "Abort command issued" in log:
-            ...
-      get:
-        err_lines = log.get('Wrapper')
-        for line in err_lines:
-            assert line.get('pid') == 3064
-            assert line.get('client_ip') == '10.20.30.40'
-            assert line.get('module') == 'rhnServer'
-            assert line.get('function') == 'server_certificate.valid'
-            assert line.get('raw_log') ..
-            ...
-     last:
-        last_line_stat = log.last.get('timestamp')
-
-    -----------
-
-    """
-    return LogLineList(context.content)
