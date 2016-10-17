e48f3281cab5a5d2f09bc0f9b6c8f346e0b93eef
Paul Wayper <paulway@redhat.com>
2016-10-17 12:05:22 +1100

    Fixing flake8 space-before-class issues

--- falafel/mappers/cinder_conf.py
+++ falafel/mappers/cinder_conf.py
@@ -1,5 +1,6 @@
 from .. import mapper, IniConfigFile
 
+
 @mapper("cinder.conf")
 class CinderConf(IniConfigFile):
     pass
--- falafel/mappers/nova_conf.py
+++ falafel/mappers/nova_conf.py
@@ -1,5 +1,6 @@
 from .. import mapper, IniConfigFile
 
+
 @mapper("nova.conf")
 class NovaConf(IniConfigFile):
     pass
