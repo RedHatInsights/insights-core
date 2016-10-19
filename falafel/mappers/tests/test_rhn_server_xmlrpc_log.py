f766cb018b58aa592464b126165d6844ed96e637
Paul Wayper <paulway@redhat.com>
2016-10-20 09:36:01 +1100

    Check for capture of IPv6 host addresses

--- falafel/mappers/tests/test_rhn_server_xmlrpc_log.py
+++ falafel/mappers/tests/test_rhn_server_xmlrpc_log.py
@@ -10,13 +10,11 @@ LOG_DATA = """
 2016/04/11 10:49:12 -04:00 11601 192.168.18.163: xmlrpc/registration.register_osad
 2016/04/11 10:49:17 -04:00 11599 192.168.18.163: xmlrpc/registration.register_osad_jid
 2016/04/11 12:57:26 -04:00 11594 192.168.18.28: rhnServer/server_certificate.valid('Server id ID-1000010124 not found in database',)
+2016/07/27 04:44:41 -04:00 25923 2620:10a:0:4::40: xmlrpc/queue.get(1000014812, 2, 'checkins enabled')
 """
-
 
 def test_log_data():
     log = rhn_server_xmlrpc.ServerXMLRPCLog(context_wrap(LOG_DATA))
-
-    print log
 
     # Check the first log line for all fields
     line = log.get('10.4.4.17: xmlrpc/registration.welcome_message')[0]
@@ -43,3 +41,8 @@ def test_log_data():
     line = log.get('__add_hw_profile_no_auth')[0]
     assert line['client_id'] == '1000010125'
     assert line['args'] == "'items: 6'"
+
+    # Check that we can get IPv6 addresses correctly
+    line = log.get('checkins enabled')[0]
+    assert line['client_ip'] == '2620:10a:0:4::40'
+
