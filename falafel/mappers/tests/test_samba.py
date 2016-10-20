0174023f508ecdcb83bad339193ca6b6c0aa0ac7
Paul Wayper <paulway@redhat.com>
2016-10-21 08:50:17 +1100

    Fixing some flake8-unfriendly spacing in test file

--- falafel/mappers/tests/test_samba.py
+++ falafel/mappers/tests/test_samba.py
@@ -6,43 +6,42 @@ SAMBA_CONFIG = """
 # smb.conf(5) manual page in order to understand the options listed
 #...
 #======================= Global Settings =====================================
-	
+
 [global]
-	
+
 #...
 # Hosts Allow/Hosts Deny lets you restrict who can connect, and you can
 # specifiy it as a per share option as well
 #
-	workgroup = MYGROUP
-	server string = Samba Server Version %v
-	
-;	netbios name = MYSERVER
-	
-;	interfaces = lo eth0 192.168.12.2/24 192.168.13.2/24 
-;	hosts allow = 127. 192.168.12. 192.168.13.
-	
+    workgroup = MYGROUP
+    server string = Samba Server Version %v
+
+;   netbios name = MYSERVER
+
+;   interfaces = lo eth0 192.168.12.2/24 192.168.13.2/24
+;   hosts allow = 127. 192.168.12. 192.168.13.
+
 # --------------------------- Logging Options -----------------------------
 #
 # Log File let you specify where to put logs and how to split them up.
 #
 # Max Log Size let you specify the max size log files should reach
-	
-	# logs split per machine
-	log file = /var/log/samba/log.%m
-	# max 50KB per log file, then rotate
-	max log size = 50
-	
+
+    # logs split per machine
+    log file = /var/log/samba/log.%m
+    # max 50KB per log file, then rotate
+    max log size = 50
+
 # ----------------------- Standalone Server Options ------------------------
 #
 # Scurity can be set to user, share(deprecated) or server(deprecated)
 #
-# Backend to store user information in. New installations should 
-# use either tdbsam or ldapsam. smbpasswd is available for backwards 
+# Backend to store user information in. New installations should
+# use either tdbsam or ldapsam. smbpasswd is available for backwards
 # compatibility. tdbsam requires no further configuration.
 
-	security = user
-	passdb backend = tdbsam
-
+    security = user
+    passdb backend = tdbsam
 
 #...
 # --------------------------- Printing Options -----------------------------
@@ -56,14 +55,14 @@ SAMBA_CONFIG = """
 # Printcap Name let you specify an alternative printcap file
 #
 # You can choose a non default printing system using the Printing option
-	
-	load printers = yes
-	cups options = raw
 
-;	printcap name = /etc/printcap
-	#obtain list of printers automatically on SystemV
-;	printcap name = lpstat
-;	printing = cups
+    load printers = yes
+    cups options = raw
+
+;   printcap name = /etc/printcap
+    #obtain list of printers automatically on SystemV
+;   printcap name = lpstat
+;   printing = cups
 
 # --------------------------- Filesystem Options ---------------------------
 #
@@ -75,57 +74,58 @@ SAMBA_CONFIG = """
 # Note: these options can also be set just per share, setting them in global
 # makes them the default for all shares
 
-;	map archive = no
-;	map hidden = no
-;	map read only = no
-;	map system = no
-;	store dos attributes = yes
+;   map archive = no
+;   map hidden = no
+;   map read only = no
+;   map system = no
+;   store dos attributes = yes
 
 
 #============================ Share Definitions ==============================
-	
+
 [homes]
-	comment = Home Directories
-	browseable = no
-	writable = yes
-;	valid users = %S
-;	valid users = MYDOMAIN\%S
-	
+    comment = Home Directories
+    browseable = no
+    writable = yes
+;   valid users = %S
+;   valid users = MYDOMAIN\%S
+
 [printers]
-	comment = All Printers
-	path = /var/spool/samba
-	browseable = no
-	guest ok = no
-	writable = no
-	printable = yes
-	
+    comment = All Printers
+    path = /var/spool/samba
+    browseable = no
+    guest ok = no
+    writable = no
+    printable = yes
+
 # Un-comment the following and create the netlogon directory for Domain Logons
-;	[netlogon]
-;	comment = Network Logon Service
-;	path = /var/lib/samba/netlogon
-;	guest ok = yes
-;	writable = no
-;	share modes = no
-	
-	
+;   [netlogon]
+;   comment = Network Logon Service
+;   path = /var/lib/samba/netlogon
+;   guest ok = yes
+;   writable = no
+;   share modes = no
+
+
 # Un-comment the following to provide a specific roving profile share
 # the default is to use the user's home directory
-;	[Profiles]
-;	path = /var/lib/samba/profiles
-;	browseable = no
-;	guest ok = yes
-	
-	
+;   [Profiles]
+;   path = /var/lib/samba/profiles
+;   browseable = no
+;   guest ok = yes
+
+
 # A publicly accessible directory, but read only, except for people in
 # the "staff" group
-;	[public]
-;	comment = Public Stuff
-;	path = /home/samba
-;	public = yes
-;	writable = yes
-;	printable = no
-;	write list = +staff
+;   [public]
+;   comment = Public Stuff
+;   path = /home/samba
+;   public = yes
+;   writable = yes
+;   printable = no
+;   write list = +staff
 """
+
 
 def test_match():
     config = samba.SambaConfig(context_wrap(SAMBA_CONFIG))
