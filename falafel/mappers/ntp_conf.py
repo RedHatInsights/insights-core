797336363bc081a2590c353816039da3a59665d9
Paul Wayper <paulway@redhat.com>
2016-11-11 13:19:02 +1100

    Some keywords can appear without any item - store those as well

--- falafel/mappers/ntp_conf.py
+++ falafel/mappers/ntp_conf.py
@@ -6,16 +6,10 @@ driftfile /var/lib/ntp/drift
 
 ...
 
-<<<<<<< HEAD
 Keyword, optionally followed immediately by values.  Keywords can be repeated
 multiple times - those that are (e.g. server, peer, fudge, restrict) have a
 unique item following the keyword, so we form a dictionary based on that.
 Some keywords have no values - we record these as keys but with no value.
-=======
-Keyword followed immediately by values.  Keywords can be repeated multiple
-times - those that are (e.g. server, peer, fudge, restrict) have a unique
-item following the keyword, so we form a dictionary based on that.
->>>>>>> Adding a mapper to read the NTP configuration file 'ntp.conf'.
 """
 
 from .. import Mapper, mapper, get_active_lines
