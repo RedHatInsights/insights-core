94b006c4329192402ea170042eb298a141d07eb2
Bob Fahr <bfahr@redhat.com>
2016-11-22 19:29:59 +0530

    Revert "Merge branch 'update_mapper_satellite_version' into 'master'"
    This reverts merge request !276

--- falafel/mappers/satellite_version.py
+++ falafel/mappers/satellite_version.py
@@ -1,51 +1,102 @@
-"""
-satellite_version - File /usr/share/foreman/lib/satellite/version.rb
-====================================================================
+from .. import mapper, Mapper, get_active_lines
 
-Module for parsing the content of file ``version.rb`` or ``satellite_version``,
-which is a simple file in foreman-debug or sosreport archives of Satellite 6.x.
-
-Typical content of "satellite_version" is::
-
-    COMMAND> cat /usr/share/foreman/lib/satellite/version.rb
-
-    module Satellite
-      VERSION = "6.1.3"
-    end
-
-Note:
-    This module can only be used for Satellite 6.x
-
-Examples:
-    >>> sat6_ver = shared[SatelliteVersion]
-    >>> sat6_ver.full
-    "6.1.3"
-    >>> sat6_ver.version
-    "6.1.3"
-    >>> sat6_ver.major
-    6
-    >>> sat6_ver.minor
-    1
-    >>> sat6_ver.release
-    None
-
-"""
-from .. import mapper, Mapper
+foreman_sat6_ver_map = {
+    '1.5': '6.0',
+    '1.6.0.53': '6.0.8',
+    '1.7': '6.1',
+    '1.7.2.33': '6.1.1',
+    '1.7.2.36': '6.1.2',
+    '1.7.2.43': '6.1.3',
+    '1.7.2.46': '6.1.4',
+    '1.7.2.49': '6.1.5',
+    '1.7.2.50': '6.1.6',
+    '1.7.2.53': '6.1.7',
+    '1.7.2.55': '6.1.8',
+    '1.7.2.56': '6.1.9',
+    '1.11': '6.2',
+}
 
 
 @mapper('satellite_version.rb')
-class Satellite6Version(Mapper):
-    """ Class for parsing the content of ``satellite_version``."""
+@mapper('installed-rpms')
+class SatelliteVersion(Mapper):
+    """
+    Version of Satellite 5.x and 6.x:
+    - E.g.
+      Satellite 5.x:
+       version:      "5.3.0"
+       version_full: "5.3.0.27-1.el5sat-noarch"
 
+      Satellite 6.x:
+       version:      "6.2.0"
+       version_full: "6.2.0.11-1.el7sat.noarch"
+
+       version:      "6.1"
+       version_full: None
+    ---
+    For Satellite 6.x
+    - https://access.redhat.com/articles/1343683
+    - https://bugzilla.redhat.com/show_bug.cgi?id=1191584
+    1. Check the version information in below files at first:
+       - satellite_version
+       - usr/share/foreman/lib/satellite/version.rb
+    2. Check the version of foreman or satellite-installer, when the files
+       listed in (1) are not found.
+                            Sat 6.0     Sat 6.1     Sat 6.2
+       foreman              1.5.x       1.7.x       1.11.0
+       satellite-installer  -           -           6.2.x
+
+    For Satellite 5.x
+    - https://access.redhat.com/solutions/1224043
+      Return the version of satellite-schema directly
+      > 5.3~5.7: satellite-schema
+      > 5.0~5.2: rhn-satellite-schema
+      Because of satellite-branding is not deployed in Sat 5.0~5.2, and
+      satellite-schema can also be used for checking the version, so here we use
+      satellite-schema instead of satellite-branding
+    """
     def parse_content(self, content):
-        # To keep compatible with shared reducer satellite_version
-        self.full = self.release = None
+        self.version_full = None
         self.version = None
-        for line in content:
-            if line.strip().upper().startswith('VERSION'):
-                self.full = line.split()[-1].strip('"')
-                self.version = self.full
-                break
+        # Spec: satellite_version
+        if 'version' in self.file_path.lower():
+            for line in content:
+                if line.strip().upper().startswith('VERSION'):
+                    self.version = line.split()[-1].strip('"')
+                    self.version_full = self.version
+                    break
+        # Spec: installed-rpms
+        else:
+            sat5_pkg = ('satellite-schema-', 'rhn-satellite-schema-')
+            sat6_pkg = ('foreman-1')  # Add "-1" to check the `foreman` package only
+            # From Sat 6.2, we can also check `satellite-installer` instead of `foreman`
+            sat62_pkg = ('satellite-installer-')
+            for line in get_active_lines(content, comment_char="COMMAND>"):
+                # for 6.x
+                if line.startswith(sat6_pkg):
+                    pkg_ver = line.split('-')[1]
+                    self.version = foreman_sat6_ver_map.get(pkg_ver)
+                    # for `1.5`, `1.7` and `1.11`
+                    if self.version is None:
+                        pkg_mj_ver = '.'.join(pkg_ver.split('.')[:2])
+                        self.version = foreman_sat6_ver_map.get(pkg_mj_ver)
+                    self.version_full = self.version
+                    # for 6.2, need to check `satellite-installer` further
+                    if self.version != '6.2':
+                        break
+                # for 6.2 (if `satellite-installer` is installed)
+                elif line.startswith(sat62_pkg):
+                    # get the version of `satellite-installer`
+                    self.version_full = line.split()[0][len(sat62_pkg):]
+                    self.version = '.'.join(self.version_full.split('.')[:3])
+                    break
+                # for 5.x
+                elif line.startswith(sat5_pkg):
+                    # get the version of `[rhn-]satellite-schema`
+                    pkg = line.split()[0]
+                    self.version_full = pkg[pkg.find('schema-') + 7:]
+                    self.version = '.'.join(self.version_full.split('.')[:3])
+                    break
 
     @property
     def major(self):
--- falafel/mappers/tests/test_satellite_version.py
+++ falafel/mappers/tests/test_satellite_version.py
@@ -1,5 +1,41 @@
 from falafel.tests import context_wrap
-from falafel.mappers.satellite_version import Satellite6Version
+from falafel.mappers.satellite_version import SatelliteVersion
+
+installed_rpms_5 = """
+satellite-branding-5.5.0.22-1.el6sat.noarch                 Wed May 18 14:50:17 2016
+satellite-doc-indexes-5.6.0-2.el6sat.noarch                 Wed May 18 14:47:49 2016
+satellite-repo-5.6.0.3-1.el6sat.noarch                      Wed May 18 14:37:34 2016
+satellite-schema-5.6.0.10-1.el6sat.noarch                   Wed May 18 14:53:03 2016
+satyr-0.16-2.el6.x86_64                                     Wed May 18 14:16:08 2016
+scdb-1.15.8-1.el6sat.noarch                                 Wed May 18 14:48:14 2016
+scl-utils-20120927-27.el6_6.x86_64                          Wed May 18 14:18:16 2016
+SDL-1.2.14-6.el6.x86_64                                     Wed May 18 14:16:25 2016
+"""
+
+installed_rpms_61 = """
+foreman-1.7.2.53-1.el7sat.noarch                            Wed May 18 14:16:25 2016
+scl-utils-20120927-27.el6_6.x86_64                          Wed May 18 14:18:16 2016
+SDL-1.2.14-6.el6.x86_64                                     Wed May 18 14:16:25 2016
+"""
+
+installed_rpms_62 = """
+foreman-1.11.0.53-1.el7sat.noarch                           Wed May 18 14:16:25 2016
+scl-utils-20120927-27.el7_6.x86_64                          Wed May 18 14:18:16 2016
+SDL-1.2.14-6.el7.x86_64                                     Wed May 18 14:16:25 2016
+satellite-installer-6.2.0.11-1.el7sat.noarch                Wed May 18 14:16:25 2016
+"""
+
+installed_rpms_62_1 = """
+foreman-1.11.0.53-1.el7sat.noarch                           Wed May 18 14:16:25 2016
+scl-utils-20120927-27.el7_6.x86_64                          Wed May 18 14:18:16 2016
+SDL-1.2.14-6.el7.x86_64                                     Wed May 18 14:16:25 2016
+"""
+
+installed_rpms_60 = """
+foreman-1.6.0.53-1.el6sat.noarch                            Wed May 18 14:16:25 2016
+scl-utils-20120927-27.el6_6.x86_64                          Wed May 18 14:18:16 2016
+SDL-1.2.14-6.el6.x86_64                                     Wed May 18 14:16:25 2016
+"""
 
 satellite_version = """
 COMMAND> cat /usr/share/foreman/lib/satellite/version.rb
@@ -16,18 +52,49 @@ SDL-1.2.14-6.el6.x86_64                                     Wed May 18 14:16:25 
 """
 
 
+def test_get_sat5_version():
+    result = SatelliteVersion(context_wrap(installed_rpms_5, path=''))
+    assert result.version_full == "5.6.0.10-1.el6sat.noarch"
+    assert result.version == "5.6.0"
+    assert result.major == 5
+    assert result.minor == 6
+
+
 def test_get_sat6_version():
-    result = Satellite6Version(context_wrap(satellite_version, path='satellite_version'))
-    assert result.full == "6.1.3"
+    result = SatelliteVersion(context_wrap(satellite_version, path='satellite_version'))
+    assert result.version_full == "6.1.3"
     assert result.version == "6.1.3"
+
+    result = SatelliteVersion(context_wrap(installed_rpms_60, path=''))
+    assert result.version_full == "6.0.8"
+    assert result.version == "6.0.8"
+
+    result = SatelliteVersion(context_wrap(installed_rpms_61, path=''))
+    assert result.version_full == "6.1.7"
+    assert result.version == "6.1.7"
+
+    result = SatelliteVersion(context_wrap(installed_rpms_62, path='satellite'))
+    assert result.version_full == "6.2.0.11-1.el7sat.noarch"
+    assert result.version == "6.2.0"
     assert result.major == 6
-    assert result.minor == 1
-    assert result.release is None
+    assert result.minor == 2
+
+    result = SatelliteVersion(context_wrap(installed_rpms_62_1, path='satellite'))
+    assert result.version_full == "6.2"
+    assert result.version == "6.2"
+    assert result.major == 6
+    assert result.minor == 2
 
 
 def test_get_no_sat_version():
-    result = Satellite6Version(context_wrap(no_sat, path='satellite_version'))
+    result = SatelliteVersion(context_wrap(no_sat, path='satellite_version'))
     assert result.version is None
-    assert result.full is None
+    assert result.version_full is None
+    assert result.major is None
+    assert result.minor is None
+
+    result = SatelliteVersion(context_wrap(satellite_version, path='satellite'))
+    assert result.version is None
+    assert result.version_full is None
     assert result.major is None
     assert result.minor is None
