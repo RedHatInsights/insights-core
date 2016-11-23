27eeca78d7c829dfaafbf1967898026228e6cdd9
Bob Fahr <bfahr@redhat.com>
2016-11-23 13:56:51 -0600

    Convert ls_etc shared mapper to class
    * Replased exising function mapper with class mapper LsEtc
    * Updated tests
    * Added documentation

--- falafel/mappers/tests/test_ls_etc.py
+++ falafel/mappers/tests/test_ls_etc.py
@@ -1,4 +1,4 @@
-from falafel.mappers.ls_etc import parse_ls_etc
+from falafel.mappers.ls_etc import LsEtc
 from falafel.tests import context_wrap
 
 
@@ -24,10 +24,25 @@ lrwxrwxrwx.  1 0 0   15 Jul  6 23:32 S97rhnsd -> ../init.d/rhnsd
 
 
 def test_ls_etc():
-    ls_etc = parse_ls_etc(context_wrap(LS_ETC))
-    assert len(ls_etc.get("/etc/sysconfig").get('files')) == 3
-    assert ls_etc.get("/etc/sysconfig").get('files') == ['ebtables-config',
-                                                         'firewalld', 'grub']
-    assert ls_etc.get("/etc/sysconfig").get('directories') == ['cbq', 'console']
-    assert ls_etc.get("/etc/rc.d/rc3.d").get('files') == ['K50netconsole',
-                                                    'S10network', 'S97rhnsd']
+    ls_etc = LsEtc(context_wrap(LS_ETC))
+    assert "/etc/sysconfig" in ls_etc
+    assert len(ls_etc.files_of("/etc/sysconfig")) == 3
+    assert ls_etc.files_of("/etc/sysconfig") == ['ebtables-config', 'firewalld', 'grub']
+    assert ls_etc.dirs_of("/etc/sysconfig") == ['.', '..', 'cbq', 'console']
+    assert ls_etc.specials_of("/etc/sysconfig") == []
+    assert ls_etc.total_of("/etc/sysconfig") == 96
+    grub = ls_etc.dir_entry("/etc/sysconfig", "grub")
+    assert grub is not None
+    assert grub == {
+        'group': '0',
+        'name': 'grub',
+        'links': 1,
+        'perms': 'rwxrwxrwx.',
+        'raw_entry': 'lrwxrwxrwx.  1 0 0   17 Jul  6 23:32 grub -> /etc/default/grub',
+        'owner': '0',
+        'link': '/etc/default/grub',
+        'date': 'Jul  6 23:32',
+        'type': 'l',
+        'size': 17}
+    assert ls_etc.files_of("/etc/rc.d/rc3.d") == ['K50netconsole',
+                                                  'S10network', 'S97rhnsd']
