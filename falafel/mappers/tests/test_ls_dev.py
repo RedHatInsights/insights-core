954ef9331b543f8fc4b8c2b70fbb7a38644769b9
Yi Zhang <yizha@redhat.com>
2016-11-25 17:06:51 +0800

    Convert ls_dev shared mapper to class
    * Replased exising function mapper with class mapper LsDev
    * Updated tests
    * Added documentation

--- falafel/mappers/tests/test_ls_dev.py
+++ falafel/mappers/tests/test_ls_dev.py
@@ -1,4 +1,4 @@
-from falafel.mappers.ls_dev import parse_ls_dev
+from falafel.mappers.ls_dev import LsDev
 from falafel.tests import context_wrap
 
 LS_DEV = """
@@ -9,8 +9,9 @@ brw-rw----.  1 0  6 253,   1 Aug  4 16:56 dm-1
 brw-rw----.  1 0  6 253,  10 Aug  4 16:56 dm-10
 crw-rw-rw-.  1 0  5   5,   2 Aug  5  2016 ptmx
 drwxr-xr-x.  2 0  0        0 Aug  4 16:56 pts
+lrwxrwxrwx.  1 0  0       25 Oct 25 14:48 initctl -> /run/systemd/initctl/fifo
 
-/dev/fedora:
+/dev/rhel:
 total 0
 drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .
 drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..
@@ -24,15 +25,33 @@ drwxr-xr-x.  2 0 0     140 Jul 25 10:00 .
 drwxr-xr-x. 23 0 0    3720 Jul 25 12:43 ..
 crw-------.  1 0 0 10, 236 Jul 25 10:00 control
 lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 docker-253:0-1443032-pool -> ../dm-3
-lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 fedora-home -> ../dm-2
-lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 fedora-root -> ../dm-0
-lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 fedora-swap -> ../dm-1
+lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 rhel-home -> ../dm-2
+lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 rhel-root -> ../dm-0
+lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 rhel-swap -> ../dm-1
 """
 
 
 def test_ls_dev():
-    ls_dev = parse_ls_dev(context_wrap(LS_DEV))
-    assert len(ls_dev.get("/dev/mapper")) == 4
-    assert ls_dev.get("/dev/fedora") == ['home', 'root', 'swap']
-    assert ls_dev.get("/dev/mapper") == ['docker-253:0-1443032-pool', 'fedora-home', 'fedora-root', 'fedora-swap']
-    assert ls_dev.get("/dev") == ['dm-0', 'dm-1', 'dm-10']
+    ls_dev = LsDev(context_wrap(LS_DEV))
+    assert ls_dev.listing_of("/dev/rhel") == {
+        'home': {'group': '0', 'name': 'home', 'links': 1, 'perms': 'rwxrwxrwx.',
+                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2', 'owner': '0', 'link': '../dm-2',
+                 'date': 'Jul 25 10:00', 'type': 'l', 'size': 7},
+        'root': {'group': '0', 'name': 'root', 'links': 1, 'perms': 'rwxrwxrwx.',
+                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 root -> ../dm-0', 'owner': '0', 'link': '../dm-0',
+                 'date': 'Jul 25 10:00', 'type': 'l', 'size': 7},
+        'swap': {'group': '0', 'name': 'swap', 'links': 1, 'perms': 'rwxrwxrwx.',
+                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 swap -> ../dm-1', 'owner': '0', 'link': '../dm-1',
+                 'date': 'Jul 25 10:00', 'type': 'l', 'size': 7},
+        '..': {'group': '0', 'name': '..', 'links': 23, 'perms': 'rwxr-xr-x.',
+               'raw_entry': 'drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..', 'owner': '0', 'date': 'Jul 25 12:43',
+               'type': 'd',
+               'size': 3720},
+        '.': {'group': '0', 'name': '.', 'links': 2, 'perms': 'rwxr-xr-x.',
+              'raw_entry': 'drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .', 'owner': '0',
+              'date': 'Jul 25 10:00', 'type': 'd', 'size': 100}}
+    assert ls_dev.listings.get("/dev/mapper")['files'] == ['control', 'docker-253:0-1443032-pool', 'rhel-home',
+
+                                                           'rhel-root', 'rhel-swap']
+
+    assert ls_dev.listings.get("/dev/mapper")['entries']['rhel-home']['link'] == "../dm-2"
