c5cd6156d621a241e02ac7db07c8e255541961da
Bob Fahr <bfahr@redhat.com>
2016-12-15 16:06:59 -0600

    Add new shared mapper for neutron conf file
    * Add new shared mapper module neutron_conf to parse neurton.conf file.
    * Add tests and documentation.

---
+++ falafel/mappers/neutron_conf.py
@@ -0,0 +1,33 @@
+"""
+neutron conf - File /etc/neutron/neutron.conf
+=============================================
+This class provides parsing for the file ``/etc/neutron/neutron.conf``.
+
+Sample input data is in the format::
+
+    [DEFAULT]
+    # debug = False
+    debug = False
+    # verbose = True
+    verbose = False
+    core_plugin =neutron.plugins.openvswitch.ovs_neutron_plugin.OVSNeutronPluginV2
+
+    [quotas]
+    default_quota = -1
+    quota_network = 10
+    [agent]
+    report_interval = 60
+
+    [keystone_authtoken]
+    auth_host = ost-controller-lb-del.om-l.dsn.inet
+    auth_port = 35357
+
+See the ``IniConfigFile`` class for examples.
+"""
+from .. import IniConfigFile, mapper
+
+
+@mapper('neutron.conf')
+class NeutronConf(IniConfigFile):
+    """Class to parse file ``neutron.conf``."""
+    pass
