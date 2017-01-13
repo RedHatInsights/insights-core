f679558c74d0ff4e91a9802835683c705ce0b1f5
Bob Fahr <bfahr@redhat.com>
2017-01-13 13:54:07 -0600

    Convert nproc and limits mappers to class
    * Converted existing shared mapper function to classes for each of the
      two specs and removed existing mapper.
    * Revised tests and added documentation.

--- falafel/mappers/nproc.py
+++ falafel/mappers/nproc.py
@@ -1,42 +1,53 @@
-import os
-from .. import mapper
+"""
+limits : Files ``limits.conf`` and ``*-nproc.conf``
+===================================================
+Parses lines from ``/etc/security/limits.conf`` and from
+``/etc/security/limits.d/*-nproc.conf``.  Contents of
+all files are the same but the path of each file is
+different.  Each file is parsed and any lines containing
+the string `nproc` are saved.  The inputs/outputs for
+each type of file are the same.
+
+Sample input data looks like::
+
+    #oracle       hard    nproc   1024
+    #oracle       hard    nproc   2024
+    *             -       nproc   2048
+    oracle        soft    nproc   4096# this is soft
+    oracle        hard    nproc   65536
+    user          -       nproc   12345
+
+Examples:
+    >>> conf = shared[LimitsConf]
+    >>> conf.file_name
+    'limits.conf'
+    >>> conf.data
+    [['*', '-', 'nproc', '2048'],
+     ['oracle', 'soft', 'nproc', '4096'],
+     ['oracle', 'hard', 'nproc', '65536'],
+     ['user', '-', 'nproc', '12345']
+    ]
+"""
+from .. import Mapper, mapper, get_active_lines
+
+
+def _parse_content(content):
+    data = []
+    for line in get_active_lines(content):
+        if 'nproc' in line:
+            data.append(line.split())
+    return data
 
 
 @mapper("limits.conf")
-@mapper("nproc.conf")
-def get_nproc(context):
-    """
-    - Get nproc lines from limits.conf and *.-nproc.conf
-    - Returns any nproc lines from the contents of the passed in mapper file. Given the
-      'etc/security/limits.conf' as an example:
-      --- INPUT ---
-      #oracle       hard    nproc   1024
-       #oracle       hard    nproc   2024
-      *             -       nproc   2048
-      oracle        soft    nproc   4096# this is soft
-      oracle        hard    nproc   65536
-      user          -       nproc   12345
+class LimitsConf(Mapper):
+    """Parse ``limits.conf`` file contents."""
+    def parse_content(self, content):
+        self.data = _parse_content(content)
 
-      --- OUTPUT ---
-      {'limits.conf':
-          [
-              ['*', '-', 'nproc', '2048'],
-              ['oracle', 'soft', 'nproc', '4096'],
-              ['oracle', 'hard', 'nproc', '65536'],
-              ['user', '-', 'nproc', '12345']
-          ]
-      }
-    """
-    result = {}
-    cfg_file = os.path.basename(context.path)
-    if cfg_file.strip():
-        nproc_lines = []
-        for line in context.content:
-            line = line.strip()
-            if line.startswith('#'):
-                continue
-            if line and 'nproc' in line:
-                line = line.split('#')[0].split()
-                nproc_lines.append(line)
-        result[cfg_file] = nproc_lines
-    return result
+
+@mapper("nproc.conf")
+class NprocConf(Mapper):
+    """Parse ``*-nproc.conf`` file contents."""
+    def parse_content(self, content):
+        self.data = _parse_content(content)
--- falafel/mappers/tests/test_nproc.py
+++ falafel/mappers/tests/test_nproc.py
@@ -1,6 +1,4 @@
-import unittest
-
-from falafel.mappers import nproc
+from falafel.mappers.nproc import LimitsConf, NprocConf
 from falafel.tests import context_wrap
 
 LIMITS_CONF_1 = """
@@ -38,40 +36,45 @@ NPROC_CONF_PATH = "etc/security/limits.d/90-nproc.conf"
 FAKE_PATH = "etc/security/limits.d/80-custom.conf"
 
 
-class TestNproc(unittest.TestCase):
-    def test_limits_conf_1(self):
-        data = nproc.get_nproc(context_wrap(LIMITS_CONF_1, path=LIMITS_CONF_PATH))
-        assert len(data) == 1
-        assert len(data.get('limits.conf')) == 4
-        assert data.get('limits.conf') == [['*', '-', 'nproc', '2048'],
-                                           ['oracle', 'soft', 'nproc', '4096'],
-                                           ['oracle', 'hard', 'nproc', '65536'],
-                                           ['user', '-', 'nproc', '12345']]
+def test_limits_conf_1():
+    conf = LimitsConf(context_wrap(LIMITS_CONF_1, path=LIMITS_CONF_PATH))
+    assert len(conf.data) == 4
+    assert conf.file_name == 'limits.conf'
+    assert conf.data == [
+        ['*', '-', 'nproc', '2048'],
+        ['oracle', 'soft', 'nproc', '4096'],
+        ['oracle', 'hard', 'nproc', '65536'],
+        ['user', '-', 'nproc', '12345']
+    ]
 
-    def test_limits_conf_2(self):
-        data = nproc.get_nproc(context_wrap(LIMITS_CONF_2, path=LIMITS_CONF_PATH))
-        assert len(data) == 1
-        assert len(data.get('limits.conf')) == 0
-        assert data.get('limits.conf') == []
 
-    def test_nproc_conf_1(self):
-        data = nproc.get_nproc(context_wrap(NPROC_CONF_1, path=NPROC_CONF_PATH))
-        assert len(data) == 1
-        assert len(data.get('90-nproc.conf')) == 6
-        assert data.get('90-nproc.conf') == [['*', 'soft', 'nproc', '1024'],
-                                             ['root', 'soft', 'nproc', 'unlimited'],
-                                             ['fred', 'hard', 'nproc', '12345'],
-                                             ['fred', 'soft', 'nproc', '12345'],
-                                             ['fred', 'hard', 'nproc', '3388'],
-                                             ['oracle', '-', 'nproc', '4096']]
+def test_limits_conf_2():
+    conf = LimitsConf(context_wrap(LIMITS_CONF_2, path=LIMITS_CONF_PATH))
+    assert conf.file_name == 'limits.conf'
+    assert conf.data == []
 
-    def test_nproc_conf_2(self):
-        data = nproc.get_nproc(context_wrap(NPROC_CONF_2, path=NPROC_CONF_PATH))
-        assert len(data) == 1
-        assert len(data.get('90-nproc.conf')) == 0
-        assert data.get('90-nproc.conf') == []
 
-    def test_nproc_conf_3(self):
-        data = nproc.get_nproc(context_wrap(NPROC_CONF_2, path=FAKE_PATH))
-        assert len(data) == 1
-        assert data == {'80-custom.conf': []}
+def test_nproc_conf_1():
+    conf = NprocConf(context_wrap(NPROC_CONF_1, path=NPROC_CONF_PATH))
+    assert len(conf.data) == 6
+    assert conf.file_name == '90-nproc.conf'
+    assert conf.data == [
+        ['*', 'soft', 'nproc', '1024'],
+        ['root', 'soft', 'nproc', 'unlimited'],
+        ['fred', 'hard', 'nproc', '12345'],
+        ['fred', 'soft', 'nproc', '12345'],
+        ['fred', 'hard', 'nproc', '3388'],
+        ['oracle', '-', 'nproc', '4096']
+    ]
+
+
+def test_nproc_conf_2():
+    conf = NprocConf(context_wrap(NPROC_CONF_2, path=NPROC_CONF_PATH))
+    assert conf.file_name == '90-nproc.conf'
+    assert conf.data == []
+
+
+def test_nproc_conf_3():
+    conf = NprocConf(context_wrap(NPROC_CONF_2, path=FAKE_PATH))
+    assert conf.file_name == '80-custom.conf'
+    assert conf.data == []
