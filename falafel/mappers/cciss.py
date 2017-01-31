649f63e173e71ee8e1cc1e1fd83f961898dd8da6
Paul Wayper <paulway@redhat.com>
2017-01-31 15:42:29 +1100

    Documented CCISS driver mapper

--- falafel/mappers/cciss.py
+++ falafel/mappers/cciss.py
@@ -1,45 +1,58 @@
+"""
+Cciss - Files ``/proc/driver/cciss/cciss*``
+===========================================
+
+Reads the ``/proc/driver/cciss/cciss*`` files and converts them into a
+dictionary in the *data* property.
+
+Example:
+    >>> cciss = shared[Cciss]
+    >>> cciss.data['Logical drives']
+    '1'
+    >>> 'IRQ' in cciss.data
+    True
+"""
+
 from .. import Mapper, mapper
 
 
 @mapper('cciss')
 class Cciss(Mapper):
     '''
-    Raw Data
-    -----
+    Raw Data::
 
-    cciss0: HP Smart Array P220i Controller
-    Board ID: 0x3355103c
-    Firmware Version: 3.42
-    IRQ: 82
-    Logical drives: 1
-    Sector size: 8192
-    Current Q depth: 0
-    Current # commands on controller: 0
-    Max Q depth since init: 84
-    Max # commands on controller since init: 111
-    Max SG entries since init: 128
-    Sequential access devices: 0
+        cciss0: HP Smart Array P220i Controller
+        Board ID: 0x3355103c
+        Firmware Version: 3.42
+        IRQ: 82
+        Logical drives: 1
+        Sector size: 8192
+        Current Q depth: 0
+        Current # commands on controller: 0
+        Max Q depth since init: 84
+        Max # commands on controller since init: 111
+        Max SG entries since init: 128
+        Sequential access devices: 0
 
-    cciss/c0d0:  299.96GB   RAID 1(1+0)
+        cciss/c0d0:  299.96GB   RAID 1(1+0)
 
-    Output
-    ------
+    Output::
 
-    {
-        "Sequential access devices": "0",
-        "Current Q depth": "0",
-        "cciss0": "HP Smart Array P220i Controller",
-        "Board ID": "0x3355103c",
-        "IRQ": "82",
-        "cciss/c0d0": "299.96GB   RAID 1(1+0)",
-        "Logical drives": "1",
-        "Current # commands on controller": "0",
-        "Sector size": "8192",
-        "Firmware Version": "3.42",
-        "Max # commands on controller since init": "111",
-        "Max SG entries since init": "128",
-        "Max Q depth since init": "84"
-    }
+        data = {
+            "Sequential access devices": "0",
+            "Current Q depth": "0",
+            "cciss0": "HP Smart Array P220i Controller",
+            "Board ID": "0x3355103c",
+            "IRQ": "82",
+            "cciss/c0d0": "299.96GB   RAID 1(1+0)",
+            "Logical drives": "1",
+            "Current # commands on controller": "0",
+            "Sector size": "8192",
+            "Firmware Version": "3.42",
+            "Max # commands on controller since init": "111",
+            "Max SG entries since init": "128",
+            "Max Q depth since init": "84"
+        }
 
     '''
 
