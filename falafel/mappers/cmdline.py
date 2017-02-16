4340cf1056cb210be99f785da061751e8b6d0d42
Paul Wayper <paulway@redhat.com>
2017-02-16 15:35:39 +1100

    CmdLine mapper documentation with sample input and example usage
    We also fixed some wording, made the test string nicer, eliminated an
    unneeded check, and made sure we have 100% test coverage.

--- falafel/mappers/cmdline.py
+++ falafel/mappers/cmdline.py
@@ -1,26 +1,64 @@
+"""
+CmdLine - file ``/proc/cmdline``
+================================
+
+This reads the ``/proc/cmdline`` file, which contains the arguments given to
+the currently running kernel on boot.
+
+Sample input::
+
+    BOOT_IMAGE=/vmlinuz-3.10.0-327.36.3.el7.x86_64 root=/dev/system_vg/Root ro rd.lvm.lv=system_vg/Root crashkernel=auto rd.lvm.lv=system_vg/Swap rhgb quiet LANG=en_GB.utf8
+
+Examples:
+
+    >>> cmd = shared[CmdLine]
+    >>> cmd.data['BOOT_IMAGE']
+    ['/vmlinuz-3.10.0-327.36.3.el7.x86_64']
+    >>> cmd.data['rd.lvm.lv']
+    ['system_vg/Root', 'system_vg/Swap']
+    >>> 'crashkernel' in cmd.data
+    True
+    >>> 'autofs' in cmd.data
+    False
+    >>> 'quiet' in cmd.data
+    True
+    >>> cmd.data['quiet']
+    None
+
+"""
+
 from .. import Mapper, mapper, LegacyItemAccess
 
 
 @mapper('cmdline')
 class CmdLine(LegacyItemAccess, Mapper):
+    """
+    A mapper class for parsing the Linux kernel command line as given in
+    ``/proc/cmdline``.
+    """
 
     def parse_content(self, content):
         """
-        Content format is like "ro root=/dev/mapper/vg_rootvg-lv_root"
+        If an element doesn't contain "=", set itself as key and the
+        corresponding value to None.
 
-        If a property doesn't have "=" as spliter, set itself as key and its corresponding value is None.
-        If a property contains "=", the corresponding values are stored in a list.
-        For special cmdlines that include two "=", just like: root=LABEL=/1
-        "root" will be the key and "LABEL=/1" will be the value in the returned list.
-        Note: Some parameters(the returned keys) might be still effective even if there is '#' before it. For example: '#rhgb'.
-            Add necessary checking conditions when using this shared mapper in reducer.
+        If an element contains "=", the corresponding values are stored in a
+        list.
+
+        For special command line elements that include two "=", e.g.
+        ``root=LABEL=/1``, "root" will be the key and "LABEL=/1" will be the
+        value in the returned list.
+
+        Note: Some parameters (the returned keys) might be still effective
+        even if there is '#' before it, e.g.: ``#rhgb``.  This should
+        be checked by the reducer.
         """
+
         cmdline_properties = {}
         line = content[0]
-        for el in line.strip().split():
-            if not el:
-                continue
-            elif "=" not in el:
+        for el in line.strip().split(None):
+            # Thanks to strip and split, el always contains something.
+            if "=" not in el:
                 cmdline_properties[el] = None
             else:
                 (key, value) = el.split("=", 1)
--- falafel/mappers/tests/test_cmdline.py
+++ falafel/mappers/tests/test_cmdline.py
@@ -1,10 +1,10 @@
 from falafel.mappers.cmdline import CmdLine
 from falafel.tests import context_wrap
 
-CMDLINE = """
-ro root=/dev/mapper/vg_rootvg-lv_root rd_LVM_LV=vg_rootvg/lv_root rd_LVM_LV=vg_rootvg/lv_swap rd_NO_LUKS rd_NO_MD rd_NO_DM LANG=en_US.UTF-8 SYSFONT=latarcyrheb-sun16 KEYBOARDTYPE=pc KEYTABLE=us crashkernel=191M@0M hugepagesz=1073741824 hugepages=50 rhgb quiet\n
-
-""".strip()
+CMDLINE = "ro root=/dev/mapper/vg_rootvg-lv_root rd_LVM_LV=vg_rootvg/lv_root" +\
+    " rd_LVM_LV=vg_rootvg/lv_swap rd_NO_LUKS rd_NO_MD rd_NO_DM " +\
+    "LANG=en_US.UTF-8 SYSFONT=latarcyrheb-sun16 KEYBOARDTYPE=pc KEYTABLE=us " +\
+    "        crashkernel=191M@0M hugepagesz=1073741824 hugepages=50 rhgb quiet"
 
 
 class TestCmdLine():
@@ -14,3 +14,5 @@ class TestCmdLine():
         assert cmd_line.data.get('ro') is None
         assert cmd_line.data.get('root') == ['/dev/mapper/vg_rootvg-lv_root']
         assert cmd_line.data.get('rd_LVM_LV') == ['vg_rootvg/lv_root', 'vg_rootvg/lv_swap']
+        # Handle non-alphabet characters and extra spaces
+        assert cmd_line.data['crashkernel'] == ['191M@0M']
