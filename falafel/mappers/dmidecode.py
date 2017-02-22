ed7d9331962fcb4be0361ff1c29e36d9361d354e
Paul Wayper <paulway@redhat.com>
2017-02-22 13:14:25 +1100

    DMIDecode mapper documentation with sample data and code

--- falafel/mappers/dmidecode.py
+++ falafel/mappers/dmidecode.py
@@ -1,3 +1,105 @@
+"""
+DMIDecode - Command ``dmidecode``
+=================================
+
+Parses the output of the ``dmidecode`` command to catalogue the hardware
+associated with the system.
+
+In general, DMIdecode recognises the sections of device information,
+separated by blank lines, processed in the following way.
+
+* It uses the descriptor line that precedes the indented device information
+  (e.g. 'BIOS Information') as the name for that section, converting the name
+  into lower case and replacing spaces with underscores (e.g.
+  'bios_information') to look more Pythonic.
+
+* Within each section, data is split up on colons.
+
+* Lines such as 'Characteristics' that end with a colon and have one or more
+  further indented lines after them are converted into a list of the values so
+  indented.
+
+The common information retrieved from dmidecode is available in several
+convenience properties:
+
+* **system_info** - Information about the machine itself
+* **bios** - the BIOS information
+* **bios_vendor** - the BIOS's 'Vendor' attribute
+* **bios_date** - the BIOS's 'Release Date' attribute
+* **processor_manufacturer** - the processor's 'Manufacturer' attribute
+* **virt_what** - similar to the program ``virt-what``, the product,
+  manufacturer and vendor information is checked for recognised values that
+  indicate a virtualised environment and the first found is returned.  If no
+  virtualised environment is found, ``None`` is returned.
+* **is_present** - this indicates whether dmidecode information was found.
+
+Sample input::
+
+    # dmidecode 2.2
+    SMBIOS 2.4 present.
+    104 structures occupying 3162 bytes.
+    Table at 0x000EE000.
+    Handle 0x0000
+        DMI type 0, 24 bytes.
+        BIOS Information
+            Vendor: HP
+            Version: A08
+            Release Date: 09/27/2008
+            Address: 0xF0000
+            Runtime Size: 64 kB
+            ROM Size: 4096 kB
+            Characteristics:
+                PCI is supported
+                PNP is supported
+                BIOS is upgradeable
+                BIOS shadowing is allowed
+                ESCD support is available
+                Boot from CD is supported
+                Selectable boot is supported
+                EDD is supported
+                5.25"/360 KB floppy services are supported (int 13h)
+                5.25"/1.2 MB floppy services are supported (int 13h)
+                3.5"/720 KB floppy services are supported (int 13h)
+                Print screen service is supported (int 5h)
+                8042 keyboard services are supported (int 9h)
+                Serial services are supported (int 14h)
+                Printer services are supported (int 17h)
+                CGA/mono video services are supported (int 10h)
+                ACPI is supported
+                USB legacy is supported
+                BIOS boot specification is supported
+                Function key-initiated network boot is supported``
+    Handle 0x0100
+        DMI type 1, 27 bytes.
+        System Information
+            Manufacturer: HP
+            Product Name: ProLiant BL685c G1
+            Version: Not Specified
+            Serial Number: 3H6CMK2537
+            UUID: 58585858-5858-3348-3643-4D4B32353337
+            Wake-up Type: Power Switch
+
+Examples:
+
+    >>> dmi = shared[DMIDecode]
+    >>> dmi.is_present
+    True
+    >>> len(dmi['bios_information'])
+    1
+    >>> dmi['bios_information'][0]['vendor']
+    'HP'
+    >>> dmi.bios_vendor
+    'HP'
+    >>> dmi.bios_date
+    datetime.date(2008, 9, 27)
+    >>> len(dmi.bios['characteristics'])
+    20
+    >>> dmi.bios['characteristics'][0]
+    'PCI is supported'
+    >>> dmi.virt_what
+    None
+"""
+
 import re
 from datetime import date
 from .. import LegacyItemAccess, Mapper, mapper, defaults
@@ -5,6 +107,9 @@ from .. import LegacyItemAccess, Mapper, mapper, defaults
 
 @mapper('dmidecode')
 class DMIDecode(Mapper, LegacyItemAccess):
+    """
+    Class for DMI information.
+    """
 
     PRODUCT_MAP = {
         "VMware": "vmware",
@@ -30,28 +135,31 @@ class DMIDecode(Mapper, LegacyItemAccess):
 
     @property
     def system_info(self):
-        """Convenience method to get system information"""
+        """(str): Convenience method to get system information"""
         return self["system_information"][0] if "system_information" in self else None
 
     @property
     def bios(self):
-        """Convenience method to get BIOS information"""
+        """(str): Convenience method to get BIOS information"""
         return self["bios_information"][0] if "bios_information" in self else None
 
     @property
     @defaults()
     def bios_vendor(self):
+        """(str): Convenience method to get BIOS vendor"""
         return self["bios_information"][0]["vendor"]
 
     @property
     @defaults()
     def bios_date(self):
+        """(datetime.date): Get the BIOS release date in datetime.date format"""
         month, day, year = map(int, self["bios_information"][0]["release_date"].split("/"))
         return date(year, month, day)
 
     @property
     @defaults()
     def processor_manufacturer(self):
+        """(str): Convenience method to get the processor manufacturer"""
         return self["processor_information"][0]["manufacturer"]
 
     @property
@@ -71,8 +179,9 @@ class DMIDecode(Mapper, LegacyItemAccess):
         be the ``content`` attribute from the context object passed to a
         mapper.
 
-        The function returns the type of virtualized environment found, or
-        ``None`` if virtualizion could not be determined.
+        Returns:
+            (str): the type of virtualized environment found, or ``None`` if
+            virtualization could not be determined.
         '''
         sys_info = self.data.get("system_information", [{}])[0]
         bios_info = self.data.get("bios_information", [{}])[0]
@@ -90,6 +199,7 @@ class DMIDecode(Mapper, LegacyItemAccess):
 
     @property
     def is_present(self):
+        """(bool): Is there any decodable data in the content?"""
         return bool(self.data)
 
 
@@ -155,16 +265,13 @@ def parse_dmidecode(dmidecode_content, pythonic_keys=False):
                 section = None
 
         if not section:
-            section = fix_key(nbline)
+            # Ignore 'Table at 0xBFFCB000' and similar.
+            if not ('Table' in nbline or 'table' in nbline):
+                section = fix_key(nbline)
 
     if section in obj:
         obj[section].append(current)
     else:
         obj[section] = [current]
-
-    # Remove nonsense key-value
-    for k in obj.keys():
-        if "Table" in k or "table" in k:
-            del obj[k]
 
     return obj
