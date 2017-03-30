3905593c94f7d46d8a9785853ce2a5fe3f1bf337
Xiangce Liu <xiangceliu@redhat.com>
2017-03-30 22:38:02 +0800

    Remove the deprecated `get_vdsm_id` from host_vdsm_id
    - and modify attribute `uuid` to a property interface

--- falafel/mappers/host_vdsm_id.py
+++ falafel/mappers/host_vdsm_id.py
@@ -1,10 +1,28 @@
+"""
+vdsm.id- File /etc/vdsm/vdsm.id
+===============================
+
+Module for parsing the content of file ``vdsm.id``, which is a simple file.
+
+Typical content of "vdsm.id" is::
+
+    # VDSM UUID info
+    #
+    F7D9D983-6233-45C2-A387-9B0C33CB1306
+
+Examples:
+    >>> vd = shared[VDSMId]
+    >>> vd.uuid
+    "F7D9D983-6233-45C2-A387-9B0C33CB1306"
+
+"""
 from .. import Mapper, mapper
 from ..mappers import get_active_lines
 
 
 @mapper("vdsm_id")
 class VDSMId(Mapper):
-    """Class for VDSM IDs."""
+    """Class for parsing `vdsm.id` file."""
 
     def parse_content(self, content):
         """
@@ -12,11 +30,9 @@ class VDSMId(Mapper):
         - E.g.: F7D9D983-6233-45C2-A387-9B0C33CB1306
         """
         lines = get_active_lines(content)
-        self.uuid = lines[0].strip() if len(lines) > 0 else None
-        self.data = self.uuid
+        self.data = lines[0].strip() if len(lines) > 0 else None
 
-
-@mapper("vdsm_id")
-def get_vdsm_id(context):
-    """Deprecated, do not use."""
-    return VDSMId(context).uuid
+    @property
+    def uuid(self):
+        """Return the UUID in `vdsm.id` file."""
+        return self.data
