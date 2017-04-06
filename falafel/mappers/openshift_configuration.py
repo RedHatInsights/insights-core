cbd46f63423ae70588db99c9eb7b4e7076b9288a
Paul Wayper <paulway@redhat.com>
2017-04-07 09:30:33 +1000

    Adding subheadings for the two mappers for clarity

--- falafel/mappers/openshift_configuration.py
+++ falafel/mappers/openshift_configuration.py
@@ -1,10 +1,20 @@
 """
-openshift configuration files - File
-======================================================================================
-``/etc/origin/node/node-config.yaml`` and ``/etc/origin/master/master-config.yaml``
-are configuration files of openshift Master node and Node node. Their format are both
-``yaml``.
+openshift configuration files
+=============================
 
+``/etc/origin/node/node-config.yaml`` and
+``/etc/origin/master/master-config.yaml`` are configuration files of
+openshift Master node and Node node. Their format are both ``yaml``.
+
+OseNodeConfig - file ``/etc/origin/node/node-config.yaml``
+----------------------------------------------------------
+
+Reads the OpenShift node configuration
+
+OseMasterConfig - file ``/etc/origin/master/master-config.yaml``
+----------------------------------------------------------------
+
+Reads the Openshift master configuration
 
 Examples:
     >>> result = shared[OseMasterConfig]
