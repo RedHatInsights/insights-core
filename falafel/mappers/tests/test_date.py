519df0835c3e3eebeef08f949410709437e516c7
Paul Wayper <paulway@redhat.com>
2017-04-27 11:42:29 +1000

    Date tests at 100% code coverage
    Added tests for raising exceptions when processing invalid or truncated dates.

--- falafel/mappers/tests/test_date.py
+++ falafel/mappers/tests/test_date.py
@@ -1,11 +1,15 @@
-from falafel.mappers.date import Date, DateUTC
+from falafel.mappers.date import Date, DateUTC, DateParseException
 from falafel.tests import context_wrap
+
+import unittest
 
 DATE_OUTPUT1 = "Mon May 30 10:49:14 %s 2016"
 DATE_OUTPUT2 = "Thu Oct 22 12:59:28 %s 2015"
+DATE_OUTPUT_TRUNCATED = "Thu Oct 22"
+DATE_OUTPUT_INVALID = "Fon Pla 34 27:63:89 CST 20-1"
 
 
-class TestDate():
+class TestDate(unittest.TestCase):
     def test_get_date1(self):
         DATE = DATE_OUTPUT1 % ('CST')
         date_info = Date(context_wrap(DATE))
@@ -19,6 +23,16 @@ class TestDate():
         assert date_info.data == DATE
         assert date_info.datetime is not None
         assert date_info.timezone == 'EDT'
+
+    def test_get_date_truncated(self):
+        with self.assertRaisesRegexp(DateParseException, 'Expected six date parts.  Got '):
+            date_info = Date(context_wrap(DATE_OUTPUT_TRUNCATED))
+            self.assertIsNone(date_info)
+
+    def test_get_date_invalid(self):
+        with self.assertRaisesRegexp(DateParseException, DATE_OUTPUT_INVALID):
+            date_info = Date(context_wrap(DATE_OUTPUT_INVALID))
+            self.assertIsNone(date_info)
 
 
 class TestDateUTC():
