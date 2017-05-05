8cd60a1ee9c6a8d5735c767d0a30d36cf6c01fc4
Paul Wayper <paulway@mabula.net>
2017-05-06 04:50:19 +1000

    YumReposD tests 100% code coverage, removed print statements (#191)

--- falafel/mappers/tests/test_yum_repos_d.py
+++ falafel/mappers/tests/test_yum_repos_d.py
@@ -25,7 +25,6 @@ REPOPATH = 'etc/yum.repos.d/rhel-source.repo'
 def test_yum_repos_d():
     repos_info = YumReposD(context_wrap(REPOINFO, path=REPOPATH))
 
-    print repos_info.get('rhel-source')
     assert repos_info.get('rhel-source') == {
             'name': 'Red Hat Enterprise Linux $releasever - $basearch - Source',
             'baseurl': ['ftp://ftp.redhat.com/pub/redhat/linux/enterprise/$releasever/en/os/SRPMS/'],
@@ -33,7 +32,6 @@ def test_yum_repos_d():
             'gpgcheck': '1',
             'gpgkey': ['file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release',
                        'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release1']}
-    print repos_info.get('rhel-source-beta')
     assert repos_info.get('rhel-source-beta') == {
             'name': 'Red Hat Enterprise Linux $releasever Beta - $basearch - Source',
             'baseurl': ['ftp://ftp.redhat.com/pub/redhat/linux/beta/$releasever/en/os/SRPMS/',
@@ -44,3 +42,5 @@ def test_yum_repos_d():
                        'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release']}
     assert repos_info.file_name == 'rhel-source.repo'
     assert repos_info.file_path == 'etc/yum.repos.d/rhel-source.repo'
+
+    assert sorted(list(repos_info)) == sorted(['rhel-source', 'rhel-source-beta'])
