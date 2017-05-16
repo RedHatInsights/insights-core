dbd0cc7da668c2cd7af5ce568df7f81afa530196
Xiaoxue Wang <xxwjoy@hotmail.com>
2017-05-16 08:16:19 -0500

    Update mapper certificates_enddate.py (#187)
    * Update mappers certificates_enddate.py
    
    - Not use "pre_command" in specs.py
    - New mapper input, parse method and output
     - Change the CommandSpec in specs.py to pass insight-client's test
     - Adjust the mapper parse method according to cmd's output
    
    * Make get_certificates_path() method to a propery certificates_path
    
    * Ensure the test coverage is 100%
    
    * Fix a flake8 error in test file
    
    * Fix a typo in docstings
    
    * Adjust the property certificates_path 's return value

--- falafel/mappers/certificates_enddate.py
+++ falafel/mappers/certificates_enddate.py
@@ -1,38 +1,69 @@
 """
-openssl x509 -noout -enddate -in {crt} - Command
-================================================
+/usr/bin/openssl x509 -noout -enddate -in path/to/cert/file - Command
+=====================================================================
 
 This command gets the enddates of certificate files.
-The value 'crt' is the output of the pre_command
-"/usr/bin/find /etc/origin/node /etc/origin/master /etc/pki -type f".
+
+Typical output of this command is::
+
+    /usr/bin/find: '/etc/origin/node': No such file or directory
+    /usr/bin/find: '/etc/origin/master': No such file or directory
+    notAfter=May 25 16:39:40 2019 GMT
+    FileName= /etc/origin/node/cert.pem
+    unable to load certificate
+    139881193203616:error:0906D066:PEM routines:PEM_read_bio:bad end line:pem_lib.c:802:
+    unable to load certificate
+    140695459370912:error:0906D06C:PEM routines:PEM_read_bio:no start line:pem_lib.c:703:Expecting: TRUSTED CERTIFICATE
+    notAfter=May 25 16:39:40 2019 GMT
+    FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+    notAfter=Dec  9 10:55:38 2017 GMT
+    FileName= /etc/pki/consumer/cert.pem
+    notAfter=Jan  1 04:59:59 2022 GMT
+    FileName= /etc/pki/entitlement/3343502840335059594.pem
+    notAfter=Aug 31 02:19:59 2017 GMT
+    FileName= /etc/pki/consumer/cert.pem
+    notAfter=Jan  1 04:59:59 2022 GMT
+    FileName= /etc/pki/entitlement/2387590574974617178.pem
 
 Examples:
     >>> cert_enddate = shared[CertificatesEnddate]
-    >>> cert_enddate.file_name
-    'server.crt'
-    >>> cert_enddate.get_expiration_date()
-    datetime.datetime(2018, 11, 25, 3, 2, 4)
+    >>> paths = cert_enddate.get_certificates_path
+    >>> paths[0]
+    /etc/origin/node/cert.pem
+    >>> cert_enddate.get_expiration_date(paths[0])
+    datetime(2019, 05, 25, 16, 39, 40)
 """
 
-import datetime
+from datetime import datetime
 from .. import Mapper, mapper
 
 
 @mapper("certificates_enddate")
 class CertificatesEnddate(Mapper):
-    "Class to parse the expiration date"
+    "Class to parse the expiration dates"
 
     def parse_content(self, content):
-        """Parse the content of crt file"""
-        self.data = (content[0].split("=")[-1].rsplit(" ", 1)[0]
-                     if (len(content) == 1 and "=" in content[0])
-                     else None)
+        """Parse the content of crt files"""
+        self.data = {}
+        datestamp = None
+        for l in content:
+            if datestamp and l.startswith("FileName="):
+                self.data[l.split("=")[-1].strip()] = datestamp
+                datestamp = None
+            elif l.startswith("notAfter="):
+                datestamp = l.split("=")[-1].rsplit(" ", 1)[0]
+            else:
+                datestamp = None
 
-    def get_expiration_date(self):
+    @property
+    def certificates_path(self):
+        """return filepaths in list"""
+        return self.data.keys() if self.data else []
+
+    def get_expiration_date(self, path):
         """datetime: return the expiration date in datetime format"""
         try:
-            return (datetime.datetime.strptime(self.data, '%b %d %H:%M:%S %Y')
-                    if self.data else None)
+            return (datetime.strptime(self.data[path], '%b %d %H:%M:%S %Y')
+                    if self.data and path in self.data else None)
         except:
-            raise Exception("Unable to parse the expiration data of %s" %
-                            self.file_path)
+            raise Exception("Unable to parse the expiration data of %s" % path)
--- falafel/mappers/tests/test_certificates_enddate.py
+++ falafel/mappers/tests/test_certificates_enddate.py
@@ -1,89 +1,99 @@
+import unittest
+from datetime import datetime
 from falafel.mappers.certificates_enddate import CertificatesEnddate
 from falafel.tests import context_wrap
-import datetime
 
-import pytest
 
 CRT1 = """
-notAfter=Nov 24 02:58:22 2021 GMT
+/usr/bin/find: '/etc/origin/node': No such file or directory
+/usr/bin/find: '/etc/origin/master': No such file or directory
+notAfter=May 25 16:39:40 2019 GMT
+FileName= /etc/origin/node/cert.pem
+unable to load certificate
+139881193203616:error:0906D066:PEM routines:PEM_read_bio:bad end line:pem_lib.c:802:
+unable to load certificate
+140695459370912:error:0906D06C:PEM routines:PEM_read_bio:no start line:pem_lib.c:703:Expecting: TRUSTED CERTIFICATE
+notAfter=May 25 16:39:40 2019 GMT
+FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+notAfter=Dec  9 10:55:38 2017 GMT
+FileName= /etc/pki/consumer/cert.pem
+notAfter=Jan  1 04:59:59 2022 GMT
+FileName= /etc/pki/entitlement/3343502840335059594.pem
+notAfter=Aug 31 02:19:59 2017 GMT
+FileName= /etc/pki/consumer/cert.pem
+notAfter=Jan  1 04:59:59 2022 GMT
+FileName= /etc/pki/entitlement/2387590574974617178.pem
 """.strip()
 
-CRT1_PATH = "etc/origin/master/ca.crt"
-
-CRT2 = """
-notAfter=Nov 25 03:02:04 2018 GMT
-""".strip()
-
-CRT2_PATH = "etc/origin/node/server.crt"
+CRT2 = ""
 
 CRT3 = """
-notAfter=Mar 21 08:32:37 2017 GMT
+FileName= /etc/origin/node/cert.pem
+notAfter=May 25 16:39:40 2019 GMT
+FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+notAfter=Dec  9 10:55:38 2017 GMT
+FileName= /etc/pki/consumer/cert.pem
 """.strip()
 
-CRT3_PATH = "etc/pki/ovirt-engine/ca.pem"
-
-CRT4 = ""
+CRT4 = """
+notAfter=May 25 16:39:40 2019 GMT
+FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+unable to load certificate
+140463633168248:error:0906D06C:PEM routines:PEM_read_bio:no start line:pem_lib.c:701:Expecting: TRUSTED CERTIFICATE
+notAfter=Dec  9 10:55:38 2017 GMT
+FileName= /etc/pki/consumer/cert.pem
+notAfter=Jan  1 04:59:59 2022 GMT
+""".strip()
 
 CRT5 = """
-InvalidVaule=I am not sure this will happen or not .
-""".strip()
-
-CRT5_PATH = "/etc/pki/what-ever"
-
-# These are the conditions that the pre_command 'crt' send the openssl bad input.
-# 'crt' is a not a vaild certificate file.
-CRT6 = """
+notAfter=May 25 16:39:40 2019 GMT
+FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+notAfter=Dec  9 10:55:38 2017 GMT
 unable to load certificate
-140549596768160:error:0906D06C:PEM routines:PEM_read_bio:no start line:pem_lib.c:703:Expecting: TRUSTED CERTIFICATE
+140463633168248:error:0906D06C:PEM routines:PEM_read_bio:no start line:pem_lib.c:701:Expecting: TRUSTED CERTIFICATE
+FileName= /etc/pki/consumer/cert.pem
+notAfter=Jan  1 04:59:59 2022 GMT
 """.strip()
 
-CRT6_PATH = "/etc/pki/ca-trust/source/README"
-
-# 'crt' is the stderr of find command.
-# eg. "/usr/bin/find: '/etc/origin/master': No such file or directory"
-CRT7 = """
-Unknown option '/etc/origin/master':
-usage: x509 args
- -inform arg     - input format - default PEM (one of DER, NET or PEM)
- -outform arg    - output format - default PEM (one of DER, NET or PEM)
-...
+CRT6 = """
+notAfter=May 25 16:39:40 2019
+FileName= /etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem
+notAfter=Dec  9 10:55:38 20 GMT
+FileName= /etc/pki/consumer/cert.pem
 """.strip()
 
-# Path like this? Not sure
-CRT7_PATH = "/usr/bin/find: '/etc/origin/master': No such file or directory"
+PATH1 = "/etc/origin/node/cert.pem"
 
 
 def test_certificates_enddate():
-    Cert1 = CertificatesEnddate(context_wrap(CRT1, path=CRT1_PATH))
-    assert Cert1.file_name == 'ca.crt'
-    assert Cert1.get_expiration_date() == datetime.datetime(2021, 11, 24, 2, 58, 22)
 
-    Cert2 = CertificatesEnddate(context_wrap(CRT2, path=CRT2_PATH))
-    assert Cert2.file_name == 'server.crt'
-    assert Cert2.get_expiration_date() == datetime.datetime(2018, 11, 25, 3, 2, 4)
+    Cert1 = CertificatesEnddate(context_wrap(CRT1))
+    assert PATH1 in Cert1.certificates_path
+    assert Cert1.get_expiration_date(PATH1) == datetime(2019, 05, 25, 16, 39, 40)
 
-    Cert3 = CertificatesEnddate(context_wrap(CRT3, path=CRT3_PATH))
-    assert Cert3.file_path == CRT3_PATH
-    assert Cert3.file_name == 'ca.pem'
-    assert Cert3.get_expiration_date() == datetime.datetime(2017, 03, 21, 8, 32, 37)
+    Cert2 = CertificatesEnddate(context_wrap(CRT2))
+    assert Cert2.certificates_path == []
 
-    Cert4 = CertificatesEnddate(context_wrap(CRT4, path=CRT3_PATH))
-    assert Cert4.file_path == CRT3_PATH
-    assert Cert4.file_name == 'ca.pem'
-    assert Cert4.get_expiration_date() is None
+    Cert3 = CertificatesEnddate(context_wrap(CRT3))
+    assert (Cert3.certificates_path == [
+            '/etc/pki/consumer/cert.pem',
+            '/etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem'])
 
-    Cert5 = CertificatesEnddate(context_wrap(CRT5, path=CRT5_PATH))
-    assert Cert5.file_path == CRT5_PATH
-    assert Cert5.file_name == 'what-ever'
-    with pytest.raises(Exception) as e:
-        assert Cert5.get_expiration_date()
-    assert 'Unable to parse the expiration data of' in str(e)
+    Cert4 = CertificatesEnddate(context_wrap(CRT4))
+    assert (Cert4.certificates_path == [
+            '/etc/pki/consumer/cert.pem',
+            '/etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem'])
 
-    Cert6 = CertificatesEnddate(context_wrap(CRT6, path=CRT6_PATH))
-    assert Cert6.file_name == 'README'
-    assert Cert6.get_expiration_date() is None
+    Cert5 = CertificatesEnddate(context_wrap(CRT5))
+    assert (Cert5.certificates_path == [
+            '/etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem'])
 
-    Cert7 = CertificatesEnddate(context_wrap(CRT7, path=CRT7_PATH))
-    assert Cert7.file_path == CRT7_PATH
-    assert Cert7.file_name == 'master\': No such file or directory'
-    assert Cert7.get_expiration_date() is None
+
+class TestCertificatesEnddateRaise(unittest.TestCase):
+    def test_certificates_enddate_raise(self):
+        Cert6 = CertificatesEnddate(context_wrap(CRT6))
+        assert (Cert6.certificates_path == [
+                '/etc/pki/consumer/cert.pem',
+                '/etc/pki/ca-trust/extracted/pem/email-ca-bundle.pem'])
+        with self.assertRaises(Exception):
+            Cert6.get_expiration_date('/etc/pki/consumer/cert.pem')
