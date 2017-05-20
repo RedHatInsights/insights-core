6a4cc90fada850bf5d03a6b89366b5f6e08fdf51
Bob Fahr <bfahr@redhat.com>
2017-05-20 11:20:27 -0500

    Add LVM shared reducer and fixes for LVM mappers (#126)
    * Add LVM shared reducer and fixes for LVM mappers
    
    This commit fixes some problems parsing output of the lvs, vgs,
    and pvs commands in columnar format with headings. New mappers were
    added to parse the table format `LvsHeadings`, `PvsHeadings`, and
    `VgsHeadings`.  A shared reducer `Lvm` was also added to combine all of
    the LVM information regardless of the source.  Tests and documentation
    were added for all mappers and the reducer.

--- falafel/mappers/__init__.py
+++ falafel/mappers/__init__.py
@@ -156,3 +156,124 @@ def unsplit_lines(lines, cont_char='\\'):
             unsplit_lines = []
     if unsplit_lines:
         yield ''.join(unsplit_lines)
+
+
+def calc_offset(lines, target, invert_search=False):
+    """
+    Function to search for a line in a list starting with a target string.
+    If `target` is `None` or an empty string then `0` is returned.  This
+    allows checking `target` here instead of having to check for an empty
+    target in the calling function. Each line is stripped of leading spaces
+    prior to comparison with each target however target is not stripped.
+    See `parse_fixed_table` in this module for sample usage.
+
+    Arguments:
+        lines (list): List of strings.
+        target (list): List of strings to search for at the beginning of any
+            line in lines.
+        invert_search (boolean): If `True` this flag causes the search to continue
+            until the first line is found not matching anything in target.
+            An empty line is implicitly included in target.  Default is `False`.
+            This would typically be used if trimming trailing lines off of a
+            file by passing `reversed(lines)` as the `lines` argument.
+
+    Returns:
+        int: index into the `lines` indicating the location of `target`. If
+            `target` is `None` or an empty string `0` is returned as the offset.
+            If `invert_search` is `True` the index returned will point to the line
+            after the last target was found.
+
+    Raises:
+        ValueError: Exception is raised if `target` string is specified and it
+            was not found in the input lines.
+    """
+    if target and target[0] is not None:
+        for offset, line in enumerate(l.strip() for l in lines):
+            found_any = any([line.startswith(t) for t in target])
+            if not invert_search and found_any:
+                return offset
+            elif invert_search and not(line == '' or found_any):
+                return offset
+
+        # If we get here then we didn't find any of the targets
+        raise ValueError("Line containing '{}' was not found in table".format(','.join(target)))
+    else:
+        # If no target then return index 0
+        return 0
+
+
+def parse_fixed_table(table_lines,
+                      heading_ignore=None,
+                      header_substitute=None,
+                      trailing_ignore=None):
+    """
+    Function to parse table data containing column headings in the first row and
+    data in fixed positions in each remaining row of table data.
+    Table columns must not contain spaces within the column name.  Column headings
+    are assumed to be left justified and the column data width is the width of the
+    heading label plus all whitespace to the right of the label.  This function
+    will handle blank columns.
+
+    Arguments:
+        table_lines (list): List of strings with the first line containing column
+            headings separated by spaces, and the remaining lines containing
+            table data in left justified format.
+        heading_ignore (list): Optional list of strings to search for at
+            beginning of line.  All lines before this line will be ignored.
+            If specified then it must be present in the file or `ValueError` will
+            be raised.
+        header_substitute (list): Optional list of tuples containing
+            `(old_string_value, new_string_value)` to be used to modify header
+            values.  If whitespace is present in a column it must be replaced with
+            non-whitespace characters in order for the table to be parsed correctly.
+        trailing_ignore (list): Optional list of strings to look for at the end
+            rows of the content.  Lines starting with these strings will be ignored,
+            thereby truncating the rows of data.
+
+    Returns:
+        list: Returns a list of dict for each row of column data.  Dict keys
+            are the column headings in the same case as input.
+
+    Raises:
+        ValueError: Raised if `heading_ignore` is specified and not found in `table_lines`.
+
+    Sample input::
+
+        Column1    Column2    Column3
+        data1      data 2     data   3
+        data4      data5      data6
+
+    Examples:
+        >>> table_data = parse_fixed_table(table_lines)
+        >>> table_data
+        [{'Column1': 'data1', 'Column2': 'data 2', 'Column3': 'data   3'},
+         {'Column1': 'data4', 'Column2': 'data5', 'Column3': 'data6'}]
+    """
+    heading_ignore = heading_ignore if heading_ignore is not None else []
+    trailing_ignore = trailing_ignore if trailing_ignore is not None else []
+
+    first_line = calc_offset(table_lines, heading_ignore)
+    try:
+        last_line = len(table_lines) - calc_offset(reversed(table_lines),
+                                                   trailing_ignore,
+                                                   invert_search=True)
+    except ValueError:
+        last_line = len(table_lines)
+
+    header = table_lines[first_line]
+    if header_substitute:
+        for old_val, new_val in header_substitute:
+            header = header.replace(old_val, new_val)
+    col_headers = header.strip().split()
+    col_index = [header.index(c) for c in col_headers]
+
+    table_data = []
+    for line in table_lines[first_line + 1:last_line]:
+        col_data = {
+            col_headers[c]: line[col_index[c]:col_index[c + 1]].strip()
+            for c in range(len(col_index) - 1)
+        }
+        col_data[col_headers[-1]] = line[col_index[-1]:].strip()
+        table_data.append(col_data)
+
+    return table_data
