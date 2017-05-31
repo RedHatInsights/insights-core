9d4386f309c22244ada8d97de827bfd61fdfaee2
Kyle Lape <klape@redhat.com>
2017-05-31 10:35:22 -0500

    Adding back archive tool (#28)
    * Adding back archive tool
    
    * Include archive tool unit tests in pytest config
    
    * Removing archive tool external install in shippable.yml

---
+++ falafel/archive/__init__.py
@@ -0,0 +1,13 @@
+import sys
+from falafel.core import load_package
+
+
+def get_archives(module_name, system_filter):
+    load_package(module_name)
+    m = sys.modules[module_name]
+    for sub_m in m.__all__:
+        demo_submodule = sys.modules[".".join([module_name, sub_m])]
+        if hasattr(demo_submodule, "demo"):
+            for a in demo_submodule.demo:
+                if system_filter in a.name:
+                    yield a
