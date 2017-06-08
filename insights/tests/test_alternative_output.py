dc57b7abe0afd4c2d48af1b703f2bace1d268cfa
Paul Wayper <paulway@mabula.net>
2017-06-08 18:48:48 +1000

    AlternativesOutput mapper class definition (#47)
    * AlternativesOutput mapper class definition
    
    * Add JavaAlternatives mapper using AlternativesOutput mapper class
    
    * Simplify AlternativesOutput to only one program
    
    The ``alternatives --display (program)`` command can only ever output one
    program.  This makes the object and parsing considerably simpler:
    
    * The output is self-naming, so subclasses don't have to declare anything.
    * The object now has 'program', 'status', 'link', 'best', and 'paths' as
      attributes rather than as dictionary keys.
    * If more than one 'status is' line is found, this raises a ParseException.
    * All attributes are initialised to None (or [] for paths).
    
    The main behaviour of this is now tested in tests/test_alternatives_output.py
    
    The JavaAlternatives mapper now uses this class.
    
    All tests pass and have 100% code coverage.
    
    * Minor documentation formatting correction
    
    * Minor post-rename rebase fixups
    
    * Path fix after post-rename rebase

--- insights/tests/test_alternative_output.py
+++ insights/tests/test_alternative_output.py
@@ -1,9 +1,11 @@
+# -*- coding: UTF-8 -*-
+from insights.core import AlternativesOutput
 from insights.tests import context_wrap
-from insights.parsers.display_java import default_java, JavaAlternatives
+from insights.parsers import ParseException
 
 import unittest
 
-alter_java = """
+ALT_JAVA_160 = """
 java - status is auto.
  link currently points to /usr/lib/jvm/jre-1.6.0-ibm.x86_64/bin/java
 /usr/lib/jvm/jre-1.6.0-ibm.x86_64/bin/java - priority 16091
@@ -25,64 +27,34 @@ java - status is auto.
  slave jre_exports: /usr/lib/jvm-exports/jre-1.4.2-gcj
  slave jre: /usr/lib/jvm/jre-1.4.2-gcj
 Current `best' version is /usr/lib/jvm/jre-1.6.0-ibm.x86_64/bin/java.
-""".strip().encode('utf8')
+"""
 
-alter_no_java = """
-/usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java - priority 170079
- slave ControlPanel: (null)
- slave keytool: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/keytool
- slave orbd: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/orbd
- slave pack200: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/pack200
- slave policytool: (null)
- slave rmid: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/rmid
- slave rmiregistry: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/rmiregistry
- slave servertool: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/servertool
- slave tnameserv: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/tnameserv
- slave unpack200: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/unpack200
- slave jre_exports: /usr/lib/jvm-exports/jre-1.7.0-openjdk.x86_64
- slave jre: /usr/lib/jvm/jre-1.7.0-openjdk.x86_64
- slave java.1.gz: /usr/share/man/man1/java-java-1.7.0-openjdk.1.gz
- slave keytool.1.gz: /usr/share/man/man1/keytool-java-1.7.0-openjdk.1.gz
- slave orbd.1.gz: /usr/share/man/man1/orbd-java-1.7.0-openjdk.1.gz
- slave pack200.1.gz: /usr/share/man/man1/pack200-java-1.7.0-openjdk.1.gz
- slave rmid.1.gz: /usr/share/man/man1/rmid-java-1.7.0-openjdk.1.gz
- slave rmiregistry.1.gz: /usr/share/man/man1/rmiregistry-java-1.7.0-openjdk.1.gz
- slave servertool.1.gz: /usr/share/man/man1/servertool-java-1.7.0-openjdk.1.gz
- slave tnameserv.1.gz: /usr/share/man/man1/tnameserv-java-1.7.0-openjdk.1.gz
- slave unpack200.1.gz: /usr/share/man/man1/unpack200-java-1.7.0-openjdk.1.gz
-Current `best' version is /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java.
-""".strip().encode('utf8')
+ALT_MTA = """
+mta - status is auto.
+ link currently points to /usr/sbin/sendmail.postfix
+/usr/sbin/sendmail.postfix - priority 30
+ slave mta-mailq: /usr/bin/mailq.postfix
+ slave mta-newaliases: /usr/bin/newaliases.postfix
+Current `best' version is /usr/sbin/sendmail.postfix.
+"""
+
+DUPLICATED_STATUS_LINE = """
+mta - status is auto.
+mta - status is auto.
+"""
+
+MISSING_STATUS_LINE = """
+ link currently points to /usr/sbin/sendmail.postfix
+/usr/sbin/sendmail.postfix - priority 30
+ slave mta-mailq: /usr/bin/mailq.postfix
+ slave mta-newaliases: /usr/bin/newaliases.postfix
+Current `best' version is /usr/sbin/sendmail.postfix.
+"""
 
 
-def test_no_java():
-    java = default_java(context_wrap(alter_no_java))
-    assert java is None
-
-
-def test_has_java():
-    java = default_java(context_wrap(alter_java))
-    assert java == "/usr/lib/jvm/jre-1.6.0-ibm.x86_64/bin/java"
-
-
-class Test_Class(unittest.TestCase):
-
-    def test_class_no_java(self):
-        java = JavaAlternatives(context_wrap(alter_no_java))
-        self.assertTrue(hasattr(java, 'program'))
-        self.assertIsNone(java.program)
-        # Direct access
-        self.assertTrue(hasattr(java, 'status'))
-        self.assertIsNone(java.status)
-        self.assertTrue(hasattr(java, 'link'))
-        self.assertIsNone(java.link)
-        self.assertTrue(hasattr(java, 'paths'))
-        self.assertEqual(java.paths, [])
-        self.assertTrue(hasattr(java, 'best'))
-        self.assertIsNone(java.best)
-
-    def test_class_has_java(self):
-        java = JavaAlternatives(context_wrap(alter_java))
-
+class TestAlternatives(unittest.TestCase):
+    def test_basic_alternatives(self):
+        java = AlternativesOutput(context_wrap(ALT_JAVA_160, path='/etc/alternatives/java'))
         self.assertTrue(hasattr(java, 'program'))
         self.assertEqual(java.program, 'java')
         self.assertTrue(hasattr(java, 'status'))
@@ -119,3 +91,37 @@ class Test_Class(unittest.TestCase):
             'tnameserv', 'jre_exports', 'jre'
         ]))
         self.assertEqual(java.paths[1]['slave']['ControlPanel'], '(null)')
+
+    def test_mta_alternatives(self):
+        mtas = AlternativesOutput(context_wrap(ALT_MTA))
+
+        self.assertTrue(hasattr(mtas, 'program'))
+        self.assertEqual(mtas.program, 'mta')
+        self.assertTrue(hasattr(mtas, 'status'))
+        self.assertEqual(mtas.status, 'auto')
+        self.assertTrue(hasattr(mtas, 'link'))
+        self.assertEqual(mtas.link, '/usr/sbin/sendmail.postfix')
+        self.assertTrue(hasattr(mtas, 'best'))
+        self.assertEqual(mtas.best, '/usr/sbin/sendmail.postfix')
+
+        self.assertTrue(hasattr(mtas, 'paths'))
+        self.assertIsInstance(mtas.paths, list)
+        self.assertEqual(len(mtas.paths), 1)
+
+        self.assertIn('path', mtas.paths[0])
+        self.assertIn('priority', mtas.paths[0])
+        self.assertIn('slave', mtas.paths[0])
+
+    def test_failure_modes(self):
+        # Duplicate status line raises ParseException
+        with self.assertRaisesRegexp(ParseException, 'Program line for mta'):
+            alts = AlternativesOutput(context_wrap(DUPLICATED_STATUS_LINE))
+            self.assertIsNone(alts.program)
+
+        # Missing status line results in no data
+        alts = AlternativesOutput(context_wrap(MISSING_STATUS_LINE))
+        self.assertIsNone(alts.program)
+        self.assertIsNone(alts.status)
+        self.assertIsNone(alts.link)
+        self.assertIsNone(alts.best)
+        self.assertEqual(alts.paths, [])
