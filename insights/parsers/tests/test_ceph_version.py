57924c10f846d252c407d135dc3b89b766803746
shzhou12 <shzhou@redhat.com>
2017-06-19 13:32:53 +0800

    Update ceph_version to add version 2.2 (#105)

--- insights/parsers/tests/test_ceph_version.py
+++ insights/parsers/tests/test_ceph_version.py
@@ -9,6 +9,7 @@ CV3 = "error"
 CV4 = "ceph version 10.2.2-38.el7cp (b83334e01379f267fb2f9ce729d74a0a8fa1e92c)"
 CV5 = "ceph version 1"
 CV6 = "ceph version 1.2.3-5"
+CV7 = "ceph version 10.2.5-37.el7cp (033f137cde8573cfc5a4662b4ed6a63b8a8d1464)"
 
 
 def test_ceph_version():
@@ -33,3 +34,8 @@ def test_ceph_version():
     assert ceph_version4.release == "2.0"
     assert ceph_version4.major == '2'
     assert ceph_version4.minor == "0"
+
+    ceph_version7 = CephVersion(context_wrap(CV7))
+    assert ceph_version7.release == "2.2"
+    assert ceph_version7.major == '2'
+    assert ceph_version7.minor == "2"
