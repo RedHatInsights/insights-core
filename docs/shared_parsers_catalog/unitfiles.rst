debb3ddfdd156bd091eb466b36a84db71ccbfeb8
Jakub Svoboda <jsvoboda@redhat.com>
2017-06-21 17:02:46 +0200

    Unitfiles parser enhanced to deal with all unit states and enhanced with a new method. (#114)
    * Added unitfiles parser to documentation.

---
+++ docs/shared_parsers_catalog/unitfiles.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.parsers.systemd.unitfiles
+   :members:
+   :show-inheritance:
--- insights/parsers/systemd/unitfiles.py
+++ insights/parsers/systemd/unitfiles.py
@@ -1,3 +1,39 @@
+"""
+unitfiles - systemctl unit files (services)
+===========================================
+
+The UnitFiles class parses the output of `/bin/systemctl list-unit-files` and provides
+information about enabled services and existing services.
+
+Example:
+
+    >>> conf = shared[UnitFiles]
+    >>> conf.is_on('existing-enabled-service.service')
+    True
+    >>> conf.is_on('existing-disabled-service.service')
+    False
+    >>> conf.is_on('nonexistent-service.service')
+    False
+    >>> conf.exists('existing-enabled-service.service')
+    True
+    >>> conf.exists('existing-disabled-service.service')
+    True
+    >>> conf.exists('nonexistent-service.service')
+    False
+    >>> 'existing-enabled-service.service' in conf.services
+    True
+    >>> 'existing-disabled-service.service' in conf.services
+    True
+    >>> 'nonexistent-service.service' in conf.services
+    False
+    >>> conf.services['existing-enabled-service.service']
+    True
+    >>> conf.services['existing-disabled-service.service']
+    False
+    >>> conf.services['nonexistent-service.service']
+    KeyError: 'nonexistent-service.service'
+"""
+
 from ... import Parser, parser
 from .. import get_active_lines
 
@@ -24,9 +60,12 @@ class UnitFiles(Parser):
         Args:
             content (context.content): Parser context content
         """
-        # 'static' means 'on' to fulfill dependency of something else that is on
-        valid_states = {'enabled', 'static', 'disabled', 'invalid'}
-        on_states = {'enabled', 'static'}
+        # man systemctl - "is-enabled" knows these states
+        valid_states = {'enabled', 'enabled-runtime', 'linked', 'linked-runtime', 'masked',
+                        'masked-runtime', 'static', 'indirect', 'disabled', 'generated',
+                        'transient', 'bad', 'invalid'}
+        # man systemctl - "is-enabled" considers these to be enabled
+        on_states = {'enabled', 'enabled-runtime', 'static', 'indirect', 'generated', 'transient'}
 
         for line in get_active_lines(content):
             parts = line.split(None)  # AWK like split, strips whitespaces
@@ -45,6 +84,19 @@ class UnitFiles(Parser):
             service_name (str): service name including '.service'
 
         Returns:
-            bool: True if service is enabled, False otherwise
+            Union[bool, None]: True if service is enabled, False if it is disabled. None if the
+            service doesn't exist.
         """
-        return self.services.get(service_name, False)
+        return self.services.get(service_name, None)
+
+    def exists(self, service_name):
+        """
+        Checks if the service is listed in systemctl.
+
+        Args:
+            service_name (str): service name including '.service'
+
+        Returns:
+            bool: True if service exists, False otherwise.
+        """
+        return service_name in self.service_list
