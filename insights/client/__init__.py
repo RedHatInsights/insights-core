f21a6b5b4009049701b28e711a503649af3cf757
Richard Brantley <brantleyr@gmail.com>
2017-06-22 14:48:06 -0400

    added version functions, beefed up fetch logic

--- insights/client/__init__.py
+++ insights/client/__init__.py
@@ -4,11 +4,32 @@ from . import client
 from .constants import InsightsConstants as constants
 from .auto_config import try_auto_configuration
 from .client_config import parse_config_file, InsightsClient, set_up_options
+import logging
+
+LOG_FORMAT = ("%(asctime)s %(levelname)s %(message)s")
+APP_NAME = constants.app_name
+logger = logging.getLogger(APP_NAME)
+handler = None
 
 
-def get_version():
+def get_versions():
+    '''
+    returns (dict): {'core': str, 'client_api': str}
+    '''
+    return {'core': get_core_version(), 'client_api': get_client_api_version()}
+
+
+def get_core_version():
+    '''
+        returns (str): version of the core
+    '''
+    from .. import get_nvr
+    return get_nvr()
+
+
+def get_client_api_version():
     """
-        returns (str): version of the client
+        returns (str): version of the client api
     """
     return constants.version
 
@@ -54,37 +75,83 @@ def parse_options():
     return parse_config_file(options.conf), options
 
 
-def fetch(egg_url=constants.egg_path, force=False):
+def fetch(egg_url=constants.egg_path, force=False, verbose=False):
     """
         returns (str): path to new egg.  None if no update.
     """
+    # Setup the config and options
+    InsightsClient.config, InsightsClient.options = parse_options()
+    if verbose:
+        setattr(InsightsClient.options, 'verbose', True)
+    client.set_up_logging()
+
+    # Searched for cached etag information
     current_etag = None
     import os
     if os.path.isfile(constants.core_etag_file):
         with open(constants.core_etag_file, 'r') as etag_file:
             current_etag = etag_file.read().strip()
+            logger.debug('Found etag %s', current_etag)
 
+    # Setup the new request for core retrieval
+    logger.debug('Making request to %s for new core', egg_url)
     request = urllib2.Request(egg_url)
+
+    # If the etag was found and we are not force fetching
+    # Then add it to the request
     if current_etag and not force:
+        logger.debug('Requesting new core with etag %s', current_etag)
         request.add_header('If-None-Match', current_etag)
+    else:
+        logger.debug('Found no etag or forcing fetch')
+
+    # Initialize the data stream
     opener = urllib2.build_opener(DefaultErrorHandler())
     datastream = opener.open(request)
     data = datastream.read()
 
-    if data:
+    # Debug information
+    if datastream:
+        for attr in ['status', 'code', 'message', 'msg']:
+            if hasattr(datastream, attr):
+                logger.debug('%s: %s', attr, getattr(datastream, attr))
+        if datastream.headers:
+            for header in datastream.headers.dict:
+                logger.debug('%s: %s', header, datastream.headers.dict[header])
+
+    # If data was received, write the new egg and etag
+    if data and datastream.code == 200:
+
+        # Setup tmp egg path
         import tempfile
         tmpdir = tempfile.mkdtemp()
         tmp_egg_path = os.path.join(tmpdir, 'insights-core.egg')
 
+        # Write the new core
         with open(tmp_egg_path, 'wb') as handle:
+            logger.debug('Data received, writing core to %s', tmp_egg_path)
             handle.write(data)
 
+        # Write the new etag
         with open(constants.core_etag_file, 'w') as etag_file:
+            logger.debug('Cacheing etag to %s', constants.core_etag_file)
             etag_file.write(datastream.headers.dict['etag'])
 
+        # Return the tmp egg path
         return tmp_egg_path
 
+    # Received a 304 not modified
+    # Return nothing
+    elif datastream.code == 304:
+        logger.debug('No data received')
+        logger.debug('Tags match, not updating core')
+        return None
+
+    # Something unexpected received
     else:
+        logger.debug('Received Code %s', datastream.code)
+        logger.debug('Not writing new core, or updating etag')
+        logger.debug('Please check config, error reaching %s', egg_url)
         return None
 
 
