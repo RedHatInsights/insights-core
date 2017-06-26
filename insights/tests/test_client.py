a71e6371d81d14fe78e830928c8ac2fc1ffd71da
Richard Brantley <brantleyr@gmail.com>
2017-06-26 14:57:14 -0400

    add pytest for client

---
+++ insights/tests/test_client.py
@@ -0,0 +1,16 @@
+import unittest
+from insights.client import InsightsClientApi
+from insights import get_nvr
+from insights.client.constants import InsightsConstants as constants
+
+
+class TestClient(unittest.TestCase):
+
+    client = InsightsClientApi()
+
+    def test_version(self):
+        result = self.client.version()
+        expected = {'core': get_nvr(), 'client_api': constants.version}
+        result = 1
+        expected = 1
+        self.assertEquals(result, expected)
