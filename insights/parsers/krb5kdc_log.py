10464806905b5e8563bdfcff7ca7d93ed8142f00
Paul Wayper <paulway@mabula.net>
2017-07-03 18:24:51 +1000

    Kerberos KDC log parser for 1.x branch (#147)
    * Adding spec for Kerberos KDC logs
    * Documentation markup include file from template
    * Kerberos KDC Log parser with tests and documentation
    * `get()` method parses logs into fields
    * `get_after()` method waiting on #119
    * Using get_after() from updated LogFileOutput class
    * Testing use of get_after() from updated LogFileOutput class
    * Adding note about year in logs when using get_after()
    * Reworked documentation for current docs layout

---
+++ insights/parsers/krb5kdc_log.py
@@ -0,0 +1,86 @@
+"""
+Kerberos KDC Logs - file ``/var/log/krb5kdc.log``
+=================================================
+
+"""
+
+from .. import LogFileOutput, parser
+
+import re
+
+
+@parser('kerberos_kdc_log')
+class KerberosKDCLog(LogFileOutput):
+    '''
+    Read the ``/var/log/krb5kdc.log`` file.  For more usage information see
+    the ``LogFileOutput`` parser class.
+
+    Sample log file::
+
+        Apr 01 03:36:11 ldap.example.com krb5kdc[24569](info): TGS_REQ (4 etypes {18 17 16 23}) 10.250.3.150: ISSUE: authtime 1427873771, etypes {rep=18 tkt=18 ses=18}, sasher@EXAMPLE.COM for HTTP/sepdt138.example.com@EXAMPLE.COM
+        Apr 01 03:36:11 ldap.example.com krb5kdc[24569](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+        Apr 01 03:36:11 ldap.example.com krb5kdc[24549](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+        Apr 01 03:36:11 ldap.example.com krb5kdc[24546](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+        Apr 01 03:36:33 ldap.example.com krb5kdc[24556](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+        Apr 01 03:36:36 ldap.example.com krb5kdc[24568](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+        Apr 01 03:38:34 ldap.example.com krb5kdc[24551](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+        Apr 01 03:39:43 ldap.example.com krb5kdc[24549](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+
+    Examples:
+        >>> log = shared[KerberosKDCLog]
+        >>> # log.get is a generator, so get list to test length
+        >>> len(list(log.get('Decrypt integrity check failed')))
+        1
+        >>> from datetime import datetime
+        >>> len(log.get_after(datetime(2017, 4, 1, 3, 36, 30)))  # Apr 01 03:36:30
+        4
+    '''
+
+    def get(self, keyword):
+        '''
+        Find logs by keyword and parse them into a dictionary with the keys:
+
+        * `timestamp`
+        * `system`
+        * `service`
+        * `pid`
+        * `level`
+        * `message`
+        * `line` - the full line as originally given.
+
+        If the log line is not in the standard format, only the 'line' field
+        will be stored in the dictionary.
+
+        Parameters:
+            keyword(str): a keyword to search for in the line
+
+        Yields:
+            (dict): A dictionary for each parsed line in the input.
+        '''
+        line_re = re.compile(
+            r'(?P<timestamp>\w{3} \d\d \d\d:\d\d:\d\d) ' +
+            r'(?P<system>\w\S+) ' +
+            r'(?P<service>\w+)\[(?P<pid>\d+)\]\((?P<level>\w+)\): ' +
+            r'(?P<message>.*)'
+        )
+        for line in self.lines:
+            if keyword in line:
+                data = {
+                    'line': line
+                }
+                match = line_re.search(line)
+                if match:
+                    data.update(match.groupdict())
+                yield data
+
+    def get_after(self, timestamp, lines=None):
+        '''
+        Get all lines after a given date.  The year of the logs is assumed to
+        be the year in the given timestamp (see LogFileOutput's documentation
+        for more details).
+
+        *NOTE*: Because /var/log/krb5kdc.log timestamps by default have no
+        year, the year of the logs will be inferred from the year in your
+        timestamp.  This will also work around December/January crossovers.
+        '''
+        return super(KerberosKDCLog, self).get_after(timestamp, lines, '%b %d %H:%M:%S')
---
+++ insights/parsers/tests/test_krb5kdc_log.py
@@ -0,0 +1,52 @@
+from insights.parsers.krb5kdc_log import KerberosKDCLog
+from insights.tests import context_wrap
+
+from datetime import datetime
+
+KRB5KDC_LOG = """
+Apr 01 03:36:01 ldap.example.com krb5kdc[24554](info): TGS_REQ (7 etypes {18 17 23 3 1 24 -135}) 10.250.2.47: UNKNOWN_SERVER: authtime 0,  opshzl@EXAMPLE.COM for cifs/secifs1.example.com@EXAMPLE.COM, Server not found in Kerberos database
+Apr 01 03:36:04 ldap.example.com krb5kdc[24554](info): TGS_REQ (7 etypes {18 17 23 3 1 24 -135}) 10.250.18.199: UNKNOWN_SERVER: authtime 0,  pdtsro@EXAMPLE.COM for cifs/nyqrm4.example.com@EXAMPLE.COM, Server not found in Kerberos database
+Apr 01 03:36:11 ldap.example.com krb5kdc[24554](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.3.150: NEEDED_PREAUTH: sasher@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+Apr 01 03:36:11 ldap.example.com krb5kdc[24570](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.3.150: ISSUE: authtime 1427873771, etypes {rep=18 tkt=18 ses=18}, sasher@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM
+Apr 01 03:36:11 ldap.example.com krb5kdc[24569](info): TGS_REQ (4 etypes {18 17 16 23}) 10.250.3.150: ISSUE: authtime 1427873771, etypes {rep=18 tkt=18 ses=18}, sasher@EXAMPLE.COM for HTTP/sepdt138.example.com@EXAMPLE.COM
+Apr 01 03:36:11 ldap.example.com krb5kdc[24569](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+Apr 01 03:36:11 ldap.example.com krb5kdc[24549](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+Apr 01 03:36:11 ldap.example.com krb5kdc[24546](info): AS_REQ (4 etypes {18 17 16 23}) 10.250.17.96: NEEDED_PREAUTH: niz@EXAMPLE.COM for krbtgt/EXAMPLE.COM@EXAMPLE.COM, Additional pre-authentication required
+Apr 01 03:36:33 ldap.example.com krb5kdc[24556](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+Apr 01 03:36:36 ldap.example.com krb5kdc[24568](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:38:34 ldap.example.com krb5kdc[24551](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:39:43 ldap.example.com krb5kdc[24549](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:39:43 ldap.example.com krb5kdc[24557](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:40:01 ldap.example.com krb5kdc[24570](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:40:15 ldap.example.com krb5kdc[24557](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+Apr 01 03:40:18 ldap.example.com krb5kdc[24560](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+Apr 01 03:40:20 ldap.example.com krb5kdc[24555](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:40:21 ldap.example.com krb5kdc[24560](info): preauth (encrypted_timestamp) verify failure: No matching key in entry
+Apr 01 03:40:42 ldap.example.com krb5kdc[24573](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+Apr 01 03:40:42 ldap.example.com krb5kdc[24568](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed
+krb5kdc: setsockopt(7,IPV6_V6ONLY,1) worked
+"""
+
+
+def test_krb5kdc_log():
+    log = KerberosKDCLog(context_wrap(KRB5KDC_LOG))
+    assert len(log.lines) == 21
+    decrypt_failed_logs = list(log.get('Decrypt integrity check failed'))
+    assert len(decrypt_failed_logs) == 5
+    assert decrypt_failed_logs[0] == {
+        'line': 'Apr 01 03:36:33 ldap.example.com krb5kdc[24556](info): preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed',
+        'timestamp': 'Apr 01 03:36:33',
+        'system': 'ldap.example.com',
+        'service': 'krb5kdc',
+        'pid': '24556',
+        'level': 'info',
+        'message': 'preauth (encrypted_timestamp) verify failure: Decrypt integrity check failed'
+    }
+    # Lines that don't parse still get found and just have the 'line' key
+    assert len(list(log.get('setsockopt'))) == 1
+    assert list(log.get('setsockopt'))[0] == {
+        'line': 'krb5kdc: setsockopt(7,IPV6_V6ONLY,1) worked'
+    }
+
+    # test get_after, including continuation line
+    assert len(list(log.get_after(datetime(2016, 4, 1, 3, 40, 0)))) == 8
