03ce50c8edd23d6ab32421cdec95561f9872d56f
Paul Wayper <paulway@mabula.net>
2017-07-04 16:59:56 +1000

    Neutron plugin documentation (#172)
    * Documentation markup include file from template
    * Documenting NeutronPlugin parser
    * Keeping documentation to current standards

---
+++ docs/shared_parsers_catalog/neutron_plugin.rst
@@ -0,0 +1,4 @@
+.. automodule:: insights.parsers.neutron_plugin
+   :members:
+   :show-inheritance:
+
--- insights/parsers/neutron_plugin.py
+++ insights/parsers/neutron_plugin.py
@@ -1,36 +1,63 @@
+'''
+NeutronPlugin - file ``/etc/neutron/plugin.ini``
+================================================
+
+The ``NeutronPlugin`` class parses the Neutron plugin configuration file.
+See the ``IniConfigFile`` class for more usage information.
+'''
+
 from .. import parser, LegacyItemAccess, IniConfigFile
 
 
 @parser("neutron_plugin.ini")
 class NeutronPlugin(LegacyItemAccess, IniConfigFile):
     """
-    parsing plugin.ini and return dict.
-    :return: a dict(dict)   Example:
-    {
-        "ml2": {
-            "extension_drivers": "",
-            "mechanism_drivers": "openvswitch,linuxbridge",
-            "path_mtu": "0",
-            "tenant_network_types": "local,flat,vlan,gre,vxlan",
-            "type_drivers": "local,flat,vlan,gre,vxlan"
-        },
-        "ml2_type_flat": {
-            "flat_networks": "*"
-        },
-        "ml2_type_geneve": {},
-        "ml2_type_gre": {
-            "tunnel_id_ranges": "20:100"
-        },
-        "ml2_type_vlan": {
-            "network_vlan_ranges": "physnet1:1000:2999"
-        },
-        "ml2_type_vxlan": {
-            "vni_ranges": "10:100",
-            "vxlan_group": "224.0.0.1"
-        },
-        "securitygroup": {
-            "enable_security_group": "True"
-        }
-    }
+    Parse the ``/etc/neutron/plugin.ini`` configuration file.
+
+    Sample configuration file::
+
+        [ml2]
+        type_drivers = local,flat,vlan,gre,vxlan
+        tenant_network_types = local,flat,vlan,gre,vxlan
+        mechanism_drivers =openvswitch,linuxbridge
+        extension_drivers =
+
+        [ml2_type_flat]
+        flat_networks =*
+        # Example:flat_networks = physnet1,physnet2
+        # Example:flat_networks = *
+
+        [ml2_type_vlan]
+        network_vlan_ranges =physnet1:1000:2999
+        # Example: network_vlan_ranges = physnet1:1000:2999,physnet2
+
+        [ml2_type_gre]
+        tunnel_id_ranges =20:100
+
+        [ml2_type_vxlan]
+        vni_ranges =10:100
+        vxlan_group =224.0.0.1
+
+        [ml2_type_geneve]
+        # (ListOpt) Comma-separated list of <vni_min>:<vni_max> tuples enumerating
+        # ranges of Geneve VNI IDs that are available for tenant network allocation.
+        #
+        # vni_ranges =
+
+        [securitygroup]
+        enable_security_group = True
+
+    Examples:
+
+        >>> nconf = shared[NeutronPlugin]
+        >>> 'ml2' in nconf
+        True
+        >>> nconf.has_option('ml2', 'type_drivers')
+        True
+        >>> nconf.get("ml2_type_flat", "flat_networks")
+        "*"
+        >>> nconf.items('ml2_type_vxlan')
+        {'vni_ranges': '10:100',
+         'vxlan_group': '224.0.0.1'}
     """
     pass
