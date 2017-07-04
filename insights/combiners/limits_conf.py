f7b5d59226c7a2f292bab8d2aa8d3cb13126aaf1
Paul Wayper <paulway@mabula.net>
2017-07-04 19:54:27 +1000

    Limits conf combiner (#70)
    * More efficient and neater search loop thanks to Bob Fahr
    * Changing limits.conf to a PatternSpec to match limits.d
    * Main implementation and documentation
    * Test AllLimitsConf combiner, all tests pass, 100% code coverage
    * Create limits_conf combiner documentation
    * Fix flake8 space requirements
    * Store only last limit found
    * Test that only the last limit found is stored
    * Enforce sorting by name (limits.conf comes before limits.d)

---
+++ insights/combiners/limits_conf.py
@@ -0,0 +1,75 @@
+"""
+Limits configuration
+====================
+
+The limits files are normally available to rules as a list of LimitsConf
+objects.  This combiner turns those into one set of data, and provides a
+``find_all()`` method to search the rules from all the files.
+"""
+
+from insights.core.plugins import combiner
+from insights.parsers.limits_conf import LimitsConf
+
+
+@combiner(requires=[LimitsConf])
+class AllLimitsConf(object):
+    """
+    Combiner for accessing all the limits configuration files.
+
+    Attributes:
+        domains(set): the set of domains found in all data files.
+        limits(list): a list of the original LimitsConf parser results.
+        rules(list): the entire list of rules.
+    """
+    def __init__(self, local, shared):
+        rules = []
+        domains = set([])
+        self.limits = shared[LimitsConf]
+        # Store the tuple of domain, type and item against the row of that
+        # limit in our full list of rules.
+        rule_tuples_found = {}
+        rule_number = 0
+        for limits in sorted(shared[LimitsConf], key=lambda f: f.file_path):
+            # The rule dictionary contains the file_path as a key for later.
+            # Make sure that we keep only the last rule for this combination
+            # of domain, type and item, according to https://access.redhat.com/solutions/199993
+            for rule in limits.rules:
+                rule_tuple = (rule['domain'], rule['type'], rule['item'])
+                if rule_tuple in rule_tuples_found:
+                    row_number = rule_tuples_found[rule_tuple]
+                    rules[row_number] = rule
+                else:
+                    rules.append(rule)
+                    rule_tuples_found[rule_tuple] = rule_number
+                    rule_number += 1
+            # The domains update is much easier :-)
+            domains.update(limits.domains)
+
+        self.rules = rules
+        self.domains = domains
+        super(AllLimitsConf, self).__init__()
+
+    def find_all(self, **kwargs):
+        """
+        Find all the rules that match the given parameters.  We cheat a bit
+        here and combine the results from the find_all() method from the
+        original parsers.  Otherwise we'd have to reimplement the _matches
+        method from the LimitsConf class.
+
+        Examples:
+            >>> data = shared[AllLimitsConf]
+            >>> results = data.find_all(domain='nproc')
+            >>> len(results)
+            1
+            >>> results[0]['domain']
+            'nproc'
+
+        Parameters:
+            **kwargs(dict): key-value pairs for the search data.
+
+        Returns:
+            (list): a list of the rules matching the given keywords, as
+                determined by the ``_matches()`` method in the ``LimitsConf``
+                class.
+        """
+        return [match for data in self.limits for match in data.find_all(**kwargs)]
---
+++ insights/combiners/tests/test_limits_conf.py
@@ -0,0 +1,53 @@
+from insights.parsers.limits_conf import LimitsConf
+from insights.combiners.limits_conf import AllLimitsConf
+from insights.tests import context_wrap
+
+LIMITS_CONF = """
+#oracle soft nproc 2047
+#oracle hard nproc 16384
+oracle soft nofile 1024
+oracle hard nofile 65536
+oracle soft stack 10240
+oracle hard stack 3276
+root       soft    nproc     unlimited
+""".strip()
+
+LIMITS_D_CONF = """
+@jackuser - rtprio 70
+@jackuser - memlock 4194304
+""".strip()
+
+DUP_LIMITS_D_CONF = """
+oracle hard stack 1926
+"""
+
+LIMITS_CONF_PATH = "etc/security/limits.conf"
+LIMITS_D_PATH = "etc/security/limits.d/95-jack.conf"
+DUP_LIMITS_D_CONF_PATH = "etc/security/limits.d/oracle.conf"
+
+
+def test_all_limits_conf():
+    data1 = LimitsConf(context_wrap(LIMITS_CONF, path=LIMITS_CONF_PATH))
+    data2 = LimitsConf(context_wrap(LIMITS_D_CONF, path=LIMITS_D_PATH))
+    context = {LimitsConf: [data1, data2]}
+    all_data = AllLimitsConf({}, context)
+
+    assert len(all_data.rules) == 7
+    assert all_data.rules[0] == {'domain': 'oracle', 'type': 'soft', 'item': 'nofile', 'value': 1024, 'file': LIMITS_CONF_PATH}
+    assert all_data.rules[1] == {'domain': 'oracle', 'type': 'hard', 'item': 'nofile', 'value': 65536, 'file': LIMITS_CONF_PATH}
+    assert all_data.rules[2] == {'domain': 'oracle', 'type': 'soft', 'item': 'stack', 'value': 10240, 'file': LIMITS_CONF_PATH}
+    assert all_data.rules[3] == {'domain': 'oracle', 'type': 'hard', 'item': 'stack', 'value': 3276, 'file': LIMITS_CONF_PATH}
+    assert all_data.rules[4] == {'domain': 'root', 'type': 'soft', 'item': 'nproc', 'value': -1, 'file': LIMITS_CONF_PATH}
+    assert all_data.rules[5] == {'domain': '@jackuser', 'type': '-', 'item': 'rtprio', 'value': 70, 'file': LIMITS_D_PATH}
+    assert all_data.rules[6] == {'domain': '@jackuser', 'type': '-', 'item': 'memlock', 'value': 4194304, 'file': LIMITS_D_PATH}
+
+    assert all_data.domains == set({'oracle', 'root', '@jackuser'})
+
+    assert all_data.find_all(domain='root') == [all_data.rules[4]]
+
+    data3 = LimitsConf(context_wrap(DUP_LIMITS_D_CONF, path=DUP_LIMITS_D_CONF_PATH))
+    context = {LimitsConf: [data1, data3]}
+    all_data = AllLimitsConf({}, context)
+    # Check de-duplication of rules
+    assert len(all_data.rules) == 5  # No extra rule
+    assert all_data.rules[3] == {'domain': 'oracle', 'type': 'hard', 'item': 'stack', 'value': 1926, 'file': DUP_LIMITS_D_CONF_PATH}
