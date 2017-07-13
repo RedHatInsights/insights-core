83c0f3087130e37deca014d1d76840702bd7652f
Paul Wayper <paulway@mabula.net>
2017-07-13 16:48:17 +1000

    Heat_log parser add get_after for 1.x branch (#205)
    * Adding method from LogFileOutput
    
    * Testing new method
    
    * Correction: get_after yields, not returns
    
    * Updated to time_format property

--- insights/parsers/tests/test_heat_log.py
+++ insights/parsers/tests/test_heat_log.py
@@ -2,6 +2,8 @@ from insights.parsers.heat_log import HeatApiLog
 from insights.parsers.heat_log import HeatEngineLog
 from insights.tests import context_wrap
 
+from datetime import datetime
+
 
 HEAT_API_LOG = """
 2016-11-09 14:39:29.223 3844 WARNING oslo_config.cfg [-] Option "rpc_backend" from group "DEFAULT" is deprecated for removal.  Its value may be silently ignored in the future.
@@ -18,15 +20,17 @@ HEAT_ENGINE_LOG = """
 2016-11-09 14:32:44.158 4392 WARNING heat.engine.environment [-] OS::Neutron::RouterGateway is HIDDEN. Use the `external_gateway_info` property in the router resource to set up the gateway.
 2016-11-09 14:32:44.160 4392 INFO heat.engine.environment [-] Loading /etc/heat/environment.d/default.yaml
 2016-11-09 14:32:44.168 4392 INFO heat.engine.environment [-] Registered: [Plugin](User:False) AWS::EC2::Instance -> <class 'heat.engine.resources.aws.ec2.instance.Instance'>
-2016-11-09 14:32:44.169 4392 INFO heat.engine.environment [-] Registered: [Plugin](User:False) OS::Heat::AccessPolicy -> <class 'heat.engine.resources.openstack.heat.access_policy.AccessPolicy'>
+2016-11-09 14:32:45.169 4392 INFO heat.engine.environment [-] Registered: [Plugin](User:False) OS::Heat::AccessPolicy -> <class 'heat.engine.resources.openstack.heat.access_policy.AccessPolicy'>
 """
 
 
 def test_heat_api_log():
     log = HeatApiLog(context_wrap(HEAT_API_LOG))
     assert len(log.get('INFO')) == 3
+    assert len(list(log.get_after(datetime(2016, 11, 9, 14, 39, 30)))) == 4
 
 
 def test_heat_engine_log():
     log = HeatEngineLog(context_wrap(HEAT_ENGINE_LOG))
     assert len(log.get('INFO')) == 3
+    assert len(list(log.get_after(datetime(2016, 11, 9, 14, 32, 45)))) == 1
