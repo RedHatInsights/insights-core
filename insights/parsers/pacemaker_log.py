06503ed1f7d9e867a6ea0ca19d63d626adcec9e2
Paul Wayper <paulway@mabula.net>
2017-07-13 16:20:12 +1000

    Pacemaker_log parser add get_after for 1.x branch (#192)
    * Documentation markup include file from template
    
    * Writing documentation and adding get_after()
    
    * Testing new get_after() method
    
    * Correction: get_after yields, not returns
    
    * Adding tests for get_after()
    
    * Updating to just use time_format from LogFileOutput

--- insights/parsers/pacemaker_log.py
+++ insights/parsers/pacemaker_log.py
@@ -1,13 +1,35 @@
+"""
+PacemakerLog - file ``/var/log/pacemaker.log``
+==============================================
+"""
+
 from .. import LogFileOutput, parser
 
 
 @parser('pacemaker.log')
 class PacemakerLog(LogFileOutput):
     """
-    ----------------- example of pacemaker.log --------------------------
-    Aug 21 12:58:40 [11656] example.redhat.com        cib:     info: crm_client_destroy: 	Destroying 0 events
-    Aug 21 12:59:53 [11655] example.redhat.comm pacemakerd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
-    Aug 21 12:59:53 [11661] example.redhat.comm       crmd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
-    Aug 21 12:59:53 [11655] example.redhat.com  pacemakerd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
+    Read the pacemaker log file.  Uses the ``LogFileOutput`` class parser
+    functionality - see the base class for more details.
+
+    Sample pacemaker.log::
+
+        Aug 21 12:58:40 [11656] example.redhat.com        cib:     info: crm_client_destroy: 	Destroying 0 events
+        Aug 21 12:59:53 [11655] example.redhat.com pacemakerd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
+        Aug 21 12:59:53 [11661] example.redhat.com       crmd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
+        Aug 21 12:59:53 [11655] example.redhat.com pacemakerd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)
+
+    Note:
+        Because pacemaker timestamps by default have no year, the
+        year of the logs will be inferred from the year in your timestamp.
+        This will also work around December/January crossovers.
+
+    Examples:
+        >>> pm = shared(PacemakerLog)
+        >>> pm.get('crmd')
+        ['Aug 21 12:59:53 [11661] example.redhat.comm       crmd:     info: pcmk_quorum_notification: 	Membership 12: quorum retained (3)']
+        >>> from datetime import datetime
+        >>> len(list(pm.get_after(datetime(21, 8, 2017, 12, 59, 50))))
+        3
     """
-    pass
+    time_format = '%b %d %H:%M:%S'
--- insights/parsers/tests/test_pacemaker_log.py
+++ insights/parsers/tests/test_pacemaker_log.py
@@ -1,6 +1,8 @@
 from insights.parsers.pacemaker_log import PacemakerLog
 from insights.tests import context_wrap
 
+from datetime import datetime
+
 PACEMAKER_LOG = """
 Aug 21 12:58:32 [11661] example.redhat.com       crmd:     info: crm_timer_popped: 	PEngine Recheck Timer (I_PE_CALC) just popped (900000ms)
 Aug 21 12:58:32 [11661] example.redhat.com       crmd:   notice: do_state_transition: 	State transition S_IDLE -> S_POLICY_ENGINE [ input=I_PE_CALC cause=C_TIMER_POPPED origin=crm_timer_popped ]
@@ -11,10 +13,11 @@ Aug 21 12:58:32 [11660] example.redhat.com    pengine:     info: determine_onlin
 Aug 21 12:58:32 [11660] example.redhat.com    pengine:     info: determine_online_status: 	Node d-gqynnw1 is online
 Aug 21 12:58:32 [11660] example.redhat.com    pengine:   notice: stage6: 	Delaying fencing operations until there are resources to manage
 Aug 21 12:58:32 [11660] example.redhat.com    pengine:   notice: process_pe_message: 	Calculated Transition 125: /var/lib/pacemaker/pengine/pe-input-14.bz2
-Aug 21 12:58:32 [11661] example.redhat.com       crmd:     info: do_state_transition: 	State transition S_POLICY_ENGINE -> S_TRANSITION_ENGINE [ input=I_PE_SUCCESS
+Aug 21 12:58:33 [11661] example.redhat.com       crmd:     info: do_state_transition: 	State transition S_POLICY_ENGINE -> S_TRANSITION_ENGINE [ input=I_PE_SUCCESS
 """
 
 
 def test_pacemaker_log():
     pacemaker = PacemakerLog(context_wrap(PACEMAKER_LOG))
     assert "Progressed to state S_POLICY_ENGINE after C_TIMER_POPPED" in pacemaker
+    assert len(list(pacemaker.get_after(datetime(2017, 8, 21, 12, 58, 30)))) == 10
