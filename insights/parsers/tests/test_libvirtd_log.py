26d58b68ec70ef49dfeba03812e92555e8be91b8
Paul Wayper <paulway@mabula.net>
2017-07-13 16:54:03 +1000

    Libvirtd_log parser add get_after for master branch (#242)
    * Testing new get_after() method
    
    * Correction: get_after yields, not returns
    
    * Updating to just use time_format from LogFileOutput

--- insights/parsers/tests/test_libvirtd_log.py
+++ insights/parsers/tests/test_libvirtd_log.py
@@ -1,9 +1,11 @@
 from insights.parsers.libvirtd_log import LibVirtdLog
 from insights.tests import context_wrap
 
+from datetime import datetime
+
 LIBVIRTD_LOG = """
-2013-10-23 17:32:19.909+0000: 14069: debug : virConnectOpen:1331 : name=qemu+tls://AA.BB.CC.DD/system
-2013-10-23 17:32:19.909+0000: 14069: debug : virConnectGetConfigFile:953 : Loading config file '/etc/libvirt/libvirt.conf'
+2013-10-23 17:32:18.909+0000: 14069: debug : virConnectOpen:1331 : name=qemu+tls://AA.BB.CC.DD/system
+2013-10-23 17:32:18.909+0000: 14069: debug : virConnectGetConfigFile:953 : Loading config file '/etc/libvirt/libvirt.conf'
 2013-10-23 17:32:19.909+0000: 14069: debug : do_open:1127 : name "qemu+tls://AA.BB.CC.DD/system" to URI components:
   scheme qemu+tls
   server AA.BB.CC.DD
@@ -20,7 +22,7 @@ LIBVIRTD_LOG = """
 2013-10-23 17:32:19.957+0000: 14069: warning : virNetTLSContextCheckCertificate:1102 : Certificate check failed Certificate [session] owner does not match the hostname AA.BB.CC.DD
 2013-10-23 17:32:19.957+0000: 14069: error : virNetTLSContextCheckCertificate:1105 : authentication failed: Failed to verify peer's certificate
 
-"""
+""".strip()
 
 
 def test_libvirtd_log():
@@ -28,3 +30,5 @@ def test_libvirtd_log():
     assert "Certificate check failed Certificate" in log
 
     assert log.get("authentication failed: Failed to verify peer's certificate") == ["2013-10-23 17:32:19.957+0000: 14069: error : virNetTLSContextCheckCertificate:1105 : authentication failed: Failed to verify peer's certificate"]
+    # Includes continuation of URI component expansion
+    assert len(list(log.get_after(datetime(2013, 10, 23, 17, 32, 19)))) == 15
