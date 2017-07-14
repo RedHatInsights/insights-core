09a37b6153149c4d0a45efb5995ce014cf614552
Paul Wayper <paulway@mabula.net>
2017-07-14 14:32:53 +1000

    Vdsm_log parser gets full documentation and get_after() method for 1.x branch (#214)
    * Documentation markup include file from template
    
    * Adding documentation and get_after() method
    
    * Complete testing
    
    * Updating to just use time_format from LogFileOutput

--- insights/parsers/tests/test_vdsm_log.py
+++ insights/parsers/tests/test_vdsm_log.py
@@ -1,17 +1,90 @@
-import re
 from insights.parsers.vdsm_log import VDSMLog
 from insights.tests import context_wrap
 
-LOG_REGEX = re.compile(r"Migration is stuck: Hasn't progressed in (\d)+(.)(\d)+ seconds. Aborting.")
+from datetime import datetime
 
-VDSM_LOG = """
-Thread-13::DEBUG::2016-07-13 04:45:39,371::fileSD::262::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '0+1 records in\n0+1 records out\n361 bytes (361 B) copied, 0.0012658 s, 285 kB/s\n'; <rc> = 0
-Migration is stuck: Hasn't progressed in 300.093597889 seconds. Aborting.
-jsonrpc.Executor-worker-2::INFO::2016-07-13 04:45:51,454::logUtils::44::dispatcher::(wrapper) Run and protect: repoStats(options=None)
-
+# This log commonly has \n in it - hence r""
+VDSM_LOG = r"""
+Thread-60::DEBUG::2015-05-04 00:01:07,490::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\n1+0 records out\n4096 bytes (4.1 kB) copied, 0.000147196 s, 27.8 MB/s\n'; <rc> = 0
+Thread-64::DEBUG::2015-05-04 00:01:07,894::blockSD::600::Storage.Misc.excCmd::(getReadDelay) '/bin/dd if=/dev/fe4d8910-ac67-4307-a3e7-be8a56d1c559/metadata iflag=direct of=/dev/null bs=4096 count=1' (cwd None)
+Thread-64::DEBUG::2015-05-04 00:01:07,909::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\n1+0 records out\n4096 bytes (4.1 kB) copied, 0.00020047 s, 20.4 MB/s\n'; <rc> = 0
+Thread-63::DEBUG::2015-05-04 00:01:08,050::blockSD::600::Storage.Misc.excCmd::(getReadDelay) '/bin/dd if=/dev/b0c17a6d-c5a5-4646-b4b5-edd85bc658db/metadata iflag=direct of=/dev/null bs=4096 count=1' (cwd None)
+Thread-63::DEBUG::2015-05-04 00:01:08,061::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\n1+0 records out\n4096 bytes (4.1 kB) copied, 0.000632011 s, 6.5 MB/s\n'; <rc> = 0
+VM Channels Listener::DEBUG::2015-05-04 00:01:10,830::vmChannels::95::vds::(_handle_timeouts) Timeout on fileno 47.
+Thread-13::DEBUG::2015-05-04 00:01:15,853::task::595::TaskManager.Task::(_updateState) Task=`57faf240-0efe-44e2-8485-ffa182bbc9dd`::moving from state init -> state preparing
+Thread-13::INFO::2015-05-04 00:01:15,854::logUtils::44::dispatcher::(wrapper) Run and protect: repoStats(options=None)
+Thread-13::INFO::2015-05-04 00:01:15,854::logUtils::47::dispatcher::(wrapper) Run and protect: repoStats, Return response: {u'cf7dab23-6b5b-45f4-9e27-ab06fbc01759': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000153278', 'lastCheck': '8.8', 'valid': True}, u'5a30691d-4fae-4023-ae96-50704f6b253c': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000147196', 'lastCheck': '8.4', 'valid': True}, u'b0c17a6d-c5a5-4646-b4b5-edd85bc658db': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000632011', 'lastCheck': '7.8', 'valid': True}, u'fe4d8910-ac67-4307-a3e7-be8a56d1c559': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.00020047', 'lastCheck': '7.9', 'valid': True}, u'e70cce65-0d02-4da4-8781-6aeeef5c86ff': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000208457', 'lastCheck': '9.6', 'valid': True}, u'c3a10b1e-574e-4edd-ad00-a59cacc705b5': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000201237', 'lastCheck': '8.8', 'valid': True}}
 """
 
 
 def test_vdsm_log():
     vdsm_log = VDSMLog(context_wrap(VDSM_LOG))
-    assert "Migration is stuck:" in vdsm_log
+    assert "VM Channels Listener" in vdsm_log
+    test_newline_handling = vdsm_log.get('SUCCESS')
+    assert len(test_newline_handling) == 3
+    assert test_newline_handling[0] == "Thread-60::DEBUG::2015-05-04 00:01:07,490::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\\n1+0 records out\\n4096 bytes (4.1 kB) copied, 0.000147196 s, 27.8 MB/s\\n'; <rc> = 0"
+    parsed_newlines = list(vdsm_log.parse_lines(test_newline_handling))
+    assert parsed_newlines[0] == {
+        'raw_line': "Thread-60::DEBUG::2015-05-04 00:01:07,490::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\\n1+0 records out\\n4096 bytes (4.1 kB) copied, 0.000147196 s, 27.8 MB/s\\n'; <rc> = 0",
+        'thread': "Thread-60",
+        'level': "DEBUG",
+        'timestamp': "2015-05-04 00:01:07,490",
+        'datetime': datetime(2015, 5, 4, 0, 1, 7),
+        'module': "blockSD",
+        'line': "600",
+        'logname': "Storage.Misc.excCmd",
+        'funcname': "getReadDelay",
+        'message': "SUCCESS: <err> = '1+0 records in\\n1+0 records out\\n4096 bytes (4.1 kB) copied, 0.000147196 s, 27.8 MB/s\\n'; <rc> = 0"
+    }
+    # test get_after()
+    assert len(list(vdsm_log.get_after(datetime(2015, 5, 4, 0, 1, 10)))) == 4
+    assert len(list(vdsm_log.get_after(
+        datetime(2015, 5, 4, 0, 1, 10), vdsm_log.get('dispatcher')
+    ))) == 2
+    assert len(list(vdsm_log.get_after(
+        datetime(2015, 5, 4, 0, 1, 10), vdsm_log.parse_lines(vdsm_log.get('dispatcher'))
+    ))) == 2
+
+
+BAD_VDSM_LOG = r"""
+Thread-13::INFO::2015-33-45 00:01:15,854
+Thread-13::INFO::2015-05-04 00:01:15,854::
+Thread-13::INFO::2015-05-04 00:01:15,854::logUtils::47::dispatcher::(wrapper) Run and protect: repoStats, Return response: {u'cf7dab23-6b5b-45f4-9e27-ab06fbc01759': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000153278', 'lastCheck': '8.8', 'valid': True}, u'5a30691d-4fae-4023-ae96-50704f6b253c': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000147196', 'lastCheck': '8.4', 'valid': True}, u'b0c17a6d-c5a5-4646-b4b5-edd85bc658db': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000632011', 'lastCheck': '7.8', 'valid': True}, u'fe4d8910-ac67-4307-a3e7-be8a56d1c559': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.00020047', 'lastCheck': '7.9', 'valid': True}, u'e70cce65-0d02-4da4-8781-6aeeef5c86ff': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000208457', 'lastCheck': '9.6', 'valid': True}, u'c3a10b1e-574e-4edd-ad00-a59cacc705b5': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000201237', 'lastCheck': '8.8', 'valid': True}}
+FATAL ERROR
+"""
+
+
+def test_bad_vdsm_log_parsing():
+    vdsm_log = VDSMLog(context_wrap(BAD_VDSM_LOG))
+    parsed = list(vdsm_log.parse_lines(vdsm_log.lines))
+    assert len(parsed) == 4
+    assert parsed[0] == {
+        'raw_line': 'Thread-13::INFO::2015-33-45 00:01:15,854',
+        'thread': 'Thread-13',
+        'level': 'INFO',
+        'timestamp': '2015-33-45 00:01:15,854',
+    }
+    assert parsed[1] == {
+        'raw_line': 'Thread-13::INFO::2015-05-04 00:01:15,854::',
+        'thread': 'Thread-13',
+        'level': 'INFO',
+        'timestamp': '2015-05-04 00:01:15,854',
+        'datetime': datetime(2015, 5, 4, 0, 1, 15),
+        'module': ''  # vestigial field
+    }
+    assert parsed[2] == {
+        'raw_line': "Thread-13::INFO::2015-05-04 00:01:15,854::logUtils::47::dispatcher::(wrapper) Run and protect: repoStats, Return response: {u'cf7dab23-6b5b-45f4-9e27-ab06fbc01759': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000153278', 'lastCheck': '8.8', 'valid': True}, u'5a30691d-4fae-4023-ae96-50704f6b253c': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000147196', 'lastCheck': '8.4', 'valid': True}, u'b0c17a6d-c5a5-4646-b4b5-edd85bc658db': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000632011', 'lastCheck': '7.8', 'valid': True}, u'fe4d8910-ac67-4307-a3e7-be8a56d1c559': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.00020047', 'lastCheck': '7.9', 'valid': True}, u'e70cce65-0d02-4da4-8781-6aeeef5c86ff': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000208457', 'lastCheck': '9.6', 'valid': True}, u'c3a10b1e-574e-4edd-ad00-a59cacc705b5': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000201237', 'lastCheck': '8.8', 'valid': True}}",
+        'thread': 'Thread-13',
+        'level': 'INFO',
+        'timestamp': '2015-05-04 00:01:15,854',
+        'datetime': datetime(2015, 5, 4, 0, 1, 15),
+        'module': 'logUtils',
+        'line': '47',
+        'logname': 'dispatcher',
+        'funcname': 'wrapper',
+        'message': "Run and protect: repoStats, Return response: {u'cf7dab23-6b5b-45f4-9e27-ab06fbc01759': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000153278', 'lastCheck': '8.8', 'valid': True}, u'5a30691d-4fae-4023-ae96-50704f6b253c': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000147196', 'lastCheck': '8.4', 'valid': True}, u'b0c17a6d-c5a5-4646-b4b5-edd85bc658db': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000632011', 'lastCheck': '7.8', 'valid': True}, u'fe4d8910-ac67-4307-a3e7-be8a56d1c559': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.00020047', 'lastCheck': '7.9', 'valid': True}, u'e70cce65-0d02-4da4-8781-6aeeef5c86ff': {'code': 0, 'version': 3, 'acquired': True, 'delay': '0.000208457', 'lastCheck': '9.6', 'valid': True}, u'c3a10b1e-574e-4edd-ad00-a59cacc705b5': {'code': 0, 'version': 0, 'acquired': True, 'delay': '0.000201237', 'lastCheck': '8.8', 'valid': True}}"
+    }
+    assert parsed[3] == {
+        'raw_line': 'FATAL ERROR',
+        'thread': 'FATAL ERROR'
+    }
--- insights/parsers/vdsm_log.py
+++ insights/parsers/vdsm_log.py
@@ -1,14 +1,94 @@
-from .. import LogFileOutput, parser
+"""
+VDSMLog - file ``/var/log/vdsm/vdsm.log``
+=========================================
+"""
+
+from insights import LogFileOutput, parser
+
+from datetime import datetime
 
 
 @parser('vdsm.log')
 class VDSMLog(LogFileOutput):
     """
-    ----------------- Output sample of command lspci --------------------------
-    00:00.0 Host bridge: Intel Corporation 2nd Generation Core Processor Family DRAM Controller (rev 09)
-    00:02.0 VGA compatible controller: Intel Corporation 2nd Generation Core Processor Family Integrated Graphics Controller (rev 09)
-    03:00.0 Network controller: Intel Corporation Centrino Advanced-N 6205 [Taylor Peak] (rev 34)
-    0d:00.0 System peripheral: Ricoh Co Ltd PCIe SDXC/MMC Host Controller (rev 07)
-    ----------------- Output sample of command lspci --------------------------
+    Logs from the Virtual Desktop and Server Manager.
+
+    Uses ``LogFileOutput`` as the base class - see its documentation for
+    more specific usage details.
+
+    Sample logs from VDSM::
+
+        Thread-60::DEBUG::2015-05-08 18:01:03,071::blockSD::600::Storage.Misc.excCmd::(getReadDelay) '/bin/dd if=/dev/5a30691d-4fae-4023-ae96-50704f6b253c/metadata iflag=direct of=/dev/null bs=4096 count=1' (cwd None)
+        Thread-60::DEBUG::2015-05-08 18:01:03,090::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\\n1+0 records out\\n4096 bytes (4.1 kB) copied, 0.00038933 s, 10.5 MB/s\\n'; <rc> = 0
+        Thread-65::DEBUG::2015-05-08 18:01:04,835::blockSD::600::Storage.Misc.excCmd::(getReadDelay) '/bin/dd if=/dev/e70cce65-0d02-4da4-8781-6aeeef5c86ff/metadata iflag=direct of=/dev/null bs=4096 count=1' (cwd None)
+        Thread-65::DEBUG::2015-05-08 18:01:04,857::blockSD::600::Storage.Misc.excCmd::(getReadDelay) SUCCESS: <err> = '1+0 records in\\n1+0 records out\\n4096 bytes (4.1 kB) copied, 0.000157193 s, 26.1 MB/s\\n'; <rc> = 0
+        Thread-4662::DEBUG::2015-05-08 18:01:05,560::task::595::TaskManager.Task::(_updateState) Task=`9a7948f6-b6d9-42c2-b91f-7e0346dfc1d6`::moving from state init -> state preparing
+
+    Usage:
+        >>> log = shared[VDSMLog]
+        >>> log.get('TaskManager')
+        'Thread-4662::DEBUG::2015-05-08 18:01:05,560::task::595::TaskManager.Task::(_updateState) Task=`9a7948f6-b6d9-42c2-b91f-7e0346dfc1d6`::moving from state init -> state preparing'
+        >>> list(log.parse_lines(log.get('TaskManager')))[0]  # from generator to list to subscript
+        {'raw_line': 'Thread-4662::DEBUG::2015-05-08 18:01:05,560::task::595::TaskManager.Task::(_updateState) Task=`9a7948f6-b6d9-42c2-b91f-7e0346dfc1d6`::moving from state init -> state preparing',
+         'thread': 'Thread-4662',
+         'level': 'DEBUG',
+         'timestamp': '2015-05-08 18:01:05,560',
+         'datetime': datetime(2015, 5, 8, 18, 1, 5),
+         'module': 'task',
+         'line': '595',
+         'logname': 'TaskManager.Task',
+         'funcname': '_updateState',
+         'message': 'Task=`9a7948f6-b6d9-42c2-b91f-7e0346dfc1d6`::moving from state init -> state preparing'
+        }
     """
-    pass
+
+    def parse_lines(self, lines):
+        """
+        Parse log lines, e.g. from keep_scan or get, into fields
+
+        Parameters:
+            lines(list): List of lines to parse
+
+        Yields:
+            Dictionaries corresponding to the data in each line split into
+            functional elements:
+
+            * **raw_line** - the original unparsed line
+            * **thread** - the thread or task ID
+            * **level** - the log level
+            * **timestamp** - the date of the log line (as a string)
+            * **datetime** - the date as a datetime object (if conversion is possible)
+            * **module** - the module logging the message
+            * **line** - the line reporting this message within the module
+            * **logname** - the logger's name
+            * **funcname** - the function name reporting the message
+            * **message** - the body of the message
+
+            The **datetime** field is only present if the `timestamp` field
+            is present contains a time stamp in the valid format.
+
+            If the line is too short, for some reason, then as many fields as
+            possible are pulled from the line.
+        """
+
+        # funcname is in brackets before message, so parse those separately
+        fieldnames = ('thread', 'level', 'timestamp', 'module', 'line', 'logname')
+        for line in lines:
+            parts = line.split('::', 6)
+            fields = {'raw_line': line}
+            fields.update({k: v for k, v in zip(fieldnames, parts)})
+            if len(parts) == 7:
+                func, msg = parts[6].split(' ', 1)
+                fields['funcname'] = func[1:-1]  # remove surrounding brackets
+                fields['message'] = msg
+            # Did we get a timestamp in there?
+            if 'timestamp' in fields:
+                # Try to convert the datetime if possible
+                try:
+                    fields['datetime'] = datetime.strptime(
+                        fields['timestamp'][:-4],  # skip milliseconds
+                        self.time_format
+                    )
+                except:
+                    pass
+            yield fields
