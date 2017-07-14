54182333c0e0ad7e822992b84a7a395865e1e27d
Xiaoxue Wang <xiaoxwan@redhat.com>
2017-07-14 01:19:17 -0500

    Add 'macpools' field into RHEV metadata (#161)

--- insights/parsers/multinode.py
+++ insights/parsers/multinode.py
@@ -90,7 +90,7 @@ class RHEV(MultinodeMetadata):
         "storagedomains", "hosts", "api_version",
         "datacenters", "networks", "rhev_version",
         "coordinator_version", "diskprofiles", "vms",
-        "clusters"
+        "clusters", "macpools"
     ]
 
 
