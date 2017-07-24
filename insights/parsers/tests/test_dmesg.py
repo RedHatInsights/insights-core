36824d6d04a0df56087f7ec16c39fc5d08e9facd
Paul Wayper <paulway@mabula.net>
2017-07-24 18:01:57 +1000

    Dmesg `log_after` method for master branch (#273)
    * Implementing get_after() method similar to LogFileOutput but using seconds since boot
    * Testing new get_after() method
      Conflicts: insights/parsers/tests/test_dmesg.py
    * Simplify the has_startswith message search code
    * The line regex will always match and contain a 'message' group, since it matches
      `.*` and everything else is optional.  Therefore we can use the regex match
      directly in the `any()` list comprehension.
    * Test that `has_startswith` is false when given prefixes that do not occur at the
      start of any line - including strings that occur elsewhere in a line.
    * Fix documentation formatting of lines parameter to `get_after()`
    
    * DmesgLineList has no filters here, so don't test them

--- insights/parsers/tests/test_dmesg.py
+++ insights/parsers/tests/test_dmesg.py
@@ -1,4 +1,3 @@
-from insights.config.factory import add_filter
 from insights.parsers.dmesg import DmesgLineList
 from insights.tests import context_wrap
 
@@ -22,7 +21,41 @@ HP HPSA Driver (v 3.4.4-1-RH2)
 lo: Dropping TSO features since no CSUM feature duplicated.
 """.strip()
 
-add_filter("dmesg", ["HPSA", "Dropping", "bonding:"])
+TS_MSGINFO = """
+[    0.000000] tsc: Fast TSC calibration using PIT
+[    0.000000] tsc: Detected 2693.827 MHz processor
+[    0.000026] Calibrating delay loop (skipped), value calculated using timer frequency.. 5387.65 BogoMIPS (lpj=2693827)
+[    0.000028] pid_max: default: 32768 minimum: 301
+[    0.000048] Security Framework initialized
+[    0.000053] SELinux:  Initializing.
+[    0.000059] SELinux:  Starting in permissive mode
+[    0.001043] Dentry cache hash table entries: 2097152 (order: 12, 16777216 bytes)
+[    0.003709] Inode-cache hash table entries: 1048576 (order: 11, 8388608 bytes)
+[    0.004791] Mount-cache hash table entries: 4096
+[    0.004944] Initializing cgroup subsys memory
+[    0.004958] Initializing cgroup subsys net_prio
+[    0.004982] CPU: Physical Processor ID: 0
+[    0.004983] CPU: Processor Core ID: 0
+[    0.004987] ENERGY_PERF_BIAS: Set to 'normal', was 'performance'
+[    0.004987] ENERGY_PERF_BIAS: View and update with x86_energy_perf_policy(8)
+[    0.005828] mce: CPU supports 9 MCE banks
+[    0.005841] CPU0: Thermal monitoring enabled (TM1)
+[    0.005851] Last level iTLB entries: 4KB 0, 2MB 0, 4MB 0
+[    0.005852] Last level dTLB entries: 4KB 64, 2MB 0, 4MB 0
+[    0.005853] tlb_flushall_shift: 6
+[    0.005933] Freeing SMP alternatives: 28k freed
+[    0.006814] ACPI: Core revision 20130517
+[    0.014594] ACPI: All ACPI Tables successfully acquired
+[    0.015670] ftrace: allocating 25815 entries in 101 pages
+[    0.024837] smpboot: Max logical packages: 2
+[    0.024847] DMAR: Host address width 39
+[    0.025063] Enabling x2apic
+[    0.025064] Enabled x2apic
+[    0.025069] Switched APIC routing to cluster x2apic.
+[    0.025482] ..TIMER: vector=0x30 apic1=0 pin1=2 apic2=-1 pin2=-1
+[    0.035484] smpboot: CPU0: Intel(R) Core(TM) i7-4800MQ CPU @ 2.70GHz (fam: 06, model: 3c, stepping: 03)
+[    0.035490] TSC deadline timer enabled
+"""
 
 
 def test_dmesg():
@@ -31,3 +64,16 @@ def test_dmesg():
     assert len(dmesg_info.get("lo:")) == 2
     assert "Dropping" in dmesg_info
     assert dmesg_info.has_startswith("bonding:")
+    assert not dmesg_info.has_startswith("xfs:")
+    assert list(dmesg_info.get_after(0.0001)) == [
+        '[    8.687252] HP HPSA Driver (v 3.4.4-1-RH2) 2.5.0',
+        'lo: Dropping TSO features since no CSUM feature duplicated.']
+
+    ts_info = DmesgLineList(context_wrap(TS_MSGINFO))
+    assert ts_info
+    assert ts_info.get('Processor Core ID') == ['[    0.004983] CPU: Processor Core ID: 0']
+    assert ts_info.has_startswith('ENERGY_PERF_BIAS')
+    assert not ts_info.has_startswith('Intel(R) Core(TM)')
+    assert 'tlb_flushall_shift' in ts_info
+    assert len(list(ts_info.get_after(0.024847))) == 7
+    assert len(list(ts_info.get_after(0.024847, lines=ts_info.get('x2apic')))) == 3
