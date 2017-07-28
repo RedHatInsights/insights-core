1071393e75d2523ed739a1873af18791dbe441af
Paul Wayper <paulway@mabula.net>
2017-07-28 14:24:00 +1000

    Improvements to VgDisplay for `vgdisplay -vv` command handling for 1.x branch (#272)
    * Fetch vgdisplay -vv if possible, or just vgdisplay
    
    * Improvements to VgDisplay for vgdisplay -vv command handling
    
    Substantial rewrite of the parsing of the output, both to cope with the
    extended information from ``vgdisplay -vv`` and to reduce the number of
    corner cases.
    
    This keeps the previous structure of a list of volume groups in the 'vginfo_dict' key
    of a 'data' dictionary.  All previous tests pass.  However, we significantly extend
    the keys stored, we store logical and physical volume data inside each volume group
    structure, and we ignore informational and verbose-status lines more easily.
    
    * Documentation markup include file from template
    
    * Documenting VgDisplay parser class
    
    * Removed old 'data' accessor in favour of other properties
    
    * Removing data dictionary property, updating documentation

--- insights/parsers/vgdisplay.py
+++ insights/parsers/vgdisplay.py
@@ -1,89 +1,155 @@
-from .. import parser, Parser, get_active_lines
-
-
-FILTER_INFO = list()
+"""
+VgDisplay - command ``vgdisplay``
+=================================
 
 """
-ERROR_KEY = "LVM_FILTER_ISSUE"
-"""
-FILTER_INFO.append("Couldn't find device with uuid")
-FILTER_INFO.append("physical volumes missing")
+
+from .. import parser, Parser
+
+import re
 
 
 @parser('vgdisplay')
 class VgDisplay(Parser):
     """
-    Returns a list of dicts and list contain debug info:
-    - the keys in each dict are the row headers
-    - each item in the list represents a VG or list contain debug info
-    - there is a empty line between different VG
-    {
-        "vginfo_dict":
-      [
-        {
-            'VG Name': 'rhel_hp-dl160g8-3',
-            'Format': 'lvm2',
-            'VG Access': 'read/write',
-            'Free  PE / Size': '11 / 44.00 MiB',
-            'Metadata Sequence No': '5',
-            'Alloc PE / Size': '119098 / 465.23 GiB',
-            'Total PE': '119109',
-            'VG UUID': 'by0Dl3-0lpB-MxEz-f6GO-9LYO-YRAQ-GufNZD',
-            'Metadata Areas': '1'
-        },
+    Parse the output of the ``vgdisplay -vv`` or ``vgdisplay`` commands.
 
-        {
-            'VG Name': 'rhel_hp-dl260g7-4',
-            'Format': 'lvm2',
-            'VG Access': 'read/write',
-            'Free  PE / Size': '11/ 44.00 MiB',
-            'VG UUID': 'by0Dl3-0lpB-MxEz-f6GO-9LYO-YRAQ-GufNZN',
-            'Alloc PE / Size': '119098 / 465.23 GiB'
-        }
+    The ``vg_list`` property is the main access to the list of volume groups
+    in the command output.  Each volume group is stored as a dictionary of
+    keys and values drawn from the property list of the volume group.  The
+    volume group's logical and physical volumes are stored in the 'Logical
+    Volumes' and 'Physical Volumes' sub-keys, respectively.
 
-        ],
+    Sample command output of ``vgdisplay -vv`` (pruned for clarity)::
 
-        "debug_info":
-        [
-            "Couldn't find device with uuid VVLmw8-e2AA-ECfW-wDPl-Vnaa-0wW1-utv7tV.",
-            "There are 1 physical volumes missing."
-        ]
-    }
+        Couldn't find device with uuid VVLmw8-e2AA-ECfW-wDPl-Vnaa-0wW1-utv7tV.
+
+        --- Volume group ---
+        VG Name               RHEL7CSB
+        System ID
+        Format                lvm2
+        Metadata Areas        1
+        Metadata Sequence No  13
+        ...
+
+        --- Logical volume ---
+        LV Path                /dev/RHEL7CSB/Root
+        LV Name                Root
+        VG Name                RHEL7CSB
+        LV Size                29.30 GiB
+        ...
+
+        --- Physical volumes ---
+        PV Name               /dev/mapper/luks-96c66446-77fd-4431-9508-f6912bd84194
+        PV UUID               EfWV9V-03CX-E6zc-JkMw-yQae-wdzp-Je1KUn
+        PV Status             allocatable
+        Total PE / Free PE    118466 / 4036
+
+    Volume groups are kept in the ``vg_list`` property in the order they were
+    found in the file.
+
+    Lines containing 'Couldn't find device with uuid' and 'missing physical
+    volume' are stored in a ``debug_info`` property.
+
+    Examples:
+        >>> vg_info = shared[VgDisplay]
+        >>> len(vg_info.vg_list)
+        1
+        >>> vgdata = vg_info.vg_list[0]
+        >>> vgdata['VG Name']
+        'RHEL7CSB'
+        >>> vgdata['VG Size']
+        '462.76 GiB'
+        >>> 'Logical Volumes' in vgdata
+        True
+        >>> lvs = vgdata['Logical Volumes']
+        >>> type(lvs)
+        dict
+        >>> lvs.keys()  # Note - keyed by device name
+        ['/dev/RHEL7CSB/Root']
+        >>> lvs['/dev/RHEL7CSB/Root']['LV Name']
+        'Root'
+        >>> lvs['/dev/RHEL7CSB/Root']['LV Size']
+        '29.30 GiB'
+        >>> 'Physical Volumes' in vgdata
+        True
+        >>> vgdata['Physical Volumes'].keys()
+        ['/dev/mapper/luks-96c66446-77fd-4431-9508-f6912bd84194']
+        >>> vgdata['Physical Volumes']['/dev/mapper/luks-96c66446-77fd-4431-9508-f6912bd84194']['PV UUID']
+        'EfWV9V-03CX-E6zc-JkMw-yQae-wdzp-Je1KUn'
+        >>> vg_info.debug_info
+        ["Couldn't find device with uuid VVLmw8-e2AA-ECfW-wDPl-Vnaa-0wW1-utv7tV."]
     """
 
+    _FILTER_INFO = [
+        "Couldn't find device with uuid",
+        "physical volumes missing",
+    ]
+
     def parse_content(self, content):
-        self.data = {}
-        vginfo_dict_list = []
-        vginfo_dict = {}
-        debug_info = []
-        for line in get_active_lines(content):
+        self.vg_list = []
+        self.debug_info = []
+
+        # Each section starts with a '(VG|LV|PV) Name' header.  This then
+        # sets the amount of space between the start of the key and the start
+        # of the value.  Unfortunately, there may be only one space between
+        # the key and the value and there are spaces in the key, so it's
+        # impossible to tell where the key stops and the value ends on space
+        # alone.  However, the header sets the left-most column the value
+        # starts at, so once we pick up the header we ignore the regex match
+        # and get the key and value as substrings.
+        header_re = re.compile(r'^(?P<key>VG Name|LV Path|PV Name)\s+(?P<val>\S.*)$')
+        # To save different handling, we use this to refer to the current
+        # dictionary we're storing data in, whether it be a VG, LV or PV.
+        current_data_store = None
+        value_start_column = 0
+        # Each section of key-value data starts with the header as above and
+        # ends with a blank line.  This prevents parsing of random error
+        # and information messages as key-value pairs.
+        in_keyval_section = False
+        for line in content:
             line = line.strip()
-            if 'System ID' in line or '--- Volume' in line:
+            if any(debug in line for debug in self._FILTER_INFO):
+                if line not in self.debug_info:
+                    self.debug_info.append(line)
                 continue
-            if any(debug in line for debug in FILTER_INFO):
-                debug_info.append(line)
-                continue
-            else:
-                if line.startswith("VG Name"):
-                    # New section starts with VG Name since blank lines are removed
-                    if vginfo_dict:
-                        vginfo_dict_list.append(vginfo_dict)
-                    (key, value) = line.split('   ', 1)
-                    vginfo_dict = {}
-                elif line.startswith('Metadata Sequence'):
-                    (key, value) = line.split('  ', 1)
-                else:
-                    (key, value) = line.split('   ', 1)
-                vginfo_dict[key.strip()] = value.strip()
 
-        # Record the last VG and debug info
-        set_info = list(set(debug_info))
-        vginfo_dict_list.append(vginfo_dict) if vginfo_dict else None
-        self.data['vginfo_dict'] = vginfo_dict_list
-        self.data['debug_info'] = set_info
-
-
-@parser('vgdisplay')
-def get_vginfo(context):
-    """Deprecated, use class :py:class:`VgDisplay` instead."""
-    return VgDisplay(context).data
+            match = header_re.match(line)
+            # Headers mark the start of a keyval section, but they do match
+            # inside, so only match if outside a keyval section.
+            if not in_keyval_section and match:
+                in_keyval_section = True
+                key, value = match.group('key', 'val')
+                # Determine column for start of value - remember, this string
+                # is now stripped so this may look different from your examples
+                value_start_column = line.index(value)
+                if key == "VG Name":
+                    # New Volume Group - new vg_info_dict entry
+                    self.vg_list.append({
+                        # Logical and Physical volumes stored by name / device
+                        'Logical Volumes': {},
+                        'Physical Volumes': {},
+                        'VG Name': value,
+                    })
+                    current_data_store = self.vg_list[-1]
+                elif key == 'LV Path':
+                    # New Logical Volume - append to 'Logical Volumes'
+                    current_data_store = {
+                        key: value,
+                    }
+                    self.vg_list[-1]['Logical Volumes'][value] = current_data_store
+                elif key == 'PV Name':
+                    # New Physical Volume - append to 'Physical Volumes'
+                    current_data_store = {
+                        key: value,
+                    }
+                    self.vg_list[-1]['Physical Volumes'][value] = current_data_store
+            elif line == '':
+                # Blank line = new section, stop expecting keys and values
+                in_keyval_section = False
+            elif in_keyval_section and len(line) > value_start_column:
+                # Extract key and value by substring
+                key = line[:value_start_column].strip()
+                value = line[value_start_column:].strip()
+                # Finally, add this key/val pair to the current data store.
+                current_data_store[key] = value
