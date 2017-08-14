c0a48b4671b4d4d9e881c16e30b2e39b0225a6a2
Xiangce Liu <xiangceliu@redhat.com>
2017-08-14 21:45:07 +0800

    Enhance hammer_ping for master (#295)
    * Enhance hammer_ping
    
    - now can handle some unexpected outputs
    - update interfaces
    
    * Suggested changes to the HammerPing class
    
    * Make status_of_service and response_of_service dictionaries in their own
      right rather than having to look them up in a data dictionary.
    * Simplify code down as a result.
    * Add documentation and tests for the above.
    * Remove the print statement
    
    - shift the attribute's docstring to class's docstring
    - rename `is_ok` to `are_all_ok` to keep compatible with the 1.x branch

--- insights/parsers/tests/test_hammer_ping.py
+++ insights/parsers/tests/test_hammer_ping.py
@@ -1,5 +1,49 @@
-from insights.parsers import hammer_ping
+from insights.parsers.hammer_ping import HammerPing
 from insights.tests import context_wrap
+
+HAMMERPING_ERR_1 = """
+Error: Connection refused - connect(2) for "localhost" port 443
+"""
+
+HAMMERPING_ERR_2 = """
+Could not load the API description from the server
+ - is the server down?
+ - was 'foreman-rake apipie:cache' run on the server when using apipie cache? (typical production settings)
+Warning: An error occured while loading module hammer_cli_csv
+Could not load the API description from the server
+ - is the server down?
+ - was 'foreman-rake apipie:cache' run on the server when using apipie cache? (typical production settings)
+Warning: An error occured while loading module hammer_cli_foreman
+"""
+
+HAMMERPING_ERR_3 = """
+candlepin:
+    Status:          ok
+    Server Response: Duration: 61ms
+candlepin_auth:
+    Status:          ok
+"""
+
+HAMMERPING_OK = """
+candlepin:
+    Status:          ok
+    Server Response: Duration: 61ms
+candlepin_auth:
+    Status:          ok
+    Server Response: Duration: 61ms
+pulp:
+    Status:          ok
+    Server Response: Duration: 61ms
+pulp_auth:
+    Status:          ok
+    Server Response: Duration: 61ms
+elasticsearch:
+    Status:          ok
+    Server Response: Duration: 35ms
+foreman_tasks:
+    Status:          ok
+    server Response: Duration: 1ms
+""".strip()
 
 HAMMERPING = """
 candlepin:
@@ -7,13 +51,13 @@ candlepin:
     Server Response:Message:404 Resource Not Found
 candlepin_auth:
     Status:          FAIL
-    Server Response: Message: Katello::Resources::Candlepin::CandlepinPing: 404 Resource Not Found <html>... (skipping some generic 404 html page details) </html> (GET /candlepin/status)
+    Server Response: Message: Katello::Resources::Candlepin::CandlepinPing: 404 Resource Not Found
 pulp:
     Status:          ok
     Server Response: Duration: 61ms
 pulp_auth:
-    Status:          ok
-    Server Response: Duration: 27ms
+    Status:
+    Server Response:
 elasticsearch:
     Status:          ok
     Server Response: Duration: 35ms
@@ -23,31 +67,59 @@ foreman_tasks:
 """.strip()
 
 
-def test_hammer_ping():
-    status = hammer_ping.HammerPing(context_wrap(HAMMERPING))
-    dic = status.data
+def test_hammer_ping_err_1():
+    status = HammerPing(context_wrap(HAMMERPING_ERR_1))
+    assert not status.are_all_ok
+    assert status.errors is not []
 
-    assert len(status) == 6
-    assert len(dic) == 6
 
-    assert dic.keys() == [
-        'candlepin', 'candlepin_auth', 'foreman_tasks', 'elasticsearch',
-        'pulp_auth', 'pulp'
+def test_hammer_ping_err_2():
+    status = HammerPing(context_wrap(HAMMERPING_ERR_2))
+    assert not status.are_all_ok
+    assert status.errors is not []
+
+
+def test_hammer_ping_err_3():
+    status = HammerPing(context_wrap(HAMMERPING_ERR_3))
+    assert not status.are_all_ok
+    assert status.errors is not []
+
+
+def test_hammer_ping_ok():
+    status = HammerPing(context_wrap(HAMMERPING_OK))
+
+    assert status.are_all_ok
+    assert status.service_list == [
+        'candlepin', 'candlepin_auth', 'pulp', 'pulp_auth',
+        'elasticsearch', 'foreman_tasks'
     ]
+    assert status.services_of_status('FAIL') == []
+    assert 'nonexistent' not in status.service_list
+    assert 'nonexistent' not in status.status_of_service
+    assert 'nonexistent' not in status.response_of_service
 
-    assert dic['candlepin'].keys() == ['status', 'response']
-    assert dic['candlepin']['status'] == 'FAIL'
-    assert dic['candlepin']['response'] == '404 Resource Not Found'
 
-    assert dic['pulp_auth']['status'] == 'ok'
-    assert dic['pulp_auth']['response'] == '27ms'
+def test_hammer_ping():
+    status = HammerPing(context_wrap(HAMMERPING))
 
-    assert not status.is_ok('candlepin_auth')
-    assert status.is_ok('pulp')
+    assert not status.are_all_ok
+    assert status.service_list == [
+        'candlepin', 'candlepin_auth', 'pulp', 'pulp_auth',
+        'elasticsearch', 'foreman_tasks'
+    ]
+    assert status.services_of_status('OK') == [
+        'pulp', 'elasticsearch', 'foreman_tasks'
+    ]
+    assert status.services_of_status('FAIL') == [
+        'candlepin', 'candlepin_auth'
+    ]
+    assert status.status_of_service['pulp_auth'] == ''
+    assert status.status_of_service['candlepin'] == 'fail'
+    assert status.status_of_service['elasticsearch'] == 'ok'
+    assert status.response_of_service['pulp_auth'] == ''
+    assert status.response_of_service['candlepin_auth'] == 'Message: Katello::Resources::Candlepin::CandlepinPing: 404 Resource Not Found'
+    assert status.response_of_service['elasticsearch'] == 'Duration: 35ms'
 
-    assert status.response_of('candlepin') == '404 Resource Not Found'
-    assert status.response_of('elasticsearch') == '35ms'
-
-    # A service that isn't in the list should return false values
-    assert not status.is_ok('nonexistent')
-    assert status.response_of('nonexistent') == ''
+    assert 'nonexistent' not in status.service_list
+    assert 'nonexistent' not in status.status_of_service
+    assert 'nonexistent' not in status.response_of_service
