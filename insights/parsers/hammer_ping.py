a6594df47f341df160fc48858255888b18fded71
Xiangce Liu <xiangceliu@redhat.com>
2017-08-14 21:39:37 +0800

    Enhance hammer_ping for 1.x (#294)
    * Enhance hammer_ping
    
    - now can handle some unexpected outputs
    - update interfaces
    
    * Suggested changes to the HammerPing class
    
    * Make status_of_service and response_of_service dictionaries in their own
      right rather than having to look them up in a data dictionary.
    * Simplify code down as a result.
    * Add documentation and tests for the above.
    * Remove the print statement
    
    * Revert the removed APIs and document them as deprecated
    
    * Update 'is_ok' to 'are_all_ok' in example correspondingly

--- insights/parsers/hammer_ping.py
+++ insights/parsers/hammer_ping.py
@@ -1,20 +1,12 @@
 """
-hammer_ping - command ``/usr/bin/hammer ping``
-==============================================
+HammerPing - command ``/usr/bin/hammer ping``
+=============================================
 
 The hammer ping parser reads the output of ``hammer ping`` and turns it into
-a dictionary of service statuses in the ``data`` property.  Each service has
-its status stored in the 'status' key and its response in the 'repsonse' key;
-the response is trimmed so that it is either the response time in milliseonds
-or the HTTP message.
-
-The easier way to access this data is with the two helper methods:
-
-* ``is_ok(service)`` returns True if the service is defined in the ``hammer
-  ping`` output and if its status is given as '**ok**', or False otherwise.
-* ``response_of(service)`` returns the server response (after the 'Message:'
-  or 'Duration:' text) if the service is listed in the ``hammer ping`` output,
-  or '' otherwise.
+a dictionary of service statuses in the ``status_of_service`` property and
+the corresponding response in the ``response_of_service`` property.  The list
+of services by name in the order they were discovered is given in the
+``service_list`` property.
 
 Sample output of ``hammer ping``::
 
@@ -30,23 +22,17 @@ Sample output of ``hammer ping``::
 
 Examples:
 
-    >>> status = shared[HammerPing]
-    >>> status.data['candlepin'] # The complete data record
-    {'status': 'FAIL', 'response': '404 Resource Not Found'}
-    >>> status.data['elasticsearch']
-    {'status': 'ok', 'response', '1ms'}
-    >>> status.data['candlepin']['status'] == 'on'
+    >>> hammer = shared[HammerPing]
+    >>> 'unknown_service' in hammer.service_list
     False
-    >>> status.is_ok('candlepin_auth') # New style accessor
+    >>> hammer.status_of_service['candlepin']
+    'FAIL'
+    >>> hammer.response_of_service['candlepin']
+    'Message: 404 Resource Not Found'
+    >>> hammer.are_all_ok
     False
-    >>> status.is_ok('foreman_tasks')
-    True
-    >>> status.is_ok('unknown_service') # Service not OK if not found
-    False
-    >>> status.response_of('foreman_tasks')
-    '1ms'
-    >>> status.response_of('candlepin')
-    '404 Resource Not Found'
+    >>> hammer.services_of_status('OK')
+    ['elasticsearch', 'foreman_tasks']
 """
 from .. import parser, Parser
 
@@ -54,45 +40,100 @@ from .. import parser, Parser
 @parser("hammer_ping")
 class HammerPing(Parser):
     """
-    Read the ``hammer ping`` status and convert it to a dictionary of
-    response statuses and information.
+    Read the ``hammer ping`` status and convert it to dictionaries of
+    status and response information.
 
-    The status and response information are available in the ``data``
-    property keyed to the name of each service, and the ``is_ok`` and
-    ``response_of`` methods are provided as conveniences.
     """
+    def __init__(self, *args, **kwargs):
+        self.status_of_service = {}
+        """dict: The status of each service, converted to lower case."""
+        self.response_of_service = {}
+        """dict: The response of each service, as is."""
+        self.service_list = []
+        """list: The list of service names in order."""
+        self.errors = []
+        """list: Any error messages encountered during parsing."""
+        self.are_all_ok = False
+        """bool: Were all services read correctly and are they all in 'ok' state?"""
+        self.data = {}
+        """
+        .. warning::
+            Deprecated attribute, please get what you want via accessing other attributes correspondingly.
+
+        data (dict): The status and response information with key to the name of each service
+        """
+        super(HammerPing, self).__init__(*args, **kwargs)
+
+    def parse_content(self, content):
+        # The first line should be a service
+        if len(content[0].split()) > 1:
+            self.errors.append(content[0])
+        # Each service occupies 3 lines
+        elif len(content) % 3 == 0:
+            lines = iter(content)
+            for service_no in range(0, len(content) / 3):
+                service_line = lines.next().strip()
+                status_line = lines.next().strip()
+                response_line = lines.next().strip()
+
+                service = service_line.split(':')[0]
+                status = status_line.split(':', 1)[-1].strip().lower()
+                response = response_line.split(':', 1)[-1].strip()
+
+                self.service_list.append(service)
+                self.status_of_service[service] = status
+                self.response_of_service[service] = response
+
+                # Deprecated attribute
+                self.data[service] = {'status': status, 'response': response}
+        else:
+            self.errors.extend(content)
+
+        self.are_all_ok = (
+            not self.errors and
+            all(status == 'ok' for status in self.status_of_service.values())
+        )
+
+    def services_of_status(self, status='ok'):
+        """
+        List of the services in the given status.
+
+        Arguments:
+            status (str): the status code to search for, defaulting to 'ok'.
+                The value is converted to lower case.
+
+        Returns: List of service names having that status.
+        """
+        status = status.lower()
+        return [svc for svc in self.service_list
+                if self.status_of_service[svc] == status]
+
     def __len__(self):
         """
-        (int): the number of items in the underlying data.
+        .. warning::
+            Deprecated function, please get the length of :attr:`service_list` instead.
+
+        int: the number of items in the underlying data.
         """
         return len(self.data)
 
     def is_ok(self, service):
         """
-        (bool): is the service defined in the data and is it listed as 'ok'?
+        .. warning::
+            Deprecated function, please use the :attr:`status_of_service` instead.
+
+        bool: is the service defined in the data and is it listed as 'ok'?
         """
         return service in self.data and 'ok' == self.data[service]['status']
 
     def response_of(self, service):
         """
-        (str): the status of the given service, or '' if the service is not
+        .. warning::
+            Deprecated function, please use the :attr:`response_of_service` instead.
+
+        str: the status of the given service, or '' if the service is not
         in the hammer ping output.
         """
         if service not in self.data:
             return ''
         return self.data[service]['response']
-
-    def parse_content(self, content):
-        i = 0
-        dic = {}
-        while i < len(content):
-            service_line, status_line, response_line = content[i:i + 3]
-            status = status_line.split(':', 1)[1].strip()
-            response = response_line.split(':', 2)[2].strip()
-            dic[service_line.strip(': ')] = {
-                'status': status,
-                'response': response
-            }
-            # status messages are three lines long - skip three lines.
-            i += 3
-        self.data = dic
