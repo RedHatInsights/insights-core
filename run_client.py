f2d3c48a35e6f38f802896f3d1c013266d85f79b
Kyle Lape <klape@redhat.com>
2017-08-15 17:04:23 -0500

    Handle startup method properly in run_client script

--- run_client.py
+++ run_client.py
@@ -4,16 +4,23 @@ from insights.client.config import compile_config, CONFIG as config
 from insights.client import InsightsClient
 from insights.client.client import set_up_logging, handle_startup
 
-compile_config()
-set_up_logging()
-v = handle_startup()
-if v is not None:
-    if v is not False:
-        print v
-else:
-    client = InsightsClient()
-    tar = client.collect(check_timestamp=False)
-    if not config['no_upload']:
-        client.upload(tar)
+
+def main():
+    compile_config()
+    set_up_logging()
+    v = handle_startup()
+    if v is not None:
+        if type(v) != bool:
+            print v
+        return
     else:
-        print tar
+        client = InsightsClient()
+        tar = client.collect(check_timestamp=False)
+        if not config['no_upload']:
+            client.upload(tar)
+        else:
+            print tar
+
+
+if __name__ == "__main__":
+    main()
