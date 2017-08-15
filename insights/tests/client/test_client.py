8563e048b0cf5b445cd08fdfc5ddabd2041dbfa0
Kyle Lape <klape@redhat.com>
2017-08-15 17:59:09 -0500

    Fix CI build failures
    - typo on logger name
    - client unit tests now fail when reading py.test args.  Need to hide
      the args from the test.

--- insights/tests/client/test_client.py
+++ insights/tests/client/test_client.py
@@ -1,10 +1,20 @@
+import sys
+
 from insights.client import InsightsClient
 from insights import get_nvr
 from insights.client.constants import InsightsConstants as constants
 
 
 def test_version():
-    client = InsightsClient(logging_file='/tmp/insights.log')
-    result = client.version()
-    expected = {'core': get_nvr(), 'client_api': constants.version}
-    assert result == expected
+
+    # Hack to prevent client from parsing args to py.test
+    tmp = sys.argv
+    sys.argv = []
+
+    try:
+        client = InsightsClient(logging_file='/tmp/insights.log')
+        result = client.version()
+        expected = {'core': get_nvr(), 'client_api': constants.version}
+        assert result == expected
+    finally:
+        sys.argv = tmp
