519f2bf599e8bde3bb7f47b91b73faf74e9d1ff2
wushiqinlou <jiazhang@redhat.com>
2017-08-28 15:54:44 +0800

    Enhance config systemd (#353)
    * Enhance config systemd
     - Modify content format
     - Change RawConfigParser for python version

--- insights/parsers/systemd/config.py
+++ insights/parsers/systemd/config.py
@@ -5,10 +5,11 @@ Service systemd files are in ``/usr/lib/systemd/system``, and Their content form
 ``config``.
 """
 
-from insights.contrib.ConfigParser import RawConfigParser as cp
 import StringIO
 from insights.core import Parser, LegacyItemAccess
 from insights.core.plugins import parser
+from insights.parsers import unsplit_lines
+from insights.contrib.ConfigParser import RawConfigParser as cp
 
 
 @parser('systemd_docker')
@@ -18,57 +19,54 @@ class SystemdDocker(Parser):
     The content of file ``/usr/lib/systemd/system/docker.service`` is recorded
     via INI format, ``ConfigParser`` could be used to parse the content.
 
-    Example::
-
-        [Unit]
-        Description=Docker Application Container Engine
-        Documentation=http://docs.docker.com
-        After=network.target
-        Wants=docker-storage-setup.service
-        Requires=rhel-push-plugin.socket
+    Typical output of the ``systemd_docker`` command is::
 
         [Service]
         Type=notify
-        NotifyAccess=all
         EnvironmentFile=-/etc/sysconfig/docker
         EnvironmentFile=-/etc/sysconfig/docker-storage
         EnvironmentFile=-/etc/sysconfig/docker-network
         Environment=GOTRACEBACK=crash
-        ExecStart=/bin/sh -c '/usr/bin/docker-current daemon \
-             --authorization-plugin=rhel-push-plugin \
-             --exec-opt native.cgroupdriver=systemd \
-             $OPTIONS \
-             $DOCKER_STORAGE_OPTIONS \
-             $DOCKER_NETWORK_OPTIONS \
-             $ADD_REGISTRY \
-             $BLOCK_REGISTRY \
-             $INSECURE_REGISTRY \
-             2>&1 | /usr/bin/forward-journald -tag docker'
-        Restart=on-abnormal
-        StandardOutput=null
-        StandardError=null
+        ExecStart=/bin/sh -c '/usr/bin/docker-current daemon \\
+        --authorization-plugin=rhel-push-plugin \\
+        --exec-opt native.cgroupdriver=systemd \\
+        $OPTIONS \\
+        $DOCKER_STORAGE_OPTIONS \\
+        $DOCKER_NETWORK_OPTIONS \\
+        $ADD_REGISTRY \\
+        $BLOCK_REGISTRY \\
+        $INSECURE_REGISTRY \\
+        2>&1 | /usr/bin/forward-journald -tag docker'
+        LimitNOFILE=1048576
 
-        [Install]
-        WantedBy=multi-user.target
+    Example:
+        >>> docker_content = '''
+        [Service]
+        Type=notify
+        EnvironmentFile=-/etc/sysconfig/docker
+        EnvironmentFile=-/etc/sysconfig/docker-storage
+        EnvironmentFile=-/etc/sysconfig/docker-network
+        Environment=GOTRACEBACK=crash
+        ExecStart=/bin/sh -c '/usr/bin/docker-current daemon \\
+                  --authorization-plugin=rhel-push-plugin \\
+                  --exec-opt native.cgroupdriver=systemd \\
+                  $OPTIONS \\
+                  $DOCKER_STORAGE_OPTIONS \\
+                  $DOCKER_NETWORK_OPTIONS \\
+                  $ADD_REGISTRY \\
+                  $BLOCK_REGISTRY \\
+                  $INSECURE_REGISTRY \\
+                  2>&1 | /usr/bin/forward-journald -tag docker'
+        LimitNOFILE=1048576
+        '''.strip()
 
-    Parse Result::
-
-        {
-            "Unit":{
-                "Description": "Docker Application Container Engine",
-                "After": "network.target"
-                ...
-            },
-            "Service": {
-                "Type": "notify",
-                "NotifyAccess": "all"
-                ...
-            }
-            "Install":{
-                "WantedBy": "multi-user.target"
-                ...
-            }
-        }
+        >>> from insights.tests import context_wrap
+        >>> shared = {SystemdDocker: SystemdDocker(context_wrap(docker_content))}
+        >>> docker_config_info = shared[SystemdDocker]
+        >>> docker_config_info["Service"]["ExecStart"]
+        "/bin/sh -c '/usr/bin/docker-current daemon --authorization-plugin=rhel-push-plugin --exec-opt native.cgroupdriver=systemd $OPTIONS $DOCKER_STORAGE_OPTIONS $DOCKER_NETWORK_OPTIONS $ADD_REGISTRY $BLOCK_REGISTRY $INSECURE_REGISTRY 2>&1 | /usr/bin/forward-journald -tag docker'"
+        >>> len(docker_config_info["Service"]["EnvironmentFile"])
+        3
     """
 
     def parse_content(self, content):
@@ -82,23 +80,25 @@ class SystemdSystemConf(LegacyItemAccess, Parser):
     Systemd configuration files are recorded via INI format as well, we can
     share the same parser ``ConfigParser`` here.
 
-    Example::
+    Typical output of the ``systemd_system.conf`` command is::
 
         [Manager]
-        LogLevel=info
-        LogTarget=journal-or-kmsg
+        RuntimeWatchdogSec=0
+        ShutdownWatchdogSec=10min
+
+    Example:
+        >>> systemd_content = '''
+        [Manager]
+        RuntimeWatchdogSec=0
+        ShutdownWatchdogSec=10min
         ...
+        '''.strip()
 
-    Parse Result::
-
-        {
-            "Manager":{
-                "LogLevel": "info",
-                "LogTarget": "journal-or-kmsg",
-                ...
-            }
-        }
-
+        >>> from insights.tests import context_wrap
+        >>> shared = {SystemdSystemConf: SystemdSystemConf(context_wrap(systemd_content))}
+        >>> systemd_config_info = shared[SystemdSystemConf]
+        >>> systemd_config_info["Manager"]["RuntimeWatchdogSec"]
+        "0"
     """
 
     def parse_content(self, content):
@@ -110,13 +110,47 @@ class SystemdSystemConf(LegacyItemAccess, Parser):
 
 @parser('systemd_openshift_node')
 class SystemdOpenshiftNode(Parser):
-    """Class for atomic-openshift-node systemd configuration. Example is like SystemdDocker"""
+    """Class for atomic-openshift-node systemd configuration.
+
+    The content of file ``/usr/lib/systemd/system/atomic-openshift-node.service``
+    is recorded via INI format, ``ConfigParser`` could be used to parse the content.
+
+    Typical output of the ``systemd_openshift_node`` command is::
+
+        [Service]
+        Type=notify
+        RestartSec=5s
+        OOMScoreAdjust=-999
+        ExecStartPost=/usr/bin/sleep 10
+        ExecStartPost=/usr/sbin/sysctl --system
+
+    Example:
+        >>> openshift_content = '''
+        [Service]
+        Type=notify
+        RestartSec=5s
+        OOMScoreAdjust=-999
+        ExecStartPost=/usr/bin/sleep 10
+        ExecStartPost=/usr/sbin/sysctl --system
+        ...
+        '''.strip()
+
+        >>> from insights.tests import context_wrap
+        >>> shared = {SystemdOpenshiftNode: SystemdOpenshiftNode(context_wrap(openshift_content))}
+        >>> openshift_config_info = shared[SystemdOpenshiftNode]
+        >>> openshift_config_info["Service"]["RestartSec"]
+        "5s"
+        >>> len(openshift_config_info["Service"]["ExecStartPost"])
+        2
+    """
+
     def parse_content(self, content):
         self.data = parse_systemd_ini(content)
 
 
 class MultiOrderedDict(dict):
     """Class for condition that duplicate keys exist"""
+
     def __setitem__(self, key, value):
         if isinstance(value, list) and key in self:
             self[key].extend(value)
@@ -126,6 +160,7 @@ class MultiOrderedDict(dict):
 
 def parse_systemd_ini(content):
     """Function to parse config format file, the result format is dictionary"""
+
     Config = cp(dict_type=MultiOrderedDict)
     Config.optionxform = str
     Config.readfp(StringIO.StringIO('\n'.join(content)))
@@ -135,9 +170,9 @@ def parse_systemd_ini(content):
         dict_section = {}
         for option in Config.options(section):
             value = Config.get(section, option).splitlines()
-            value = map(lambda s: s.rstrip("\\").strip(), value)
-            value = filter(lambda s: bool(s), value)
-            dict_section[option] = "\n".join(value)
+            value = list(unsplit_lines(value))
+            # If the len of value is 1, then set the value as string
+            dict_section[option] = value[0] if len(value) == 1 else value if len(value) > 1 else ''
         dict_all[section] = dict_section
 
     return dict_all
