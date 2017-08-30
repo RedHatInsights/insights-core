5889000b0b997e8b90c89d342c64d2827926ef8f
Jesse Jaggars <jjaggars@redhat.com>
2017-08-30 09:55:06 -0400

    travis build now works for forks

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ py.test
 if [ "`python -V 2>&1`" != "Python 2.6.9" ]; then 
     flake8;
     sphinx-build -W -b html -qa -E docs docs/_build/html;
-    if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ -n "$DOCKER_USER" ] && [ -n "$DOCKER_PASS" ]; then
         docker login -u $DOCKER_USER -p $DOCKER_PASS
         export REPO=jhjaggars/insights-core
         docker build -f Dockerfile -t $REPO:$COMMIT .
