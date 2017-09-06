020a3bc358b9bf9a6a347efb40e60084cf304f4f
Xiaoxue Wang <xiaoxwan@redhat.com>
2017-09-06 11:03:59 +0800

    Enhance class SCSI with two more ParseExceptions cases (#405)
    - And refactor unit test to pytest style.

--- insights/parsers/tests/test_scsi.py
+++ insights/parsers/tests/test_scsi.py
@@ -2,7 +2,7 @@ from insights.parsers.scsi import SCSI
 from insights.parsers import ParseException
 from insights.tests import context_wrap
 
-import unittest
+import pytest
 
 SCSI_OUTPUT = """
 Attached devices:
@@ -45,37 +45,63 @@ Host: scsi0 Channel: 03 Id: 00 Lun: 00
   Type:   RAID                             ANSI  SCSI revision: 05
 """
 
+SCSI_OUTPUT_INNORMAL_VEMDOR_LINE = """
+Attached devices:
+Host: scsi0 Channel: 03 Id: 00 Lun: 00
+  Vendor: HP       Model: P420i            Rev: 3.:
+  Type:   RAID                             ANSI  SCSI revision: 05
+"""
 
-class TestSCSIParser(unittest.TestCase):
-    def test_parse(self):
-        context = context_wrap(SCSI_OUTPUT)
-        result = SCSI(context)
-        self.assertEqual(len(result), 5)
-        r = result[0]
-        self.assertEqual(r.host, "scsi0")
-        self.assertEqual(r.get('host'), 'scsi0')
-        self.assertEqual(r.channel, "03")
-        self.assertEqual(r.id, "00")
-        self.assertEqual(r.lun, "00")
-        self.assertEqual(r.vendor, "HP")
-        self.assertEqual(r.model, "P420i")
-        self.assertEqual(r.rev, "3.54")
-        self.assertEqual(r.type, "RAID")
-        self.assertEqual(r.ansi__scsi_revision, "05")
+SCSI_OUTPUT_EMPTY = """
+"""
 
-        r = result[1]
-        self.assertEqual(r.model, "LOGICAL VOLUME")
 
-        r = result[4]
-        self.assertEqual(r.type, "Direct-Access")
+def test_parse():
+    context = context_wrap(SCSI_OUTPUT)
+    result = SCSI(context)
+    assert len(result) == 5
+    r = result[0]
+    assert r.host == "scsi0"
+    assert r.get('host') == 'scsi0'
+    assert r.channel == "03"
+    assert r.id == "00"
+    assert r.lun == "00"
+    assert r.vendor == "HP"
+    assert r.model == "P420i"
+    assert r.rev == "3.54"
+    assert r.type == "RAID"
+    assert r.ansi__scsi_revision == "05"
 
-        self.assertEqual([disc.vendor for disc in result], ['HP'] * 5)
+    r = result[1]
+    assert r.model == "LOGICAL VOLUME"
 
-    def test_single_spaced_ansi_scsi(self):
-        result = SCSI(context_wrap(SCSI_OUTPUT_SINGLE_SPACED_ANSI_SCSI))
-        self.assertEqual(len(result), 4)
+    r = result[4]
+    assert r.type == "Direct-Access"
 
-    def test_missing_header(self):
-        with self.assertRaisesRegexp(ParseException, 'Expected Header: Attached devices:'):
-            result = SCSI(context_wrap(SCSI_OUTPUT_MISSING_HEADER))
-            self.assertIsNone(result)
+    assert [disc.vendor for disc in result] == ['HP'] * 5
+
+
+def test_single_spaced_ansi_scsi():
+    result = SCSI(context_wrap(SCSI_OUTPUT_SINGLE_SPACED_ANSI_SCSI))
+    assert len(result) == 4
+
+
+def test_missing_header():
+    with pytest.raises(ParseException) as excinfo:
+        result = SCSI(context_wrap(SCSI_OUTPUT_MISSING_HEADER))
+        assert result is None
+    assert 'Expected Header: Attached devices:' in str(excinfo.value)
+
+
+def test_missing_innormal_rev():
+    with pytest.raises(ParseException) as excinfo:
+        result = SCSI(context_wrap(SCSI_OUTPUT_INNORMAL_VEMDOR_LINE))
+        assert result is None
+    assert 'Parse error for current line:' in str(excinfo.value)
+
+
+def test_empty():
+    with pytest.raises(ParseException) as excinfo:
+        result = SCSI(context_wrap(SCSI_OUTPUT_EMPTY))
+        assert result is None
+    assert 'Empty content of file /proc/scsi/scsi' in str(excinfo.value)
