cf744adeff09dacd43f64936c77e2a8c45f0a913
Xiangce Liu <xiangceliu@redhat.com>
2017-09-18 10:45:57 -0500

    Enhance cmdline.py and always store values in list (#431)
    - Store values into list by default no matter one or more values
    - Set value to `True` instead of `None` for element without value
    - Refine the docstring and exmaple
    - Remove the ".data" access method from example and test

--- insights/parsers/cmdline.py
+++ insights/parsers/cmdline.py
@@ -1,9 +1,9 @@
 """
-CmdLine - file ``/proc/cmdline``
+CmdLine - File ``/proc/cmdline``
 ================================
 
-This reads the ``/proc/cmdline`` file, which contains the arguments given to
-the currently running kernel on boot.
+This parser reads the ``/proc/cmdline`` file, which contains the arguments
+given to the currently running kernel on boot.
 
 Sample input::
 
@@ -12,18 +12,20 @@ Sample input::
 Examples:
 
     >>> cmd = shared[CmdLine]
-    >>> cmd.data['BOOT_IMAGE']
+    >>> cmd['BOOT_IMAGE']
     ['/vmlinuz-3.10.0-327.36.3.el7.x86_64']
-    >>> cmd.data['rd.lvm.lv']
+    >>> cmd['rd.lvm.lv']
     ['system_vg/Root', 'system_vg/Swap']
-    >>> 'crashkernel' in cmd.data
-    True
-    >>> 'autofs' in cmd.data
+    >>> 'autofs' in cmd
     False
-    >>> 'quiet' in cmd.data
-    True
-    >>> cmd.data['quiet']
+    >>> cmd.get('autofs')
     None
+    >>> 'quiet' in cmd
+    True
+    >>> cmd.get('quiet')
+    [True]
+    >>> cmd['crashkernel']
+    ['auto']
 
 """
 
@@ -35,32 +37,34 @@ class CmdLine(LegacyItemAccess, Parser):
     """
     A parser class for parsing the Linux kernel command line as given in
     ``/proc/cmdline``.
+
+    Parsing Logic::
+
+        Parses all elements in command line to a dict where the key is the
+        element itself and the value is a list stores its corresponding values.
+
+        If an element doesn't contain "=", set the corresponding value to `True`.
+
+        If an element contains "=", set the corresponding value to the whole right
+        value of the "=".
+
+        Note:
+            For special command line elements that include two "=", e.g.
+            ``root=LABEL=/1``, "root" will be the key and "LABEL=/1" will be
+            the value in the returned list.
+
+            Some parameters (the returned keys) might be still effective
+            even if there is '#' before it, e.g.: ``#rhgb``.  This should
+            be checked by the rule.
     """
 
     def parse_content(self, content):
-        """
-        If an element doesn't contain "=", set itself as key and the
-        corresponding value to None.
-
-        If an element contains "=", the corresponding values are stored in a
-        list.
-
-        For special command line elements that include two "=", e.g.
-        ``root=LABEL=/1``, "root" will be the key and "LABEL=/1" will be the
-        value in the returned list.
-
-        Note: Some parameters (the returned keys) might be still effective
-        even if there is '#' before it, e.g.: ``#rhgb``.  This should
-        be checked by the rule.
-        """
-
-        cmdline_properties = {}
-        line = content[0]
-        for el in line.strip().split(None):
+        self.data = {}
+        for el in content[0].strip().split(None):
             # Thanks to strip and split, el always contains something.
-            if "=" not in el:
-                cmdline_properties[el] = None
-            else:
-                (key, value) = el.split("=", 1)
-                cmdline_properties.setdefault(key, []).append(value)
-        self.data = cmdline_properties
+            key, value = el, True
+            if "=" in el:
+                key, value = el.split("=", 1)
+            if key not in self.data:
+                self.data[key] = []
+            self.data[key].append(value)
