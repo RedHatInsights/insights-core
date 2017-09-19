ee7b9064ccd3a6aa8be6a0197ddbc5fb17fd5b66
Flos Lonicerae <lonicerae@users.noreply.github.com>
2017-09-19 15:50:17 +0800

    Fix bug in cmdline for parsing parameters and enhance (#422)
    * Fixed a bug when parsing parameters which presented in both solo format and key-value format.
    
     - Inspired by the log `'NoneType' object has no attribute 'append'`.
    
    * Refine the code and docstring

--- insights/parsers/tests/test_cmdline.py
+++ insights/parsers/tests/test_cmdline.py
@@ -1,18 +1,29 @@
 from insights.parsers.cmdline import CmdLine
 from insights.tests import context_wrap
 
-CMDLINE = "ro root=/dev/mapper/vg_rootvg-lv_root rd_LVM_LV=vg_rootvg/lv_root" +\
+CMDLINE_V1 = "ro root=/dev/mapper/vg_rootvg-lv_root rd_LVM_LV=vg_rootvg/lv_root" +\
     " rd_LVM_LV=vg_rootvg/lv_swap rd_NO_LUKS rd_NO_MD rd_NO_DM " +\
     "LANG=en_US.UTF-8 SYSFONT=latarcyrheb-sun16 KEYBOARDTYPE=pc KEYTABLE=us " +\
-    "        crashkernel=191M@0M hugepagesz=1073741824 hugepages=50 rhgb quiet"
+    "        crashkernel=191M@0M hugepagesz=1073741824 hugepages=50 rhgb quiet" +\
+    " audit audit=1"
+
+CMDLINE_V2 = "ro root=/dev/mapper/vg_rootvg-lv_root rd_LVM_LV=vg_rootvg/lv_root" +\
+    " rd_LVM_LV=vg_rootvg/lv_swap rd_NO_LUKS rd_NO_MD rd_NO_DM " +\
+    "LANG=en_US.UTF-8 SYSFONT=latarcyrheb-sun16 KEYBOARDTYPE=pc KEYTABLE=us " +\
+    "        crashkernel=191M@0M hugepagesz=1073741824 hugepages=50 rhgb quiet" +\
+    " audit=1 audit=0 audit audit"
 
 
-class TestCmdLine():
-    def test_cmdline(self):
-        cmd_line = CmdLine(context_wrap(CMDLINE))
-        assert len(cmd_line.data) == 15
-        assert cmd_line.data.get('ro') is None
-        assert cmd_line.data.get('root') == ['/dev/mapper/vg_rootvg-lv_root']
-        assert cmd_line.data.get('rd_LVM_LV') == ['vg_rootvg/lv_root', 'vg_rootvg/lv_swap']
-        # Handle non-alphabet characters and extra spaces
-        assert cmd_line.data['crashkernel'] == ['191M@0M']
+def test_cmdline():
+    cmd_line = CmdLine(context_wrap(CMDLINE_V1))
+    assert len(cmd_line.data) == 16
+    assert cmd_line.get('ro') is None
+    assert cmd_line.get('root') == ['/dev/mapper/vg_rootvg-lv_root']
+    assert cmd_line.get('rd_LVM_LV') == ['vg_rootvg/lv_root', 'vg_rootvg/lv_swap']
+    # Handle non-alphabet characters and extra spaces
+    assert cmd_line['crashkernel'] == ['191M@0M']
+    assert isinstance(cmd_line.get('audit'), list)
+    assert cmd_line.get('audit') == [None, '1']
+
+    cmd_line = CmdLine(context_wrap(CMDLINE_V2))
+    assert cmd_line.get('audit') == ['1', '0', None, None]
