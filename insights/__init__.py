187acb0f7eb620412a9b06e752a4d526cdd9aca3
Renaud Métrich <1163635+rmetrich@users.noreply.github.com>
2017-09-21 23:31:49 +0200

    Parser for 'journal_since_boot' ('sos_commands/logs/journalctl_--no-pager_--boot') (#479)
    - Renamed 'Messages' into 'Syslog' and moved to core/
    - Created 'Messages' as a child class of 'Syslog'
    - Created 'JournalSinceBoot' as a child class of 'Syslog'
    - Created corresponding unit tests

--- insights/__init__.py
+++ insights/__init__.py
@@ -3,6 +3,7 @@ from .core import Scannable, LogFileOutput, Parser, IniConfigFile  # noqa: F401
 from .core import FileListing, LegacyItemAccess, SysconfigOptions  # noqa: F401
 from .core import YAMLParser                                       # noqa: F401
 from .core import AttributeDict  # noqa: F401
+from .core import Syslog  # noqa: F401
 from .core.plugins import metadata, parser, rule  # noqa: F401
 from .core.plugins import datasource, condition, incident  # noqa: F401
 from .core.plugins import make_response, make_metadata  # noqa: F401
