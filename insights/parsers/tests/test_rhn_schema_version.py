8e5471b88de6c56954d0ef9411785d17d5d32a3a
Xiangce Liu <xiangceliu@redhat.com>
2017-09-22 04:07:50 -0500

    Document rhn schema version - 1.x (#484)
    * Refactor to class parser
    * Revert to function parser
    - since it's very sample, for class parser, developer have to check if
      its attribute is empty or not

--- insights/parsers/tests/test_rhn_schema_version.py
+++ insights/parsers/tests/test_rhn_schema_version.py
@@ -1,11 +1,17 @@
 from insights.tests import context_wrap
 from insights.parsers.rhn_schema_version import rhn_schema_version
 
-schema_content = """
+schema_content_ok = """
 5.6.0.10-2.el6sat
+""".strip()
+
+schema_content_no = """
+-bash: /usr/bin/rhn-schema-version: No such file or directory
 """.strip()
 
 
 def test_rhn_schema_version():
-    result = rhn_schema_version(context_wrap(schema_content))
+    result = rhn_schema_version(context_wrap(schema_content_ok))
     assert result == "5.6.0.10-2.el6sat"
+    result = rhn_schema_version(context_wrap(schema_content_no))
+    assert result is None
