f4a8740ea177c277037432756f1776ddaab54b9a
zhangyi733 <yizha@redhat.com>
2017-09-22 12:23:28 +1000

    Add parser fc match 1 x (#483)
    * Add parser fc-match
    * Add Class FCMatch
    * Add property fonts_info
    * Add testcase
    * Add corresponding item in insights/config/specs.py
    * Use command `fc-match -sv "sans:regular:roman" family fontformat` instead
    * Add __getitem__ and __iter__ function
    * Update command in insights/config/specs.py
    * Update example in docstring and annotations
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>

---
+++ insights/parsers/fc_match.py
@@ -0,0 +1,71 @@
+"""
+/usr/bin/fc-match -sv 'sans:regular:roman' family fontformat - Command
+======================================================================
+
+This command gets the fonts information in the current system.
+
+Typical output of this command is::
+
+    Pattern has 2 elts (size 16)
+        family: "DejaVu Sans"(s)
+        fontformat: "TrueType"(w)
+
+    Pattern has 2 elts (size 16)
+        family: "DejaVu Sans"(s)
+        fontformat: "TrueType"(w)
+
+    Pattern has 2 elts (size 16)
+        family: "DejaVu Sans"(s)
+        fontformat: "TrueType"(w)
+
+    Pattern has 2 elts (size 16)
+        family: "Nimbus Sans L"(s)
+        fontformat: "Type 1"(s)
+
+    Pattern has 2 elts (size 16)
+        family: "Standard Symbols L"(s)
+        fontformat: "Type 1"(s)
+
+Examples:
+    >>> fc_match = shared[FCMatch]
+    >>> fc_match_info = fc_match[0]
+    >>> fc_match_info
+    {'fontformat': '"TrueType"(w)', 'family': '"DejaVu Sans"(s)'}
+"""
+
+from .. import Parser, parser
+
+
+@parser("fc-match")
+class FCMatch(Parser):
+    """
+    Class to parse command `/usr/bin/fc-match -sv 'sans:regular:roman' family fontformat`.
+    This object provides the ' __getitem__' and '__iter__' methods to allow it to
+    be used as a list to iterate over the ``data`` data, e.g.::
+
+        >>> fc_match = shared[FCMatch]
+        >>> for item in fc_match:
+        >>>     print item["family"]
+        >>> fc_match_info = fc_match[0]
+    """
+
+    def parse_content(self, content):
+        self.data = []
+        font = {}
+        for l in content:
+            if ":" in l:
+                k, v = l.strip().split(":")
+                if k == "family":
+                    font["family"] = v.strip()
+                else:
+                    font["fontformat"] = v.strip()
+                    if font not in self.data:
+                        self.data.append(font)
+                        font = {}
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __iter__(self):
+        for item in self.data:
+            yield item
