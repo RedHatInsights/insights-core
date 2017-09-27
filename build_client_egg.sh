3934d699d25917a6dbc6c2bc47094c501d2bf62b
Kyle Lape <klape@redhat.com>
2017-09-27 13:51:58 -0500

    Strip out unused modules from client egg (#460)
    Currenly only a small subset of modules in the core are used by the
    client.  We will add modules back as we integrate features in the core
    into the client.

--- build_client_egg.sh
+++ build_client_egg.sh
@@ -7,10 +7,17 @@ mkdir -p tmp/EGG-INFO
 cp insights_core.egg-info/* tmp/EGG-INFO
 cp -r insights tmp
 cd tmp
-rm -rf insights/tests
 rm -rf insights/archive
-rm -rf insights/parsers/tests
-rm -rf insights/combiners/tests
+rm -rf insights/combiners
+rm -rf insights/contrib/pyparsing.py
+rm -rf insights/plugins
+rm -rf insights/tests
+rm -rf insights/tools
+
+# Remove all parsers but keep the package because it's imported in core/__init__.py
+rm -rf insights/parsers/*
+cp ../insights/parsers/__init__.py insights/parsers
+
 find insights -name '*.pyc' -delete
 zip ../insights.egg -r EGG-INFO/ insights/
 cd ..
