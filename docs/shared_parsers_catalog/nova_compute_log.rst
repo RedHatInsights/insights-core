f3456b002786182dea27d9b481950537a1c604bc
Chenlizhong <lichen@redhat.com>
2017-09-29 16:30:35 +0800

    Add nova_compute_log parser (#531)
    Signed-off-by: lichen <lichen@redhat.com>

---
+++ docs/shared_parsers_catalog/nova_compute_log.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.parsers.nova_compute_log
+   :members:
+   :show-inheritance:
---
+++ insights/parsers/tests/test_nova_compute_log.py
@@ -0,0 +1,19 @@
+from insights.parsers.nova_compute_log import NovaComputeLog
+from insights.tests import context_wrap
+
+from datetime import datetime
+
+compute_log = """
+2016-08-12 13:15:46.343 32386 WARNING nova.api.ec2.cloud [-] Deprecated: The in tree EC2 API is deprecated as of Kilo release and may be removed in a future release. The stackforge ec2-api project http://git.openstack.org/cgit/stackforge/ec2-api/ is the target replacement for this functionality.
+2016-08-12 14:27:16.498 32499 WARNING keystonemiddleware.auth_token [-] Authorization failed for token
+2016-08-12 14:27:16.500 32499 WARNING keystonemiddleware.auth_token [-] Identity response: {"error": {"message": "Could not find token: 786c3cee52d14baeae98262c6a2f3a4e", "code": 404, "title": "Not Found"}}
+2016-08-12 14:27:16.502 32499 WARNING keystonemiddleware.auth_token [-] Authorization failed for token
+"""""
+
+
+def test_nova_compute_log():
+    log = NovaComputeLog(context_wrap(compute_log))
+    assert len(log.get(["WARNING", "Authorization failed"])) == 2
+    assert len(log.get("786c3cee52d14baeae98262c6a2f3a4e")) == 1
+    assert len(log.get("Authorization failed")) == 2
+    assert len(list(log.get_after(datetime(2016, 8, 12, 14, 20, 0)))) == 3
