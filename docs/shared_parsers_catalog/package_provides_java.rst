9d4e368b35c19f417ff16f307311a1f7cfe78486
zhangyi733 <yizha@redhat.com>
2017-10-10 11:59:29 +1000

    Add parser package_provides_java and combiner running_java_package (#530)
    * Add parser package_provides_java and combiner running_java_package
    
    * Add parser package_provides_java
    * Add combiner running_java_package
    * Add items in insights/config/specs.py and insights/specs.py
    * Add rst files
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Use parser package_provides_java and pre_command to handle previous issue
    
    * Remove combiner unning_java_package and its testcase
    * Use pre_command java_command_package to get running_java with its package
    * Use parser package_provides_java to handle the output from pre_command
    * Update specs.py in both insights/config/ and insights/
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Remove unused files readlink.py and test_readlink.py
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Add more detailed documentation in parser file
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Update parser package_provides_java
    
    * Add attributes "command" and "package"
    * Update documentation
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Enhance parser package_provides_java
    
    * Add raise-exception in parser package_provides_java
    * Use foreach instead of with_args_from in insights/specs.py
    * Add combiner package_provides_java
    * Add and enhance corresponding testcases for combiner and parser
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Refine the combiner after discussion with Yi
    
    - update `self.data` to a dict
    - add `running_javas` to return all the running java commands
    - udpate docstrings and examples
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Update the docstring of the combiner
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

---
+++ docs/shared_parsers_catalog/package_provides_java.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.parsers.package_provides_java
+   :members:
+   :show-inheritance:
---
+++ insights/combiners/tests/test_package_provides_java.py
@@ -0,0 +1,24 @@
+from insights.parsers.package_provides_java import PackageProvidesJava
+from insights.combiners.package_provides_java import PackageProvidesJavaAll
+from insights.tests import context_wrap
+
+PACKAGE_COMMAND_MATCH_1 = """
+/usr/lib/jvm/jre/bin/java java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64
+"""
+PACKAGE_COMMAND_MATCH_2 = """
+/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64
+"""
+
+
+def test_packages_provide_java():
+    pack1 = PackageProvidesJava(context_wrap(PACKAGE_COMMAND_MATCH_1))
+    pack2 = PackageProvidesJava(context_wrap(PACKAGE_COMMAND_MATCH_2))
+    result = PackageProvidesJavaAll([pack1, pack2])
+    assert sorted(result.running_javas) == sorted(
+            ['/usr/lib/jvm/jre/bin/java',
+             '/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java'])
+    assert result["/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java"] == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
+    assert result.get_package("/usr/lib/jvm/jre/bin/java") == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
+    assert result.get("/usr/lib/jvm/jre/bin/java") == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
+    assert result.get_package("/usr/lib/jre/bin/java") is None
+    assert result.get("/usr/lib/jre/bin/java") is None
