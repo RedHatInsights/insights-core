871681a8e45b0699518d38e321dd78fcd91ecd47
zhangyi733 <yizha@redhat.com>
2017-10-11 17:42:08 +1000

    Enhance parser and specs package provides java (#570)
    * Update pre_command java_command_package to handle process using java directly
      instead of /usr/bin/java
    * Raise exception when java command is not provided by package installed
      through yum or rpm
    * Update docstring in parser
    * Enhance testcase to cover new added code logic
    * Enhance testcase for combiner package_provides_java.py
    * Add usecause that uses java directly
    * Remove usecase that running java is not provided by package installed
      through yum or rpm
    * Update pre_command in insights/specs.py
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>
    
    * Add the missed `rqm -qf` to pre_command `java_command_package`
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/tests/test_package_provides_java.py
+++ insights/combiners/tests/test_package_provides_java.py
@@ -9,15 +9,21 @@ PACKAGE_COMMAND_MATCH_2 = """
 /usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64
 """
 
+PACKAGE_COMMAND_MATCH_3 = """
+java java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64
+"""
+
 
 def test_packages_provide_java():
     pack1 = PackageProvidesJava(context_wrap(PACKAGE_COMMAND_MATCH_1))
     pack2 = PackageProvidesJava(context_wrap(PACKAGE_COMMAND_MATCH_2))
-    result = PackageProvidesJavaAll([pack1, pack2])
+    pack3 = PackageProvidesJava(context_wrap(PACKAGE_COMMAND_MATCH_3))
+    result = PackageProvidesJavaAll([pack1, pack2, pack3])
     assert sorted(result.running_javas) == sorted(
-            ['/usr/lib/jvm/jre/bin/java',
-             '/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java'])
-    assert result["/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java"] == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
+        ['/usr/lib/jvm/jre/bin/java',
+         '/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java', 'java'])
+    assert result[
+               "/usr/lib/jvm/java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64/bin/java"] == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
     assert result.get_package("/usr/lib/jvm/jre/bin/java") == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
     assert result.get("/usr/lib/jvm/jre/bin/java") == "java-1.8.0-openjdk-headless-1.8.0.141-3.b16.el6_9.x86_64"
     assert result.get_package("/usr/lib/jre/bin/java") is None
