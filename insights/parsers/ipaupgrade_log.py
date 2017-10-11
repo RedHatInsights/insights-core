fbf789d2eddf773f89582cce6f895ee2424f2ff0
wushiqinlou <jiazhang@redhat.com>
2017-10-11 16:23:31 +0800

    Add parser IpaupgradeLog - 1.x (#576)
    * Add description for log file
    
    * Modify comment
    
    * Modify title
    
    * Change item name

---
+++ insights/parsers/ipaupgrade_log.py
@@ -0,0 +1,34 @@
+"""
+IpaupgradeLog - ``/var/log/ipaupgrade.log`` log File for IPA upgrade
+====================================================================
+This file records the information of IPA server upgrade process while
+executing command ``ipa-server-upgrade``
+"""
+
+from .. import LogFileOutput, parser
+
+
+@parser('ipaupgrade_log')
+class IpaupgradeLog(LogFileOutput):
+    """
+    This parser is used to parse the content of file `/var/log/ipaupgrade.log`.
+
+    It uses the LogFileOutput base class.
+
+    Typical content of ``ipaupgrade.log`` file is::
+
+        2017-08-07T07:36:50Z DEBUG Starting external process
+        2017-08-07T07:36:50Z DEBUG args=/bin/systemctl is-active pki-tomcatd@pki-tomcat.service
+        2017-08-07T07:36:50Z DEBUG Process finished, return code=0
+        2017-08-07T07:36:50Z DEBUG stdout=active
+        2017-08-07T07:41:50Z ERROR IPA server upgrade failed: Inspect /var/log/ipaupgrade.log and run command ipa-server-upgrade manually.
+
+    Example:
+        >>> ipaupgradelog = shared[IpaupgradeLog]
+        >>> len(list(log.get('DEBUG')))
+        4
+        >>> from datetime import datetime
+        >>> len(log.get_after(datetime(2017, 8, 7, 7, 37, 30)))
+        1
+    """
+    time_format = '%Y-%m-%dT%H:%M:%SZ'
