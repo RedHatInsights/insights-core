2e05834a349a2934f14e9fbc276566871860dd41
Chris Sams <csams@users.noreply.github.com>
2017-10-12 10:55:03 -0500

    API to dump and load filter strings to/from locations on the python path. (#556)

--- insights/core/filters.py
+++ insights/core/filters.py
@@ -1,8 +1,13 @@
+import os
+import pkgutil
 import re
 import six
+import yaml as ser
 from collections import defaultdict
 
+import insights
 from insights.core import dr
+
 
 # TODO: consider case insensitive and regex
 FILTERS = defaultdict(set)
@@ -37,3 +42,50 @@ def apply_filters(target, lines):
             if re.search(f, l):
                 results.append(l)
     return results
+
+
+_filename = ".".join(["filters", ser.__name__])
+_dumps = ser.dump
+_loads = ser.safe_load
+
+
+def loads(string):
+    """Loads the filters dictionary given a string."""
+    d = _loads(string)
+    for k, v in d.items():
+        FILTERS[dr.get_component(k) or k] = set(v)
+
+
+def load(stream=None):
+    """
+    Loads filters from a stream, normally an open file. If one is
+    not passed, filters are loaded from a default location within
+    the project.
+    """
+    if stream:
+        loads(stream.read())
+    else:
+        data = pkgutil.get_data(insights.__name__, _filename)
+        return loads(data) if data else None
+
+
+def dumps():
+    """Returns a string representation of the FILTERS dictionary."""
+    d = {}
+    for k, v in FILTERS.items():
+        d[dr.get_name(k)] = list(v)
+    return _dumps(d)
+
+
+def dump(stream=None):
+    """
+    Dumps a string representation of `FILTERS` to a stream, normally an
+    open file. If none is passed, `FILTERS` is dumped to a default location
+    within the project.
+    """
+    if stream:
+        stream.write(dumps())
+    else:
+        path = os.path.join(os.path.dirname(insights.__file__), _filename)
+        with open(path, "wu") as f:
+            f.write(dumps())
