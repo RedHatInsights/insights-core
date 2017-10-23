a2cb6f6670342810818c8b1aa241e008813692fa
Paul Wayper <paulway@mabula.net>
2017-10-23 18:14:09 +1000

    SelinuxConfig parser docs and enhancements for 1.x branch (#624)
    * Documentation markup include file from template
    
    * Documenting Selinux_Config parser

--- insights/parsers/selinux_config.py
+++ insights/parsers/selinux_config.py
@@ -1,25 +1,42 @@
-from .. import get_active_lines, Parser, parser
+"""
+SelinuxConfig - file ``/etc/selinux/config``
+============================================
+"""
+from .. import get_active_lines, Parser, LegacyItemAccess, parser
+from ..parsers import split_kv_pairs
 
 
 @parser("selinux-config")
-class SelinuxConfig(Parser):
+class SelinuxConfig(Parser, LegacyItemAccess):
+    """
+    Parse the SELinux configuration file.
+
+    Produces a simple dictionary of keys and values from the configuration
+    file contents , stored in the ``data`` attribute.  The object also
+    functions as a dictionary itself thanks to the
+    :py:class:`insights.core.LegacyItemAccess` mixin class.
+
+    Sample configuration file::
+
+        # This file controls the state of SELinux on the system.
+        # SELINUX= can take one of these three values:
+        #     enforcing - SELinux security policy is enforced.
+        #     permissive - SELinux prints warnings instead of enforcing.
+        #     disabled - No SELinux policy is loaded.
+        SELINUX=enforcing
+        # SELINUXTYPE= can take one of these two values:
+        #     targeted - Targeted processes are protected,
+        #     minimum - Modification of targeted policy. Only selected processes are protected.
+        #     mls - Multi Level Security protection.
+        SELINUXTYPE=targeted
+
+    Examples:
+        >>> conf = shared[SelinuxConfig]
+        >>> conf['SELINUX']
+        'enforcing'
+        >>> 'AUTORELABEL' in conf
+        False
+    """
 
     def parse_content(self, content):
-        """
-        parsing selinux-config and return a list(dict).
-        Input Example:
-            {"hostname" : "elpmdb01a.glic.com",
-            "content" : "SELINUX=disabled\n#protection.\nSELINUXTYPE=targeted \n"
-            ...
-            }
-        Output Example:
-        [   {'SELINUX': 'disabled',
-            'SELINUXTYPE': 'targeted'
-            }
-        ]
-        """
-        result = {}
-        for line in get_active_lines(content):
-            key, _, value = line.partition("=")
-            result[key.strip()] = value.strip()
-        self.data = result
+        self.data = split_kv_pairs(get_active_lines(content))
