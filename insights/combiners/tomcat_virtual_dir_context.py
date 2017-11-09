87d6bbf2909bbe7c78b1e4f1c1b07e75cdfc3fbe
Stanislav Kontár <skontar@gmail.com>
2017-11-09 23:48:50 +0100

    Fix spec command for tomcat_virtual_dir_context  (#683)
    * Fix spec command for tomcat_virtual_dir_context
    
    The old command relied on shell glob expansion which
    is not executed. The new command uses 'find' instead.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1497986
    
    * Make TomcatVirtualDirContext reject lines from old spec
    
    * Add precommand
    
    * Add combiner for two types of data
    
    * Flake8 fixes
    
    * Correct inheritance
    
    * Fix 3.x combiner and spec
    
    * Actually update the old command in 3.x spec

---
+++ insights/combiners/tomcat_virtual_dir_context.py
@@ -0,0 +1,38 @@
+"""
+TomcatVirtualDirContextCombined - check VirtualDirContext option used in Tomcat
+===============================================================================
+
+This combiner provides information about whether a VirtualDirContext is used in config files in
+both default locations or location derived from running Tomcat process.
+
+Examples::
+
+    >>> shared[TomcatVirtualDirContextCombined].data
+    {'/usr/share/tomcat/conf/server.xml':
+     ['    <Resources className="org.apache.naming.resources.VirtualDirContext"'],
+     }
+"""
+from ..core.plugins import combiner
+from ..parsers.tomcat_virtual_dir_context import TomcatVirtualDirContextFallback, \
+    TomcatVirtualDirContextTargeted
+
+
+@combiner([TomcatVirtualDirContextFallback, TomcatVirtualDirContextTargeted])
+class TomcatVirtualDirContextCombined(object):
+    """
+    Combiner for VirtualDirContext usage in Tomcat config files.
+    """
+    def __init__(self, tomcat_vdc_fallback, tomcat_vdc_targeted):
+        self.data = {}
+        fallback = tomcat_vdc_fallback
+        targeted = tomcat_vdc_targeted
+        for parser in (fallback, targeted):
+            if parser:
+                for key in parser.data:
+                    if key not in self.data:
+                        self.data[key] = []
+                    self.data[key] += parser.data[key]
+
+        # Deduplicate lines
+        for key, value in self.data.iteritems():
+            self.data[key] = sorted(set(value))
