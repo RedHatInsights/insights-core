71d6b551b4bf3660808402af5dbc2287bd08df6c
zhangyi733 <yizha@redhat.com>
2017-11-30 02:22:35 -0600

    Add parser catalina_server_log to parser default catalina log file (#761)
    * Add parser tomcat_server_log to parser default tomcat log file
    
    * Enhance catalina server log for Tomcat
    
    * Rename tomcat_server_log to catalina_server_log
    * Add time_format for catalina_server_log
    * Enhance testcase for catalina_server_log
    * Add more log files in specs.py to meet Satellite requirements
    * Update catalina.out item in specs.py to make it work in current client
    
    * Remove unused blank line
    
    * Remove tab from docstring
    
    * Rename test_tomcat_server_log.py to test_catalina_server_log.py
    
    * Fix flake8 error
    
    * Remove unused file tomcat_server_log.py
    
    Signed-off-by: Yi Zhang <yizha@redhat.com>

---
+++ insights/parsers/catalina_server_log.py
@@ -0,0 +1,56 @@
+"""
+CatalinaServerlog - ``catalina*.log`` files for Tomcat
+======================================================
+.. note::
+    On RHEL6, the default tomcat server log file is under /var/log/tomcat6/.
+    On RHEL7, the default tomcat server log file is under /var/log/tomcat/.
+
+Note that the standard format of Catalina log lines spreads the information
+over two lines::
+
+    INFO: Command line argument: -Djava.io.tmpdir=/var/cache/tomcat/temp
+    Nov 28, 2017 2:11:20 PM org.apache.catalina.startup.VersionLoggerListener log
+    INFO: Command line argument: -Djava.util.logging.config.file=/usr/share/tomcat/conf/logging.properties
+    Nov 28, 2017 2:11:20 PM org.apache.catalina.startup.VersionLoggerListener log
+    INFO: Command line argument: -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
+    Nov 28, 2017 2:11:20 PM org.apache.catalina.core.AprLifecycleListener lifecycleEvent
+    INFO: The APR based Apache Tomcat Native library which allows optimal performance in production environments was not found on the java.library.path: /usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib
+    Nov 28, 2017 2:11:22 PM org.apache.coyote.AbstractProtocol init
+    INFO: Initializing ProtocolHandler ["http-bio-18080"]
+    Nov 28, 2017 2:11:23 PM org.apache.coyote.AbstractProtocol init
+    SEVERE: Failed to initialize end point associated with ProtocolHandler ["http-bio-18080"]
+
+However, this parser only recognises single lines.
+Please refer to its super-class :class:`insights.core.LogFileOutput`.
+
+When using this parser, consider using a filter or a scan method, e.g.::
+
+    CatalinaServerlog.filters.append('initializing ProtocolHandler')
+    CatalinaServerlog.keep_scan('init_pro', 'initializing ProtocolHandler')
+
+Examples:
+    >>> CatalinaServerlog.filters.append('Failed to initialize')
+    >>> log = shared[CatalinaServerlog]
+    >>> log.file_path
+    '/var/log/tomcat/catalina.2017-11-28.log'
+    >>> catalina.file_name
+    'catalina.2017-11-28.log'
+    >>> error_msgs = log.get('Failed to initialize')
+    >>> error_msgs[0]['raw_message']
+    'SEVERE: Failed to initialize end point associated with ProtocolHandler ["http-bio-18080"]'
+    >>> '/var/cache/tomcat/temp' in log
+    True
+    >>> from datetime import datetime
+    >>> log.get_after(datetime(2017, 11, 28, 14, 11, 21))[0]['raw_message']
+    'Nov 28, 2017 2:11:22 PM org.apache.coyote.AbstractProtocol init'
+"""
+
+from .. import LogFileOutput, parser
+
+
+@parser("catalina_server_log")
+class CatalinaServerlog(LogFileOutput):
+    """
+    Read the default tomcat server log file.
+    """
+    time_format = '%b %d, %Y %I:%M:%S %p'
