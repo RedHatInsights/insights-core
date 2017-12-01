e49f58d65e004ba825c5195fcf4f1ce8fce5b177
Chenlizhong <lichen@redhat.com>
2017-11-30 23:26:15 -0600

    Add parser for object_expirer.conf (#771)
    * Add parser for object_expirer.conf
    
    * Update proxy_server.conf also
    
    * Update the doc
    
    Signed-off-by: lichen <lichen@redhat.com>

--- insights/parsers/swift_conf.py
+++ insights/parsers/swift_conf.py
@@ -1,15 +1,20 @@
 """
-SwiftProxyServerConf - file ``/etc/swift/proxy-server.conf``
-============================================================
+Swift Conf Files - file ``/etc/swift/``
+=======================================
 
 This module provides parsers for swift config files under /etc/swift directory.
+
+SwiftObjectExpirerConf - file ``/etc/swift/object-expirer.conf``
+----------------------------------------------------------------
+
+SwiftProxyServerConf - file ``/etc/swift/proxy-server.conf``
+------------------------------------------------------------
 """
 
 from .. import IniConfigFile, parser
-from insights.specs import proxy_server_conf
 
 
-@parser(proxy_server_conf)
+@parser("swift_proxy_server_conf")
 class SwiftProxyServerConf(IniConfigFile):
     """
     This class is to parse the content of the ``/etc/swift/proxy-server.conf``.
@@ -43,5 +48,54 @@ class SwiftProxyServerConf(IniConfigFile):
         'egg:swift#catch_errors'
         >>> proxy_server_conf.getint('DEFAULT', 'bind_port')
         8080
+    """
+    pass
+
+
+@parser("swift_object_expirer_conf")
+class SwiftObjectExpirerConf(IniConfigFile):
+    """
+    This class is to parse the content of the ``/etc/swift/object-expirer.conf``.
+
+    `/etc/swift/object-expirer.conf`` is in the standard 'ini' format and is
+    read by the :py:class:`insights.core.IniConfigFile` parser class.
+
+    Sample configuration file::
+
+        [DEFAULT]
+
+        [object-expirer]
+        # auto_create_account_prefix = .
+        auto_create_account_prefix = .
+        process=0
+        concurrency=1
+        recon_cache_path=/var/cache/swift
+        interval=300
+        reclaim_age=604800
+        report_interval=300
+        processes=0
+        expiring_objects_account_name=expiring_objects
+
+        [pipeline:main]
+        pipeline = catch_errors cache proxy-server
+
+        [app:proxy-server]
+        use = egg:swift#proxy
+
+        [filter:cache]
+        use = egg:swift#memcache
+        memcache_servers = 172.16.64.60:11211
+
+        [filter:catch_errors]
+        use = egg:swift#catch_errors
+
+    Examples:
+        >>> object_expirer_conf = shared[SwiftObjectExpirerConf]
+        >>> 'filter:cache' in proxy_server_conf
+        True
+        >>> proxy_server_conf.get('filter:cache', 'memcache_servers')
+        '172.16.64.60:11211'
+        >>> proxy_server_conf.getint('object-expirer', 'report_interval')
+        300
     """
     pass
