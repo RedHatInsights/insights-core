315278c76594147cf1aa9017bb2ef49a4d90d191
vishwanathjadhav <vjadhav@redhat.com>
2018-01-10 09:52:36 +0530

    Parser net snmp (#868)
    * added formal code of parser
    
    * Added code for parser
    
    * added doc strings
    
    * added doc strings
    
    * Updated docstrings
    
    * Removed white spaces
    
    * 1. Removed hard coding of data ids.
    2. Moved parsing code to parse_content func.
    3. Change doc string, added return type when data is absent.
    4. Organized contents in specs file according to order.
    
    * Removed shippable Warnings
    1. Fixed flake8 warnings.
    2. Modified doc strings.
    Changed class name before there was spelling mistake.
    
    * Modified assert check
    
    * Modifed get method
    Remvoed maping funtion.
    
    * Modified doc strings
    
    * Modified doc strings
    1. Moved example section to class
    2. Adjusted Resultant data.

---
+++ docs/shared_parsers_catalog/snmp.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.parsers.snmp
+   :members:
+   :show-inheritance:
---
+++ insights/parsers/snmp.py
@@ -0,0 +1,81 @@
+"""
+TcpIpStats - file ``/proc/net/snmp``
+====================================
+The ``TcpIpStats`` class implements the parsing of ``/proc/net/snmp``
+file, which contains TCP/IP stats of individual layer.
+
+"""
+
+from .. import Parser, parser, LegacyItemAccess
+
+
+@parser('proc_snmp_ipv4')
+class TcpIpStats(Parser, LegacyItemAccess):
+    """
+    Parser for ``/proc/net/snmp`` file.
+
+    Sample input is provided in the *Examples*.
+
+    Examples:
+
+        >>> SNMP_CONTENT = '''
+        ... Ip: Forwarding DefaultTTL InReceives InHdrErrors InAddrErrors ForwDatagrams InUnknownProtos InDiscards InDelivers OutRequests OutDiscards OutNoRoutes ReasmTimeout ReasmReqds ReasmOKs ReasmFails FragOKs FragFails FragCreates
+        ... Ip: 2 64 43767 0 0 0 0 0 41807 18407 12 73 0 0 0 10 0 0 0
+        ... Icmp: InMsgs InErrors InCsumErrors InDestUnreachs InTimeExcds InParmProbs InSrcQuenchs InRedirects InEchos InEchoReps InTimestamps InTimestampReps InAddrMasks InAddrMaskReps OutMsgs OutErrors OutDestUnreachs OutTimeExcds OutParmProbs OutSrcQuenchs OutRedirects OutEchos OutEchoReps OutTimestamps OutTimestampReps OutAddrMasks OutAddrMaskReps
+        ... Icmp: 34 0 0 34 0 0 0 0 0 0 0 0 0 0 44 0 44 0 0 0 0 0 0 0 0 0 0
+        ... IcmpMsg: InType3 OutType3
+        ... IcmpMsg: 34 44
+        ... Tcp: RtoAlgorithm RtoMin RtoMax MaxConn ActiveOpens PassiveOpens AttemptFails EstabResets CurrEstab InSegs OutSegs RetransSegs InErrs OutRsts InCsumErrors
+        ... Tcp: 1 200 120000 -1 444 0 0 6 7 19269 17050 5 4 234 0
+        ... Udp: InDatagrams NoPorts InErrors OutDatagrams RcvbufErrors SndbufErrors InCsumErrors IgnoredMulti
+        ... Udp: 18905 34 0 1348 0 0 0 3565
+        ... UdpLite: InDatagrams NoPorts InErrors OutDatagrams RcvbufErrors SndbufErrors InCsumErrors IgnoredMulti
+        ... UdpLite: 0 0 0 0 0 0 0 0
+        ... '''.strip()
+        >>> from insights.tests import context_wrap
+        >>> shared = {TcpIpStats: TcpIpStats(context_wrap(SNMP_CONTENT))}
+        >>> stats = shared[TcpIpStats]
+        >>> snmp_stats = stats.get("Ip")
+        >>> print snmp_stats["DefaultTTL"]
+        64
+        >>> snmp_stats = stats.get("Udp")
+        >>> print snmp_stats["InDatagrams"]
+        18905
+
+
+    Resultant Data::
+
+        {
+            'Ip':
+                {
+                    'FragCreates': 0,
+                    'ReasmFails': 10,
+                    'Forwarding': 2,
+                    'ReasmOKs': 0,
+                    'ReasmReqds': 0,
+                    'ReasmTimeout': 0,
+                    ...
+                    ...
+                },
+            'Icmp':
+                {
+                    'InRedirects': 0,
+                    'InMsgs': 34,
+                    'InSrcQuenchs': 0,
+                    ...
+                    ...
+                }
+            ...
+            ...
+        }
+    """
+    def parse_content(self, content):
+        snmp_stats = {}
+        data_id = []
+        for line in content:
+            line_split = line.split()
+            if line_split[1].isdigit():
+                snmp_stats[line_split[0].replace(":", "")] = dict(zip(data_id, map(int, line_split[1:])))
+            else:
+                data_id = line_split[1:]
+        self.data = snmp_stats
