e38ceb8a5359aecf7499cc73920399f955d3d34c
vishwanathjadhav <vjadhav@redhat.com>
2018-01-11 10:51:59 +0530

    Parser net snmp ipv6 (#883)
    * Added parser code for parsing for /proc/net/snmp6
    
    * Removed flake8 warning and fixed Travis CI issue
    
    * Added spec and parser to snmp6 file
    
    * Added snmp6 test in another test function
    
    * Removed flake8 warnings

--- insights/parsers/tests/test_snmp.py
+++ insights/parsers/tests/test_snmp.py
@@ -1,4 +1,5 @@
 from insights.parsers.snmp import TcpIpStats
+from insights.parsers.snmp import TcpIpStatsIPV6
 from insights.tests import context_wrap
 
 PROC_SNMP = """
@@ -17,6 +18,107 @@ UdpLite: 0 10 0 0 0 100
 """.strip()
 
 PROC_SNMP_NO = """
+""".strip()
+
+PROC_SNMP6 = """
+Ip6InReceives                   	757
+Ip6InHdrErrors                  	0
+Ip6InTooBigErrors               	0
+Ip6InNoRoutes                   	0
+Ip6InAddrErrors                 	0
+Ip6InUnknownProtos              	0
+Ip6InTruncatedPkts              	0
+Ip6InDiscards                   	0
+Ip6InDelivers                   	748
+Ip6OutForwDatagrams             	0
+Ip6OutRequests                  	713
+Ip6OutDiscards                  	0
+Ip6OutNoRoutes                  	0
+Ip6ReasmTimeout                 	0
+Ip6ReasmReqds                   	0
+Ip6ReasmOKs                     	0
+Ip6ReasmFails                   	0
+Ip6FragOKs                      	0
+Ip6FragFails                    	0
+Ip6FragCreates                  	0
+Ip6InMcastPkts                  	99
+Ip6OutMcastPkts                 	71
+Ip6InOctets                     	579410
+Ip6OutOctets                    	1553244
+Ip6InMcastOctets                	9224
+Ip6OutMcastOctets               	5344
+Ip6InBcastOctets                	0
+Ip6OutBcastOctets               	0
+Ip6InNoECTPkts                  	759
+Ip6InECT1Pkts                   	0
+Ip6InECT0Pkts                   	0
+Ip6InCEPkts                     	0
+Icmp6InMsgs                     	94
+Icmp6InErrors                   	0
+Icmp6OutMsgs                    	41
+Icmp6OutErrors                  	0
+Icmp6InCsumErrors               	0
+Icmp6InDestUnreachs             	0
+Icmp6InPktTooBigs               	0
+Icmp6InTimeExcds                	0
+Icmp6InParmProblems             	0
+Icmp6InEchos                    	0
+Icmp6InEchoReplies              	0
+Icmp6InGroupMembQueries         	28
+Icmp6InGroupMembResponses       	0
+Icmp6InGroupMembReductions      	0
+Icmp6InRouterSolicits           	0
+Icmp6InRouterAdvertisements     	62
+Icmp6InNeighborSolicits         	3
+Icmp6InNeighborAdvertisements   	1
+Icmp6InRedirects                	0
+Icmp6InMLDv2Reports             	0
+Icmp6OutDestUnreachs            	0
+Icmp6OutPktTooBigs              	0
+Icmp6OutTimeExcds               	0
+Icmp6OutParmProblems            	0
+Icmp6OutEchos                   	0
+Icmp6OutEchoReplies             	0
+Icmp6OutGroupMembQueries        	0
+Icmp6OutGroupMembResponses      	0
+Icmp6OutGroupMembReductions     	0
+Icmp6OutRouterSolicits          	1
+Icmp6OutRouterAdvertisements    	0
+Icmp6OutNeighborSolicits        	3
+Icmp6OutNeighborAdvertisements  	3
+Icmp6OutRedirects               	0
+Icmp6OutMLDv2Reports            	34
+Icmp6InType130                  	28
+Icmp6InType134                  	62
+Icmp6InType135                  	3
+Icmp6InType136                  	1
+Icmp6OutType133                 	1
+Icmp6OutType135                 	3
+Icmp6OutType136                 	3
+Icmp6OutType143                 	34
+Udp6InDatagrams                 	0
+Udp6NoPorts                     	0
+Udp6InErrors                    	0
+Udp6OutDatagrams                	0
+Udp6RcvbufErrors                	0
+Udp6SndbufErrors                	0
+Udp6InCsumErrors                	0
+UdpLite6InDatagrams             	0
+UdpLite6NoPorts                 	0
+UdpLite6InErrors                	0
+UdpLite6OutDatagrams            	0
+UdpLite6RcvbufErrors            	0
+UdpLite6SndbufErrors            	0
+UdpLite6InCsumErrors            	0
+""".strip()
+
+PROC_SNMP6_ODD = """
+Ip6InReceives                   	757
+Ip6InHdrErrors                  	0
+Icmp6OutMLDv2Reports            	0
+Icmp6InType130                  	28
+Icmp6InType134                  	62
+Ip6InDiscards
 """.strip()
 
 
@@ -50,3 +152,16 @@ def test_snmp():
     stats = TcpIpStats(context_wrap(PROC_SNMP_NO))
     snmp_stats = stats.get("Ip")
     assert snmp_stats is None
+
+
+def test_snmp6():
+    stats = TcpIpStatsIPV6(context_wrap(PROC_SNMP6))
+    snmp6_stats_RX = stats.get("Ip6InReceives")
+    snmp6_stats_MLD = stats.get("Icmp6OutMLDv2Reports")
+    assert snmp6_stats_RX == 757
+    assert snmp6_stats_MLD == 34
+    stats = TcpIpStatsIPV6(context_wrap(PROC_SNMP6_ODD))
+    snmp6_stats_disx = stats.get("Ip6InDiscards")
+    snmp6_stats_odd = stats.get("some_unknown")
+    assert snmp6_stats_disx is None
+    assert snmp6_stats_odd is None
