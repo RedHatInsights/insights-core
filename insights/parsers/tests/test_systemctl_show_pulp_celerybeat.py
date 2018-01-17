0563bde28454fb95b2f044c3a484273263b7886b
TripleJQK <33711530+TripleJQK@users.noreply.github.com>
2018-01-17 13:31:24 +0800

    Add 3 new parsers SystemctlShowPulp* (#887)
    * Add 3 new parsers in systemctl_show.py
    
    * Fix tipical output unmapping, make spec systemctl_pulp_resource_manager shorter
    
    * make the format of pulp_resmg in spec file better
    
    * update format in the example part for pulp classes
    
    * Add subheading comments for three pulp related parsers
    
    * edit subheading for '----' length
    
    * edit subheading for '----' length for pulp_celerybeat
    
    * fix typo

---
+++ insights/parsers/tests/test_systemctl_show_pulp_celerybeat.py
@@ -0,0 +1,34 @@
+from insights.parsers.systemctl_show import SystemctlShowPulpCelerybeat
+from insights.tests import context_wrap
+
+
+SYSTEMCTL_SHOW_PULP_CELERYBEAT = """
+WatchdogUSec=0
+WatchdogTimestamp=Thu 2018-01-11 14:22:33 CST
+WatchdogTimestampMonotonic=105028136
+StartLimitInterval=10000000
+StartLimitBurst=5
+StartLimitAction=none
+FailureAction=none
+PermissionsStartOnly=no
+RootDirectoryStartOnly=no
+RemainAfterExit=no
+GuessMainPID=yes
+MainPID=2810
+ControlPID=0
+FileDescriptorStoreMax=0
+StatusErrno=0
+Result=success
+ExecMainStartTimestamp=Thu 2018-01-11 14:22:33 CST
+ExecMainStartTimestampMonotonic=105028117
+ExecMainExitTimestampMonotonic=0
+ExecMainPID=2810
+LimitNOFILE=4096
+"""
+
+
+def test_systemctl_show_pulp_celerybeat():
+    context = context_wrap(SYSTEMCTL_SHOW_PULP_CELERYBEAT)
+    result = SystemctlShowPulpCelerybeat(context).data
+    assert result["LimitNOFILE"] == "4096"
+    assert len(result) == 21
