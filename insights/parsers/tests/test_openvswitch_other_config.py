d18c624a6d971e1bf1c49b8d713cedaacfd126fa
Chenlizhong <lichen@redhat.com>
2018-01-18 10:25:20 +0800

    Add ovs other config parser 1.x (#899)
    * Add ovs-vsctl get other_config command into specs
    
    * Add parser openvswitch_other_config
    
    * Add doc
    
    * Update parser openvswitch_other_config
    
    * remove whitespace in blank lines
    
    * Use optlist_to_dict function
    
    Signed-off-by: chenlizhong <lichen@redhat.com>
    
    * Update strip and docstring

---
+++ insights/parsers/tests/test_openvswitch_other_config.py
@@ -0,0 +1,55 @@
+from insights.parsers.openvswitch_other_config import OpenvSwitchOtherConfig
+from insights.tests import context_wrap
+
+other_config_1 = """
+{dpdk-init="true", dpdk-lcore-mask="30000003000", dpdk-socket-mem="4096,4096", pmd-cpu-mask="30000003000"}
+""".strip()
+
+other_config_2 = """
+{dpdk-init="true", dpdk-lcore-mask="c00000c00", dpdk-socket-mem="4096,4096", pmd-cpu-mask="c00000c00"}
+""".strip()
+
+other_config_3 = """
+{}
+""".strip()
+
+other_config_4 = """
+{pmd-cpu-mask="3000000000030030030"}
+""".strip()
+
+other_config_5 = """
+ovs-vsctl: unix:/var/run/openvswitch/db.sock: database connection failed (Permission denied)
+""".strip()
+
+other_config_6 = """
+bash: ovs-vsctl: command not found...
+""".strip()
+
+
+def test_openvswitch_():
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_1))
+    assert "pmd-cpu-mask" in result
+    assert result.get("dpdk-init") == "true"
+    assert result.get("dpdk-socket-mem") == "4096,4096"
+    assert result.get("pmd-cpu-mask") == "30000003000"
+    assert result.get("dpdk-lcore-mask") == "30000003000"
+    assert result["dpdk-lcore-mask"] == "30000003000"
+
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_2))
+    assert result.get("dpdk-init") == "true"
+    assert result.get("dpdk-socket-mem") == "4096,4096"
+    assert result.get("pmd-cpu-mask") == "c00000c00"
+
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_3))
+    assert result.get("dpdk-init") is None
+
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_4))
+    assert result.get("dpdk-init") is None
+    assert result.get("pmd-cpu-mask") == "3000000000030030030"
+
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_5))
+    assert result.get("dpdk-init") is None
+
+    result = OpenvSwitchOtherConfig(context_wrap(other_config_6))
+    assert result.get("dpdk-init") is None
+    assert "pmd-cpu-mask" not in result
