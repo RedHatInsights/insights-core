23790ca0300478d9e3cf3889bcdd36e42c23904f
TripleJQK <jquan@redhat.com>
2018-01-23 16:23:32 +0800

    Add 3 new parsers SystemctlShowPulp*  (#888)
    * Use 3.x format in systemctl_show.py for new 3 pulp parsers
    
    * Add doctest for systemctl_show, merge the unit test for previous test_systemctl_show_* to one file test_systemctl_show
    
    * delete the test unit files for previous systemctl_show_*
    
    * add pulp related setting into specs
    
    * delete the pulp test unit
    
    * fix error in systemctl_show.py
    
    * delete wrong blank
    
    * fix format of file
    
    * edit the format for file
    
    * fix file format for docstring
    
    * add pulp parsers in specs_insights_archive file
    
    * fix pulp related content

--- insights/parsers/systemctl_show.py
+++ insights/parsers/systemctl_show.py
@@ -1,7 +1,6 @@
 """
 Systemd command ``show`` outputs
 ================================
-
 This command shows all the paramters for this service, these parameters can be set
 through unit files corresponding to that service or from command line.
 
@@ -13,6 +12,14 @@ SystemctlShowCinderVolume - command ``systemctl show openstack-cinder-volume``
 SystemctlShowMariaDB - command ``systemctl show mariadb``
 ---------------------------------------------------------
 
+SystemctlShowPulpWorkers - command ``systemctl show pulp_workers``
+------------------------------------------------------------------
+
+SystemctlShowPulpResourceManager - command ``systemctl show pulp_resource_manager``
+-----------------------------------------------------------------------------------
+
+SystemctlShowPulpCelerybeat - command ``systemctl show pulp_celerybeat``
+------------------------------------------------------------------------
 """
 
 from .. import LegacyItemAccess
@@ -52,10 +59,10 @@ class SystemctlShow(LegacyItemAccess, Parser):
 
 @parser(Specs.systemctl_cinder_volume)
 class SystemctlShowCinderVolume(SystemctlShow):
+    """
+    Class for ``systemctl show openstack-cinder-volume``.
 
-    """Class for ``systemctl show openstack-cinder-volume``
-
-       Typical output of ``/bin/systemctl show openstack-cinder-volume`` command is::
+    Typical output of ``/bin/systemctl show openstack-cinder-volume`` command is::
 
         Restart=no
         NotifyAccess=none
@@ -69,22 +76,23 @@ class SystemctlShowCinderVolume(SystemctlShow):
         LimitAS=18446744073709551615
         LimitNPROC=63391
         Transient=no
+        LimitNOFILE=4096
         ...
 
     Examples:
-        >>> data = shared[SystemctlShowCinderVolume]
-        >>> data["LimitNOFILE"]
-        65536
+        >>> systemctl_show_cinder_volume["LimitNOFILE"]
+        '4096'
+
     """
     pass
 
 
 @parser(Specs.systemctl_mariadb)
 class SystemctlShowMariaDB(SystemctlShow):
+    """
+    Class for ``systemctl show mariadb``.
 
-    """Class for ``systemctl show mariadb``
-
-       Typical output of ``/bin/systemctl show mariadb`` command is::
+    Typical output of ``/bin/systemctl show mariadb`` command is::
 
         Type=simple
         Restart=no
@@ -98,11 +106,112 @@ class SystemctlShowMariaDB(SystemctlShow):
         ControlGroup=/system.slice/mariadb.service
         After=network.target -.mount systemd-journald.socket tmp.mount basic.target syslog.target system.slice
         MemoryCurrent=18446744073709551615
+        LimitNOFILE=4096
         ...
 
     Examples:
-        >>> SrvMariaDB = shared[SystemctlShowMariaDB]
-        >>> SrvMariaDB["After"]
-        network.target -.mount systemd-journald.socket tmp.mount basic.target syslog.target system.slice
+        >>> systemctl_show_mariadb["LimitNOFILE"]
+        '4096'
+
+    """
+    pass
+
+
+@parser(Specs.systemctl_pulp_workers)
+class SystemctlShowPulpWorkers(SystemctlShow):
+    """
+    Class for ``systemctl show pulp_workers``.
+
+    Typical output of ``/bin/systemctl show pulp_workers`` command is::
+
+        Type=oneshot
+        Restart=no
+        NotifyAccess=none
+        RestartUSec=100ms
+        TimeoutStartUSec=0
+        TimeoutStopUSec=1min 30s
+        WatchdogUSec=0
+        WatchdogTimestampMonotonic=0
+        ExecMainStartTimestamp=Thu 2018-01-11 14:22:33 CST
+        ExecMainStartTimestampMonotonic=105521850
+        ExecMainExitTimestamp=Thu 2018-01-11 14:22:33 CST
+        ExecMainExitTimestampMonotonic=105593405
+        ExecStart={ path=/usr/libexec/pulp-manage-workers ; argv[]=/usr/libexec/pulp-manage-workers start ; ignore_err
+        ExecStop={ path=/usr/libexec/pulp-manage-workers ; argv[]=/usr/libexec/pulp-manage-workers stop ; ignore_error
+        Slice=system.slice
+        After=systemd-journald.socket system.slice network.target basic.target
+        LimitNOFILE=4096
+        ...
+
+    Examples:
+        >>> systemctl_show_pulp_workers["LimitNOFILE"]
+        '4096'
+
+    """
+    pass
+
+
+@parser(Specs.systemctl_pulp_resmg)
+class SystemctlShowPulpResourceManager(SystemctlShow):
+    """
+    Class for ``systemctl show pulp_resource_manager``.
+
+    Typical output of ``/bin/systemctl show pulp_resource_manager`` command is::
+
+        Type=simple
+        Restart=no
+        NotifyAccess=none
+        RestartUSec=100ms
+        TimeoutStartUSec=1min 30s
+        TimeoutStopUSec=1min 30s
+        ExecMainStartTimestamp=Thu 2018-01-11 14:22:33 CST
+        ExecMainStartTimestampMonotonic=105028117
+        ExecMainExitTimestampMonotonic=0
+        ExecMainPID=2810
+        ExecMainCode=0
+        ExecMainStatus=0
+        ExecStart={ path=/usr/bin/celery ; argv[]=/usr/bin/celery worker -A pulp.server.async.app -n resource_manager@
+        Slice=system.slice
+        After=basic.target network.target system.slice -.mount systemd-journald.socket
+        LimitNOFILE=4096
+        ...
+
+    Examples:
+        >>> systemctl_show_pulp_resource_manager["LimitNOFILE"]
+        '4096'
+
+    """
+    pass
+
+
+@parser(Specs.systemctl_pulp_celerybeat)
+class SystemctlShowPulpCelerybeat(SystemctlShow):
+    """
+    Class for ``systemctl show pulp_celerybeat``.
+
+    Typical output of ``/bin/systemctl show pulp_celerybeat`` command is::
+
+        Type=simple
+        Restart=no
+        NotifyAccess=none
+        RestartUSec=100ms
+        TimeoutStartUSec=1min 30s
+        TimeoutStopUSec=1min 30s
+        ExecMainStartTimestamp=Thu 2018-01-11 14:22:32 CST
+        ExecMainStartTimestampMonotonic=104261679
+        ExecMainExitTimestampMonotonic=0
+        ExecMainPID=2747
+        ExecMainCode=0
+        ExecMainStatus=0
+        ExecStart={ path=/usr/bin/celery ; argv[]=/usr/bin/celery beat --scheduler=pulp.server.async.scheduler.Schedul
+        Slice=system.slice
+        After=basic.target network.target system.slice -.mount systemd-journald.socket
+        LimitNOFILE=4096
+        ...
+
+    Examples:
+        >>> systemctl_show_pulp_celerybeat["LimitNOFILE"]
+        '4096'
+
     """
     pass
