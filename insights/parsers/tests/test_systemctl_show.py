fe4da25a237e9cf1a2299ca61eb670c382bf57d6
TripleJQK <jquan@redhat.com>
2018-01-23 16:23:32 +0800

    Add 3 new parsers SystemctlShowPulp*  (#888)
    * Use 3.x format in systemctl_show.py for new 3 pulp parsers
    
    * Add doctest for systemctl_show, merge the unit test for previous test_systemctl_show_* to one file test_systemctl_show
    
    * delete the test unit files for previous systemctl_show_*
    
    * add pulp related setting into specs
    
    * delete the pulp test unit
    
    * fix error in systemctl_show.py
    
    * delete wrong blank
    
    * fix format of file
    
    * edit the format for file
    
    * fix file format for docstring
    
    * add pulp parsers in specs_insights_archive file
    
    * fix pulp related content

---
+++ insights/parsers/tests/test_systemctl_show.py
@@ -0,0 +1,80 @@
+from insights.tests import context_wrap
+import doctest
+from insights.parsers import systemctl_show
+from insights.parsers.systemctl_show import SystemctlShowCinderVolume
+from insights.parsers.systemctl_show import SystemctlShowMariaDB
+from insights.parsers.systemctl_show import SystemctlShowPulpWorkers
+from insights.parsers.systemctl_show import SystemctlShowPulpResourceManager
+from insights.parsers.systemctl_show import SystemctlShowPulpCelerybeat
+
+
+SYSTEMCTL_SHOW_EXAMPLES = """
+WatchdogUSec=0
+WatchdogTimestamp=Thu 2018-01-11 14:22:33 CST
+WatchdogTimestampMonotonic=105028136
+StartLimitInterval=10000000
+StartLimitBurst=5
+StartLimitAction=none
+FailureAction=none
+PermissionsStartOnly=no
+RootDirectoryStartOnly=no
+RemainAfterExit=no
+GuessMainPID=yes
+MainPID=2810
+ControlPID=0
+FileDescriptorStoreMax=0
+StatusErrno=0
+Result=success
+ExecMainStartTimestamp=Thu 2018-01-11 14:22:33 CST
+ExecMainStartTimestampMonotonic=105028117
+ExecMainExitTimestampMonotonic=0
+ExecMainPID=2811
+LimitNOFILE=4096
+"""
+
+
+def test_systemctl_show_cinder_volume():
+    context = SystemctlShowCinderVolume(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    assert context["LimitNOFILE"] == "4096"
+    assert len(context.data) == 21
+
+
+def test_systemctl_show_mariadb():
+    context = SystemctlShowMariaDB(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    assert context["LimitNOFILE"] == "4096"
+    assert len(context.data) == 21
+
+
+def test_systemctl_show_pulp_workers():
+    context = SystemctlShowPulpWorkers(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    assert context["LimitNOFILE"] == "4096"
+    assert len(context.data) == 21
+
+
+def test_systemctl_show_pulp_resource_manager():
+    context = SystemctlShowPulpResourceManager(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    assert context["LimitNOFILE"] == "4096"
+    assert len(context.data) == 21
+
+
+def test_systemctl_show_pulp_celerybeat():
+    context = SystemctlShowPulpCelerybeat(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    assert context["LimitNOFILE"] == "4096"
+    assert len(context.data) == 21
+
+
+def test_systemctl_show_doc_examples():
+    env = {
+        'SystemctlShowCinderVolume': SystemctlShowCinderVolume,
+        'systemctl_show_cinder_volume': SystemctlShowCinderVolume(context_wrap(SYSTEMCTL_SHOW_EXAMPLES)),
+        'SystemctlShowMariaDB': SystemctlShowMariaDB,
+        'systemctl_show_mariadb': SystemctlShowMariaDB(context_wrap(SYSTEMCTL_SHOW_EXAMPLES)),
+        'SystemctlShowPulpWorkers': SystemctlShowPulpWorkers,
+        'systemctl_show_pulp_workers': SystemctlShowPulpWorkers(context_wrap(SYSTEMCTL_SHOW_EXAMPLES)),
+        'SystemctlShowPulpResourceManager': SystemctlShowPulpResourceManager,
+        'systemctl_show_pulp_resource_manager': SystemctlShowPulpResourceManager(context_wrap(SYSTEMCTL_SHOW_EXAMPLES)),
+        'SystemctlShowPulpCelerybeat': SystemctlShowPulpCelerybeat,
+        'systemctl_show_pulp_celerybeat': SystemctlShowPulpCelerybeat(context_wrap(SYSTEMCTL_SHOW_EXAMPLES))
+    }
+    failed, total = doctest.testmod(systemctl_show, globs=env)
+    assert failed == 0
