a3fc70ceea7e8160ed724b6d13360d6a3abd7fdd
Paul Wayper <paulway@mabula.net>
2018-02-20 10:14:12 +1100

    Pulp worker defaults for master branch (#954)
    * Adding PulpWorkerDefaults parser with docs and tests
    
    * Documentation markup include file from template
    
    * Added spec for /etc/default/pulp_workers file

---
+++ insights/parsers/tests/test_pulp_worker_defaults.py
@@ -0,0 +1,28 @@
+from ...parsers import pulp_worker_defaults
+from ...tests import context_wrap
+
+import doctest
+
+pulp_worker_defaults_in_docs = '''
+# Configuration file for Pulp's Celery workers
+
+# Define the number of worker nodes you wish to have here. This defaults to the number of processors
+# that are detected on the system if left commented here.
+PULP_CONCURRENCY=1
+
+# Configure Python's encoding for writing all logs, stdout and stderr
+PYTHONIOENCODING="UTF-8"
+'''
+
+
+def test_pulp_worker_defaults_docs():
+    env = {
+        'PulpWorkerDefaults': pulp_worker_defaults.PulpWorkerDefaults,
+        'shared': {
+            pulp_worker_defaults.PulpWorkerDefaults: pulp_worker_defaults.PulpWorkerDefaults(
+                context_wrap(pulp_worker_defaults_in_docs)
+            )
+        },
+    }
+    failed, total = doctest.testmod(pulp_worker_defaults, globs=env)
+    assert failed == 0
