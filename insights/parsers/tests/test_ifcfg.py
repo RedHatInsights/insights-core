a1b37bdfbaef232339f366572fb5da4615bbe57f
vishwanathjadhav <vjadhav@redhat.com>
2018-03-08 11:15:53 +0530

    Modified IfCFG parser (#992)
    * Modified code to remove spacess added around the operator while
    configuring interface with options
    
    * Modified to keep in sync with 1.x
    
    * Removed white space
    
    * removed white space and modified wrong string
    
    * Added test cases

--- insights/parsers/tests/test_ifcfg.py
+++ insights/parsers/tests/test_ifcfg.py
@@ -100,7 +100,18 @@ BOOTPROTO=none
 ONBOOT=yes
 USERCTL=no
 IPV6INIT=no
-BONDING_OPTS="mode=balance-xor primary=eth1 arp_interval=1000 arp_ip_target=+10.11.96.1"
+BONDING_OPTS="mode=balance-xor primary=eth1 arp_interval=1000 arp_ip_target=+10.11.96.1 downdelay =0"
+""".strip()
+
+IFCFG_CONFIG_STR_ERROR = """
+DEVICE=bond0
+IPADDR=10.11.96.172
+NETMASK=255.255.252.0
+BOOTPROTO=none
+ONBOOT=yes
+USERCTL=no
+IPV6INIT=no
+BONDING_OPTS="mode=balance-xor primary = eth1 arp_interval= 1000 arp_ip_target=+10.11.96.1 downdelay =0"
 """.strip()
 
 IFCFG_PATH_NAMED_BOND_MODE = "etc/sysconfig/network-scripts/ifcfg-en0"
@@ -236,3 +247,15 @@ def test_ifcfg_badly_named_bond_mode():
     r = IfCFG(context)
 
     assert r.bonding_mode is None
+
+
+def test_ifcfg_bonding_opts():
+    context = context_wrap(IFCFG_TEST_NAMED_BOND_MODE)
+    context.path = IFCFG_PATH_NAMED_BOND_MODE
+
+    r = IfCFG(context)
+
+    assert r["BONDING_OPTS"]["mode"] == "balance-xor"
+    assert r["BONDING_OPTS"]["arp_ip_target"] == "+10.11.96.1"
+    assert r["BONDING_OPTS"]["downdelay"] == "0"
+    assert r.bonding_mode == 2
