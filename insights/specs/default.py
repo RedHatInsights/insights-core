1a110559f8a50dcb0bda44f9a9c69963caef0625
TripleJQK <jquan@redhat.com>
2018-03-13 09:20:35 +0800

    Parser for get /etc/splice/checkin.conf (#1004)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -59,6 +59,7 @@ class DefaultSpecs(Specs):
     brctl_show = simple_command("/usr/sbin/brctl show")
     candlepin_log = simple_file("/var/log/candlepin/candlepin.log")
     candlepin_error_log = simple_file("/var/log/candlepin/error.log")
+    checkin_conf = simple_file("/etc/splice/checkin.conf")
     ps_aux = simple_command("/bin/ps aux")
     ps_auxcww = simple_command("/bin/ps auxcww")
     ps_auxww = simple_command("/bin/ps auxww")
