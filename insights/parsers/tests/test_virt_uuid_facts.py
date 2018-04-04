a0cf6f428917b1f96db911f0e0803c2f7e9a2786
TripleJQK <jquan@redhat.com>
2018-04-04 09:52:51 -0500

    New parser for etc rhsm facts virt UUID facts (#1072)
    * new parser for virt_uuid.facts
    
    * fix typo
    
    * edit the flake8 format
    
    * delete wrong entry in insights_archive
    
    (cherry picked from commit b6c090c49b3788d4519038eac0d26ec62b73adb6)

---
+++ insights/parsers/tests/test_virt_uuid_facts.py
@@ -0,0 +1,29 @@
+import doctest
+
+from insights.parsers import virt_uuid_facts
+from insights.parsers.virt_uuid_facts import VirtUuidFacts
+from insights.tests import context_wrap
+
+
+VIRT_UUID_FACTS = """
+{"virt.uuid": "4546B285-6C41-5D6R-86G5-0BFR4B3625FS", "uname.machine": "x86"}
+""".strip()
+
+
+def test_virt_uuid_facts():
+    result = VirtUuidFacts(context_wrap(VIRT_UUID_FACTS))
+
+    assert result.data == {
+            'virt.uuid': '4546B285-6C41-5D6R-86G5-0BFR4B3625FS',
+            "uname.machine": "x86"
+    }
+    assert result.data['virt.uuid'] == '4546B285-6C41-5D6R-86G5-0BFR4B3625FS'
+
+
+def test_virt_uuid_facts_doc_examples():
+    env = {
+        'VirtUuidFacts': VirtUuidFacts,
+        'virt_uuid_facts': VirtUuidFacts(context_wrap(VIRT_UUID_FACTS)),
+    }
+    failed, total = doctest.testmod(virt_uuid_facts, globs=env)
+    assert failed == 0
