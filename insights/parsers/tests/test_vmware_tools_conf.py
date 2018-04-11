07b550bc815357d3caa8c2da52c996da824c9db5
Sachin Patil <psachin@redhat.com>
2018-04-11 12:48:13 +0530

    Parser for `/etc/vmware-tools/tools.conf`
    Included `glob` to match `Examples` in docstring. That should avoid
    setting up and context object in docstring itself.
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- insights/parsers/tests/test_vmware_tools_conf.py
+++ insights/parsers/tests/test_vmware_tools_conf.py
@@ -17,15 +17,18 @@ vmtoolsd.data = /tmp/vmtoolsd.log
 
 
 def test_vmware_tools_conf():
-    result = vmware_tools_conf.VMwareToolsConf(context_wrap(CONF))
-    assert result.sections() == ['guestinfo', 'logging']
-    assert result.has_option('guestinfo', 'disable-query-diskinfo') is True
-    assert result.getboolean('guestinfo', 'disable-query-diskinfo') is True
-    assert result.get('guestinfo', 'disable-query-diskinfo') == 'true'
-    assert result.get('logging', 'vmtoolsd.handler') == 'file'
-    assert result.get('logging', 'vmtoolsd.data') == '/tmp/vmtoolsd.log'
+    conf = vmware_tools_conf.VMwareToolsConf(context_wrap(CONF))
+    assert conf.sections() == ['guestinfo', 'logging']
+    assert conf.has_option('guestinfo', 'disable-query-diskinfo') is True
+    assert conf.getboolean('guestinfo', 'disable-query-diskinfo') is True
+    assert conf.get('guestinfo', 'disable-query-diskinfo') == 'true'
+    assert conf.get('logging', 'vmtoolsd.handler') == 'file'
+    assert conf.get('logging', 'vmtoolsd.data') == '/tmp/vmtoolsd.log'
 
 
 def test_vmware_tools_conf_documentation():
-    failed_count, tests = doctest.testmod(vmware_tools_conf)
+    failed_count, tests = doctest.testmod(
+        vmware_tools_conf,
+        globs={'conf': vmware_tools_conf.VMwareToolsConf(context_wrap(CONF))}
+    )
     assert failed_count == 0
--- insights/parsers/vmware_tools_conf.py
+++ insights/parsers/vmware_tools_conf.py
@@ -7,33 +7,32 @@ is in the standard 'ini' format and is read by the IniConfigFile
 parser. ``vmtoolsd.service`` provided by ``open-vm-tools`` package is
 configured using ``/etc/vmware-tools/tools.conf``.
 
+
+Sample ``/etc/vmware-tools/tools.conf`` file::
+
+    [guestinfo]
+    disable-query-diskinfo = true
+
+    [logging]
+    log = true
+
+    vmtoolsd.level = debug
+    vmtoolsd.handler = file
+    vmtoolsd.data = /tmp/vmtoolsd.log
+
+
 Examples:
 
-    >>> from insights.parsers.vmware_tools_conf import VMwareToolsConf
-    >>> from insights.tests import context_wrap
-    >>> CONF = '''
-    ... [guestinfo]
-    ... disable-query-diskinfo = true
-    ...
-    ... [logging]
-    ... log = true
-    ...
-    ... vmtoolsd.level = debug
-    ... vmtoolsd.handler = file
-    ... vmtoolsd.data = /tmp/vmtoolsd.log
-    ... '''
-    >>> result = VMwareToolsConf(context_wrap(CONF))
-    >>> result.sections()
+    >>> conf.sections()
     ['guestinfo', 'logging']
-    >>> result.has_option('guestinfo', 'disable-query-diskinfo')
+    >>> conf.has_option('guestinfo', 'disable-query-diskinfo')
     True
-    >>> result.getboolean('guestinfo', 'disable-query-diskinfo')
+    >>> conf.getboolean('guestinfo', 'disable-query-diskinfo')
     True
-    >>> result.get('guestinfo', 'disable-query-diskinfo')
+    >>> conf.get('guestinfo', 'disable-query-diskinfo')
     'true'
 
 """
-
 from .. import IniConfigFile, parser
 from insights.specs import Specs
 
