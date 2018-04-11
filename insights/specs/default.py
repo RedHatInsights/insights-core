ed88e162039681c4d76cfdef3f0e5a5cdb3531d3
Sachin <iclcoolster@gmail.com>
2018-04-11 13:38:10 +0530

    Parser for `/etc/vmware-tools/tools.conf` (#1089)
    * Parser for `/etc/vmware-tools/tools.conf`
    
    A configuration file parser based on IniConfigFile.
    
    I've also included doctest based on conversation in
    https://github.com/RedHatInsights/insights-core/pull/1080
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>
    
    * Parser for `/etc/vmware-tools/tools.conf`
    
    Included `glob` to match `Examples` in docstring. That should avoid
    setting up and context object in docstring itself.
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>
    (cherry picked from commit e2851ad75ae1d40cb3d36882bf2c3d6e2cf12708)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -577,6 +577,7 @@ class DefaultSpecs(Specs):
     vdsm_conf = simple_file("etc/vdsm/vdsm.conf")
     vdsm_id = simple_file("etc/vdsm/vdsm.id")
     vdsm_log = simple_file("var/log/vdsm/vdsm.log")
+    vmware_tools_conf = simple_file("etc/vmware-tools/tools.conf")
     vgs = None  # simple_command('/sbin/vgs -v -o +vg_mda_count,vg_mda_free,vg_mda_size,vg_mda_used_count,vg_tags --config="global{locking_type=0}"')
     vgs_noheadings = simple_command("/sbin/vgs --nameprefixes --noheadings --separator='|' -a -o vg_all --config=\"global{locking_type=0}\"")
     vgs_noheadings_all = simple_command("/sbin/vgs --nameprefixes --noheadings --separator='|' -a -o vg_all --config='global{locking_type=0} devices{filter=[\"a|.*|\"]}'")
