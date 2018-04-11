e2851ad75ae1d40cb3d36882bf2c3d6e2cf12708
Sachin <iclcoolster@gmail.com>
2018-04-11 13:38:10 +0530

    Parser for `/etc/vmware-tools/tools.conf` (#1089)
    * Parser for `/etc/vmware-tools/tools.conf`
    
    A configuration file parser based on IniConfigFile.
    
    I've also included doctest based on conversation in
    https://github.com/RedHatInsights/insights-core/pull/1080
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>
    
    * Parser for `/etc/vmware-tools/tools.conf`
    
    Included `glob` to match `Examples` in docstring. That should avoid
    setting up and context object in docstring itself.
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

---
+++ insights/parsers/tests/test_vmware_tools_conf.py
@@ -0,0 +1,34 @@
+import doctest
+
+from insights.parsers import vmware_tools_conf
+from insights.tests import context_wrap
+
+CONF = '''
+[guestinfo]
+disable-query-diskinfo = true
+
+[logging]
+log = true
+
+vmtoolsd.level = debug
+vmtoolsd.handler = file
+vmtoolsd.data = /tmp/vmtoolsd.log
+'''
+
+
+def test_vmware_tools_conf():
+    conf = vmware_tools_conf.VMwareToolsConf(context_wrap(CONF))
+    assert conf.sections() == ['guestinfo', 'logging']
+    assert conf.has_option('guestinfo', 'disable-query-diskinfo') is True
+    assert conf.getboolean('guestinfo', 'disable-query-diskinfo') is True
+    assert conf.get('guestinfo', 'disable-query-diskinfo') == 'true'
+    assert conf.get('logging', 'vmtoolsd.handler') == 'file'
+    assert conf.get('logging', 'vmtoolsd.data') == '/tmp/vmtoolsd.log'
+
+
+def test_vmware_tools_conf_documentation():
+    failed_count, tests = doctest.testmod(
+        vmware_tools_conf,
+        globs={'conf': vmware_tools_conf.VMwareToolsConf(context_wrap(CONF))}
+    )
+    assert failed_count == 0
