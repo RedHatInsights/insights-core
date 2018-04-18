70731230c7d6c48cfdf89d0f7bc30088a206fdec
Paul Wayper <paulway@redhat.com>
2018-04-18 15:32:07 +1000

    Mark as heading, not subheading

--- insights/parsers/jboss_domain_log.py
+++ insights/parsers/jboss_domain_log.py
@@ -1,6 +1,6 @@
 """
 JbossDomainServerLog - file ``$JBOSS_SERVER_LOG_DIR/server.log*``
------------------------------------------------------------------
+=================================================================
 
 Parser for the JBoss Domain Server Log File.
 """
