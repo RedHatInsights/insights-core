465695b3a94532ae89b73349d1b9373dd7b369bb
Christopher Sams <csams@redhat.com>
2018-04-18 11:25:34 -0500

    Hostname parser raises ParseException instead of defaulting attrs to None on invalid data. #1113

--- insights/parsers/tests/test_hostname.py
+++ insights/parsers/tests/test_hostname.py
@@ -1,3 +1,4 @@
+import pytest
 from insights.parsers.hostname import Hostname
 from insights.tests import context_wrap
 
@@ -12,30 +13,36 @@ rhel7
 """
 
 
-def test_hostname():
+def test_full_hostname():
     data = Hostname(context_wrap(HOSTNAME))
     assert data.fqdn == "rhel7.example.com"
     assert data.hostname == "rhel7"
     assert data.domain == "example.com"
     assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
 
+
+def test_full_multiline_hostname():
     data = Hostname(context_wrap(HOSTNAME_MULTILINE, strip=False))
     assert data.fqdn == "rhel7.example.com"
     assert data.hostname == "rhel7"
     assert data.domain == "example.com"
     assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
 
+
+def test_short_hostname():
     data = Hostname(context_wrap(HOSTNAME_SHORT))
     assert data.fqdn == "rhel7"
     assert data.hostname == "rhel7"
     assert data.domain == ""
 
+
+def test_short_multiline_hostname():
     data = Hostname(context_wrap(HOSTNAME_SHORT_MULTILINE, strip=False))
     assert data.fqdn == "rhel7"
     assert data.hostname == "rhel7"
     assert data.domain == ""
 
-    data = Hostname(context_wrap(""))
-    assert data.fqdn is None
-    assert data.hostname is None
-    assert data.domain is None
+
+def test_empty_hostname():
+    with pytest.raises(Exception):
+        Hostname(context_wrap(""))
