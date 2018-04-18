5c088b0e1a36261720c2114a84f405a45d00efc6
Chris Sams <csams@users.noreply.github.com>
2018-04-18 11:01:22 -0500

    Ease hostname restrictions by filtering blank lines. Fix #1055. (#1112)

--- insights/parsers/tests/test_hostname.py
+++ insights/parsers/tests/test_hostname.py
@@ -2,7 +2,14 @@ from insights.parsers.hostname import Hostname
 from insights.tests import context_wrap
 
 HOSTNAME = "rhel7.example.com"
+HOSTNAME_MULTILINE = """
+rhel7.example.com
+"""
+
 HOSTNAME_SHORT = "rhel7"
+HOSTNAME_SHORT_MULTILINE = """
+rhel7
+"""
 
 
 def test_hostname():
@@ -12,7 +19,18 @@ def test_hostname():
     assert data.domain == "example.com"
     assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
 
+    data = Hostname(context_wrap(HOSTNAME_MULTILINE, strip=False))
+    assert data.fqdn == "rhel7.example.com"
+    assert data.hostname == "rhel7"
+    assert data.domain == "example.com"
+    assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
+
     data = Hostname(context_wrap(HOSTNAME_SHORT))
+    assert data.fqdn == "rhel7"
+    assert data.hostname == "rhel7"
+    assert data.domain == ""
+
+    data = Hostname(context_wrap(HOSTNAME_SHORT_MULTILINE, strip=False))
     assert data.fqdn == "rhel7"
     assert data.hostname == "rhel7"
     assert data.domain == ""
