618c155b1fb7934b55c96e23a8fe9a07694a5ff6
Sachin Patil <psachin@redhat.com>
2018-04-27 07:51:21 +0530

    Sample & examples in respective class
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- insights/parsers/vdsm_conf.py
+++ insights/parsers/vdsm_conf.py
@@ -1,62 +1,6 @@
 """
 VDSMConfIni & VDSMLoggerConf
 ============================
-
-``VDSMConfIni`` parsers configuration file ``/etc/vdsm/vdsm.conf``
-format and is read by the IniConfigFile parser.
-
-Sample configuration file::
-
-    [vars]
-    ssl = true
-    cpu_affinity = 1
-
-    [addresses]
-    management_port = 54321
-    qq = 345
-
-
-Examples:
-    >>> 'vars' in conf
-    True
-    >>> conf.get('addresses', 'qq')
-    '345'
-    >>> conf.getboolean('vars', 'ssl')
-    True
-    >>> conf.getint('addresses', 'management_port')
-    54321
-
-
-``VDSMLoggerConf`` parsers configuration file ``/etc/vdsm/logger.conf``
-format and is read by the IniConfigFile parser.
-
-Sample configuration file::
-
-    [loggers]
-    keys=root,vds,storage,virt,ovirt_hosted_engine_ha,ovirt_hosted_engine_ha_config,IOProcess,devel
-
-    [handlers]
-    keys=console,syslog,logfile
-
-    [formatters]
-    keys=long,simple,none,sysform
-
-Examples:
-
-    >>> len(vdsm_logger_conf.sections())
-    18
-    >>> vdsm_logger_conf.has_option('formatter_long', 'class')
-    True
-    >>> vdsm_logger_conf.has_option('loggers', 'keys')
-    True
-    >>> vdsm_logger_conf.getboolean('logger_root', 'propagate')
-    False
-    >>> vdsm_logger_conf.items('loggers')
-    {'keys': 'root,vds,storage,virt,ovirt_hosted_engine_ha,ovirt_hosted_engine_ha_config,IOProcess,devel'}
-    >>> vdsm_logger_conf.get('logger_ovirt_hosted_engine_ha', 'level')
-    'DEBUG'
-    >>> vdsm_logger_conf.get('formatter_sysform', 'datefmt')
-    ''
 """
 
 from .. import IniConfigFile, parser
@@ -65,11 +9,82 @@ from insights.specs import Specs
 
 @parser(Specs.vdsm_conf)
 class VDSMConfIni(IniConfigFile):
-    """Class for VDSM configuration file content."""
+    """``VDSMConfIni`` parsers configuration file ``/etc/vdsm/vdsm.conf``
+    format and is read by the IniConfigFile parser.
+
+    Sample configuration file::
+
+        [vars]
+        ssl = true
+        cpu_affinity = 1
+
+        [addresses]
+        management_port = 54321
+        qq = 345
+
+
+    Examples:
+        >>> 'vars' in conf
+        True
+        >>> conf.get('addresses', 'qq')
+        '345'
+        >>> conf.getboolean('vars', 'ssl')
+        True
+        >>> conf.getint('addresses', 'management_port')
+        54321
+
+    """
     pass
 
 
 @parser(Specs.vdsm_logger_conf)
 class VDSMLoggerConf(IniConfigFile):
-    """Parse VDSM logger configuration file content."""
+    """``VDSMLoggerConf`` parsers configuration file
+    ``/etc/vdsm/logger.conf`` format and is read by the IniConfigFile
+    parser.
+
+    Sample configuration file::
+
+        [loggers]
+        keys=root,vds,storage,virt,ovirt_hosted_engine_ha,ovirt_hosted_engine_ha_config,IOProcess,devel
+
+        [formatter_long]
+        format: %(asctime)s %(levelname)-5s (%(threadName)s) [%(name)s] %(message)s (%(module)s:%(lineno)d)
+        class: vdsm.logUtils.TimezoneFormatter
+
+        [logger_root]
+        level=DEBUG
+        handlers=syslog,logfile
+        propagate=0
+
+        [formatters]
+        keys=long,simple,none,sysform
+
+        [logger_ovirt_hosted_engine_ha]
+        level=DEBUG
+        handlers=
+        qualname=ovirt_hosted_engine_ha
+        propagate=1
+
+        [formatter_sysform]
+        format= vdsm %(name)s %(levelname)s %(message)s
+        datefmt=
+
+    Examples:
+        >>> len(vdsm_logger_conf.sections())
+        18
+        >>> vdsm_logger_conf.has_option('formatter_long', 'class')
+        True
+        >>> vdsm_logger_conf.has_option('loggers', 'keys')
+        True
+        >>> vdsm_logger_conf.getboolean('logger_root', 'propagate')
+        False
+        >>> vdsm_logger_conf.items('loggers')
+        {'keys': 'root,vds,storage,virt,ovirt_hosted_engine_ha,ovirt_hosted_engine_ha_config,IOProcess,devel'}
+        >>> vdsm_logger_conf.get('logger_ovirt_hosted_engine_ha', 'level')
+        'DEBUG'
+        >>> vdsm_logger_conf.get('formatter_sysform', 'datefmt')
+        ''
+
+    """
     pass
