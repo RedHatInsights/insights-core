11f9d0e6aa0bc4ac682b3853e5f8fb993de7174d
Xiangce Liu <xiangceliu@redhat.com>
2018-05-04 13:53:32 +0800

    Update AttributeDict (#1124)
    * Modify the AttributeDict to only setattr for specified keys
    
    * Re-Make AttributeDict inherit from dict
    
    - reuse AttributeDict instead of namedtuple for grub_conf and proc_limits
    
    * Update the docstring
    
    * Mark `__iter__` as deprecated
    
    * Make 'fixed_attrs' is necessary for AttributeDict
    
    - Refine the docstring
    
    * Fix error for python 2.6
    
    * Change `mount_options` and `fs_mntops` to MountOpts back
    
    - uniform the docstring
    
    * Remove useless line
    
    * Update as per Bob's feedback
    
    - Modify the typos in docstring
    - Use fixed parameters instead of "*args, **kwargs"
    - Remove the `type_info` which makes the usage complex
    - Fix mistake in `Examples`
    
    * Fix the mistake in the init of Limits in proc_limit
    
    * Fix flake8 errors
    
    * Add missed default `data` for BootEntry in grub_conf
    
    * Deprecate the AttributeDict
    
    - Backward compatibility is broken
     > __iter__ is replaced by items()
    
    * Modify GrubConf and ProcLimits to keep backward compatibility
    
    - update docstring
    
    * Revert the old test of test_fstab and test_mount
    
    (cherry picked from commit f5c22f8facc34a0a5e110c0c501b2726e95450b1)

--- insights/parsers/mount.py
+++ insights/parsers/mount.py
@@ -10,30 +10,39 @@ implements parsing for the ``mount`` command output which looks like::
     /dev/mapper/HostVG-Config on /etc/shadow type ext4 (rw,noatime,seclabel,stripe=256,data=ordered)
     dev/sr0 on /run/media/root/VMware Tools type iso9660 (ro,nosuid,nodev,relatime,uid=0,gid=0,iocharset=utf8,mode=0400,dmode=0500,uhelper=udisks2) [VMware Tools]
 
-The information is stored as a list of ``AttributeDict`` objects.  Each
-``AttributeDict`` object contains attributes for the following information that
-are listed in the same order as in the command output::
+The information is stored as a list of :class:`MountEntry` objects.  Each
+:class:`MountEntry` object contains attributes for the following information that
+are listed in the same order as in the command output:
 
-    - filesystem: (str) Name of filesystem
-    - mount_point: (str) Name of mount point for filesystem
-    - mount_type: (str) Name of filesystem type
-    - mount_options: (AttributeDict) Mount options as ``AttributeDict`` object
-    - mount_label: (str) Only present if optional label is present
-    - mount_clause: (str) Full string from command output
+ * ``filesystem`` - Name of filesystem
+ * ``mount_point`` - Name of mount point for filesystem
+ * ``mount_type`` - Name of filesystem type
+ * ``mount_options`` - Mount options as a dictionary
+ * ``mount_label`` - Only present if optional label is present
+ * ``mount_clause`` - Full string from command output
 
-The `` mount_options`` contains the mount options as attributes accessible
-via the attribute name as it appears in the command output.  For instance, the
-options ``(rw,dmode=0500)`` may be accessed as ''mnt_row_info.rw`` with the value
-``True`` and ``mnt_row_info.dmode`` with the value "0500".  The ``in`` operator
-may be used to determine if an option is present.
+The ``mount_options`` is wrapped as a :class:`MountOpts` object which contains
+below fixed attributes:
+
+* ``rw`` - Read write
+* ``ro`` - Read only
+* ``defaults`` - Use default options: rw, suid, dev, exec, auto, nouser, async, and relatime
+* ``relatime`` - Inode access times relative to modify or change time
+* ``seclabel`` - `seclabel` is enabled or not
+* ``attr2`` - `opportunistic` improvement is enabled or not
+* ``inode64`` - `inode64` is enabled or not
+* ``noquota`` - Disk quotas are enforced or not
+
+For instance, the option ``rw`` in ``(rw,dmode=0500)`` may be accessed as
+``mnt_row_info.rw`` with the value ``True``, the ``dmode`` can be accessed as
+``mnt_row_info.dmode`` with the value ``0500``.
 
 MountEntry lines are also available in a ``mounts`` property, keyed on the
 mount point.
 
 Examples:
-    >>> mnt_info = shared[Mount]
-    >>> mnt_info
-    <insights.parsers.mount.Mount at 0x7fd4a7d3bbd0>
+    >>> type(mnt_info)
+    <class 'insights.parsers.mount.Mount'>
     >>> len(mnt_info)
     4
     >>> mnt_info[3].__dict__
@@ -52,18 +61,121 @@ Examples:
     >>> mnt_info[3].mount_options
     {'dmode': '0500', 'gid': '0', 'iocharset': 'utf8', 'mode': '0400', 'nodev': True,
      'nosuid': True, 'relatime': True, 'ro': True, 'uhelper': 'udisks2', 'uid': '0'}
-    >>> mnt_info[3].mount_options.dmode
-    >>> 'dmode' in mnt_info[3].mount_options
+    >>> mnt_info[3].mount_options.ro
     True
     >>> mnt_info.mounts['/run/media/root/VMware Tools'].filesystem
     'dev/sr0'
 """
 
-from ..parsers import optlist_to_dict, keyword_search
-from .. import Parser, parser, get_active_lines, AttributeDict
-
 import re
 from insights.specs import Specs
+from ..parsers import optlist_to_dict, keyword_search
+from .. import Parser, parser, get_active_lines, LegacyItemAccess
+
+
+class MountOpts(object):
+    """
+    An object representing the mount options found in mount or fstab entry.
+    Each option in the comma-separated list is a key, and 'key=value'
+    pairs such as 'gid=5' are split so that e.g. the key is 'gid' and the value
+    is '5'.  Otherwise, the key is the option name and its value is 'True'.
+
+    In addition, the following attributes will always be available and are
+    False if not defined in the options list.
+
+    Attributes:
+        rw (bool): Read write
+        ro (bool): Read only
+        defaults: (bool): Use default options: rw, suid, dev, exec, auto, nouser, async, and relatime
+        relatime (bool): Inode access times relative to modify or change time
+        seclabel (bool): "seclabel" is enabled or not
+        attr2 (bool): "opportunistic" improvement is enabled or not
+        inode64 (bool): "inode64" is enabled or not
+        noquota (bool): Disk quotas are enforced or not
+    """
+    attrs = {
+        'rw': False,
+        'ro': False,
+        'defaults': False,
+        'relatime': False,
+        'seclabel': False,
+        'attr2': False,
+        'inode64': False,
+        'noquota': False
+    }
+
+    def __init__(self, data={}):
+        # Use '_data' but not 'data' since the 'data' could be an mount option
+        self._data = data
+        for k, v in MountOpts.attrs.items():
+            if k not in data:
+                setattr(self, k, v)
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __contains__(self, item):
+        return item in self._data
+
+    def get(self, item, default=None):
+        """Returns value of key ``item`` in self._data or ``default``
+        if key is not present.
+
+        Parameters:
+            item: Key to get from ``self.data``.
+            default: Default value to return if key is not present.
+
+        Returns:
+            (str): String value of the stored item, or the default if not found.
+        """
+        return self._data.get(item, default)
+
+    def items(self):
+        """
+        To keep backward compatibility and let it can be iterated as a
+        dictionary.
+        """
+        for k, v in self._data.items():
+            yield k, v
+
+
+class MountEntry(LegacyItemAccess):
+    """
+    An object representing an entry in the output of ``mount`` command.  Each
+    entry contains below fixed attributes:
+
+    Attributes:
+        mount_clause (str): Full string from command output
+        filesystem (str): Name of filesystem
+        mount_point (str): Name of mount point for filesystem
+        mount_type (str): Name of filesystem type
+        mount_options (MountOpts): Mount options as a :class:`insights.parsers.mount.MountOpts` object
+    """
+    attrs = {
+            'mount_clause': '',
+            'filesystem': '',
+            'mount_point': '',
+            'mount_type': '',
+            'mount_options': MountOpts(),
+    }
+
+    def __init__(self, data={}):
+        self.data = data
+        for k, v in MountEntry.attrs.items():
+            if k not in data:
+                setattr(self, k, v)
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def items(self):
+        """
+        To keep backward compatibility and let it can be iterated as a
+        dictionary.
+        """
+        for k, v in self.data.items():
+            yield k, v
 
 
 @parser(Specs.mount)
@@ -71,7 +183,7 @@ class Mount(Parser):
     """Class of information for all output from ``mount`` command.
 
     Attributes:
-        rows (list of AttributeDict): List of ``AttributeDict`` objects for
+        rows (list of MountEntry): List of :class:`MountEntry` objects for
             each row of the command output.
 
     Raises:
@@ -111,20 +223,20 @@ class Mount(Parser):
                 mount['mount_point'] = match.group('mount_point')
                 mount['mount_type'] = match.group('mount_type')
                 mount_options = match.group('mount_options')
-                mount['mount_options'] = AttributeDict(optlist_to_dict(mount_options))
+                mount['mount_options'] = MountOpts(optlist_to_dict(mount_options))
                 if match.group('mount_label'):
                     mount['mount_label'] = match.group('mount_label')
             else:
                 mount['parse_error'] = 'Unable to parse line'
 
-            entry = AttributeDict(mount)
+            entry = MountEntry(mount)
             self.rows.append(entry)
             if match:
                 self.mounts[mount['mount_point']] = entry
 
     def get_dir(self, path):
         """
-        AttributeDict: returns the mount point that contains the given path.
+        MountEntry: returns the mount point that contains the given path.
 
         This finds the most specific mount path that contains the given path,
         by successively removing the directory or file name on the end of
