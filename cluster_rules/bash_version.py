1a39c8dc85a4d06ce34fb141ab9a4f6df10a343d
Christopher Sams <csams@redhat.com>
2018-05-22 18:17:00 -0400

    Add initial cluster support.

---
+++ cluster_rules/bash_version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from insights import run
+from insights.parsers.installed_rpms import InstalledRpms
+from insights.combiners.hostname import hostname
+from insights.core.plugins import cluster_fact, cluster_rule
+
+
+@cluster_fact(InstalledRpms)
+def bash_version(rpms):
+    rpm = rpms.get_max("bash")
+    return {"name": rpm.name, "version": rpm.nvr}
+
+
+# bet there'll be a bunch of these
+@cluster_fact(hostname)
+def get_hostname(hn):
+    return {"hostname": hn.fqdn}
+
+
+@cluster_rule(bash_version, get_hostname)
+def bash_rule(bash, hn):
+    print(bash)
+    print(hn)
+
+
+if __name__ == "__main__":
+    run(bash_version, print_summary=True)
