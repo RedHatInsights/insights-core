dca56a26b4457eed8c201425f2327f5c8ede1338
Christopher Sams <csams@redhat.com>
2018-06-17 10:42:49 -0500

    Change cluster_rule to rule, add some content to cluster rules, and fix common prefix handling.

--- cluster_rules/bash_version.py
+++ cluster_rules/bash_version.py
@@ -2,7 +2,7 @@
 from insights import run
 from insights.parsers.installed_rpms import InstalledRpms
 from insights.combiners.hostname import hostname
-from insights.core.plugins import fact, cluster_rule
+from insights.core.plugins import fact, rule
 
 
 @fact(InstalledRpms)
@@ -11,16 +11,14 @@ def bash_version(rpms):
     return {"name": rpm.name, "version": rpm.nvr}
 
 
-# bet there'll be a bunch of these
 @fact(hostname)
 def get_hostname(hn):
     return {"hostname": hn.fqdn}
 
 
-@cluster_rule(bash_version, get_hostname)
+@rule(bash_version, get_hostname, cluster=True)
 def bash_rule(bash, hn):
-    print(bash)
-    print(hn)
+    pass
 
 
 if __name__ == "__main__":
