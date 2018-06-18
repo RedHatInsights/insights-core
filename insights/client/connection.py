be05891da3abe899c5f1d46e2e31eea5bc180ae9
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2018-06-18 17:00:58 -0400

    Config overhaul (#1189)
    Up to this point there has been a global configuration module that has been
    accessed directly throughout the client codebase.  This makes it difficult to
    independently test the configuration as well as other components that use the
    configuration.
    
    This change centralizes all configuration logic in the InsightsConfig class;
    any function or module that requires config will have to either create an
    InsightsConfig instance or have one passed in from elsewhere.
    
    Other changes:
    
    - Change from getitem syntax to getattr syntax (looks more like an object)
    - Rename old InsightsConfig class (manages uploader.json) to InsightsUploaderConf
    - Refactors and improvements to option validation and implications
    - Combine default config options and argparse dict in config.py

--- insights/client/connection.py
+++ insights/client/connection.py
@@ -23,7 +23,6 @@ from .utilities import (determine_hostname,
                         write_unregistered_file)
 from .cert_auth import rhsmCertificate
 from .constants import InsightsConstants as constants
-from .config import CONFIG as config
 from .schedule import get_scheduler
 
 warnings.simplefilter('ignore')
@@ -56,12 +55,13 @@ class InsightsConnection(object):
     Helper class to manage details about the connection
     """
 
-    def __init__(self):
+    def __init__(self, config):
         self.user_agent = constants.user_agent
-        self.username = config["username"]
-        self.password = config["password"]
+        self.config = config
+        self.username = self.config.username
+        self.password = self.config.password
 
-        self.cert_verify = config["cert_verify"]
+        self.cert_verify = self.config.cert_verify
         if type(self.cert_verify) in six.string_types:
             if self.cert_verify.lower() == 'false':
                 self.cert_verify = False
@@ -69,28 +69,28 @@ class InsightsConnection(object):
                 self.cert_verify = True
 
         protocol = "https://"
-        insecure_connection = config["insecure_connection"]
+        insecure_connection = self.config.insecure_connection
         if insecure_connection:
             # This really should not be used.
             protocol = "http://"
             self.cert_verify = False
 
-        self.auto_config = config['auto_config']
-        self.base_url = protocol + config["base_url"]
-        self.upload_url = config["upload_url"]
+        self.auto_config = self.config.auto_config
+        self.base_url = protocol + self.config.base_url
+        self.upload_url = self.config.upload_url
         if self.upload_url is None:
-            if config["analyze_container"]:
+            if self.config.analyze_container:
                 self.upload_url = self.base_url + "/uploads/image"
             else:
                 self.upload_url = self.base_url + "/uploads"
-        self.api_url = config["api_url"]
+        self.api_url = self.config.api_url
         if self.api_url is None:
             self.api_url = self.base_url
-        self.branch_info_url = config["branch_info_url"]
+        self.branch_info_url = self.config.branch_info_url
         if self.branch_info_url is None:
             self.branch_info_url = self.base_url + "/v1/branch_info"
-        self.authmethod = config['authmethod']
-        self.systemid = config['systemid']
+        self.authmethod = self.config.authmethod
+        self.systemid = self.config.systemid or None
         self.get_proxies()
         self.session = self._init_session()
         # need this global -- [barfing intensifies]
@@ -150,7 +150,7 @@ class InsightsConnection(object):
         logger.debug("NO PROXY: %s", no_proxy)
 
         # CONF PROXY TAKES PRECEDENCE OVER ENV PROXY
-        conf_proxy = config['proxy']
+        conf_proxy = self.config.proxy
         if ((conf_proxy is not None and
              conf_proxy.lower() != 'None'.lower() and
              conf_proxy != "")):
@@ -361,10 +361,10 @@ class InsightsConnection(object):
                         "SUCCESS" if api_success else "FAILURE")
             if cert_success and upload_success and api_success:
                 logger.info("\nConnectivity tests completed successfully")
-                logger.info("See %s for more details.", config['logging_file'])
+                logger.info("See %s for more details.", self.config.logging_file)
             else:
                 logger.info("\nConnectivity tests completed with some errors")
-                logger.info("See %s for more details.", config['logging_file'])
+                logger.info("See %s for more details.", self.config.logging_file)
                 rc = 1
         except requests.ConnectionError as exc:
             print(exc)
@@ -498,10 +498,10 @@ class InsightsConnection(object):
                 'remote_branch': remote_branch,
                 'remote_leaf': remote_leaf,
                 'hostname': client_hostname}
-        if config['display_name'] is not None:
-            data['display_name'] = config['display_name']
-        if config['display_name'] is not None:
-            data['display_name'] = config['display_name']
+        if self.config.display_name is not None:
+            data['display_name'] = self.config.display_name
+        if self.config.display_name is not None:
+            data['display_name'] = self.config.display_name
         data = json.dumps(data)
         post_system_url = self.api_url + '/v1/systems'
         logger.debug("POST System: %s", post_system_url)
@@ -559,7 +559,7 @@ class InsightsConnection(object):
         """
         Do grouping on register
         """
-        group_id = config['group']
+        group_id = self.config.group
         systems = {'machine_id': generate_machine_id()}
         self.group_systems(group_id, systems)
 
@@ -588,7 +588,7 @@ class InsightsConnection(object):
             # check the 'unregistered_at' key of the response
             unreg_status = json.loads(res.content).get('unregistered_at', 'undefined')
             # set the global account number
-            config['account_number'] = json.loads(res.content).get('account_number', 'undefined')
+            self.config.account_number = json.loads(res.content).get('account_number', 'undefined')
         except ValueError:
             # bad response, no json object
             return False
@@ -615,7 +615,7 @@ class InsightsConnection(object):
             logger.info(
                 "Successfully unregistered from the Red Hat Insights Service")
             write_unregistered_file()
-            get_scheduler().remove_scheduling()
+            get_scheduler(self.config).remove_scheduling()
             return True
         except requests.ConnectionError as e:
             logger.debug(e)
@@ -648,7 +648,7 @@ class InsightsConnection(object):
         write_to_disk(constants.registered_file)
 
         # Do grouping
-        if config['group'] is not None:
+        if self.config.group is not None:
             self.do_group()
 
         # Display registration success messasge to STDOUT and logs
@@ -664,10 +664,10 @@ class InsightsConnection(object):
                 logger.debug(system)
                 logger.debug(system.json())
 
-        if config['group'] is not None:
-            return (message, client_hostname, config['group'], config['display_name'])
-        elif config['display_name'] is not None:
-            return (message, client_hostname, "None", config['display_name'])
+        if self.config.group is not None:
+            return (message, client_hostname, self.config.group, self.config.display_name)
+        elif self.config.display_name is not None:
+            return (message, client_hostname, "None", self.config.display_name)
         else:
             return (message, client_hostname, "None", "")
 
@@ -690,7 +690,7 @@ class InsightsConnection(object):
         files = {
             'file': (file_name, open(data_collected, 'rb'), mime_type)}
 
-        if config["analyze_container"]:
+        if self.config.analyze_container:
             logger.debug('Uploading container, image, mountpoint or tarfile.')
             upload_url = self.upload_url
         else:
@@ -711,8 +711,8 @@ class InsightsConnection(object):
             logger.error("Upload archive failed with status code  %s", upload.status_code)
             return upload
         try:
-            config['account_number'] = the_json["upload"]["account_number"]
+            self.config.account_number = the_json["upload"]["account_number"]
         except:
-            config['account_number'] = None
+            self.config.account_number = None
         logger.debug("Upload duration: %s", upload.elapsed)
         return upload
