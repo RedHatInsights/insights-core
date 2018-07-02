49b1fbd037648c283c6de6fb2a33b328435937a0
Pete Savage <psav@users.noreply.github.com>
2018-07-02 16:19:32 +0100

    Added some more fs tests (#1236)
    * Added test to change touch times
    * Added parametrized test of modes
    
    (cherry picked from commit 18486e6d57cb71a60e99bdfb88e1413c0626913c)

--- insights/tests/test_fs.py
+++ insights/tests/test_fs.py
@@ -1,13 +1,14 @@
-import os
 import shutil
 import tempfile
-
 from contextlib import closing
+
+import os
+import pytest
+
 from insights.util import fs
 
 
 def test_remove_exists():
-    full_path = None
     with closing(tempfile.NamedTemporaryFile(delete=False)) as f:
         full_path = f.name
         f.write(b'test')
@@ -42,3 +43,27 @@ def test_exists_ensure_path_exists():
     assert os.path.exists(path)
     fs.ensure_path(path)
     assert os.path.exists(path)
+
+
+@pytest.fixture(params=['644', '775', '777'])
+def mode(request):
+    yield request.param
+
+
+@pytest.fixture
+def mode_dir(mode):
+    path = os.path.join('/tmp/insights-test-{mode}'.format(mode=mode))
+    yield path
+    fs.remove(path)
+
+
+def test_ensure_path_mode(mode, mode_dir):
+    fs.ensure_path(mode_dir, int(mode, 8))
+    assert oct(os.stat(mode_dir).st_mode)[-3:] == mode
+
+
+def test_touch_times():
+    path = os.path.join('/tmp/insights-test-touch-times')
+    fs.touch(path, (1259798405, 1259798400))
+    assert os.stat(path).st_atime == 1259798405
+    assert os.stat(path).st_mtime == 1259798400
