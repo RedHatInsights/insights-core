015f6e37439e593249870c60e4c11f4a07588044
TripleJQK <jquan@redhat.com>
2018-07-16 15:58:14 +0800

    parser for ls_var_spool_postfix_maildrop (#1260)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -339,6 +339,7 @@ class DefaultSpecs(Specs):
     ls_var_spool_clientmq = simple_command("/bin/ls -ln /var/spool/clientmqueue")
     ls_var_tmp = simple_command("/bin/ls -ln /var/tmp")
     ls_var_run = simple_command("/bin/ls -lnL /var/run")
+    ls_var_spool_postfix_maildrop = simple_command("/bin/ls -ln /var/spool/postfix/maildrop")
     ls_osroot = simple_command("/bin/ls -lan /")
     ls_var_www = simple_command("/bin/ls -la /dev/null /var/www")  # https://github.com/RedHatInsights/insights-core/issues/827
     lvdisplay = simple_command("/sbin/lvdisplay")
