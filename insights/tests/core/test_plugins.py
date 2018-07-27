171ee6257894af6ac9e62bb2aaeabe100e6d631f
Chris Sams <csams@users.noreply.github.com>
2018-07-27 14:42:54 -0500

    Generalize make_response to be class based and extendable. (#1295)

--- insights/tests/core/test_plugins.py
+++ insights/tests/core/test_plugins.py
@@ -3,89 +3,62 @@ import pytest
 from insights.core import plugins
 
 
-def test_make_metadata():
+def test_validate_good_response():
+    assert plugins.make_response("a_test", foo="bar") == {
+        "type": "rule",
+        "error_key": "a_test",
+        "foo": "bar"
+    }
+    assert plugins.make_fail("a_test", foo="bar") == {
+        "type": "rule",
+        "error_key": "a_test",
+        "foo": "bar"
+    }
+    assert plugins.make_pass("a_test", foo="bar") == {
+        "type": "pass",
+        "pass_key": "a_test",
+        "foo": "bar"
+    }
+    assert plugins.make_fingerprint("a_test", foo="bar") == {
+        "type": "fingerprint",
+        "fingerprint_key": "a_test",
+        "foo": "bar"
+    }
     assert plugins.make_metadata(foo="bar") == {
         "type": "metadata",
         "foo": "bar"
     }
+    assert plugins.make_metadata_key("foo", "bar") == {
+        "type": "metadata_key",
+        "key": "foo",
+        "value": "bar"
+    }
 
 
-def test_make_metadata_should_not_allow_type():
-    with pytest.raises(Exception):
-        plugins.make_metadata(type="not_allowed", foo="bar")
-
-
-def test_validate_response_good():
-    assert plugins.validate_response({
-        "type": "rule",
-        "error_key": "a_test",
-        "foo": "bar"}) is None
-
-
-def test_validate_response_invalid_types():
+def test_disallow_invalid_keys():
     for bad in [[], None, set(), "", 1, lambda x: x]:
         with pytest.raises(plugins.ValidationException):
-            plugins.validate_response(bad)
-
-
-def test_validate_response_missing_type():
-    with pytest.raises(plugins.ValidationException):
-        plugins.validate_response({"error_key": "an_error"})
-
-
-def test_validate_response_invalid_type_key():
-    with pytest.raises(plugins.ValidationException):
-        plugins.validate_response({"type": "dance off"})
-
-
-def test_validate_response_missing_error_key():
-    with pytest.raises(plugins.ValidationException):
-        plugins.validate_response({"type": "rule", "foo": "bar"})
-
-
-def test_validate_response_invalid_error_key_type():
-    for bad in [{}, None, 1, lambda x: x, set()]:
+            plugins.make_response(bad)
         with pytest.raises(plugins.ValidationException):
-            plugins.validate_response({
-                "type": "rule",
-                "error_key": bad
-            })
+            plugins.make_metadata_key(bad, "foo")
 
 
-def test_make_response_too_big():
+def test_disallow_type_key():
+    with pytest.raises(plugins.ValidationException):
+        plugins.make_response("foo", type="dance off")
+
+
+def test_missing_error_key():
+    with pytest.raises(plugins.ValidationException):
+        plugins.make_response(None, foo="bar")
+    with pytest.raises(plugins.ValidationException):
+        plugins.make_metadata_key(None, "foo")
+
+
+def test_response_too_big():
     content = "foo" * 50000
     assert plugins.make_response("TESTING", big=content) == {
         "type": "rule",
         "error_key": "TESTING",
         "max_detail_length_error": len(json.dumps({"error_key": "TESTING", "type": "rule", "big": content}))
     }
-
-
-def test_validate_fingerprint_good():
-    assert plugins.validate_response({
-        "type": "fingerprint",
-        "fingerprint_key": "FINGERPRINT",
-        "foo": "bar"}) is None
-
-
-def test_validate_response_missing_fingerprint_key():
-    with pytest.raises(plugins.ValidationException):
-        plugins.validate_response({"type": "fingerprint", "foo": "bar"})
-
-
-def test_make_fingerprint_too_big():
-    content = "foo" * 50000
-    assert plugins.make_fingerprint("TESTING", big=content) == {
-        "type": "fingerprint",
-        "fingerprint_key": "TESTING",
-        "max_detail_length_error": len(json.dumps({"fingerprint_key": "TESTING", "type": "fingerprint", "big": content}))
-    }
-
-
-def test_validate_fingerprint_invalid_fingerprint_key_type():
-    for bad in [{}, None, 1, lambda x: x, set()]:
-        with pytest.raises(plugins.ValidationException):
-            plugins.validate_response({
-                "type": "fingerprint",
-                "fingerprint_key": bad
-            })
