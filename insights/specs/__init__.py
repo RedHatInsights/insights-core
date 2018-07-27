3af2d431a9400450d0810bfb965975cdc2d3e9d0
wushiqinlou <jiazhang@redhat.com>
2018-07-27 11:12:17 +0800

    Add parser init_process_cgroup (#1290)
    * Add parser init_process_cgroup
    * Fix is_container function
    * Add docstring
    * Enhance docstring
    * Fix issues
    * Fix format
    * Fix docstring
    * Fix docstring2

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -155,6 +155,7 @@ class Specs(SpecSet):
     imagemagick_policy = RegistryPoint(multi_output=True)
     init_ora = RegistryPoint()
     initscript = RegistryPoint(multi_output=True)
+    init_process_cgroup = RegistryPoint()
     installed_rpms = RegistryPoint()
     interrupts = RegistryPoint()
     ip6tables_permanent = RegistryPoint()
