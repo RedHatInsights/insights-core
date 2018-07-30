a257513fa29e7008dedcaac2cc65f26c42317fdb
Štěpán Tomsa <driezasson@me.com>
2018-07-30 15:36:00 +0200

    Do not retry if server returns 412 on upload (#1251)
    Status code 412 means that the machine is unregistered. Thus there is
    almost no point in retrying as this is not a temporary condition. The
    --retry parameter is ignored regardless of whether the .unregistered
    file is actually written.
    
    (cherry picked from commit fb0ad64b92ca3ae09cdeb83afb1bacd3b2571301)

--- insights/tests/client/test_client.py
+++ insights/tests/client/test_client.py
@@ -3,7 +3,11 @@ import sys
 from insights.client import InsightsClient
 from insights.client.config import InsightsConfig
 from insights import package_info
+from mock.mock import patch
+from mock.mock import Mock
 
+
+# @TODO DRY the args hack.
 
 def test_version():
 
@@ -16,5 +20,67 @@ def test_version():
         client = InsightsClient(config)
         result = client.version()
         assert result == "%s-%s" % (package_info["VERSION"], package_info["RELEASE"])
+    finally:
+        sys.argv = tmp
+
+
+@patch('insights.client.client.constants.sleep_time', 0)
+@patch('insights.client.client.InsightsConnection.upload_archive',
+       return_value=Mock(status_code=500))
+def test_upload_500_retry(upload_archive):
+
+    # Hack to prevent client from parsing args to py.test
+    tmp = sys.argv
+    sys.argv = []
+
+    try:
+        retries = 3
+
+        config = InsightsConfig(logging_file='/tmp/insights.log', retries=retries)
+        client = InsightsClient(config)
+        client.upload('/tmp/insights.tar.gz')
+
+        upload_archive.assert_called()
+        assert upload_archive.call_count == retries
+    finally:
+        sys.argv = tmp
+
+
+@patch('insights.client.client.InsightsConnection.handle_fail_rcs')
+@patch('insights.client.client.InsightsConnection.upload_archive',
+       return_value=Mock(status_code=412))
+def test_upload_412_no_retry(upload_archive, handle_fail_rcs):
+
+    # Hack to prevent client from parsing args to py.test
+    tmp = sys.argv
+    sys.argv = []
+
+    try:
+        config = InsightsConfig(logging_file='/tmp/insights.log', retries=3)
+        client = InsightsClient(config)
+        client.upload('/tmp/insights.tar.gz')
+
+        upload_archive.assert_called_once()
+    finally:
+        sys.argv = tmp
+
+
+@patch('insights.client.connection.write_unregistered_file')
+@patch('insights.client.client.InsightsConnection.upload_archive',
+       return_value=Mock(**{"status_code": 412,
+                            "json.return_value": {"unregistered_at": "now", "message": "msg"}}))
+def test_upload_412_write_unregistered_file(upload_archive, write_unregistered_file):
+
+    # Hack to prevent client from parsing args to py.test
+    tmp = sys.argv
+    sys.argv = []
+
+    try:
+        config = InsightsConfig(logging_file='/tmp/insights.log', retries=3)
+        client = InsightsClient(config)
+        client.upload('/tmp/insights.tar.gz')
+
+        unregistered_at = upload_archive.return_value.json()["unregistered_at"]
+        write_unregistered_file.assert_called_once_with(unregistered_at)
     finally:
         sys.argv = tmp
