1a0466edafbfa682e7b32d2ed86c62313769a4f0
Štěpán Tomsa <stomsa@redhat.com>
2018-07-30 12:19:32 +0200

    Test separate get_conf_* methods
    Create separate tests for InsightsUploadConf.get_conf_*
    methods. Modify existing collect function tests to trust those methods
    and not to tests their internal working.

--- insights/tests/client/test_collect.py
+++ insights/tests/client/test_collect.py
@@ -2,18 +2,12 @@
 
 from insights.client.client import collect
 from insights.client.config import InsightsConfig
-from json import dump as json_dump, dumps as json_dumps
-from mock.mock import call, Mock, patch, PropertyMock
-from pytest import raises
+from json import dump as json_dump
+from mock.mock import Mock, patch, PropertyMock
 from tempfile import TemporaryFile
 
 
-branch_info = Mock()
-stdin_payload_uploader_json = json_dumps({"key": "value"})
-stdin_payload = {"uploader.json": stdin_payload_uploader_json, "sig": "signature"}
-remove_file = "/tmp/remove.conf"
-collection_rules_file = "/tmp/collection_rules"
-collection_fallback_file = "/tmp/collection_rules"
+stdin_payload = {"uploader.json": "some JSON", "sig": "some signature"}
 
 
 def collect_args(*insights_config_args, **insights_config_custom_kwargs):
@@ -25,279 +19,150 @@ def collect_args(*insights_config_args, **insights_config_custom_kwargs):
     return InsightsConfig(*insights_config_args, **insights_config_all_kwargs), Mock()
 
 
-def patch_get_branch_info():
+def patch_get_branch_info(old_function):
     """
     Sets a static response to get_branch_info method.
     """
-    def decorator(old_function):
-        patcher = patch("insights.client.client.get_branch_info", return_value=branch_info)
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.get_branch_info")
+    return patcher(old_function)
 
 
-def patch_stdin():
+def patch_stdin(old_function):
     """
     Sets a static JSON data to stdin.
     """
-    def decorator(old_function):
-        stdin = TemporaryFile("w+t")
-        json_dump(stdin_payload, stdin)
-        stdin.seek(0)
+    stdin = TemporaryFile("w+t")
+    json_dump(stdin_payload, stdin)
+    stdin.seek(0)
 
-        patcher = patch("insights.client.client.sys.stdin", new_callable=PropertyMock(return_value=stdin))
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.sys.stdin", new_callable=PropertyMock(return_value=stdin))
+    return patcher(old_function)
 
 
-def patch_isfile(isfile):
+def patch_get_conf_stdin(old_function):
     """
-    Makes isfile return the passed result.
+    Mocks InsightsUploadConf.get_conf_stdin.
     """
-    def decorator(old_function):
-        patcher = patch("insights.client.client.os.path.isfile", return_value=isfile)
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.InsightsUploadConf.get_conf_stdin")
+    return patcher(old_function)
 
 
-def patch_collection_remove_file():
+def patch_get_conf_file(old_function):
     """
-    Makes collection_remove_file contain a fixed path.
+    Mocks InsightsUploadConf.get_conf_file so it returns a fixed configuration.
     """
-    def decorator(old_function):
-        patcher = patch("insights.client.client.constants.collection_remove_file", remove_file)
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.InsightsUploadConf.get_conf_file")
+    return patcher(old_function)
 
 
-def patch_raw_config_parser(items=[]):
+def patch_get_rm_conf(old_function):
     """
-    Mocks RawConfigParser so it returns the passed items.
+    Mocks InsightsUploadConf.get_rm_conf so it returns a fixed configuration.
     """
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.ConfigParser.RawConfigParser",
-                          **{"return_value.items.return_value": items})
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.InsightsUploadConf.get_rm_conf")
+    return patcher(old_function)
 
 
-def patch_validate_gpg_sig(valid):
-    """
-    Makes validate_gpg_sig return the passed result.
-    """
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.InsightsUploadConf.validate_gpg_sig", return_value=valid)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_collection_rules_file():
-    """
-    Makes collection_rules_file contain a fixed path.
-    """
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.constants.collection_rules_file", "/tmp/collection_rules")
-        return patcher(old_function)
-    return decorator
-
-
-def patch_collection_fallback_file():
-    """
-    Makes collection_fallback_file contain a fixed path.
-    """
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.constants.collection_fallback_file", collection_fallback_file)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_try_disk(return_values):
-    """
-    Makes try_disk sequentially return the passed contents.
-    """
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.InsightsUploadConf.try_disk", side_effect=return_values)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_data_collector():
+def patch_data_collector(old_function):
     """
     Replaces DataCollector with a dummy mock.
     """
-    def decorator(old_function):
-        patcher = patch("insights.client.client.DataCollector")
-        return patcher(old_function)
-    return decorator
+    patcher = patch("insights.client.client.DataCollector")
+    return patcher(old_function)
 
 
-def patch_named_temporary_file():
+@patch_data_collector
+@patch_get_conf_file
+@patch_get_conf_stdin
+@patch_get_branch_info
+def test_get_conf_file(get_branch_info, get_conf_stdin, get_conf_file, data_collector):
     """
-    Mocks NamedTemporaryFile so it sequentially builds rules objects. Adds these object to test function arguments.
+    If there is no config passed via stdin, it is loaded from a file instead.
     """
-    def decorator(old_function):
-        rules_fp = Mock()
-        rules_fp.name = "rules name"
-        sig_fp = Mock()
-        sig_fp.name = "sig name"
-
-        patcher = patch("insights.client.collection_rules.NamedTemporaryFile",
-                        side_effect=[rules_fp, sig_fp],
-                        rules_fp=rules_fp,
-                        sig_fp=sig_fp)
-        return patcher(old_function)
-    return decorator
-
-
-@patch_data_collector()
-@patch_raw_config_parser()
-@patch_collection_remove_file()
-@patch_isfile(False)
-@patch_stdin()
-@patch_get_branch_info()
-def test_remove_file_not_exists(get_branch_info, stdin, isfile, raw_config_parser, data_collector):
-    config, pconn = collect_args(from_stdin=True, gpg=False)
-    collect(config, pconn)
-
-    isfile.assert_called_once_with(remove_file)
-    raw_config_parser.assert_not_called()
-
-
-@patch_data_collector()
-@patch_raw_config_parser([])
-@patch_collection_remove_file()
-@patch_isfile(True)
-@patch_stdin()
-@patch_get_branch_info()
-def test_remove_file_exists(get_branch_info, stdin, isfile, raw_config_parser, data_collector):
-    config, pconn = collect_args(from_stdin=True, gpg=False)
-    collect(config, pconn)
-
-    isfile.assert_called_once_with(remove_file)
-    raw_config_parser.assert_called_once_with()
-    raw_config_parser.return_value.read.assert_called_once_with(remove_file)
-    raw_config_parser.return_value.items.assert_called_once_with("remove")
-
-
-@patch_data_collector()
-@patch_stdin()
-@patch_named_temporary_file()
-@patch_get_branch_info()
-def test_stdin_write_rules(get_branch_info, named_temporary_file, stdin, data_collector):
-    config, pconn = collect_args(from_stdin=True, gpg=False)
-    collect(config, pconn)
-
-    calls = [call(delete=False),
-             call.rules_fp.write(stdin_payload_uploader_json.encode("utf-8")),
-             call.rules_fp.flush(),
-             call(delete=False),
-             call.sig_fp.write("signature".encode("utf-8")),
-             call.sig_fp.flush()]
-    named_temporary_file.assert_has_calls(calls)
-
-
-@patch_data_collector()
-@patch_validate_gpg_sig(True)
-@patch_stdin()
-@patch_named_temporary_file()
-@patch_get_branch_info()
-def test_validate_gpg_sig(get_branch_info, named_temporary_file, stdin, validate_gpg_sig, data_collector):
-    config, pconn = collect_args(from_stdin=True, gpg=True)
-    collect(config, pconn)
-
-    validate_gpg_sig.assert_called_with(named_temporary_file.rules_fp.name, named_temporary_file.sig_fp.name)
-
-
-@patch_try_disk([{}])
-@patch_validate_gpg_sig(False)
-@patch_stdin()
-@patch_named_temporary_file()
-@patch_get_branch_info()
-def test_invalid_gpg(get_branch_info, named_temporary_file, stdin, validate_gpg_sig, try_disk):
-    config, pconn = collect_args(from_stdin=True, gpg=True)
-
-    with raises(Exception):
-        collect(config, pconn)
-    try_disk.assert_not_called()  # Ensure it’s not ValueError
-
-
-@patch_data_collector()
-@patch_try_disk([{"version": "1.2.3"}])
-@patch_collection_rules_file()
-@patch_stdin()
-@patch_get_branch_info()
-def test_collection_rules_file_conf_try_disk(get_branch_info, stdin, try_disk, data_collector):
-    config, pconn = collect_args(gpg="perhaps")
-    collect(config, pconn)
-    try_disk.assert_called_once_with(collection_rules_file, "perhaps")
-
-
-@patch_try_disk([{"version": None}])
-@patch_stdin()
-@patch_get_branch_info()
-def test_collection_rules_file_conf_no_version_error(get_branch_info, stdin, try_disk):
-    config, pconn = collect_args()
-    with raises(ValueError):
-        collect(config, pconn)
-
-
-@patch_data_collector()
-@patch_try_disk([None, {"version": "1.2.3"}])
-@patch_collection_fallback_file()
-@patch_collection_rules_file()
-@patch_stdin()
-@patch_get_branch_info()
-def test_fallback_file_conf_try_disk(get_branch_info, stdin, try_disk, data_collector):
-    config, pconn = collect_args(gpg="perhaps")
-    collect(config, pconn)
-    calls = [call(collection_rules_file, "perhaps"), call(collection_fallback_file, "perhaps")]
-    try_disk.assert_has_calls(calls)
-    assert try_disk.call_count == len(calls)
-
-
-@patch_try_disk([None, {"version": None}])
-@patch_stdin()
-@patch_get_branch_info()
-def test_fallback_file_conf_no_version_error(get_branch_info, stdin, try_disk):
-    config, pconn = collect_args()
-    with raises(ValueError):
-        collect(config, pconn)
-
-
-@patch_try_disk([None, None])
-@patch_stdin()
-@patch_get_branch_info()
-def test_no_file_conf_error(get_branch_info, stdin, try_disk):
-    config, pconn = collect_args()
-    with raises(ValueError):
-        collect(config, pconn)
-
-
-@patch_data_collector()
-@patch_try_disk([{"version": "1.2.3"}])
-@patch_collection_rules_file()
-@patch_isfile(False)
-@patch_stdin()
-@patch_get_branch_info()
-def test_return_without_rm_conf(get_branch_info, stdin, isfile, try_disk, data_collector):
     config, pconn = collect_args()
     collect(config, pconn)
 
-    collection_rules = {"version": "1.2.3", "file": collection_rules_file}
-    rm_conf = None
+    get_conf_stdin.assert_not_called()
+    get_conf_file.assert_called_once_with()
+
+
+@patch_data_collector
+@patch_get_conf_file
+@patch_get_conf_stdin
+@patch_stdin
+@patch_get_branch_info
+def test_get_conf_stdin(get_branch_info, stdin, get_conf_stdin, get_conf_file, data_collector):
+    """
+    If there is config passed via stdin, use it and do not look for it in files.
+    """
+    config, pconn = collect_args(from_stdin=True)
+    collect(config, pconn)
+
+    get_conf_stdin.assert_called_once_with(stdin_payload)
+    get_conf_file.assert_not_called()
+
+
+@patch_data_collector
+@patch_get_rm_conf
+@patch_get_conf_stdin
+@patch_stdin
+@patch_get_branch_info
+def test_get_rm_conf_stdin(get_branch_info, stdin, get_conf_stdin, get_rm_conf, data_collector):
+    """
+    Load configuration of files removed from collection when collection rules are loaded from stdin.
+    """
+    config, pconn = collect_args(from_stdin=True)
+    collect(config, pconn)
+
+    get_rm_conf.assert_called_once_with()
+
+
+@patch_data_collector
+@patch_get_rm_conf
+@patch_get_conf_file
+@patch_get_branch_info
+def test_get_rm_conf_file(get_branch_info, get_conf_file, get_rm_conf, data_collector):
+    """
+    Load configuration of files removed from collection when collection rules are loaded from a file.
+    """
+    config, pconn = collect_args(from_stdin=False)
+    collect(config, pconn)
+
+    get_rm_conf.assert_called_once_with()
+
+
+@patch_data_collector
+@patch_get_rm_conf
+@patch_get_conf_stdin
+@patch_stdin
+@patch_get_branch_info
+def test_data_collector_stdin(get_branch_info, stdin, get_conf_stdin, get_rm_conf, data_collector):
+    """
+    Configuration from stdin is passed to the DataCollector along with removed files configuration.
+    """
+    config, pconn = collect_args(from_stdin=True)
+    collect(config, pconn)
+
+    collection_rules = get_conf_stdin.return_value
+    rm_conf = get_rm_conf.return_value
+    branch_info = get_branch_info.return_value
     data_collector.return_value.run_collection.assert_called_once_with(collection_rules, rm_conf, branch_info)
+    data_collector.return_value.done.assert_called_once_with(collection_rules, rm_conf)
 
 
-@patch_data_collector()
-@patch_try_disk([{"version": "1.2.3"}])
-@patch_collection_rules_file()
-@patch_raw_config_parser([("files", "/etc/remove.conf,/tmp/remove.conf")])
-@patch_isfile(True)
-@patch_stdin()
-@patch_get_branch_info()
-def test_return_with_rm_conf(get_branch_info, stdin, isfile, raw_config_parser, try_disk, data_collector):
-    config, pconn = collect_args()
+@patch_data_collector
+@patch_get_rm_conf
+@patch_get_conf_file
+@patch_get_branch_info
+def test_data_collector_file(get_branch_info, get_conf_file, get_rm_conf, data_collector):
+    """
+    Configuration from a file is passed to the DataCollector along with removed files configuration.
+    """
+    config, pconn = collect_args(from_stdin=False)
     collect(config, pconn)
 
-    collection_rules = {"version": "1.2.3", "file": collection_rules_file}
-    rm_conf = {"files": ["/etc/remove.conf", "/tmp/remove.conf"]}
+    collection_rules = get_conf_file.return_value
+    rm_conf = get_rm_conf.return_value
+    branch_info = get_branch_info.return_value
     data_collector.return_value.run_collection.assert_called_once_with(collection_rules, rm_conf, branch_info)
+    data_collector.return_value.done.assert_called_once_with(collection_rules, rm_conf)
--- insights/tests/client/test_update_rules.py
+++ insights/tests/client/test_update_rules.py
@@ -1,124 +1,53 @@
 # -*- coding: UTF-8 -*-
 
-from insights.client.config import InsightsConfig
 from insights.client.client import update_rules
-from mock.mock import call, Mock, patch
+from mock.mock import Mock, patch
 from pytest import raises
 
 
-collection_rules_file = "/tmp/collection_rules"
-collection_fallback_file = "/tmp/fallback"
-
-
-def update_rules_args(*insights_config_args, **insights_config_custom_kwargs):
+def patch_insights_upload_conf():
     """
-    Instantiates InsightsConfig with a default logging_file argument.
+    Mocks InsightsUploadConf.
     """
-    insights_config_all_kwargs = {"logging_file": "/tmp/insights.log"}
-    insights_config_all_kwargs.update(insights_config_custom_kwargs)
-    return InsightsConfig(*insights_config_args, **insights_config_all_kwargs), Mock()
-
-
-def patch_get_collection_rules(collection_rules):
     def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.InsightsUploadConf.get_collection_rules",
-                        return_value=collection_rules)
+        patcher = patch("insights.client.client.InsightsUploadConf")
         return patcher(old_function)
     return decorator
 
 
-def patch_get_connection():
-    def decorator(old_function):
-        patcher = patch("insights.client.client.get_connection", return_value=None)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_collection_rules_file():
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.constants.collection_rules_file", collection_rules_file)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_collection_fallback_file():
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.constants.collection_fallback_file", collection_fallback_file)
-        return patcher(old_function)
-    return decorator
-
-
-def patch_try_disk(disk_confs):
-    def decorator(old_function):
-        patcher = patch("insights.client.collection_rules.InsightsUploadConf.try_disk", side_effect=disk_confs)
-        return patcher(old_function)
-    return decorator
-
-
-@patch_get_collection_rules({"version": "1.2.3"})
-@patch_get_connection()
-def test_no_connection_error(get_connection, get_collection_rules):
-    config, pconn = update_rules_args()
+@patch_insights_upload_conf()
+def test_no_connection_error(insights_upload_conf):
+    """
+    Error is raised when connection couldn't be established.
+    """
+    config = Mock()
     with raises(ValueError):
         update_rules(config, None)
 
+    insights_upload_conf.return_value.get_conf_update.assert_not_called()
 
-@patch_get_collection_rules({"version": "1.2.3"})
-def test_get_collection_rules(get_collection_rules):
-    config, pconn = update_rules_args()
+
+@patch_insights_upload_conf()
+def test_configuration(insights_upload_conf):
+    """
+    Configuration is created with given options and retrieved connection.
+    """
+    config = Mock()
+    pconn = Mock()
     update_rules(config, pconn)
-    get_collection_rules.assert_called_once_with()
+
+    insights_upload_conf.assert_called_once_with(config, conn=pconn)
 
 
-@patch_get_collection_rules({"version": None})
-def test_dyn_conf_no_version_error(get_collection_rules):
-    config, pconn = update_rules_args()
-    with raises(ValueError):
-        update_rules(config, pconn)
+@patch_insights_upload_conf()
+def test_return_get_conf_update(insights_upload_conf):
+    """
+    Updated configuration is loaded and returned.
+    """
+    config = Mock()
+    pconn = Mock()
+    result = update_rules(config, pconn)
 
-
-@patch_try_disk([{"version": "1.2.3"}])
-@patch_collection_rules_file()
-@patch_get_collection_rules(None)
-def test_collection_rules_file_conf_try_disk(get_collection_rules, try_disk):
-    gpg = "perhaps"
-    config, pconn = update_rules_args(gpg=gpg)
-    update_rules(config, pconn)
-    try_disk.assert_called_once_with(collection_rules_file, gpg)
-
-
-@patch_try_disk([{"version": None}])
-@patch_get_collection_rules({"version": None})
-def test_collection_rules_file_conf_no_version_error(get_collection_rules, try_disk):
-    config, pconn = update_rules_args()
-    with raises(ValueError):
-        update_rules(config, pconn)
-
-
-@patch_try_disk([None, {"version": "1.2.3"}])
-@patch_collection_fallback_file()
-@patch_collection_rules_file()
-@patch_get_collection_rules(None)
-def test_fallback_file_conf_try_disk(get_collection_rules, try_disk):
-    gpg = "perhaps"
-    config, pconn = update_rules_args(gpg=gpg)
-    update_rules(config, pconn)
-    calls = [call(collection_rules_file, gpg), call(collection_fallback_file, gpg)]
-    try_disk.assert_has_calls(calls)
-    assert try_disk.call_count == len(calls)
-
-
-@patch_try_disk([None, {"version": None}])
-@patch_get_collection_rules(None)
-def test_fallback_file_conf_no_version_error(get_collection_rules, try_disk):
-    config, pconn = update_rules_args()
-    with raises(ValueError):
-        update_rules(config, pconn)
-
-
-@patch_try_disk([None, None])
-@patch_get_collection_rules(None)
-def test_no_file_conf_error(get_collection_rules, try_disk):
-    config, pconn = update_rules_args()
-    with raises(ValueError):
-        update_rules(config, pconn)
+    get_conf_update = insights_upload_conf.return_value.get_conf_update
+    get_conf_update.assert_called_once_with()
+    assert result == get_conf_update.return_value
