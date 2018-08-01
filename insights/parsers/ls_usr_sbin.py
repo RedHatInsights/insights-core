d0cf7b5002c0531fe56163b8c91cbc8c56e772c6
TripleJQK <jquan@redhat.com>
2018-08-01 09:35:14 +0800

    fix wrong content

--- insights/parsers/ls_usr_sbin.py
+++ insights/parsers/ls_usr_sbin.py
@@ -8,11 +8,11 @@ The ``ls -ln /usr/sbin`` command provides information for the listing of the
 Sample input is shown in the Examples. See ``FileListing`` class for
 additional information.
 
-For ls_usr_sbin, it may collect a lot of files or directories that may not be 
+For ls_usr_sbin, it may collect a lot of files or directories that may not be
 necessary, so a default filter `add_filter(Specs.ls_usr_sbin, "total")` has
 been added in this parser.
 
-If addtional file or directory need to be collected by this parser, please 
+If addtional file or directory need to be collected by this parser, please
 add related filter to corresponding code.
 
 Sample added filter:
