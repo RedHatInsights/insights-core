0463acd632749b4c04301a86c10a3c7f0fca773e
Xiangce Liu <xiangceliu@redhat.com>
2018-08-02 23:30:13 +0800

    New parser for SAP host_profile (#1297)
    * New Parser for SAP host_profile
    
    * Skip when there are incorrect lines
    
    * Add fixed filters for sap_host_profile
    
    (cherry picked from commit 5fef9a20ee8683bfa221698ccf62227c27c13caf)

---
+++ insights/parsers/tests/test_sap_host_profile.py
@@ -0,0 +1,48 @@
+from insights.parsers import sap_host_profile, SkipException
+from insights.parsers.sap_host_profile import SAPHostProfile
+from insights.tests import context_wrap
+import doctest
+import pytest
+
+HOST_PROFILE_DOC = """
+SAPSYSTEMNAME = SAP
+SAPSYSTEM = 99
+service/porttypes = SAPHostControl SAPOscol SAPCCMS
+DIR_LIBRARY =
+DIR_EXECUTABLE = /usr/sap/hostctrl/exe
+DIR_PROFILE = /usr/sap/hostctrl/exe
+DIR_GLOBAL = /usr/sap/hostctrl/exe
+DIR_INSTANCE = /usr/sap/hostctrl/exe
+DIR_HOME = /usr/sap/hostctrl/work
+""".strip()
+
+HOST_PROFILE_AB = """
+SAPSYSTEMNAME = SAP
+SAPSYSTEM = 99
+service/porttypes = SAPHostControl SAPOscol SAPCCMS
+DIR_LIBRARY = /usr/sap/hostctrl/exe
+DIR_EXECUTABLE = /usr/sap/hostctrl/exe
+DIR_PROFILE = /usr/sap/hostctrl/exe
+DIR_GLOBAL
+""".strip()
+
+
+def test_sap_host_profile():
+    hpf = SAPHostProfile(context_wrap(HOST_PROFILE_DOC))
+    assert "SAPSYSTEM" in hpf
+    assert hpf["DIR_GLOBAL"] == "/usr/sap/hostctrl/exe"
+    assert hpf["DIR_LIBRARY"] == ""
+
+
+def test_sap_host_profile_abnormal():
+    with pytest.raises(SkipException) as s:
+        SAPHostProfile(context_wrap(HOST_PROFILE_AB))
+    assert "Incorrect line: 'DIR_GLOBAL'" in str(s)
+
+
+def test_doc_examples():
+    env = {
+            'hpf': SAPHostProfile(context_wrap(HOST_PROFILE_DOC)),
+          }
+    failed, total = doctest.testmod(sap_host_profile, globs=env)
+    assert failed == 0
