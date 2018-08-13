e345243700baea820c0b731f3cfd60a3f6e44566
TripleJQK <jquan@redhat.com>
2018-08-14 02:13:44 +0800

    parser for /etc/sysconfig/foreman-tasks (#1316)
    * parser for /etc/sysconfig/foreman-tasks
    
    * fix wrong doc
    
    * fix wrong part in docstring test
    
    (cherry picked from commit 4610f4e1a357aa52cddc6443174db6d6ede6e4bf)

---
+++ insights/parsers/foreman_tasks_config.py
@@ -0,0 +1,57 @@
+"""
+ForemanTasksConfig - file ``/etc/sysconfig/foreman-tasks``
+==========================================================
+"""
+from insights.specs import Specs
+
+from . import split_kv_pairs
+from .. import LegacyItemAccess, Parser, get_active_lines, parser
+
+
+@parser(Specs.foreman_tasks_config)
+class ForemanTasksConfig(Parser, LegacyItemAccess):
+    """
+    Parse the foreman-tasks configuration file.
+
+    Produces a simple dictionary of keys and values from the configuration
+    file contents , stored in the ``data`` attribute.  The object also
+    functions as a dictionary itself thanks to the
+    :py:class:`insights.core.LegacyItemAccess` mixin class.
+
+    Sample configuration file::
+
+        FOREMAN_USER=foreman
+        BUNDLER_EXT_HOME=/usr/share/foreman
+        RAILS_ENV=production
+        FOREMAN_LOGGING=warn
+        FOREMAN_LOGGING_SQL=warn
+        FOREMAN_TASK_PARAMS="-p foreman"
+        FOREMAN_LOG_DIR=/var/log/foreman
+
+        RUBY_GC_MALLOC_LIMIT=4000100
+        RUBY_GC_MALLOC_LIMIT_MAX=16000100
+        RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR=1.1
+        RUBY_GC_OLDMALLOC_LIMIT=16000100
+        RUBY_GC_OLDMALLOC_LIMIT_MAX=16000100
+
+        #Set the number of executors you want to run
+        #EXECUTORS_COUNT=1
+
+        #Set memory limit for executor process, before it's restarted automatically
+        #EXECUTOR_MEMORY_LIMIT=2gb
+
+        #Set delay before first memory polling to let executor initialize (in sec)
+        #EXECUTOR_MEMORY_MONITOR_DELAY=7200 #default: 2 hours
+
+        #Set memory polling interval, process memory will be checked every N seconds.
+        #EXECUTOR_MEMORY_MONITOR_INTERVAL=60
+
+    Examples:
+        >>> foreman_tasks_config['RAILS_ENV']
+        'production'
+        >>> 'AUTO' in foreman_tasks_config
+        False
+    """
+
+    def parse_content(self, content):
+        self.data = split_kv_pairs(get_active_lines(content))
