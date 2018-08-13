19a3a5fe136c3da1f41d00b02f47a4f9eb0ade66
Sachin Patil <psachin@redhat.com>
2018-08-13 09:43:47 +0530

    Keeping file insights/parsers/engine_log.py
    Not removing ``insights/parsers/engine_log.py`` for backward compatibility.
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

---
+++ insights/parsers/engine_log.py
@@ -0,0 +1,7 @@
+"""
+EngineLog - file ``/var/log/ovirt-engine/engine.log``
+====================================================
+
+The actual class can be found in the file -- ``insights/parsers/ovirt_engine_log.py``
+"""
+from insights.parsers.ovirt_engine_log import EngineLog  # noqa
