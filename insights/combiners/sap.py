3d34332f2ea025a8b86ff8cc287313796e524efa
Xiangce Liu <xiangceliu@redhat.com>
2018-08-14 02:50:40 +0800

    Update saphostctrl parser and sap combiner (#1319)
    * Update the saphostctrl_instances parser and sap combiner
    
    - https://bugzilla.redhat.com/show_bug.cgi?id=1547563
    - Both the combiner and parser are newly developed
    - rename the python file and class name
    - remove the inheritance between Lssap and the SAPHostCtrlInstance
    
    * Update doc entry
    
    * Raise SkipException when no sap instances
    
    * Add docstring for attribute data
    
    (cherry picked from commit 6f22cca6e30312aa806db73ea6607e5f39346cef)

--- insights/combiners/sap.py
+++ insights/combiners/sap.py
@@ -20,58 +20,117 @@ https://wiki.scn.sap.com/wiki/x/rDK7Gg
 
 """
 
+from collections import namedtuple
+from insights import LegacyItemAccess
+from insights.parsers import SkipException
 from insights.core.plugins import combiner
 from insights.combiners.hostname import hostname
 from insights.parsers.lssap import Lssap
+from insights.parsers.saphostctrl import SAPHostCtrlInstances
 
 
-@combiner(hostname, Lssap)
-class Sap(object):
+SAPInstances = namedtuple("SAPInstances",
+        field_names=["name", "hostname", "sid", "type", "number", "version"])
+"""namedtuple: Type for storing the SAP instance."""
+
+
+@combiner(hostname, optional=[SAPHostCtrlInstances, Lssap])
+class Sap(LegacyItemAccess):
     """
     Combiner for analyzing the SAP instances running on the system.
+
+    Prefer SAPHostCtrlInstances to Lssap.
 
     Examples:
         >>> type(saps)
         <class 'insights.combiners.sap.Sap'>
+        >>> saps['D16'].number
+        '16'
+        >>> saps.sid('HDB16')
+        'HA2'
+        >>> saps.hostname('HDB16')
+        'lu0417'
+        >>> 'D22' in saps.local_instances
+        False
         >>> saps.is_hana
         True
         >>> saps.is_netweaver
         True
         >>> saps.is_ascs
         False
+
+    Attributes:
+        all_instances (list): List all the SAP instances listed by the command.
+        local_instances (list): List SAP instances which are running on the system.
     """
-    sap_type = {'D': 'netweaver',
-                'HDB': 'hana',
-                'ASCS': 'ascs'}
 
-    def __init__(self, hostname, sap):
+    def __init__(self, hostname, insts, lssap):
         hn = hostname.hostname
-        # Rule only matters if the SAP instance returned by lssap refers to this
-        # host
-        instances = [i['Instance'] for i in sap.data if hn == i['SAPLOCALHOST']]
-        sap_apps = []
-        for i in instances:
-            sap_apps.extend([v
-                for k, v in self.sap_type.items()
-                if i.startswith(k)])
-        self._sap_apps = list(set(sap_apps))
+        self.data = {}
+        self.local_instances = []
+        self.all_instances = []
+        self._types = set()
+        if insts:
+            for inst in insts.data:
+                k = inst['InstanceName']
+                self.all_instances.append(k)
+                if hn == inst['Hostname']:
+                    self.local_instances.append(k)
+                    self._types.add(inst['InstanceType'])
+                self.data[k] = SAPInstances(k,
+                                            inst['Hostname'],
+                                            inst['SID'],
+                                            inst['InstanceType'],
+                                            inst['SystemNumber'],
+                                            inst['SapVersionInfo'])
+        elif lssap:
+            for inst in lssap.data:
+                k = inst['Instance']
+                t = k.rstrip('1234567890')
+                self.all_instances.append(k)
+                if hn == inst['SAPLOCALHOST']:
+                    self.local_instances.append(k)
+                    self._types.add(t)
+                self.data[k] = SAPInstances(k,
+                                            inst['SAPLOCALHOST'],
+                                            inst['SID'],
+                                            t,
+                                            inst['Nr'],
+                                            inst['Version'])
+        else:
+            raise SkipException('No SAP instance.')
 
-    @property
-    def running_saps(self):
-        """list: List SAP instances running on the system."""
-        return self._sap_apps
+    def version(self, instance):
+        """str: Returns the version of the ``instance``."""
+        return self.data[instance].version if instance in self.data else None
+
+    def sid(self, instance):
+        """str: Returns the sid of the ``instance``."""
+        return self.data[instance].sid if instance in self.data else None
+
+    def type(self, instance):
+        """str: Returns the type code of the ``instance``."""
+        return self.data[instance].type if instance in self.data else None
+
+    def hostname(self, instance):
+        """str: Returns the hostname of the ``instance``."""
+        return self.data[instance].hostname if instance in self.data else None
+
+    def number(self, instance):
+        """str: Returns the systeme number of the ``instance``."""
+        return self.data[instance].number if instance in self.data else None
 
     @property
     def is_netweaver(self):
         """bool: SAP Netweaver is running on the system."""
-        return 'netweaver' in self._sap_apps
+        return 'D' in self._types
 
     @property
     def is_hana(self):
         """bool: SAP Hana is running on the system."""
-        return 'hana' in self._sap_apps
+        return 'HDB' in self._types
 
     @property
     def is_ascs(self):
         """bool: SAP System Central Services is running on the system."""
-        return 'ascs' in self._sap_apps
+        return 'ASCS' in self._types
