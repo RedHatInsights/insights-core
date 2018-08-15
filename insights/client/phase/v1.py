5bf1f54a1c3c0ba85ba2d42c8e0564401facae9e
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2018-08-15 11:57:18 -0400

    Be more careful about deleting parent directory of archive after upload (#1367)
    * move archive deletion + egg rotation to phase rather than upload method
    
    * be more careful about deleting directories

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -165,6 +165,23 @@ def collect_and_output(client, config):
             resp = client.upload(tar_file)
         else:
             logger.info('Archive saved at %s', tar_file)
-        if resp and config["to_json"]:
-            print(json.dumps(resp))
+        if resp:
+            if config["to_json"]:
+                print(json.dumps(resp))
+
+            # delete the archive
+            if config.keep_archive:
+                logger.info('Insights archive retained in ' + tar_file)
+            else:
+                client.delete_archive(tar_file, delete_parent_dir=True)
+
+            # if we are rotating the eggs and success on upload do rotation
+            try:
+                client.rotate_eggs()
+            except IOError:
+                message = ("Failed to rotate %s to %s" %
+                           (constants.insights_core_newest,
+                            constants.insights_core_last_stable))
+                logger.debug(message)
+                raise IOError(message)
     sys.exit()
