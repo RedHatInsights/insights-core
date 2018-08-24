bf4ecfaad6f2c24a439407199489033d0bdc2c42
Jesse Jaggars <jhjaggars@gmail.com>
2018-08-24 21:14:25 +0000

    adding amq broker parser

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -2,6 +2,7 @@ from insights.core.spec_factory import SpecSet, RegistryPoint
 
 
 class Specs(SpecSet):
+    amq_broker = RegistryPoint(multi_output=True)
     auditd_conf = RegistryPoint()
     audit_log = RegistryPoint(filterable=True)
     autofs_conf = RegistryPoint()
