b621820365c21bead8c547320ad55d164f4d14a8
Jeremy Crafts <jcrafts@redhat.com>
2018-08-27 15:00:59 -0400

    get hostname archive path from uploader.json

--- insights/client/data_collector.py
+++ insights/client/data_collector.py
@@ -13,6 +13,7 @@ import six
 from subprocess import Popen, PIPE, STDOUT
 from tempfile import NamedTemporaryFile
 
+from insights.util import mangle
 from ..contrib.soscleaner import SOSCleaner
 from .utilities import _expand_paths
 from .constants import InsightsConstants as constants
@@ -187,6 +188,11 @@ class DataCollector(object):
             return
 
         for c in conf['commands']:
+            # remember hostname archive path
+            if c['command'].startswith('/bin/hostname'):
+                self.hostname_path = os.path.join(
+                    'insights_commands', mangle.mangle_command(c['command']))
+
             if c['command'] in rm_conf.get('commands', []):
                 logger.warn("WARNING: Skipping command %s", c['command'])
             elif self.mountpoint == "/" or c.get("image"):
@@ -224,7 +230,8 @@ class DataCollector(object):
         """
         if self.config.obfuscate:
             cleaner = SOSCleaner(quiet=True)
-            clean_opts = CleanOptions(self.config, self.archive.tmp_dir, rm_conf)
+            clean_opts = CleanOptions(
+                self.config, self.archive.tmp_dir, rm_conf, self.hostname_path)
             fresh = cleaner.clean_report(clean_opts, self.archive.archive_dir)
             if clean_opts.keyword_file is not None:
                 os.remove(clean_opts.keyword_file.name)
@@ -236,7 +243,7 @@ class CleanOptions(object):
     """
     Options for soscleaner
     """
-    def __init__(self, config, tmp_dir, rm_conf):
+    def __init__(self, config, tmp_dir, rm_conf, hostname_path):
         self.report_dir = tmp_dir
         self.domains = []
         self.files = []
@@ -257,6 +264,6 @@ class CleanOptions(object):
                 pass
 
         if config.obfuscate_hostname:
-            self.hostname_path = "insights_commands/hostname"
+            self.hostname_path = hostname_path
         else:
             self.hostname_path = None
