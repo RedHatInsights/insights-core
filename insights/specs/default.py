82c2bca77c50f0616d897c0e51a0c3e493021a07
ppandey-redhat <39727896+ppandey-redhat@users.noreply.github.com>
2018-08-30 07:23:50 +0530

    Updated neutron_conf parser to support filter (#1400)
    (cherry picked from commit afaa5eba90cddbe2ddd1b9e64acae108e0811b87)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -394,7 +394,7 @@ class DefaultSpecs(Specs):
     netstat_i = simple_command("/bin/netstat -i")
     netstat_s = simple_command("/bin/netstat -s")
     networkmanager_dispatcher_d = glob_file("/etc/NetworkManager/dispatcher.d/*-dhclient")
-    neutron_conf = simple_file("/etc/neutron/neutron.conf")
+    neutron_conf = first_file(["/var/lib/config-data/neutron/etc/neutron/neutron.conf", "/etc/neutron/neutron.conf"])
     neutron_l3_agent_log = simple_file("/var/log/neutron/l3-agent.log")
     neutron_metadata_agent_ini = first_file(["/var/lib/config-data/neutron/etc/neutron/metadata_agent.ini", "/etc/neutron/metadata_agent.ini"])
     neutron_metadata_agent_log = first_file(["/var/log/containers/neutron/metadata-agent.log", "/var/log/neutron/metadata-agent.log"])
