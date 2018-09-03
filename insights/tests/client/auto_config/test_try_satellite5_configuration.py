78d6d5a215514d81af7d1071a2b9d33c8fd164c5
Štěpán Tomsa <stomsa@redhat.com>
2018-09-03 10:30:49 +0200

    Fix Satellite 5 auto configuration
    A set_auto_configuration function call was missing a config
    argument. That caused a runtime error if auto configuration falls back
    to Satellite 5.

---
+++ insights/tests/client/auto_config/test_try_satellite5_configuration.py
@@ -0,0 +1,20 @@
+from insights.client.auto_config import _try_satellite5_configuration
+from mock.mock import Mock, patch
+
+
+@patch("insights.client.auto_config.set_auto_configuration")
+@patch("insights.client.auto_config.open", return_value=["serverURL=https://some-hostname/",
+                                                                 "sslCACert=some-certificate",
+                                                                 "enableProxy=0"])
+@patch("insights.client.auto_config._read_systemid_file")
+@patch("insights.client.auto_config.os.path.isfile", return_value=True)
+def test_set_auto_configuration(isfile_mock, read_systemid_file_mock, open_mock, set_auto_configuration_mock):
+    """
+    set_auto_configuration_mock is called with correct arguments.
+    """
+    config_mock = Mock()
+    _try_satellite5_configuration(config_mock)
+    set_auto_configuration_mock.assert_called_once_with(config_mock,
+                                                        "some-hostname/redhat_access",
+                                                        "some-certificate",
+                                                        None)
