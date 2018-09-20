f2ebf4b459803f17b523217f5e3b7fb91a767c77
Štěpán Tomsa <stomsa@redhat.com>
2018-09-20 12:28:26 +0200

    Fix remove file usage in collection tests
    test_get_rm_conf.test_no_file did not work properly: it tested the
    default value of the remote_file constant instead of the patched
    one. Fixed this to be consistend with other tests.
    
    test_collect.patch_isfile was patching the isfile method and the
    collection_remove_file constant at once, which was not very
    transparent and violated SRP. Split into two methods.
    
    Changed mocked removed files in test_collect so their names are not so
    confusing. It looked like configuration file names, but they are files
    to be removed. Also renamed the variables to make their purpose
    clearer.

--- insights/tests/client/collection_rules/test_get_rm_conf.py
+++ insights/tests/client/collection_rules/test_get_rm_conf.py
@@ -4,8 +4,8 @@ from .helpers import insights_upload_conf
 from mock.mock import patch
 
 
-remove_file = '/etc/insights-client/remove.conf'
-remove_files = ["/etc/remove.conf", "/tmp/remove.conf"]
+conf_remove_file = '/tmp/remove.conf'
+removed_files = ["/etc/some_file", "/tmp/another_file"]
 
 
 def patch_isfile(isfile):
@@ -29,28 +29,28 @@ def patch_raw_config_parser(items):
     return decorator
 
 
-@patch_isfile(False)
 @patch_raw_config_parser([])
-def test_no_file(raw_config_parser, isfile):
-    upload_conf = insights_upload_conf(remove_file=remove_file)
+@patch_isfile(False)
+def test_no_file(isfile, raw_config_parser):
+    upload_conf = insights_upload_conf(remove_file=conf_remove_file)
     result = upload_conf.get_rm_conf()
 
-    isfile.assert_called_once_with(remove_file)
+    isfile.assert_called_once_with(conf_remove_file)
     raw_config_parser.assert_not_called()
 
     assert result is None
 
 
-@patch_raw_config_parser([("files", ",".join(remove_files))])
+@patch_raw_config_parser([("files", ",".join(removed_files))])
 @patch_isfile(True)
 def test_return(isfile, raw_config_parser):
-    upload_conf = insights_upload_conf(remove_file=remove_file)
+    upload_conf = insights_upload_conf(remove_file=conf_remove_file)
     result = upload_conf.get_rm_conf()
 
-    isfile.assert_called_once_with(remove_file)
+    isfile.assert_called_once_with(conf_remove_file)
 
     raw_config_parser.assert_called_once_with()
-    raw_config_parser.return_value.read.assert_called_with(remove_file)
+    raw_config_parser.return_value.read.assert_called_with(conf_remove_file)
     raw_config_parser.return_value.items.assert_called_with('remove')
 
-    assert result == {"files": remove_files}
+    assert result == {"files": removed_files}
