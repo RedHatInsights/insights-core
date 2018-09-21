11b33fbb03586c7bd63973891cf2c23a1e64c5bd
Chris Sams <csams@users.noreply.github.com>
2018-09-21 10:26:05 -0500

    Convert evaluators to formatters. Allow them to run all at once or incrementally. (#1444)

--- insights/formats/_json.py
+++ insights/formats/_json.py
@@ -1,11 +1,12 @@
 import json
 
-from insights.formats import EvaluatorFormatterAdapter, EvaluatorFormatter
+from insights.core.evaluators import SingleEvaluator
+from insights.formats import EvaluatorFormatterAdapter
 
 
-class JsonFormat(EvaluatorFormatter):
-    def dump(self, data):
-        return json.dumps(data)
+class JsonFormat(SingleEvaluator):
+    def postprocess(self):
+        json.dump(self.get_response(), self.stream)
 
 
 class JsonFormatterAdapter(EvaluatorFormatterAdapter):
