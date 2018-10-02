020757f893932f696985a371cf14f31a14a33346
Jeremy Crafts <jcrafts@redhat.com>
2018-10-02 15:25:29 -0400

    POC for how you would use insights client as a module

---
+++ platform_poc.py
@@ -0,0 +1,9 @@
+from insights.client import InsightsClient
+from insights.client.config import InsightsConfig
+
+config = InsightsConfig(
+    payload='test.tar.gz',
+    content_type='application/vnd.redhat.advisor.test+tgz',
+    upload_url='https://cert-api.access.redhat.com/r/insights/platform/upload/api/v1/upload')
+client = InsightsClient(config)
+client.upload()  # or client.upload(path=config.payload)
