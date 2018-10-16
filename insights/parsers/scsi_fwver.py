5f4f352422f7956bff85e8401cdd35eb0f0675b3
vishawanathjadhav <vjadhav@redhat.com>
2018-10-16 17:22:41 +0530

    Added local changes

--- insights/parsers/scsi_fwver.py
+++ insights/parsers/scsi_fwver.py
@@ -1,5 +1,6 @@
 """
 SCSIFWver - file ``/sys/class/scsi_host/host[0-9]*/fwrev``
+==========================================================
 
 This parser parses the content from fwver file from individual
 SCSI hosts. This parser will return dictionary formated data.
@@ -9,15 +10,15 @@ Sample Content from ``/sys/class/scsi_host/host0/fwrev``::
     2.02X12 (U3H2.02X12), sli-3
 
 
-Sample Output:
-    {
-        'host0': '2.02X12 (U3H2.02X12), sli-3',
-    }
-
+Examples:
+    >>> type(scsi_obj)
+    <class 'insights.parsers.scsi_fwver.SCSIFWver'>
+    >>> scsi_obj.data
+    {'host0': ['2.02X12 (U3H2.02X12)', ' sli-3']}
+    >>> scsi_obj.scsi_host
+    'host0'
 """
 
-import re
-from collections import OrderedDict
 from .. import parser, get_active_lines, LegacyItemAccess, CommandParser
 from insights.specs import Specs
 
@@ -32,12 +33,12 @@ class SCSIFWver(LegacyItemAccess, CommandParser):
     Properties:
         scsi_host (str): scsi host file name deriver from file path.
     """
-    
+
     def __init__(self, context):
         super(SCSIFWver, self).__init__(context)
         self.data = {}
         self.scsi_host = context.path.rsplit("/")[-2]
-    
+
     def parse_content(self, content):
         for line in get_active_lines(content):
             self.data[self.scsi_host] = line.split(',')
@@ -45,6 +46,6 @@ class SCSIFWver(LegacyItemAccess, CommandParser):
     @property
     def host_mode(self):
         """
-        (list) it will return the scsi host modes when set else None.
+        (list): It will return the scsi host modes when set else `None`.
         """
         return self.data[self.scsi_host]
