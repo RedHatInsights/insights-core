2c0bb8e01a893eee8fc9fdb1c1ddfe031d3f894b
Kyle Lape <klape@redhat.com>
2018-10-23 17:19:16 -0500

    Remove dangling branch_info mock

--- insights/tests/client/connection/test_branch_info.py
+++ insights/tests/client/connection/test_branch_info.py
@@ -4,7 +4,6 @@ from mock.mock import Mock, patch
 
 @patch("insights.client.connection.InsightsConnection._init_session")
 @patch("insights.client.connection.InsightsConnection.get_proxies")
-@patch("insights.client.connection.constants.cached_branch_info", "/tmp/insights-test-cached-branchinfo")
 def test_request(get_proxies, init_session):
     """
     The request to get branch info is issued with correct timeout set.
