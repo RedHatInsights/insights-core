fcf117e8b577ceb2f8831bc4031e076ae9bdb203
Flos Lonicerae <qguo@redhat.com>
2018-10-23 16:07:40 +0800

    Split MD5CheckSum parser into prelink version and normal version (#1487)
    * Split MD5CheckSum parser into prelink version and normal version
    
     - Fix md5chk_files spec. Don't check libfreeblpriv3.so, libsoftokn3.so and pem file as prelink has been deprecated on RHEL7
     - Check extra timezone files
     - Keep prelink_orig_md5 spec disabled as it needs extra work under current framework
    
    * Remove unnecessary attribute. Mix the LegacyItemAccess
    
    (cherry picked from commit d1d0ee7a0ab8ba3684086557854ed1639035043d)

--- insights/parsers/md5check.py
+++ insights/parsers/md5check.py
@@ -6,20 +6,16 @@ Module for processing output of the ``prelink -y --md5`` or ``md5sum`` command.
 
 The name and md5 checksums of specified files are stored as lists. Each
 entry will be processed by the shared parser.
-
-This MD5CheckSum class provide the following properties:
-
-* ``filename``: the file name which derived from the output.
-* ``md5sum``: the checksums for the file.
 """
-from .. import parser, CommandParser
-from insights.specs import Specs
+
+from .. import parser, CommandParser, LegacyItemAccess
+from ..parsers import ParseException
+from ..specs import Specs
 
 
-@parser(Specs.prelink_orig_md5)
-class MD5CheckSum(CommandParser):
+class MD5CheckSum(LegacyItemAccess, CommandParser):
     """
-    Class to parse ``prelink -y --md5`` or ``md5sum`` command information.
+    Common class to parse ``prelink -y --md5`` or ``md5sum`` command information.
 
     The output of these two commands contains two fields, the first is the
     md5 checksum and the second is the file name.
@@ -27,41 +23,53 @@ class MD5CheckSum(CommandParser):
     Especially, we add /dev/null as one of the specified file to be checked,
     as ``md5sum`` command can read from stdin without feeding a file.
 
-    Attributes::
-
-        filename (str): File name.
-        md5sum (str): MD5 checksums of that file.
-
     Sample input for ``prelink -y --md5`` or ``md5sum`` command::
 
         d1e6613cfb62d3f111db7bdda39ac821  /usr/lib64/libsoftokn3.so
 
-    Examples::
+    Examples:
 
-        >>> collection = shared[MD5CheckSum]
-        >>> len(collection) # All MD5 checksums in a list
+        >>> type(md5info)
+        <class 'insights.parsers.md5check.MD5CheckSum'>
+        >>> len(md5info.files)
         1
-        >>> record = collection[0]
-        >>> record.filename
-        /usr/lib64/libsoftokn3.so
-        >>> record.md5sum
-        d1e6613cfb62d3f111db7bdda39ac821
+        >>> "/usr/lib64/libsoftokn3.so" in md5info.files
+        True
+        >>> md5info['/usr/lib64/libsoftokn3.so']
+        'd1e6613cfb62d3f111db7bdda39ac821'
 
+    Attributes:
+        data (dict): All MD5 checksums are stored in this dictionary with the
+                     filename as the key and the checksum as the value.
+        files (list): Return the all file names in the checksum results.
     """
-    @property
-    def filename(self):
-        """ str: Return the file name for the current file."""
-        return self.data.get('filename')
-
-    @property
-    def md5sum(self):
-        """ str: Return the checksum for the current file."""
-        return self.data.get('md5sum')
 
     def parse_content(self, content):
+        if not content:
+            raise ParseException("Input content is empty.")
         self.data = {}
-        fields = content[0].strip().split() if content else None
-        # skip the '/dev/null'
-        if fields and len(fields) == 2 and fields[1] != "/dev/null":
-            self.data["md5sum"] = fields[0]
-            self.data["filename"] = fields[1]
+        self.files = []
+        for line in content:
+            md5sum, filename = line.strip().split(' ', 1)
+            md5sum, filename = md5sum.strip(), filename.strip()
+            # skip the '/dev/null'
+            if len(md5sum) == 32 and filename != "/dev/null":
+                self.data[filename] = md5sum
+        if self.data:
+            self.files = self.data.keys()
+
+
+@parser(Specs.prelink_orig_md5)
+class PrelinkMD5(MD5CheckSum):
+    """
+    A parser for parsing the output of the ``prelink -y --md5``
+    """
+    pass
+
+
+@parser(Specs.md5chk_files)
+class NormalMD5(MD5CheckSum):
+    """
+    A parser for parsing the output of the ``md5sum``
+    """
+    pass
