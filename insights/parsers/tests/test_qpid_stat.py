0816dbffe08e130e635b334c5a46254d0838e87d
TripleJQK <jquan@redhat.com>
2018-11-08 05:06:02 +0800

    new parser for QpidStatG (#1521)
    * new parser for QpidStatG
    
    * make basic class
    
    * Move more code to base class
    
    - and rename the base class to command name
    
    * Update as per Bob's suggestion
    
    (cherry picked from commit 307eff5a1f9eb7e165f8cd1fa67d2b066179ae3e)

--- insights/parsers/tests/test_qpid_stat.py
+++ insights/parsers/tests/test_qpid_stat.py
@@ -1,6 +1,8 @@
+import doctest
+
 from insights.parsers import qpid_stat
 from insights.tests import context_wrap
-import doctest
+
 
 QPID_STAT_Q_DOCS = '''
 Queues
@@ -21,11 +23,45 @@ Subscriptions
   1                    celery                                                                     qpid.10.20.1.10:5671-10.20.1.10:33786   celery            21409           Y            CREDIT      5          0
 '''
 
+QPID_STAT_G_DOCS = '''
+Broker Summary:
+  uptime          cluster       connections  sessions  exchanges  queues
+  ========================================================================
+  97d 0h 16m 24s  <standalone>  23           37        14         33
+
+Aggregate Broker Statistics:
+  Statistic                   Messages    Bytes
+  ========================================================
+  queue-depth                 0           0
+  total-enqueues              1,727,129   42,597,828,309
+  total-dequeues              1,727,129   42,597,828,309
+  persistent-enqueues         28,733      23,896,468
+  persistent-dequeues         28,733      23,896,468
+  transactional-enqueues      0           0
+  transactional-dequeues      0           0
+  flow-to-disk-depth          0           0
+  flow-to-disk-enqueues       0           0
+  flow-to-disk-dequeues       0           0
+  acquires                    1,727,129
+  releases                    0
+  discards-no-route           41,171,496
+  discards-ttl-expired        0
+  discards-limit-overflow     0
+  discards-ring-overflow      0
+  discards-lvq-replace        0
+  discards-subscriber-reject  0
+  discards-purged             0
+  reroutes                    0
+  abandoned                   0
+  abandoned-via-alt           0
+'''
+
 
 def test_qpid_stat_q_docs():
     env = {
+        'qpid_stat_g': qpid_stat.QpidStatG(context_wrap(QPID_STAT_G_DOCS)),
         'qpid_stat_q': qpid_stat.QpidStatQ(context_wrap(QPID_STAT_Q_DOCS)),
-        'qpid_stat_u': qpid_stat.QpidStatU(context_wrap(QPID_STAT_U_DOCS)),
+        'qpid_stat_u': qpid_stat.QpidStatU(context_wrap(QPID_STAT_U_DOCS))
     }
     failed, total = doctest.testmod(qpid_stat, globs=env)
     assert failed == 0
@@ -144,3 +180,71 @@ def test_qpid_stat_u():
         'celery',
         'katello_event_queue',
     ]
+
+
+QPID_STAT_G = """
+COMMAND> qpid-stat -g --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671
+
+Broker Summary:
+  uptime          cluster       connections  sessions  exchanges  queues
+  ========================================================================
+  97d 0h 16m 24s  <standalone>  23           37        14         33
+
+Aggregate Broker Statistics:
+  Statistic                   Messages    Bytes
+  ========================================================
+  queue-depth                 0           0
+  total-enqueues              1,727,129   42,597,828,309
+  total-dequeues              1,727,129   42,597,828,309
+  persistent-enqueues         28,733      23,896,468
+  persistent-dequeues         28,733      23,896,468
+  transactional-enqueues      0           0
+  transactional-dequeues      0           0
+  flow-to-disk-depth          0           0
+  flow-to-disk-enqueues       0           0
+  flow-to-disk-dequeues       0           0
+  acquires                    1,727,129
+  releases                    0
+  discards-no-route           41,171,496
+  discards-ttl-expired        0
+  discards-limit-overflow     0
+  discards-ring-overflow      0
+  discards-lvq-replace        0
+  discards-subscriber-reject  0
+  discards-purged             0
+  reroutes                    0
+  abandoned                   0
+  abandoned-via-alt           0
+"""
+
+
+def test_qpid_stat_g():
+    qpid_list = qpid_stat.QpidStatG(context_wrap(QPID_STAT_G))
+    assert qpid_list.data[0].get('uptime') == '97d 0h 16m 24s'
+    assert qpid_list.data[0].get('cluster') == '<standalone>'
+    assert qpid_list.data[0].get('connections') == '23'
+    assert qpid_list.data[0].get('sessions') == '37'
+    assert qpid_list.data[0].get('exchanges') == '14'
+    assert qpid_list.data[0].get('queues') == '33'
+    assert qpid_list.data[1].get('Statistic') == 'queue-depth'
+    assert qpid_list.data[1].get('Messages') == '0'
+    assert qpid_list.data[1].get('Bytes') == '0'
+    assert qpid_list.data[11].get('Statistic') == 'acquires'
+    assert qpid_list.data[11].get('Messages') == '1,727,129'
+    assert qpid_list.data[11].get('Bytes') == ''
+
+    # test iteration
+    assert [d.get('Messages') for d in qpid_list] == [
+            None, '0', '1,727,129', '1,727,129', '28,733',
+            '28,733', '0', '0', '0', '0', '0', '1,727,129',
+            '0', '41,171,496', '0', '0', '0', '0', '0',
+            '0', '0', '0', '0'
+    ]
+    assert qpid_list.by_queue['<standalone>'] == {
+            'uptime': '97d 0h 16m 24s',
+            'sessions': '37',
+            'queues': '33',
+            'connections': '23',
+            'cluster': '<standalone>',
+            'exchanges': '14'
+    }
