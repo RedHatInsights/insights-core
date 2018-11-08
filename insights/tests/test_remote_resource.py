463b07bd2b227ef3b0fe16b4460be8501d432f6b
lhuett <lhuett@users.noreply.github.com>
2018-11-08 12:42:12 -0600

    Callouts datasource using cachecontrol (#1522)
    * Created a new datasource that allows usage of remote web resources.
    
    * Clean up
    
    * Adding requests_cache to setup.py
    
    * Corrected issue with response encode in mock_web_server.py
    
    * Corrected issue with response encode in mock_web_server.py... cont.
    
    * Corrected docstring issues.
    
    * Moved remote_resouce to module
    
    * Added remote_resource to api_index.rst for pydocs
    
    * Added cer_path variable to RemoteResource to allow cert for https
    requests
    Cleaned up code andd added doc
    
    * Corrected flake8 error
    
    * Add the ability to get content from a remote resouce such as a Rest
    service API. Using CacheControl library to control caching of
    response data.
    
    * Clean-up and adjustments.
    
    * Corrected issue with tests. ControlCache tests will only be ran for
    Python 2.7 and above.
    
    * Removed skip if python 2.6 from tests
    test_get_cached_remote_resource
    test_get_cached_remote_resource_not_found
    
    * Cleaned up some things.
    
    (cherry picked from commit 04b284479ac74cf43271e3e03cf81d21ab9f460a)

---
+++ insights/tests/test_remote_resource.py
@@ -0,0 +1,54 @@
+from insights.core.remote_resource import RemoteResource, CachedRemoteResource
+from insights.tests.mock_web_server import TestMockServer
+import sys
+import pytest
+
+GOOD_PAYLOAD = b'Successful return from Mock Service'
+NOT_FOUND = b'{"error":{"code": "404", "message": "Not Found"}}'
+
+
+class TestRemoteResource(TestMockServer):
+
+    # Test RemoteResource
+    def test_get_remote_resource(self):
+        rr = RemoteResource()
+
+        url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
+        rtn = rr.get(url)
+        assert GOOD_PAYLOAD in rtn.content
+
+    # Test RemoteResource not found
+    def test_get_remote_resource_not_found(self):
+        rr = RemoteResource()
+
+        url = 'http://localhost:{port}/moc/'.format(port=self.server_port)
+        rtn = rr.get(url)
+        assert rtn.content == NOT_FOUND
+
+    # Test CachedRemoteResource not cached
+    def test_get_cached_remote_resource(self):
+        crr = CachedRemoteResource()
+
+        url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
+        rtn = crr.get(url)
+        assert GOOD_PAYLOAD in rtn.content
+
+    # Test CachedRemoteResource returns cached response
+    @pytest.mark.skipif(sys.version_info < (2, 7), reason="CacheControl requires python 2.7 or higher")
+    def test_get_cached_remote_resource_cached(self):
+        crr = CachedRemoteResource()
+
+        url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
+        rtn = crr.get(url)
+        cont_1 = rtn.content
+        rtn = crr.get(url)
+        cont_2 = rtn.content
+        assert cont_1 == cont_2
+
+    # Test CachedRemoteResource not found
+    def test_get_cached_remote_resource_not_found(self):
+        crr = CachedRemoteResource()
+
+        url = 'http://localhost:{port}/moc/'.format(port=self.server_port)
+        rtn = crr.get(url)
+        assert rtn.content == NOT_FOUND
