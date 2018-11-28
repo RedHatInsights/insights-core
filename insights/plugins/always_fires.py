82a682f04e1ac23042dbcfb7e44522c0409a99bb
Chris Sams <csams@users.noreply.github.com>
2018-11-28 13:33:56 -0600

    Add tags to components and update pydoc. (#1553)
    * Add tags to components and update a pile of documentation.
    
    Documentation updates include significant pydoc enhancements in
    insights.core.dr and insights.core.plugins.
    
    * Fix docstrings per feedback.
    
    * Fix double inclusion of ComponentType in documentation. Other minor doc fixes.

--- insights/plugins/always_fires.py
+++ insights/plugins/always_fires.py
@@ -1,7 +1,7 @@
 from insights.core.plugins import make_response, rule
 
 
-@rule()
+@rule(tags=["test"])
 def report():
     if True:
         return make_response("ALWAYS_FIRES", kernel="this is junk")
