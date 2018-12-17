ec53a5b5f9695f9d25a3d26af7eee0d54a0e0a50
rasrivas-redhat <rasrivas@redhat.com>
2018-12-17 12:18:43 +0530

    removed the flake error

--- insights/parsers/tests/test_ifcfg_static_route.py
+++ insights/parsers/tests/test_ifcfg_static_route.py
@@ -6,7 +6,7 @@ import pytest
 
 CONTEXT_PATH_DEVICE_1 = "etc/sysconfig/network-scripts/route-test-net"
 
-STATIC_ROUTE_1="""
+STATIC_ROUTE_1 = """
 ADDRESS0=10.65.223.0
 NETMASK0=255.255.254.0
 GATEWAY0=10.65.223.1
@@ -14,7 +14,7 @@ GATEWAY0=10.65.223.1
 
 CONTEXT_PATH_DEVICE_2 = "etc/sysconfig/network-scripts/route-test-net-11"
 
-STATIC_ROUTE_2="""
+STATIC_ROUTE_2 = """
 ADDRESS0=
 NETMASK0=255.255.254.0
 GATEWAY0=10.65.223.1
@@ -22,12 +22,14 @@ GATEWAY0=10.65.223.1
 
 CONTEXT_PATH_DEVICE_3 = "etc/sysconfig/network-scripts/ifcfg-eth0"
 
+
 def test_static_route_connection_1():
     context = context_wrap(STATIC_ROUTE_1, CONTEXT_PATH_DEVICE_1)
     r = IfCFGStaticRoute(context)
     assert r.static_route == 'test-net'
     assert r.data == {'ADDRESS0': '10.65.223.0', 'NETMASK0': '255.255.254.0', 'GATEWAY0': '10.65.223.1'}
-    assert r.data['ADDRESS0'] ==  '10.65.223.0'
+    assert r.data['ADDRESS0'] == '10.65.223.0'
+
 
 def test_static_route_connection_2():
     context = context_wrap(STATIC_ROUTE_1, CONTEXT_PATH_DEVICE_2)
@@ -35,17 +37,20 @@ def test_static_route_connection_2():
     assert r.static_route == 'test-net-11'
     assert r.data == {'ADDRESS0': '10.65.223.0', 'NETMASK0': '255.255.254.0', 'GATEWAY0': '10.65.223.1'}
 
+
 def test_missing_columns():
     with pytest.raises(AssertionError):
         context = context_wrap(STATIC_ROUTE_2, CONTEXT_PATH_DEVICE_1)
         r = IfCFGStaticRoute(context)
-        assert r.data['ADDRESS0'] ==  '10.65.223.0'
+        assert r.data['ADDRESS0'] == '10.65.223.0'
+
 
 def test_missing_index():
     with pytest.raises(IndexError):
         context = context_wrap(STATIC_ROUTE_2, CONTEXT_PATH_DEVICE_3)
         r = IfCFGStaticRoute(context)
         assert r.static_route == 'eth0'
+
 
 def test_ifcfg_static_route_doc_examples():
     env = {
