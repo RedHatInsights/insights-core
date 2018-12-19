ed8f4cfb9eccc0718b0c3f764e84e47ff389085c
Xiangce Liu <xiangceliu@redhat.com>
2018-12-19 10:20:45 +0800

    Add parsers GrubbyDefaultKernel (#1601)
    * Add parsers GrubbyDefaultKernel
    
    * Add more case to examples
    
    (cherry picked from commit 40cd60d668370bf2aed55532b708da66b0616e01)

--- insights/parsers/grubby.py
+++ insights/parsers/grubby.py
@@ -2,29 +2,74 @@
 grubby - command ``/usr/sbin/grubby``
 =====================================
 
-This parser returns the output of the ``grubby --default-index`` command.
+This is a collection of parsers that all deal with the command ``grubby``.
+Parsers included in this module are:
 
-Examples:
+GrubbyDefaultIndex - command ``grubby --default-index``
+-------------------------------------------------------
 
-    >>> grubby_default_index.default_index
-    0
+GrubbyDefaultKernel - command ``grubby --default-kernel``
+---------------------------------------------------------
 """
 
 from insights.specs import Specs
-from .. import parser, Parser
-from . import SkipException
+from insights.parsers import SkipException, ParseException
+from insights import CommandParser
+from insights import parser
 
 
 @parser(Specs.grubby_default_index)
-class GrubbyDefaultIndex(Parser):
+class GrubbyDefaultIndex(CommandParser):
     """
-    This parser will parse the output of command ``grubby --default-index``.
+    This parser parses the output of command ``grubby --default-index``.
+
+    The typical output of this command is::
+
+        0
+
+    Examples:
+        >>> grubby_default_index.default_index
+        0
+
+    Raises:
+        SkipException: When output is empty
+        ParseException: When output is invalid
 
     Attributes:
         default_index (int): the numeric index of the current default boot entry, count from 0
     """
     def parse_content(self, content):
-        if content and content[0].isdigit():
-            self.default_index = int(content[0])
-        else:
-            raise SkipException('Invalid default index value.')
+        if not content:
+            raise SkipException('Empty output')
+        if len(content) != 1 or not content[0].isdigit():
+            raise ParseException('Invalid output: {0}', content)
+        self.default_index = int(content[0])
+
+
+@parser(Specs.grubby_default_kernel)
+class GrubbyDefaultKernel(CommandParser):
+    """
+    This parser parses the output of command ``grubby --default-kernel``.
+
+    The typical output of this command is::
+
+        /boot/vmlinuz-2.6.32-573.el6.x86_64
+
+    Examples:
+
+        >>> grubby_default_kernel.default_kernel
+        '/boot/vmlinuz-2.6.32-573.el6.x86_64'
+
+    Raises:
+        SkipException: When output is empty
+        ParseException: When output is invalid
+
+    Attributes:
+        default_kernel(str): The default kernel name for next boot
+    """
+    def parse_content(self, content):
+        if not content:
+            raise SkipException('Empty output')
+        if len(content) != 1:
+            raise ParseException('Invalid output: {0}', content)
+        self.default_kernel = content[0].strip()
