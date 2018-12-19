d1eac0c1715698795cb26a097f8d19b0f69479dd
Xiangce Liu <xiangceliu@redhat.com>
2018-12-19 11:01:38 +0800

    Add interface to CmdLine return the RAW cmdline (#1606)
    - and enhance the documentation
    
    (cherry picked from commit 484137e04307642eb16a2344c023431f851a29c5)

--- insights/parsers/cmdline.py
+++ insights/parsers/cmdline.py
@@ -1,35 +1,14 @@
 """
-CmdLine - File ``/proc/cmdline``
+CmdLine - file ``/proc/cmdline``
 ================================
 
 This parser reads the ``/proc/cmdline`` file, which contains the arguments
 given to the currently running kernel on boot.
 
-Sample input::
-
-    BOOT_IMAGE=/vmlinuz-3.10.0-327.36.3.el7.x86_64 root=/dev/system_vg/Root ro rd.lvm.lv=system_vg/Root crashkernel=auto rd.lvm.lv=system_vg/Swap rhgb quiet LANG=en_GB.utf8
-
-Examples:
-
-    >>> cmd = shared[CmdLine]
-    >>> cmd['BOOT_IMAGE']
-    ['/vmlinuz-3.10.0-327.36.3.el7.x86_64']
-    >>> cmd['rd.lvm.lv']
-    ['system_vg/Root', 'system_vg/Swap']
-    >>> 'autofs' in cmd
-    False
-    >>> cmd.get('autofs')
-    None
-    >>> 'quiet' in cmd
-    True
-    >>> cmd.get('quiet')
-    [True]
-    >>> cmd['crashkernel']
-    ['auto']
-
 """
 
-from .. import Parser, parser, LegacyItemAccess
+from insights import Parser, parser, LegacyItemAccess
+from insights.parsers import SkipException, ParseException
 from insights.specs import Specs
 
 
@@ -49,19 +28,50 @@ class CmdLine(LegacyItemAccess, Parser):
         If an element contains "=", set the corresponding value to the whole right
         value of the "=".
 
-        Note:
-            For special command line elements that include two "=", e.g.
-            ``root=LABEL=/1``, "root" will be the key and "LABEL=/1" will be
-            the value in the returned list.
+    .. note::
 
-            Some parameters (the returned keys) might be still effective
-            even if there is '#' before it, e.g.: ``#rhgb``.  This should
-            be checked by the rule.
+        For special command line elements that include two "=", e.g.
+        ``root=LABEL=/1``, "root" will be the key and "LABEL=/1" will be
+        the value in the returned list.
+
+        Some parameters (the returned keys) might be still effective
+        even if there is '#' before it, e.g.: ``#rhgb``.  This should
+        be checked by the rule.
+
+    Sample input::
+
+        BOOT_IMAGE=/vmlinuz-3.10.0-327.36.3.el7.x86_64 root=/dev/system_vg/Root ro rd.lvm.lv=system_vg/Root crashkernel=auto rd.lvm.lv=system_vg/Swap rhgb quiet LANG=en_GB.utf8
+
+    Examples:
+        >>> cmd['BOOT_IMAGE']
+        ['/vmlinuz-3.10.0-327.36.3.el7.x86_64']
+        >>> cmd['rd.lvm.lv']
+        ['system_vg/Root', 'system_vg/Swap']
+        >>> 'autofs' in cmd
+        False
+        >>> cmd.get('autofs') is None
+        True
+        >>> 'quiet' in cmd
+        True
+        >>> cmd.get('quiet')
+        [True]
+        >>> cmd['crashkernel']
+        ['auto']
+
+    Attributes:
+        data (dict): Parsed booting arguments are stored in this dictionary
+        cmdline (str): The RAW line of the ``/proc/cmdline``
     """
 
     def parse_content(self, content):
+        if not content:
+            raise SkipException('Empty output')
+        if len(content) != 1:
+            raise ParseException('Invalid output: {0}', content)
+
         self.data = {}
-        for el in content[0].strip().split(None):
+        self.cmdline = content[0].strip()
+        for el in self.cmdline.split():
             # Thanks to strip and split, el always contains something.
             key, value = el, True
             if "=" in el:
