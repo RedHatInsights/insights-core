fad656094e35fca17d042b5398e9eb339dd74422
rasrivas-redhat <rasrivas@redhat.com>
2019-01-15 11:25:27 +0530

    updated the test cases for raw_bonding_value and raw_master_value

--- insights/parsers/tests/test_ifcfg.py
+++ insights/parsers/tests/test_ifcfg.py
@@ -110,6 +110,28 @@ IPV6INIT=no
 BONDING_OPTS="mode=balance-xor primary=eth1 arp_interval=1000 arp_ip_target=+10.11.96.1 downdelay =0"
 """.strip()
 
+IFCFG_TEST_RAW_MASTER_VALUE = """
+DEVICE="eth0"
+IPADDR=10.11.96.172
+NETMASK=255.255.252.0
+BOOTPROTO=none
+ONBOOT=yes
+USERCTL=no
+IPV6INIT=no
+MASTER="bond0"
+""".strip()
+
+IFCFG_TEST_RAW_BONDING_VALUE = """
+DEVICE="bond0"
+IPADDR=10.11.96.172
+NETMASK=255.255.252.0
+BOOTPROTO=none
+ONBOOT=yes
+USERCTL=no
+IPV6INIT=no
+BONDING_OPTS="mode=balance-xor primary=eth1 arp_interval=1000 arp_ip_target=+10.11.96.1 downdelay =0"
+""".strip()
+
 IFCFG_CONFIG_STR_ERROR = """
 DEVICE=bond0
 IPADDR=10.11.96.172
@@ -122,6 +144,8 @@ BONDING_OPTS="mode=balance-xor primary = eth1 arp_interval= 1000 arp_ip_target=+
 """.strip()
 
 IFCFG_PATH_NAMED_BOND_MODE = "etc/sysconfig/network-scripts/ifcfg-en0"
+
+IFCFG_PATH_NAMED_BOND_MASTER = "etc/sysconfig/network-scripts/ifcfg-eth0"
 
 IFCFG_TEST_BADLY_NAMED_BOND_MODE = """
 DEVICE=bond0
@@ -298,3 +322,23 @@ def test_ifcfg_blankline():
     context.path = IFCFG_PATH_BLANK_LINE
     r = IfCFG(context)
     assert r.has_empty_line is True
+
+
+def test_ifcfg_raw_bonding_master_value():
+    context = context_wrap(IFCFG_TEST_RAW_MASTER_VALUE)
+    context.path = IFCFG_PATH_NAMED_BOND_MASTER
+
+    r = IfCFG(context)
+
+    assert r["raw_device_value"] == '"eth0"'
+    assert r["raw_master_value"] == '"bond0"'
+
+
+def test_ifcfg_raw_bonding_value():
+    context = context_wrap(IFCFG_TEST_RAW_BONDING_VALUE)
+    context.path = IFCFG_PATH_NAMED_BOND_MODE
+
+    r = IfCFG(context)
+
+    assert r["raw_device_value"] == '"bond0"'
+    assert r["raw_bonding_value"] == '"mode=balance-xor primary=eth1 arp_interval=1000 arp_ip_target=+10.11.96.1 downdelay =0"'
