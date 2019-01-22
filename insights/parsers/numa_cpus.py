6f732e98b5f16655cb3c20716619283d0e389128
vishawanathjadhav <vjadhav@redhat.com>
2019-01-22 17:55:25 +0530

    removed shippable fail

--- insights/parsers/numa_cpus.py
+++ insights/parsers/numa_cpus.py
@@ -14,8 +14,8 @@ Examples:
     <class 'insights.parsers.numa_cpus.NUMACpus'>
     >>> numa_cpus_obj.numa_node_name
     'node0'
-    >>> numa_cpus_obj.numa_node_details()
-    {'numa_node_range': ['0-6', '14-20'], 'total_cpus': 14, 'numa_node_name': 'node0'}
+    >>> numa_cpus_obj.numa_node_details() == {'numa_node_range': ['0-6', '14-20'], 'total_cpus': 14, 'numa_node_name': 'node0'}
+    True
     >>> numa_cpus_obj.numa_node_cpus
     ['0-6', '14-20']
     >>> numa_cpus_obj.total_numa_node_cpus
