c4f28ecae36f077d1d2679e737d76c34d2dce759
vishwanathjadhav <vjadhav@redhat.com>
2019-01-29 13:18:09 +0530

    1. Added generic parser for modinfo (#1654)
    * 1. Added generic parser for modinfo
    2. Added specific parser class for '/sbin/modinfo i40e' cmd output
    
    * 1. Modified code logic
    2. Modified doc examples
    3. Added Error Handling
    4. Changed file name and Class name
    5. Added specs in insights_archive.py
    
    * 1. Removed calculations from properties
    2. Added some methods to asses lists
    
    * Added examples in doc
    
    * Modified doc string
    
    * Modified comments
    
    * Added one more doc string

--- insights/__init__.py
+++ insights/__init__.py
@@ -24,7 +24,7 @@ import sys
 import yaml
 
 from .core import Scannable, LogFileOutput, Parser, IniConfigFile  # noqa: F401
-from .core import FileListing, LegacyItemAccess, SysconfigOptions  # noqa: F401
+from .core import FileListing, LegacyItemAccess, ModInfo, SysconfigOptions  # noqa: F401
 from .core import YAMLParser, JSONParser, XMLParser, CommandParser  # noqa: F401
 from .core import AttributeDict  # noqa: F401
 from .core import Syslog  # noqa: F401
