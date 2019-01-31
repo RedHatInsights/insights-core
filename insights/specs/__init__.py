a50fcf34c2eda400937f80c7ba892d4947bdb5b7
Xiangce Liu <xiangceliu@redhat.com>
2019-01-31 11:36:10 +0800

    remove the point entry for test spec

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -418,7 +418,6 @@ class Specs(SpecSet):
     sap_hdb_version = RegistryPoint(multi_output=True)
     sap_host_profile = RegistryPoint(filterable=True)
     sapcontrol_getsystemupdatelist = RegistryPoint()
-    sapcontrol_getsystemupdatelist_test = RegistryPoint()
     saphostctl_getcimobject_sapinstance = RegistryPoint(filterable=True)
     saphostexec_status = RegistryPoint()
     saphostexec_version = RegistryPoint()
