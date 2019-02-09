b07ec938618ebb54c975f54da242b87e03420c29
vishawanathjadhav <vjadhav@redhat.com>
2019-02-10 02:50:33 +0530

    Added parser for SCTP eps

---
+++ docs/shared_parsers_catalog/sctp_eps.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.parsers.sctp_eps
+   :members:
+   :show-inheritance:
--- insights/parsers/sctp_eps.py
+++ insights/parsers/sctp_eps.py
@@ -1,6 +1,6 @@
 """
-SCTPEps - file ``/proc/net/sctp/eps ``
-======================================
+SCTPEps - file ``/proc/net/sctp/eps``
+=====================================
 
 This provides detail information about SCTP endpoints.
 Which includes Endpoints, Socket, Socket type, Socket State,
@@ -8,35 +8,39 @@ hash bucket, bind port, UID, socket inodes, Local IP
 address.
 
 Typical content of ``eps`` file is::
+
     ENDPT            SOCK             STY SST HBKT LPORT   UID INODE     LADDRS
-    ffff88017e0a0200 ffff880299f7fa00 2   10  29   11165   200 299689357 10.0.0.102 10.0.0.70 
-    ffff880612e81c00 ffff8803c28a1b00 2   10  30   11166   200 273361203 10.0.0.102 10.0.0.70 
-    ffff88061fba9800 ffff88061f8a3180 2   10  31   11167   200 273361145 10.0.0.102 10.0.0.70 
-    ffff88031e6f1a00 ffff88031dbdb180 2   10  32   11168   200 273365974 10.0.0.102 10.0.0.70
+    ffff88017e0a0200 ffff880300f7fa00 2   10  29   11165   200 299689357 10.0.0.102 10.0.0.70
+    ffff880612e81c00 ffff8803c28a1b00 2   10  30   11166   200 273361203 10.0.0.102 10.0.0.70 172.31.1.2
 
 Output data is stored in the dictionary format
 
 Examples:
     >>> type(sctp_info)
     <class 'insights.parsers.sctp_eps.SCTPEps'>
+    >>> sorted(sctp_info.get_ports()) == sorted(['11165', '11166'])
+    True
+    >>> sorted(sctp_info.get_local_ips()) == sorted(['10.0.0.102', '10.0.0.70', '172.31.1.2'])
+    True
 """
 
-from insights import Parser, parser, get_active_lines
-from insights.specs import Specs
+from insights import Parser, parser
 from insights.parsers import SkipException, ParseException
+from insights.specs import Specs
 
 
 @parser(Specs.sctp_eps)
 class SCTPEps(Parser):
     """
     This parser parses the content of ``/proc/net/sctp/eps`` file.
-    Currently we need "Bind Port" and "Local address"
+    And returns a list of dictionaries. The dictionary contains details
+    of individual SCTP endpoint"
     """
-    
+
     def parse_content(self, content):
         if (not content) or (not self.file_path):
             raise SkipException("No Contents")
-        
+
         COLUMN_IDX = {
             'ENDPT': 'endpoints',
             'SOCK': 'socket',
@@ -48,9 +52,9 @@ class SCTPEps(Parser):
             'INODE': 'inode',
             'LADDRS': 'local_addr'
         }
-        
+
         self.data = []
-        exp_column = sorted(COLUMN_IDX.keys())
+        exp_column = COLUMN_IDX.keys()
         for line in content:
             row = {}
             line = line.strip()
@@ -59,10 +63,47 @@ class SCTPEps(Parser):
                 if len(line) == len(exp_column):
                     columns = line
                 else:
-                    raise ParseException("Unrecognised Column Name".format(b=raws))
+                    raise ParseException("Contents are not compatible to this parser".format(row))
             else:
                 for idx, val in enumerate(columns):
-                    key = COLUMN_IDX[val]
-                    row[key]=line[idx]
+                    if val == "LADDRS":
+                        key = COLUMN_IDX[val]
+                        row[key] = []
+                        while (idx != len(line)):
+                            row[key].append(line[idx])
+                            idx = idx + 1
+                    else:
+                        key = COLUMN_IDX[val]
+                        row[key] = line[idx]
                 self.data.append(row)
 
+    def get_ports(self):
+        """
+        (list): This function returns a list of SCTP ports if SCTP
+                endpoints are created, else `[]`.
+        """
+        sctp_ports = []
+        for eps in self.data:
+            sctp_ports.append(eps['local_port'])
+        return sctp_ports
+
+    def get_local_ips(self):
+        """
+        (list): This function returns a list of all local ip addresses
+                if SCTP endpoints are created, else `[]`.
+        """
+        sctp_ips = set([])
+        for eps in self.data:
+            sctp_ips = set.union(sctp_ips, set(eps['local_addr']))
+        return list(sctp_ips)
+
+    def get_eps_ips(self):
+        """
+        (dict): This function returns a dict of all endpoints and corresponding
+                local ip addresses used by SCTP endpoints if SCTP endpoints are
+                created, else `None`.
+        """
+        sctp_eps = {}
+        for eps in self.data:
+            sctp_eps[eps['endpoints']] = eps['local_addr']
+        return sctp_eps
