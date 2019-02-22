725e5b6ef1697700ee7589ebea406212de8a9408
Sachin Patil <psachin@redhat.com>
2019-02-22 11:13:12 +0530

    Ensure that the attribute incorrect_volume_ownership returns volume path
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- insights/parsers/rhev_data_center.py
+++ insights/parsers/rhev_data_center.py
@@ -3,6 +3,7 @@ rhev_data_center - datasource ``rhev_data_center``
 ==================================================
 """
 import json
+from re import compile as re_compile
 from insights import Parser, parser
 from insights.specs import Specs
 
@@ -16,31 +17,26 @@ class RhevDataCenter(Parser):
     Attributes:
         data(list): A list of the parsed output returned by `rhev_data_center` Datasource.
 
-    The following are available in ``data``:
+        incorrect_volume_ownership(list): Volumes attached to the RHEV VMs in
+          the Data Center having incorrect file ownership.
+
+    The following are available in ``data`` and ``incorrect_volume_ownership``:
 
         * ``name``  - file owner
         * ``group`` - file group
         * ``path``  - full path of a file
 
     Examples:
-        >>> assert len(rhev_dc.data) == 3
-        >>> assert len(rhev_dc.bad_image_ownership) == 1
-        >>> assert rhev_dc.bad_image_ownership[0]['path'] == '/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137'
-        >>> assert rhev_dc.bad_image_ownership[0]['name'] == 'root'
-        >>> assert rhev_dc.bad_image_ownership[0]['group'] == 'root'
+        >>> assert len(rhev_dc.data) == 4
+        >>> assert len(rhev_dc.incorrect_volume_ownership) == 1
+        >>> assert rhev_dc.incorrect_volume_ownership[0]['path'] == '/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137'
+        >>> assert rhev_dc.incorrect_volume_ownership[0]['name'] == 'root'
+        >>> assert rhev_dc.incorrect_volume_ownership[0]['group'] == 'root'
+
     """
     def parse_content(self, content):
         self.data = json.loads(''.join(content))
 
-    @property
-    def bad_image_ownership(self):
-        """Returns full path of RHEV VM images in the Data Center not having correct file ownership.
-
-        Returns:
-            (list): [{'name': 'root', 'group': 'root', 'path': '/rhev/data-center/mnt/example.com:_nfsshare_data/a384bf5d/images/b7d6cc07/4d6e5dea'}]
-        """
-        bad_ownership = []
-        for each in self.data:
-            if '/images/' in each['path']:
-                bad_ownership.append(each)
-        return bad_ownership
+        # Full path of volumes attached to the RHEV VMs in the Data Center not having correct file ownership.
+        _vol_path_pattern = re_compile("/images/[0-9,a-z,-]*/[0-9,a-z,-]*")
+        self.incorrect_volume_ownership = [each for each in self.data if _vol_path_pattern.search(each['path'])]
--- insights/parsers/tests/test_rhev_data_center.py
+++ insights/parsers/tests/test_rhev_data_center.py
@@ -4,17 +4,17 @@ from insights.tests import context_wrap
 
 
 RHEV_DATA_CENTER_INSIGHTS = """
-[{"name": "root", "group": "root", "path": "/rhev/data-center/mnt/r430-21.example.com:_home_nfsshare_data"}, {"name": "root", "group": "root", "path": "/rhev/data-center/mnt/r430-21.example.redhat.com:_home_nfsshare_iso"}, {"name": "root", "group": "root", "path": "/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137"}]
+[{"name": "root", "group": "root", "path": "/rhev/data-center/mnt/r430-21.example.com:_home_nfsshare_data"}, {"name": "root", "group": "root", "path": "/rhev/data-center/mnt/r430-21.example.redhat.com:_home_nfsshare_iso"}, {"name": "root", "group": "root", "path": "/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137"}, {"name": "root", "group": "root", "path": "/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3"}]
 """
 
 
 def test_ls_rhev_data_center():
     files = ls_dc.RhevDataCenter(context_wrap(RHEV_DATA_CENTER_INSIGHTS))
-    assert len(files.data) == 3
-    assert len(files.bad_image_ownership) == 1
-    assert files.bad_image_ownership[0]['path'] == '/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137'
-    assert files.bad_image_ownership[0]['name'] == 'root'
-    assert files.bad_image_ownership[0]['group'] == 'root'
+    assert len(files.data) == 4
+    assert len(files.incorrect_volume_ownership) == 1
+    assert files.incorrect_volume_ownership[0]['path'] == '/rhev/data-center/mnt/host1.example.com:_nfsshare_data/a384bf5d-db92-421e-926d-bfb99a6b4b28/images/b7d6cc07-d1f1-44b3-b3c0-7067ec7056a3/4d6e5dea-995f-4a4e-b487-0f70361f6137'
+    assert files.incorrect_volume_ownership[0]['name'] == 'root'
+    assert files.incorrect_volume_ownership[0]['group'] == 'root'
 
 
 def test_rhev_data_center_doc_examples():
