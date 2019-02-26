b542bb8fa6efe1cbffad2d13e82457839a197165
Xiangce Liu <xiangceliu@redhat.com>
2019-02-26 16:14:01 +0800

    New parser ForemanProxySshConfig (#1706)
    * New parser ForemanProxySshConfig
    
    - and skip empty content
    
    * Fix a typo in docstring

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -728,6 +728,7 @@ class DefaultSpecs(Specs):
     ss = simple_command("/usr/sbin/ss -tupna")
     ssh_config = simple_file("/etc/ssh/ssh_config")
     ssh_foreman_config = simple_file("/usr/share/foreman/.ssh/ssh_config")
+    ssh_foreman_proxy_config = simple_file("/usr/share/foreman-proxy/.ssh/ssh_config")
     sshd_config = simple_file("/etc/ssh/sshd_config")
     sshd_config_perms = simple_command("/bin/ls -l /etc/ssh/sshd_config")
     sssd_config = simple_file("/etc/sssd/sssd.conf")
