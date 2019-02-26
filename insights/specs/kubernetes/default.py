5f8556e296453541c7de69948cede1be5ee87519
Jesse Jaggars <jhjaggars@gmail.com>
2019-02-26 15:52:17 +0000

    adding registrypoints for k8s data sources

--- insights/specs/kubernetes/default.py
+++ insights/specs/kubernetes/default.py
@@ -1,17 +1,17 @@
 import yaml
 from insights.core.plugins import datasource
-from insights.core.spec_factory import SpecSet
+from insights.specs import Specs
 from insights.specs.kubernetes import (kube_resource, KubeClient,
         KubeOutputProvider)
 
 
-class KubeSpecs(SpecSet):
-    ns_info = kube_resource(kind="Namespace")
-    nodes = kube_resource(kind="Node")
-    crds = kube_resource(kind="CustomResourceDefinition", api_version="apiextensions.k8s.io/v1beta1")
+class KubeSpecs(Specs):
+    k8s_namespaces = kube_resource(kind="Namespace")
+    k8s_nodes = kube_resource(kind="Node")
+    k8s_crds = kube_resource(kind="CustomResourceDefinition", api_version="apiextensions.k8s.io/v1beta1")
 
     @datasource(crds, KubeClient)
-    def crs(broker):
+    def k8s_crs(broker):
         client = broker[KubeClient]
         cr_list = []
         for crd in yaml.safe_load(broker[KubeSpecs.crds].content)["items"]:
