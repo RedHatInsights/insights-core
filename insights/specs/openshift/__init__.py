d4cf3553c9b7d95c47f149844e29efb85ab3d909
Jesse Jaggars <jhjaggars@gmail.com>
2019-03-12 12:48:08 -0500

    openshift spec refactoring (#1729)
    * openshift spec refactoring
    
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>
    
    * no longer treating OpenshiftClient as a component
    
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>
    (cherry picked from commit 610ec6609f86dbb5fd1289d040cc84cd5b9d7111)

--- insights/specs/openshift/__init__.py
+++ insights/specs/openshift/__init__.py
@@ -1,12 +1,12 @@
 import os
-import yaml
+import json
 
 from kubernetes import config
 from kubernetes.client import ApiClient, Configuration
 from openshift.dynamic import DynamicClient
 
 from insights.core.context import ExecutionContext
-from insights.core.plugins import component, datasource
+from insights.core.plugins import datasource
 from insights.core.serde import deserializer, serializer
 from insights.core.spec_factory import ContentProvider, SerializedRawOutputProvider
 from insights.util import fs
@@ -24,7 +24,7 @@ class OpenshiftOutputProvider(ContentProvider):
 
     def load(self):
         doc = self.k8s.resources.get(**self.client_kwargs).get().to_dict()
-        return yaml.dump(doc, default_flow_style=False)
+        return json.dumps(doc)
 
     def write(self, dst):
         fs.ensure_path(os.path.dirname(dst))
@@ -57,7 +57,6 @@ class OpenshiftContext(ExecutionContext):
     pass
 
 
-@component(OpenshiftContext)
 class OpenshiftClient(object):
     def __init__(self, ctx=None, cfg=None):
         cfg = cfg or os.environ.get("KUBECONFIG")
@@ -72,15 +71,15 @@ class OpenshiftClient(object):
         self.k8s = DynamicClient(k8s_client)  # stole this from config.new_client_from_config
 
 
-class openshift_resource(object):
+class resource(object):
     def __init__(self, kind, api_version="v1", **kwargs):
         # encode group into the api_version string if necessary
         self.client_kwargs = kwargs
         self.client_kwargs["kind"] = kind
         self.client_kwargs["api_version"] = api_version
         self.__name__ = self.__class__.__name__
-        datasource(OpenshiftClient)(self)
+        datasource(OpenshiftContext)(self)
 
     def __call__(self, broker):
-        client = broker[OpenshiftClient]
+        client = OpenshiftClient()
         return OpenshiftOutputProvider(client, **self.client_kwargs)
