3a62aa30dee85ef7d0b91986511404046b6bf19b
shzhou <shzhou@redhat.com>
2019-03-15 14:54:35 +0800

    Add parser ls_origin_local_volumes_pods

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -241,6 +241,7 @@ class Specs(SpecSet):
     ls_docker_volumes = RegistryPoint()
     ls_etc = RegistryPoint()
     ls_ocp_cni_openshift_sdn = RegistryPoint()
+    ls_origin_local_volumes_pods = RegistryPoint()
     lsinitrd_lvm_conf = RegistryPoint()
     lsmod = RegistryPoint()
     lsof = RegistryPoint(filterable=True)
