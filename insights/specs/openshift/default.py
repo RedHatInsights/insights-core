c175e4820946c18cd31dd43cddaa3adf98d802ab
Jesse Jaggars <jhjaggars@gmail.com>
2019-03-21 23:38:28 +0000

    adding more ocp4 datasources

--- insights/specs/openshift/default.py
+++ insights/specs/openshift/default.py
@@ -1,4 +1,6 @@
-from insights import specs
+import json
+from insights import specs, datasource
+from insights.core.spec_factory import DatasourceProvider
 from . import GVK, foreach_resource, resource
 
 
@@ -10,13 +12,19 @@ def cr_gvk(crd):
 
 
 class OpenshiftSpecsImpl(specs.Openshift):
+    cluster_operators = resource(kind="ClusterOperator", api_version="config.openshift.io/v1")
+    crds = resource(kind="CustomResourceDefinition", api_version="apiextensions.k8s.io/v1beta1")
+    crs = foreach_resource(crds, cr_gvk)
+    machine_configs = resource(kind="MachineConfig", api_version="machineconfiguration.openshift.io/v1")
+    machines = resource(kind="Machine", api_version="machine.openshift.io/v1beta1")
     namespaces = resource(kind="Namespace")
     nodes = resource(kind="Node")
     pods = resource(kind="Pod", field_selector="status.phase!=Succeeded")
-    cluster_operators = resource(kind="ClusterOperator", api_version="config.openshift.io/v1")
-    machines = resource(kind="Machine", api_version="machine.openshift.io/v1beta1")
-    machine_configs = resource(kind="MachineConfig", api_version="machineconfiguration.openshift.io/v1")
-    storage_classes = resource(kind="StorageClass")
     pvcs = resource(kind="PersistentVolumeClaim")
-    crds = resource(kind="CustomResourceDefinition", api_version="apiextensions.k8s.io/v1beta1")
-    crs = foreach_resource(crds, cr_gvk)
+    storage_classes = resource(kind="StorageClass")
+
+    @datasource(namespaces)
+    def machine_id(broker):
+        doc = json.loads(broker[OpenshiftSpecsImpl.namespaces].content)
+        v = next(o["metadata"]["uid"] for o in doc["items"] if o["metadata"]["name"] == "kube-system")
+        return DatasourceProvider(content=v, relative_path="/etc/insights-client/machine-id")
