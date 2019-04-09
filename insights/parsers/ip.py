e27fdd26fe404e959b83a4ca719578ab2285deec
vishawanathjadhav <vjadhav@redhat.com>
2019-04-09 12:56:31 +0530

    Modified doc strings

--- insights/parsers/ip.py
+++ insights/parsers/ip.py
@@ -280,7 +280,7 @@ class RouteDevices(CommandParser):
     * ``netmask`` - the CIDR notation of the network as a string (e.g. '24')
     * ``table`` - the routing table, or None if not given.
 
-    Destinations given as **broadcast**, **throw**, **local**,
+    Destinations given as **broadcast**, **throw**,
     **unreachable**, **prohibit**, **blackhole** and **nat** are discarded.
 
     Destinations given as **unicast**, **multicast**
