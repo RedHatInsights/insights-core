aa6bd95637167f550fb2a132899f962792253893
Xiangce Liu <xiangceliu@redhat.com>
2019-04-12 09:45:10 +0800

    Update as per actual test result

--- insights/parsers/aws_instance_type.py
+++ insights/parsers/aws_instance_type.py
@@ -8,7 +8,7 @@ which is used to check the type of the AWS instance on the host.
 
 """
 
-from insights.parsers import SkipException
+from insights.parsers import SkipException, ParseException
 from insights import parser, CommandParser
 from insights.specs import Specs
 
@@ -25,9 +25,10 @@ class AWSInstanceType(CommandParser):
 
     Raises:
         SkipException: When content is empty or no parse-able content.
+        ParseException: When type cannot be recognized.
 
     Attributes:
-        type: The name of AWS instance type in all uppercase lettes. E.g. R3, R4, R5, or X1.
+        type: The name of AWS instance type in all uppercase letters. E.g. R3, R4, R5, or X1.
         raw: The fully type string returned by the ``curl`` command.
 
     Examples:
@@ -38,17 +39,16 @@ class AWSInstanceType(CommandParser):
     """
 
     def parse_content(self, content):
-        if not content:
-            raise SkipException('Empty content.')
+        if (not content or len(content) > 1 or 'curl: ' in content[0]):
+            raise SkipException()
 
-        self.type = self.raw = None
-        for line in content:
-            if 'xlarge' in line:
-                self.raw = [i for i in line.split() if 'xlarge' in i and '.' in i][0]
-                self.type = self.raw.split('.')[0].upper()
+        self.raw = self.type = None
+        if '.' in content[0]:
+            self.raw = content[0].strip()
+            self.type = self.raw.split('.')[0].upper()
 
-        if self.raw is None:
-            raise SkipException('Not AWS.')
+        if not self.type:
+            raise ParseException('Unrecognized type: "{0}"', content[0])
 
     def __str__(self):
         return "<aws_type: {t}, raw: {r}>".format(t=self.type, r=self.raw)
--- insights/parsers/tests/test_aws_instance_type.py
+++ insights/parsers/tests/test_aws_instance_type.py
@@ -3,28 +3,37 @@ import doctest
 from insights.parsers import aws_instance_type
 from insights.parsers.aws_instance_type import AWSInstanceType
 from insights.tests import context_wrap
-from insights.parsers import SkipException
+from insights.parsers import SkipException, ParseException
 
 AWS_TYPE = "r3.xlarge"
-AWS_TYPE_LINES = """
-*   Trying 169.254.169.254...
-* TCP_NODELAY set
-t2.xlarge
+AWS_TYPE_AB_1 = """
+curl: (7) Failed to connect to 169.254.169.254 port 80: Connection timed out
 """.strip()
-AWS_TYPE_AB = """
-*   Trying 169.254.169.254...
-* TCP_NODELAY set
-* Connection failed
-* connect to 169.254.169.254 port 80 failed: Operation timed out
-* Failed to connect to 169.254.169.254 port 80: Operation timed out
-* Closing connection 0
-curl: (7) Failed to connect to 169.254.169.254 port 80: Operation timed out
+AWS_TYPE_AB_2 = """
+curl: (7) couldn't connect to host
+""".strip()
+AWS_TYPE_AB_3 = """
+curl: (28) connect() timed out!
+""".strip()
+AWS_TYPE_AB_4 = """
+.micro
 """.strip()
 
 
-def test_aws_instance_type_ab_timeout():
+
+def test_aws_instance_type_ab_other():
     with pytest.raises(SkipException):
-        AWSInstanceType(context_wrap(AWS_TYPE_AB))
+        AWSInstanceType(context_wrap(AWS_TYPE_AB_1))
+
+    with pytest.raises(SkipException):
+        AWSInstanceType(context_wrap(AWS_TYPE_AB_2))
+
+    with pytest.raises(SkipException):
+        AWSInstanceType(context_wrap(AWS_TYPE_AB_3))
+
+    with pytest.raises(ParseException) as pe:
+        AWSInstanceType(context_wrap(AWS_TYPE_AB_4))
+        assert 'Unrecognized type' in str(pe)
 
 
 def test_aws_instance_type_ab_empty():
@@ -36,12 +45,6 @@ def test_aws_instance_type():
     aws = AWSInstanceType(context_wrap(AWS_TYPE))
     assert aws.type == "R3"
     assert aws.raw == "r3.xlarge"
-
-
-def test_aws_instance_type_multi_lines():
-    aws = AWSInstanceType(context_wrap(AWS_TYPE_LINES))
-    assert aws.type == "T2"
-    assert aws.raw == "t2.xlarge"
 
 
 def test_doc_examples():
