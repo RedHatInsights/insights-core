1e0678d1361311a8f49dc5fc8434e065d75ba8db
Jakub Svoboda <jsvoboda@redhat.com>
2019-04-23 21:05:46 +0000

    New parser and spec for bind crypto policy in RHEL8. (#1842)
    * New parser and spec for bind crypto policy in RHEL8.
    
    * review feedback
    
    (cherry picked from commit 80ee08c7c452c9404a01c57c0273bb38aa173319)

--- insights/parsers/tests/test_crypto_policies_doc_examples.py
+++ insights/parsers/tests/test_crypto_policies_doc_examples.py
@@ -1,7 +1,7 @@
 from insights.tests import context_wrap
 from insights.parsers import crypto_policies
 from insights.parsers.crypto_policies import CryptoPoliciesOpensshserver, CryptoPoliciesConfig, \
-    CryptoPoliciesStateCurrent
+    CryptoPoliciesStateCurrent, CryptoPoliciesBind
 import doctest
 
 
@@ -17,12 +17,23 @@ STATECURRENT = """
 LEGACY
 """.strip()
 
+BIND = """
+disable-algorithms "." {
+RSAMD5;
+DSA;
+};
+disable-ds-digests "." {
+GOST;
+};
+""".strip()
+
 
 def test_crypto_policies_doc():
     env = {
         'cp_os': CryptoPoliciesOpensshserver(context_wrap(OPENSSHSERVER)),
         'cp_c': CryptoPoliciesConfig(context_wrap(CONFIG, path="/etc/crypto-policies/config")),
         'cp_sc': CryptoPoliciesStateCurrent(context_wrap(STATECURRENT, path="/etc/crypto-policies/state/current")),
+        'cp_bind': CryptoPoliciesBind(context_wrap(BIND)),
     }
     failed, total = doctest.testmod(crypto_policies, globs=env)
     assert failed == 0
