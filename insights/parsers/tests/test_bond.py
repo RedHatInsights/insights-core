6fe3a9163a7e01e9a68047d161ad27ea8a132340
Rahul Srivastava <rasrivas@dhcp223-44.pnq.redhat.com>
2019-04-25 09:48:53 +0530

    enhacement of bond parser

--- insights/parsers/tests/test_bond.py
+++ insights/parsers/tests/test_bond.py
@@ -54,12 +54,17 @@ Active Aggregator Info:
 
 Slave Interface: eth1
 MII Status: up
+Speed: 1000 Mbps
+Duplex: full
 Link Failure Count: 0
 Permanent HW addr: 00:16:35:5e:42:fc
 Aggregator ID: 3
 
 Slave Interface: eth2
 MII Status: up
+Speed: 1000 Mbps
+Duplex: full
+Speed
 Link Failure Count: 0
 Permanent HW addr: 00:16:35:5e:02:7e
 Aggregator ID: 2
@@ -135,6 +140,84 @@ Slave queue ID: 0
 
 BONDINFO_MODE_6 = BONDINFO_MODE_5.replace("Currently Active Slave: enp17s0f0", "")
 
+BOND_INFO_DATA = """
+Ethernet Channel Bonding Driver: v3.7.1 (April 27, 2011)
+
+Bonding Mode: IEEE 802.3ad Dynamic link aggregation
+Transmit Hash Policy: layer2 (0)
+MII Status: up
+MII Polling Interval (ms): 100
+Up Delay (ms): 0
+Down Delay (ms): 0
+
+802.3ad info
+LACP rate: fast
+Min links: 0
+Aggregator selection policy (ad_select): stable
+System priority: 65535
+System MAC address: 52:54:00:b5:ba:8b
+Active Aggregator Info:
+    Aggregator ID: 2
+    Number of ports: 1
+    Actor Key: 9
+    Partner Key: 1
+    Partner Mac Address: 6e:50:47:d6:00:46
+
+Slave Interface: ens10
+MII Status: up
+Speed: 1000 Mbps
+Duplex: full
+Link Failure Count: 0
+Permanent HW addr: 52:54:00:b5:ba:8b
+Slave queue ID: 0
+Aggregator ID: 2
+Actor Churn State: none
+Partner Churn State: none
+Actor Churned Count: 0
+Partner Churned Count: 0
+details actor lacp pdu:
+    system priority: 65535
+    system mac address: 52:54:00:b5:ba:8b
+    port key: 9
+    port priority: 255
+    port number: 1
+    port state: 63
+details partner lacp pdu:
+    system priority: 65534
+    system mac address: 6e:50:47:d6:00:46
+    oper key: 1
+    port priority: 65535
+    port number: 1
+    port state: 63
+
+Slave Interface: ens11
+MII Status: down
+Speed: Unknown
+Duplex: Unknown
+Link Failure Count: 0
+Permanent HW addr: 52:54:00:50:d2:0f
+Slave queue ID: 0
+Aggregator ID: 1
+Actor Churn State: churned
+Partner Churn State: churned
+Actor Churned Count: 1
+Partner Churned Count: 1
+details actor lacp pdu:
+    system priority: 65535
+    system mac address: 52:54:00:b5:ba:8b
+    port key: 0
+    port priority: 255
+    port number: 2
+    port state: 71
+details partner lacp pdu:
+    system priority: 65535
+    system mac address: 00:00:00:00:00:00
+    oper key: 1
+    port priority: 255
+    port number: 1
+    port state: 1
+""".strip()
+
 
 def test_netstat_doc_examples():
     env = {
@@ -145,7 +228,6 @@ def test_netstat_doc_examples():
 
 
 def test_bond_class():
-
     bond_obj = Bond(context_wrap(BONDINFO_1, CONTEXT_PATH))
     assert bond_obj.file_name == 'bond0'
     assert not bond_obj.partner_mac_address
@@ -174,6 +256,14 @@ def test_bond_class():
     bond_obj_2 = Bond(context_wrap(BONDINFO_MODE_6, CONTEXT_PATH))
     assert bond_obj_2.bond_mode == '1'
     assert bond_obj_2.active_slave is None
+
+    bond_obj_3 = Bond(context_wrap(BONDINFO_1, CONTEXT_PATH))
+    assert bond_obj_3.file_name == 'bond0'
+    assert bond_obj_3.slave_interface == ['eno1', 'eno2']
+    assert bond_obj_3.slave_duplex == ['full', 'full']
+    assert bond_obj_3.slave_speed == ['1000 Mbps', '1000 Mbps']
+    assert bond_obj_3.slave_link_failure_count == ['0', '0']
+    assert bond_obj_3.mii_status == ['up', 'up', 'up']
 
     with pytest.raises(ParseException) as exc:
         bond_obj = Bond(context_wrap(BONDINFO_UNKNOWN_BOND_MODE, CONTEXT_PATH))
