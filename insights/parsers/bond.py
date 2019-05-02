3d0cd968a521708da0ce3d9b1b2d169ae9a5e20e
Rahul Srivastava <rasrivas@localhost.localdomain>
2019-05-02 22:28:10 +0530

    added the extra line

--- insights/parsers/bond.py
+++ insights/parsers/bond.py
@@ -1,10 +1,13 @@
 """
 Bond - file ``/proc/net/bonding``
 =================================
+
 Provides plugins access to the network bonding information gathered from
 all the files starteing with "bond." located in the
 ``/proc/net/bonding`` directory.
+
 Typical content of ``bond.*`` file is::
+
     Ethernet Channel Bonding Driver: v3.2.4 (January 28, 2008)
     Bonding Mode: IEEE 802.3ad Dynamic link aggregation
     Transmit Hash Policy: layer2 (0)
@@ -12,6 +15,7 @@ Typical content of ``bond.*`` file is::
     MII Polling Interval (ms): 500
     Up Delay (ms): 0
     Down Delay (ms): 0
+
     802.3ad info
     LACP rate: slow
     Active Aggregator Info:
@@ -20,19 +24,23 @@ Typical content of ``bond.*`` file is::
             Actor Key: 17
             Partner Key: 1
             Partner Mac Address: 00:00:00:00:00:00
+
     Slave Interface: eth1
     MII Status: up
     Link Failure Count: 0
     Permanent HW addr: 00:16:35:5e:42:fc
     Aggregator ID: 3
+
     Slave Interface: eth2
     MII Status: up
     Link Failure Count: 0
     Permanent HW addr: 00:16:35:5e:02:7e
     Aggregator ID: 2
+
 Data is modeled as an array of ``Bond`` objects (``bond`` being a
 pattern file specification gathering data from files located in
 ``/proc/net/bonding``.
+
 Examples:
     >>> type(bond_info)
     <class 'insights.parsers.bond.Bond'>
