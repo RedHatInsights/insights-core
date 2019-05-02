5f2859795059259e4cc35e518dd933840d3b9451
Nikhil Chawla <nichawla@redhat.com>
2019-05-02 12:36:24 +0530

    Added Test Case for the Parser

---
+++ insights/parsers/tests/test_getconf_pagesize.py
@@ -0,0 +1,21 @@
+import pytest
+from insights.parsers.getconf_pagesize import GetconfPageSize
+from insights.tests import context_wrap
+from insights.parsers import ParseException
+
+GETCONFPAGESIZE1 = """
+4096
+""".strip()
+
+GETCONFPAGESIZE2 = """
+16384
+""".strip()
+
+class Testgetconfpagesize():
+    def test_getconf_PAGESIZE1(self):
+        result = GetconfPageSize(context_wrap(GETCONFPAGESIZE1))
+        assert result.page_size == 4096
+
+    def test_getconf_PAGESIZE2(self):
+        result = GetconfPageSize(context_wrap(GETCONFPAGESIZE2))
+        assert result.page_size == 16384
