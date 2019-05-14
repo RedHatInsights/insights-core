eb5059bc477005037473f20a304d9d4fe72378c7
Flos Lonicerae <qguo@redhat.com>
2019-05-14 17:38:08 +0800

    Syslog can return relevant logs by specifying the process name (#1918)
    * Syslog can return relevant logs by specifying the process name
    
    * Handle logs w/o process id
    
    * Rewrite log analysis without regex
    
    * Move the timestamp valid check to _parse_line
    
    * Fix flake8 errors
    
    * Remove unused lines

--- insights/core/__init__.py
+++ insights/core/__init__.py
@@ -1250,10 +1250,29 @@ class Syslog(LogFileOutput):
 
             info_splits = info.split()
             if len(info_splits) == 5:
-                msg_info['timestamp'] = ' '.join(info_splits[:3])
+                logstamp = ' '.join(info_splits[:3])
+                try:
+                    datetime.datetime.strptime(logstamp, self.time_format)
+                except ValueError:
+                    return msg_info
+                msg_info['timestamp'] = logstamp
                 msg_info['hostname'] = info_splits[3]
                 msg_info['procname'] = info_splits[4]
         return msg_info
+
+    def get_logs_by_procname(self, proc):
+        """
+        Parameters:
+            proc(str): The process or facility that you're looking for
+
+        Yields:
+            (dict): The parsed syslog messages produced by that process or facility
+        """
+        for line in self.lines:
+            l = self._parse_line(line)
+            procid = l.get('procname', '')
+            if proc == procid or proc == procid.split('[')[0]:
+                yield l
 
 
 class IniConfigFile(ConfigParser):
