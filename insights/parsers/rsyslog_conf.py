ae30f34055e81c73c774496aa1a429d1ca4ee344
vishawanathjadhav <vjadhav@redhat.com>
2019-05-24 15:07:18 +0530

    Added rsyslog.conf parser in RHEL8

--- insights/parsers/rsyslog_conf.py
+++ insights/parsers/rsyslog_conf.py
@@ -1,6 +1,11 @@
 """
-RsyslogConf - file ``/etc/rsyslog.conf``
-========================================
+Parser - file ``/etc/rsyslog.conf``
+===================================
+
+RsyslogConf - file ``/etc/rsyslog.conf`` RHEL-6/7
+-------------------------------------------------
+RsyslogConf8 - file ``/etc/rsyslog.conf`` RHEL-8
+-------------------------------------------------
 
 The rsyslog configuration files can include statements with two different
 line based formats along with snippets of 'RainerScript' that can span
@@ -12,17 +17,12 @@ Due to high parsing complexity, this parser presents a simple line-based
 view of the file that meets the needs of the current rules.
 
 Example:
-    >>> content = '''
-    ... :fromhost-ip, regex, "10.0.0.[0-9]" /tmp/my_syslog.log
-    ... $ModLoad imtcp
-    ... $InputTCPServerRun 10514"
-    ... '''.strip()
-    >>> from insights.tests import context_wrap
-    >>> rsl = RsyslogConf(context_wrap(content))
+    >>> type(rsl)
+    <class 'insights.parsers.rsyslog_conf.RsyslogConf'>
     >>> len(rsl)
-    3
+    5
     >>> len(list(rsl))
-    3
+    5
     >>> any('imtcp' in n for n in rsl)
     True
 """
@@ -30,9 +30,10 @@ from .. import Parser, parser, get_active_lines
 
 import re
 from insights.specs import Specs
+from insights.components.rhel_version import IsRhel6, IsRhel7, IsRhel8
 
 
-@parser(Specs.rsyslog_conf)
+@parser(Specs.rsyslog_conf, [IsRhel6, IsRhel7])
 class RsyslogConf(Parser):
     """
     Parses `/etc/rsyslog.conf` info simple lines.
@@ -77,3 +78,130 @@ class RsyslogConf(Parser):
     def __iter__(self):
         for d in self.data:
             yield d
+
+
+@parser(Specs.rsyslog_conf, IsRhel8)
+class RsyslogConf8(Parser):
+    """
+    This combiner is designed for RHEL-8 and other versions of RHEL. There is
+    a difference between configuration ``/etc/rsyslog.conf`` file on RHEL-8 and
+    other versions of RHEL.
+
+    Example:
+        >>> type(rsys)
+        <class 'insights.parsers.rsyslog_conf.RsyslogConf8'>
+        >>> len(rsys.module_details)
+        5
+        >>> len(rsys.include_details)
+        1
+    """
+
+    def parse_content(self, content):
+        self._module_details = {}
+        self._include_details = {}
+        self._input_details = {}
+        self._global_details = {}
+        self._log_details = {}
+
+        self.data = get_active_lines(content)
+        for line in self.data:
+            # Parse module and its configurations on two different line
+            if ('$' not in line) and (line.startswith('module')) and '(' in line and (not line.endswith(')')):
+                module_name = re.split(r'\W+', line)[2]
+                self._module_details[module_name] = {}
+            # Parse module and its configurations on two different line.
+            elif ('$' not in line) and ('module' not in line) and '(' not in line and line.endswith(')'):
+                line_split = re.findall(r'[a-z.:/*A-Z0-9_]+', line)
+                log_level = line_split[0]
+                status = line_split[1]
+                self._module_details[module_name][log_level] = status
+            # Parse module and its configurations on one line.
+            elif ('$' not in line) and (line.startswith('module')) and '(' in line and (line.endswith(')')):
+                line_split = re.findall(r'[a-z:A-Z0-9_]+', line)
+                module_name = line_split[2]
+                self._module_details[module_name] = {}
+                if len(line_split) > 3:
+                    timestamp_format = line_split[3]
+                    timestamp = line_split[4]
+                    self._module_details[module_name][timestamp_format] = timestamp
+            # Parse included configration files and modes.
+            elif ('$' not in line) and (line.startswith('include')) and '(' in line and (line.endswith(')')):
+                line_split = re.findall(r'[a-z.:/*A-Z0-9_]+', line)
+                file_path = line_split[2]
+                self._include_details[file_path] = {}
+                file_mode = line_split[3]
+                modes = line_split[4]
+                self._include_details[file_path][file_mode] = modes
+            # Parse input module and its ports.
+            elif ('$' not in line) and (line.startswith('input')) and '(' in line and (line.endswith(')')):
+                line_split = re.findall(r'[a-z.:/*A-Z0-9_]+', line)
+                input_type = line_split[2]
+                port = line_split[4]
+                self._input_details[input_type] = port
+            # Parse global workspace.
+            elif ('$' not in line) and (line.startswith('global')) and '(' in line and (line.endswith(')')):
+                line_split = re.findall(r'[a-z.:/*A-Z0-9_]+', line)
+                global_dir = line_split[1]
+                path = line_split[2]
+                self._global_details[global_dir] = path
+            # Parse other configuations
+            else:
+                line_split = line.split()
+                self._log_details[line_split[0]] = line_split[1]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        for d in self.data:
+            yield d
+
+    @property
+    def module_details(self):
+        """
+        This will return the dict of all the configured modules on success else
+        it will return empty dict `{}` on failure.
+
+        Returns (dict): module details.
+        """
+        return self._module_details
+
+    @property
+    def input_details(self):
+        """
+        This will return the dict of all the configured input methods on success else
+        it will return empty dict `{}` on failure.
+
+        Returns (dict): configuration input details .
+        """
+        return self._input_details
+
+    @property
+    def include_details(self):
+        """
+        This will return the dict of all included configuration files on success else
+        it will return empty dict `{}` on failure.
+
+        Returns (dict): included configuration details.
+        """
+        return self._include_details
+
+    @property
+    def global_details(self):
+        """
+        This will return the dict of all global dir for all the confiburation files on
+        success else it will return empty dict `{}` on failure.
+
+        Returns (dict): included configuration details.
+        """
+        return self._global_details
+
+    @property
+    def log_details(self):
+        """
+        This will return the dict of all log confiburation details on success else it
+        will return empty dict `{}` on failure.
+
+        Returns (dict): included configuration details.
+        """
+        return self._log_details
--- insights/parsers/tests/test_rsyslog_conf.py
+++ insights/parsers/tests/test_rsyslog_conf.py
@@ -1,5 +1,8 @@
+import doctest
 from insights.tests import context_wrap
-from insights.parsers.rsyslog_conf import RsyslogConf
+from insights.parsers import rsyslog_conf
+from insights.parsers.rsyslog_conf import RsyslogConf, RsyslogConf8
+
 
 RSYSLOG_CONF_0 = """
 :fromhost-ip, regex, "10.0.0.[0-9]" /tmp/my_syslog.log
@@ -14,6 +17,76 @@ RSYSLOG_CONF_1 = r"""
 #$ModLoad imtcp.so
 #$InputTCPServerRun 514
 :msg, regex, "\/vob\/.*\.[cpp|c|java]" /var/log/appMessages
+""".strip()
+
+RSYSLOG_CONF_RHEL8 = """
+# rsyslog configuration file
+
+# For more information see /usr/share/doc/rsyslog-*/rsyslog_conf.html
+# or latest version online at http://www.rsyslog.com/doc/rsyslog_conf.html
+# If you experience problems, see http://www.rsyslog.com/doc/troubleshoot.html
+
+#### MODULES ####
+
+module(load="imuxsock" 	  # provides support for local system logging (e.g. via logger command)
+\t\tSysSock.Use="off") # Turn off message reception via local log socket;
+\t\t# local messages are retrieved through imjournal now.
+module(load="imjournal" 	    # provides access to the systemd journal
+\t\tStateFile="imjournal.state") # File to store the position in the journal
+#module(load="imklog") # reads kernel messages (the same are read from journald)
+#module(load"immark") # provides --MARK-- message capability
+
+module(load="abc" xyz="on")
+# Provides UDP syslog reception
+# for parameters see http://www.rsyslog.com/doc/imudp.html
+module(load="imudp") # needs to be done just once
+input(type="imudp" port="514")
+
+# Provides TCP syslog reception
+# for parameters see http://www.rsyslog.com/doc/imtcp.html
+#module(load="imtcp") # needs to be done just once
+#input(type="imtcp" port="514")
+
+#### GLOBAL DIRECTIVES ####
+
+# Where to place auxiliary files
+global(workDirectory="/var/lib/rsyslog")
+
+# Use default timestamp format
+module(load="builtin:omfile" Template="RSYSLOG_TraditionalFileFormat")
+
+# Include all config files in /etc/rsyslog.d/
+include(file="/etc/rsyslog.d/*.conf" mode="optional")
+
+
+#### RULES ####
+
+# Log all kernel messages to the console.
+# Logging much else clutters up the screen.
+kern.*                                                 /dev/console
+
+# Log anything (except mail) of level info or higher.
+# Don't log private authentication messages!
+*.info;mail.none;authpriv.none;cron.none                /var/log/messages
+
+# The authpriv file has restricted access.
+authpriv.*                                              /var/log/secure
+
+# Log all the mail messages in one place.
+mail.*                                                  -/var/log/maillog
+
+
+# Log cron stuff
+cron.*                                                  /var/log/cron
+
+# Everybody gets emergency messages
+*.emerg                                                 :omusrmsg:*
+
+# Save news errors of level crit and higher in a special file.
+uucp,news.crit                                          /var/log/spooler
+
+# Save boot messages also to boot.log
+local7.*                                                /var/log/boot.log
 """.strip()
 
 
@@ -39,6 +112,21 @@ def test_rsyslog_conf_0():
     assert m.config_val('ModLoad') == 'imtcp'
     assert m.config_val('ForwardSyslogHost', 'syslog.example.com') == 'syslog.example.com'
 
+    ctx = context_wrap(RSYSLOG_CONF_RHEL8)
+    rsys = RsyslogConf8(ctx)
+    assert rsys.module_details == {'imuxsock': {'SysSock.Use': 'off'},
+                                   'imjournal': {'StateFile': 'imjournal.state'},
+                                   'abc': {'xyz': 'on'},
+                                   'imudp': {},
+                                   'builtin:omfile': {'Template': 'RSYSLOG_TraditionalFileFormat'}}
+    d = list(rsys)
+    assert len(d) == 18
+    assert len(rsys.module_details) == 5
+    assert len(rsys.log_details) == 8
+    assert rsys.global_details == {'workDirectory': '/var/lib/rsyslog'}
+    assert rsys.input_details == {'imudp': '514'}
+    assert rsys.include_details == {'/etc/rsyslog.d/*.conf': {'mode': 'optional'}}
+
 
 def test_rsyslog_conf_1():
     ctx = context_wrap(RSYSLOG_CONF_1)
@@ -49,3 +137,12 @@ def test_rsyslog_conf_1():
     # Test that commented-out config items are not detected
     assert 'ModLoad' not in m.config_items
     assert 'InputTCPServerRun' not in m.config_items
+
+
+def test_rsyslog_rhel8_doc_examples():
+    env = {
+        'rsl': RsyslogConf(context_wrap(RSYSLOG_CONF_0)),
+        'rsys': RsyslogConf8(context_wrap(RSYSLOG_CONF_RHEL8))
+    }
+    failed, total = doctest.testmod(rsyslog_conf, globs=env)
+    assert failed == 0
