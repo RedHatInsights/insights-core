0cbc994df75c8d23e7edffab518439ca5b929ce2
Nikhil Chawla <nichawla@redhat.com>
2019-05-27 11:15:39 +0530

    Added requested fixes

--- insights/parsers/getconf_pagesize.py
+++ insights/parsers/getconf_pagesize.py
@@ -1,13 +1,13 @@
 """
 GetconfPageSize - command ``/usr/sbin/getconf PAGE_SIZE``
-===========================================================
+=========================================================
 
 This very simple parser returns the output of the ``getconf PAGE_SIZE`` command.
 
 Examples:
 
     >>> pagesize_parsed.page_size
-    '4096'
+    4096
 """
 from . import ParseException
 from .. import parser, CommandParser
--- insights/parsers/tests/test_getconf_pagesize.py
+++ insights/parsers/tests/test_getconf_pagesize.py
@@ -1,5 +1,7 @@
 from insights.parsers.getconf_pagesize import GetconfPageSize
 from insights.tests import context_wrap
+from insights.parsers import getconf_pagesize
+import doctest
 
 GETCONFPAGESIZE1 = """
 4096
@@ -10,11 +12,20 @@ GETCONFPAGESIZE2 = """
 """.strip()
 
 
-class Testgetconfpagesize():
-    def test_getconf_PAGESIZE1(self):
-        result = GetconfPageSize(context_wrap(GETCONFPAGESIZE1))
-        assert result.page_size == 4096
+def test_getconf_PAGESIZE1():
+    result = GetconfPageSize(context_wrap(GETCONFPAGESIZE1))
+    assert result.page_size == 4096
 
-    def test_getconf_PAGESIZE2(self):
-        result = GetconfPageSize(context_wrap(GETCONFPAGESIZE2))
-        assert result.page_size == 16384
+
+def test_getconf_PAGESIZE2():
+    result = GetconfPageSize(context_wrap(GETCONFPAGESIZE2))
+    assert result.page_size == 16384
+
+
+def test_doc():
+    env = {
+            "pagesize_parsed": GetconfPageSize(context_wrap(GETCONFPAGESIZE1))
+    }
+    print(env)
+    failed, total = doctest.testmod(getconf_pagesize, globs=env)
+    assert failed == 0
