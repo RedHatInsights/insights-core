d69edd008d535936499f5a5b91e83d03c77ff8e5
Xiangce Liu <xiangceliu@redhat.com>
2019-05-30 05:14:54 +0800

    Remove the subscription_manager_list_installed from insights_archive.py (#1934)
    * Remove the subscription_manager_list_installed from insights_archive.py
    
    * Remove the spec from the default.py as well
    
    (cherry picked from commit c90c242fc4c835fc9d49bad3484b4518d15841b0)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -811,7 +811,6 @@ class DefaultSpecs(Specs):
     subscription_manager_facts_list = simple_command("/usr/bin/subscription-manager facts --list")
     subscription_manager_id = simple_command("/usr/bin/subscription-manager identity")
     subscription_manager_list_consumed = simple_command('/usr/bin/subscription-manager list --consumed')
-    subscription_manager_list_installed = simple_command('/usr/bin/subscription-manager list --installed')
     subscription_manager_release_show = simple_command('/usr/bin/subscription-manager release --show')
     subscription_manager_repos_list_enabled = simple_command('/usr/bin/subscription-manager repos --list-enabled')
     swift_conf = first_file(["/var/lib/config-data/puppet-generated/swift/etc/swift/swift.conf", "/etc/swift/swift.conf"])
