ec0502c439f18e3af0f684c5711a1edf035e5c7b
XiaoXue Wang <xiaoxwan@redhat.com>
2019-06-12 13:37:40 +0800

    Add spec for command lsinitrd

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -262,6 +262,7 @@ class Specs(SpecSet):
     ls_etc = RegistryPoint()
     ls_ocp_cni_openshift_sdn = RegistryPoint()
     ls_origin_local_volumes_pods = RegistryPoint()
+    lsinitrd = RegistryPoint(filterable=True)
     lsinitrd_lvm_conf = RegistryPoint()
     lsmod = RegistryPoint()
     lsof = RegistryPoint(filterable=True)
