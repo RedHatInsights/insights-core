18da99dd263020dc4f4171c333623e7d20627871
Sachin <psachin@redhat.com>
2019-06-25 10:57:13 +0530

    Enhance QemuXML to include Openstack metadata (#1981)
    This patch parses the Nova metadata section form XML:
    
      <metadata>
        <nova:instance xmlns:nova="http://openstack.org/xmlns/libvirt/nova/1.0">
          <nova:package version="14.0.3-8.el7ost"/>
          <nova:name>VM_AT_01</nova:name>
          <nova:creationTime>2017-10-09 08:51:28</nova:creationTime>
          <nova:flavor name="vpc1-cf1-foo-bar">
            <nova:memory>8096</nova:memory>
            <nova:disk>10</nova:disk>
            <nova:swap>0</nova:swap>
            <nova:ephemeral>0</nova:ephemeral>
            <nova:vcpus>4</nova:vcpus>
          </nova:flavor>
          <nova:owner>
            <nova:user uuid="96e9d2b749ea48fcb5a911e6f0e144f2">vpcdi1-01</nova:user>
            <nova:project uuid="5a50e9d0d19746158958be0c759793fb">vpcdi1</nova:project>
          </nova:owner>
          <nova:root type="image" uuid="1a05a423-dfae-428a-ae54-1614d8024e76"/>
        </nova:instance>
      </metadata>
    
    to:
    
    >>> xml.nova
    {'version': '14.0.3-8.el7ost',
     'instance_name': 'VM_AT_01',
     'created': '2017-10-09 08:51:28',
     'flavor_name': 'vpc1-cf1-foo-bar',
     'flavor_memory': '8096',
     'flavor_disk': '10',
     'flavor_swap': '0',
     'flavor_ephemeral': '0',
     'flavor_vcpus': '4',
     'user': 'vpcdi1-01',
     'user_uuid': '96e9d2b749ea48fcb5a911e6f0e144f2',
     'project': 'vpcdi1',
     'project_uuid': '5a50e9d0d19746158958be0c759793fb',
     'root_disk_type': 'image',
     'root_disk_uuid': '1a05a423-dfae-428a-ae54-1614d8024e76'}
    
    Resolves: #1717
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- insights/parsers/tests/test_qemu_xml.py
+++ insights/parsers/tests/test_qemu_xml.py
@@ -388,6 +388,268 @@ or other application using the libvirt API.
 </domstatus>
 """.strip()
 
+XML_RHOSP = """
+<!--
+WARNING: THIS IS AN AUTO-GENERATED FILE. CHANGES TO IT ARE LIKELY TO BE
+OVERWRITTEN AND LOST. Changes to this xml configuration should be made using:
+  virsh edit instance-000008d6
+or other application using the libvirt API.
+-->
+
+<domain type='kvm'>
+  <name>instance-000008d6</name>
+  <uuid>601da52c-450f-4d8e-b502-503431536fa69</uuid>
+  <metadata>
+    <nova:instance xmlns:nova="http://openstack.org/xmlns/libvirt/nova/1.0">
+      <nova:package version="14.0.3-8.el7ost"/>
+      <nova:name>django_vm_001</nova:name>
+      <nova:creationTime>2017-10-09 08:51:28</nova:creationTime>
+      <nova:flavor name="vpc1-cf1-foo-bar">
+        <nova:memory>8096</nova:memory>
+        <nova:disk>10</nova:disk>
+        <nova:swap>0</nova:swap>
+        <nova:ephemeral>0</nova:ephemeral>
+        <nova:vcpus>4</nova:vcpus>
+      </nova:flavor>
+      <nova:owner>
+        <nova:user uuid="96e9d2b749ea48fcb5a911e6f0e144f2">django_user_01</nova:user>
+        <nova:project uuid="5a50e9d0d19746158958be0c759793fb">vpcdi1</nova:project>
+      </nova:owner>
+      <nova:root type="image" uuid="1a05a423-dfae-428a-ae54-1614d8024e76"/>
+    </nova:instance>
+  </metadata>
+  <memory unit='KiB'>8290304</memory>
+  <currentMemory unit='KiB'>8290304</currentMemory>
+  <vcpu placement='static'>4</vcpu>
+  <cputune>
+    <shares>4096</shares>
+    <vcpupin vcpu='0' cpuset='1'/>
+    <vcpupin vcpu='1' cpuset='2'/>
+    <vcpupin vcpu='2' cpuset='13'/>
+    <vcpupin vcpu='3' cpuset='14'/>
+    <emulatorpin cpuset='1-2,13-14'/>
+  </cputune>
+  <numatune>
+    <memory mode='strict' nodeset='0-1'/>
+    <memnode cellid='0' mode='strict' nodeset='0'/>
+    <memnode cellid='1' mode='strict' nodeset='1'/>
+  </numatune>
+  <sysinfo type='smbios'>
+    <system>
+      <entry name='manufacturer'>Red Hat</entry>
+      <entry name='product'>OpenStack Compute</entry>
+      <entry name='version'>14.0.3-8.el7ost</entry>
+      <entry name='serial'>1b52c42f-b1eb-47f4-a8c0-9904d4735fc9</entry>
+      <entry name='uuid'>601da52c-450f-4d8e-b502-50151536fa69</entry>
+      <entry name='family'>Virtual Machine</entry>
+    </system>
+  </sysinfo>
+  <os>
+    <type arch='x86_64' machine='pc-i440fx-rhel7.3.0'>hvm</type>
+    <boot dev='hd'/>
+    <smbios mode='sysinfo'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+  </features>
+  <cpu mode='host-passthrough'>
+    <topology sockets='4' cores='1' threads='1'/>
+    <numa>
+      <cell id='0' cpus='0-1' memory='4145152' unit='KiB'/>
+      <cell id='1' cpus='2-3' memory='4145152' unit='KiB'/>
+    </numa>
+  </cpu>
+  <clock offset='utc'>
+    <timer name='pit' tickpolicy='delay'/>
+    <timer name='rtc' tickpolicy='catchup'/>
+    <timer name='hpet' present='no'/>
+  </clock>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/libexec/qemu-kvm</emulator>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2' cache='none'/>
+      <source file='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/disk'/>
+      <target dev='vda' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
+    </disk>
+    <disk type='block' device='disk'>
+      <driver name='qemu' type='raw' cache='none' io='native'/>
+      <source dev='/dev/disk/by-id/dm-uuid-mpath-3600a09803830316d512b4a6b38374143'/>
+      <target dev='vdb' bus='virtio'/>
+      <serial>dd036ba1-bb5f-4c1a-a369-a918080bff7f</serial>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
+    </disk>
+    <controller type='usb' index='0'>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x01' function='0x2'/>
+    </controller>
+    <controller type='pci' index='0' model='pci-root'/>
+    <interface type='bridge'>
+      <mac address='aa:aa:ee:bb:10:aa'/>
+      <source bridge='qbrf975e53f-e0'/>
+      <target dev='tapf975e53f-e0'/>
+      <model type='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+    </interface>
+    <serial type='file'>
+      <source path='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/console.log'/>
+      <target port='0'/>
+    </serial>
+    <serial type='pty'>
+      <target port='1'/>
+    </serial>
+    <console type='file'>
+      <source path='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/console.log'/>
+      <target type='serial' port='0'/>
+    </console>
+    <input type='tablet' bus='usb'>
+      <address type='usb' bus='0' port='1'/>
+    </input>
+    <input type='mouse' bus='ps2'/>
+    <input type='keyboard' bus='ps2'/>
+    <graphics type='vnc' port='-1' autoport='yes' listen='0.0.0.0' keymap='fr'>
+      <listen type='address' address='0.0.0.0'/>
+    </graphics>
+    <video>
+      <model type='cirrus' vram='16384' heads='1' primary='yes'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+    </video>
+    <memballoon model='virtio'>
+      <stats period='10'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0'/>
+    </memballoon>
+  </devices>
+</domain>
+""".strip()
+
+XML_RHOSP_2 = """
+<!--
+WARNING: THIS IS AN AUTO-GENERATED FILE. CHANGES TO IT ARE LIKELY TO BE
+OVERWRITTEN AND LOST. Changes to this xml configuration should be made using:
+  virsh edit instance-000008d6
+or other application using the libvirt API.
+-->
+
+<domain type='kvm'>
+  <name>instance-000008d7</name>
+  <uuid>601da52c-450f-4d8e-b502-503431536fa69</uuid>
+  <metadata>
+    <nova:instance xmlns:nova="http://openstack.org/xmlns/libvirt/nova/1.0">
+      <nova:package version="14.0.3-8.el7ost"/>
+      <nova:name>VM_AT_01</nova:name>
+      <nova:creationTime>2017-10-09 08:51:28</nova:creationTime>
+    </nova:instance>
+  </metadata>
+  <memory unit='KiB'>8290304</memory>
+  <currentMemory unit='KiB'>8290304</currentMemory>
+  <vcpu placement='static'>4</vcpu>
+  <cputune>
+    <shares>4096</shares>
+    <vcpupin vcpu='0' cpuset='1'/>
+    <vcpupin vcpu='1' cpuset='2'/>
+    <vcpupin vcpu='2' cpuset='13'/>
+    <vcpupin vcpu='3' cpuset='14'/>
+    <emulatorpin cpuset='1-2,13-14'/>
+  </cputune>
+  <numatune>
+    <memory mode='strict' nodeset='0-1'/>
+    <memnode cellid='0' mode='strict' nodeset='0'/>
+    <memnode cellid='1' mode='strict' nodeset='1'/>
+  </numatune>
+  <sysinfo type='smbios'>
+    <system>
+      <entry name='manufacturer'>Red Hat</entry>
+      <entry name='product'>OpenStack Compute</entry>
+      <entry name='version'>14.0.3-8.el7ost</entry>
+      <entry name='serial'>1b52c42f-b1eb-47f4-a8c0-9904d4735fc9</entry>
+      <entry name='uuid'>601da52c-450f-4d8e-b502-50151536fa69</entry>
+      <entry name='family'>Virtual Machine</entry>
+    </system>
+  </sysinfo>
+  <os>
+    <type arch='x86_64' machine='pc-i440fx-rhel7.3.0'>hvm</type>
+    <boot dev='hd'/>
+    <smbios mode='sysinfo'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+  </features>
+  <cpu mode='host-passthrough'>
+    <topology sockets='4' cores='1' threads='1'/>
+    <numa>
+      <cell id='0' cpus='0-1' memory='4145152' unit='KiB'/>
+      <cell id='1' cpus='2-3' memory='4145152' unit='KiB'/>
+    </numa>
+  </cpu>
+  <clock offset='utc'>
+    <timer name='pit' tickpolicy='delay'/>
+    <timer name='rtc' tickpolicy='catchup'/>
+    <timer name='hpet' present='no'/>
+  </clock>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/libexec/qemu-kvm</emulator>
+    <disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2' cache='none'/>
+      <source file='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/disk'/>
+      <target dev='vda' bus='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
+    </disk>
+    <disk type='block' device='disk'>
+      <driver name='qemu' type='raw' cache='none' io='native'/>
+      <source dev='/dev/disk/by-id/dm-uuid-mpath-3600a09803830316d512b4a6b38374143'/>
+      <target dev='vdb' bus='virtio'/>
+      <serial>dd036ba1-bb5f-4c1a-a369-a918080bff7f</serial>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
+    </disk>
+    <controller type='usb' index='0'>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x01' function='0x2'/>
+    </controller>
+    <controller type='pci' index='0' model='pci-root'/>
+    <interface type='bridge'>
+      <mac address='aa:aa:ee:bb:10:aa'/>
+      <source bridge='qbrf975e53f-e0'/>
+      <target dev='tapf975e53f-e0'/>
+      <model type='virtio'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+    </interface>
+    <serial type='file'>
+      <source path='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/console.log'/>
+      <target port='0'/>
+    </serial>
+    <serial type='pty'>
+      <target port='1'/>
+    </serial>
+    <console type='file'>
+      <source path='/var/lib/nova/instances/601da52c-450f-4d8e-b502-50151536fa69/console.log'/>
+      <target type='serial' port='0'/>
+    </console>
+    <input type='tablet' bus='usb'>
+      <address type='usb' bus='0' port='1'/>
+    </input>
+    <input type='mouse' bus='ps2'/>
+    <input type='keyboard' bus='ps2'/>
+    <graphics type='vnc' port='-1' autoport='yes' listen='0.0.0.0' keymap='fr'>
+      <listen type='address' address='0.0.0.0'/>
+    </graphics>
+    <video>
+      <model type='cirrus' vram='16384' heads='1' primary='yes'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+    </video>
+    <memballoon model='virtio'>
+      <stats period='10'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0'/>
+    </memballoon>
+  </devices>
+</domain>
+""".strip()
+
 
 def test_vm_xml():
     xml = qemu_xml.QemuXML(context_wrap(XML_NUMA, path='/etc/libvirt/qemu/vm.xml'))
@@ -428,8 +690,31 @@ def test_blank_xml():
     assert xml.parse_dom() == {}
 
 
+def test_rhosp_xml():
+    xml = qemu_xml.OpenStackInstanceXML(context_wrap(XML_RHOSP, path='/etc/libvirt/qemu/instance-000008d6.xml'))
+    assert xml.domain_name == 'instance-000008d6'
+    assert xml.nova.get('version') == '14.0.3-8.el7ost'
+    assert xml.nova.get('instance_name') == 'django_vm_001'
+    assert xml.nova.get('user') == 'django_user_01'
+    assert xml.nova.get('root_disk_type') == 'image'
+    assert xml.nova.get('flavor_vcpus') == '4'
+
+    rhosp_xml_2 = qemu_xml.OpenStackInstanceXML(context_wrap(XML_RHOSP_2, path='/etc/libvirt/qemu/instance-000008d7.xml'))
+    assert rhosp_xml_2.nova == {'version': '14.0.3-8.el7ost', 'instance_name': 'VM_AT_01', 'created': '2017-10-09 08:51:28'}
+    assert rhosp_xml_2.domain_name == 'instance-000008d7'
+
+    # XML not having OSP metadata
+    xml = qemu_xml.OpenStackInstanceXML(context_wrap(XML_NUMA, path='/etc/libvirt/qemu/numa_instance_001.xml'))
+    assert xml.domain_name == 'numa_instance_001'
+
+    # The XML struture is different from OSP instance XML
+    xml = qemu_xml.OpenStackInstanceXML(context_wrap(XML_IDM_CLIENT, path='/etc/libvirt/qemu/idm_001.xml'))
+    assert xml.domain_name == 'idm_001'
+
+
 def test_documentation():
-    env = {'xml_numa': qemu_xml.QemuXML(context_wrap(XML_NUMA, path='/etc/libvirt/qemu/vm.xml'))}
+    env = {'xml_numa': qemu_xml.QemuXML(context_wrap(XML_NUMA, path='/etc/libvirt/qemu/vm.xml')),
+           'rhosp_xml': qemu_xml.OpenStackInstanceXML(context_wrap(XML_RHOSP, path='/etc/libvirt/qemu/instance-000008d6.xml'))}
     failed_count, tests = doctest.testmod(qemu_xml, globs=env)
     assert failed_count == 0
 
