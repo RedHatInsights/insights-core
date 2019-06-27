13fcc91a350bbef49c0b3c2d4286f74e07a7ef2d
Bob Fahr <bfahr@redhat.com>
2019-06-26 21:02:26 -0500

    Update md5check to fix regex in spec (#2008)
    * Change spec to foreach_execute to fix regex problem identified
      in #1413
    * Change spec ref to multi_output type
    * Add combiner and tests combine parsers, similar to original parser
    * Remove non-functional spec prelink_orig_md5 and add specs
      combiner necessary files with existing spec
    
    Signed-off-by: Bob Fahr <bfahr@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -500,6 +500,9 @@ class DefaultSpecs(Specs):
     manila_conf = first_file(["/var/lib/config-data/puppet-generated/manila/etc/manila/manila.conf", "/etc/manila/manila.conf"])
     mariadb_log = simple_file("/var/log/mariadb/mariadb.log")
     max_uid = simple_command("/bin/awk -F':' '{ if($3 > max) max = $3 } END { print max }' /etc/passwd")
+    md5chk_files = foreach_execute(
+        ["/etc/pki/product/69.pem", "/etc/pki/product-default/69.pem", "/usr/lib/libsoftokn3.so", "/usr/lib64/libsoftokn3.so", "/usr/lib/libfreeblpriv3.so", "/usr/lib64/libfreeblpriv3.so"],
+        "/usr/bin/md5sum %s")
     mdstat = simple_file("/proc/mdstat")
     meminfo = first_file(["/proc/meminfo", "/meminfo"])
     messages = simple_file("/var/log/messages")
@@ -667,8 +670,6 @@ class DefaultSpecs(Specs):
                               glob_file("/database/postgresql-*.log")
                               ])
     puppetserver_config = simple_file("/etc/sysconfig/puppetserver")
-    md5chk_files = simple_command("/usr/bin/md5sum /dev/null /etc/pki/{product,product-default}/69.pem")
-    prelink_orig_md5 = None
     prev_uploader_log = simple_file("var/log/redhat-access-insights/redhat-access-insights.log.1")
     proc_snmp_ipv4 = simple_file("proc/net/snmp")
     proc_snmp_ipv6 = simple_file("proc/net/snmp6")
