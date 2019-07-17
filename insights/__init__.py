7d6bb9c36274af0df46a601e738d7695af197e8b
Chris Sams <csams@users.noreply.github.com>
2019-07-17 10:09:07 -0500

    Create a make_info response type for rules. (#2074)
    Fixes https://github.com/RedHatInsights/insights-core/issues/2073
    
    Signed-off-by: Christopher Sams <csams@redhat.com>
    (cherry picked from commit 886eebeb878995137e7e9f66e4629956c6ca6ebc)

--- insights/__init__.py
+++ insights/__init__.py
@@ -37,7 +37,7 @@ from .core.hydration import create_context
 from .core.plugins import combiner, fact, metadata, parser, rule  # noqa: F401
 from .core.plugins import datasource, condition, incident  # noqa: F401
 from .core.plugins import make_response, make_metadata, make_fingerprint  # noqa: F401
-from .core.plugins import make_pass, make_fail  # noqa: F401
+from .core.plugins import make_pass, make_fail, make_info  # noqa: F401
 from .core.filters import add_filter, apply_filters, get_filters  # noqa: F401
 from .core.serde import Hydration
 from .formats import get_formatter
