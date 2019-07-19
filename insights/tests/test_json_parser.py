ac7438de9a624cd3b307695b7dfe2de5c6019bb6
François Cami <fcami@users.noreply.github.com>
2019-07-19 17:29:49 +0200

    JSONParser: protect call to from_dict (#2056) (#2057)
    * JSONParser: remove call to from_dict (#2056)
    
    JSONParser can currently only parse JSON objects as calling
    from_dict will fail if the JSON text is an array.
    Remove 'self.doc = from_dict' as it seems unused.
    
    Signed-off-by: François Cami <fcami@redhat.com>
    
    * JSONParser: add test for when json text is an array (#2056)
    
    Signed-off-by: François Cami <fcami@redhat.com>
    (cherry picked from commit e5f91ebca074ec8ebde950798bf787a7c3d97d65)

--- insights/tests/test_json_parser.py
+++ insights/tests/test_json_parser.py
@@ -7,10 +7,17 @@ from insights.tests import context_wrap
 class MyJsonParser(JSONParser):
     pass
 
+json_test_strings = {
+    '{"a": "1", "b": "2"}': {'a': '1', 'b': '2'},
+    '[{"a": "1", "b": "2"},{"a": "3", "b": "4"}]':
+        [{'a': '1', 'b': '2'}, {'a': '3', 'b': '4'}]
+}
+
 
 def test_json_parser_success():
-    ctx = context_wrap('{"a": "2"}')
-    assert MyJsonParser(ctx)
+    for jsonstr in json_test_strings:
+        ctx = context_wrap(jsonstr)
+        assert MyJsonParser(ctx).data == json_test_strings[jsonstr]
 
 
 def test_json_parser_failure():
