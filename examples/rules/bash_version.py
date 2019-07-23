e4a4bf07deb8b214b9bd437ddad516d268570a3a
Chris Sams <csams@users.noreply.github.com>
2019-07-22 20:47:58 -0500

    Return the nvra instead of the InstalledRpm object. (#2101)
    Fixes #2100
    
    Signed-off-by: Christopher Sams <csams@redhat.com>
    (cherry picked from commit ab6d67f704329b0d7fc83768cdf5540d89adc03b)

--- examples/rules/bash_version.py
+++ examples/rules/bash_version.py
@@ -11,7 +11,7 @@ or from the examples/rules directory::
 
 $ python sample_rules.py
 """
-from insights.core.plugins import make_pass, rule
+from insights.core.plugins import make_info, rule
 from insights.parsers.installed_rpms import InstalledRpms
 
 KEY = "BASH_VERSION"
@@ -21,5 +21,5 @@ CONTENT = "Bash RPM Version: {{ bash_version }}"
 
 @rule(InstalledRpms)
 def report(rpms):
-    bash_ver = rpms.get_max('bash')
-    return make_pass(KEY, bash_version=bash_ver)
+    bash = rpms.get_max('bash')
+    return make_info(KEY, bash_version=bash.nvra)
