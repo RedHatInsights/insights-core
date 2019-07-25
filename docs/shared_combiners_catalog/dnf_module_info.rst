5f60efcfc385baab9df970ea76a4e10bb1361adf
Xiangce Liu <xiangceliu@redhat.com>
2019-07-25 17:02:13 +0800

    Add deps to specs and add doc
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

---
+++ docs/shared_combiners_catalog/dnf_module_info.rst
@@ -0,0 +1,3 @@
+.. automodule:: insights.combiners.dnf_module_info
+    :members:
+    :show-inheritance:
--- insights/specs/default.py
+++ insights/specs/default.py
@@ -27,6 +27,7 @@ from insights.core.spec_factory import first_file, listdir
 from insights.parsers.mount import Mount, ProcMounts
 from insights.parsers.dnf_module import DnfModuleList
 from insights.combiners.cloud_provider import CloudProvider
+from insights.components.rhel_version import IsRhel8
 from insights.specs import Specs
 
 from grp import getgrgid
@@ -227,7 +228,7 @@ class DefaultSpecs(Specs):
     dmidecode = simple_command("/usr/sbin/dmidecode")
     dmsetup_info = simple_command("/usr/sbin/dmsetup info -C")
     dnf_modules = glob_file("/etc/dnf/modules.d/*.module")
-    dnf_module_list = simple_command("/usr/bin/dnf -C --noplugins module list")
+    dnf_module_list = simple_command("/usr/bin/dnf -C --noplugins module list", deps=IsRhel8)
 
     @datasource(DnfModuleList)
     def dnf_module_names(broker):
@@ -236,7 +237,7 @@ class DefaultSpecs(Specs):
             return [m for m in dml]
         raise SkipComponent()
 
-    dnf_module_info = foreach_execute(dnf_module_names, "/usr/bin/dnf -C --noplugins module info %s")
+    dnf_module_info = foreach_execute(dnf_module_names, "/usr/bin/dnf -C --noplugins module info %s", deps=IsRhel8)
     dnsmasq_config = glob_file(["/etc/dnsmasq.conf", "/etc/dnsmasq.d/*.conf"])
     docker_info = simple_command("/usr/bin/docker info")
     docker_list_containers = simple_command("/usr/bin/docker ps --all --no-trunc")
