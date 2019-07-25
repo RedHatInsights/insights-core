45befa61374c5bd90f2b9e559cfe95d4f7994b46
“Saloni <sagarg@redhat.com>
2019-07-25 14:59:15 +0530

    Fixed issues in EtcdConf parser
    Signed-off-by: Saloni <sagarg@redhat.com>

--- insights/parsers/etcd_conf.py
+++ insights/parsers/etcd_conf.py
@@ -1,70 +1,88 @@
 """
-ETCD Configuration Files
-========================
-Parsing and extracting data from output of config file ``/etc/etcd/etcd.conf``.
-
-Parsers provided by this module are:
 EtcdConf - file ``/etc/etcd/etcd.conf``
----------------------------------------
+=======================================
+
+The EtcdConf class parses the file ``/etc/etcd/etcd.conf``. The
+etcd.conf is in the standard 'ini' format and is read by the base
+parser class `IniConfigFile`.
+
+Typical contents of the file look like::
+
+ETCD_NAME=f05-h19-000-1029p.rdu2.scalelab.redhat.com
+ETCD_LISTEN_PEER_URLS=https://10.1.40.235:2380
+ETCD_DATA_DIR=/var/lib/etcd/
+#ETCD_WAL_DIR=
+#ETCD_SNAPSHOT_COUNT=10000
+ETCD_HEARTBEAT_INTERVAL=500
+ETCD_ELECTION_TIMEOUT=2500
+ETCD_LISTEN_CLIENT_URLS=https://10.1.40.235:2379
+#ETCD_MAX_SNAPSHOTS=5
+#ETCD_MAX_WALS=5
+#ETCD_CORS=
+
+
+#[cluster]
+ETCD_INITIAL_ADVERTISE_PEER_URLS=https://10.1.40.235:2380
+ETCD_INITIAL_CLUSTER=f05-h19-000-1029p.rdu2.scalelab.redhat.com=https://10.1.40.235:2380
+ETCD_INITIAL_CLUSTER_STATE=new
+ETCD_INITIAL_CLUSTER_TOKEN=etcd-cluster-1
+#ETCD_DISCOVERY=
+#ETCD_DISCOVERY_SRV=
+#ETCD_DISCOVERY_FALLBACK=proxy
+#ETCD_DISCOVERY_PROXY=
+ETCD_ADVERTISE_CLIENT_URLS=https://10.1.40.235:2379
+#ETCD_STRICT_RECONFIG_CHECK=false
+#ETCD_AUTO_COMPACTION_RETENTION=0
+#ETCD_ENABLE_V2=true
+ETCD_QUOTA_BACKEND_BYTES=8589934592
+
+#[proxy]
+#ETCD_PROXY=off
+#ETCD_PROXY_FAILURE_WAIT=5000
+#ETCD_PROXY_REFRESH_INTERVAL=30000
+#ETCD_PROXY_DIAL_TIMEOUT=1000
+#ETCD_PROXY_WRITE_TIMEOUT=5000
+#ETCD_PROXY_READ_TIMEOUT=0
+
+#[security]
+ETCD_TRUSTED_CA_FILE=/etc/etcd/ca.crt
+ETCD_CLIENT_CERT_AUTH=true
+ETCD_CERT_FILE=/etc/etcd/server.crt
+ETCD_KEY_FILE=/etc/etcd/server.key
+#ETCD_AUTO_TLS=false
+ETCD_PEER_TRUSTED_CA_FILE=/etc/etcd/ca.crt
+ETCD_PEER_CLIENT_CERT_AUTH=true
+ETCD_PEER_CERT_FILE=/etc/etcd/peer.crt
+ETCD_PEER_KEY_FILE=/etc/etcd/peer.key
+#ETCD_PEER_AUTO_TLS=false
+
+#[logging]
+ETCD_DEBUG=False
+
+#[profiling]
+#ETCD_ENABLE_PPROF=false
+#ETCD_METRICS=basic
+#
+[auth]
+ETCD_AUTH_TOKEN=simple
+
+Examples:
+
+    >>> conf.get('auth', 'ETCD_AUTH_TOKEN')
+    u'simple'
+
+    >>> conf.has_option('member', 'ETCD_NAME')
+    True
 """
 
-from .. import Parser, parser, get_active_lines
-from ..parsers import split_kv_pairs
+
+from .. import IniConfigFile, parser, add_filter
 from insights.specs import Specs
-from insights.parsers import ParseException, SkipException
+
+add_filter(Specs.etcd_conf, ["["])
+
 
 @parser(Specs.etcd_conf)
-class EtcdConf(Parser):
-    """
-    Parser for configuration for ETCD.
-
-    Typical Output looks like::
-
-        ETCD_NAME=f05-h19-000-1029p.rdu2.scalelab.redhat.com
-        ETCD_LISTEN_PEER_URLS=https://10.1.40.235:2380
-        ETCD_DATA_DIR=/var/lib/etcd/
-        #ETCD_WAL_DIR=
-        #ETCD_SNAPSHOT_COUNT=10000
-        ETCD_HEARTBEAT_INTERVAL=500
-        ETCD_ELECTION_TIMEOUT=2500   
-    
-    Examples: 
-        >>> result.get_active_setting_value('ETCD_DATA_DIR') == '/var/lib/etcd/'
-        True
-
-        >>> result.get_active_setting_value('ETCD_DATA_DIR')
-        '/var/lib/etcd/'
-        
-        >>> result.get_active_setting_value('ETCD_HEARTBEAT_INTERVAL') == '500'
-        True
-
-        >>> result.get_active_setting_value('ETCD_ELECTION_TIMEOUT') == '500'
-        False
-    """
-
-    def __init__(self, *args, **kwargs):
-        self.active_lines_unparsed = []
-        self.active_settings = {}
-        super(EtcdConf, self).__init__(*args, **kwargs)
-
-    def parse_content(self, content):
-        """
-        Main parsing class method which stores all interesting data from the content.
-        Args:
-            content (context.content): Parser context content
-        """
-
-        if not content:
-            raise SkipException("Content is empty.")
-
-        self.active_lines_unparsed = get_active_lines(content)
-        self.active_settings = split_kv_pairs(content, use_partition=False)
-
-    def get_active_setting_value(self, setting_name):
-        """
-        Access active setting value by setting name.
-        Args:
-            setting_name (string): Setting name
-        """
-        return self.active_settings[setting_name]
-    
+class EtcdConf(IniConfigFile):
+    """Class for etcd.conf file content."""
+    pass
--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -130,7 +130,7 @@ class Specs(SpecSet):
     etc_journald_conf_d = RegistryPoint(multi_output=True)
     etc_journald_conf = RegistryPoint()
     etc_machine_id = RegistryPoint()
-    etcd_conf = RegistryPoint()
+    etcd_conf = RegistryPoint(filterable=True)
     ethernet_interfaces = RegistryPoint()
     ethtool_a = RegistryPoint(multi_output=True)
     ethtool_c = RegistryPoint(multi_output=True)
