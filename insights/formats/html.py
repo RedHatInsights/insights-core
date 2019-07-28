8604c057b9631f02f9941ef203f17072aed8f8d2
Christopher Sams <csams@redhat.com>
2019-07-27 21:50:05 -0500

    Fix whitespace and pre tag.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- insights/formats/html.py
+++ insights/formats/html.py
@@ -27,89 +27,89 @@ class HtmlFormat(Formatter):
     """
 
     CONTENT = """
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        <title>{{root}}</title>
-      </head>
-      <body>
-        <div class="container">
-          <p>
-            <h2>Analysis of {{root}}</h2>
-            <h4>Performed at {{start_time}} UTC</h4>
-          </p>
-          <div class="card">
-            <div class="card-header">System Information</div>
-            <div class="card-body">
-            <pre>
+    <title>{{root}}</title>
+  </head>
+  <body>
+    <div class="container">
+      <p>
+        <h2>Analysis of {{root}}</h2>
+        <h4>Performed at {{start_time}} UTC</h4>
+      </p>
+      <div class="card">
+        <div class="card-header">System Information</div>
+        <div class="card-body">
+        <pre>
 {% for rule in rules.make_info %}
 {{-rule.body}}
 {% endfor -%}
-            </pre
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-header">Rule Results</div>
-            <div class="card-body">
-              <div class="accordion" id="ruleAccordion">
-              {%- for group, results in rules.items() %}
-              {%- if group != "make_info" %}
-              {%- for rule in results %}
-                <div class="card">
-                  <div class="card-header bg-{% if group == "make_pass" %}success{% else %}danger{% endif %}" id="heading_{{rule.id}}">
-                    <h2 class="mb-0">
-                      <button class="btn btn-{% if group == "make_pass" %}success{% else %}danger{% endif %} text-white" type="button" data-toggle="collapse" data-target="#{{rule.id}}" aria-expanded="true" aria-controls="{{rule.id}}">
-                      {{rule.name}}
-                      </button>
-                    </h2>
-                  </div>
-                  <div id="{{rule.id}}" class="collapse" aria-labelledby="heading_{{rule.id}}" data-parent="#ruleAccordion">
-                    <div class="card-body">
-                    <pre>
+        </pre>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">Rule Results</div>
+        <div class="card-body">
+          <div class="accordion" id="ruleAccordion">
+          {%- for group, results in rules.items() %}
+          {%- if group != "make_info" %}
+          {%- for rule in results %}
+            <div class="card">
+              <div class="card-header bg-{% if group == "make_pass" %}success{% else %}danger{% endif %}" id="heading_{{rule.id}}">
+                <h2 class="mb-0">
+                  <button class="btn btn-{% if group == "make_pass" %}success{% else %}danger{% endif %} text-white" type="button" data-toggle="collapse" data-target="#{{rule.id}}" aria-expanded="true" aria-controls="{{rule.id}}">
+                  {{rule.name}}
+                  </button>
+                </h2>
+              </div>
+              <div id="{{rule.id}}" class="collapse" aria-labelledby="heading_{{rule.id}}" data-parent="#ruleAccordion">
+                <div class="card-body">
+                <pre>
 {{rule.body}}
-                    </pre>
-                    <hr />
-                    Contributing data:
-                    <ol>
-                    {% for d in rule.datasources %}
-                      <li>
-                      {{d}}
-                      </li>
-                    {% endfor %}
-                    </ol>
-                    <hr />
-              Documentation:
-                    <pre>
+                </pre>
+                <hr />
+                Contributing data:
+                <ol>
+                {% for d in rule.datasources %}
+                  <li>
+                  {{d}}
+                  </li>
+                {%- endfor %}
+                </ol>
+                <hr />
+          Documentation:
+                <pre>
 {{rule.mod_doc}}
 {{rule.rule_doc}}
-                    </pre>
-                    <hr />
-                    Rule source: {{rule.rule_path}}
-                    </div>
-                  </div>
+                </pre>
+                <hr />
+                Rule source: {{rule.rule_path}}
                 </div>
-              {%- endfor %}
-              {% endif %}
-              {% endfor %}
               </div>
             </div>
+          {%- endfor %}
+          {% endif %}
+          {% endfor %}
           </div>
         </div>
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-      </body>
-    </html>
-    """
+      </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+    """.strip()
 
     def find_root(self):
         """
