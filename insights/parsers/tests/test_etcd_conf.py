2a96d69b7aab0c521d41af5d9ede1269fdc03554
Saloni Garg <sagarg@redhat.com>
2019-07-31 05:36:06 +0530

    Fixed Etcd conf parser (#2111)
    * Add Parser for /etc/etcd/etcd.conf
    
    Signed-off-by: “Saloni <salonigarg2310@gmail.com>
    Signed-off-by: “Saloni <“sagarg@redhat.com”>
    Signed-off-by: Saloni Garg <sagarg@redhat.com>
    Signed-off-by: Saloni <sagarg@redhat.com>
    
    * Fixed issues in EtcdConf parser
    
    Signed-off-by: Saloni <sagarg@redhat.com>
    
    * Added .rst file for Shared Catalogue
    
    Signed-off-by: Saloni <sagarg@redhat.com>
    
    * Fixed pytest3 error
    
    Signed-off-by: Saloni <sagarg@redhat.com>
    
    * Minor fixes in etcd_conf.py and etcd_conf.rst
    
    Signed-off-by: “Saloni <“sagarg@redhat.com”>
    
    * Fixed doc indentation
    
    Signed-off-by: “Saloni <“sagarg@redhat.com”>
    (cherry picked from commit f536553003abaa77a911cdf94ad43935f7346d64)

---
+++ insights/parsers/tests/test_etcd_conf.py
@@ -0,0 +1,38 @@
+import pytest
+import doctest
+from insights.parsers import etcd_conf, SkipException
+from insights.tests import context_wrap
+from insights.parsers.etcd_conf import EtcdConf
+
+ETCD_CONF = """
+
+[member]
+ETCD_NAME=f05-h19-000-1029p.rdu2.scalelab.redhat.com
+ETCD_LISTEN_PEER_URLS=https://10.1.40.235:2380
+ETCD_DATA_DIR=/var/lib/etcd/
+#ETCD_WAL_DIR=
+ETCD_SNAPSHOT_COUNT=10000
+ETCD_HEARTBEAT_INTERVAL=500
+
+[auth]
+ETCD_AUTH_TOKEN=simple
+
+""".strip()
+
+
+def test_etcd_conf_empty():
+    with pytest.raises(SkipException):
+        assert etcd_conf.EtcdConf(context_wrap('')) is None
+
+
+def test_etcd_conf():
+    conf = EtcdConf(context_wrap(ETCD_CONF))
+    assert conf.has_option('member', 'ETCD_HEARTBEAT_INTERVAL') is True
+
+
+def test_etcd_conf_documentation():
+    failed_count, tests = doctest.testmod(
+        etcd_conf,
+        globs={'conf': EtcdConf(context_wrap(ETCD_CONF))}
+    )
+    assert failed_count == 0
