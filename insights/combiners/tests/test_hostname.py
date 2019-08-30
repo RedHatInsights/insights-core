2b2a1d5fa3ff39c56084d1b44a7fd6a6518bacde
Xiangce Liu <xiangceliu@redhat.com>
2019-08-30 14:47:52 +0800

    Add more Hostname parsers
    - update the Hostname Combiner as well
    - Sap Combiner is udpated too
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/tests/test_hostname.py
+++ insights/combiners/tests/test_hostname.py
@@ -1,12 +1,15 @@
 import pytest
-from insights.parsers.hostname import Hostname
+import doctest
+from insights.parsers.hostname import Hostname as HnF, HostnameShort as HnS, HostnameDefault as HnD
 from insights.parsers.facter import Facter
 from insights.parsers.systemid import SystemID
-from insights.combiners.hostname import hostname
+from insights.combiners import hostname
+from insights.combiners.hostname import Hostname, hostname as hn_func
 from insights.tests import context_wrap
 
-HOSTNAME = "rhel7.example.com"
+HOSTNAME_FULL = "rhel7.example.com"
 HOSTNAME_SHORT = "rhel7"
+HOSTNAME_DEF = "rhel7"
 FACTS_FQDN = """
 COMMAND> facter
 
@@ -67,45 +70,68 @@ SYSTEMID_NO_PROFILE_NAME = '''
 
 
 def test_get_hostname():
-    hn = Hostname(context_wrap(HOSTNAME))
-    expected = (HOSTNAME, HOSTNAME_SHORT, 'example.com')
-    result = hostname(hn, None, None)
+    hnf = HnF(context_wrap(HOSTNAME_FULL))
+    expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
+    result = Hostname(hnf, None, None, None, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
-    hn = Hostname(context_wrap(HOSTNAME_SHORT))
+    hns = HnS(context_wrap(HOSTNAME_SHORT))
     expected = (HOSTNAME_SHORT, HOSTNAME_SHORT, '')
-    result = hostname(hn, None, None)
+    result = Hostname(None, None, hns, None, None)
+    assert result.fqdn == expected[0]
+    assert result.hostname == expected[1]
+    assert result.domain == expected[2]
+
+    hnd = HnD(context_wrap(HOSTNAME_DEF))
+    expected = (HOSTNAME_DEF, HOSTNAME_DEF, '')
+    result = Hostname(None, hnd, None, None, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
 
 def test_get_facter_hostname():
-    hn = Facter(context_wrap(FACTS_FQDN))
+    ft = Facter(context_wrap(FACTS_FQDN))
     expected = ('ewa-satellite.example.com', 'ewa-satellite', 'example.com')
-    result = hostname(None, hn, None)
+    result = Hostname(None, None, None, ft, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
 
 def test_get_systemid_hostname():
-    hn = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
+    sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
     expected = ('example_profile', 'example_profile', '')
-    result = hostname(None, None, hn)
+    result = Hostname(None, None, None, None, sid)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
 
 def test_get_all_hostname():
-    hn = Hostname(context_wrap(HOSTNAME))
-    fhn = Facter(context_wrap(FACTS_FQDN))
-    shn = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
-    expected = (HOSTNAME, HOSTNAME_SHORT, 'example.com')
-    result = hostname(hn, fhn, shn)
+    hnf = HnF(context_wrap(HOSTNAME_FULL))
+    hns = HnS(context_wrap(HOSTNAME_SHORT))
+    hnd = HnD(context_wrap(HOSTNAME_DEF))
+    ft = Facter(context_wrap(FACTS_FQDN))
+    sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
+    expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
+    result = Hostname(hnf, hnd, hns, ft, sid)
+    assert result.fqdn == expected[0]
+    assert result.hostname == expected[1]
+    assert result.domain == expected[2]
+
+
+def test_hostname_function():
+    # can be removed once the function being removed
+    hnf = HnF(context_wrap(HOSTNAME_FULL))
+    hns = HnS(context_wrap(HOSTNAME_SHORT))
+    hnd = HnD(context_wrap(HOSTNAME_DEF))
+    ft = Facter(context_wrap(FACTS_FQDN))
+    sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
+    expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
+    result = hn_func(hnf, hnd, hns, ft, sid)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
@@ -113,5 +139,19 @@ def test_get_all_hostname():
 
 def test_hostname_raise():
     with pytest.raises(Exception):
-        hn = Hostname(context_wrap(""))
-        hostname(hn, None, None)
+        sid = SystemID(context_wrap(SYSTEMID_NO_PROFILE_NAME))
+        Hostname(None, None, None, None, sid)
+
+
+def test_hostname_doc():
+    hnf = HnF(context_wrap(HOSTNAME_FULL))
+    hns = HnS(context_wrap(HOSTNAME_SHORT))
+    hnd = HnD(context_wrap(HOSTNAME_DEF))
+    ft = Facter(context_wrap(FACTS_FQDN))
+    sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
+    env = {
+            'hostname': Hostname(hnf, hnd, hns, ft, sid),
+            'hn': hn_func(hnf, hnd, hns, ft, sid)
+          }
+    failed, total = doctest.testmod(hostname, globs=env)
+    assert failed == 0
--- insights/parsers/hostname.py
+++ insights/parsers/hostname.py
@@ -1,7 +1,17 @@
 """
-hostname - command ``/bin/hostname``
-====================================
+Hostname - command ``hostname``
+===============================
 
+Parsers contained in this module are:
+
+Hostname - command ``hostname -f``
+----------------------------------
+
+HostnameDefault - command ``hostname``
+--------------------------------------
+
+HostnameShort - command ``hostname -s``
+---------------------------------------
 """
 
 from insights.parsers import ParseException
@@ -12,109 +22,87 @@ from insights.specs import Specs
 class HostnameBase(CommandParser):
     """
     The base parser class for command ``hostname``.
+
+    Raises:
+        ParseException: When the output contains multiple non-empty lines.
     """
     def parse_content(self, content):
         content = list(filter(None, content))
         if len(content) != 1:
             msg = "hostname data contains multiple non-empty lines"
             raise ParseException(msg)
+        self.raw = content[0].strip()
+        self.hostname = self.raw.split(".")[0] if self.raw else None
 
 
 @parser(Specs.hostname)
 class Hostname(HostnameBase):
     """
-    This parser simply reads the output of ``/bin/hostname``, which is the
+    This parser simply reads the output of ``hostname -f``, which is the
     configured fully qualified domain name of the client system.  It then
     splits it into ``hostname`` and ``domain`` and stores these as attributes,
     along with the unmodified name in the ``fqdn`` attribute.
 
     Examples:
-
-    >>> hostname.fqdn
-    'www.example.com'
-    >>> hostname.hostname
-    'www'
-    >>> hostname.domain
-    'example.com'
+        >>> hostname.raw
+        'rhel7.example.com'
+        >>> hostname.fqdn
+        'rhel7.example.com'
+        >>> hostname.hostname
+        'rhel7'
+        >>> hostname.domain
+        'example.com'
 
     Attributes:
+        raw: The raw output of the ``hostname -f`` command.
         fqdn: The fully qualified domain name of the host. The same to
             ``hostname`` when domain part is not set.
         hostname: The hostname.
-        domain: The domain get from the fqdn.
+        domain: The domain get from the ``fqdn``.
     """
     def parse_content(self, content):
         super(Hostname, self).parse_content(content)
-        raw = content[0].strip()
-        self.fqdn = raw
-        self.hostname = raw.split(".")[0] if raw else None
-        self.domain = ".".join(raw.split(".")[1:]) if raw else None
+        self.fqdn = self.raw
+        self.domain = ".".join(self.raw.split(".")[1:]) if self.raw else None
 
-    def __str__(self):
-        return "<hostname: {h}, domain: {d}>".format(h=self.hostname, d=self.domain)
+    def __repr__(self):
+        return "<raw: {r}, hostname: {h}, domain: {d}>".format(r=self.raw, h=self.hostname, d=self.domain)
 
 
 @parser(Specs.hostname_default)
 class HostnameDefault(HostnameBase):
     """
-    This parser simply reads the output of ``/bin/hostname``, which is the
-    configured fully qualified domain name of the client system.  It then
-    splits it into ``hostname`` and ``domain`` and stores these as attributes,
-    along with the unmodified name in the ``fqdn`` attribute.
+    This parser simply reads the output of ``hostname``.
 
     Examples:
-
-    >>> hostname.fqdn
-    'www.example.com'
-    >>> hostname.hostname
-    'www'
-    >>> hostname.domain
-    'example.com'
+        >>> hostname_def.raw
+        'rhel7'
+        >>> hostname_def.hostname
+        'rhel7'
 
     Attributes:
+        raw: The raw output of the ``hostname`` command.
         hostname: The hostname.
     """
-    def parse_content(self, content):
-        super(Hostname, self).parse_content(content)
-        raw = content[0].strip()
-        self.hostname = raw.split(".")[0] if raw else None
-
-    def __str__(self):
-        return "<hostname: {h}, domain: {d}>".format(h=self.hostname, d=self.domain)
+    def __repr__(self):
+        return "<raw: {r}, hostname: {h}>".format(r=self.raw, h=self.hostname)
 
 
 @parser(Specs.hostname_short)
 class HostnameShort(HostnameBase):
     """
-    This parser simply reads the output of ``/bin/hostname``, which is the
-    configured fully qualified domain name of the client system.  It then
-    splits it into ``hostname`` and ``domain`` and stores these as attributes,
-    along with the unmodified name in the ``fqdn`` attribute.
+    This parser simply reads the output of ``hostname -s``, which is the
+    configured short hostname of the client system.
 
     Examples:
-
-    >>> hostname.fqdn
-    'www.example.com'
-    >>> hostname.hostname
-    'www'
-    >>> hostname.domain
-    'example.com'
+        >>> hostname_s.raw
+        'rhel7'
+        >>> hostname_s.hostname
+        'rhel7'
 
     Attributes:
-        fqdn: The fully qualified domain name of the host. The same to
-            ``hostname`` when domain part is not set.
+        raw: The raw output of the ``hostname -s`` command.
         hostname: The hostname.
-        domain: The domain get from the fqdn.
     """
-    def parse_content(self, content):
-        content = list(filter(None, content))
-        if len(content) != 1:
-            msg = "hostname data contains multiple non-empty lines"
-            raise ParseException(msg)
-        raw = content[0].strip()
-        self.fqdn = raw
-        self.hostname = raw.split(".")[0] if raw else None
-        self.domain = ".".join(raw.split(".")[1:]) if raw else None
-
-    def __str__(self):
-        return "<hostname: {h}, domain: {d}>".format(h=self.hostname, d=self.domain)
+    def __repr__(self):
+        return "<raw: {r}, hostname: {h}>".format(r=self.raw, h=self.hostname)
