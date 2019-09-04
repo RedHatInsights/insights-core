f97b8d0a88754068c84196b93ba5bbe565c75c9f
Xiangce Liu <xiangceliu@redhat.com>
2019-09-04 16:13:15 +0800

    New parsers for "hostname" and "hostname -s" (#2170)
    * new parsers for hostname
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Add more Hostname parsers
    
    - update the Hostname Combiner as well
    - Sap Combiner is udpated too
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * fix flake8 errors
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update the return type of hostname
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update the hostname in sos_archive
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/hostname.py
+++ insights/combiners/hostname.py
@@ -2,32 +2,92 @@
 Hostname
 ========
 
-Combiner for ``hostname`` information. It uses the results of the
-``Hostname`` parser, ``Facter`` and the ``SystemID`` parser to get the fqdn,
+Combiner for ``hostname`` information. It uses the results of all the
+``Hostname`` parsers, ``Facter`` and the ``SystemID`` parser to get the fqdn,
 hostname and domain information.
 
-Examples:
-    >>> hn = shared[Hostname]
-    >>> hn.fqdn
-    rhel7.redhat.com
-    >>> hn.hostname
-    rhel7
-    >>> hn.domain
-    redhat.com
-    >>> hn
-    Hostname(fqdn='rhel7.redhat.com', hostname='rhel7', domain='redhat.com')
 
 """
 
-from collections import namedtuple
 from insights.core.plugins import combiner
 from insights.core.serde import deserializer, serializer
-from insights.parsers.hostname import Hostname as hname
-from insights.parsers.facter import Facter as facter
-from insights.parsers.systemid import SystemID as systemid
+from insights.parsers.hostname import Hostname as HnF, HostnameShort as HnS, HostnameDefault as HnD
+from insights.parsers.facter import Facter
+from insights.parsers.systemid import SystemID
+from insights.util import deprecated
 
-Hostname = namedtuple("Hostname", field_names=["fqdn", "hostname", "domain"])
-"""namedtuple: Type for storing the hostname information."""
+
+@combiner([HnF, HnD, HnS, Facter, SystemID])
+class Hostname(object):
+    """
+    Check hostname, facter and systemid to get the fqdn, hostname and domain.
+
+    Prefer hostname to facter and systemid.
+
+    Examples:
+        >>> type(hostname)
+        <class 'insights.combiners.hostname.Hostname'>
+        >>> hostname.fqdn
+        'rhel7.example.com'
+        >>> hostname.hostname
+        'rhel7'
+        >>> hostname.domain
+        'example.com'
+
+    Raises:
+        Exception: If no hostname can be found in any of the source parsers.
+    """
+    def __init__(self, hf, hd, hs, ft, sid):
+        self.fqdn = self.hostname = self.domain = None
+
+        if hf or hs or hd or ft:
+            hn = hf or hs or hd or ft
+            self.hostname = self.fqdn = hn.hostname
+            self.domain = ''
+            if hf and hf.fqdn:
+                self.fqdn = hf.fqdn
+                self.domain = hf.domain
+            elif ft and ft.fqdn:
+                self.fqdn = ft.fqdn
+                self.domain = ft.domain if ft.domain else ".".join(self.fqdn.split(".")[1:])
+        else:
+            self.fqdn = sid.get("profile_name")
+            if self.fqdn:
+                self.hostname = self.fqdn.split(".")[0]
+                self.domain = ".".join(self.fqdn.split(".")[1:])
+
+        if not self.hostname or not self.fqdn:
+            raise Exception("Unable to get hostname.")
+
+
+@combiner([HnF, HnD, HnS, Facter, SystemID])
+def hostname(hf, hd, hs, ft, sid):
+    """
+    .. warning::
+        This combiner methode is deprecated, please use
+        :py:class:`insights.combiners.hostname.Hostname` instead.
+
+    Check hostname, facter and systemid to get the fqdn, hostname and domain.
+
+    Prefer hostname to facter and systemid.
+
+    Examples:
+        >>> hn.fqdn
+        'rhel7.example.com'
+        >>> hn.hostname
+        'rhel7'
+        >>> hn.domain
+        'example.com'
+
+    Returns:
+        insights.combiners.hostname.Hostname: A class with `fqdn`,
+        `hostname` and `domain` attributes.
+
+    Raises:
+        Exception: If no hostname can be found in any of the source parsers.
+    """
+    deprecated(hostname, "Use the `Hostname` class instead.")
+    return Hostname(hf, hd, hs, ft, sid)
 
 
 @serializer(Hostname)
@@ -38,35 +98,3 @@ def serialize(obj, root=None):
 @deserializer(Hostname)
 def deserialize(_type, data, root=None):
     return Hostname(**data)
-
-
-@combiner([hname, facter, systemid])
-def hostname(hn, ft, si):
-    """Check hostname, facter and systemid to get the fqdn, hostname and domain.
-
-    Prefer hostname to facter and systemid.
-
-    Returns:
-        insights.combiners.hostname.Hostname: A named tuple with `fqdn`,
-        `hostname` and `domain` components.
-
-    Raises:
-        Exception: If no hostname can be found in any of the three parsers.
-    """
-
-    if not hn or not hn.fqdn:
-        hn = ft
-
-    if hn and hn.fqdn:
-        fqdn = hn.fqdn
-        hostname = hn.hostname if hn.hostname else fqdn.split(".")[0]
-        domain = hn.domain if hn.domain else ".".join(fqdn.split(".")[1:])
-        return Hostname(fqdn, hostname, domain)
-    else:
-        fqdn = si.get("profile_name") if si else None
-        if fqdn:
-            hostname = fqdn.split(".")[0]
-            domain = ".".join(fqdn.split(".")[1:])
-            return Hostname(fqdn, hostname, domain)
-
-    raise Exception("Unable to get hostname.")
--- insights/parsers/tests/test_hostname.py
+++ insights/parsers/tests/test_hostname.py
@@ -1,9 +1,11 @@
 import pytest
-from insights.parsers.hostname import Hostname
+import doctest
+from insights.parsers import hostname
+from insights.parsers.hostname import Hostname, HostnameShort, HostnameDefault
 from insights.tests import context_wrap
 
-HOSTNAME = "rhel7.example.com"
-HOSTNAME_MULTILINE = """
+HOSTNAME_FULL = "rhel7.example.com"
+HOSTNAME_FULL_MULTILINE = """
 rhel7.example.com
 """
 
@@ -14,35 +16,60 @@ rhel7
 
 
 def test_full_hostname():
-    data = Hostname(context_wrap(HOSTNAME))
+    data = Hostname(context_wrap(HOSTNAME_FULL))
     assert data.fqdn == "rhel7.example.com"
+    assert data.raw == "rhel7.example.com"
     assert data.hostname == "rhel7"
     assert data.domain == "example.com"
-    assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
+    assert "{0}".format(data) == "<raw: rhel7.example.com, hostname: rhel7, domain: example.com>"
 
 
 def test_full_multiline_hostname():
-    data = Hostname(context_wrap(HOSTNAME_MULTILINE, strip=False))
+    data = Hostname(context_wrap(HOSTNAME_FULL_MULTILINE, strip=False))
     assert data.fqdn == "rhel7.example.com"
     assert data.hostname == "rhel7"
     assert data.domain == "example.com"
-    assert "{0}".format(data) == "<hostname: rhel7, domain: example.com>"
+    assert "{0}".format(data) == "<raw: rhel7.example.com, hostname: rhel7, domain: example.com>"
 
 
 def test_short_hostname():
-    data = Hostname(context_wrap(HOSTNAME_SHORT))
-    assert data.fqdn == "rhel7"
+    data = HostnameShort(context_wrap(HOSTNAME_SHORT))
+    assert data.raw == "rhel7"
     assert data.hostname == "rhel7"
-    assert data.domain == ""
+    assert "{0}".format(data) == "<raw: rhel7, hostname: rhel7>"
 
 
 def test_short_multiline_hostname():
-    data = Hostname(context_wrap(HOSTNAME_SHORT_MULTILINE, strip=False))
-    assert data.fqdn == "rhel7"
+    data = HostnameShort(context_wrap(HOSTNAME_SHORT_MULTILINE, strip=False))
+    assert data.raw == "rhel7"
     assert data.hostname == "rhel7"
-    assert data.domain == ""
+    assert "{0}".format(data) == "<raw: rhel7, hostname: rhel7>"
+
+
+def test_default_hostname():
+    data = HostnameDefault(context_wrap(HOSTNAME_SHORT))
+    assert data.raw == "rhel7"
+    assert data.hostname == "rhel7"
+    assert "{0}".format(data) == "<raw: rhel7, hostname: rhel7>"
+
+
+def test_default_multiline_hostname():
+    data = HostnameDefault(context_wrap(HOSTNAME_SHORT_MULTILINE, strip=False))
+    assert data.raw == "rhel7"
+    assert data.hostname == "rhel7"
+    assert "{0}".format(data) == "<raw: rhel7, hostname: rhel7>"
 
 
 def test_empty_hostname():
     with pytest.raises(Exception):
         Hostname(context_wrap(""))
+
+
+def test_hostname_doc():
+    env = {
+            'hostname': Hostname(context_wrap(HOSTNAME_FULL)),
+            'hostname_def': HostnameDefault(context_wrap(HOSTNAME_SHORT)),
+            'hostname_s': HostnameShort(context_wrap(HOSTNAME_SHORT)),
+          }
+    failed, total = doctest.testmod(hostname, globs=env)
+    assert failed == 0
