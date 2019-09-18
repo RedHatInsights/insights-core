88e2204facf6cb7a2846e551fe1941990eb19620
Xiaonan He <xhe@redhat.com>
2019-09-18 22:34:13 +0800

    cover the classes of prodsec-rules
    CpuVulnsMeltdown - file ``/sys/devices/system/cpu/vulnerabilities/meltdown``
    CpuVulnsSpectreV1 - file ``/sys/devices/system/cpu/vulnerabilities/spectre_v1``
    CpuVulnsSpectreV2 - file ``/sys/devices/system/cpu/vulnerabilities/spectre_v2``
    CpuVulnsSpecStoreBypass - file ``/sys/devices/system/cpu/vulnerabilities/spec_store_bypass``
    CpuVulnsSmt - file ``/sys/devices/system/cpu/vulnerabilities/l1tf``
    CpuVulnsMds - file ``/sys/devices/system/cpu/vulnerabilities/mds``
    
    Signed-off-by: Xiaonan He <xhe@redhat.com>

--- insights/parsers/cpu_vulns.py
+++ insights/parsers/cpu_vulns.py
@@ -1,7 +1,26 @@
 """
+Parsers for file ``/sys/devices/system/cpu/vulnerabilities/*`` outputs
+======================================================================
+
+This module provides the following parsers:
+
+CpuVulnsMeltdown - file ``/sys/devices/system/cpu/vulnerabilities/meltdown``
+----------------------------------------------------------------------------
+
+CpuVulnsSpectreV1 - file ``/sys/devices/system/cpu/vulnerabilities/spectre_v1``
+-------------------------------------------------------------------------------
+
 CpuVulnsSpectreV2 - file ``/sys/devices/system/cpu/vulnerabilities/spectre_v2``
-===============================================================================
-Module for parsing the output of file ``/sys/devices/system/cpu/vulnerabilities/spectre_v2``.
+-------------------------------------------------------------------------------
+
+CpuVulnsSpecStoreBypass - file ``/sys/devices/system/cpu/vulnerabilities/spec_store_bypass``
+--------------------------------------------------------------------------------------------
+
+CpuVulnsSmt - file ``/sys/devices/system/cpu/vulnerabilities/l1tf``
+-------------------------------------------------------------------
+
+CpuVulnsMds - file ``/sys/devices/system/cpu/vulnerabilities/mds``
+------------------------------------------------------------------
 
 """
 
@@ -13,32 +32,175 @@ from insights.parsers import SkipException
 
 
 @parser(Specs.cpu_vulns)
-class CpuVulnsSpectreV2(Parser):
+class CpuVulns(Parser):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/*``
+
+    Attributes:
+        value (str): the result parsed of `/sys/devices/system/cpu/vulnerabilities/*`
+
+    Raises:
+        SkipException: When the name of file parsed is not expected or content is empty
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(CpuVulns, self).__init__(*args, **kwargs)
+        input_name = args[0].metadata['input_name']
+        if self.file_name != input_name:
+            raise SkipException("Not expected file: {0}".format(self.file_name))
+
+    def parse_content(self, content):
+        if not content:
+            raise SkipException("Input content is empty")
+        self.value = content[0]
+
+
+@parser(Specs.cpu_vulns)
+class CpuVulnsMeltdown(CpuVulns):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/meltdown``
+
+    Typical output of file ``/sys/devices/system/cpu/vulnerabilities/meltdown`` looks like::
+
+        Mitigation: PTI
+
+    Examples:
+        >>> type(md)
+        <class 'insights.parsers.cpu_vulns.CpuVulnsMeltdown'>
+        >>> md.value
+        'Mitigation: PTI'
+
+    Raises:
+        SkipException: When file name is not 'meltdown' or file content is empty
+
+    Attributes:
+        value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/meltdown'
+
+    """
+    pass
+
+
+@parser(Specs.cpu_vulns)
+class CpuVulnsSpectreV1(CpuVulns):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/spectre_v1``
+
+    Typical output of file ``/sys/devices/system/cpu/vulnerabilities/spectre_v1`` looks like::
+
+        Mitigation: Load fences
+
+    Examples:
+        >>> type(sp_v1)
+        <class 'insights.parsers.cpu_vulns.CpuVulnsSpectreV1'>
+        >>> sp_v1.value
+        'Mitigation: Load fences'
+
+    Raises:
+        SkipException: When file name is not 'spectre_v1' or file content is empty
+
+    Attributes:
+        value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/spectre_v1'
+
+    """
+    pass
+
+
+@parser(Specs.cpu_vulns)
+class CpuVulnsSpectreV2(CpuVulns):
     """
     Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/spectre_v2``
 
     Typical output of file ``/sys/devices/system/cpu/vulnerabilities/spectre_v2`` looks like::
 
-        Mitigation: Full generic retpoline, IBPB: conditional, IBRS_FW, STIBP: conditional, RSB filling
+        Vulnerable: Retpoline without IBPB
 
     Examples:
-        >>> type(obj)
+        >>> type(sp_v2)
         <class 'insights.parsers.cpu_vulns.CpuVulnsSpectreV2'>
-        >>> obj.value
-        'Mitigation: Full generic retpoline, IBPB: conditional, IBRS_FW, STIBP: conditional, RSB filling'
+        >>> sp_v2.value
+        'Vulnerable: Retpoline without IBPB'
 
     Raises:
-        SkipException: When input content is empty
+        SkipException: When file name is not 'spectre_v2' or file content is empty
 
     Attributes:
         value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/spectre_v2'
 
     """
+    pass
 
-    def parse_content(self, content):
-        EMPTY = "Input content is empty"
 
-        if not content:
-            raise SkipException(EMPTY)
+@parser(Specs.cpu_vulns)
+class CpuVulnsSpecStoreBypass(CpuVulns):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/spec_store_bypass``
 
-        self.value = content[0]
+    Typical output of file ``/sys/devices/system/cpu/vulnerabilities/spec_store_bypass`` looks like::
+
+        Mitigation: Speculative Store Bypass disabled
+
+    Examples:
+        >>> type(ssb)
+        <class 'insights.parsers.cpu_vulns.CpuVulnsSpecStoreBypass'>
+        >>> ssb.value
+        'Mitigation: Speculative Store Bypass disabled'
+
+    Raises:
+        SkipException: When file name is not 'spec_store_bypass' or file content is empty
+
+    Attributes:
+        value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/spec_store_bypass'
+
+    """
+    pass
+
+
+@parser(Specs.cpu_vulns)
+class CpuVulnsSmt(CpuVulns):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/l1tf``
+
+    Typical output of file ``/sys/devices/system/cpu/vulnerabilities/l1tf`` looks like::
+
+        Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable
+
+    Examples:
+        >>> type(smt)
+        <class 'insights.parsers.cpu_vulns.CpuVulnsSmt'>
+        >>> smt.value
+        'Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable'
+
+    Raises:
+        SkipException: When file name is not 'l1tf' or file content is empty
+
+    Attributes:
+        value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/l1tf'
+
+    """
+    pass
+
+
+@parser(Specs.cpu_vulns)
+class CpuVulnsMds(CpuVulns):
+    """
+    Class for parsing file ``/sys/devices/system/cpu/vulnerabilities/mds``
+
+    Typical output of file ``/sys/devices/system/cpu/vulnerabilities/mds`` looks like::
+
+        Vulnerable: Clear CPU buffers attempted, no microcode; SMT vulnerable
+
+    Examples:
+        >>> type(mds)
+        <class 'insights.parsers.cpu_vulns.CpuVulnsMds'>
+        >>> mds.value
+        'Vulnerable: Clear CPU buffers attempted, no microcode; SMT vulnerable'
+
+    Raises:
+        SkipException: When file name is not 'mds' or file content is empty
+
+    Attributes:
+        value (str): the result parsed of '/sys/devices/system/cpu/vulnerabilities/mds'
+
+    """
+    pass
--- insights/parsers/tests/test_cpu_vulns.py
+++ insights/parsers/tests/test_cpu_vulns.py
@@ -1,46 +1,319 @@
 from insights.parsers import SkipException
-from insights.parsers import cpu_vulns
-from insights.parsers.cpu_vulns import CpuVulnsSpectreV2
 from insights.tests import context_wrap
+from insights.parsers import cpu_vulns
+from insights.parsers.cpu_vulns import CpuVulnsMeltdown
+from insights.parsers.cpu_vulns import CpuVulnsSpectreV1
+from insights.parsers.cpu_vulns import CpuVulnsSpectreV2
+from insights.parsers.cpu_vulns import CpuVulnsSpecStoreBypass
+from insights.parsers.cpu_vulns import CpuVulnsSmt
+from insights.parsers.cpu_vulns import CpuVulnsMds
 import pytest
 import doctest
 
+INPUT_MELTDOWN = """
+Mitigation: PTI
+""".strip()
+
+INPUT_SPECTRE_V1 = """
+Mitigation: Load fences
+""".strip()
+
+INPUT_SPECTRE_V2_RHEL_7_1 = """
+Mitigation: Full generic retpoline, IBPB: conditional, IBRS_FW, STIBP: conditional, RSB filling
+""".strip()
 
 INPUT_SPECTRE_V2_RHEL_7 = """
-Mitigation: Full generic retpoline, IBPB: conditional, IBRS_FW, STIBP: conditional, RSB filling
+Vulnerable: Retpoline without IBPB
 """.strip()
 
 INPUT_SPECTRE_V2_RHEL_6 = """
 Mitigation: IBRS (kernel)
 """.strip()
 
+INPUT_SPEC_STORE_BYPASS = """
+Mitigation: Speculative Store Bypass disabled
+""".strip()
 
-def test_cpu_vulns_spectre_v2_rhel7():
-    spectre = CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_7))
-    assert spectre.value == INPUT_SPECTRE_V2_RHEL_7
+INPUT_SPEC_STORE_BYPASS_2 = """
+Not affected
+""".strip()
+
+INPUT_SPEC_STORE_BYPASS_3 = """
+Vulnerable
+""".strip()
+
+INPUT_SMT = """
+Mitigation: PTE Inversion; VMX: conditional cache flushes, SMT vulnerable
+""".strip()
+
+# This could occur if you've not updated to the latest intel-microcode package or if Intel has not released new microcode for your processor. You'll see the following in this situation:
+INPUT_MDS = """
+Vulnerable: Clear CPU buffers attempted, no microcode; SMT vulnerable
+""".strip()
+
+# Processors that aren't vulnerable to MDS will report the following:
+INPUT_MDS_2 = """
+Not affected
+""".strip()
+
+# Processors that have Hyper-Threading support enabled will indicate that SMT is vulnerable:
+INPUT_MDS_3 = """
+Mitigation: Clear CPU buffers; SMT vulnerable
+""".strip()
+
+# The file will contain the following contents for processors that do not support Intel Hyper-Threading or where Hyper-Threading has been disabled:
+INPUT_MDS_4 = """
+Mitigation: Clear CPU buffers; SMT disabled
+""".strip()
+
+# The kernel is unable to reliably determine whether Hyper-Threading is enabled when running in a virtual environment. Updated host kernel packages, updated host qemu packages with proper configuration to pass through the host CPU type to the guest, and updated guest kernel packages will show the following status inside of the virtual environment:
+INPUT_MDS_5 = """
+Mitigation: Clear CPU buffers; SMT Host state unknown
+""".strip()
 
 
-def test_cpu_vulns_spectre_v2_rhel6():
-    spectre = CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_6))
-    assert spectre.value == INPUT_SPECTRE_V2_RHEL_6
+# CpuVulnsMeltdown
+def test_cpu_vulns_meltdown():
+    spectre = CpuVulnsMeltdown(context_wrap(INPUT_MELTDOWN,
+        path='/tmp/meltdown',
+        metadata={'input_name': 'meltdown'}))
+    assert spectre.value == INPUT_MELTDOWN
 
 
-def test_cpu_vulns_spectre_v2_documentation():
-    """
-    Here we test the examples in the documentation automatically using doctest.
-    We set up an environment which is similar to what a rule writer might see -
-    a '/sys/devices/system/cpu/vulnerabilities/spectre_v2' output that has been
-    passed in as a parameter to the rule declaration.
-    """
-    env = {'obj': CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_7))}
-    failed, total = doctest.testmod(cpu_vulns, globs=env)
-    assert failed == 0
-
-
-def test_cpu_vulns_spectre_v2_exp():
+def test_cpu_vulns_meltdown_exp1():
     """
     Here test the examples cause expections
     """
     with pytest.raises(SkipException) as sc1:
-        CpuVulnsSpectreV2(context_wrap(""))
+        CpuVulnsMeltdown(context_wrap(''))
     assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_meltdown_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsMeltdown(context_wrap(INPUT_MELTDOWN,
+            path='/tmp/meltdown',
+            metadata={'input_name': 'abcde'}))
+    assert "Not expected file" in str(sc1)
+
+
+# CpuVulnsSpecStoreBypass
+def test_cpu_vulns_spec_store_bypass():
+    spectre = CpuVulnsSpecStoreBypass(context_wrap(INPUT_SPEC_STORE_BYPASS,
+        path='/tmp/spec_store_bypass',
+        metadata={'input_name': 'spec_store_bypass'}))
+    assert spectre.value == INPUT_SPEC_STORE_BYPASS
+
+
+def test_cpu_vulns_spec_store_bypass_2():
+    spectre = CpuVulnsSpecStoreBypass(context_wrap(INPUT_SPEC_STORE_BYPASS_2,
+        path='/tmp/spec_store_bypass',
+        metadata={'input_name': 'spec_store_bypass'}))
+    assert spectre.value == INPUT_SPEC_STORE_BYPASS_2
+
+
+def test_cpu_vulns_spec_store_bypass_3():
+    spectre = CpuVulnsSpecStoreBypass(context_wrap(INPUT_SPEC_STORE_BYPASS_3,
+        path='/tmp/spec_store_bypass',
+        metadata={'input_name': 'spec_store_bypass'}))
+    assert spectre.value == INPUT_SPEC_STORE_BYPASS_3
+
+
+def test_cpu_vulns_spec_store_bypass_exp1():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpecStoreBypass(context_wrap(''))
+    assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_spec_store_bypass_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpecStoreBypass(context_wrap(INPUT_SPEC_STORE_BYPASS,
+            path='/tmp/spec_store_bypass',
+            metadata={'input_name': 'abcde'}))
+    assert "Not expected file" in str(sc1)
+
+
+# CpuVulnsSmt
+def test_cpu_vulns_smt():
+    spectre = CpuVulnsSmt(context_wrap(INPUT_SMT,
+        path='/tmp/l1tf',
+        metadata={'input_name': 'l1tf'}))
+    assert spectre.value == INPUT_SMT
+
+
+def test_cpu_vulns_smt_exp1():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSmt(context_wrap(''))
+    assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_smt_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSmt(context_wrap(INPUT_SMT,
+            path='/tmp/l1tf',
+            metadata={'input_name': 'abcde'}))
+    assert "Not expected file" in str(sc1)
+
+
+# CpuVulnsMds
+def test_cpu_vulns_mds():
+    spectre = CpuVulnsMds(context_wrap(INPUT_MDS,
+        path='/tmp/mds',
+        metadata={'input_name': 'mds'}))
+    assert spectre.value == INPUT_MDS
+
+
+def test_cpu_vulns_mds_2():
+    spectre = CpuVulnsMds(context_wrap(INPUT_MDS_2,
+        path='/tmp/mds',
+        metadata={'input_name': 'mds'}))
+    assert spectre.value == INPUT_MDS_2
+
+
+def test_cpu_vulns_mds_3():
+    spectre = CpuVulnsMds(context_wrap(INPUT_MDS_3,
+        path='/tmp/mds',
+        metadata={'input_name': 'mds'}))
+    assert spectre.value == INPUT_MDS_3
+
+
+def test_cpu_vulns_mds_4():
+    spectre = CpuVulnsMds(context_wrap(INPUT_MDS_4,
+        path='/tmp/mds',
+        metadata={'input_name': 'mds'}))
+    assert spectre.value == INPUT_MDS_4
+
+
+def test_cpu_vulns_mds_exp1():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsMds(context_wrap(''))
+    assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_mds_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsMds(context_wrap(INPUT_MDS,
+            path='/tmp/mds',
+            metadata={'input_name': 'abcde'}))
+    assert "Not expected file" in str(sc1)
+
+
+# CpuVulnsSpectreV1
+def test_cpu_vulns_spectre_v1():
+    spectre = CpuVulnsSpectreV1(context_wrap(INPUT_SPECTRE_V1,
+        path='/tmp/spectre_v1',
+        metadata={'input_name': 'spectre_v1'}))
+    assert spectre.value == INPUT_SPECTRE_V1
+
+
+def test_cpu_vulns_spectre_v1_exp1():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpectreV1(context_wrap(''))
+    assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_spectre_v1_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpectreV1(context_wrap(INPUT_SPECTRE_V1,
+            path='/tmp/spectre_v1',
+            metadata={'input_name': 'spectre_v5'}))
+    assert "Not expected file" in str(sc1)
+
+
+# CpuVulnsSpectreV2
+def test_cpu_vulns_spectre_v2_rhel7():
+    """
+    Here test the examples for spectre_v2
+    """
+    spectre = CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_7,
+        path='/tmp/spectre_v2',
+        metadata={'input_name': 'spectre_v2'}))
+    assert spectre.value == INPUT_SPECTRE_V2_RHEL_7
+
+
+def test_cpu_vulns_spectre_v2_rhel6():
+    """
+    Here test the examples for spectre_v2
+    """
+    spectre = CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_6,
+        path='/tmp/spectre_v2',
+        metadata={'input_name': 'spectre_v2'}))
+    assert spectre.value == INPUT_SPECTRE_V2_RHEL_6
+
+
+def test_cpu_vulns_spectre_v2_exp1():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpectreV2(context_wrap(''))
+    assert "Input content is empty" in str(sc1)
+
+
+def test_cpu_vulns_spectre_v2_exp2():
+    """
+    Here test the examples cause expections
+    """
+    with pytest.raises(SkipException) as sc1:
+        CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_6,
+            path='/tmp/spectre_v2',
+            metadata={'input_name': 'spectre_v5'}))
+    assert "Not expected file" in str(sc1)
+
+
+def test_cpu_vulns_documentation():
+    """
+    Here we test the examples in the documentation automatically using doctest.
+    We set up an environment which is similar to what a rule writer might see -
+    a '/sys/devices/system/cpu/vulnerabilities/spectre_v1' output that has been
+    passed in as a parameter to the rule declaration.
+    """
+    env = {
+        'sp_v1': CpuVulnsSpectreV1(context_wrap(INPUT_SPECTRE_V1,
+            path='/tmp/spectre_v1',
+            metadata={'input_name': 'spectre_v1'})),
+        'sp_v2': CpuVulnsSpectreV2(context_wrap(INPUT_SPECTRE_V2_RHEL_7,
+            path='/tmp/spectre_v2',
+            metadata={'input_name': 'spectre_v2'})),
+        'md': CpuVulnsMeltdown(context_wrap(INPUT_MELTDOWN,
+            path='/tmp/meltdown',
+            metadata={'input_name': 'meltdown'})),
+        'ssb': CpuVulnsSpecStoreBypass(context_wrap(INPUT_SPEC_STORE_BYPASS,
+            path='/tmp/spec_store_bypass',
+            metadata={'input_name': 'spec_store_bypass'})),
+        'smt': CpuVulnsSmt(context_wrap(INPUT_SMT,
+            path='/tmp/l1tf',
+            metadata={'input_name': 'l1tf'})),
+        'mds': CpuVulnsMds(context_wrap(INPUT_MDS,
+            path='/tmp/mds',
+            metadata={'input_name': 'mds'}))}
+
+    failed, total = doctest.testmod(cpu_vulns, globs=env)
+    assert failed == 0
