f1dcc4bd39b470cb39a7e88e8fc1c85d4535728e
pratikgadiya12 <pratikgadiya12@users.noreply.github.com>
2019-09-27 07:02:25 +0530

    Podman parsers (#2204)
    * Podman parsers
    
    - Wherever possible have used docker parsers
    - New Podman parsers
        - PodmanListImages
        - PodmanListContainers
        - PodmanInspectImage
        - PodmanInspectContainer
    
    Implements:
    https://projects.engineering.redhat.com/browse/CPSEARCH-4266
    Signed-off-by: Pratik Gadiya <pgadiya@redhat.com>
    
    * New files for podman parsers
    
    Signed-off-by: Pratik Gadiya <pgadiya@redhat.com>
    
    * Removed LegacyItemAccess from podman parsers
    
    - podman_inspect
    - podman_list
    
    Signed-off-by: Pratik Gadiya <pgadiya@redhat.com>
    
    * Fixed review comments
    
    Signed-off-by: Pratik Gadiya <pgadiya@redhat.com>
    
    * Refactored the code and added .rst files
    
    Signed-off-by: Pratik Gadiya <pgadiya@redhat.com>
    
    * Add Attributes for sub-classes
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 590d2cc66a34b77fdf5e8b9bb11190727244faab)

---
+++ insights/parsers/podman_inspect.py
@@ -0,0 +1,76 @@
+"""
+PodmanInspect - Command ``podman inspect --type={TYPE}``
+========================================================
+
+This module parses the output of the ``podman inspect`` command.  This uses
+the ``core.marshalling.unmarshal`` function to parse the JSON output from the
+commands.  The data is stored as a dictionary.
+
+Sample input::
+
+    [
+    {
+        "Id": "97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07",
+        "Created": "2016-06-23T05:12:25.433469799Z",
+        "Path": "/bin/bash",
+        "Args": [],
+        "Name": "/sample_webapp",
+        "State": {
+            "Status": "running",
+            "Running": true,
+            "Paused": false,
+            "Restarting": false,
+    ...
+
+Examples:
+
+    >>> image.get('ID') # new-style access
+    '97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07'
+    >>> image.get('State').get('Paused') # sub-dictionaries
+    False
+
+"""
+
+from insights import parser, CommandParser
+from insights.core.marshalling import unmarshal
+from insights.parsers import SkipException
+from insights.specs import Specs
+
+
+class PodmanInspect(CommandParser, dict):
+    """
+    Parse the output of command "podman inspect --type=image" and "podman
+    inspect --type=container".  The output of these two commands is formatted
+    as JSON, so "json.loads" is an option to parse the output in the future.
+
+    Raises:
+        SkipException: If content is not provided
+    """
+
+    def parse_content(self, content):
+        content = "\n".join(list(content))
+
+        if not content:
+            raise SkipException
+
+        try:
+            inspect_data = unmarshal(content)
+            self.update(inspect_data[0])
+        except:
+            raise SkipException
+
+
+@parser(Specs.podman_image_inspect)
+class PodmanInspectImage(PodmanInspect):
+    """
+    Parse podman image inspect output using the PodmanInspect parser class.
+    """
+    pass
+
+
+@parser(Specs.podman_container_inspect)
+class PodmanInspectContainer(PodmanInspect):
+    """
+    Parse podman container inspect output using the PodmanInspect parser class.
+    """
+    pass
