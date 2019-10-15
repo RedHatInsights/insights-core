e22f0d9c9c762e1bd722cc7cdc10fde95f01b3b4
Xiaonan He <xhe@redhat.com>
2019-10-15 11:02:00 +0800

    Improve documentation for parser of cpu_vulns (#2249)
    * improve docstring and add descriptions and example for parser
    
    Signed-off-by: Xiaonan He <xhe@redhat.com>
    
    * improve the docstring for parser cpu_vulns.py
    
    Signed-off-by: Xiaonan He <xhe@redhat.com>
    (cherry picked from commit cd5bfa0bb6aa4f01108021c0726f3fc78096e346)

--- insights/parsers/cpu_vulns.py
+++ insights/parsers/cpu_vulns.py
@@ -1,10 +1,8 @@
 """
-Parsers for cpu vulnerabilities file output
-===========================================
+CpuVulns - files ``/sys/devices/system/cpu/vulnerabilities/*``
+==============================================================
 
-This module provides the following parser:
-
-CpuVulns - ``/sys/devices/system/cpu/vulnerabilities/*``
+Parser to parse the output of files ``/sys/devices/system/cpu/vulnerabilities/*``
 """
 
 from insights import Parser
@@ -16,10 +14,38 @@ from insights.parsers import SkipException
 @parser(Specs.cpu_vulns)
 class CpuVulns(Parser):
     """
-    Base class for current parser
+    Base class to parse ``/sys/devices/system/cpu/vulnerabilities/*`` files,
+    the file content will be stored in a string.
+
+    Sample output for files:
+        ``/sys/devices/system/cpu/vulnerabilities/spectre_v1``::
+            Mitigation: Load fences
+
+        ``/sys/devices/system/cpu/vulnerabilities/spectre_v2``::
+            Vulnerable: Retpoline without IBPB
+
+        ``/sys/devices/system/cpu/vulnerabilities/meltdown``::
+            Mitigation: PTI
+
+        ``/sys/devices/system/cpu/vulnerabilities/spec_store_bypass``::
+            Mitigation: Speculative Store Bypass disabled
+
+    Examples:
+        >>> type(sp_v1)
+        <class 'insights.parsers.cpu_vulns.CpuVulns'>
+        >>> type(sp_v1) == type(sp_v2) == type(md) == type(ssb)
+        True
+        >>> sp_v1.value
+        'Mitigation: Load fences'
+        >>> sp_v2.value
+        'Vulnerable: Retpoline without IBPB'
+        >>> md.value
+        'Mitigation: PTI'
+        >>> ssb.value
+        'Mitigation: Speculative Store Bypass disabled'
 
     Attributes:
-        value (str): the result parsed
+        value (str): The result parsed
 
     Raises:
         SkipException: When file content is empty
