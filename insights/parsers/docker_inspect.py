c69bf2c7354444e3418a42a6b3ebbf8b449f15b1
Xiangce Liu <xiangceliu@redhat.com>
2019-10-24 22:55:42 +0800

    Update docker_list and docker_inspect (#2253)
    * Update docker_list and docker_inspect
    
    - use parse_fixed_table in docker_list
    - mix DockerInspect with dict
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update the example test of docker_inspect
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * fix flake8 errors
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * fix doc errors
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * fix type row->rows
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/docker_inspect.py
+++ insights/parsers/docker_inspect.py
@@ -4,63 +4,65 @@ DockerInspect - Command ``docker inspect --type={TYPE}``
 
 This module parses the output of the ``docker inspect`` command.  This uses
 the ``core.marshalling.unmarshal`` function to parse the JSON output from the
-commands.  The data is stored as a nested dictionary in the ``data``
-attribute, and uses the ``LegacyItemAccess`` mixin class to provide ``get``
-and ``contains`` access to the dictionary via the object itself.
-
-Sample input::
-
-    [
-    {
-        "Id": "97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07",
-        "Created": "2016-06-23T05:12:25.433469799Z",
-        "Path": "/bin/bash",
-        "Args": [],
-        "Name": "/sample_webapp",
-        "State": {
-            "Status": "running",
-            "Running": true,
-            "Paused": false,
-            "Restarting": false,
-    ...
-
-Examples:
-
-    >>> image = shared[DockerInspectContainer]
-    >>> image.data['ID'] # new-style access
-    '97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07'
-    >>> image['Name'] # old-style access
-    '/sample_webapp'
-    >>> image.get('State').get('Paused') # sub-dictionaries
-    False
+commands.  The parsed data can be accessed a dictionary via the object itself.
 
 """
 
-from .. import LegacyItemAccess, parser, CommandParser
-from ..core.marshalling import unmarshal
+from insights import parser, CommandParser
+from insights.core.marshalling import unmarshal
+from insights.parsers import SkipException
 from insights.specs import Specs
 
 
-class DockerInspect(LegacyItemAccess, CommandParser):
+class DockerInspect(CommandParser, dict):
     """
     Parse the output of command "docker inspect --type=image" and "docker
     inspect --type=container".  The output of these two commands is formatted
     as JSON, so "json.loads" is an option to parse the output in the future.
+
+    Raises:
+        SkipException: If content is not provided
     """
 
     def parse_content(self, content):
+        if not content:
+            raise SkipException
+
         content = "\n".join(list(content))
         try:
             inspect_data = unmarshal(content)
-            self.data = inspect_data[0]
+            self.update(inspect_data[0])
         except:
-            self.data = {}
+            raise SkipException
+
+    @property
+    def data(self):
+        return self
 
 
 @parser(Specs.docker_image_inspect)
 class DockerInspectImage(DockerInspect):
     """
     Parse docker image inspect output using the DockerInspect parser class.
+
+    Sample input::
+
+        [
+            {
+                "Id": "882ab98aae5394aebe91fe6d8a4297fa0387c3cfd421b2d892bddf218ac373b2",
+                "RepoTags": [
+                    "rhel7_imagemagick:latest"
+                ],
+                "RepoDigests": [],
+        ...
+
+    Examples:
+
+        >>> image['Id'] == '882ab98aae5394aebe91fe6d8a4297fa0387c3cfd421b2d892bddf218ac373b2'
+        True
+        >>> image['RepoTags'][0] == 'rhel7_imagemagick:latest'
+        True
+
     """
     pass
 
@@ -69,5 +71,31 @@ class DockerInspectImage(DockerInspect):
 class DockerInspectContainer(DockerInspect):
     """
     Parse docker container inspect output using the DockerInspect parser class.
+
+    Sample input::
+
+        [
+            {
+                "Id": "97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07",
+                "Created": "2016-06-23T05:12:25.433469799Z",
+                "Path": "/bin/bash",
+                "Args": [],
+                "Name": "/hehe2",
+                "State": {
+                    "Status": "running",
+                    "Running": true,
+                    "Paused": false,
+                    "Restarting": false,
+            ...
+
+    Examples:
+
+        >>> container['Id'] == '97d7cd1a5d8fd7730e83bb61ecbc993742438e966ac5c11910776b5d53f4ae07'
+        True
+        >>> container['Name'] == '/hehe2'
+        True
+        >>> container.get('State').get('Paused') # sub-dictionaries
+        False
+
     """
     pass
--- insights/parsers/docker_list.py
+++ insights/parsers/docker_list.py
@@ -20,34 +20,9 @@ available in two formats:
 * The new format stores each dictionary in a dictionary keyed on the value of
   a given field, given by the subclass.
 
-Sample output of command ``/usr/bin/docker images --all --no-trunc --digests``::
-
-    REPOSITORY                           TAG                 DIGEST              IMAGE ID                                                           CREATED             VIRTUAL SIZE
-    rhel7_imagemagick                    latest              <none>              882ab98aae5394aebe91fe6d8a4297fa0387c3cfd421b2d892bddf218ac373b2   4 days ago          785.4 MB
-    rhel6_nss-softokn                    latest              <none>              dd87dad2c7841a19263ae2dc96d32c501ee84a92f56aed75bb67f57efe4e48b5   5 days ago          449.7 MB
-
-
-Sample output of command ``/usr/bin/docker ps --all --no-trunc --size``::
-
-    CONTAINER ID                                                       IMAGE                                                              COMMAND                                            CREATED             STATUS                        PORTS                  NAMES               SIZE
-    95516ea08b565e37e2a4bca3333af40a240c368131b77276da8dec629b7fe102   bd8638c869ea40a9269d87e9af6741574562af9ee013e03ac2745fb5f59e2478   "/bin/sh -c 'yum install -y vsftpd-2.2.2-6.el6'"   51 minutes ago      Exited (137) 50 minutes ago                          tender_rosalind     4.751 MB (virtual 200.4 MB)
-    03e2861336a76e29155836113ff6560cb70780c32f95062642993b2b3d0fc216   rhel7_httpd                                                        "/usr/sbin/httpd -DFOREGROUND"                     45 seconds ago      Up 37 seconds                 0.0.0.0:8080->80/tcp   angry_saha          796 B (virtual 669.2 MB)
-
-Examples:
-    >>> images.rows[0]['REPOSITORY']
-    'rhel6_vsftpd'
-    >>> images.rows[1]['VIRTUAL SIZE']
-    '785.4 MB'
-    >>> images.data['rhel6_vsftpd']['CREATED']
-    '37 minutes ago'
-    >>> containers.rows[0]['STATUS']
-    'Up 37 seconds'
-    >>> containers.data['tender_rosalind']['STATUS']
-    'Exited (137) 18 hours ago'
 """
-import re
 from insights import CommandParser, parser
-from insights.parsers import SkipException
+from insights.parsers import SkipException, parse_fixed_table
 from insights.specs import Specs
 
 
@@ -65,8 +40,15 @@ class DockerList(CommandParser):
 
     Why not just use hard-coded fields and columns?  So that we can adapt to
     different output lists.
+
+    Raises:
+        NotImplementedError: If `key_field` or `attr_name` is not defined
+        SkipException: If no data to parse
     """
     key_field = None
+    heading_ignore = []
+    attr_name = ''
+    substitutions = []
 
     def parse_content(self, content):
         """
@@ -76,52 +58,85 @@ class DockerList(CommandParser):
         If the ``key_field`` property is set, use this to key a ``data``
         dictionary attribute.
         """
-        self.rows = []
-        if len(content) < 2:
-            raise SkipException("No data.")
+        if not (self.key_field and self.attr_name):
+            raise NotImplementedError("'key_field' or 'attr_name' is not defined")
 
-        # Parse header, remembering column numbers for data capture.  We use
-        # a finditer to get the positions, and we find by field rather than
-        # splitting on three or more spaces because of this.
-        headers = []
-        field_re = re.compile(r'\w+(\s\w+)*')
-        for match in field_re.finditer(content[0]):
-            headers.append({'name': match.group(), 'start': match.start()})
+        self.rows = parse_fixed_table(content,
+                                      heading_ignore=self.heading_ignore,
+                                      header_substitute=self.substitutions)
 
-        # Parse the rest of the line.  Each field starts at the column
-        # given by the header and ends with at least three spaces.
-        for line in content[1:]:
-            # I think the dictionary comprehension version of this is too
-            # complicated for words :-)
-            row = {}
-            for header in headers:
-                value = line[header['start']:].split('   ', 1)[0]
-                if value == '':
-                    value = None
-                row[header['name']] = value
-            self.rows.append(row)
+        if not self.rows:
+            raise SkipException('No data.')
 
-        # If we have a key_field set, construct a data dictionary on it.
-        # Note that duplicates will be overwritten, but we ignore '<none>'.
-        if self.key_field and self.key_field in self.rows[0]:
-            self.data = {}
-            for row in self.rows:
-                k = row[self.key_field]
-                if k is not None and k != '<none>':
-                    self.data[k] = row
+        data = {}
+        for row in self.rows:
+            k = row.get(self.key_field)
+            for sub in self.substitutions:
+                row[sub[0]] = row.pop(sub[1]) if sub[1] in row else None
+            if k is not None and k != '<none>':
+                data[k] = row
+        setattr(self, self.attr_name, data)
 
 
 @parser(Specs.docker_list_images)
 class DockerListImages(DockerList):
     """
     Handle the list of docker images using the DockerList parser class.
+
+    Sample output of command ``docker images --all --no-trunc --digests``::
+
+        REPOSITORY                           TAG                 DIGEST              IMAGE ID                                                           CREATED             VIRTUAL SIZE
+        rhel7_imagemagick                    latest              <none>              882ab98aae5394aebe91fe6d8a4297fa0387c3cfd421b2d892bddf218ac373b2   4 days ago          785.4 MB
+        rhel6_nss-softokn                    latest              <none>              dd87dad2c7841a19263ae2dc96d32c501ee84a92f56aed75bb67f57efe4e48b5   5 days ago          449.7 MB
+
+    Attributes:
+        rows (list): List of row dictionaries.
+        images (dict): Dictionary keyed on the value of the "REPOSITORY" fileld
+
+    Examples:
+        >>> images.rows[0]['REPOSITORY']
+        'rhel6_vsftpd'
+        >>> images.rows[1]['VIRTUAL SIZE']
+        '785.4 MB'
+        >>> images.images['rhel6_vsftpd']['CREATED']
+        '37 minutes ago'
     """
     key_field = 'REPOSITORY'
+    heading_ignore = [key_field]
+    attr_name = 'images'
+    substitutions = [("IMAGE ID", "IMAGE_ID"), ("VIRTUAL SIZE", "VIRTUAL_SIZE")]
+
+    @property
+    def data(self):
+        return self.images
 
 
 @parser(Specs.docker_list_containers)
 class DockerListContainers(DockerList):
     """
     Handle the list of docker images using the DockerList parser class.
+
+    Sample output of command ``docker ps --all --no-trunc --size``::
+
+        CONTAINER ID                                                       IMAGE                                                              COMMAND                                            CREATED             STATUS                        PORTS                  NAMES               SIZE
+        95516ea08b565e37e2a4bca3333af40a240c368131b77276da8dec629b7fe102   bd8638c869ea40a9269d87e9af6741574562af9ee013e03ac2745fb5f59e2478   "/bin/sh -c 'yum install -y vsftpd-2.2.2-6.el6'"   51 minutes ago      Exited (137) 50 minutes ago                          tender_rosalind     4.751 MB (virtual 200.4 MB)
+        03e2861336a76e29155836113ff6560cb70780c32f95062642993b2b3d0fc216   rhel7_httpd                                                        "/usr/sbin/httpd -DFOREGROUND"                     45 seconds ago      Up 37 seconds                 0.0.0.0:8080->80/tcp   angry_saha          796 B (virtual 669.2 MB)
+
+    Attributes:
+        rows (list): List of row dictionaries.
+        containers(dict): Dictionary keyed on the value of the "NAMES" field
+
+    Examples:
+        >>> containers.rows[0]['STATUS']
+        'Up 37 seconds'
+        >>> containers.containers['tender_rosalind']['STATUS']
+        'Exited (137) 18 hours ago'
     """
     key_field = 'NAMES'
+    heading_ignore = ['CONTAINER']
+    attr_name = 'containers'
+    substitutions = [("CONTAINER ID", "CONTAINER_ID")]
+
+    @property
+    def data(self):
+        return self.containers
