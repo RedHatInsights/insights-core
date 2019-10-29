adf0c9ff7af871bffb44768748982ff9a3ba24ce
Xiaonan He <xhe@redhat.com>
2019-10-29 15:46:03 +0800

    Add document for combiner of cpu_vulns_all
    Signed-off-by: Xiaonan He <xhe@redhat.com>

--- insights/combiners/cpu_vulns_all.py
+++ insights/combiners/cpu_vulns_all.py
@@ -1,19 +1,9 @@
 """
-CpuVulnsAll - combiner for cpu vulnerabilities:
-===============================================
+CpuVulnsAll - files ``/sys/devices/system/cpu/vulnerabilities/*``
+=================================================================
 
-cpu vulnerabilities includes:
-    * CpuVulnsAll - file /sys/devices/system/cpu/vulnerabilities/*
-
-Examples:
-    >>> type(cvb)
-    <class 'insights.combiners.cpu_vulns_all.CpuVulnsAll'>
-    >>> list(cvb.keys())
-    ['meltdown', 'spectre_v1']
-
-Raises:
-    SkipComponent: Not available data
-
+This combiner provides an interface to CPU vulnerabilities parsers for files
+``/sys/devices/system/cpu/vulnerabilities/*``
 """
 
 from insights.core.plugins import combiner
@@ -24,7 +14,26 @@ from insights.parsers import SkipComponent
 @combiner(CpuVulns)
 class CpuVulnsAll(dict):
     """
-    This combiner provides an interface to CPU vulnerabilities parsers.
+    Class to capsulate the parsers of cpu_vulns, which includes all files in
+    ``/sys/devices/system/cpu/vulnerabilities/*``. Files information will be
+    stored in a list of dictionaries, each dictionary is for one file, the
+    dictionary key is the file name, dictionary value is the file content.
+
+    Sample output for files:
+        ``/sys/devices/system/cpu/vulnerabilities/spectre_v1``::
+            Mitigation: Load fences
+         ``/sys/devices/system/cpu/vulnerabilities/meltdown``::
+            Mitigation: PTI
+
+    Examples:
+        >>> type(cvb)
+        <class 'insights.combiners.cpu_vulns_all.CpuVulnsAll'>
+        >>> list(cvb.keys())
+        ['meltdown', 'spectre_v1']
+
+    Raises:
+        SkipComponent: Not available data
+
     """
 
     def __init__(self, cpu_vulns):
