d935537517f8da4f8d9c3b9dc466aec18ecb78ff
Akshay Gaikwad <akshay196@users.noreply.github.com>
2019-11-14 15:38:10 +0000

    Add sos archive path for httpd_M (#2306)
    * This adds datasource path from sos archive for httpd_M specs.
    
    Signed-off-by: Akshay Gaikwad <akgaikwa@redhat.com>

--- insights/specs/sos_archive.py
+++ insights/specs/sos_archive.py
@@ -58,6 +58,7 @@ class SosSpecs(Specs):
     hostname = first_file(["sos_commands/general/hostname_-f", "sos_commands/host/hostname_-f"])
     hostname_default = first_file(["sos_commands/general/hostname", "sos_commands/host/hostname", "/etc/hostname", "hostname"])
     hostname_short = first_file(["sos_commands/general/hostname", "sos_commands/host/hostname", "/etc/hostname", "hostname"])
+    httpd_M = simple_file("sos_commands/apache/apachectl_-M")
     installed_rpms = first_file(["sos_commands/rpm/package-data", "installed-rpms"])
     ip_addr = first_of([simple_file("sos_commands/networking/ip_-d_address"), simple_file("sos_commands/networking/ip_address")])
     ip_route_show_table_all = simple_file("sos_commands/networking/ip_route_show_table_all")
