1b054df6b080a9955a86fa73e388568fbeb6e931
vishwanathjadhav <vjadhav@redhat.com>
2019-11-15 14:56:33 +0530

    Enhanced to resolve content exception issue (#2304)
    * Enhanced to resolve containt exception issue when specs is sourced from RHEL-7
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    
    * Modified Parser name to keep backword compatibility
    
    - fix commit conficts
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    (cherry picked from commit d962fd6b7e89d560d3722a1a776cc6b3f5c10cc2)

--- insights/parsers/sctp.py
+++ insights/parsers/sctp.py
@@ -7,8 +7,11 @@ Parsers provided by this module include:
 SCTPEps - file ``/proc/net/sctp/eps``
 -------------------------------------
 
-SCTPAsc - file ``/proc/net/sctp/assocs``
-----------------------------------------
+SCTPAsc - file ``/proc/net/sctp/assocs`` on RHEL-6
+---------------------------------------------------
+
+SCTPAsc7 - file ``/proc/net/sctp/assocs`` on RHEL-7
+---------------------------------------------------
 
 SCTPSnmp - file ``/proc/net/sctp/snmp``
 ---------------------------------------
@@ -18,6 +21,7 @@ from insights import Parser, parser
 from insights.parsers import SkipException, ParseException
 from . import keyword_search
 from insights.specs import Specs
+from insights.components.rhel_version import IsRhel6, IsRhel7
 
 
 @parser(Specs.sctp_eps)
@@ -125,8 +129,7 @@ class SCTPEps(Parser):
         return keyword_search(self.data, **args)
 
 
-@parser(Specs.sctp_asc)
-class SCTPAsc(Parser):
+class SCTPAscBase(Parser):
     """
     This parser parses the content of ``/proc/net/sctp/assocs`` file.
     And returns a list of dictionaries. The dictionary contains details
@@ -136,56 +139,14 @@ class SCTPAsc(Parser):
     remote addr, heartbeat interval, max in-stream, max out-stream, max
     retransmission attempt, number of init chunks send, number of shutdown
     chunks send, data chunks retransmitted'
-
-    Typical contents of ``/proc/net/sctp/assocs`` file are::
-
-        ASSOC     SOCK   STY SST ST HBKT ASSOC-ID TX_QUEUE RX_QUEUE UID INODE LPORT RPORT LADDRS <-> RADDRS HBINT INS OUTS MAXRT T1X T2X RTXC
-        ffff88045ac7e000 ffff88062077aa00 2   1   4  1205  963        0        0     200 273361167 11567 11166  10.0.0.102 10.0.0.70 <-> *10.0.0.109 10.0.0.77      1000     2     2   10    0    0        0
-        ffff88061fbf2000 ffff88060ff92500 2   1   4  1460  942        0        0     200 273360669 11566 11167  10.0.0.102 10.0.0.70 <-> *10.0.0.109 10.0.0.77      1000     2     2   10    0    0        0
-
-    Output data is stored in the list of dictionaries
-
-    Examples:
-        >>> type(sctp_asc)
-        <class 'insights.parsers.sctp.SCTPAsc'>
-        >>> sorted(sctp_asc.sctp_local_ports) == sorted(['11567','11566'])
-        True
-        >>> sorted(sctp_asc.sctp_remote_ports) == sorted(['11166','11167'])
-        True
-        >>> sorted(sctp_asc.sctp_local_ips) == sorted(['10.0.0.102', '10.0.0.70'])
-        True
-        >>> sorted(sctp_asc.sctp_remote_ips) == sorted(['*10.0.0.109', '10.0.0.77'])
-        True
-        >>> sorted(sctp_asc.search(local_port='11566')) == sorted([{'init_chunks_send': '0', 'uid': '200', 'shutdown_chunks_send': '0', 'max_outstream': '2', 'tx_que': '0', 'inode': '273360669', 'hrtbt_intrvl': '1000', 'sk_type': '2', 'remote_addr': ['*10.0.0.109', '10.0.0.77'], 'data_chunks_retrans': '0', 'local_addr': ['10.0.0.102', '10.0.0.70'], 'asc_id': '942', 'max_instream': '2', 'remote_port': '11167', 'asc_state': '4', 'max_retrans_atmpt': '10', 'sk_state': '1', 'socket': 'ffff88060ff92500', 'asc_struct': 'ffff88061fbf2000', 'local_port': '11566', 'hash_bkt': '1460', 'rx_que': '0'}])
-        True
     """
-    COLUMN_IDX = [
-        'asc_struct',
-        'socket',
-        'sk_type',
-        'sk_state',
-        'asc_state',
-        'hash_bkt',
-        'asc_id',
-        'tx_que',
-        'rx_que',
-        'uid',
-        'inode',
-        'local_port',
-        'remote_port',
-        'local_addr',
-        'remote_addr',
-        'hrtbt_intrvl',
-        'max_instream',
-        'max_outstream',
-        'max_retrans_atmpt',
-        'init_chunks_send',
-        'shutdown_chunks_send',
-        'data_chunks_retrans',
-        'relation',   # should be ignore
-    ]
 
     def parse_content(self, content):
+        self.data = []
+        self._sctp_local_ports = set()
+        self._sctp_remote_ports = set()
+        self._sctp_local_ips = set()
+        self._sctp_remote_ips = set()
         if (not content) or (not self.file_path):
             raise SkipException("No Contents")
 
@@ -194,7 +155,6 @@ class SCTPAsc(Parser):
         if "LPORT" not in line or len(line) != keys_cnt:
             raise ParseException("Contents are not compatible to this parser".format(line))
 
-        self.data = []
         laddr_idx = line.index('LADDRS')
         raddr_ridx = len(line) - line.index('RADDRS')
         for line in content[1:]:
@@ -209,10 +169,6 @@ class SCTPAsc(Parser):
             line_1.extend(line_end)
             self.data.append(dict(zip(self.COLUMN_IDX[:-1], line_1)))
 
-        self._sctp_local_ports = set()
-        self._sctp_remote_ports = set()
-        self._sctp_local_ips = set()
-        self._sctp_remote_ips = set()
         for line in self.data:
             self._sctp_local_ports.add(line['local_port'])
             self._sctp_remote_ports.add(line['remote_port'])
@@ -257,6 +213,121 @@ class SCTPAsc(Parser):
                 when args search do not match then it returns `[]`.
         """
         return keyword_search(self.data, **args)
+
+
+@parser(Specs.sctp_asc, IsRhel6)
+class SCTPAsc(SCTPAscBase):
+    """
+    This parser parses the file ``/proc/net/sctp/assocs`` from RHEL-6. It has
+    different columns as compare to RHEL-7.
+
+    Typical contents of ``/proc/net/sctp/assocs`` on RHEL-6 file are::
+
+        ASSOC     SOCK   STY SST ST HBKT ASSOC-ID TX_QUEUE RX_QUEUE UID INODE LPORT RPORT LADDRS <-> RADDRS HBINT INS OUTS MAXRT T1X T2X RTXC
+        ffff88045ac7e000 ffff88062077aa00 2   1   4  1205  963        0        0     200 273361167 11567 11166  10.0.0.102 10.0.0.70 <-> *10.0.0.109 10.0.0.77      1000     2     2   10    0    0        0
+        ffff88061fbf2000 ffff88060ff92500 2   1   4  1460  942        0        0     200 273360669 11566 11167  10.0.0.102 10.0.0.70 <-> *10.0.0.109 10.0.0.77      1000     2     2   10    0    0        0
+
+    Output data is stored in the list of dictionaries
+
+    Examples:
+        >>> type(sctp_asc)
+        <class 'insights.parsers.sctp.SCTPAsc'>
+        >>> sorted(sctp_asc.sctp_local_ports) == sorted(['11567','11566'])
+        True
+        >>> sorted(sctp_asc.sctp_remote_ports) == sorted(['11166','11167'])
+        True
+        >>> sorted(sctp_asc.sctp_local_ips) == sorted(['10.0.0.102', '10.0.0.70'])
+        True
+        >>> sorted(sctp_asc.sctp_remote_ips) == sorted(['*10.0.0.109', '10.0.0.77'])
+        True
+        >>> sorted(sctp_asc.search(local_port='11566')) == sorted([{'init_chunks_send': '0', 'uid': '200', 'shutdown_chunks_send': '0', 'max_outstream': '2', 'tx_que': '0', 'inode': '273360669', 'hrtbt_intrvl': '1000', 'sk_type': '2', 'remote_addr': ['*10.0.0.109', '10.0.0.77'], 'data_chunks_retrans': '0', 'local_addr': ['10.0.0.102', '10.0.0.70'], 'asc_id': '942', 'max_instream': '2', 'remote_port': '11167', 'asc_state': '4', 'max_retrans_atmpt': '10', 'sk_state': '1', 'socket': 'ffff88060ff92500', 'asc_struct': 'ffff88061fbf2000', 'local_port': '11566', 'hash_bkt': '1460', 'rx_que': '0'}])
+        True
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.COLUMN_IDX = [
+            'asc_struct',
+            'socket',
+            'sk_type',
+            'sk_state',
+            'asc_state',
+            'hash_bkt',
+            'asc_id',
+            'tx_que',
+            'rx_que',
+            'uid',
+            'inode',
+            'local_port',
+            'remote_port',
+            'local_addr',
+            'remote_addr',
+            'hrtbt_intrvl',
+            'max_instream',
+            'max_outstream',
+            'max_retrans_atmpt',
+            'init_chunks_send',
+            'shutdown_chunks_send',
+            'data_chunks_retrans',
+            'relation',   # should be ignore
+        ]
+        super(SCTPAsc, self).__init__(*args, **kwargs)
+
+
+@parser(Specs.sctp_asc, IsRhel7)
+class SCTPAsc7(SCTPAscBase):
+    """
+    This parser parses the file ``/proc/net/sctp/assocs`` from RHEL-7. It has
+    different columns as compare to RHEL-6.
+
+    Typical contents of ``/proc/net/sctp/assocs`` on RHEL-7 file are::
+
+        ASSOC     SOCK   STY SST ST HBKT ASSOC-ID TX_QUEUE RX_QUEUE UID INODE LPORT RPORT LADDRS <-> RADDRS HBINT INS OUTS MAXRT T1X T2X RTXC wmema wmemq sndbuf rcvbuf
+        ffff8805d36b3000 ffff880f8911f380 0   10  3  0    12754        0        0       0 496595 3868   3868  10.131.222.5 <-> *10.131.160.81 10.131.176.81        30000    17    10   10    0    0        0        11        12  1000000  2000000
+        ffff8805f17e1000 ffff881004aff380 0   10  3  0    12728        0        0       0 532396 3868   3868  10.131.222.3 <-> *10.131.160.81 10.131.176.81        30000    17    10   10    0    0        0        13        14  3000000  4000000
+
+
+    Output data is stored in the list of dictionaries
+
+    Examples:
+        >>> type(sctp_asc_7)
+        <class 'insights.parsers.sctp.SCTPAsc7'>
+        >>> sctp_asc_7.sctp_local_ips == sorted(['10.131.222.5', '10.131.222.3'])
+        True
+        >>> sctp_asc_7.data[0]['rcvbuf']
+        '2000000'
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.COLUMN_IDX = [
+            'asc_struct',
+            'socket',
+            'sk_type',
+            'sk_state',
+            'asc_state',
+            'hash_bkt',
+            'asc_id',
+            'tx_que',
+            'rx_que',
+            'uid',
+            'inode',
+            'local_port',
+            'remote_port',
+            'local_addr',
+            'remote_addr',
+            'hrtbt_intrvl',
+            'max_instream',
+            'max_outstream',
+            'max_retrans_atmpt',
+            'init_chunks_send',
+            'shutdown_chunks_send',
+            'data_chunks_retrans',
+            'wmema',
+            'wmemq',
+            'sndbuf',
+            'rcvbuf',
+            'relation',   # should be ignore
+        ]
+        super(SCTPAsc7, self).__init__(*args, **kwargs)
 
 
 @parser(Specs.sctp_snmp)
