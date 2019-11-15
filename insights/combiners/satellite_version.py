424ceb428c195cb33957e7524511edd3f4d2b9f0
Xiangce Liu <xiangceliu@redhat.com>
2019-11-15 10:47:23 +0800

    Make SatelliteVersion support Capsule
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/satellite_version.py
+++ insights/combiners/satellite_version.py
@@ -2,20 +2,26 @@
 Satellite Version
 =================
 
-Combiner for Satellite Version information. It uses the results of
-the ``Satellite6Version`` parser and the ``InstalledRpms`` parser to determine
-the satellite version
+Combiner to get Satellite Server and Capsule Version information. It uses the
+results of the :class:`insights.parsers.satellite_version:Satellite6Version`
+parser and the :class:`insights.parsers.installed_rpms.InstalledRpms` parser to
+determine the version.
 
 """
 
-from .. import combiner, SkipComponent
-from ..parsers.satellite_version import Satellite6Version as Sat6Ver
-from ..parsers.installed_rpms import InstalledRpms
+from insights import combiner, SkipComponent
+from insights.parsers.satellite_version import Satellite6Version as Sat6Ver
+from insights.parsers.installed_rpms import InstalledRpms
 
 
-# Only track 6.0.x and 6.1.x. See https://access.redhat.com/articles/1343683
+# NOTE:
+# The following table only tracks 6.0.x and 6.1.x.
+# See https://access.redhat.com/articles/1343683
 # But, there are some mistakes in the KCS, the versions in below map
 # are corrected according to the corresponding ERRATA pages.
+#
+# Update: Thu Nov 14 10:51:19 CST 2019
+#
 sat6_ver_map = {
         # Sat     foreman     candlepin   katello
         '6.0.8': ('1.6.0.53', '0.9.23', '1.5.0'),
@@ -41,7 +47,7 @@ def _parse_sat_version(version):
     return [major, minor]
 
 
-@combiner([Sat6Ver, InstalledRpms])
+@combiner(InstalledRpms, optional=[Sat6Ver])
 class SatelliteVersion(object):
     """
     Check the parsers
@@ -51,38 +57,35 @@ class SatelliteVersion(object):
 
     Below is the logic to determine the satellite version::
 
-        1. For Satellite 6.1.x:
+        1. For Satellite 6.1:
+
             a. Check the version information in below files at first
                - https://access.redhat.com/solutions/1392633
-
                File: /usr/share/foreman/lib/satellite/version.rb
 
-            b. Check the version of package foreman, candlepin and katello,
-               when the files listed in (a) are not found.
+            b. Check the version of package foreman, candlepin and katello, E.g.
                - https://access.redhat.com/articles/1343683
-                 NOTE: There are some mistakes in the KCS, the versions in this
-                       combiner are corrected according to the corresponding
-                       ERRATA.
 
                             Sat 6.0.8   Sat 6.1.10  Sat 6.1.11
                foreman      1.6.0.53    1.7.2.61    1.7.2.62
                candlepin    0.9.23      0.9.49.16   0.9.49.19
                katello      1.5.0       2.2.0       2.2.0
 
-        2. For Satellite 6.2.x:
+        2. For Satellite 6.2 and newer:
+
+           Check the version of satellite/satellite-capsule directly:
            - https://access.redhat.com/solutions/1392633
 
-           Check the version of package `satellite` directly:
-
-                            Sat 6.0.x   Sat 6.1.x   Sat 6.2.x
-                satellite   -           -           6.2.x
+                                    Sat 6.0.x   Sat 6.1.x   Sat 6.2.x
+                satellite           -           -           6.2.x
+                satellite-capsule   -           -           6.2.x
 
         3. For Satellite 5.x
            - https://access.redhat.com/solutions/1224043
-             NOTE: Because of `satellite-branding` is not deployed in Satellite
-                   5.0~5.2, and `satellite-schema` can also be used for checking
-                   the version, here checked `satellite-schema` instead of
-                   `satellite-branding`.
+             NOTE: Because of satellite-branding is not deployed in Satellite
+                   5.0~5.2, and satellite-schema can also be used for checking
+                   the version, here checked satellite-schema instead of
+                   satellite-branding.
 
            Check the version of package satellite-schema directly:
 
@@ -103,7 +106,6 @@ class SatelliteVersion(object):
             However, this type of Exceptions will be ignored.
 
     Examples:
-        >>> sat_ver = shared[satellite_version]
         >>> sat_ver.major
         6
         >>> sat_ver.minor
@@ -116,53 +118,49 @@ class SatelliteVersion(object):
         '1.0.el7sat'
 
     """
-    def __init__(self, sat6_ver, rpms):
+    def __init__(self, rpms, sat6_ver):
         self.full = None
         self.version = None
         self.release = None
         self.major = None
         self.minor = None
+        self.is_capsule = 'satellite-capsule' in rpms
 
-        # For Satellite 6.1.x, if satellite_version/version.rb is available:
+        # For Satellite 6.1, if satellite_version/version.rb is available:
         if sat6_ver:
-            # no 'release' in this situation
+            # no 'release' in this case, but more accurate
             self.full = sat6_ver.full
             self.version = sat6_ver.version
             self.major = sat6_ver.major
             self.minor = sat6_ver.minor
-            return
-        if rpms:
-            # For Satellite 6.2.x, check the ``satellite`` package directly
-            sat62_pkg = rpms.get_max('satellite')
+        else:
+            # For Satellite 6.2 and newer, check the satellite package directly
+            # - for Capsule, ONLY 6.2 and newer are supported
+            sat62_pkg = rpms.get_max('satellite') or rpms.get_max('satellite-capsule')
             if sat62_pkg:
                 self.full = sat62_pkg.package
                 self.version = sat62_pkg.version
                 self.release = sat62_pkg.release
                 self.major, self.minor = _parse_sat_version(self.version)
-                return
-            # For Satellite 6.0.x/6.1.x, check the version of:
-            # - foreman, candlepin and katello
-            fman = rpms.get_max('foreman')
-            cndp = rpms.get_max('candlepin')
-            ktlo = rpms.get_max('katello')
-            if fman and cndp and ktlo:
-                for sat_ver, map_ver in sat6_ver_map.items():
-                    if all(pkg.version.startswith(mv) for pkg, mv
-                            in zip([fman, cndp, ktlo], map_ver)):
-                        # no 'release' in this situation
-                        self.major, self.minor = _parse_sat_version(sat_ver)
-                        self.full = self.version = sat_ver
-                        return
-                # satellite packages conflict
-                raise SkipComponent("RPMs conflict, unable to determine Satellite version")
-            # For Satellite 5.x
-            sat5_pkg = rpms.get_max('satellite-schema')
-            sat5_pkg = sat5_pkg if sat5_pkg else rpms.get_max('rhn-satellite-schema')
-            if sat5_pkg:
-                self.full = sat5_pkg.package
-                self.version = sat5_pkg.version
-                self.release = sat5_pkg.release
-                self.major, self.minor = _parse_sat_version(self.version)
-                return
-            raise SkipComponent("Not a Satellite machine")
-        raise SkipComponent("No RPMs list, unable to determine Satellite version")
+            else:
+                # For Satellite 6.0/6.1, check the version of:
+                # - foreman, candlepin and katello
+                fman = rpms.get_max('foreman')
+                cndp = rpms.get_max('candlepin')
+                ktlo = rpms.get_max('katello')
+                if fman and cndp and ktlo:
+                    for sat_ver, map_ver in sat6_ver_map.items():
+                        if all(pkg.version.startswith(mv) for pkg, mv in zip([fman, cndp, ktlo], map_ver)):
+                            # no 'release' in this situation
+                            self.major, self.minor = _parse_sat_version(sat_ver)
+                            self.full = self.version = sat_ver
+                else:
+                    # For Satellite 5.x
+                    sat5_pkg = rpms.get_max('satellite-schema') or rpms.get_max('rhn-satellite-schema')
+                    if sat5_pkg:
+                        self.full = sat5_pkg.package
+                        self.version = sat5_pkg.version
+                        self.release = sat5_pkg.release
+                        self.major, self.minor = _parse_sat_version(self.version)
+        if not self.full:
+            raise SkipComponent("Not a Satellite machine or unable to determine Satellite version")
