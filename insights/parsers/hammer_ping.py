e6cff609126f803dc4eb86fcc9ae2440a45920df
Huanhuan Li <huali@redhat.com>
2019-12-03 17:35:16 +0800

    Enhance hammer ping
    * Fixes 2358
    * Sometimes there are only two lines instead of three lines for some service
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/parsers/hammer_ping.py
+++ insights/parsers/hammer_ping.py
@@ -2,11 +2,8 @@
 HammerPing - command ``/usr/bin/hammer ping``
 =============================================
 
-The hammer ping parser reads the output of ``hammer ping`` and turns it into
-a dictionary of service statuses in the ``status_of_service`` property and
-the corresponding response in the ``response_of_service`` property.  The list
-of services by name in the order they were discovered is given in the
-``service_list`` property.
+The hammer ping parser reads the output of ``hammer ping``.  The list
+of services is given in the ``service_list`` property by order.
 
 Sample output of ``hammer ping``::
 
@@ -25,9 +22,9 @@ Examples:
     >>> hammer = shared[HammerPing]
     >>> 'unknown_service' in hammer.service_list
     False
-    >>> hammer.status_of_service['candlepin']
+    >>> hammer['candlepin']['Status']
     'FAIL'
-    >>> hammer.response_of_service['candlepin']
+    >>> hammer['candlepin']['Server Response']
     'Message: 404 Resource Not Found'
     >>> hammer.are_all_ok
     False
@@ -40,14 +37,12 @@ from insights.specs import Specs
 
 
 @parser(Specs.hammer_ping)
-class HammerPing(CommandParser):
+class HammerPing(CommandParser, dict):
     """
     Read the ``hammer ping`` status and convert it to dictionaries of
     status and response information.
 
     Attributes:
-        status_of_service (dict): The status of each service, converted to lower case
-        response_of_service (dict): The response of each service, as is
         service_list (list): The list of service names in order
         errors (list): Any error messages encountered during parsing
         are_all_ok (bool): Are all services read correctly and are they all in 'ok' state?
@@ -63,40 +58,27 @@ class HammerPing(CommandParser):
 
         Returns: List of service names having that status.
         """
-        status = status.lower()
-        return [svc for svc in self.service_list
-                if self.status_of_service[svc] == status]
+        return sorted([svc for svc in self if self[svc]['Status'].lower() == status.lower()])
 
     def parse_content(self, content):
-        self.status_of_service = {}
-        self.response_of_service = {}
         self.service_list = []
         self.errors = []
-        self.are_all_ok = False
 
-        # The first line should be a service
         content = get_active_lines(content, comment_char="COMMAND>")
-        if len(content[0].split()) > 1:
-            self.errors.append(content[0])
-        # Each service occupies 3 lines
-        elif len(content) % 3 == 0:
-            lines = iter(content)
-            for service_no in range(0, len(content) // 3):
-                service_line = next(lines).strip()
-                status_line = next(lines).strip()
-                response_line = next(lines).strip()
-
-                service = service_line.split(':')[0]
-                status = status_line.split(':', 1)[-1].strip().lower()
-                response = response_line.split(':', 1)[-1].strip()
-
-                self.service_list.append(service)
-                self.status_of_service[service] = status
-                self.response_of_service[service] = response
-        else:
-            self.errors.extend(content)
-
+        service_name = None
+        for line in content:
+            items = [item.strip() for item in line.split(':', 1)]
+            if len(items) == 2 and items[0]:
+                if not items[1] and items[0] not in ['Status', 'Server Response']:
+                    service_name = items[0]
+                    self[service_name] = {}
+                    continue
+                elif service_name is not None:
+                    self[service_name][items[0]] = items[1]
+                    continue
+            self.errors.append(line)
         self.are_all_ok = (
             not self.errors and
-            all(status == 'ok' for status in self.status_of_service.values())
+            all(self[item]['Status'] == 'ok' for item in self)
         )
+        self.service_list = sorted(list(self.keys()))
