ae8cb0a28db71c14a96d4b6a1e08d30880409df8
Jeremy Crafts <jcrafts@redhat.com>
2019-12-04 13:59:38 -0500

    try hostname if hostname -f doesn't work

--- insights/collect.py
+++ insights/collect.py
@@ -21,7 +21,7 @@ from insights import apply_configs, apply_default_enabled, dr
 from insights.core import blacklist
 from insights.core.serde import Hydration
 from insights.util import fs
-from insights.util.subproc import call
+from insights.util.subproc import call, CalledProcessError
 
 SAFE_ENV = {
     "PATH": os.path.pathsep.join([
@@ -250,7 +250,10 @@ def collect(manifest=default_manifest, tmp_path=None, compress=False, rm_conf={}
 
     to_persist = get_to_persist(client.get("persist", set()))
 
-    hostname = call("hostname -f", env=SAFE_ENV).strip()
+    try:
+        hostname = call("hostname -f", env=SAFE_ENV).strip()
+    except CalledProcessError:
+        hostname = call("hostname", env=SAFE_ENV).strip()
     suffix = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     relative_path = "insights-%s-%s" % (hostname, suffix)
     tmp_path = tmp_path or tempfile.gettempdir()
