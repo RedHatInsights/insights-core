c1aa57cb29512f01b2343c438a4a788b13d6e844
shlao <shlao@redhat.com>
2019-12-12 18:16:54 +0800

    Add doc for class ReadLinkEMtab
    Signed-off-by: shlao <shlao@redhat.com>

--- insights/parsers/readlink_e_mtab.py
+++ insights/parsers/readlink_e_mtab.py
@@ -20,6 +20,7 @@ from insights import parser, CommandParser
 
 @parser(Specs.readlink_e_etc_mtab)
 class ReadLinkEMtab(CommandParser):
+    """Class for command: readlink -e /etc/mtab"""
     def parse_content(self, content):
         if content is None or len(content) == 0:
             raise SkipException("No Data from command: readlink -e /etc/mtab")
