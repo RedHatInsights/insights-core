cc8ba1800dfded77aada4c3106445c47ca34e46b
Stephen <tsadams@gmail.com>
2019-12-13 11:34:55 -0500

    Use utf8 in glob (#2372)
    * Change encoding in glob_file
    
    If a file contains a unicode symbol, it gets corrupted in rules that try
    to read it. By encoding with utf-8 instead of ascii, the symbol is
    properly decoded by the rule.
    
    This ties to this JIRA issue:
    https://projects.engineering.redhat.com/browse/RHCLOUD-3610
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Use codecs.open for glob_files to ensure proper encoding
    
    It's possible that some files could contain characters that are
    improperly decoded in rules. This establishes utf-8 across the board so
    that it's less likely to misinterpret a character.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Update _stream function in glob_file
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- insights/core/spec_factory.py
+++ insights/core/spec_factory.py
@@ -4,6 +4,7 @@ import os
 import re
 import six
 import traceback
+import codecs
 
 from collections import defaultdict
 from glob import glob
@@ -243,12 +244,8 @@ class TextFileProvider(FileProvider):
             rc, out = self.ctx.shell_out(args, keep_rc=True, env=SAFE_ENV)
             self.rc = rc
             return out
-        if six.PY3:
-            with open(self.path, "r", encoding="ascii", errors="surrogateescape") as f:
-                return [l.rstrip("\n") for l in f]
-        else:
-            with open(self.path, "rU") as f:  # universal newlines
-                return [l.rstrip("\n") for l in f]
+        with codecs.open(self.path, "r", encoding="utf-8", errors="surrogateescape") as f:
+            return [l.rstrip("\n") for l in f]
 
     def _stream(self):
         """
@@ -265,12 +262,8 @@ class TextFileProvider(FileProvider):
                     with streams.connect(*args, env=SAFE_ENV) as s:
                         yield s
                 else:
-                    if six.PY3:
-                        with open(self.path, "r", encoding="ascii", errors="surrogateescape") as f:
-                            yield f
-                    else:
-                        with open(self.path, "rU") as f:  # universal newlines
-                            yield f
+                    with codecs.open(self.path, "r", encoding="utf-8", errors="surrogateescape") as f:
+                        yield f
         except StopIteration:
             raise
         except Exception as ex:
