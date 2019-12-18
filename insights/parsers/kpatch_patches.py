7f8b54f5fa6d98a0e43cc25bc984480f6ad98867
Xiangce Liu <xiangceliu@redhat.com>
2019-12-18 10:49:46 +0800

    Add kpatch_patch_files to insigths_archive and update doc (#2379)
    * add kpatch_patch_files to insigths_archive.py
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update docs
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update docs in header
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * refine the attributes and examples
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/kpatch_patches.py
+++ insights/parsers/kpatch_patches.py
@@ -3,8 +3,8 @@ KpatchPatches - report locally stored kpatch patches
 ====================================================
 
 This parser creates a list of the module names of locally
-stored kpatch modules. If no modules are installed, a
-ContentException will be raised.
+stored kpatch modules returned by command ``ls /var/lib/kpatch/\`uname -r\`/``.
+If no modules are installed, a ContentException will be raised.
 
 """
 
@@ -16,6 +16,19 @@ from insights.specs import Specs
 class KpatchPatches(CommandParser):
     """
     A parser for getting modules names of locally stored kpatch-patch files.
+
+    Sample output of `ls /var/lib/kpatch/\`uname -r\`/` looks like::
+
+        kpatch-3_10_0-1062-1-5.ko kpatch-3_10_0-1062-1-6.ko
+
+    Attributes:
+        patches (str): List of the name of kpatch patches. The dashes are
+            converted to underscores, file suffix are removed, and duplicated
+            names are removed as well
+
+    Examples:
+        >>> kp.patches
+        ['kpatch_3_10_0_1062_1_5', 'kpatch_3_10_0_1062_1_6']
     """
 
     def parse_content(self, content):
