045899f992325753530ec0ac299e133bf50f5516
Jitka Obselkova <41325380+jobselko@users.noreply.github.com>
2019-12-20 03:58:18 +0100

    Remove unused ulimit spec (#2377)
    Signed-off-by: Jitka Obselkova <jobselko@redhat.com>

--- insights/specs/insights_archive.py
+++ insights/specs/insights_archive.py
@@ -275,7 +275,6 @@ class InsightsArchiveSpecs(Specs):
     teamdctl_state_dump = glob_file("insights_commands/teamdctl_*_state_dump")
     tomcat_vdc_fallback = simple_file("insights_commands/find_.usr.share_-maxdepth_1_-name_tomcat_-exec_.bin.grep_-R_-s_VirtualDirContext_--include_.xml")
     tuned_adm = simple_file("insights_commands/tuned-adm_list")
-    ulimit_hard = simple_file("insights_commands/ulimit_-a_-H")
     uname = simple_file("insights_commands/uname_-a")
     uptime = simple_file("insights_commands/uptime")
     version_info = simple_file("version_info")
