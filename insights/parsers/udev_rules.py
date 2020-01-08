0cba4ab1f534ca8b0be2f3f0a7c5be0dac6ec5f5
Xiangce Liu <xiangceliu@redhat.com>
2020-01-08 16:48:03 +0800

    Update the docsting
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/udev_rules.py
+++ insights/parsers/udev_rules.py
@@ -1,15 +1,11 @@
 """
+UdevRules - file ``/usr/lib/udev/rules.d/``
+===========================================
+
+The parsers included in this module are:
+
 UdevRulesFCWWPN - file ``/usr/lib/udev/rules.d/59-fc-wwpn-id.rules``
-====================================================================
-
-The parser UdevRulesFCWWPN returns the content of the file "/usr/lib/udev/rules.d/59-fc-wwpn-id.rules"
-
-Examples:
-
-    >>> type(udev_rules)
-    <class 'insights.parsers.udev_rules.UdevRulesFCWWPN'>
-    >>> 'ENV{FC_TARGET_WWPN}!="$*"; GOTO="fc_wwpn_end"' in udev_rules.lines
-    True
+--------------------------------------------------------------------
 """
 from insights import parser
 from insights.core import LogFileOutput
@@ -19,8 +15,19 @@ from insights.specs import Specs
 @parser(Specs.udev_fc_wwpn_id_rules)
 class UdevRulesFCWWPN(LogFileOutput):
     """
-    Parse data from the ``/usr/lib/udev/rules.d/59-fc-wwpn-id.rules`` file.
-    For current rule, we just need to check if there is wrong syntax in this file, and don't need to
-    parse the content, so we use LogFileOutput here.
+    Read the content of ``/usr/lib/udev/rules.d/59-fc-wwpn-id.rules`` file.
+
+    .. note::
+
+        The syntax of the `.rules` file is complex, and no rules require to
+        get the serialized parsed result currently.  An only existing rule's
+        supposed to check the syntax of some specific line, so here the
+        :class:`insights.core.LogFileOutput` is the base class.
+
+    Examples:
+        >>> type(udev_rules)
+        <class 'insights.parsers.udev_rules.UdevRulesFCWWPN'>
+        >>> 'ENV{FC_TARGET_WWPN}!="$*"; GOTO="fc_wwpn_end"' in udev_rules.lines
+        True
     """
     pass
