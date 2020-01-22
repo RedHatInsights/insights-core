568f59d6e7ed48a586f423189ca5f975eab4c994
Stephen <tsadams@gmail.com>
2020-01-22 15:40:41 -0500

    Validate machine ID (#2419)
    * Add a validation step for the machine id
    
    We want to make sure that the machine ID is valid before making any API
    calls.
    
    RHCLOUD-3962
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * add a sys exit
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Add corrective steps to the error
    
    the user should be able to fix this issue on their own
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Further clarity the remediation steps
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Remove unused var
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Add unit test for bad machine_id
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * Remove unneeded file read
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    (cherry picked from commit e648c8c8a081de1a7a1a6f5792ba778a1f3046f6)

--- insights/client/utilities.py
+++ insights/client/utilities.py
@@ -135,7 +135,13 @@ def generate_machine_id(new=False,
         logger.debug("Creating %s", destination_file)
         write_to_disk(destination_file, content=machine_id)
 
-    return str(machine_id).strip()
+    try:
+        uuid.UUID(machine_id, version=4)
+        return str(machine_id).strip()
+    except ValueError:
+        logger.error("Invalid machine ID: %s", machine_id)
+        logger.error("Remove %s and a new one will be generated.\nRerun the client with --register", destination_file)
+        sys.exit(constants.sig_kill_bad)
 
 
 def _expand_paths(path):
