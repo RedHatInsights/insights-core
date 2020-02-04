20dea2bccdcafc87ba65ccc2a5432d29c1758c0e
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2020-02-04 16:00:39 -0500

    consolidate temp file deletion into single func called at exit (#2439)
    * consolidate temp file deletion into single func called at exit
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * remove leftover comment
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * remove unused imports
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * keep log message same as today, point at file
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix var name
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix InsightsArchive() call in compliance module
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -285,13 +285,6 @@ def collect_and_output(client, config):
     if resp:
         if config.to_json:
             print(json.dumps(resp))
-
-        if not config.payload:
-            # delete the archive
-            if config.keep_archive:
-                logger.info('Insights archive retained in ' + insights_archive)
-            else:
-                client.delete_archive(insights_archive, delete_parent_dir=True)
     client.delete_cached_branch_info()
 
     # rotate eggs once client completes all work successfully
