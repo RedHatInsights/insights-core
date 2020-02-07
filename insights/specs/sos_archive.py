3511f4ac895e1c4a3d01b532fb859f1dea3ec75c
vishwanathjadhav <vjadhav@redhat.com>
2020-02-07 13:34:00 +0530

    Parser for  ip neigh show (#2448)
    * Added parser for ip neigh show
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    
    * Added test case for new parser specs ip neigh show
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    
    * Added test case for new parser specs ip neigh show
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    
    * Added test case and signer
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>
    
    Pass DCO
    
    * Test case
    
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>

--- insights/specs/sos_archive.py
+++ insights/specs/sos_archive.py
@@ -62,6 +62,7 @@ class SosSpecs(Specs):
     httpd_M = simple_file("sos_commands/apache/apachectl_-M")
     installed_rpms = first_file(["sos_commands/rpm/package-data", "installed-rpms"])
     ip_addr = first_of([simple_file("sos_commands/networking/ip_-d_address"), simple_file("sos_commands/networking/ip_address")])
+    ip_neigh_show = first_file(["sos_commands/networking/ip_-s_-s_neigh_show", "sos_commands/networking/ip_neigh_show"])
     ip_route_show_table_all = simple_file("sos_commands/networking/ip_route_show_table_all")
     ip_s_link = first_of([simple_file("sos_commands/networking/ip_-s_-d_link"), simple_file("sos_commands/networking/ip_-s_link"), simple_file("sos_commands/networking/ip_link")])
     iptables = first_file(["/etc/sysconfig/iptables", "/etc/sysconfig/iptables.save"])
