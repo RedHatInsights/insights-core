355df909c134f2dbb50afea466a692f7686dc1d0
Huanhuan Li <huali@redhat.com>
2020-02-11 11:12:45 +0800

    Add parser for "/etc/firewalld/firewalld.conf"
    Signed-off-by: Huanhuan Li <huali@redhat.com>

---
+++ insights/parsers/firewall_config.py
@@ -0,0 +1,30 @@
+"""
+FirewallDConf - file ``/etc/firewalld/firewalld.conf``
+======================================================
+
+The FirewallDConf class parses the file ``/etc/firewalld/firewalld.conf``.
+And returns a dict contains the firewall configurations.
+
+Examples:
+
+    >>> type(firewalld)
+    <class 'insights.parsers.firewall_config.FirewallDConf'>
+    >>> 'DefaultZone' in firewalld
+    True
+    >>> firewalld['DefaultZone']
+    'public'
+"""
+
+from insights.specs import Specs
+from insights import Parser, parser
+from insights.parsers import split_kv_pairs
+
+
+@parser(Specs.firewalld_conf)
+class FirewallDConf(Parser, dict):
+    """
+    Class for parsing ``/etc/firewalld/firewalld.conf`` file.
+    """
+
+    def parse_content(self, content):
+        self.update(split_kv_pairs(content, use_partition=False))
