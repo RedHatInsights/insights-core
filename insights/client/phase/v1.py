c972a39d4c607b0af4debfcc3b960c23c43c3092
Stephen Adams <tsadams@gmail.com>
2020-02-12 14:40:52 -0500

    Fix syntax error
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -69,7 +69,7 @@ def pre_update(client, config):
             sys.exit(constants.sig_kill_bad)
     
     if os.path.isfile(config.remove_file):
-        if not validate_remove_file(config.remove_file:
+        if not validate_remove_file(config.remove_file):
             sys.exit(constants.sig_kill_bad)
 
     # handle cron stuff
