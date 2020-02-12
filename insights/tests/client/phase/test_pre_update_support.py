df00d2c443d9729bf87328581acbdfa516f42cd4
Stephen Adams <tsadams@gmail.com>
2020-02-12 14:41:01 -0500

    Fix pre-update tests
    Needed to throw in a fake remove.conf since we don't expect it to be
    there and we now test it all the time if it exists.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- insights/tests/client/phase/test_pre_update_support.py
+++ insights/tests/client/phase/test_pre_update_support.py
@@ -15,7 +15,8 @@ def patch_insights_config(old_function):
                        "return_value.load_all.return_value.disable_schedule": False,
                        "return_value.load_all.return_value.analyze_container": False,
                        "return_value.load_all.return_value.test_connection": False,
-                       "return_value.load_all.return_value.support": True})
+                       "return_value.load_all.return_value.support": True,
+                       "return_value.load_all.return_value.remove_file": '/tmp/foo'})
     return patcher(old_function)
 
 
