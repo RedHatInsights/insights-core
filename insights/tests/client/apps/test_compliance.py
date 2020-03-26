8c8809f8b76a1db451ce6640f85cf1f882cc2a98
Daniel Lobato García <me@daniellobato.me>
2020-03-26 14:29:34 +0100

    compliance: Download tailoring file for tailored policies (#2492)
    * compliance: Download tailoring file for tailored policies
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Add tailored to testing mock responses
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Add Tailoring file path to mock lambda
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Adapt responses to new API endpoint
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Fix test syntax
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Correct search parameter
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Modify get_policies response test
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Add tests for download and buliding tailoring command
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Added test for request without 'tailored'
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>
    
    * Update mocking library
    
    Signed-off-by: Daniel Lobato Garcia <me@daniellobato.me>

--- insights/tests/client/apps/test_compliance.py
+++ insights/tests/client/apps/test_compliance.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 from insights.client.apps.compliance import ComplianceClient, COMPLIANCE_CONTENT_TYPE
-from mock.mock import patch, Mock
+from mock.mock import patch, Mock, mock_open
 from pytest import raises
 import os
 
@@ -12,9 +12,9 @@ PATH = '/usr/share/xml/scap/ref_id.xml'
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None, compressor='gz')
 def test_oscap_scan(config, assert_rpms):
     compliance_client = ComplianceClient(config)
-    compliance_client.get_policies = lambda: [{'ref_id': 'foo'}]
+    compliance_client.get_policies = lambda: [{'attributes': {'ref_id': 'foo', 'tailored': False}}]
     compliance_client.find_scap_policy = lambda ref_id: '/usr/share/xml/scap/foo.xml'
-    compliance_client.run_scan = lambda ref_id, policy_xml, output_path: None
+    compliance_client.run_scan = lambda ref_id, policy_xml, output_path, tailoring_file_path: None
     compliance_client.archive.archive_tmp_dir = '/tmp'
     compliance_client.archive.archive_name = 'insights-compliance-test'
     archive, content_type = compliance_client.oscap_scan()
@@ -26,7 +26,7 @@ def test_oscap_scan(config, assert_rpms):
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
 def test_missing_packages(config, call):
     compliance_client = ComplianceClient(config)
-    compliance_client.get_policies = lambda: [{'ref_id': 'foo'}]
+    compliance_client.get_policies = lambda: [{'attributes': {'ref_id': 'foo'}}]
     compliance_client.find_scap_policy = lambda ref_id: '/usr/share/xml/scap/foo.xml'
     compliance_client.run_scan = lambda ref_id, policy_xml: None
     with raises(SystemExit):
@@ -37,7 +37,7 @@ def test_missing_packages(config, call):
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
 def test_errored_rpm_call(config, call):
     compliance_client = ComplianceClient(config)
-    compliance_client.get_policies = lambda: [{'ref_id': 'foo'}]
+    compliance_client.get_policies = lambda: [{'attributes': {'ref_id': 'foo'}}]
     compliance_client.find_scap_policy = lambda ref_id: '/usr/share/xml/scap/foo.xml'
     compliance_client.run_scan = lambda ref_id, policy_xml: None
     with raises(SystemExit):
@@ -48,9 +48,9 @@ def test_errored_rpm_call(config, call):
 def test_get_policies(config):
     compliance_client = ComplianceClient(config)
     compliance_client.hostname = 'foo'
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': [{'attributes': {'profiles': 'data'}}]})))
-    assert compliance_client.get_policies() == 'data'
-    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/systems', params={'search': 'name=foo'})
+    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': [{'attributes': 'data'}]})))
+    assert compliance_client.get_policies() == [{'attributes': 'data'}]
+    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/profiles', params={'search': 'system_names=foo'})
 
 
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
@@ -59,7 +59,7 @@ def test_get_policies_no_policies(config):
     compliance_client.hostname = 'foo'
     compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': []})))
     assert compliance_client.get_policies() == []
-    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/systems', params={'search': 'name=foo'})
+    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/profiles', params={'search': 'system_names=foo'})
 
 
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
@@ -68,7 +68,7 @@ def test_get_policies_error(config):
     compliance_client.hostname = 'foo'
     compliance_client.conn.session.get = Mock(return_value=Mock(status_code=500))
     assert compliance_client.get_policies() == []
-    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/systems', params={'search': 'name=foo'})
+    compliance_client.conn.session.get.assert_called_with('https://localhost/app/compliance/profiles', params={'search': 'system_names=foo'})
 
 
 @patch("insights.client.apps.compliance.linux_distribution", return_value=(None, '6.5', None))
@@ -123,3 +123,38 @@ def test_run_scan_fail(config, call):
     with raises(SystemExit):
         compliance_client.run_scan('ref_id', '/nonexistent', output_path)
     call.assert_called_with("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent', keep_rc=True, env=env)
+
+
+@patch("insights.client.config.InsightsConfig")
+def test_tailored_file_is_not_downloaded_if_not_needed(config):
+    compliance_client = ComplianceClient(config)
+    assert compliance_client.download_tailoring_file({'attributes': {'tailored': False}}) is None
+
+
+@patch("insights.client.config.InsightsConfig")
+def test_tailored_file_is_not_downloaded_if_tailored_is_missing(config):
+    compliance_client = ComplianceClient(config)
+    assert compliance_client.download_tailoring_file({'id': 'foo', 'attributes': {'ref_id': 'aaaaa'}}) is None
+
+
+@patch("insights.client.apps.compliance.open", new_callable=mock_open)
+@patch("insights.client.config.InsightsConfig")
+def test_tailored_file_is_downloaded_if_needed(config, call):
+    compliance_client = ComplianceClient(config)
+    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': [{'attributes': 'data'}]})))
+    tailoring_file_path = "/var/tmp/oscap_tailoring_file-aaaaa.xml"
+    assert tailoring_file_path == compliance_client.download_tailoring_file({'id': 'foo', 'attributes': {'tailored': True, 'ref_id': 'aaaaa'}})
+
+
+@patch("insights.client.config.InsightsConfig")
+def test_build_oscap_command_does_not_append_tailoring_path(config):
+    compliance_client = ComplianceClient(config)
+    expected_command = 'oscap xccdf eval --profile aaaaa --results output_path xml_sample'
+    assert expected_command == compliance_client.build_oscap_command('aaaaa', 'xml_sample', 'output_path', None)
+
+
+@patch("insights.client.config.InsightsConfig")
+def test_build_oscap_command_append_tailoring_path(config):
+    compliance_client = ComplianceClient(config)
+    expected_command = 'oscap xccdf eval --profile aaaaa --tailoring-file tailoring_path --results output_path xml_sample'
+    assert expected_command == compliance_client.build_oscap_command('aaaaa', 'xml_sample', 'output_path', 'tailoring_path')
