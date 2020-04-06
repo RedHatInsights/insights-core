c9659555021324073929dc07c238cedc47e8dbcd
Stephen Adams <tsadams@gmail.com>
2020-04-06 15:02:50 -0400

    Allow checks for remove.conf and tags.conf at the same time
    If we exit the check right after remove.conf failures, we won't be able
    to see the tag failures. This change allows us to check both before
    returning False
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- insights/client/collection_rules.py
+++ insights/client/collection_rules.py
@@ -413,19 +413,22 @@ class InsightsUploadConf(object):
         '''
         Validate remove.conf and tags.conf
         '''
+        failures = []
         if not os.path.isfile(self.remove_file):
             logger.warn("WARNING: Remove file does not exist")
-            return False
+            failures.append("WARNING: Remove file does not exist")
         if not os.path.isfile(self.tags_file):
             logger.warn("WARNING: Tags file does not exist")
-            return False
+            failures.append("WARNING: Tags file does not exist")
         else:
             with open(self.tags_file) as f:
                 data = f.read()
                 tags = yaml.load(data, Loader=yaml.CLoader)
             if type(tags) != dict:
-                logger.error("ERROR: Tags file not in yaml format")
-                return False
+                failures.append("ERROR: Tags file not in yaml format")
+        if failures:
+            logger.warn(failure for failure in failures)
+            return False
         # Make sure permissions are 600
         mode = stat.S_IMODE(os.stat(self.remove_file).st_mode)
         if not mode == 0o600:
