32f387e8c4c5b5a9f17ab95284efecbfda8a48ae
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2020-04-08 15:50:47 -0400

    Split remove conf (#2481)
    * split yaml remove.conf into two new files
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * add create_report function to InsightsUploadConf
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * write report to archive, tweak parsing, skip remove.conf if empty
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * move empty file warnings to debug level, fix report function
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix line
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * update --validate function
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * start to update unit tests
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix perms check, return None for empty conf instead of {}
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * warning when running with invalid perms, error when using --validate
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * tweak old rm_conf parse, unit tests, make report into json dump
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * DRY the conf load functions
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * add kb link to deprecation warning
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * add message to use new formats to use YAML
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * exit 0 on --validate and empty configuration
    change "remove file" in logging to "blacklist configuration"
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * remove extra call to validate
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix unit tests, skip mock_open failures in QE
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix unit tests some more, improve error handling
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix imports, definitions
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * flake
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * use .yaml file extension
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * add components to file-redaction.conf
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * change more *redaction.conf references to .yaml
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- insights/tests/client/test_skip_commands_files.py
+++ insights/tests/client/test_skip_commands_files.py
@@ -16,7 +16,7 @@ def test_omit_before_expanded_paths(InsightsFile, parse_file_spec):
 
     collection_rules = {'files': [{"file": "/etc/pam.d/vsftpd", "pattern": [], "symbolic_name": "vsftpd"}], 'commands': {}}
     rm_conf = {'files': ["/etc/pam.d/vsftpd"]}
-    data_collector.run_collection(collection_rules, rm_conf, {})
+    data_collector.run_collection(collection_rules, rm_conf, {}, '')
     parse_file_spec.assert_not_called()
     InsightsFile.assert_not_called()
 
@@ -32,7 +32,7 @@ def test_omit_after_expanded_paths(InsightsFile, parse_file_spec):
 
     collection_rules = {'files': [{"file": "/etc/yum.repos.d/()*.*\\.repo", "pattern": [], "symbolic_name": "yum_repos_d"}], 'commands': {}}
     rm_conf = {'files': ["/etc/yum/repos.d/test.repo"]}
-    data_collector.run_collection(collection_rules, rm_conf, {})
+    data_collector.run_collection(collection_rules, rm_conf, {}, '')
     parse_file_spec.assert_called_once()
     InsightsFile.assert_not_called()
 
@@ -51,7 +51,7 @@ def test_omit_symbolic_name(InsightsCommand, InsightsFile, parse_file_spec):
                         'commands': [{"command": "/sbin/chkconfig --list", "pattern": [], "symbolic_name": "chkconfig"}],
                         'pre_commands': []}
     rm_conf = {'files': ["vsftpd"], "commands": ["chkconfig"]}
-    data_collector.run_collection(collection_rules, rm_conf, {})
+    data_collector.run_collection(collection_rules, rm_conf, {}, '')
     parse_file_spec.assert_not_called()
     InsightsFile.assert_not_called()
     InsightsCommand.assert_not_called()
@@ -71,7 +71,7 @@ def test_symbolic_name_bc(InsightsArchive, InsightsFile, InsightsCommand):
                         'commands': [{"command": "/sbin/chkconfig --list", "pattern": []}],
                         'pre_commands': []}
     rm_conf = {'files': ["vsftpd"], "commands": ["chkconfig"]}
-    data_collector.run_collection(collection_rules, rm_conf, {})
+    data_collector.run_collection(collection_rules, rm_conf, {}, '')
     InsightsFile.assert_called_once()
     InsightsCommand.assert_called_once()
     InsightsArchive.return_value.add_to_archive.assert_has_calls(
@@ -125,5 +125,5 @@ def test_omit_after_parse_command(InsightsCommand, run_pre_command):
 
     collection_rules = {'commands': [{"command": "/sbin/ethtool -i", "pattern": [], "pre_command": "iface", "symbolic_name": "ethtool"}], 'files': [], "pre_commands": {"iface": "/sbin/ip -o link | awk -F ': ' '/.*link\\/ether/ {print $2}'"}}
     rm_conf = {'commands': ["/sbin/ethtool -i eth0"]}
-    data_collector.run_collection(collection_rules, rm_conf, {})
+    data_collector.run_collection(collection_rules, rm_conf, {}, '')
     InsightsCommand.assert_not_called()
