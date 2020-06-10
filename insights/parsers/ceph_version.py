76faca28972e32b13a7cb81b9d1289abc3c545e3
Xiangce Liu <xiangceliu@redhat.com>
2020-06-10 09:39:54 +0800

    Update the deprecated/useless items in ceph_version (#2618)
    * Update the deprecated/useless items in ceph_version
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Mark CephVersionError as deprecated
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/ceph_version.py
+++ insights/parsers/ceph_version.py
@@ -1,6 +1,6 @@
 """
-CephVersion - command ``/usr/bin/ceph -v``
-==========================================
+CephVersion - command ``ceph -v``
+=================================
 
 This module provides plugins access to the Ceph version information gathered from
 the ``ceph -v`` command. This module parses the community version to the Red Hat
@@ -9,27 +9,16 @@ release version.
 The Red Hat Ceph Storage releases and corresponding Ceph package releases are
 documented in https://access.redhat.com/solutions/2045583
 
-Typical output of the ``ceph -v`` command is::
-
-    ceph version 0.94.9-9.el7cp (b83334e01379f267fb2f9ce729d74a0a8fa1e92c)
-
-Note:
-    This module can only be used for Ceph.
-
-Example:
-    >>> ceph_ver = shared[CephVersion]
-    >>> ceph_ver.version
-    '1.3.3'
-    >>> ceph_ver.major
-    '1.3'
-    >>> ceph_ver.minor
-    '3'
 """
 
-from .. import parser, CommandParser
 import re
+from insights import parser, CommandParser
+from insights.parsers import SkipException
 from insights.specs import Specs
+from insights.util import deprecated
 
+# TODO: the following metrics need update timely per:
+# - https://access.redhat.com/solutions/2045583
 community_to_release_map = {
     "0.94.1-15": {'version': "1.3.0", 'major': '1.3', 'minor': '0', 'downstream_release': 'NA'},
     "0.94.3-3": {'version': "1.3.1", 'major': '1.3', 'minor': '1', 'downstream_release': 'NA'},
@@ -64,45 +53,40 @@ community_to_release_map = {
 }
 
 
-class CephVersionError(Exception):
-    """
-    Exception subclass for errors related to the content data and the
-    CephVersion class.
-
-    This exception should not be caught by rules plugins unless it is necessary
-    for the plugin to return a particular answer when a problem occurs with
-    ceph version data.  If a plugin catches this exception it must reraise it so that
-    the engine has the opportunity to handle it/log it as necessary.
-    """
-
-    def __init__(self, message, errors):
-        """Class constructor"""
-
-        super(CephVersionError, self).__init__(message)
-        self.errors = errors
-        self.message = message
-
-
 @parser(Specs.ceph_v)
 class CephVersion(CommandParser):
-    """ Class for parsing the content of ``ceph_version``."""
+    """
+    Class for parsing the output of command ``ceph -v``.
+
+    Typical output of the ``ceph -v`` command is::
+
+        ceph version 0.94.9-9.el7cp (b83334e01379f267fb2f9ce729d74a0a8fa1e92c)
+
+    Example:
+        >>> ceph_v.version
+        '1.3.3'
+        >>> ceph_v.major
+        '1.3'
+        >>> ceph_v.minor
+        '3'
+    """
 
     def parse_content(self, content):
         # Parse Ceph Version Content and get Release, Major, Minor number
         if not content:
-            raise CephVersionError("Empty Ceph Version Line", content)
+            raise SkipException("Empty Ceph Version Line", content)
 
         ceph_version_line = content[-1]
         # re search pattern
         pattern_community = r'((\d{1,2})\.(\d{1,2})\.((\d{1,2})|x))((\-(\d+)))'
         community_version_mo = re.search(pattern_community, str(ceph_version_line), 0)
         if not community_version_mo:
-            raise CephVersionError("Wrong Format Ceph Version", content)
+            raise SkipException("Wrong Format Ceph Version", content)
 
         community_version = community_version_mo.group(0)
         release_data = community_to_release_map.get(community_version, None)
         if not release_data:
-            raise CephVersionError("No Mapping Release Version. Ceph Release Number is Null", content)
+            raise SkipException("No Mapping Release Version. Ceph Release Number is Null", content)
 
         self.version = release_data['version']
         self.major = release_data['major']
@@ -113,3 +97,14 @@ class CephVersion(CommandParser):
             "major": int(community_version_mo.group(3)),
             "minor": int(community_version_mo.group(4))
         }
+
+
+class CephVersionError(Exception):
+    """
+    .. note::
+        This class is deprecated, please use :py:class:`insights.parsers.SkipException` instead.
+    """
+
+    def __init__(self, *args, **kwargs):
+        deprecated(CephVersionError, "Use SkipException instead.")
+        super(CephVersionError, self).__init__(*args, **kwargs)
