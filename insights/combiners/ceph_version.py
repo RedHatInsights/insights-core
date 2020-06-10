995d4289d7c80a29928900a5ba44dd05e06c5345
Xiangce Liu <xiangceliu@redhat.com>
2020-06-10 13:44:03 +0800

    Add is_els to CephVersion (#2621)
    * Add is_els to CephVersion
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Add test case for CephVersion.is_els
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/ceph_version.py
+++ insights/combiners/ceph_version.py
@@ -6,23 +6,6 @@ Combiner for Ceph Version information. It uses the results of
 the ``CephVersion``, ``CephInsights`` and ``CephReport`` parsers.
 The order from most preferred to least preferred is `CephVersion``, ``CephInsights``, ``CephReport``.
 
-Examples:
-    >>> type(cv)
-    <class 'insights.combiners.ceph_version.CephVersion'>
-    >>> cv.version
-    '3.2'
-    >>> cv.major
-    '3'
-    >>> cv.minor
-    '2'
-    >>> cv.downstream_release
-    '0'
-    >>> cv.upstream_version["release"]
-    12
-    >>> cv.upstream_version["major"]
-    2
-    >>> cv.upstream_version["minor"]
-    8
 """
 
 from insights import combiner
@@ -37,7 +20,37 @@ class CephVersion(object):
     """
     Combiner for Ceph Version information. It uses the results of
     the ``CephVersion``, ``CephInsights`` and ``CephReport`` parsers.
-    The order from most preferred to least preferred is `CephVersion``, ``CephInsights``, ``CephReport``.
+
+    The prefered parsing order is `CephVersion``, ``CephInsights``, ``CephReport``.
+
+    Attributes:
+        version (str): The Red Hat release version
+        major (str): The major version of Red Hat release version
+        minor (str): The minor version of Red Hat release version
+        is_els (boolean): If the verion in 'Extended life cycle support (ELS) add-on' phase
+        downstream_release (str): The downstream release info
+        upstream_version (dict): The detailed upstream version info with the
+            following keys `release (int)`, `major (int)` and `minor (int)`.
+
+    Examples:
+        >>> type(cv)
+        <class 'insights.combiners.ceph_version.CephVersion'>
+        >>> cv.version
+        '3.2'
+        >>> cv.major
+        '3'
+        >>> cv.minor
+        '2'
+        >>> cv.is_els
+        False
+        >>> cv.downstream_release
+        '0'
+        >>> cv.upstream_version["release"]
+        12
+        >>> cv.upstream_version["major"]
+        2
+        >>> cv.upstream_version["minor"]
+        8
     """
 
     def __init__(self, cv, ci, cr):
@@ -45,6 +58,7 @@ class CephVersion(object):
             self.version = cv.version
             self.major = cv.major
             self.minor = cv.minor
+            self.is_els = cv.is_els
             self.downstream_release = cv.downstream_release
             self.upstream_version = cv.upstream_version
         elif ci:
@@ -53,6 +67,7 @@ class CephVersion(object):
             self.version = cv.version
             self.major = cv.major
             self.minor = cv.minor
+            self.is_els = cv.is_els
             self.downstream_release = cv.downstream_release
             self.upstream_version = cv.upstream_version
         else:
@@ -61,5 +76,6 @@ class CephVersion(object):
             self.version = cv.version
             self.major = cv.major
             self.minor = cv.minor
+            self.is_els = cv.is_els
             self.downstream_release = cv.downstream_release
             self.upstream_version = cv.upstream_version
