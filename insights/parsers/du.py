76552fd0bf07a01006d756e999933aef00f41c71
Rohan Arora <roarora@redhat.com>
2020-06-17 15:35:03 +0530

    Correct doc syntax
    Signed-off-by: Rohan Arora <roarora@redhat.com>

--- insights/parsers/du.py
+++ insights/parsers/du.py
@@ -7,7 +7,7 @@ Module for the processing of output from the ``du`` command.
 Parsers provided by this module include:
 
 DiskUsageDir - command ``du -s -k {directory}``
---------------------------------
+-----------------------------------------------
 
 """
 from insights import parser, CommandParser
