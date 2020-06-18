99fc80fa22ae5f02f72edfe7b4ff5bed9bd17ed8
Jesse Jaggars <jjaggars@redhat.com>
2020-06-18 09:55:26 -0400

    This PR adds a new lvm parser that depends on the lvmconfig command. (#2575)
    * adding lvmconfig
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * first pass at a parser
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * crutching on json to load the data
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * updating tests
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * inferring warnings via whitespace, rather than hardcoding a single line
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * moving lvm test data to it's own file so I can ignore flake8 errors about its formatting
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * adding spec to archive specs
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    (cherry picked from commit 4825dbbf1d77e6e6aca60500bbb1ba7128b8ba64)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -570,6 +570,10 @@ class DefaultSpecs(Specs):
     lsscsi = simple_command("/usr/bin/lsscsi")
     lvdisplay = simple_command("/sbin/lvdisplay")
     lvm_conf = simple_file("/etc/lvm/lvm.conf")
+    lvmconfig = first_of([
+        simple_command("/usr/sbin/lvmconfig --type full"),
+        simple_command("/usr/sbin/lvm dumpconfig --type full"),
+    ])
     lvs = None  # simple_command('/sbin/lvs -a -o +lv_tags,devices --config="global{locking_type=0}"')
     lvs_noheadings = simple_command("/sbin/lvs --nameprefixes --noheadings --separator='|' -a -o lv_name,lv_size,lv_attr,mirror_log,vg_name,devices,region_size,data_percent,metadata_percent,segtype,seg_monitor --config=\"global{locking_type=0}\"")
     lvs_noheadings_all = simple_command("/sbin/lvs --nameprefixes --noheadings --separator='|' -a -o lv_name,lv_size,lv_attr,mirror_log,vg_name,devices,region_size,data_percent,metadata_percent,segtype --config='global{locking_type=0} devices{filter=[\"a|.*|\"]}'")
