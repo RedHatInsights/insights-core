9c09ae0ee2c7e425096ff4530579525c9507c4c1
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2020-06-18 13:56:29 -0400

    create thread for sdnotify (#2561)
    * create thread for sdnotify
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- insights/tests/client/test_client.py
+++ insights/tests/client/test_client.py
@@ -9,7 +9,7 @@ from insights.client.config import InsightsConfig
 from insights import package_info
 from insights.client.constants import InsightsConstants as constants
 from insights.client.utilities import generate_machine_id
-from mock.mock import patch, Mock, mock_open, call
+from mock.mock import patch, Mock, call
 
 
 class FakeConnection(object):
@@ -408,39 +408,6 @@ def test_platform_upload(_legacy_upload, _, path_exists):
     client = InsightsClient(config)
     client.upload('test.gar.gz', 'test.content.type')
     _legacy_upload.assert_not_called()
-
-
-@patch('insights.client.client.write_to_disk')
-@patch('insights.client.client.open', new_callable=mock_open)
-@patch('insights.client.client.systemd_notify')
-@patch('insights.client.client.read_pidfile')
-@patch('insights.client.os.path.exists', return_value=True)
-@patch('insights.client.connection.InsightsConnection.upload_archive', return_value=Mock(status_code=200, text='{}'))
-def test_legacy_upload_systemd(_, path_exists, read_pidfile, systemd_notify, op, wtd):
-    '''
-    Pidfile is read and systemd-notify is called for legacy upload
-    '''
-    config = InsightsConfig(legacy_upload=True)
-    config.account_number = ''  # legacy registration thing
-    client = InsightsClient(config)
-    client.upload('test.gar.gz', 'test.content.type')
-    read_pidfile.assert_called_once()
-    systemd_notify.assert_called_once_with(read_pidfile.return_value)
-
-
-@patch('insights.client.client.systemd_notify')
-@patch('insights.client.client.read_pidfile')
-@patch('insights.client.os.path.exists', return_value=True)
-@patch('insights.client.connection.InsightsConnection.upload_archive', return_value=Mock(status_code=200))
-def test_platform_upload_systemd(_, path_exists, read_pidfile, systemd_notify):
-    '''
-    Pidfile is read and systemd-notify is called for platform upload
-    '''
-    config = InsightsConfig(legacy_upload=False)
-    client = InsightsClient(config)
-    client.upload('test.gar.gz', 'test.content.type')
-    read_pidfile.assert_called_once()
-    systemd_notify.assert_called_once_with(read_pidfile.return_value)
 
 
 @patch('insights.client.os.path.exists', return_value=True)
