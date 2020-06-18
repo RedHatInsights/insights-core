a83ec28ad1889a97e4aa5701410523293a428647
Jesse Jaggars <jjaggars@redhat.com>
2020-06-18 09:55:26 -0400

    This PR adds a new lvm parser that depends on the lvmconfig command. (#2575)
    * adding lvmconfig
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * first pass at a parser
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * crutching on json to load the data
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * updating tests
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * inferring warnings via whitespace, rather than hardcoding a single line
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * moving lvm test data to it's own file so I can ignore flake8 errors about its formatting
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    
    * adding spec to archive specs
    
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>
    (cherry picked from commit e5bfb43bca883383f1d6fcfe080a77ec4cea0850)

--- insights/specs/insights_archive.py
+++ insights/specs/insights_archive.py
@@ -154,6 +154,10 @@ class InsightsArchiveSpecs(Specs):
     lssap = simple_file("insights_commands/usr.sap.hostctrl.exe.lssap")
     lsscsi = simple_file("insights_commands/lsscsi")
     lvdisplay = simple_file("insights_commands/lvdisplay")
+    lvmconfig = first_file([
+        simple_file("insights_commands/lvmconfig_--type_full"),
+        simple_file("insights_commands/lvm_dumpconfig_--type_full"),
+    ])
     lvs_noheadings = simple_file("insights_commands/lvs_--nameprefixes_--noheadings_--separator_-a_-o_lv_name_lv_size_lv_attr_mirror_log_vg_name_devices_region_size_data_percent_metadata_percent_segtype_seg_monitor_--config_global_locking_type_0")
     lvs_noheadings_all = simple_file("insights_commands/lvs_--nameprefixes_--noheadings_--separator_-a_-o_lv_name_lv_size_lv_attr_mirror_log_vg_name_devices_region_size_data_percent_metadata_percent_segtype_--config_global_locking_type_0_devices_filter_a")
     max_uid = simple_file("insights_commands/awk_-F_if_3_max_max_3_END_print_max_.etc.passwd")
