dd8252a277411a999d6dfac328e0446e43f9d0f2
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2020-06-22 16:24:37 -0400

    ignore failure to write .registered file if non-root (#2623)
    * ignore failure to write .registered file if non-root
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * fix except block
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>
    
    * specify a permission denied failure in except block
    
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- insights/client/connection.py
+++ insights/client/connection.py
@@ -12,6 +12,7 @@ import pkg_resources
 import platform
 import xml.etree.ElementTree as ET
 import warnings
+import errno
 # import io
 from tempfile import TemporaryFile
 # from datetime import datetime, timedelta
@@ -841,7 +842,7 @@ class InsightsConnection(object):
         logger.debug("Upload duration: %s", upload.elapsed)
         return upload
 
-    def upload_archive(self, data_collected, content_type, duration):
+    def upload_archive(self, data_collected, content_type, duration=None):
         """
         Do an HTTPS Upload of the archive
         """
@@ -880,7 +881,14 @@ class InsightsConnection(object):
             # 202 from platform, no json response
             logger.debug(upload.text)
             # upload = registration on platform
-            write_registered_file()
+            try:
+                write_registered_file()
+            except OSError as e:
+                if e.errno == errno.EACCES and os.getuid() != 0:
+                    # if permissions error as non-root, ignore
+                    pass
+                else:
+                    logger.error('Could not update local registration record: %s', str(e))
         else:
             logger.debug(
                 "Upload archive failed with status code %s",
