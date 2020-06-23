80b8b17cbda16f43c17c9d5015f513dc026112e1
Xiangce Liu <xiangceliu@redhat.com>
2020-06-23 10:40:33 +0800

    New parsers for SAP trace files
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -554,6 +554,14 @@ class DefaultSpecs(Specs):
     rpm_V_packages = simple_command("/bin/rpm -V coreutils procps procps-ng shadow-utils passwd sudo chrony", keep_rc=True)
     rsyslog_conf = simple_file("/etc/rsyslog.conf")
     samba = simple_file("/etc/samba/smb.conf")
+
+    @datasource(Sap)
+    def sap_sid_name(broker):
+        sap = broker[Sap]
+        return [(sap.sid(i), i) for i in sap.local_instances]
+
+    sap_dev_disp = foreach_collect(sap_sid_name, "/usr/sap/%s/%s/work/dev_disp")
+    sap_dev_rd = foreach_collect(sap_sid_name, "/usr/sap/%s/%s/work/dev_rd")
     saphostctl_getcimobject_sapinstance = simple_command("/usr/sap/hostctrl/exe/saphostctrl -function GetCIMObject -enuminstances SAPInstance")
     sat5_insights_properties = simple_file("/etc/redhat-access/redhat-access-insights.properties")
     satellite_mongodb_storage_engine = simple_command("/usr/bin/mongo pulp_database --eval 'db.serverStatus().storageEngine'")
