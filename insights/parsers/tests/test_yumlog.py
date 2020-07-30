0ae157b1f5fd7e9b9836bd214131e5e9aa992fb3
wushiqinlou <jiazhang@redhat.com>
2020-07-30 11:30:31 +0800

    Enhance parser yum_log to handle erased package with version (#2688)
    * Enhance yumlog
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Fix condition
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit cb198b3cf5da4b60eb951fe56bd79b46e236efe8)

--- insights/parsers/tests/test_yumlog.py
+++ insights/parsers/tests/test_yumlog.py
@@ -15,6 +15,17 @@ May 23 16:09:09 Erased: katello-agent
 Jan 24 00:24:11 Updated: glibc-devel-2.12-1.149.el6_6.4.i686
 """.strip()
 
+OKAY2 = """
+May 19 23:29:12 Installed: cyrus-sasl-md5-2.1.26-23.el7.x86_64
+May 19 23:29:14 Installed: 389-ds-base-1.3.8.4-23.el7_6.x86_64
+Jul 21 09:09:39 Updated: httpd-tools-2.4.6-93.el7.x86_64
+Jul 21 09:09:39 Installed: mailcap-2.1.41-2.el7.noarch
+Jul 26 23:24:36 Erased: systemd-219-46.el7.x86_64
+Jul 28 23:24:36 Erased: systemd-219-67.el7.x86_64
+Jul 21 09:09:40 Installed: httpd-2.4.6-93.el7.x86_64
+Jul 20 09:09:40 Installed: httpd-2.4.6-97.el7.x86_64
+""".strip()
+
 ERROR = """
 May 23 18:06:24 Installed: wget-1.14-10.el7_0.1.x86_64
 Jan 24 00:24:00 Updated: glibc-2.12-1.149.el6_6.4.x86_64
@@ -79,3 +90,6 @@ def test_erased():
     yl = YumLog(context_wrap(OKAY))
     assert any(e.pkg.name == "redhat-access-insights-batch" for e in yl) is True
     assert any(e.pkg.name == "katello-agent" for e in yl) is True
+
+    yl2 = YumLog(context_wrap(OKAY2))
+    assert any(e.pkg.name == "systemd" for e in yl2) is True
