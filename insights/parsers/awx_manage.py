5582f735a5d83eb1462464393d39c2f47402d4f9
Xiangce Liu <xiangceliu@redhat.com>
2020-08-25 14:53:17 +0800

    Change spec to 'awx-manage check_license'
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/awx_manage.py
+++ insights/parsers/awx_manage.py
@@ -4,49 +4,36 @@ AwxManage - commands ``awx-manage``
 
 Parsers contains in this module are:
 
-AnsibleTowerLicense - command ``awx-manage check_license --data``
------------------------------------------------------------------
+AnsibleTowerLicense - command ``awx-manage check_license``
+----------------------------------------------------------
 """
 
 from insights import JSONParser, parser
+from insights.parsers import SkipException, ParseException
 from insights.specs import Specs
 
 
 @parser(Specs.awx_manage_check_license)
 class AnsibleTowerLicense(JSONParser):
     """
-    Parses the output of command  ``awx-manage check_license --data``
+    Parses the output of command  ``awx-manage check_license``
 
     Sample output of the command::
 
-        {"company_name": "", "instance_count": 100, "license_date": 1641013199,
-         "license_key": "********", "license_type": "enterprise",
-         "subscription_name": "Red Hat Ansible Automation, Standard (100 Managed Nodes)",
-         "valid_key": true, "current_instances": 1, "available_instances": 100,
-         "free_instances": 99, "time_remaining": 44826976, "trial": false,
-         "grace_period_remaining": 47418976,
-         "deployment_id": "919826a9a40bbded53676b868c74b507c523b370",
-         "compliant": true, "date_warning": false, "date_expired": false,
-         "features": {"activity_streams": true, "ha": true, "ldap": true,
-                      "multiple_organizations": true,
-                      "surveys": true, "system_tracking": true,
-                      "rebranding": true, "enterprise_auth": true,
-                      "workflows": true}
-        }
+        enterprise
+
+    Attributes:
+        type (str): The license type, e.g. "enterprise"
 
     Examples:
-
     >>> type(awx_license)
     <class 'insights.parsers.awx_manage.AnsibleTowerLicense'>
-    >>> awx_license['valid_key']
+    >>> awx_license.type == "enterprise"
     True
-    >>> awx_license['license_type'] == "enterprise"
-    True
-    >>> awx_license['time_remaining']
-    44826976
-    >>> awx_license['date_expired']
-    False
-    >>> awx_license['trial']
-    False
     """
-    pass
+    def parse_content(self, content):
+        if not content:
+            raise SkipException
+        if len(content) != 1:
+            raise ParseException("Invalid output: {0}".format(content))
+        self.type = content[0].strip()
--- insights/parsers/tests/test_awx_manage.py
+++ insights/parsers/tests/test_awx_manage.py
@@ -5,26 +5,21 @@ from insights.parsers.awx_manage import AnsibleTowerLicense
 from insights.tests import context_wrap
 
 
-TRI_LICENSE = """
-{"company_name": "", "instance_count": 100, "license_date": 1599364799, "license_key": "********", "subscription_name": "60 Day Evaluation of Red Hat Ansible Automation, Self-Supported (100 Managed Nodes)", "license_type": "enterprise", "trial": true, "valid_key": true, "deployment_id": "ee3fd544f8444c05915607faf3ed2aac7506badb", "current_instances": 1, "available_instances": 100, "free_instances": 99, "time_remaining": 3173549, "grace_period_remaining": 3173549, "compliant": true, "date_warning": false, "date_expired": false, "features": {"activity_streams": true, "ha": true, "ldap": true, "multiple_organizations": true, "surveys": true, "system_tracking": true, "rebranding": true, "enterprise_auth": true, "workflows": true}}
+NO_LICENSE = """
+none
+
 """.strip()
 
 STD_LICENSE = """
-{"company_name": "", "instance_count": 100, "license_date": 1641013199, "license_key": "********", "subscription_name": "Red Hat Ansible Automation, Standard (100 Managed Nodes)", "license_type": "enterprise", "valid_key": true, "deployment_id": "919826a9a40bbded53676b868c74b507c523b370", "current_instances": 1, "available_instances": 100, "free_instances": 99, "time_remaining": 44826976, "trial": false, "grace_period_remaining": 47418976, "compliant": true, "date_warning": false, "date_expired": false, "features": {"activity_streams": true, "ha": true, "ldap": true, "multiple_organizations": true, "surveys": true, "system_tracking": true, "rebranding": true, "enterprise_auth": true, "workflows": true}}
+enterprise
 """.strip()
 
 
 def test_ansible_tower_license():
-    ret = AnsibleTowerLicense(context_wrap(TRI_LICENSE))
-    assert ret['instance_count'] == 100
-    assert ret['license_date'] == 1599364799
-    assert ret['features']['ha'] is True
-    assert ret['features']['ldap'] is True
+    ret = AnsibleTowerLicense(context_wrap(NO_LICENSE))
+    assert ret.type == 'none'
     ret = AnsibleTowerLicense(context_wrap(STD_LICENSE))
-    assert ret['instance_count'] == 100
-    assert ret['license_date'] == 1641013199
-    assert ret['features']['ha'] is True
-    assert ret['features']['ldap'] is True
+    assert ret.type == 'enterprise'
 
 
 def test_awx_manage_doc_examples():
