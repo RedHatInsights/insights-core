bdd75881c9a8645148ca53dc57531b2998ab2cec
Chris Sams <csams@users.noreply.github.com>
2020-09-02 15:26:21 -0500

    Reverse context definition order. Add core3_archive spec file. (#2739)
    Core3 archives were being processed as regular archives because the context
    detection tested contexts in reverse order of their definition, and
    HostArchiveContext was defined after SerializedArchiveContext (core3 archive).
    
    Regular specs wouldn't work for core3 archives, but the client puts some
    critical files like satellite branch_info directly into the archive root, so
    I've allowed regular specs to depend on and fire in the presence of
    SerializedArchiveContext.
    
    Signed-off-by: Christopher Sams <csams@redhat.com>
    (cherry picked from commit 7809af44df433a2a70e42af2b2a724de072719cf)

---
+++ insights/specs/core3_archive.py
@@ -0,0 +1,16 @@
+"""
+This file holds specs that are in the new core3 archives but that don't have an entry
+in the meta_data directory. This happens when the client itself collects the data
+and puts it into the archive.
+"""
+from functools import partial
+
+from insights.core.context import SerializedArchiveContext
+from insights.specs import Specs
+from insights.core.spec_factory import simple_file
+
+simple_file = partial(simple_file, context=SerializedArchiveContext)
+
+
+class Core3Specs(Specs):
+    branch_info = simple_file("/branch_info")
