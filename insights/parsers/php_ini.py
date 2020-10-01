4e3df166e2303117c61eaca8dfac009431faf5b2
Martin Zibricky <mzibrick@redhat.com>
2020-10-01 19:07:17 +0200

    Make php_ini parser filterable
    Signed-off-by: Martin Zibricky <mzibrick@redhat.com>

--- insights/parsers/php_ini.py
+++ insights/parsers/php_ini.py
@@ -53,6 +53,7 @@ Example:
 import string
 from insights import parser
 from insights.core import ConfigParser
+from insights.core.filters import add_filter
 from insights.parsr.query import eq
 from insights.parsr import (Char, EOF, HangingString, InSet,
         LeftBracket, Lift, LineEnd, Literal, RightBracket,
@@ -61,6 +62,11 @@ from insights.parsr import (Char, EOF, HangingString, InSet,
 from insights.parsr.query import Directive, Entry, Section
 from insights.parsers import ParseException, SkipException
 from insights.specs import Specs
+
+filter_list = [
+    'post_max_size',
+]
+add_filter(Specs.php_ini, filter_list)
 
 
 @parser(Specs.php_ini, continue_on_error=False)
