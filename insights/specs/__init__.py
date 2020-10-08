4ca13551c6ce79a1b9859f7953105c1d0cd733c7
Martin Zibricky <mzibrick@redhat.com>
2020-10-08 08:23:40 +0200

    Remove whoopsie spec
    Signed-off-by: Martin Zibricky <mzibrick@redhat.com>

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -668,7 +668,6 @@ class Specs(SpecSet):
     vmware_tools_conf = RegistryPoint()
     vsftpd_conf = RegistryPoint(filterable=True)
     vsftpd = RegistryPoint()
-    woopsie = RegistryPoint()
     x86_pti_enabled = RegistryPoint()
     x86_ibpb_enabled = RegistryPoint()
     x86_ibrs_enabled = RegistryPoint()
