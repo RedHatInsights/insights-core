b198d120afc8235a4ec715e4002bb76e9e009884
vishawanathjadhav <vjadhav@redhat.com>
2020-10-15 08:51:10 +0530

    Fixed flake8 warnings
    Signed-off-by: vishawanathjadhav <vjadhav@redhat.com>

--- insights/parsers/abrt_ccpp.py
+++ insights/parsers/abrt_ccpp.py
@@ -9,7 +9,7 @@ Sample Data::
 
 
     # Configuration file for CCpp hook
-    
+
     # CCpp hook writes its template to the "/proc/sys/kernel/core_pattern" file
     # and stores the original template in the "/var/run/abrt/saved_core_pattern"
     # file. If you want CCpp hook to create a core dump file named according to
@@ -18,7 +18,7 @@ Sample Data::
     # instead of the template.
     # For more information about naming core dump files see 'man 5 core'.
     MakeCompatCore = yes
-    
+
     # The option allows you to set limit for the core file size in MiB.
     #
     # This value is compared to value of the MaxCrashReportSize configuration
@@ -28,16 +28,16 @@ Sample Data::
     # If MaxCrashReportSize is 0 then the value of MaxCoreFileSize is the limit.
     # If both values are 0 then the core file size is unlimited.
     MaxCoreFileSize = 0
-    
+
     # Do you want a copy of crashed binary be saved?
     # (useful, for example, when _deleted binary_ segfaults)
     SaveBinaryImage = no
-    
+
     # When this option is set to 'yes', core backtrace is generated
     # from the memory image of the crashing process. Only the crash
     # thread is present in the backtrace.
     CreateCoreBacktrace = yes
-    
+
     # Save full coredump? If set to 'no', coredump won't be saved
     # and you won't be able to report the crash to Bugzilla. Only
     # useful with CreateCoreBacktrace set to 'yes'. Please
@@ -45,26 +45,26 @@ Sample Data::
     # is set to 'yes', the core is still written to the current
     # directory.
     SaveFullCore = yes
-    
+
     # Used for debugging the hook
     #VerboseLog = 2
-    
+
     # Specify where you want to store debuginfos (default: /var/cache/abrt-di)
     #
     DebuginfoLocation = /var/cache/abrt-di
-    
+
     # ABRT will ignore crashes in executables whose absolute path matches one of
     # specified patterns.
     #
     #IgnoredPaths =
-    
+
     # ABRT will process only crashes of either allowed users or users who are
     # members of allowed group. If no allowed users nor allowed group are specified
     # ABRT will process crashes of all users.
     #
     #AllowedUsers =
     #AllowedGroups =
-    
+
 
 Example:
     >>> type(abrt_conf)
