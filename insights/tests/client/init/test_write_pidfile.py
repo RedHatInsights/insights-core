fc7da2839d4981b60dca7e2a311a6d0a9174d502
Štěpán Tomsa <stomsa@redhat.com>
2020-10-22 11:37:36 +0200

    Fix PID file write tests
    The tests verifying that the PID files are written were broken. They
    succeeded even if the file is not written. It was because incorrect
    unittest.mock usage. Fixed.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>

--- insights/tests/client/init/test_write_pidfile.py
+++ insights/tests/client/init/test_write_pidfile.py
@@ -1,11 +1,12 @@
 from insights.client import InsightsClient
 from insights.client.constants import InsightsConstants
+from mock.mock import call
 from mock.mock import patch
 
 
 @patch("insights.client.write_to_disk")
 @patch("insights.client.os.getpid")
-@patch("insights.client.utilities.get_parent_process")
+@patch("insights.client.get_parent_process")
 def test_write_pidfile(get_parent_process, getpid, write_to_disk):
     '''
     Test writing of the pidfile when InsightsClient
@@ -13,9 +14,10 @@ def test_write_pidfile(get_parent_process, getpid, write_to_disk):
     '''
     InsightsClient(from_phase=False)
     getpid.assert_called_once()
-    calls = [write_to_disk(InsightsConstants.pidfile, content=str(getpid.return_value)),
-             write_to_disk(InsightsConstants.ppidfile, content=get_parent_process.return_value)]
-    write_to_disk.has_calls(calls)
+    write_to_disk.assert_has_calls((
+        call(InsightsConstants.pidfile, content=str(getpid.return_value)),
+        call(InsightsConstants.ppidfile, content=get_parent_process.return_value)
+    ))
 
 
 @patch("insights.client.write_to_disk")
