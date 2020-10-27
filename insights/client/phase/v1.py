d110ceaa8092816b4701093c5f55695108ec4159
Glutexo <stomsa@redhat.com>
2020-10-27 14:00:00 +0100

    RHCLOUD-9230 Delete pid files on exit (#2795)
    * Couple PID files write and delete
    
    Originally, the PID files were written on the wrapper init call before
    the phases run. Their removal was however scheduled in the collect and
    output phase. That resulted in the files not being deleted if this
    phase is not run.
    
    Moved the registration of the shutdown handler that deletes the file,
    to the init phase. Like this, whenever the file gets created, its
    deletion is registered. They should no longer remain undeleted.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Fix PID file write tests
    
    The tests verifying that the PID files are written were broken. They
    succeeded even if the file is not written. It was because incorrect
    unittest.mock usage. Fixed.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Test delete PID files (not) being registered
    
    Added new tests verifying that a function that deleted the PID files
    is (not) registered when InsightsClient is initialized, depending on
    whether is is called from a phase. Not testing the actual filesystem
    delete as the actual write is untested too.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Co-authored-by: Jeremy Crafts <gravitypriest@users.noreply.github.com>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -4,13 +4,12 @@ import json
 import logging
 import os
 import sys
-import atexit
 
 from insights.client import InsightsClient
 from insights.client.config import InsightsConfig
 from insights.client.constants import InsightsConstants as constants
 from insights.client.support import InsightsSupport
-from insights.client.utilities import validate_remove_file, print_egg_versions, write_to_disk
+from insights.client.utilities import validate_remove_file, print_egg_versions
 from insights.client.schedule import get_scheduler
 from insights.client.apps.compliance import ComplianceClient
 
@@ -262,9 +261,6 @@ def post_update(client, config):
 
 @phase
 def collect_and_output(client, config):
-    # last phase, delete PID file on exit
-    atexit.register(write_to_disk, constants.pidfile, delete=True)
-    atexit.register(write_to_disk, constants.ppidfile, delete=True)
     # --compliance was called
     if config.compliance:
         config.payload, config.content_type = ComplianceClient(config).oscap_scan()
