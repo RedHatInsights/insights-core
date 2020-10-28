1d7193c221c2a1280be50a403713da6fb7c9abac
Alec Cohan <alcohan@redhat.com>
2020-10-28 14:28:34 -0400

    added additional Proof of concept material to original commit
    Signed-off-by: Derek Horton <dehort@redhat.com>

--- insights/client/apps/ansible/__init__.py
+++ insights/client/apps/ansible/__init__.py
@@ -1,6 +1,25 @@
-
+import os
+import sys
 __all__ = ("verify_playbook")
 
+####  PLAYBOOK VALIDATION ERROR MESSAGE ####
+class PlaybookValidationError(Exception):
+    """
+    Exception raised when playbook validation fails
+
+    Attributes:
+        playbook -- stringified playbook yaml from stdin
+        message -- explanation of why validation failed
+    """
+
+    def __init__(self, playbook, message = " #### PLAYBOOK VALIDATION FAILED ####: ", code = -1):
+        self.playbook = playbook
+        self.message = message
+        self.code = code
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message} {self.playbook}, error code: {self.code}'
 
 def verify_playbook(unverified_playbook: str) -> str:
     """
@@ -10,13 +29,32 @@ def verify_playbook(unverified_playbook: str) -> str:
     Output: stringified "verified" playbook
     Error: exception
     """
-    # Skeleton implementation ... "bless" the incoming playbook
+    # Skeleton implementation ... "bless" the incoming playbook 
+    ERROR = os.getenv('TOTALLY_BUSTED')
+    if ERROR:
+        raise PlaybookValidationError(unverified_playbook)
+
     verified_playbook = unverified_playbook
     return verified_playbook
 
+def read_playbook() -> str:
+    """
+    Read in the stringified playbook yaml from stdin
+    """
+    unverified_playbook = sys.stdin.readline()
+    return unverified_playbook
+
 if __name__ == "__main__":
+    ERROR = os.getenv('TOTALLY_BUSTED')
     # Read playbook from stdin
+    unverified_playbook = read_playbook()
+
     # Pass playbook to verify_playbook method
+    try:
+        verified_playbook = verify_playbook(unverified_playbook)
+    except PlaybookValidationError as e:
+        raise e
+
     # Output verified playbook
     # On error...set return code to non-zero, output error message to stderr
-    print("Verifying playbook")
+    print("Verified Playbook: ", verified_playbook)
