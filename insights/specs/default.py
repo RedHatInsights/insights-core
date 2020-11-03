2f444d0f7cf66dc78724ab60a36bd22cf9b82a92
Akshay Gaikwad <akgaikwa@redhat.com>
2020-11-03 13:03:26 +0530

    New parser for etc udev rules
    New parser similar to the udev_rules but parse files under directory
    `/etc/udev/rules.d/` directory instead.
    
    Signed-off-by: Akshay Gaikwad <akgaikwa@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -246,6 +246,7 @@ class DefaultSpecs(Specs):
     etc_journald_conf = simple_file(r"etc/systemd/journald.conf")
     etc_journald_conf_d = glob_file(r"etc/systemd/journald.conf.d/*.conf")
     etc_machine_id = simple_file("/etc/machine-id")
+    etc_udev_40_redhat_rules = simple_file("/etc/udev/rules.d/40-redhat.rules")
     etcd_conf = simple_file("/etc/etcd/etcd.conf")
     ethernet_interfaces = listdir("/sys/class/net", context=HostContext)
     ethtool = foreach_execute(ethernet_interfaces, "/sbin/ethtool %s")
