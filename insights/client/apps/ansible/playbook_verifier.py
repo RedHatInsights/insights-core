b20ee4a78fbdda2e810361316b9d4fddddd964f3
Derek Horton <dehort@redhat.com>
2020-11-05 14:46:21 -0600

    Fix flake8 issues
    Signed-off-by: Derek Horton <dehort@redhat.com>

--- insights/client/apps/ansible/playbook_verifier.py
+++ insights/client/apps/ansible/playbook_verifier.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-__all__ = ("verify_playbook", "PlaybookValidationError")
+__all__ = ("verify", "PlaybookValidationError")
 
-####  PLAYBOOK VALIDATION ERROR MESSAGE ####
+
 class PlaybookValidationError(Exception):
     """
     Exception raised when playbook validation fails
@@ -13,12 +13,13 @@ class PlaybookValidationError(Exception):
         message -- explanation of why validation failed
     """
 
-    def __init__(self, message = "PLAYBOOK VALIDATION FAILED"):
+    def __init__(self, message="PLAYBOOK VALIDATION FAILED"):
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
-        return f'{self.message}'
+        return self.message
+
 
 def verify(unverified_playbook: str) -> str:
     """
@@ -28,13 +29,14 @@ def verify(unverified_playbook: str) -> str:
     Output: stringified "verified" playbook
     Error: exception
     """
-    # Skeleton implementation ... "bless" the incoming playbook 
+    # Skeleton implementation ... "bless" the incoming playbook
     ERROR = os.getenv('ANSIBLE_PLAYBOOK_VERIFIER_THROW_ERROR')
     if ERROR:
         raise PlaybookValidationError()
 
     verified_playbook = unverified_playbook
     return verified_playbook
+
 
 def read_playbook() -> str:
     """
@@ -46,17 +48,15 @@ def read_playbook() -> str:
 
     return unverified_playbook
 
+
 if __name__ == "__main__":
-    # Read playbook from stdin
+
     unverified_playbook = read_playbook()
 
-    # Pass playbook to verify_playbook method
     try:
         verified_playbook = verify(unverified_playbook)
     except Exception as e:
         print(e, file=sys.stderr)
         sys.exit(1)
 
-    # Output verified playbook
-    # On error...set return code to non-zero, output error message to stderr
     print(verified_playbook)
