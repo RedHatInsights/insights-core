bb9550af2f5d14abb5ff353770fd05eb81f0d0fb
Jeremy Crafts <jcrafts@redhat.com>
2020-11-30 17:26:44 -0500

    update build_client_egg to clone client repo during build
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- build_client_egg.sh
+++ build_client_egg.sh
@@ -3,6 +3,14 @@ PYTHON=${1:-python}
 
 rm -f insights.zip
 rm -rf insights_core.egg-info
+
+# TEMPORARY:
+#   clone insights-client-runner if it doesn't exist
+git clone https://github.com/RedHatInsights/insights-client-runner.git insights/client 
+if [ $? -ne 0 ]; then
+    echo "ERROR: Could not clone insights-client-runner. Tread carefully."
+fi
+
 cp MANIFEST.in.client MANIFEST.in
 $PYTHON setup.py egg_info
 mkdir -p tmp/EGG-INFO
@@ -13,6 +21,8 @@ cd tmp
 rm -rf insights/archive
 find insights -path '*tests/*' -delete
 find insights -name '*.pyc' -delete
+# delete the git metadata from insights/client
+find insights/client -path '*.git/*' -delete
 
 git rev-parse --short HEAD > insights/COMMIT
 
@@ -21,4 +31,9 @@ find . -type f -exec chmod 0444 {} \;
 find . -type f -print | sort -df | xargs zip -X --no-dir-entries -r ../insights.zip
 cd ..
 rm -rf tmp
+
+# TEMPORARY:
+#   delete cloned insights-client-runner repo
+rm -rf insights/client
+
 git checkout MANIFEST.in
