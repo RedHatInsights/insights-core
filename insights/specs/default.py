e5d61f0f16b5c0e4d3ba499ac49584d2ca4c9c61
huali027 <44796653+huali027@users.noreply.github.com>
2020-12-10 12:48:28 +0800

    Add spec "corosync_cmapctl" back (#2858)
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -24,6 +24,7 @@ from insights.combiners.cloud_provider import CloudProvider
 from insights.combiners.services import Services
 from insights.combiners.sap import Sap
 from insights.parsers.lsmod import LsMod
+from insights.components.rhel_version import IsRhel8, IsRhel7
 from insights.specs import Specs
 
 
@@ -204,6 +205,16 @@ class DefaultSpecs(Specs):
     cobbler_settings = first_file(["/etc/cobbler/settings", "/conf/cobbler/settings"])
     cobbler_modules_conf = first_file(["/etc/cobbler/modules.conf", "/conf/cobbler/modules.conf"])
     corosync = simple_file("/etc/sysconfig/corosync")
+
+    @datasource([IsRhel7, IsRhel8])
+    def corosync_cmapctl_cmd_list(broker):
+        if broker.get(IsRhel7):
+            return ["/usr/sbin/corosync-cmapctl", 'corosync-cmapctl -d runtime.schedmiss.timestamp', 'corosync-cmapctl -d runtime.schedmiss.delay']
+        if broker.get(IsRhel8):
+            return ["/usr/sbin/corosync-cmapctl", '/usr/sbin/corosync-cmapctl -m stats', '/usr/sbin/corosync-cmapctl -C schedmiss']
+        raise SkipComponent()
+    corosync_cmapctl = foreach_execute(corosync_cmapctl_cmd_list, "%s")
+
     corosync_conf = simple_file("/etc/corosync/corosync.conf")
     cpu_cores = glob_file("sys/devices/system/cpu/cpu[0-9]*/online")
     cpu_siblings = glob_file("sys/devices/system/cpu/cpu[0-9]*/topology/thread_siblings_list")
