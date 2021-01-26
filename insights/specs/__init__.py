791544a83a535264dcba2acfeafc1524856f8968
Xiangce Liu <xiangceliu@redhat.com>
2021-01-26 10:00:04 +0800

    Remove the multi_output=True
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -284,7 +284,7 @@ class Specs(SpecSet):
     ksmstate = RegistryPoint()
     kubepods_cpu_quota = RegistryPoint(multi_output=True)
     lastupload = RegistryPoint(multi_output=True)
-    ld_library_path_of_pid = RegistryPoint(multi_output=True)
+    ld_library_path_of_pid = RegistryPoint()
     libssh_client_config = RegistryPoint(filterable=True)
     libssh_server_config = RegistryPoint(filterable=True)
     libvirtd_log = RegistryPoint(filterable=True)
