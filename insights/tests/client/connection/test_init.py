9e692332a953c354f4a2dfa4c7a231fe108dc152
Glutexo <stomsa@redhat.com>
2021-02-02 19:03:24 +0100

    Fix checkin URL with legacy upload (#2927)
    The Inventory URL is composed incorrectly if legacy upload
    is enabled. There is a workaround for this in place that
    disables legacy upload for actions that use the Inventory
    API. Added the check-in to the list of such actions.
    
    Removed show_status from the workaround action list,
    because the legacy upload config option does not affect
    it in any way.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>

--- insights/tests/client/connection/test_init.py
+++ insights/tests/client/connection/test_init.py
@@ -1,14 +1,41 @@
+from insights.client.auto_config import try_auto_configuration
+from insights.client.config import InsightsConfig
+from insights.client.connection import InsightsConnection
 from mock.mock import Mock
 from mock.mock import patch
-from insights.client.connection import InsightsConnection
+from pytest import mark
+from sys import argv
 
 
 @patch("insights.client.connection.InsightsConnection._init_session")
 @patch("insights.client.connection.InsightsConnection.get_proxies")
-def test_inventory_url(get_proxies, init_session):
+def test_inventory_url_from_base_url(get_proxies, init_session):
     """
-    Inventory URL is composed correctly.
+    Inventory URL is composed correctly from the given base URL.
     """
     config = Mock(base_url="www.example.com", insecure_connection=False)
     connection = InsightsConnection(config)
     assert connection.inventory_url == "https://www.example.com/inventory/v1"
+
+
+@mark.parametrize(("config_kwargs",), (({"check_results": True},), ({"checkin": True},)))
+@patch("insights.client.connection.InsightsConnection._init_session")
+@patch("insights.client.connection.InsightsConnection.get_proxies")
+@patch("insights.client.auto_config._try_satellite5_configuration")
+@patch("insights.client.auto_config._try_satellite6_configuration")
+@patch('insights.client.config.sys.argv', [argv[0]])
+def test_inventory_url_from_phase(
+    try_satellite6_configuration,
+    try_satellite5_configuration,
+    get_proxies,
+    init_session,
+    config_kwargs
+):
+    """
+    Inventory URL is composed correctly from the default configuration.
+    """
+    config = InsightsConfig(**config_kwargs)
+    config.load_all()  # Disables legacy upload.
+    try_auto_configuration(config)  # Updates base_url if legacy upload is disabled.
+    connection = InsightsConnection(config)
+    assert connection.inventory_url == "https://cert-api.access.redhat.com/r/insights/platform/inventory/v1"
