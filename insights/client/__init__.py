d3a69d993f25b19c6149bd0830f556fdc69b3f8a
Jeremy <jcrafts@redhat.com>
2021-02-03 11:29:33 -0500

    remove stale comments, use c.rh.c static path for legacy=false
    Signed-off-by: Jeremy <jcrafts@redhat.com>

--- insights/client/__init__.py
+++ insights/client/__init__.py
@@ -103,6 +103,7 @@ class InsightsClient(object):
         Get the proper url based on the configured egg release branch
         """
         if self.config.legacy_upload:
+            # append /platform to legacy URL because module update router only exists on c.rh.c
             url = self.connection.base_url + '/platform' + constants.module_router_path
         else:
             url = self.connection.base_url + constants.module_router_path
@@ -142,16 +143,8 @@ class InsightsClient(object):
         egg_gpg_url = self.config.egg_gpg_path
         if egg_url is None:
             egg_url = '/v1/static{0}/insights-core.egg'.format(egg_release)
-            # if self.config.legacy_upload:
-            #     egg_url = '/v1/static/core/insights-core.egg'
-            # else:
-            #     egg_url = '/static/insights-core.egg'
         if egg_gpg_url is None:
             egg_gpg_url = '/v1/static{0}/insights-core.egg.asc'.format(egg_release)
-            # if self.config.legacy_upload:
-            #     egg_gpg_url = '/v1/static/core/insights-core.egg.asc'
-            # else:
-            #     egg_gpg_url = '/static/insights-core.egg.asc'
         # run fetch for egg
         updated = self._fetch(egg_url,
                               constants.core_etag_file,
@@ -181,18 +174,7 @@ class InsightsClient(object):
                 current_etag = fp.read().strip()
                 logger.debug('Found etag %s', current_etag)
 
-        # it's only temporary. I promise. this is the worst timeline
-        # all for a phone popup
-        if self.config.legacy_upload:
-            url = self.connection.base_url + path
-            # verify = self.config.cert_verify
-        else:
-            url = self.connection.base_url.split('/platform')[0] + path
-            # if self.config.cert_verify is True:
-            #     # dont overwrite satellite cert
-            #     self.cert_verify = os.path.join(
-            #         constants.default_conf_dir,
-            #         'cert-api.access.redhat.com.pem')
+        url = self.connection.base_url + path
 
         # Setup the new request for core retrieval
         logger.debug('Making request to %s for new core', url)
@@ -247,7 +229,6 @@ class InsightsClient(object):
             logger.debug('Received Code %s', response.status_code)
             logger.debug('Not writing new core, or updating etag')
             logger.debug('Please check config, error reaching %s', url)
-
     def update(self):
         # dont update if running in offline mode
         if self.config.offline:
