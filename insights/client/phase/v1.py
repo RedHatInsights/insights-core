ec5a866f6852163f54ede12609fd192232ceecd8
Štěpán Tomsa <stomsa@redhat.com>
2021-02-08 12:25:49 +0100

    Split long expressions
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -73,7 +73,8 @@ def pre_update(client, config):
     if config.enable_schedule:
         # enable automatic scheduling
         logger.debug('Updating config...')
-        updated = get_scheduler(config).schedule()
+        scheduler = get_scheduler(config)
+        updated = scheduler.schedule()
         if updated:
             logger.info('Automatic scheduling for Insights has been enabled.')
         sys.exit(constants.sig_kill_ok)
@@ -194,9 +195,10 @@ def post_update(client, config):
         elif reg is False:
             # unregistered
             sys.exit(constants.sig_kill_bad)
-        if config.register:
-            if (not config.disable_schedule and
-               get_scheduler(config).schedule()):
+        if config.register and not config.disable_schedule:
+            scheduler = get_scheduler(config)
+            updated = scheduler.schedule()
+            if updated:
                 logger.info('Automatic scheduling for Insights has been enabled.')
         return
     # -------delete everything above this line-------
@@ -254,9 +256,11 @@ def post_update(client, config):
         #   system creation and upload are a single event on the platform
         if reg_check:
             logger.info('This host has already been registered.')
-        if (not config.disable_schedule and
-           get_scheduler(config).schedule()):
-            logger.info('Automatic scheduling for Insights has been enabled.')
+        if not config.disable_schedule:
+            scheduler = get_scheduler(config)
+            updated = scheduler.schedule()
+            if updated:
+                logger.info('Automatic scheduling for Insights has been enabled.')
 
     # set --display-name independent of register
     # only do this if set from the CLI. normally display_name is sent on upload
