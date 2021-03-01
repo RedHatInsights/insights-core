bfed88d39a3e30dce36a5243b3c6d141f9ed55a2
Xiangce Liu <xiangceliu@redhat.com>
2021-03-01 09:29:17 +0800

    New ld_library_path_of_user spec and UserLdLibraryPath parser (#2939)
    * New ld_library_path_of_user spec and UserLdLibraryPath parser
    
    - remove the ld_library_path_of_pid spec
    - deprecate the PidLdLibraryPath
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Keep the error code and return even error when running the export
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Use 'env' instead of 'export'
    
    - And remove the newly added PidLdLibraryPath instead of deprecating it
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Fix flake8 errors
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 0bb3a1dfe1bde6a9656a10ed29f53c1263e7a6f8)

--- insights/parsers/tests/test_ld_library_path.py
+++ insights/parsers/tests/test_ld_library_path.py
@@ -1,6 +1,6 @@
-from insights.parsers.ld_library_path import PidLdLibraryPath
+from insights.parsers.ld_library_path import UserLdLibraryPath
 from insights.tests import context_wrap
-from insights.parsers import ld_library_path, SkipException, ParseException
+from insights.parsers import ld_library_path, SkipException
 import doctest
 import pytest
 
@@ -12,46 +12,42 @@ LD_LIBRARY_PATH: Undefined variable.
 """.strip()
 
 LD_LIBRARY_PATH_DOC = """
-105901 /usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
-105902 /usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run
+/usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
+
+/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run
 """.strip()
 
 LD_LIBRARY_PATH = """
-105901 /usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
-105902 "/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run"
-105903 
-105904 ''
+/usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
+"/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run"
+
+''
 """.strip()  # noqa: W391
 
 
 def test_ld_library_path():
-    ret = PidLdLibraryPath(context_wrap(LD_LIBRARY_PATH))
+    ret = UserLdLibraryPath(context_wrap(LD_LIBRARY_PATH))
     assert len(ret) == 4
-    assert ret[0].pid == '105901'
-    assert ret[1].pid == '105902'
-    assert ret[2].pid == '105903'
-    assert ret[1].raw == LD_LIBRARY_PATH.splitlines()[1].split()[-1]
+    assert ret[0].raw == LD_LIBRARY_PATH.splitlines()[0]
+    assert ret[1].raw == LD_LIBRARY_PATH.splitlines()[1]
     assert ret[2].raw == ''
     assert ret[3].raw == "''"
     assert ret[2].path == ['']
     assert ret[3].path == ['']
-    for p in LD_LIBRARY_PATH.splitlines()[0].split()[-1].split(':'):
+    for p in LD_LIBRARY_PATH.splitlines()[0].split(':'):
         assert p in ret[0].path
-    for p in LD_LIBRARY_PATH.splitlines()[1].split()[-1].strip('"').split(':'):
+    for p in LD_LIBRARY_PATH.splitlines()[1].strip('"').split(':'):
         assert p in ret[1].path
 
 
 def test_empty_and_invalid():
     with pytest.raises(SkipException):
-        PidLdLibraryPath(context_wrap(LD_LIBRARY_PATH_EMPTY))
-
-    with pytest.raises(ParseException):
-        PidLdLibraryPath(context_wrap(LD_LIBRARY_PATH_INVALID))
+        UserLdLibraryPath(context_wrap(LD_LIBRARY_PATH_EMPTY))
 
 
 def test_doc_examples():
     env = {
-        'ld_lib_path': PidLdLibraryPath(context_wrap(LD_LIBRARY_PATH_DOC)),
+        'ld_lib_path': UserLdLibraryPath(context_wrap(LD_LIBRARY_PATH_DOC)),
     }
     failed, total = doctest.testmod(ld_library_path, globs=env)
     assert failed == 0
