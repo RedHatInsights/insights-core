add004c9026dab1b4248570420d4d40f48bbb92a
Xiangce Liu <xiangceliu@redhat.com>
2021-03-01 09:29:17 +0800

    New ld_library_path_of_user spec and UserLdLibraryPath parser (#2939)
    * New ld_library_path_of_user spec and UserLdLibraryPath parser
    
    - remove the ld_library_path_of_pid spec
    - deprecate the PidLdLibraryPath
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Keep the error code and return even error when running the export
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Use 'env' instead of 'export'
    
    - And remove the newly added PidLdLibraryPath instead of deprecating it
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Fix flake8 errors
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 00d739d8e49f5d83080febe9282c817b9d02214f)

--- insights/parsers/ld_library_path.py
+++ insights/parsers/ld_library_path.py
@@ -1,63 +1,55 @@
 """
-LdLibraryPath - LD_LIBRARY_PATH of PIDs
-=======================================
+LdLibraryPath - LD_LIBRARY_PATH of Users
+========================================
 
-Parser for parsing the environment variable LD_LIBRARY_PATH of each PID.
+Parser for parsing the environment variable LD_LIBRARY_PATH of each user
 
 """
 
 from collections import namedtuple
 from insights import parser, Parser
-from insights.parsers import SkipException, ParseException
+from insights.parsers import SkipException
 from insights.specs import Specs
 
-LdLibraryPath = namedtuple('LdLibraryPath', ('pid', 'path', 'raw'))
-"""namedtuple: Type for storing the LdLibraryPath of PID"""
+LdLibraryPath = namedtuple('LdLibraryPath', ('path', 'raw'))
+"""namedtuple: Type for storing the LD_LIBRARY_PATH of users"""
 
 
-@parser(Specs.ld_library_path_of_pid)
-class PidLdLibraryPath(Parser, list):
+@parser(Specs.ld_library_path_of_user)
+class UserLdLibraryPath(Parser, list):
     """
-    Base class for parsing the ``LD_LIBRARY_PATH`` variable of each PID of the
-    system into a list.
+    Base class for parsing the ``LD_LIBRARY_PATH`` variable of each regular
+    user of the system into a list.
 
     Typical content looks like::
 
-        105901 /usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
-        105902 /usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run
+        /usr/sap/RH1/SYS/exe/run:/usr/sap/RH1/SYS/exe/uc/linuxx86_64:/sapdb/clients/RH1/lib
+
+        /usr/sap/RH1/SYS/exe/uc/linuxx86_64:/usr/sap/RH1/SYS/exe/run
 
     Examples:
         >>> len(ld_lib_path)
-        2
+        3
         >>> isinstance(ld_lib_path[0].path, list)
         True
         >>> len(ld_lib_path[0].path)
         3
         >>> '/sapdb/clients/RH1/lib' in ld_lib_path[0].path
         True
-        >>> ld_lib_path[0].pid
-        '105901'
+        >>> '' in ld_lib_path[1].path  # The empty value is kept.
+        True
 
     Raises:
         SkipException: When the output is empty or nothing needs to parse.
-        ParseException: When the line cannot be parsed.
-
     """
 
     def parse_content(self, content):
-        if not content:
-            raise SkipException
-
         llds = []
         for line in content:
-            pid, _, raw = [s.strip() for s in line.partition(' ')]
-            paths = raw
-            if not pid.isdigit():
-                raise ParseException('Incorrect line: {0}'.format(line))
-            if raw and raw[0] == raw[-1] and raw[0] in ('\'', '"'):
-                paths = raw[1:-1]
-            paths = paths.split(':')
-            llds.append(LdLibraryPath(pid, paths, raw))
+            raw = line
+            if line and line[0] == line[-1] and line[0] in ('\'', '"'):
+                line = line[1:-1]
+            llds.append(LdLibraryPath(line.split(':'), raw))
 
         if not llds:
             raise SkipException("LD_LIBRARY_PATH not set.")
