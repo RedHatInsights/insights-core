b774fa278f31bbd0793fc6a93a35c252668d9442
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2021-03-03 17:07:33 +0000

    Add new metrics 'kernel.all.cpu.wait.total' (#2967)
    Signed-off-by: Suraj Patil <patilsuraj767@gmail.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -766,7 +766,7 @@ class DefaultSpecs(Specs):
         raise SkipComponent
 
     pmlog_summary = command_with_args(
-        "/usr/bin/pmlogsummary %s mem.util.used mem.physmem kernel.all.cpu.user kernel.all.cpu.sys kernel.all.cpu.nice kernel.all.cpu.steal kernel.all.cpu.idle disk.all.total mem.util.cached mem.util.bufmem mem.util.free",
+        "/usr/bin/pmlogsummary %s mem.util.used mem.physmem kernel.all.cpu.user kernel.all.cpu.sys kernel.all.cpu.nice kernel.all.cpu.steal kernel.all.cpu.idle disk.all.total mem.util.cached mem.util.bufmem mem.util.free kernel.all.cpu.wait.total",
         pmlog_summary_file)
     postconf_builtin = simple_command("/usr/sbin/postconf -C builtin")
     postconf = simple_command("/usr/sbin/postconf")
