4e34e6453ba81442cf5a054ba9ba42826b063e1c
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2021-03-16 17:36:04 -0400

    RHCLOUD-11737: Implement playbook signature validation logic in insights-core (#2945)
    * RHCLOUD-11737: Implement playbook signature validation logic in inisghts-core
    
    Signed-off-by: Alec Cohan <alcohan@redhat.com>
    
    * Update yaml.dump to use default_flow_style=False
    
    Signed-off-by: Alec Cohan <alcohan@redhat.com>
    
    * modify core version chececk to be less strict in the string validaion
    
    Signed-off-by: Alec Cohan <alcohan@redhat.com>
    
    * remove testing print statements
    
    Signed-off-by: Alec Cohan <alcohan@redhat.com>
    
    * flake fix
    
    Signed-off-by: Alec Cohan <alcohan@redhat.com>
    
    Co-authored-by: Jeremy Crafts <gravitypriest@users.noreply.github.com>

--- insights/client/apps/ansible/playbook_verifier/__init__.py
+++ insights/client/apps/ansible/playbook_verifier/__init__.py
@@ -1,37 +1,143 @@
 import os
+import copy
+import yaml
+import base64
+import requests
+import tempfile
+import pkgutil
+import insights.client.apps.ansible
+from logging import getLogger
+from distutils.version import LooseVersion
+from insights.client.utilities import get_version_info
+from insights.client.apps.ansible.playbook_verifier.contrib import gnupg
+from insights.client.constants import InsightsConstants as constants
 
-__all__ = ("verify", "PlaybookValidationError")
+__all__ = ("verify", "PlaybookVerificationError")
+
+SIGKEY = 'insights_signature'
+PUBLIC_KEY_FOLDER = pkgutil.get_data(insights.client.apps.ansible.__name__, 'playbook_verifier/public.gpg')    # Update this when we have the key generated
+VERSIONING_URL = 'https://cloud.redhat.com/api/v1/static/egg_version'
+EXCLUDABLE_VARIABLES = ['hosts', 'vars']
+
+logger = getLogger(__name__)
 
 
-class PlaybookValidationError(Exception):
+class PlaybookVerificationError(Exception):
     """
-    Exception raised when playbook validation fails
+    Exception raised when playbook verification fails
 
     Attributes:
         playbook -- stringified playbook yaml from stdin
-        message -- explanation of why validation failed
+        message -- explanation of why verification failed
     """
 
     def __init__(self, message="PLAYBOOK VALIDATION FAILED"):
         self.message = message
-        super().__init__(self.message)
+        super(PlaybookVerificationError, self).__init__(self.message)
 
     def __str__(self):
         return self.message
 
 
-def verify(unverified_playbook):
+def eggVersioningCheck(checkVersion):
+    currentVersion = requests.get(VERSIONING_URL)
+    print('currentVersion: ', currentVersion)
+    currentVersion = currentVersion.text
+    runningVersion = get_version_info()['core_version']
+
+    if checkVersion:
+        if LooseVersion(currentVersion.strip()) < LooseVersion(runningVersion):
+            raise PlaybookVerificationError(message="EGG VERSION ERROR: Current running egg is not the most recent version")
+
+    return currentVersion
+
+
+def getPublicKey(gpg):
+        if not PUBLIC_KEY_FOLDER:
+            raise PlaybookVerificationError(message="PUBLIC KEY IMPORT ERROR: Public key file not found")
+
+        publicKey = PUBLIC_KEY_FOLDER
+        importResults = gpg.import_keys(publicKey)
+        if (importResults.count < 1):
+            raise PlaybookVerificationError(message="PUBLIC KEY NOT IMPORTED: Public key import failed")
+
+        return importResults
+
+
+def excludeDynamicElements(snippet):
+    exclusions = snippet['vars']['insights_signature_exclude'].split(',')
+
+    for element in exclusions:
+        element = element.split('/')
+
+        # remove empty strings
+        element = [string for string in element if string != '']
+
+        if (len(element) == 1 and element[0] in EXCLUDABLE_VARIABLES):
+            del snippet[element[0]]
+        elif (len(element) == 2 and element[0] in EXCLUDABLE_VARIABLES):
+            try:
+                del snippet[element[0]][element[1]]
+            except:
+                raise PlaybookVerificationError(message='INVALID FIELD: the variable {0} defined in insights_signature_exclude does not exist.'.format(element))
+        else:
+            raise PlaybookVerificationError(message='INVALID EXCLUSION: the variable {0} is not a valid exclusion.'.format(element))
+
+    return snippet
+
+
+def executeVerification(snippet, encodedSignature):
+    gpg = gnupg.GPG(gnupghome=constants.insights_core_lib_dir)
+    serializedSnippet = bytes(yaml.dump(snippet, default_flow_style=False).encode("UTF-8"))
+
+    decodedSignature = base64.b64decode(encodedSignature)
+
+    # load public key
+    getPublicKey(gpg)
+
+    fd, fn = tempfile.mkstemp()
+    os.write(fd, decodedSignature)
+    os.close(fd)
+
+    result = gpg.verify_data(fn, serializedSnippet)
+    os.unlink(fn)
+
+    return result
+
+
+def verifyPlaybookSnippet(snippet):
+    if ('vars' not in snippet.keys()):
+        raise PlaybookVerificationError(message='VARS FIELD NOT FOUND: Verification failed')
+    elif (SIGKEY not in snippet['vars']):
+        raise PlaybookVerificationError(message='SIGNATURE NOT FOUND: Verification failed')
+
+    encodedSignature = snippet['vars'][SIGKEY]
+    snippetCopy = copy.deepcopy(snippet)
+
+    snippetCopy = excludeDynamicElements(snippetCopy)
+
+    return executeVerification(snippetCopy, encodedSignature)
+
+
+def verify(playbook, checkVersion=True, skipVerify=False):
     """
     Verify the signed playbook.
 
-    Input: stringified "broken" unverified playbook
-    Output: stringified "verified" playbook
-    Error: exception
+    Input: unverified playbook (dictionary format)
+    Output: "verified" playbook (dictionary format)
+    Error: Playbook Verification failure / Playbook Signature not found.
     """
-    # Skeleton implementation ... "bless" the incoming playbook
-    ERROR = os.getenv('ANSIBLE_PLAYBOOK_VERIFIER_THROW_ERROR')
-    if ERROR:
-        raise PlaybookValidationError()
+    logger.info('Playbook Verification has started')
 
-    verified_playbook = unverified_playbook
-    return verified_playbook
+    # Egg Version Check
+    eggVersioningCheck(checkVersion)
+
+    if not skipVerify:
+        for snippet in playbook:
+            verified = verifyPlaybookSnippet(snippet)
+
+            if not verified:
+                raise PlaybookVerificationError(message="SIGNATURE NOT VALID: Template [name: {0}] has invalid signature".format(snippet['name']))
+
+    logger.info('All templates successfully validated')
+    return playbook
