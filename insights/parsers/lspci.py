f52f9b2329cd809a7bc685cdf9409ef2bc57e00e
Xiangce Liu <xiangceliu@redhat.com>
2021-03-17 09:01:57 +0800

    fix doc errors
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/lspci.py
+++ insights/parsers/lspci.py
@@ -3,7 +3,7 @@ LsPci - Commands ``lspci``
 ==========================
 
 The parsers in this module are to parse the PCI device information gathered
-from the ``/sbin/lspci` commands.
+from the ``/sbin/lspci`` commands.
 
 LsPci - Command ``lspci -k``
 ----------------------------
