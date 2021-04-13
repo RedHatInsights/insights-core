b974df66582ebbdb43ab51952aa098d82fa539fc
Stephen <tsadams@gmail.com>
2021-04-13 16:27:01 -0400

    Remove ansible_host from insights_archive
    This one is covered by the core3specs
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- insights/specs/insights_archive.py
+++ insights/specs/insights_archive.py
@@ -13,7 +13,6 @@ class InsightsArchiveSpecs(Specs):
     abrt_status_bare = simple_file("insights_commands/abrt_status_--bare_True")
     all_installed_rpms = glob_file("insights_commands/rpm_-qa*")
     alternatives_display_python = simple_file("insights_commands/alternatives_--display_python")
-    ansible_host = simple_file("ansible_host")
     auditctl_status = simple_file("insights_commands/auditctl_-s")
     aws_instance_id_doc = simple_file("insights_commands/python_-m_insights.tools.cat_--no-header_aws_instance_id_doc")
     aws_instance_id_pkcs7 = simple_file("insights_commands/python_-m_insights.tools.cat_--no-header_aws_instance_id_pkcs7")
