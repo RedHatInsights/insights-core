b5e45f8deb0e784ddc0d46e127bb07d62c86cddf
Stephen <tsadams@gmail.com>
2021-04-22 08:41:45 -0400

    [parser] GCP parser to gather license codes from instances (#3030)
    * [parser] GCP parser to gather license codes from instances
    
    This is needed to identify marketplace systems in the GCP cloud.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    
    * [parser] fix docs for gcp_license_codes parser
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>
    (cherry picked from commit d92d173090dbfe8e7177f24812a6b3655663ffad)

---
+++ insights/parsers/gcp_license_codes.py
@@ -0,0 +1,61 @@
+"""
+GCPLicenseCodes
+===============
+
+This parser reads the output of a command
+``curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/licenses/?recursive=True"``,
+which is used to check whether the google cloud instance is a licensed marketplace instance.
+
+For more details, See: https://cloud.google.com/compute/docs/reference/rest/v1/images/get#body.Image.FIELDS.license_code
+
+"""
+import json
+
+from insights.parsers import SkipException, ParseException
+from insights import parser, CommandParser
+from insights.specs import Specs
+
+
+@parser(Specs.gcp_license_codes)
+class GCPLicenseCodes(CommandParser):
+    """
+    Class for parsing the GCP License Codes returned by command
+    ``curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/licenses/?recursive=True"``,
+
+
+    Typical Output of this command is::
+
+        [{"id": "601259152637613565"}]
+
+    Raises:
+        SkipException: When content is empty or no parse-able content.
+        ParseException: When the json is unable to be parsed
+
+    Attributes:
+        ids (list): A list containing the IDs found on the instance
+        raw (str): The full JSON of the plan returned by the ``curl`` command
+
+    Examples:
+        >>> gcp_licenses.ids == ["601259152637613565"]
+        True
+        >>> gcp_licenses.raw == [{"id": "601259152637613565"}]
+        True
+    """
+
+    def parse_content(self, content):
+        if not content or 'curl: ' in content[0]:
+            raise SkipException()
+        try:
+            license_list = json.loads(content[0])
+        except:
+            raise ParseException("Unable to parse JSON")
+
+        self.raw = license_list
+        self.ids = None
+        if len(license_list) >= 1:
+            self.ids = [l["id"] for l in license_list]
+
+    def __repr__(self):
+        return "ids: {i}, raw: {r}".format(
+            i=self.ids, r=self.raw
+        )
