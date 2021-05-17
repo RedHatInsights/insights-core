9393d533524c2eb19a3f8499ec2b113148c0fc77
jiazhang <jiazhang@redhat.com>
2021-05-17 15:30:46 +0800

    Update docstring format
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/umask.py
+++ insights/parsers/umask.py
@@ -1,22 +1,7 @@
 """
 Umask - command ``/usr/bin/umask -S``
 =====================================
-Parse the content of ``/usr/bin/umask -S`` command.
-
-Sample ``/usr/bin/umask -S`` command output::
-
-    u=rwx,g=rx,o=rx
-
-Examples:
-
-    >>> type(umask_obj)
-    <class 'insights.parsers.umask.Umask'>
-    >>> umask_obj['user']
-    'rwx'
-    >>> umask_obj['group']
-    'rx'
-    >>> umask_obj['other']
-    'rx'
+This parser parses the output of ``/usr/bin/umask -S`` command.
 """
 
 from insights import Parser, parser
@@ -26,8 +11,26 @@ from insights.specs import Specs
 
 @parser(Specs.umask)
 class Umask(Parser, dict):
-    """Parse output of ``/usr/bin/umask -S`` command"""
+    """
+    Class for parsing the output of ``/usr/bin/umask -S`` command.
+
+    Sample ``/usr/bin/umask -S`` command output::
+
+        u=rwx,g=rx,o=rx
+
+    Examples:
+
+        >>> type(umask_obj)
+        <class 'insights.parsers.umask.Umask'>
+        >>> umask_obj['user']
+        'rwx'
+        >>> umask_obj['group']
+        'rx'
+        >>> umask_obj['other']
+        'rx'
+    """
     def parse_content(self, content):
+        """Parse output of ``/usr/bin/umask -S`` command"""
         if not content:
             raise SkipException("No Contents")
         split_result = content[0].split(',')
