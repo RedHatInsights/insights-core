250f215029b2c7ec6d960ea91a5a7c3ca1ff6286
jiazhang <jiazhang@redhat.com>
2021-05-17 11:04:43 +0800

    Update parser umask
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/tests/test_umask.py
+++ insights/parsers/tests/test_umask.py
@@ -7,30 +7,23 @@ from insights.tests import context_wrap
 
 
 UMASK_DATA = """
-0022
+u=rwx,g=rx,o=rx
 """.strip()
 
 UMASK_NO_DATA = """
 """.strip()
 
-UMASK_INVALID_DATA = """
-invalid data
-""".strip()
-
 
 def test_umask():
     umask_obj = Umask(context_wrap(UMASK_DATA))
-    assert umask_obj['value'] == '0022'
+    assert umask_obj['user'] == 'rwx'
+    assert umask_obj['group'] == 'rx'
+    assert umask_obj['other'] == 'rx'
 
     with pytest.raises(SkipException) as exc:
         umask_obj = Umask(context_wrap(UMASK_NO_DATA))
         assert umask_obj is not None
     assert 'No Contents' in str(exc)
-
-    with pytest.raises(SkipException) as exc:
-        umask_obj = Umask(context_wrap(UMASK_INVALID_DATA))
-        assert umask_obj is not None
-    assert 'No Valid Contents' in str(exc)
 
 
 def test_modinfo_doc_examples():
--- insights/parsers/umask.py
+++ insights/parsers/umask.py
@@ -1,8 +1,22 @@
 """
-Umask - command ``/usr/bin/umask``
-==================================
-The ``Umask`` class implements the parsing of ``/usr/bin/umask``
-command, which is the value of default system umask.
+Umask - command ``/usr/bin/umask -S``
+=====================================
+Parse the content of ``/usr/bin/umask -S`` command.
+
+Sample ``/usr/bin/umask -S`` command output::
+
+    u=rwx,g=rx,o=rx
+
+Examples:
+
+    >>> type(umask_obj)
+    <class 'insights.parsers.umask.Umask'>
+    >>> umask_obj['user']
+    'rwx'
+    >>> umask_obj['group']
+    'rx'
+    >>> umask_obj['other']
+    'rx'
 """
 
 from insights import Parser, parser
@@ -12,38 +26,11 @@ from insights.specs import Specs
 
 @parser(Specs.umask)
 class Umask(Parser, dict):
-    """
-    Parser for ``/usr/bin/umask`` file.
-
-    Sample input is provided in the *Examples*.
-
-    Sample content::
-
-        0022
-
-    Examples:
-
-        >>> type(umask_obj)
-        <class 'insights.parsers.umask.Umask'>
-        >>> umask_obj['value']
-        '0022'
-
-
-    Resultant Data::
-
-        "0022"
-
-    Raises:
-        SkipException: When contents are empty or invalid
-
-    """
-
+    """Parse output of ``/usr/bin/umask -S`` command"""
     def parse_content(self, content):
-
         if not content:
             raise SkipException("No Contents")
-
-        if len(content) == 1 and content[0].isdigit():
-            self['value'] = content[0]
-        else:
-            raise SkipException("No Valid Contents")
+        split_result = content[0].split(',')
+        self['user'] = split_result[0].split('=')[1].strip()
+        self['group'] = split_result[1].split('=')[1].strip()
+        self['other'] = split_result[2].split('=')[1].strip()
