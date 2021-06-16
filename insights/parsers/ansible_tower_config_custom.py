dc6c517f71744ed303e7e0b004ecd79c83a81dec
jiazhang <jiazhang@redhat.com>
2021-06-16 11:04:28 +0800

    Update to use filter
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/ansible_tower_config_custom.py
+++ insights/parsers/ansible_tower_config_custom.py
@@ -3,11 +3,12 @@ AnsibleTowerConfigCustom - file ``/etc/tower/conf.d/custom.py``
 ===============================================================
 The AnsibleTowerConfigCustom class parses the file ``/etc/tower/conf.d/custom.py``.
 """
-import json
-from .. import parser, get_active_lines, Parser, LegacyItemAccess
+from insights import parser, get_active_lines, Parser, LegacyItemAccess
+from insights.core.filters import add_filter
 from insights.specs import Specs
 from insights.parsers import SkipException
 
+add_filter(Specs.ansible_tower_config_custom, ["AWX_CLEANUP_PATHS", "="])
 
 @parser(Specs.ansible_tower_config_custom)
 class AnsibleTowerConfigCustom(Parser, LegacyItemAccess):
@@ -17,73 +18,20 @@ class AnsibleTowerConfigCustom(Parser, LegacyItemAccess):
     Sample ``/etc/tower/conf.d/custom.py`` file::
 
         AWX_CLEANUP_PATHS = False
-        AUTH_LDAP_GLOBAL_OPTIONS = {
-            ldap.OPT_REFERRALS: False,
-        }
-        LOGGING['handlers']['tower_warnings']['level'] = 'DEBUG'
 
     Examples::
     >>> type(conf)
     <class 'insights.parsers.ansible_tower_config_custom.AnsibleTowerConfigCustom'>
-    >>> conf["LOGGING['handlers']['tower_warnings']['level']"]
-    'DEBUG'
-    >>> conf['AUTH_LDAP_GLOBAL_OPTIONS']['ldap.OPT_REFERRALS']
-    False
     >>> conf['AWX_CLEANUP_PATHS']
-    False
+    'False'
     """
 
     def parse_content(self, content):
         """Parse content of of ansible tower config file '/etc/tower/conf.d/custom.py'"""
+        if not content:
+            raise SkipException("No Valid Configuration")
         self.data = {}
-        json_lines = []
-        section_key = ""
-        brace_count = 0
         for line in get_active_lines(content):
-            if not section_key:
-                if "=" in line:
-                    if "{" not in line:
-                        key, value = [i.strip() for i in line.split('=', 1)]
-                        if value.startswith("'") or value.startswith('"'):
-                            value = value[1:]
-                        if value.endswith("'") or value.endswith('"'):
-                            value = value[:-1]
-                        if value == "False":
-                            value = False
-                        if value == "True":
-                            value = True
-                        self.data[key] = value
-                    else:
-                        section_key, value = [i.strip() for i in line.split('=', 1)]
-                        json_lines.append(value)
-                        brace_count += 1
-            else:
-                # Convert {} content to JSON format
-                line = line.replace("'", '"')
-                line = line.replace('"""', '"')
-                line = line.replace("False", "false")
-                line = line.replace("True", "true")
-                if ":" in line:
-                    if "'" not in line and '"' not in line and '"""' not in line:
-                        old_string = line.split(":")[0].strip()
-                        new_string = '"' + old_string + '"'
-                        line = line.replace(old_string, new_string)
-                brace_count = brace_count + line.count("{")
-                brace_count = brace_count - line.count("}")
-                json_lines.append(line.strip())
-                if brace_count == 0:
-                    total_line = "".join(json_lines)
-                    result_line = ""
-                    for i in range(len(total_line)):
-                        if total_line[i] == ",":
-                            left_brace_index = i + 1
-                            if left_brace_index < len(total_line) and total_line[left_brace_index] == "}":
-                                continue
-                        result_line = result_line + total_line[i]
-                    try:
-                        value = json.loads(result_line)
-                    except ValueError:
-                        raise SkipException('Syntax error')
-                    self.data[section_key] = value
-                    json_lines = []
-                    section_key = ""
+            if "=" in line:
+                key, value = line.split("=")
+                self.data[key.strip()] = value.strip()
--- insights/parsers/tests/test_ansible_tower_config_custom.py
+++ insights/parsers/tests/test_ansible_tower_config_custom.py
@@ -6,30 +6,20 @@ from insights.tests import context_wrap
 
 ANSIBLE_TOWER_CONFIG_CUSTOM = '''
 AWX_CLEANUP_PATHS = False
-AUTH_LDAP_GLOBAL_OPTIONS = {
-    ldap.OPT_REFERRALS: False,
-}
 LOGGING['handlers']['tower_warnings']['level'] = 'DEBUG'
-'''
+'''.strip()
 
 ANSIBLE_TOWER_CONFIG_CUSTOM_INVALID = '''
-AWX_CLEANUP_PATHS = False
-AUTH_LDAP_GLOBAL_OPTIONS = {
-    ldap.OPT_REFERRALS = False,
-}
-'''
+'''.strip()
 
 
 def test_ansible_tower_config_custom():
     conf = ansible_tower_config_custom.AnsibleTowerConfigCustom(context_wrap(ANSIBLE_TOWER_CONFIG_CUSTOM))
-    assert conf["LOGGING['handlers']['tower_warnings']['level']"] == 'DEBUG'
-    assert not conf['AUTH_LDAP_GLOBAL_OPTIONS']['ldap.OPT_REFERRALS']
-    assert not conf['AWX_CLEANUP_PATHS']
+    assert conf['AWX_CLEANUP_PATHS'] == 'False'
 
-    with pytest.raises(SkipException) as exc:
-        conf_invalid = ansible_tower_config_custom.AnsibleTowerConfigCustom(context_wrap(ANSIBLE_TOWER_CONFIG_CUSTOM_INVALID))
-        assert conf_invalid is not None
-    assert 'Syntax error' in str(exc)
+    # with pytest.raises(SkipException) as exc:
+    #     ansible_tower_config_custom.AnsibleTowerConfigCustom(context_wrap(ANSIBLE_TOWER_CONFIG_CUSTOM_INVALID))
+    # assert 'No Valid Configuration' in str(exc)
 
 
 def test_ansible_tower_config_custom_conf_documentation():
