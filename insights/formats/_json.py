fa1f49c44e55db4e7496f5fdba8c494b826026e5
Xiangce Liu <xiangceliu@redhat.com>
2021-07-20 17:03:00 +0800

    Add new option '--show-rules' to insights-run
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/formats/_json.py
+++ insights/formats/_json.py
@@ -1,12 +1,21 @@
 import json
 
 from insights.core.evaluators import SingleEvaluator as Evaluator
-from insights.formats import EvaluatorFormatterAdapter
+from insights.formats import EvaluatorFormatterAdapter, get_response_of_types
 
 
 class JsonFormat(Evaluator):
+    def __init__(self,
+            broker=None,
+            missing=False,
+            show_rules=None):
+        super(JsonFormat, self).__init__(broker)
+        self.missing = missing
+        self.show_rules = show_rules
+
     def postprocess(self):
-        json.dump(self.get_response(), self.stream)
+        response = get_response_of_types(self.get_response(), self.missing, self.show_rules)
+        json.dump(response, self.stream)
 
 
 class JsonFormatterAdapter(EvaluatorFormatterAdapter):
--- insights/formats/_yaml.py
+++ insights/formats/_yaml.py
@@ -1,7 +1,7 @@
 import yaml
 
 from insights.core.evaluators import SingleEvaluator
-from insights.formats import EvaluatorFormatterAdapter
+from insights.formats import EvaluatorFormatterAdapter, get_response_of_types
 from yaml.representer import Representer
 from insights.core import ScanMeta
 
@@ -9,8 +9,17 @@ Representer.add_representer(ScanMeta, Representer.represent_name)
 
 
 class YamlFormat(SingleEvaluator):
+    def __init__(self,
+            broker=None,
+            missing=False,
+            show_rules=None):
+        super(YamlFormat, self).__init__(broker)
+        self.missing = missing
+        self.show_rules = show_rules
+
     def postprocess(self):
-        yaml.dump(self.get_response(), self.stream)
+        response = get_response_of_types(self.get_response(), self.missing, self.show_rules)
+        yaml.dump(response, self.stream)
 
 
 class YamlFormatterAdapter(EvaluatorFormatterAdapter):
