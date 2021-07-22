6c612439a1eee8931b29743c967daa0319bb90e3
Xiangce Liu <xiangceliu@redhat.com>
2021-07-22 09:35:42 +0800

    Add more comments to the tests
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/tests/test_lspci.py
+++ insights/combiners/tests/test_lspci.py
@@ -108,6 +108,7 @@ def test_lspci_k():
         }
     ]
     assert lspci.search(Slot__startwith='00:1b.0') == []
+    # Make sure the original parser is untouched
     assert lspci_k.pci_dev_details('00:00.0').get('Kernel driver in use') == 'hsw_uncore'
     assert lspci_k.pci_dev_details('00:1b.0').get('Kernel driver in use') == 'snd_hda_intel'
 
@@ -124,6 +125,7 @@ def test_lspci_vmmkn():
         }
     ]
     assert lspci.search(Dev_Details__contains='I218') == []
+    # Make sure the original parser is untouched
     assert sorted(lspci_vmmkn[0].keys()) == sorted(['Slot', 'Class', 'Vendor',
                         'Device', 'SVendor', 'SDevice', 'Rev', 'Driver'])
 
@@ -152,6 +154,7 @@ def test_lspci_both():
             'Dev_Details': 'Audio device: Intel Corporation 8 Series HD Audio Controller (rev 04)'
         }
     ]
+    # Make sure the original parsers are untouched
     assert lspci_k.pci_dev_details('00:00.0').get('Kernel driver in use') == 'hsw_uncore'
     assert lspci_k.pci_dev_details('00:1b.0').get('Kernel driver in use') == 'snd_hda_intel'
     assert sorted(lspci_vmmkn[0].keys()) == sorted(['Slot', 'Class', 'Vendor',
