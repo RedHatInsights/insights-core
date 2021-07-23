3278a090d838d4f55a39ea4410663472b1cd9a50
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2021-07-23 09:24:10 -0500

    Revert "Add new option '--show-rules' to insights-run (#3154)" (#3162)
    This reverts commit 2091bf86b6e4fcfa460bc33ead8b868b452cf784.

--- insights/formats/_yaml.py
+++ insights/formats/_yaml.py
@@ -1,8 +1,7 @@
 import yaml
-import sys
 
 from insights.core.evaluators import SingleEvaluator
-from insights.formats import EvaluatorFormatterAdapter, get_response_of_types
+from insights.formats import EvaluatorFormatterAdapter
 from yaml.representer import Representer
 from insights.core import ScanMeta
 
@@ -10,18 +9,8 @@ Representer.add_representer(ScanMeta, Representer.represent_name)
 
 
 class YamlFormat(SingleEvaluator):
-    def __init__(self,
-            broker=None,
-            missing=False,
-            show_rules=None,
-            stream=sys.stdout):
-        super(YamlFormat, self).__init__(broker, stream=stream)
-        self.missing = missing
-        self.show_rules = [] if show_rules is None else show_rules
-
     def postprocess(self):
-        response = get_response_of_types(self.get_response(), self.missing, self.show_rules)
-        yaml.dump(response, self.stream)
+        yaml.dump(self.get_response(), self.stream)
 
 
 class YamlFormatterAdapter(EvaluatorFormatterAdapter):
