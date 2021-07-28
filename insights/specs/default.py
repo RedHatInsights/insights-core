09933944a9ecf8625f47f891d432c295745132ad
Rahul Srivastava <44598880+rasrivas-redhat@users.noreply.github.com>
2021-07-28 23:47:00 +0530

    Added dnsmasq_config spec (#3164)
    Signed-off-by: Rahul <rasrivas@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -223,6 +223,7 @@ class DefaultSpecs(Specs):
     dmsetup_status = simple_command("/usr/sbin/dmsetup status")
     dnf_conf = simple_file("/etc/dnf/dnf.conf")
     dnf_modules = glob_file("/etc/dnf/modules.d/*.module")
+    dnsmasq_config = glob_file(["/etc/dnsmasq.conf", "/etc/dnsmasq.d/*.conf"])
     docker_info = simple_command("/usr/bin/docker info")
     docker_list_containers = simple_command("/usr/bin/docker ps --all --no-trunc")
     docker_list_images = simple_command("/usr/bin/docker images --all --no-trunc --digests")
