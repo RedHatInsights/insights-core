0781cb0644e47a7282cac2f28b1ea61444bb3c4b
Xiangce Liu <xiangceliu@redhat.com>
2021-08-02 10:47:54 +0800

    Add more comments (for _json, html and _yaml)
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/formats/__init__.py
+++ insights/formats/__init__.py
@@ -173,19 +173,25 @@ def render(comp, val):
 
 
 def get_response_of_types(response, missing=True, show_rules=None):
+    # When "-m" is NOT specified, do not show the "skips" rules
     if not missing and 'skips' in response:
         response.pop('skips')
+    # When neither "-m" nor "-S" is specified, show the HIT rules only (excludes "skips")
     if not show_rules:
         return response
+    # Discard the "medadata" rules when it's not specified in the "-S" option
     if 'metadata' not in show_rules and 'metadata' in response.get('system', {}):
         response['system'].pop('metadata')
+    # Discard the "make_fail" rules when it's not specified in the "-S" option
     if 'rule' not in show_rules and 'reports' in response:
         response.pop('reports')
+    # Discard the "make_info" rules when it's not specified in the "-S" option
     if 'info' not in show_rules and 'info' in response:
         response.pop('info')
+    # Discard the "make_pass" rules when it's not specified in the "-S" option
     if 'pass' not in show_rules and 'pass' in response:
         response.pop('pass')
+    # Discard the "fingerprint" rules when it's not specified in the "-S" option
     if 'fingerprint' not in show_rules and 'fingerprints' in response:
         response.pop('fingerprints')
-
     return response
