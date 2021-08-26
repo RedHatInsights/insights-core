44c3df69f1d3f56795219dedadb1f1da92a94047
jiazhang <jiazhang@redhat.com>
2021-08-26 18:01:43 +0800

    Add parser cups_queue_ppd
    Signed-off-by: jiazhang <jiazhang@redhat.com>

---
+++ insights/specs/datasources/lpstat.py
@@ -0,0 +1,44 @@
+"""
+Custom datasources for lpstat information
+"""
+from insights.core.context import HostContext
+from insights.core.dr import SkipComponent
+from insights.core.plugins import datasource
+from insights.core.spec_factory import DatasourceProvider, simple_command
+from insights.specs import Specs
+
+
+class LocalSpecs(Specs):
+    """ Local specs used only by lpstat datasources """
+
+    lpstat_v = simple_command("/usr/bin/lpstat -v")
+    """ Returns the output of command ``/usr/bin/lpstat -v`` """
+
+
+@datasource(LocalSpecs.lpstat_v, HostContext)
+def lpstat_protocol_printers_info(broker):
+    """
+    This datasource provides the not-sensitive information collected
+    from ``/usr/bin/lpstat -v``.
+
+    Typical content of ``/usr/bin/lpstat -v`` file is::
+
+        "device for NAY_10F_Smurfs: ipp://cups.nay.redhat.com/printers/NAY_10F_Smurfs"
+
+    Returns:
+        dict: string containing non-sensitive information.
+
+    Raises:
+        SkipComponent: When the filter/path does not exist or any exception occurs.
+    """
+    try:
+        content = broker[LocalSpecs.lpstat_v].content
+        result = []
+        for line in content:
+            if "device for " in line:
+                result.append(line.rsplit(":", 1)[0])
+        if result:
+            return DatasourceProvider(content="\n".join(result), relative_path='insights_commands/lpstat_-v')
+    except Exception as e:
+        raise SkipComponent("Unexpected exception:{e}".format(e=str(e)))
+    raise SkipComponent
