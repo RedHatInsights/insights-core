32016ee43ca642d4831c823d92c5cd63c85146da
wushiqinlou <jiazhang@redhat.com>
2021-09-02 03:51:41 +0800

    Add new parser lpstat_v (#3202)
    * Add new parser lpstat_v
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Add new parser lpstat_v
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Use dict base and update datasource return docstring
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Update docstring
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Update docstring
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit 5b0956ff2249bca13107a950de70af6ace54ce6b)

--- insights/parsers/tests/test_lpstat.py
+++ insights/parsers/tests/test_lpstat.py
@@ -1,15 +1,8 @@
-"""
-Tests for lpstat parser
-=======================
-
-Note, that date time is localized (according to LC_TIME).
-
-
-"""
-
+import doctest
 import pytest
 
-from insights.parsers.lpstat import LpstatPrinters
+from insights.parsers import lpstat
+from insights.parsers.lpstat import LpstatPrinters, LpstatProtocol, SkipException
 from insights.tests import context_wrap
 
 
@@ -23,6 +16,19 @@ printer processing_printer now printing local_printer-1234.  enabled since Wed 1
 LPSTAT_P_OUTPUT_UKNOWN_STATE = """
 printer unknown_printer may be jammed.  enabled since Fri 20 Jan 2017 09:55:50 PM CET
 """
+
+
+LPSTAT_V_OUTPUT = """
+device for test_printer1: ipp
+device for test_printer2: ipp
+""".strip()
+
+LPSTAT_V_OUTPUT_INVALID_1 = """
+""".strip()
+
+LPSTAT_V_OUTPUT_INVALID_2 = """
+lpstat: Transport endpoint is not connected
+""".strip()
 
 
 def test_lpstat_parse():
@@ -71,3 +77,27 @@ def test_lpstat_printer_names_by_status(status, expected_name):
     lpstat = LpstatPrinters(context_wrap(LPSTAT_P_OUTPUT))
     names = lpstat.printer_names_by_status(status)
     assert names == [expected_name]
+
+
+def test_lpstat_protocol():
+    lpstat_protocol = LpstatProtocol(context_wrap(LPSTAT_V_OUTPUT))
+    assert lpstat_protocol["test_printer1"] == "ipp"
+
+
+def test_lpstat_protocol_invalid_state():
+    with pytest.raises(SkipException) as exc:
+        LpstatProtocol(context_wrap(LPSTAT_V_OUTPUT_INVALID_1))
+    assert 'No Valid Output' in str(exc)
+
+    with pytest.raises(SkipException) as exc:
+        LpstatProtocol(context_wrap(LPSTAT_V_OUTPUT_INVALID_2))
+    assert 'No Valid Output' in str(exc)
+
+
+def test_lpstat_doc_examples():
+    env = {
+        'lpstat_printers': LpstatPrinters(context_wrap(LPSTAT_P_OUTPUT)),
+        'lpstat_protocol': LpstatProtocol(context_wrap(LPSTAT_V_OUTPUT))
+    }
+    failed, total = doctest.testmod(lpstat, globs=env)
+    assert failed == 0
