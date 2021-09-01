c36103c583d6681f63c9e9dcbc49b8358c9dc2da
wushiqinlou <jiazhang@redhat.com>
2021-09-02 03:51:30 +0800

    Add parser cups_ppd (#3201)
    * Add parser cups_ppd
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Use dict base
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Add SkipException test
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Update docstring
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit deae0f7120397dd87cd52e88006aeaf53c137db0)

---
+++ insights/parsers/cups_ppd.py
@@ -0,0 +1,60 @@
+"""
+CupsPpd - files ``/etc/cups/ppd/*``
+===================================
+
+Parser to parse the content of files ``/etc/cups/ppd/*``
+"""
+
+from insights import Parser
+from insights import parser
+from insights.specs import Specs
+from insights.parsers import SkipException
+
+
+@parser(Specs.cups_ppd)
+class CupsPpd(Parser, dict):
+    """
+    Class to parse ``/etc/cups/ppd/*`` files.
+
+    Sample output for files::
+
+        *PPD-Adobe: "4.3"
+        *FormatVersion: "4.3"
+        *FileVersion: "2.2"
+        *LanguageVersion: English
+        *LanguageEncoding: ISOLatin1
+        *PCFileName: "ippeve.ppd"
+        *Manufacturer: "Canon"
+        *ModelName: "iR-ADV C3525/3530 PPD"
+        *Product: "(iR-ADV C3525/3530 PPD)"
+        *NickName: "iR-ADV C3525/3530 PPD"
+        *ShortNickName: "iR-ADV C3525/3530 PPD"
+        *cupsFilter2: "application/vnd.cups-pdf application/pdf 10 -"
+        *cupsFilter2: "application/vnd.cups-postscript application/postscript 10 -"
+
+    Examples:
+        >>> type(cups_ppd)
+        <class 'insights.parsers.cups_ppd.CupsPpd'>
+        >>> cups_ppd["PCFileName"]
+        '"ippeve.ppd"'
+        >>> cups_ppd["cupsFilter2"]
+        ['"application/vnd.cups-pdf application/pdf 10 -"', '"application/vnd.cups-postscript application/postscript 10 -"']
+    """
+    def parse_content(self, content):
+        if not content:
+            raise SkipException("No Valid Configuration")
+        data = {}
+        for line in content:
+            if "*" in line and ":" in line:
+                key = line.split(":")[0].split("*")[-1].strip()
+                value = line.split(":")[-1].strip()
+                if key in data:
+                    if isinstance(data[key], list):
+                        data[key].append(value)
+                    else:
+                        data[key] = [data[key], value]
+                else:
+                    data[key] = value
+        if not data:
+            raise SkipException("No Valid Configuration")
+        self.update(data)
