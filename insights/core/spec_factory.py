d11a40f601894a57a305eade46082f931cba52ba
Xiangce Liu <xiangceliu@redhat.com>
2021-09-14 09:37:48 +0800

    Get the raw from each items of the 'first_of'
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/core/spec_factory.py
+++ insights/core/spec_factory.py
@@ -932,7 +932,11 @@ class first_of(object):
     """
     def __init__(self, deps):
         self.deps = deps
-        self.raw = getattr(deps[0], 'raw') if hasattr(deps[0], 'raw') else False
+        self.raw = False
+        for c in deps:
+            if hasattr(c, 'raw'):
+                self.raw = c.raw
+                break
         self.__name__ = self.__class__.__name__
         datasource(deps)(self)
 
