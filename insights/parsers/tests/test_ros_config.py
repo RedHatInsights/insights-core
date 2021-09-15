fda4fc99eb1bc81dfdb84bf5ee2813f16bbd03a0
Aditi Puntambekar <apuntamb@redhat.com>
2021-09-15 21:53:16 +0530

    Update ros_config tests
    Signed-off-by: Aditi Puntambekar <apuntamb@redhat.com>

--- insights/parsers/tests/test_ros_config.py
+++ insights/parsers/tests/test_ros_config.py
@@ -26,19 +26,26 @@ allow local:* : enquire;
 
 def test_ros_config():
     ros_config = RosConfig(context_wrap(ROS_CONFIG_INPUT))
-    ros_config_key = ros_config['log mandatory on default']
-    assert ros_config is not None
-    assert 'mem.util.used' in ros_config_key
-    assert 'mem.physmem' in ros_config_key
-    assert 'kernel.all.cpu.user' in ros_config_key
-    assert 'kernel.all.cpu.sys' in ros_config_key
-    assert 'kernel.all.cpu.nice' in ros_config_key
-    assert 'kernel.all.cpu.steal' in ros_config_key
-    assert 'kernel.all.cpu.idle' in ros_config_key
-    assert 'kernel.all.cpu.wait.total' in ros_config_key
-    assert 'disk.all.total' in ros_config_key
-    assert 'mem.util.cached' in ros_config_key
-    assert 'mem.util.bufmem' in ros_config_key
-    assert 'mem.util.free' in ros_config_key
-    assert list(ros_config.sections()) == ['access']
-    # assert ros_config.get('access', 'disallow') == ''
+    print(ros_config.data)
+    assert ros_config.data is not None
+
+    flatlist = [element for sublist in ros_config.data for element in sublist]
+    metrics = flatlist[0][1]
+    assert flatlist[1] == '[access]'
+    assert 'mem.util.used' in metrics
+    assert 'mem.physmem' in metrics
+    assert 'kernel.all.cpu.user' in metrics
+    assert 'kernel.all.cpu.sys' in metrics
+    assert 'kernel.all.cpu.nice' in metrics
+    assert 'kernel.all.cpu.steal' in metrics
+    assert 'kernel.all.cpu.idle' in metrics
+    assert 'kernel.all.cpu.wait.total' in metrics
+    assert 'disk.all.total' in metrics
+    assert 'mem.util.cached' in metrics
+    assert 'mem.util.bufmem' in metrics
+    assert 'mem.util.free' in metrics
+    for item in flatlist[2]:
+        assert item[0] == 'allow' or 'disallow'
+        assert isinstance(item[1], list)
+        assert item[2] == ':'
+        assert isinstance(item[3], list)
