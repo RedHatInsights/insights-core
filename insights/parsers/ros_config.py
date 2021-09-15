76215b2f08f09657140463949fec1f353fa1a5cb
Aditi Puntambekar <apuntamb@redhat.com>
2021-09-15 15:08:17 +0530

    Update parsing logic for [access] section
    Signed-off-by: Aditi Puntambekar <apuntamb@redhat.com>

--- insights/parsers/ros_config.py
+++ insights/parsers/ros_config.py
@@ -1,34 +1,35 @@
 """
 RosConfig - file ``/var/lib/pcp/config/pmlogger/config.ros``
-======================================================
+============================================================
 This class provides parsing for the files:
     ``/var/lib/pcp/config/pmlogger/config.ros``
 
 Sample input data is in the format::
 
-log mandatory on default {
-    mem.util.used
-    mem.physmem
-    kernel.all.cpu.user
-    kernel.all.cpu.sys
-    kernel.all.cpu.nice
-    kernel.all.cpu.steal
-    kernel.all.cpu.idle
-    kernel.all.cpu.wait.total
-    disk.all.total
-    mem.util.cached
-    mem.util.bufmem
-    mem.util.free
-}
-[access]
-disallow .* : all;
-disallow :* : all;
-allow local:* : enquire;
+    log mandatory on default {
+        mem.util.used
+        mem.physmem
+        kernel.all.cpu.user
+        kernel.all.cpu.sys
+        kernel.all.cpu.nice
+        kernel.all.cpu.steal
+        kernel.all.cpu.idle
+        kernel.all.cpu.wait.total
+        disk.all.total
+        mem.util.cached
+        mem.util.bufmem
+        mem.util.free
+    }
+    [access]
+    disallow .* : all;
+    disallow :* : all;
+    allow local:* : enquire;
 """
 
 from insights import Parser
 from insights.parsr import EOF, EOL, Char, Literal, Many, OneLineComment, Opt, QuotedString, String, WSChar
 import string
+# import logging
 
 
 # https://man7.org/linux/man-pages/man1/pmlogger.1.html#CONFIGURATION_FILE_SYNTAX
@@ -78,63 +79,34 @@ LogSpecs = Many(LogSpec)
 parse = LogSpecs << EOF
 
 AccessHeader = WS >> Literal("[access]") << WS
-AllowRule = WS >> Literal("allow") << WS
-DisallowRule = WS >> Literal("disallow") << WS
-AllIPv4Addresses = WS >> Literal(".*") << WS
-AllIPv6Addresses = WS >> Literal(":*") << WS
-Enquire = WS >> Literal("enquire;") << WS
-Advisory = WS >> Literal("advisory;") << WS
-Mandatory = WS >> Literal("mandatory;") << WS
-All = WS >> Literal("all;") << WS
-Operation = Enquire | Advisory | Mandatory | All
-Rule = AllowRule | DisallowRule + AllIPv4Addresses | AllIPv6Addresses + Enquire | Advisory | Mandatory | All
-AccessRules = Many(Rule)
-AccessControl = AccessHeader >> AccessRules
+Allow = WS >> Literal("allow") << WS
+Disallow = WS >> Literal("disallow") << WS
+AllowDisallow = Allow | Disallow
+
+
+EnquireOp = WS >> Literal("enquire") << WS
+AdvisoryOp = WS >> Literal("advisory") << WS
+MandatoryOp = WS >> Literal("mandatory") << WS
+AllExceptOp = WS >> Literal("all except") << WS
+AllOp = WS >> Literal("all") << WS
+
+Colon = WS >> Literal(":") << WS
+
+Host = String(string.ascii_letters + string.digits + ".*:\"")
+HostList = WS >> Host.sep_by(Comma) << WS
+
+Operation = EnquireOp | AdvisoryOp | MandatoryOp | AllExceptOp | AllOp
+OperationList = WS >> Operation.sep_by(Comma | WS) << WS
+
+Semicolon = WS >> Char(";") << WS
+
+AccessRule = AllowDisallow + HostList + Colon + OperationList << Semicolon
+AccessRules = Many(AccessRule)
+AccessControl = AccessHeader + AccessRules
 Doc = LogSpecs + Opt(AccessControl)
 parse = Doc << EOF
-
-#    10.  Following all of the logging specifications, there may be an
-#         optional access control section, introduced by the literal
-#         token [access].  Thereafter come access control rules that
-#         allow or disallow operations from particular hosts or groups
-#         of hosts.
-
-#         The operations may be used to interrogate or control a
-#         running pmlogger using pmlc(1) and fall into the following
-#         classes:
-
-#         enquire
-#                interrogate the status of pmlogger and the metrics it
-#                is logging
-#         advisory
-#                Change advisory logging.
-#         mandatory
-#                Change mandatory logging.
-#         all    All of the above.
-
-#         Access control rules are of the form ``allow hostlist :
-#         operationlist ;'' and ``disallow hostlist : operationlist
-#         ;''.
-
-#         The hostlist follows the syntax and semantics for the access
-#         control mechanisms used by PMCD and are fully documented in
-#         pmcd(1).  An operationslist is a comma separated list of the
-#         operations advisory, mandatory, enquire and all.
-
-#         A missing [access] section allows all access and is
-#         equivalent to allow * : all;.
-
-#    The configuration (either from standard input or conffile) is
-#    initially scanned by pmcpp(1) with the options -rs and -I
-#    $PCP_VAR_DIR/config/pmlogger.  This extends the configuration
-#    file syntax with include file processing (%include), a common
-#    location to search for include files
-#    ($PCP_VAR_DIR/config/pmlogger), macro definitions (%define),
-#    macro expansion (%name and %{name}) and conditional inclusion of
-#    lines (%ifdef name ... %else ... %endif and %ifndef name ...
-#    %else ... %endif).
 
 
 class RosConfig(Parser):
     def parse_content(self, content):
-        self.data = parse("".join(content))
+        self.data = parse("\n".join(content))
