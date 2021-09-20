58c5733dd7693d2ab0b2544cf7b84de62ede8a4d
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2021-09-20 15:23:54 -0500

    Stop collection of facter and remove dependencies (#3224)
    * We will no longer collect facter due to issues documented in Bugzilla
      1989655
    * Update combiners to remove dependency
    * Remove spec from HostContext collection
    * Update tests
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>
    (cherry picked from commit 388063171079e4d3176e606d75ea24c9b454b3f0)

--- insights/combiners/tests/test_hostname.py
+++ insights/combiners/tests/test_hostname.py
@@ -1,7 +1,6 @@
 import pytest
 import doctest
 from insights.parsers.hostname import Hostname as HnF, HostnameShort as HnS, HostnameDefault as HnD
-from insights.parsers.facter import Facter
 from insights.parsers.systemid import SystemID
 from insights.combiners import hostname
 from insights.combiners.hostname import Hostname, hostname as hn_func
@@ -10,23 +9,6 @@ from insights.tests import context_wrap
 HOSTNAME_FULL = "rhel7.example.com"
 HOSTNAME_SHORT = "rhel7"
 HOSTNAME_DEF = "rhel7"
-FACTS_FQDN = """
-COMMAND> facter
-
-architecture => x86_64
-augeasversion => 1.1.0
-facterversion => 1.7.6
-filesystems => xfs
-fqdn => ewa-satellite.example.com
-hostname => ewa-satellite
-""".strip()
-FACTS_NO_FQDN = """
-COMMAND> facter
-
-architecture => x86_64
-facterversion => 1.7.6
-filesystems => xfs
-""".strip()
 SYSTEMID_PROFILE_NAME = '''
 <?xml version="1.0"?>
 <params>
@@ -72,30 +54,21 @@ SYSTEMID_NO_PROFILE_NAME = '''
 def test_get_hostname():
     hnf = HnF(context_wrap(HOSTNAME_FULL))
     expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
-    result = Hostname(hnf, None, None, None, None)
+    result = Hostname(hnf, None, None, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
     hns = HnS(context_wrap(HOSTNAME_SHORT))
     expected = (HOSTNAME_SHORT, HOSTNAME_SHORT, '')
-    result = Hostname(None, None, hns, None, None)
+    result = Hostname(None, None, hns, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
 
     hnd = HnD(context_wrap(HOSTNAME_DEF))
     expected = (HOSTNAME_DEF, HOSTNAME_DEF, '')
-    result = Hostname(None, hnd, None, None, None)
-    assert result.fqdn == expected[0]
-    assert result.hostname == expected[1]
-    assert result.domain == expected[2]
-
-
-def test_get_facter_hostname():
-    ft = Facter(context_wrap(FACTS_FQDN))
-    expected = ('ewa-satellite.example.com', 'ewa-satellite', 'example.com')
-    result = Hostname(None, None, None, ft, None)
+    result = Hostname(None, hnd, None, None)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
@@ -104,7 +77,7 @@ def test_get_facter_hostname():
 def test_get_systemid_hostname():
     sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
     expected = ('example_profile', 'example_profile', '')
-    result = Hostname(None, None, None, None, sid)
+    result = Hostname(None, None, None, sid)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
@@ -114,10 +87,9 @@ def test_get_all_hostname():
     hnf = HnF(context_wrap(HOSTNAME_FULL))
     hns = HnS(context_wrap(HOSTNAME_SHORT))
     hnd = HnD(context_wrap(HOSTNAME_DEF))
-    ft = Facter(context_wrap(FACTS_FQDN))
     sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
     expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
-    result = Hostname(hnf, hnd, hns, ft, sid)
+    result = Hostname(hnf, hnd, hns, sid)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
@@ -128,10 +100,9 @@ def test_hostname_function():
     hnf = HnF(context_wrap(HOSTNAME_FULL))
     hns = HnS(context_wrap(HOSTNAME_SHORT))
     hnd = HnD(context_wrap(HOSTNAME_DEF))
-    ft = Facter(context_wrap(FACTS_FQDN))
     sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
     expected = (HOSTNAME_FULL, HOSTNAME_SHORT, 'example.com')
-    result = hn_func(hnf, hnd, hns, ft, sid)
+    result = hn_func(hnf, hnd, hns, sid)
     assert result.fqdn == expected[0]
     assert result.hostname == expected[1]
     assert result.domain == expected[2]
@@ -147,11 +118,10 @@ def test_hostname_doc():
     hnf = HnF(context_wrap(HOSTNAME_FULL))
     hns = HnS(context_wrap(HOSTNAME_SHORT))
     hnd = HnD(context_wrap(HOSTNAME_DEF))
-    ft = Facter(context_wrap(FACTS_FQDN))
     sid = SystemID(context_wrap(SYSTEMID_PROFILE_NAME))
     env = {
-            'hostname': Hostname(hnf, hnd, hns, ft, sid),
-            'hn': hn_func(hnf, hnd, hns, ft, sid)
+            'hostname': Hostname(hnf, hnd, hns, sid),
+            'hn': hn_func(hnf, hnd, hns, sid)
           }
     failed, total = doctest.testmod(hostname, globs=env)
     assert failed == 0
--- insights/combiners/tests/test_uptime.py
+++ insights/combiners/tests/test_uptime.py
@@ -1,18 +1,10 @@
 import datetime
 from insights.parsers.uptime import Uptime
-from insights.parsers.facter import Facter
 from insights.combiners.uptime import uptime
 from insights.tests import context_wrap
 
 UPTIME1 = " 14:28:24 up  5:55,  4 users,  load average: 0.04, 0.03, 0.05"
 UPTIME2 = " 10:55:22 up 40 days, 3 min,  1 user,  load average: 0.49, 0.12, 0.04"
-UPTIME3 = """
-COMMAND> facts
-uptime => 21 days
-uptime_days => 21
-uptime_hours => 525
-uptime_seconds => 1893598
-""".strip()
 
 
 def total_seconds(time_delta):
@@ -21,7 +13,7 @@ def total_seconds(time_delta):
 
 def test_get_uptime_uptime1():
     ut = Uptime(context_wrap(UPTIME1))
-    upt = uptime(ut, None)
+    upt = uptime(ut)
     assert upt.currtime == '14:28:24'
     assert upt.updays == ""
     assert upt.uphhmm == '5:55'
@@ -33,7 +25,7 @@ def test_get_uptime_uptime1():
 
 def test_get_uptime_uptime2():
     ut = Uptime(context_wrap(UPTIME2))
-    upt = uptime(ut, None)
+    upt = uptime(ut)
     assert upt.currtime == '10:55:22'
     assert upt.updays == '40'
     assert upt.uphhmm == '00:03'
@@ -43,20 +35,9 @@ def test_get_uptime_uptime2():
     assert total_seconds(upt.uptime) == total_seconds(c)
 
 
-def test_get_facter_uptime():
-    ft = Facter(context_wrap(UPTIME3))
-    upt = uptime(None, ft)
-    assert upt.updays == "21"
-    assert upt.uphhmm == '21:59'
-    assert upt.loadavg is None
-    c = datetime.timedelta(days=0, hours=0, minutes=0, seconds=1893598)
-    assert total_seconds(upt.uptime) == total_seconds(c)
-
-
 def test_get_both_uptime():
     ut = Uptime(context_wrap(UPTIME2))
-    ft = Facter(context_wrap(UPTIME3))
-    upt = uptime(ut, ft)
+    upt = uptime(ut)
     assert upt.currtime == '10:55:22'
     assert upt.updays == '40'
     assert upt.uphhmm == '00:03'
