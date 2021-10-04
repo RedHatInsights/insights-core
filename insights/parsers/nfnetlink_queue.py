e336ded865465d4ec00ca50dc47708dafc49a3ba
Ryan Blakley <rblakley@redhat.com>
2021-10-04 15:47:12 -0400

    Update the nfnetlink parser (#3239)
    * Replace the assert check with if statement and raise a parse exception
      if met.
    * Updated the parse to inherit from a list, and added the data property
      for backwards compatibility.
    * Moved the docstring from the top to within the class.
    * Updated the test to look for parse and skip exceptions instead of
      assertion errors.
    
    Signed-off-by: Ryan Blakley <rblakley@redhat.com>
    (cherry picked from commit f6b11e13d361038be910407342554c4d2bfaf98f)

--- insights/parsers/nfnetlink_queue.py
+++ insights/parsers/nfnetlink_queue.py
@@ -1,66 +1,75 @@
 """
 NfnetLinkQueue - file ``/proc/net/netfilter/nfnetlink_queue``
 =============================================================
-
-Reads the ``/proc/net/netfilter/nfnetlink_queue`` file and creates a list of
-dictionaries, one dictionary per row in the file.
-
-The keys of the dictionary are (see
-https://home.regit.org/netfilter-en/using-nfqueue-and-libnetfilter_queue/):
-
-- ``queue_number``
-- ``peer_portid``: good chance it is process ID of software listening to
-  the queue
-- ``queue_total``: current number of packets waiting in the queue
-- ``copy_mode``: 0 and 1 only message only provide meta data. If 2, the
-  message provides a part of packet of size copy range.
-- ``copy_range``: length of packet data to put in message
-- ``queue_dropped``: number of packets dropped because queue was full
-- ``user_dropped``: number of packets dropped because netlink message
-  could not be sent to userspace. If this counter is not zero, try
-  to increase netlink buffer size. On the application side, you will
-  see gap in packet id if netlink message are lost.
-- ``id_sequence``: packet id of last packet
-- The last field is always '1' and is ignored.
-
-Example Input::
-
-    0  -4423     0 2 65535     0     0       22  1
-    1  -4424     0 2 65535     0     0       27  1
-
-Examples:
-
-    >>> # Set up of the environment - ignore this bit:
-    >>> nfnetlink_queue_data = '''
-    ... 0  -4423     0 2 65535     0     0       22  1
-    ... 1  -4424     0 2 65535     0     0       27  1
-    ... '''
-    >>> from insights.tests import context_wrap
-    >>> from insights.parsers.nfnetlink_queue import NfnetLinkQueue
-    >>> nf = NfnetLinkQueue(context_wrap(nfnetlink_queue_data))
-
-    >>> # Usual usage in a rule, using the argument name `nf`
-    >>> 'copy_mode' in nf.data[0]
-    True
-    >>> nf.data[0]['copy_mode']  # Note: values as integers
-    2
-
 """
-from .. import Parser, parser
+from insights import parser, Parser
+from insights.parsers import ParseException, SkipException
 from insights.specs import Specs
 
 
 @parser(Specs.nfnetlink_queue)
-class NfnetLinkQueue(Parser):
-    """Reads the ``/proc/net/netfilter/nfnetlink_queue`` file and
-    creates a list of dictionaries, one dictionary per row in the file.
+class NfnetLinkQueue(Parser, list):
+    """
+    Reads the ``/proc/net/netfilter/nfnetlink_queue`` file and creates a list of
+    dictionaries, one dictionary per row in the file.
+
+    The keys of the dictionary are (see
+    https://home.regit.org/netfilter-en/using-nfqueue-and-libnetfilter_queue/):
+
+    - ``queue_number``
+    - ``peer_portid``: good chance it is process ID of software listening to
+      the queue
+    - ``queue_total``: current number of packets waiting in the queue
+    - ``copy_mode``: 0 and 1 only message only provide meta data. If 2, the
+      message provides a part of packet of size copy range.
+    - ``copy_range``: length of packet data to put in message
+    - ``queue_dropped``: number of packets dropped because queue was full
+    - ``user_dropped``: number of packets dropped because netlink message
+      could not be sent to userspace. If this counter is not zero, try
+      to increase netlink buffer size. On the application side, you will
+      see gap in packet id if netlink message are lost.
+    - ``id_sequence``: packet id of last packet
+    - The last field is always '1' and is ignored.
+
+    Example Input::
+
+        0  -4423     0 2 65535     0     0       22  1
+        1  -4424     0 2 65535     0     0       27  1
+
+    Examples:
+
+        >>> type(nf)
+        <class 'insights.parsers.nfnetlink_queue.NfnetLinkQueue'>
+        >>> 'copy_mode' in nf.data[0]
+        True
+        >>> nf.data[0]['copy_mode']
+        2
     """
 
     def parse_content(self, content):
-        self.data = []
+        if not content:
+            raise SkipException("Empty output.")
+
+        data = []
         fields = ['queue_number', 'peer_portid', 'queue_total', 'copy_mode',
-            'copy_range', 'queue_dropped', 'user_dropped', 'id_sequence']
+                  'copy_range', 'queue_dropped', 'user_dropped', 'id_sequence']
+
         for line in content:
             parts = [int(p.strip()) for p in line.split()]
-            assert len(parts) == 9
-            self.data.append(dict(zip(fields, parts)))
+
+            if len(parts) != 9:
+                raise ParseException("Can't parse content because there isn't 9 columns provided.")
+
+            data.append(dict(zip(fields, parts)))
+
+        if not data:
+            raise SkipException("No parsed data.")
+
+        self.extend(data)
+
+    @property
+    def data(self):
+        """
+        Set data as property to keep compatibility
+        """
+        return self
