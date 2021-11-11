7d9018bb1132cc2976fc54be8791e43b9e503e60
Rahul Srivastava <44598880+rasrivas-redhat@users.noreply.github.com>
2021-11-11 10:30:52 +0530

    New parser ldap config (#3257)
    * New parser ldap config
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * added multi_output to the spec
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Updated the doc string and use the list class
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Fixed the flake8 error
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * lc
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Updated to code to handle the multiple line attribute
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Optimized the code by following the reviewer suggtions
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Added test case to the doc string
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Fixing the pytest error
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Added line stripping check
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * updated the comment message
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Updated the doc string to explain the dn block and updated the code to show complete dn attribute name
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * updated the data structure for the return and added a search method
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * fixed the pipeline fail error
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * fixed the pipeline error
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * fixed the pipeline error
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * updated the doc string example for the search method
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * updated the doc string
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Added couple of test cases and optimized the code
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    
    * Added doc test example and optimized the code
    
    Signed-off-by: Rahul <rasrivas@redhat.com>
    (cherry picked from commit dfc844e8f586b053ed7684eb71889d57c77b6bb8)

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -291,6 +291,7 @@ class Specs(SpecSet):
     kubepods_cpu_quota = RegistryPoint(multi_output=True)
     lastupload = RegistryPoint(multi_output=True)
     ld_library_path_of_user = RegistryPoint()
+    ldif_config = RegistryPoint(multi_output=True)
     libssh_client_config = RegistryPoint(filterable=True)
     libssh_server_config = RegistryPoint(filterable=True)
     libvirtd_log = RegistryPoint(filterable=True)
