00c5a01badac428df3bf979efe4a1aae904a3a9e
Xiangce Liu <xiangceliu@redhat.com>
2021-11-17 09:57:05 +0800

    Enhance the pmlog_summary to support new metrics
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/pmlog_summary.py
+++ insights/parsers/pmlog_summary.py
@@ -10,14 +10,14 @@ from insights.specs import Specs
 
 def parse(data):
     """
-    Parse a set of key/value pairs into a heirarchical dictionary of
+    Parse a set of key/value pairs into a hierarchical dictionary of
     typed values
 
     Arguments:
         data (dict): Input dictionary of key/value pairs
 
     Returns:
-        dict: Heirarchical dictionary with keys separated at "." and type
+        dict: Hierarchical dictionary with keys separated at "." and type
         conversion of the numerical values
     """
     result = {}
@@ -31,6 +31,11 @@ def parse(data):
     def insert(k, v):
         cur = result
         key_parts = k.split(".")
+        # process the '["xxx"]' part as a sub-key
+        if v.startswith('['):
+            mk, _, v = v.partition(']')
+            key_parts.append(mk.strip('"[]'))
+            v = v.strip()
 
         # walk down the structure to the correct leaf
         for part in key_parts:
@@ -70,24 +75,43 @@ class PmLogSummary(CommandParser, dict):
         kernel.all.cpu.nice  0.000 none
         kernel.all.cpu.steal  0.000 none
         kernel.all.cpu.idle  3.986 none
+        kernel.all.pressure.io.full.avg ["10 second"] 0.001 none
+        kernel.all.pressure.cpu.some.avg ["1 minute"] 14.942 none
+        kernel.all.pressure.memory.full.avg ["5 minute"] 0.002 none
         disk.all.total  0.252 count / sec
+        disk.dev.total ["vda"] 0.016 count / sec
+        disk.dev.total ["vdb"] 0.445 count / sec
+        disk.dev.total ["vdc"] 2.339 count / sec
 
     Output is parsed and stored as a dictionary.  Each value is
     stored as a dict in the form ``{'val': number or string, 'units': string}``.
     Keys are a hierarchy of the input key value split on the "." character.
-    For instance input line "mem.util.used  3133919.812 Kbyte" is parsed
-    as::
 
-        {
-            'mem': {
-                'util': {
-                    'used': {
-                        'val': 3133919.812,
-                        'units': 'Kbyte'
+    For instance::
+        1. Input line "mem.util.used  3133919.812 Kbyte" is parsed as:
+            {
+                'mem': {
+                    'util': {
+                        'used': {
+                            'val': 3133919.812,
+                            'units': 'Kbyte'
+                        }
                     }
                 }
             }
-        }
+        2. Input line "disk.dev.total ["vdc"] 2.339 count / sec" is parsed as:
+            {
+                'disk': {
+                    'dev': {
+                        'total': {
+                            'vdc': {
+                                'val': 2.339
+                                'units': 'count / sec'
+                            }
+                        }
+                    }
+                }
+            }
 
     Example:
         >>> type(pmlog_summary)
@@ -95,6 +119,10 @@ class PmLogSummary(CommandParser, dict):
         >>> 'mem' in pmlog_summary
         True
         >>> pmlog_summary['disk']['all']['total'] == {'val': 0.252, 'units': 'count / sec'}
+        True
+        >>> pmlog_summary['disk']['dev']['total']['vdc'] == {'val': 2.339, 'units': 'count / sec'}
+        True
+        >>> pmlog_summary['kernel']['all']['pressure']['memory']['full']['avg']['5 minute'] == {'val': 0.002, 'units': 'none'}
         True
     """
 
