e51700b94b941981ad3e823fc2502bfe99ca2ce5
Xiangce Liu <xiangceliu@redhat.com>
2021-12-09 13:22:38 +0800

    Update as per the review comment

--- insights/tests/datasources/test_ssl_certificate.py
+++ insights/tests/datasources/test_ssl_certificate.py
@@ -59,11 +59,11 @@ HTTPD_SSL_CONF_2 = """
   ## SSL directives
   ServerName  f.g.e.com
   SSLEngine off
-  SSLCertificateFile      "/etc/pki/katello/certs/katello-apache_d.crt"
-  SSLCertificateKeyFile   "/etc/pki/katello/private/katello-apache_d.key"
-  SSLCertificateChainFile "/etc/pki/katello/certs/katello-server-ca_d.crt"
+  SSLCertificateFile      "/etc/pki/katello/certs/katello-apache_e.crt"
+  SSLCertificateKeyFile   "/etc/pki/katello/private/katello-apache_e.key"
+  SSLCertificateChainFile "/etc/pki/katello/certs/katello-server-ca_e.crt"
   SSLVerifyClient         optional
-  SSLCACertificateFile    "/etc/pki/katello/certs/katello-default-ca_d.crt"
+  SSLCACertificateFile    "/etc/pki/katello/certs/katello-default-ca_e.crt"
   SSLVerifyDepth          3
   SSLOptions +StdEnvVars +ExportCertData
 </VirtualHost>
@@ -167,6 +167,7 @@ def test_httpd_certificate():
         HttpdConfTree: conf_tree
     }
     result = httpd_ssl_certificate_files(broker)
+    # "/etc/pki/katello/certs/katello-apache_e.crt" not in the result
     assert result == ['/etc/pki/katello/certs/katello-apache.crt', '/etc/pki/katello/certs/katello-apache_d.crt']
 
 
