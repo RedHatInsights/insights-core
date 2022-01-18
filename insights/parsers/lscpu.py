1fd63e1786f82f0d6cf3550f141391fcc9dfe9a5
Ryan Blakley <rblakley@redhat.com>
2022-01-18 13:54:52 -0500

    Fix: Update lscpu parser to support RHEL9 output (#3320)
    * In RHEL9 lscpu added indentation and several section headers that
      don't have any values after the colon. So updated the parser to handle
      these section header lines.
    
    Signed-off-by: Ryan Blakley <rblakley@redhat.com>
    (cherry picked from commit 475712161bbceb3706a7b486b3dbad7db576dba2)

--- insights/parsers/lscpu.py
+++ insights/parsers/lscpu.py
@@ -65,5 +65,10 @@ class LsCPU(CommandParser):
         self.info = {}
         split_on = ":"
         for line in content:
-            k, v = line.split(split_on, 1)
-            self.info[k.replace("(s)", "s").strip()] = v.strip()
+            # On R9 they added indentation and section headers,
+            # so make sure the split len is 2 otherwise skip.
+            kv = line.split(split_on, 1)
+            if len(kv) != 2:
+                continue
+
+            self.info[kv[0].replace("(s)", "s").strip()] = kv[1].strip()
