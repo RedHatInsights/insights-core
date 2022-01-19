a05872a68b14a74848bf85262e20893d7f87a0a6
jiazhang <jiazhang@redhat.com>
2022-01-19 17:28:13 +0800

    Update docstring
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/config_file_perms.py
+++ insights/parsers/config_file_perms.py
@@ -5,11 +5,13 @@ Configuration File Permissions parsers
 Parsers included in this module are:
 
 SshdConfigPerms - command ``/bin/ls -lH /etc/ssh/sshd_config``
--------------------------------------------------------------
+--------------------------------------------------------------
+
 Grub1ConfigPerms - command ``/bin/ls -lH /boot/grub/grub.conf``
---------------------------------------------------------------
+---------------------------------------------------------------
+
 Grub2ConfigPerms - command ``/bin/ls -lH /boot/grub2/grub.cfg``
---------------------------------------------------------------
+---------------------------------------------------------------
 """
 
 from insights.core import CommandParser
