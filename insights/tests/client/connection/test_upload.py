ee5e27c652de45e169880e9bf60503e8191187e9
ahitacat <ahitacat@redhat.com>
2022-01-26 18:38:13 +0100

    Don't check if biggest_file is None
    This patch also make the unittest compatible with python 3 and 2.6
    Signed-off-by: ahitacat <ahitacat@redhat.com>

--- insights/tests/client/connection/test_upload.py
+++ insights/tests/client/connection/test_upload.py
@@ -1,17 +1,14 @@
 from mock.mock import Mock, patch
-import logging
 from insights.client.connection import InsightsConnection
 
 
 @patch('insights.client.connection.largest_spec_in_archive', return_value=("insights/big", 100, "insights.spec-big"))
-def test_archive_too_big(largest_archive_file, caplog):
+def test_archive_too_big(largest_archive_file):
     config = Mock(base_url="www.example.com", proxy=None)
     connection = InsightsConnection(config)
-    caplog.set_level(logging.INFO)
     with patch("insights.client.connection.os.stat", **{"return_value.st_size": 104857600}):
-        connection._archive_too_big("archive_file")
-        largest_archive_file.assert_called_once_with("archive_file")
-        assert len(caplog.records) == 3
-        assert ["insights.spec-big" in rec.message for rec in caplog.records]
-        record = next(iter(caplog.records))
-        assert record.message == "Archive is 100.0 MB which is larger than the maximum allowed size of 100 MB."
+        with patch('insights.client.connection.logger.info') as mock_logger:
+            connection._archive_too_big("archive_file")
+            largest_archive_file.assert_called_once_with("archive_file")
+            assert mock_logger.call_count == 3
+            assert ["insights.spec-big" in args[0][0] for args in mock_logger.call_args_list]
