1e9c05e5b0a2a165765503dac83c2441f416f6ff
Xiangce Liu <xiangceliu@redhat.com>
2022-03-17 20:29:43 +0800

    Refine the test
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/test_remote_resource.py
+++ insights/tests/test_remote_resource.py
@@ -1,14 +1,41 @@
+import sys
+import time
+import pytest
 from cachecontrol.cache import DictCache
 from insights.core.remote_resource import RemoteResource, CachedRemoteResource
 from insights.tests.mock_web_server import TestMockServer
-import sys
-import pytest
 
 GOOD_PAYLOAD = b'Successful return from Mock Service'
 NOT_FOUND = b'{"error":{"code": "404", "message": "Not Found"}}'
 
 
 class TestRemoteResource(TestMockServer):
+
+    # Test CachedRemoteResource returns cached response with specified expire_after
+    # - To avoid the `expire_after` is set to the default 180 seconds, this case
+    #   should be tested at first
+    @pytest.mark.skipif(sys.version_info < (2, 7), reason="CacheControl requires python 2.7 or higher")
+    def test_get_cached_remote_resource_cached_expire_after(self):
+        url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
+        crr = CachedRemoteResource(expire_after=1)
+        rtn = crr.get(url)
+        exp_1 = rtn.headers['expires']
+        time.sleep(2)
+        rtn = crr.get(url)
+        exp_2 = rtn.headers['expires']
+        assert exp_1 != exp_2
+
+        crr = CachedRemoteResource(expire_after=10)
+        # Refresh the cache
+        crr.get(url)
+        time.sleep(1)
+        # Use the new expire_after
+        rtn = crr.get(url)
+        exp_1 = rtn.headers['expires']
+        time.sleep(1)
+        rtn = crr.get(url)
+        exp_2 = rtn.headers['expires']
+        assert exp_1 == exp_2
 
     # Test RemoteResource
     def test_get_remote_resource(self):
@@ -38,18 +65,6 @@ class TestRemoteResource(TestMockServer):
     @pytest.mark.skipif(sys.version_info < (2, 7), reason="CacheControl requires python 2.7 or higher")
     def test_get_cached_remote_resource_cached(self):
         crr = CachedRemoteResource()
-
-        url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
-        rtn = crr.get(url)
-        cont_1 = rtn.content
-        rtn = crr.get(url)
-        cont_2 = rtn.content
-        assert cont_1 == cont_2
-
-    # Test CachedRemoteResource returns cached response with specified expire_after
-    @pytest.mark.skipif(sys.version_info < (2, 7), reason="CacheControl requires python 2.7 or higher")
-    def test_get_cached_remote_resource_cached_1_min(self):
-        crr = CachedRemoteResource(3600)
 
         url = 'http://localhost:{port}/mock/'.format(port=self.server_port)
         rtn = crr.get(url)
