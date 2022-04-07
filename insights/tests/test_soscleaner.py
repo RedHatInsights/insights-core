dd79e67ee12fcd781af77b5dd491aff2e92bfbe7
Glutexo <stomsa@redhat.com>
2022-04-07 17:51:43 +0200

    Exclude some Specs from IP address obfuscation (#3331)
    * Exclude some Specs from obfuscation
    
    Add a list of Specs that are excluded from the IP address obfuscation. This is to prevent invalid obfuscation of four-part package versions that are indistinguishable from IPv4 addresses. The excluded Specs are known to never contain IP addesses in their output.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Fix cyclic imports
    
    Test _blacklisted_files
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Clean up the blacklisted_files test
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Test IP obfuscation blacklist
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Test mocked clean_report
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Fix test for Python 2.6
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Rename obfuscation blacklist to exempted obfuscations
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Add function comment
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Add function comment
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Rename exempt to excluded
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    Fix formatting
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Move excluded Specs to SOSCleaner
    
    The module architecture causes problems when the constants module is imported from the SOSCleaner. Also, the SOSCleaner is intended to be a standalone module.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Add more Specs to the exclude list
    
    Provided by Michael Mraka <mmraka@redhat.com>.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Make excluded Specs a list
    
    For semantics.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * Fix excluded Specs test
    
    It makes no sense to replicate the Specs list in the test. Kept the test, only made it simpler. Now it checks that the list is not empty and that its items are valid Spec names.
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    
    * fixup! Fix excluded Specs test
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>

--- insights/tests/test_soscleaner.py
+++ insights/tests/test_soscleaner.py
@@ -1,13 +1,75 @@
+from collections import namedtuple
+from contextlib import contextmanager
+from json import dump
+from os import mkdir
+from os.path import join
+from shutil import rmtree
+from tempfile import mkdtemp
+
+from mock.mock import call
+from mock.mock import Mock
+from mock.mock import patch
+from pytest import mark
+
+from insights.client.config import InsightsConfig
+from insights.client.data_collector import CleanOptions
 from insights.contrib.soscleaner import SOSCleaner
 
-from mock.mock import Mock
-from pytest import mark
+ReportItem = namedtuple("ReportItem", ("name", "relative_path"))
+
+RPM_OUTPUT_SHADOW_UTILS =\
+    "{\"name\":\"shadow-utils\","\
+    "\"epoch\":\"2\","\
+    "\"version\":\"4.1.5.1\","\
+    "\"release\":\"5.el6\","\
+    "\"arch\":\"x86_64\","\
+    "\"installtime\":\"Wed 13 Jan 2021 10:04:18 AM CET\","\
+    "\"buildtime\":\"1455012203\","\
+    "\"vendor\":\"Red Hat, Inc.\","\
+    "\"buildhost\":\"x86-027.build.eng.bos.redhat.com\","\
+    "\"sigpgp\":"\
+    "\"RSA/8, "\
+    "Tue 08 Mar 2016 11:15:08 AM CET, "\
+    "Key ID 199e2f91fd431d51\"}"
 
 
 def _soscleaner():
     soscleaner = SOSCleaner()
     soscleaner.logger = Mock()
     return soscleaner
+
+
+@contextmanager
+def _mock_report_dir(report_items):
+    tmpdir = mkdtemp()
+    try:
+        hostname_path = join(tmpdir, "hostname")
+        with open(hostname_path, "w") as hostname_file:
+            hostname_file.write("test-hostname")
+
+        data_path = join(tmpdir, "data")
+        meta_data_path = join(tmpdir, "meta_data")
+        for path in [data_path, meta_data_path]:
+            mkdir(path)
+
+        for report_item in report_items:
+            json_path = join(
+                meta_data_path, "%s.json" % report_item.name
+            )
+            with open(json_path, "w") as json_file:
+                meta_data_obj = {
+                    "name": report_item.name,
+                    "results": {
+                        "object": {
+                            "relative_path": report_item.relative_path
+                        }
+                    }
+                }
+                dump(meta_data_obj, json_file)
+
+        yield tmpdir
+    finally:
+        rmtree(tmpdir)
 
 
 @mark.parametrize(("line", "expected"), [
@@ -101,3 +163,88 @@ def test_sub_ip_no_match(line):
     soscleaner = _soscleaner()
     actual = soscleaner._sub_ip(line)
     assert actual == line
+
+
+def test_excluded_specs():
+    soscleaner = SOSCleaner()
+    assert len(soscleaner.excluded_specs) > 0
+    for spec in soscleaner.excluded_specs:
+        assert spec.find("insights.specs.Specs.") == 0
+
+
+def test_excluded_files():
+    report_items = [
+        ReportItem(
+            "insights.specs.Specs.installed_rpms",
+            "insights_commands/rpm_-qa_--qf_name_NAME_epoch_EPOCH_"
+            "version_VERSION_release_RELEASE_arch_ARCH_"
+            "installtime_INSTALLTIME_date_buildtime_BUILDTIME_"
+            "vendor_VENDOR_buildhost_BUILDHOST_sigpgp_SIGPGP_pgpsig"
+        ),
+        ReportItem(
+            "insights.specs.Specs.ip_addr",
+            "insights_commands/ip_addr"
+        )
+    ]
+
+    soscleaner = _soscleaner()
+    with _mock_report_dir(report_items) as tmpdir:
+        soscleaner.dir_path = tmpdir
+        actual = soscleaner._excluded_files()
+
+    expected = [
+        report_item.relative_path
+        for report_item in report_items
+        if report_item.name in soscleaner.excluded_specs
+    ]
+    assert actual == expected
+
+
+@patch("insights.contrib.soscleaner.SOSCleaner._clean_file")
+@patch("insights.contrib.soscleaner.SOSCleaner._excluded_files")
+@patch("insights.contrib.soscleaner.SOSCleaner._extract_sosreport")
+@patch("insights.contrib.soscleaner.SOSCleaner._make_dest_env")
+@patch("insights.contrib.soscleaner.SOSCleaner._start_logging")
+def test_clean_report(
+    _start_logging,
+    _make_dest_env,
+    _extract_sosreport,
+    excluded_files,
+    clean_file
+):
+    class FileList:
+        def __init__(self, soscleaner):
+            self.soscleaner = soscleaner
+
+        def __call__(self, data_path):
+            self.soscleaner.file_count = len(file_list_files)
+
+            paths = []
+            for file in file_list_files:
+                path = join(self.soscleaner.dir_path, data_path, file)
+                paths.append(path)
+            self.return_value = paths
+            return paths
+
+    file_list_files = [
+        "insights_commands/rpm_-qa_--qf_name_NAME_version_VERSION",
+        "insights_commands/ip_addr"
+    ]
+
+    file_list_exempt = file_list_files[0:1]
+    excluded_files.configure_mock(return_value=file_list_exempt)
+
+    config = InsightsConfig()
+    options = CleanOptions(config, None, None, None)
+    options.no_tar_file = "."
+
+    soscleaner = _soscleaner()
+    with patch(
+        "insights.contrib.soscleaner.SOSCleaner._file_list",
+        FileList(soscleaner)
+    ) as file_list:
+        soscleaner.clean_report(options, ".")
+
+    file_list_whitelisted = file_list.return_value[1:2]
+    calls_whitelisted = map(call, file_list_whitelisted)
+    assert clean_file.mock_calls == list(calls_whitelisted)
