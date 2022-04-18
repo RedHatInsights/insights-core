7c05837788ec30ea8600f8a386076f39b612cd11
huali027 <44796653+huali027@users.noreply.github.com>
2022-04-18 16:01:51 +0800

    fix: Update the spec "du_dirs" to filterable (#3384)
    * fix: Update the spec "du_dirs" to filterable
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Move the "du_dirs_list" datasource to the datasource directory
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Rename to "dir_list"
    
    * Also raise SkipComponent if there are no filters
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    (cherry picked from commit 7d78fc53a08ad537f9499ee99c4f47e46581f1c0)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -32,8 +32,9 @@ from insights.components.ceph import IsCephMonitor
 from insights.combiners.satellite_version import SatelliteVersion, CapsuleVersion
 from insights.specs import Specs
 from insights.specs.datasources import (
-    awx_manage, cloud_init, candlepin_broker, ethernet, get_running_commands, ipcs, lpstat, package_provides,
-    ps as ps_datasource, sap, satellite_missed_queues, ssl_certificate, yum_updates)
+    awx_manage, cloud_init, candlepin_broker, dir_list, ethernet,
+    get_running_commands, ipcs, lpstat, package_provides, ps as ps_datasource,
+    sap, satellite_missed_queues, ssl_certificate, yum_updates)
 from insights.specs.datasources.sap import sap_hana_sid, sap_hana_sid_SID_nr
 from insights.specs.datasources.pcp import pcp_enabled, pmlog_summary_args
 
@@ -210,12 +211,7 @@ class DefaultSpecs(Specs):
     docker_sysconfig = simple_file("/etc/sysconfig/docker")
     dotnet_version = simple_command("/usr/bin/dotnet --version")
     dracut_kdump_capture_service = simple_file("/usr/lib/dracut/modules.d/99kdumpbase/kdump-capture.service")
-
-    @datasource(HostContext)
-    def du_dirs_list(broker):
-        """ Provide a list of directorys for the ``du_dirs`` spec to scan """
-        return ['/var/lib/candlepin/activemq-artemis']
-    du_dirs = foreach_execute(du_dirs_list, "/bin/du -s -k %s")
+    du_dirs = foreach_execute(dir_list.du_dir_list, "/bin/du -s -k %s")
     engine_db_query_vdsm_version = simple_command('engine-db-query --statement "SELECT vs.vds_name, rpm_version FROM vds_dynamic vd, vds_static vs WHERE vd.vds_id = vs.vds_id" --json')
     engine_log = simple_file("/var/log/ovirt-engine/engine.log")
     etc_journald_conf = simple_file(r"etc/systemd/journald.conf")
