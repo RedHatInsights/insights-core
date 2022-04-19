3719affec8057942746cdafee7a24c383c06029a
Xiangce Liu <xiangceliu@redhat.com>
2022-04-19 11:38:25 +0800

    use  instead of
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/ibm_proc.py
+++ insights/parsers/ibm_proc.py
@@ -58,6 +58,7 @@ class IBMFirmwareLevel(Parser):
         FW950.30 (VL950_092)\x00
 
     Attributes:
+        raw (str): The RAW value of the `ibm,fw-vernum_encoded` file.
         firmware_level (str): The firmware level required by FLRT.
 
     Examples:
@@ -72,9 +73,9 @@ class IBMFirmwareLevel(Parser):
         if not content:
             raise SkipException("Empty output.")
 
-        _, fwl = content[0].split(None, 1)
+        self.raw = content[0]
 
-        if not fwl:
+        if "(" not in self.raw or ")" not in self.raw:
             raise SkipException("Nothing to parse.")
 
-        self.firmware_level = fwl.strip('()\x00')
+        self.firmware_level = self.raw[self.raw.index('(')+1:self.raw.index(')')]
