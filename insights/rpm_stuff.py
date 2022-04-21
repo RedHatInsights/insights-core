c837f0227f0c6d0242d5b60e38062ef88daec4a8
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2022-04-21 11:40:21 -0500

    Fix flake8

--- insights/rpm_stuff.py
+++ insights/rpm_stuff.py
@@ -7,9 +7,11 @@ def rpm_commands(num):
     try:
         import rpm
         success = True
+        version = rpm.__version__
         print("Successfully imported rpm module")
     except ImportError:
         print("Unable to import rpm module")
         success = False
+        version = ""
 
-    return num + num, success
+    return num + num, success, version
--- insights/tests/test_rpm_stuff.py
+++ insights/tests/test_rpm_stuff.py
@@ -1,14 +1,17 @@
 from insights import rpm_stuff
-
-_skip_tests = False
-import mock
+# import mock
 import pytest
 try:
     import rpm
+    _skip_tests = False
 except ImportError:
     _skip_tests = True
 
 
 @pytest.mark.skipif(_skip_tests, reason="Skip rpm module")
 def test_rpm_stuff():
-    assert rpm_stuff.rpm_commands(1) == (2, True)
+    result, success, version = rpm_stuff.rpm_commands(1)
+    print("RPM Version", version)
+    print("RPM Version", rpm.__version__)
+    assert result == 2
+    assert success is True
