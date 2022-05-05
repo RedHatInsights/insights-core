5d8a294808163133b311d414d1a491269a716212
Xiaoxue Wang <xiaoxwan@redhat.com>
2022-05-05 13:21:13 +0800

    Add parser bdi_read_ahead_kb for '/sys/class/bdi/*/read_ahead_kb' files (#3391)
    * Add parser bdi_read_ahead_kb for '/sys/class/bdi/*/read_ahead_kb' files
    
    Signed-off-by: XiaoXue Wang <xiaoxwan@redhat.com>
    
    * Fix the pytest and CI errors
    
    Signed-off-by: XiaoXue Wang <xiaoxwan@redhat.com>
    
    * Add doc build-entry
    
    Signed-off-by: XiaoXue Wang <xiaoxwan@redhat.com>
    
    * Fix doc CI error
    
    Signed-off-by: XiaoXue Wang <xiaoxwan@redhat.com>
    (cherry picked from commit e08fbacb2b9ade52c79478ed52a20214086529e0)

---
+++ insights/parsers/tests/test_bdi_read_ahead_kb.py
@@ -0,0 +1,31 @@
+import doctest
+import pytest
+from insights.parsers import bdi_read_ahead_kb, ParseException
+from insights.tests import context_wrap
+
+BDI_READ_AHEAD_KB = """
+128
+""".strip()
+
+BDI_READ_AHEAD_KB_INVALID = """
+invalid
+""".strip()
+
+
+def test_bdi_read_ahead_kb():
+    read_ahead_kb = bdi_read_ahead_kb.BDIReadAheadKB(context_wrap(BDI_READ_AHEAD_KB))
+    assert read_ahead_kb.read_ahead_kb == 128
+
+
+def test_invalid_bdi_read_ahead_kb():
+    with pytest.raises(ParseException) as e:
+        bdi_read_ahead_kb.BDIReadAheadKB(context_wrap(BDI_READ_AHEAD_KB_INVALID))
+    assert "Error: " in str(e)
+
+
+def test_bdi_read_ahead_kb_doc_examples():
+    env = {
+        'bdi_read_ahead_kb': bdi_read_ahead_kb.BDIReadAheadKB(context_wrap(BDI_READ_AHEAD_KB)),
+    }
+    failed, total = doctest.testmod(bdi_read_ahead_kb, globs=env)
+    assert failed == 0
