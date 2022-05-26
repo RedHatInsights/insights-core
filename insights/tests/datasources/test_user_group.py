80c52e633697baa2b444cc62a928bc7f6d0b45ee
Xiangce Liu <xiangceliu@redhat.com>
2022-05-26 17:54:16 +0800

    Add test for datasource 'groups'
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

---
+++ insights/tests/datasources/test_user_group.py
@@ -0,0 +1,32 @@
+import pytest
+
+from insights.specs import Specs
+from insights.core import filters
+from insights.specs.datasources.user_group import groups
+from insights.core.dr import SkipComponent
+
+
+def setup_function(func):
+    if Specs.group_info in filters._CACHE:
+        del filters._CACHE[Specs.group_info]
+    if Specs.group_info in filters.FILTERS:
+        del filters.FILTERS[Specs.group_info]
+
+    if func is test_group_info_list:
+        filters.add_filter(Specs.group_info, ["wheel", "mem"])
+    if func is test_group_info_no_filter:
+        filters.add_filter(Specs.group_info, [])
+
+
+def test_group_info_list():
+    broker = {}
+    result = groups(broker)
+    assert len(result) == 2
+    assert 'wheel' in result
+    assert 'mem' in result
+
+
+def test_group_info_no_filter():
+    broker = {}
+    with pytest.raises(SkipComponent):
+        groups(broker)
