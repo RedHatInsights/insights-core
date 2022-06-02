9d26096a077f870847a9f88530bbdb50f5b4d9e8
Xiangce Liu <xiangceliu@redhat.com>
2022-06-02 13:29:39 +0800

    feat: new spec and parser for /etc/sudoers (#3425)
    * New spec and parser for /etc/sudoers
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * feat: New spec and parser for /etc/sudoers
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * fix combiner to check the '#includedir' directive
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * single test func
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * add note for #inlcudedir
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * add the missed multi_output=True
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * add a base class
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * add note to the combiner for the base class
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * refine the doc
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 2b2c7de67ca0f35fcaa6119968d018c6848f013e)

---
+++ insights/tests/combiners/test_sudoers.py
@@ -0,0 +1,69 @@
+import pytest
+import doctest
+from insights.parsers.sudoers import EtcSudoers
+from insights.combiners.sudoers import Sudoers
+from insights.combiners import sudoers
+from insights.tests import context_wrap
+
+SUDOERS = """
+## Allows people in group wheel to run all commands
+%wheel  ALL=(ALL)       ALL
+## Read drop-in files from /etc/sudoers.d (the # here does not mean a comment)
+#includedir /etc/sudoers.d
+""".strip()
+SUDOERS_NO_INCLUDE = """
+## Allows people in group wheel to run all commands
+%wheel  ALL=(ALL)       ALL
+## Read drop-in files from /etc/sudoers.d (the # here does not mean a comment)
+##includedir /etc/sudoers.d
+""".strip()
+SUDOERS_FM = """
+foreman-proxy ALL = (root) NOPASSWD : /opt/puppetlabs/bin/puppet cert *
+Defaults:foreman-proxy !requiretty
+""".strip()
+SUDOERS_WH = """
+%wheel  ALL=(ALL)       ABC
+Defaults:wheel !requiretty
+""".strip()
+SUDOERS_PATH1 = "/etc/sudoers"
+SUDOERS_PATH2 = "/etc/sudoers.d/forman-proxy"
+SUDOERS_PATH3 = "/etc/sudoers.d/test"
+
+
+def test_sudoers():
+    sudo1 = EtcSudoers(context_wrap(SUDOERS, path=SUDOERS_PATH1))
+    sudo2 = EtcSudoers(context_wrap(SUDOERS_FM, path=SUDOERS_PATH2))
+    sudo3 = EtcSudoers(context_wrap(SUDOERS_WH, path=SUDOERS_PATH3))
+    sudo = Sudoers([sudo1, sudo2, sudo3])
+    assert len(sudo.lines) == 6
+    assert sudo.last("#includedir") == '#includedir /etc/sudoers.d'
+    assert len(sudo.get(['wheel', 'includedir'])) == 0
+    assert len(sudo.get(['wheel', 'includedir'], check=any)) == 4
+    assert sudo.last(['Defaults', 'includedir'], check=all) is None
+    assert len(sudo.get(['foreman-proxy', 'NOPASSWD'], check=all)) == 1
+    assert len(sudo.get(['foreman-proxy', 'NOPASSWD'], check=any)) == 2
+    wheel = sudo.get(['wheel', 'ALL=(ALL)'])
+    assert len(wheel) == 2
+    assert wheel[1] == '%wheel  ALL=(ALL)       ABC'
+    assert sudo.last("Defaults") == 'Defaults:wheel !requiretty'
+
+    with pytest.raises(TypeError):
+        sudo.get({})
+
+
+def test_sudoers_no_includedir():
+    sudo1 = EtcSudoers(context_wrap(SUDOERS_NO_INCLUDE, path=SUDOERS_PATH1))
+    sudo2 = EtcSudoers(context_wrap(SUDOERS_FM, path=SUDOERS_PATH2))
+    sudo3 = EtcSudoers(context_wrap(SUDOERS_WH, path=SUDOERS_PATH3))
+    sudo = Sudoers([sudo1, sudo2, sudo3])
+    assert len(sudo.lines) == 1
+
+
+def test_doc_examples():
+    sudo1 = EtcSudoers(context_wrap(SUDOERS, path=SUDOERS_PATH1))
+    sudo2 = EtcSudoers(context_wrap(SUDOERS_FM, path=SUDOERS_PATH2))
+    env = {
+        'sudo': Sudoers([sudo1, sudo2])
+    }
+    failed, total = doctest.testmod(sudoers, globs=env)
+    assert failed == 0
