d6938768e1946ca55ce553d561c027adbc1a0a3c
xintli <986222045@qq.com>
2022-07-14 13:02:40 +0800

    Feat: Add teamdctl_state_dump spec to insights_archive (#3455)
    * Feat: Add teamdctl_state_dump spec to insights_archive
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * fix:erroneously modify ethtool_k
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Modify path
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Add team test
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * move team_device to ethernet
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update test for team_device
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Filtered discounted teams
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * correct abbreviation
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    Co-authored-by: Xinting Li <xintli@redhat.com>
    (cherry picked from commit dea3e1f7e02a568d31c2c5cfe0b224fca169f4a7)

--- insights/specs/datasources/ethernet.py
+++ insights/specs/datasources/ethernet.py
@@ -5,6 +5,7 @@ from insights.core.context import HostContext
 from insights.core.dr import SkipComponent
 from insights.core.plugins import datasource
 from insights.core.spec_factory import simple_command
+from insights.parsers.nmcli import NmcliConnShow
 from insights.specs import Specs
 
 
@@ -53,5 +54,34 @@ def interfaces(broker):
 
         if ifaces:
             return sorted(ifaces)
+
+    raise SkipComponent
+
+
+@datasource(NmcliConnShow, HostContext)
+def team_interfaces(broker):
+    """
+    This datasource provides a list of the team device.
+
+    Sample data returned::
+
+        ['team0', 'team1']
+
+    Returns:
+        list: List of the team device.
+
+    Raises:
+        SkipComponent: When there is not any team interfaces.
+    """
+
+    content = broker[NmcliConnShow].data
+    if content:
+        team_ifaces = []
+        for x in content:
+            if 'team' in x['TYPE'] and x['DEVICE'] != '--':
+                team_ifaces.append(x['DEVICE'])
+
+        if team_ifaces:
+            return sorted(team_ifaces)
 
     raise SkipComponent
