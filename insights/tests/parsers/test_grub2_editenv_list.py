1c346de19f9aac29aa43a312c7b761fc91a7b406
Xinting Li <xintli@redhat.com>
2022-07-25 16:03:37 +0800

    modify test content name
    Signed-off-by: Xinting Li <xintli@redhat.com>

--- insights/tests/parsers/test_grub2_editenv_list.py
+++ insights/tests/parsers/test_grub2_editenv_list.py
@@ -4,7 +4,7 @@ from insights.tests.parsers import skip_exception_check
 from insights.tests import context_wrap
 
 
-GRUBENV_WITH_TUNED_PARAMS = """
+CONTENT_WITH_TUNED_PARAMS = """
 saved_entry=295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64
 kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200 
 boot_success=0
@@ -13,32 +13,32 @@ tuned_params=transparent_hugepages=never
 tuned_initrd=
 """.strip()  # noqa
 
-GRUBENV_WITHOUT_TUNED_PARAMS = """
+CONTENT_WITHOUT_TUNED_PARAMS = """
 saved_entry=295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64
 kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200 
 boot_success=0
 boot_indeterminate=2
 """.strip()  # noqa
 
-GRUBENV_RHEL7 = """
+CONTENT_RHEL7 = """
 saved_entry=Red Hat Enterprise Linux Server (3.10.0-1127.el7.x86_64) 7.8 (Maipo)
 """.strip()  # noqa
 
-GRUBENV_ERROR = """
+CONTENT_ERROR = """
 /usr/bin/grub2-editenv: error: invalid environment block.
 """
 
 
 def test_doc_examples():
     env = {
-        'grub2_editenv_list': grub2_editenv_list.Grub2EditenvList(context_wrap(GRUBENV_WITH_TUNED_PARAMS))
+        'grub2_editenv_list': grub2_editenv_list.Grub2EditenvList(context_wrap(CONTENT_WITH_TUNED_PARAMS))
     }
     failed, total = doctest.testmod(grub2_editenv_list, globs=env)
     assert failed == 0
 
 
 def test_with_tuned_params():
-    results = grub2_editenv_list.Grub2EditenvList(context_wrap(GRUBENV_WITH_TUNED_PARAMS))
+    results = grub2_editenv_list.Grub2EditenvList(context_wrap(CONTENT_WITH_TUNED_PARAMS))
     assert results is not None
     assert results.has_kernelopts
     assert results.has_tuned_params
@@ -50,13 +50,13 @@ def test_with_tuned_params():
 
 
 def test_with_error():
-    results = grub2_editenv_list.Grub2EditenvList(context_wrap(GRUBENV_ERROR))
+    results = grub2_editenv_list.Grub2EditenvList(context_wrap(CONTENT_ERROR))
     assert results is not None
     assert results.error == ["/usr/bin/grub2-editenv: error: invalid environment block."]
 
 
 def test_without_tuned_params():
-    results = grub2_editenv_list.Grub2EditenvList(context_wrap(GRUBENV_WITHOUT_TUNED_PARAMS))
+    results = grub2_editenv_list.Grub2EditenvList(context_wrap(CONTENT_WITHOUT_TUNED_PARAMS))
     assert results is not None
     assert results.has_kernelopts
     assert not results.has_tuned_params
@@ -68,7 +68,7 @@ def test_without_tuned_params():
 
 
 def test_r7():
-    results = grub2_editenv_list.Grub2EditenvList(context_wrap(GRUBENV_RHEL7))
+    results = grub2_editenv_list.Grub2EditenvList(context_wrap(CONTENT_RHEL7))
     assert results is not None
     assert not results.has_kernelopts
     assert not results.has_tuned_params
