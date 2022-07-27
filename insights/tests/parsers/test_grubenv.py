2746655f49a70701988dc58472dea727c8dc1fc0
Xinting Li <xintli@redhat.com>
2022-07-27 10:50:44 +0800

    Fix flake8 error
    Signed-off-by: Xinting Li <xintli@redhat.com>

--- insights/tests/parsers/test_grubenv.py
+++ insights/tests/parsers/test_grubenv.py
@@ -34,7 +34,7 @@ saved_entry=Red Hat Enterprise Linux Server (3.10.0-1127.el7.x86_64) 7.8 (Maipo)
 
 GRUBEDITENVLIST_WITH_TUNED_PARAMS = """
 saved_entry=295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64
-kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200 
+kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200
 boot_success=0
 boot_indeterminate=2
 tuned_params=transparent_hugepages=never
@@ -43,7 +43,7 @@ tuned_initrd=
 
 GRUBEDITENVLIST_WITHOUT_TUNED_PARAMS = """
 saved_entry=295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64
-kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200 
+kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200
 boot_success=0
 boot_indeterminate=2
 """.strip()
@@ -58,7 +58,6 @@ grub2-editenv: error: invalid environment block.
 
 GRUBEDITENVLIST_EMPTY = """
 """
-
 
 
 def test_doc_examples():
@@ -114,7 +113,7 @@ def test_grub2_editenv_list_with_tuned_params():
     assert results is not None
     assert results.has_kernelopts
     assert results.has_tuned_params
-    assert results.kernelopts == "root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200"  # noqa
+    assert results.kernelopts == "root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200"
     assert results.tuned_params == "transparent_hugepages=never"
     assert results['boot_success'] == "0"
 
@@ -130,7 +129,7 @@ def test_grub2_editenv_list_without_tuned_params():
     assert results is not None
     assert results.has_kernelopts
     assert not results.has_tuned_params
-    assert results.kernelopts == "root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200"  # noqa
+    assert results.kernelopts == "root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200"
     assert results.tuned_params == ""
     assert results['boot_indeterminate'] == "2"
 
