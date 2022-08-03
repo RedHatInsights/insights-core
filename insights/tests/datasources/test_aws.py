ddaa0b180db0ac2f529f62ff7ca09c51da14bdd5
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2022-08-03 00:27:52 -0500

    fix: Update aws specs to use IMDSv2 (#3486)
    * Update aws specs to use IMDSv2
    
    * AWS IMDSv2 uses sessions for metadata access which requires the extra step of collecting a token prior to accessing metadata
    * Add a new datasource to get the IMDSv2 token
    * Modifiy existing specs to use the token to access metadata
    * Add datasource to docs
    * This change resolves #3485
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>
    
    * Update aws specs to use IMDSv2
    
    * AWS IMDSv2 uses sessions for metadata access which requires the extra step of collecting a token prior to accessing metadata
    * Add a new datasource to get the IMDSv2 token
    * Modifiy existing specs to use the token to access metadata
    * Add datasource to docs
    * This change resolves #3485
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>
    
    * Fix py2.6 test
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>
    
    * Reduce token lifetime to 60 sec
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>
    (cherry picked from commit 9154828bcc8c101a7df2fe4eb53c1344841fc5b2)

---
+++ insights/tests/datasources/test_aws.py
@@ -0,0 +1,30 @@
+import pytest
+from mock.mock import Mock
+from insights.core.dr import SkipComponent
+from insights.specs.datasources.aws import aws_imdsv2_token, LocalSpecs
+
+
+TOKEN = "1234567890\n"
+
+
+def test_aws_imdsv2_token():
+    input_spec = Mock()
+    input_spec.content = [TOKEN, ]
+    broker = {LocalSpecs.aws_imdsv2_token: input_spec}
+    results = aws_imdsv2_token(broker)
+    assert results == TOKEN.strip()
+
+
+def test_aws_imdsv2_token_exp():
+    input_spec = Mock()
+    input_spec.content = []
+    broker = {LocalSpecs.aws_imdsv2_token: input_spec}
+    with pytest.raises(Exception) as ex:
+        aws_imdsv2_token(broker)
+    assert "Unexpected" in str(ex)
+
+    input_spec = Mock()
+    input_spec.content = ["  ", ]
+    broker = {LocalSpecs.aws_imdsv2_token: input_spec}
+    with pytest.raises(SkipComponent) as ex:
+        aws_imdsv2_token(broker)
