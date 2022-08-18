154755925e95880fd75b78f6019477fd42e74743
xintli <986222045@qq.com>
2022-08-18 14:01:14 +0800

    Feat: New grub2_editenv_list parser (#3481)
    * Feat: New grub2_editenv_list parser
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update default and init
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update __init__
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Correct docs-test error
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * modify test content name
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Add doc entry
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Moving to grubenv
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Fix flake8 error
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Add base class
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * modify self._error to self._errors
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Replace GrubEnv from handle ``/boot/grub2/grubenv`` file to ``grub2-editenv list`` command output
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update insights_archive
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update doc string
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    Co-authored-by: Xinting Li <xintli@redhat.com>

--- insights/parsers/grubenv.py
+++ insights/parsers/grubenv.py
@@ -1,14 +1,12 @@
 """
-GrubEnv - file ``/boot/grub2/grubenv``
-======================================
+GRUB environment block
+======================
+This module provides processing for the GRUB environment block.
+Parsers included are:
 
-This parser reads the GRUB environment block file. The file is laid out in a
-key=value format similar to an ini file except it doesn't have any headers.
 
-The parser stores the key/value pairs in itself which inherits a dict. This
-file is only used in Grub 2, but in RHEL8 with BLS being the default. There
-were several variables added that are referenced in the
-``/boot/loader/entries/*.conf`` files.
+GrubEnv - command ``grub2-editenv list``
+----------------------------------------
 """
 from insights import get_active_lines, parser, Parser
 from insights.parsers import SkipException
@@ -18,10 +16,26 @@ from insights.specs import Specs
 @parser(Specs.grubenv)
 class GrubEnv(Parser, dict):
     """
-    Parses the /boot/grub2/grubenv file and returns a dict
-    of the grubenv variables.
+    This parser parses the output of the command ``grub2-editenv list``, which
+    list the current variables in GRUB environment block file or error message.
+    The module stores the key/value pairs in itself which inherits a dict.
 
-    Sample output of the file::
+    This parser used to handle the contents of the ``/boot/grub2/grubenv`` file
+    before. Since the command output without error messages is the same as the
+    content in ``/boot/grub2/grubenv``, and the ``grub2-editenv list`` command
+    output will show the error messages if the file is corrupt or has serious
+    errors in content, the current parser processes the ``grub2-editenv list``
+    command and returns a dict of the grubenv variables.
+
+    Cause the sosreports only collect the grubenv file and don't collect the
+    ``grub2-editenv list`` command, the GrubEnv parser only handles the
+    ``/boot/grub2/grubenv`` content for sos_archive.
+
+    ``/boot/grub2/grubenv`` file is only used in Grub 2, but in RHEL8 with BLS
+    being the default. There were several variables added that are referenced in
+    the ``/boot/loader/entries/*.conf`` files.
+
+    Sample output of the command::
 
         saved_entry=295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64
         kernelopts=root=/dev/mapper/root_vg-lv_root ro crashkernel=auto resume=/dev/mapper/root_vg-lv_swap rd.lvm.lv=root_vg/lv_root rd.lvm.lv=root_vg/lv_swap console=tty0 console=ttyS0,115200
@@ -29,12 +43,6 @@ class GrubEnv(Parser, dict):
         boot_indeterminate=2
         tuned_params=transparent_hugepages=never
         tuned_initrd=
-
-    Attributes:
-        has_kernelopts (bool): Returns True/False depending on if kernelopts key is in the dict.
-        kernelopts (bool): Returns the string of kernelopts from the dict.
-        has_tuned_params (str): Returns True/False depending of if the tuned_params key is in the dict.
-        tuned_params (str): Returns the string of tuned_params from the dict.
 
     Examples:
         >>> type(grubenv)
@@ -50,6 +58,7 @@ class GrubEnv(Parser, dict):
         >>> grubenv['saved_entry']
         '295e1ba1696e4fad9e062f096f92d147-4.18.0-305.el8.x86_64'
     """
+
     def __init__(self, context):
         super(GrubEnv, self).__init__(context)
 
@@ -57,36 +66,45 @@ class GrubEnv(Parser, dict):
         if not content:
             raise SkipException("Empty output.")
 
+        self._errors = []
         data = dict()
         for line in get_active_lines(content):
             if "=" not in line:
+                self._errors.append(line)
                 continue
 
             key, value = line.split("=", 1)
-
-            # Some keys can have empty values, so just skip them.
             if not value:
                 continue
 
             data[key] = value
 
-        if not data:
+        if (not data) and (not self._errors):
             raise SkipException("No parsed data.")
 
         self.update(data)
 
     @property
     def has_kernelopts(self):
+        """ bool: Returns True/False depending on if kernelopts key is in the dict. """
         return "kernelopts" in self
 
     @property
     def kernelopts(self):
+        """ str: Returns the string of kernelopts from the dict. """
         return self.get("kernelopts", "")
 
     @property
     def has_tuned_params(self):
+        """ bool: Returns True/False depending of if the tuned_params key is in the dict. """
         return "tuned_params" in self
 
     @property
     def tuned_params(self):
+        """ str: Returns the string of tuned_params from the dict."""
         return self.get("tuned_params", "")
+
+    @property
+    def errors(self):
+        """ list: Returns the list of error message. """
+        return self._errors
