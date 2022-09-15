fbd9f51c3f4cb090966a38377364b01ad5750a7d
Xiangce Liu <xiangceliu@redhat.com>
2022-09-15 04:07:33 -0400

    fix: bz-2126966: use SIGTERM for rpm instead of SIGKILL
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -541,7 +541,7 @@ class DefaultSpecs(Specs):
     rhsm_releasever = simple_file('/var/lib/rhsm/cache/releasever.json')
     rndc_status = simple_command("/usr/sbin/rndc status")
     ros_config = simple_file("/var/lib/pcp/config/pmlogger/config.ros")
-    rpm_ostree_status = simple_command("/usr/bin/rpm-ostree status --json")
+    rpm_ostree_status = simple_command("/usr/bin/rpm-ostree status --json", signum=signal.SIGTERM)
     rpm_V_packages = simple_command("/bin/rpm -V coreutils procps procps-ng shadow-utils passwd sudo chrony", keep_rc=True, signum=signal.SIGTERM)
     rsyslog_conf = glob_file(["/etc/rsyslog.conf", "/etc/rsyslog.d/*.conf"])
     samba = simple_file("/etc/samba/smb.conf")
