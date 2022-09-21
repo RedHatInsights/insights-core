87f3aaac0c79ee361281a925ee48941e9aeff18b
Huanhuan Li <huali@redhat.com>
2022-09-21 11:36:08 +0800

    Reimplement the parser "CronForeman"
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/parsers/cron_jobs.py
+++ insights/parsers/cron_jobs.py
@@ -1,6 +1,6 @@
 """
-Cron job files under /etc/cron* but not users crontab
-=====================================================
+Cron job files under /etc/cron.d/*
+==================================
 
 This module contains the following parsers:
 
@@ -8,13 +8,14 @@ CronForeman - file ``/etc/cron.d/foreman``
 ------------------------------------------
 """
 
-from insights.core import Scannable
+from insights.core import Parser
+from insights.parsers import get_active_lines, ParseException
 from insights.core.plugins import parser
 from insights.specs import Specs
 
 
 @parser(Specs.cron_foreman)
-class CronForeman(Scannable):
+class CronForeman(Parser, list):
     """
     Parse the ``/etc/cron.d/foreman`` file.
 
@@ -31,28 +32,89 @@ class CronForeman(Scannable):
 
         # Send out recurring notifications
         0 7 * * *       foreman    /usr/sbin/foreman-rake reports:daily 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+        0 5 * * 0       foreman    /usr/sbin/foreman-rake reports:weekly 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+        0 3 1 * *       foreman    /usr/sbin/foreman-rake reports:monthly 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+
+        # Expire old reports
         30 7 * * *      foreman    /usr/sbin/foreman-rake reports:expire 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
         30 7 * * *      foreman    /usr/sbin/foreman-rake reports:expire days=3 report_type=ForemanOpenscap::ArfReport 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
 
+        # Expire old audits
+        0 1 * * *      foreman    /usr/sbin/foreman-rake audits:expire 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/audit.log
+
+    Attributes:
+        environment(dict): The environment variables.
+        invalid_lines(list): A list of lines which are not in expected format.
+
+    Raises:
+        ParseException: When a line starts with '@', but the nickname is unknown.
+
     Examples:
-        >>> # get all the lines containing "foreman-rake reports:expire" with "collect" method
-        >>> # CronForeman.collect('test_reports_expire', lambda n: n if "foreman-rake reports:expire" in n else "")
-        >>> isinstance(foreman_cron.test_reports_expire, list)
+        >>> type(foreman_jobs)
+        <class 'insights.parsers.cron_jobs.CronForeman'>
+        >>> len(foreman_jobs.environment)
+        4
+        >>> 'FOREMAN_HOME' in foreman_jobs.environment
         True
-        >>> len(foreman_cron.test_reports_expire) == 2
+        >>> foreman_jobs.environment['FOREMAN_HOME']
+        '/usr/share/foreman'
+        >>> len(foreman_jobs)
+        7
+        >>> foreman_jobs[0].get('minute')
+        '15'
+        >>> foreman_jobs[0].get('hour')
+        '23'
+        >>> foreman_jobs[0].get('username')
+        'foreman'
+        >>> 'db:sessions:clear' in foreman_jobs[0].get('command')
         True
-        >>> 'days' in foreman_cron.test_reports_expire[0]
-        False
-        >>> 'days' in foreman_cron.test_reports_expire[1]
-        True
-        >>> items = foreman_cron.test_reports_expire[1].split()
-        >>> items[8]
-        'days=3'
-        >>> # get the first line containing "foreman-rake reports:expire" with "any" method
-        >>> # CronForeman.any('test_reports_expire_2', lambda n: n if "foreman-rake reports:expire" in n else "")
-        >>> isinstance(foreman_cron.test_reports_expire_2, str)
-        True
-        >>> 'days' in foreman_cron.test_reports_expire_2
-        False
     """
-    pass
+    def parse_content(self, content):
+        self.environment = {}
+        self.invalid_lines = []
+        # It can use 'nicknames' for common event frequencies:
+        nicknames = {
+            '@yearly': '0 0 1 1 *',
+            '@annually': '0 0 1 1 *',
+            '@monthly': '0 0 1 * *',
+            '@weekly': '0 0 * * 0',
+            '@daily': '0 0 * * *',
+            '@hourly': '0 * * * *',
+        }
+        for line in get_active_lines(content):
+            if line.startswith('@'):
+                # Reboot is 'special':
+                if line.startswith('@reboot'):
+                    parts = line.split(None, 2)
+                    self.append({'time': '@reboot', 'username': parts[1], 'command': parts[2]})
+                    continue
+                else:
+                    parts = line.split(None, 2)
+                    if parts[0] not in nicknames:
+                        raise ParseException(
+                            "{n} not recognised as a time specification 'nickname'".format(n=parts[0])
+                        )
+                    # Otherwise, put the time spec nickname translation into
+                    # the line
+                    line = line.replace(parts[0], nicknames[parts[0]])
+            items = line.split(None, 6)
+            if len(items) == 7:
+                self.append({
+                    'minute': items[0],
+                    'hour': items[1],
+                    'day_of_month': items[2],
+                    'month': items[3],
+                    'day_of_week': items[4],
+                    'username': items[5],
+                    'command': items[6]
+                })
+            else:
+                parts = line.split('=')
+                if len(parts) == 2:
+                    self.environment[parts[0].strip()] = parts[1].strip()
+                else:
+                    self.invalid_lines.append(line)
+
+    def search(self, filter_str):
+        """Return a list of dict for lines that have `filter_str` in the command."""
+        return [r for r in self if filter_str in r['command']]
--- insights/tests/parsers/test_cron_jobs.py
+++ insights/tests/parsers/test_cron_jobs.py
@@ -1,7 +1,8 @@
 import doctest
+import pytest
 
 from insights.parsers.cron_jobs import CronForeman
-from insights.parsers import cron_jobs
+from insights.parsers import ParseException, cron_jobs
 from insights.tests import context_wrap
 
 
@@ -29,18 +30,65 @@ FOREMAN_HOME=/usr/share/foreman
 """
 
 
+CRON_FOREMAN_SAMPLE2 = """
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+testinvaludline
+
+# Clean up the session entries in the database
+15 23 * * *     foreman    /usr/sbin/foreman-rake db:sessions:clear 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+@daily foreman /usr/sbin/foreman-rake reports:expire 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+@reboot foreman /usr/sbin/foreman-rake reports:clean
+"""
+
+CRON_FOREMAN_SAMPLE3 = """
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+RAILS_ENV=production
+FOREMAN_HOME=/usr/share/foreman
+testinvaludline
+
+# Clean up the session entries in the database
+15 23 * * *     foreman    /usr/sbin/foreman-rake db:sessions:clear 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+
+@tenyearly foreman /usr/sbin/foreman-rake reports:expire 2>&1 | gawk '{ print strftime("[\%Y-\%m-\%d \%H:\%M:\%S]"), $0 }' >>/var/log/foreman/cron.log
+"""
+
+
 def test_doc():
-    CronForeman.collect('test_reports_expire', lambda n: n if "foreman-rake reports:expire" in n else "")
-    CronForeman.any('test_reports_expire_2', lambda n: n if "foreman-rake reports:expire" in n else "")
     env = {
-        'foreman_cron': CronForeman(context_wrap(CRON_FOREMAN_SAMPLE1))
+        'foreman_jobs': CronForeman(context_wrap(CRON_FOREMAN_SAMPLE1))
     }
     failed, total = doctest.testmod(cron_jobs, globs=env)
     assert failed == 0
 
 
 def test_cron_foreman():
-    CronForeman.collect('test_audit_expire', lambda n: n if "foreman-rake audits:expire" in n else "")
-    cron_foreman = CronForeman(context_wrap(CRON_FOREMAN_SAMPLE1))
-    assert len(cron_foreman.test_audit_expire) == 1
-    assert '/var/log/foreman/audit.log' in cron_foreman.test_audit_expire[0]
+    cron_foreman = CronForeman(context_wrap(CRON_FOREMAN_SAMPLE2))
+    assert len(cron_foreman) == 3
+    assert len(cron_foreman.invalid_lines) == 1
+    assert len(cron_foreman.environment) == 2
+
+    assert 'SHELL' in cron_foreman.environment
+    assert cron_foreman.environment.get('SHELL') == '/bin/sh'
+
+    assert 'testinvaludline' in cron_foreman.invalid_lines[0]
+
+    assert 'reports:expire' in cron_foreman[1].get('command')
+    assert cron_foreman[1]['minute'] == '0'
+    assert cron_foreman[1]['hour'] == '0'
+    assert len(cron_foreman.invalid_lines) == 1
+
+    assert 'time' in cron_foreman[2]
+    assert '@reboot' == cron_foreman[2].get('time')
+
+    foreman_rake_lines = cron_foreman.search('foreman-rake')
+    assert len(foreman_rake_lines) == 3
+    assert 'foreman-rake reports:expire' in foreman_rake_lines[1].get('command')
+    assert foreman_rake_lines[2].get('command') == '/usr/sbin/foreman-rake reports:clean'
+
+
+def test_cron_foreman_except():
+    with pytest.raises(ParseException):
+        CronForeman(context_wrap(CRON_FOREMAN_SAMPLE3))
