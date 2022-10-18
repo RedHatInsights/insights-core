1880a9c21b6c54c018fd0237b20ae6aeffd60d29
Mohit Kumar <87797511+mohitkumarrh@users.noreply.github.com>
2022-10-18 09:52:24 +0530

    [New-parser] parser_mpirun_version (#3542)
    * [New-parser] parser_mpirun_version
    
    Signed-off-by: Mohit Kumar <mohikuma@REDHAT.COM>
    
    * Parser for mpirun_version, Renaming parser name and remove mapping
    
    Signed-off-by: Mohit Kumar <mohikuma@REDHAT.COM>
    
    Signed-off-by: Mohit Kumar <mohikuma@REDHAT.COM>

---
+++ insights/parsers/mpirun.py
@@ -0,0 +1,36 @@
+"""
+mpirun Version - Command
+========================
+
+The parser for ``mpirun --version`` is included in this module.
+
+"""
+from insights import parser, CommandParser
+from insights.parsers import SkipException
+from insights.specs import Specs
+
+
+@parser(Specs.mpirun_version)
+class MPIrunVersion(CommandParser):
+    """
+    Class for parsing the output of the ``/usr/local/bin/mpirun --version`` command.
+
+    Sample output::
+
+        Intel(R) MPI Library for Linux* OS, Version 2019 Update 12 Build 202010429 (id: e380127cb)
+        Copyright 2003-2021, Intel Corporation.
+
+    Examples:
+        >>> mpirun_ver.year
+        '2019'
+        >>> mpirun_ver.version
+        'Version 2019 Update 08 Build 202010429 (id: e380127cb)'
+    """
+    def parse_content(self, content):
+        if not content:
+            raise SkipException("Empty content.")
+        if len(content) != 2:
+            raise SkipException("Content not parsable.")
+
+        self.year = content[0].split('Version ')[1].split(" ")[0]
+        self.version = content[0].split(', ')[-1]
