f499dec89d61badae1c8d316326531ea96f3014f
huali027 <44796653+huali027@users.noreply.github.com>
2022-10-20 13:08:36 +0800

    feat: New spec "semanage login -l" and parser (#3548)
    * feat: New spec "semanage login -l" and parser
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Return the users count with datasource
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Make selinux user filterable and return json format
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Fix typo
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    * Append "pass" to the "LinuxUserCountMapSelinuxUser" parser
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -27,7 +27,7 @@ from insights.specs.datasources import (
     aws, awx_manage, cloud_init, candlepin_broker, corosync as corosync_ds,
     dir_list, ethernet, httpd, ipcs, kernel_module_list, lpstat, md5chk,
     package_provides, ps as ps_datasource, sap, satellite_missed_queues,
-    ssl_certificate, sys_fs_cgroup_memory_tasks_number, system_user_dirs, user_group, yum_updates, luks_devices)
+    semanage, ssl_certificate, sys_fs_cgroup_memory_tasks_number, system_user_dirs, user_group, yum_updates, luks_devices)
 from insights.specs.datasources.sap import sap_hana_sid, sap_hana_sid_SID_nr
 from insights.specs.datasources.pcp import pcp_enabled, pmlog_summary_args
 from insights.specs.datasources.container import running_rhel_containers
@@ -645,6 +645,7 @@ class DefaultSpecs(Specs):
     up2date = simple_file("/etc/sysconfig/rhn/up2date")
     up2date_log = simple_file("/var/log/up2date")
     uptime = simple_command("/usr/bin/uptime")
+    users_count_map_selinux_user = semanage.users_count_map_selinux_user
     usr_journald_conf_d = glob_file(r"usr/lib/systemd/journald.conf.d/*.conf")  # note that etc_journald.conf.d also exists
     vdo_status = simple_command("/usr/bin/vdo status")
     version_info = simple_file("/version_info")
