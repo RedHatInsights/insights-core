2e2b28f5a94d94e527248c602573d48048f083df
remi <remijouannet@gmail.com>
2022-10-20 18:04:17 +0200

    Signed-off-by: remi <remijouannet@gmail.com>
    enhance and fix for softnet_data parser
    * the offset for cpu_collision, received_rps, flow_limit_count was not
    correct
    *
    https://github.com/torvalds/linux/commit/7d58e6555870d01d85b181ba2a16b217a1ea8bdd
    introduce two new columns, softnet_backlog_len and index
    * the code example in the note was wrong, is_packet_drops is a proprety
      not a function
    * additional documentation and links about softnet_data structure and
      history
    * additional unit tests

--- insights/parsers/softnet_stat.py
+++ insights/parsers/softnet_stat.py
@@ -8,42 +8,87 @@ number of packet drops packet_drops (second column), time squeeze i.e net_rx_act
 performed time_squeeze(third column), cpu collision i.e collision occur while obtaining
 device lock while transmitting cpu_collision packets (eighth column), received_rps
 number of times cpu woken up received_rps (ninth column), number of times reached flow
-limit count flow_limit_count (tenth column).
+limit count flow_limit_count (tenth column), backlog status (eleventh column),
+core id (twelfth column).
 
 The typical contents of ``cat /proc/net/softnet_stat`` file is as below::
 
-    00008e78 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    000040ee 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    0001608c 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    0000372f 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+    00358fe3 00006283 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000855fc 00000000 00000000 00000000
+    00953d1a 00000446 00000000 00000000 00000000 00000000 00000000 00000000 00000000 008eeb9a 00000000 00000000 00000001
+    02600138 00004bc7 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02328493 00000000 00000000 00000002
+    02883c47 00007e2e 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02280d49 00000000 00000000 00000003
+    01a35c9d 0002db94 00000001 00000000 00000000 00000000 00000000 00000000 00000000 008ee93a 00000000 00000000 00000004
 
     Column-01: packet_process: Packet processed by each CPU.
     Column-02: packet_drop: Packets dropped.
     Column-03: time_squeeze: net_rx_action.
-    Column-08: cpu_collision: collision occur while obtaining device lock while transmitting.
-    Column-09: received_rps: number of times cpu woken up received_rps.
-    Column-10: flow_limit_count: number of times reached flow limit count.
+    Column-09: cpu_collision: collision occur while obtaining device lock while transmitting.
+    Column-10: received_rps: number of times cpu woken up received_rps.
+    Column-11: flow_limit_count: number of times reached flow limit count.
+    Column-12: softnet_backlog_len: Backlog status
+    Column-13: index: core id owning this softnet_data
+
+For the proc file format details,
+See:
+* Linux 2.6.23 https://elixir.bootlin.com/linux/v2.6.23/source/net/core/dev.c#L2343
+* * example : "6dcad223 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000"
+* Linux 4.17 https://elixir.bootlin.com/linux/v4.17/source/net/core/net-procfs.c#L162
+* * example : "00022be3 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000"
+* Linux 5.10 https://elixir.bootlin.com/linux/v5.10/source/net/core/net-procfs.c#L172
+* * example : "00358fe3 00006283 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000855fc 00000000 00000000 00000000"
+* https://elixir.bootlin.com/linux/v4.17/source/include/linux/netdevice.h#L2810
+* https://elixir.bootlin.com/linux/v5.10/source/include/linux/netdevice.h#L3181
+
+static int softnet_seq_show(struct seq_file *seq, void *v)
+{
+	struct softnet_data *sd = v;
+	unsigned int flow_limit_count = 0;
+
+#ifdef CONFIG_NET_FLOW_LIMIT
+	struct sd_flow_limit *fl;
+
+	rcu_read_lock();
+	fl = rcu_dereference(sd->flow_limit);
+	if (fl)
+		flow_limit_count = fl->count;
+	rcu_read_unlock();
+#endif
+
+	/* the index is the CPU id owing this sd. Since offline CPUs are not
+	 * displayed, it would be othrwise not trivial for the user-space
+	 * mapping the data a specific CPU
+	 */
+	seq_printf(seq,
+		   "%08x %08x %08x %08x %08x %08x %08x %08x %08x %08x %08x %08x %08x\n",
+		   sd->processed, sd->dropped, sd->time_squeeze, 0,
+		   0, 0, 0, 0, /* was fastroute */
+		   0,	/* was cpu_collision */
+		   sd->received_rps, flow_limit_count,
+		   softnet_backlog_len(sd), (int)seq->index);
+	return 0;
+}
 
 .. Note::
             There is minimal documentation about these fields in the file, columns are not labeled and
-            could change between the kernel releases. This format is compatible for RHEL-6
-            and RHEL-7 releases. Also it is unlikely that the positions of those values will
+            could change between the kernel releases. This format is compatible for RHEL-6,
+            RHEL-7, RHEL-8 and RHEL-9 releases. Also it is unlikely that the positions of those values will
             change in short term.
 
 Examples:
     >>> SOFTNET_STATS = '''
-    ... 00008e78 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    ... 000040ee 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    ... 0001608c 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    ... 0000372f 00000001 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    ...'''.strip()
+    ... 00358fe3 00006283 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000855fc 00000000 00000000 00000000
+    ... 00953d1a 00000446 00000000 00000000 00000000 00000000 00000000 00000000 00000000 008eeb9a 00000000 00000000 00000001
+    ... 02600138 00004bc7 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02328493 00000000 00000000 00000002
+    ... 02883c47 00007e2e 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02280d49 00000000 00000000 00000003
+    ... 01a35c9d 0002db94 00000001 00000000 00000000 00000000 00000000 00000000 00000000 008ee93a 00000000 00000000 00000004
+    '''.strip()
     >>> softnet_obj = SoftNetStats(context_wrap(SOFTNET_STATS))
-    >>> softnet_obj.is_packet_drops()
+    >>> softnet_obj.is_packet_drops
     True
     >>> softnet_obj.cpu_instances
-    4
+    5
     >>> softnet_obj.per_cpu_nstat('packet_drops')
-    [0, 0, 0, 1]
+    [25219, 1094, 19399, 32302, 187284]
 """
 
 from .. import Parser, parser
@@ -69,9 +114,11 @@ class SoftNetStats(Parser):
             self.cpu_nstats.append({'packet_process': stats[0],
                             'packet_drops': stats[1],
                             'time_squeeze': stats[2],
-                            'cpu_collision': stats[7],
-                            'received_rps': stats[8],
-                            'flow_limit_count': stats[9]})
+                            'cpu_collision': stats[8],
+                            'received_rps': stats[9] if len(stats) >= 11 else 0,
+                            'flow_limit_count': stats[10] if len(stats) >= 11 else 0,
+                            'softnet_backlog_len': stats[11] if len(stats) >= 13 else 0,
+                            'index': stats[12] if len(stats) >= 13 else 0})
         self.cpu_instances = len(self.cpu_nstats)
 
     def per_cpu_nstat(self, key):
--- insights/tests/parsers/test_softnet_stat.py
+++ insights/tests/parsers/test_softnet_stat.py
@@ -9,9 +9,27 @@ SOFTNET_STAT = """
 """
 
 SOFTNET_STAT_2 = """
-00008e78 00000000 000000011 00000000 00000000 00000000 00000000 00000000 00000020 00000010
-000040ee 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000000010 00000001
+00008e78 00000000 00000011 00000000 00000000 00000000 00000000 00000000 00000020 00000010
+000040ee 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000010 00000001
 """
+
+
+SOFTNET_STAT_3 = """
+00358fe3 00006283 00000000 00000000 00000000 00000000 00000000 00000000 00000000 000855fc 00000000 00000000 00000000
+00953d1a 00000446 00000000 00000000 00000000 00000000 00000000 00000000 00000000 008eeb9a 00000000 00000000 00000001
+02600138 00004bc7 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02328493 00000000 00000000 00000002
+02883c47 00007e2e 00000000 00000000 00000000 00000000 00000000 00000000 00000000 02280d49 00000000 00000000 00000003
+01a35c9d 0002db94 00000001 00000000 00000000 00000000 00000000 00000000 00000000 008ee93a 00000000 00000000 00000000
+"""
+
+SOFTNET_STAT_4 = """
+6dcad223 00000000 00000001 00000000 00000000 00000000 00000000 00000000 00000000
+"""
+
+SOFTNET_STAT_5 = """
+00022be3 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000003 00000002 00000001
+"""
+
 SOFTNET_STAT_NO = """
 """
 
@@ -43,8 +61,42 @@ def test_softnet_stat():
     assert stats.cpu_instances == 2
     assert stats.per_cpu_nstat('packet_drops') == [0, 0]
     assert stats.per_cpu_nstat('time_squeeze') == [17, 0]
-    assert stats.per_cpu_nstat('cpu_collision') == [0, 0]
-    assert stats.per_cpu_nstat('received_rps') == [32, 16]
-    assert stats.per_cpu_nstat('flow_limit_count') == [16, 1]
+    assert stats.per_cpu_nstat('cpu_collision') == [32, 16]
+    assert stats.per_cpu_nstat('received_rps') == [0, 0]
+    assert stats.per_cpu_nstat('flow_limit_count') == [0, 0]
+    assert stats.per_cpu_nstat('softnet_backlog_len') == [0 ,0]
+    assert stats.per_cpu_nstat('packet_dr') == []
+    assert not stats.is_packet_drops
+
+    stats = SoftNetStats(context_wrap(SOFTNET_STAT_3))
+    assert stats is not None
+    assert stats.cpu_instances == 5
+    assert stats.per_cpu_nstat('time_squeeze') == [0, 0, 0, 0, 1]
+    assert stats.per_cpu_nstat('packet_drops') == [25219, 1094, 19399, 32302, 187284]
+    assert stats.per_cpu_nstat('cpu_collision') == [0, 0, 0, 0, 0]
+    assert stats.per_cpu_nstat('received_rps') == [546300, 9366426, 36865171, 36179273, 9365818]
+    assert stats.per_cpu_nstat('flow_limit_count') == [0, 0, 0 ,0 ,0]
+    assert stats.per_cpu_nstat('softnet_backlog_len') == [0, 0, 0 ,0 ,0]
+    assert stats.per_cpu_nstat('packet_dr') == []
+    assert stats.is_packet_drops
+
+    stats = SoftNetStats(context_wrap(SOFTNET_STAT_4))
+    assert stats is not None
+    assert stats.cpu_instances == 1
+    assert stats.per_cpu_nstat('packet_drops') == [0]
+    assert stats.per_cpu_nstat('time_squeeze') == [1]
+    assert stats.per_cpu_nstat('cpu_collision') == [0]
+    assert stats.per_cpu_nstat('received_rps') == [0]
+    assert stats.per_cpu_nstat('packet_dr') == []
+    assert not stats.is_packet_drops
+
+    stats = SoftNetStats(context_wrap(SOFTNET_STAT_5))
+    assert stats is not None
+    assert stats.cpu_instances == 1
+    assert stats.per_cpu_nstat('packet_drops') == [0]
+    assert stats.per_cpu_nstat('time_squeeze') == [0]
+    assert stats.per_cpu_nstat('cpu_collision') == [3]
+    assert stats.per_cpu_nstat('received_rps') == [2]
+    assert stats.per_cpu_nstat('flow_limit_count') == [1]
     assert stats.per_cpu_nstat('packet_dr') == []
     assert not stats.is_packet_drops
