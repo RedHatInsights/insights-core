46d6a5aa7d33a583aff875fd418905b8f3806908
Xiangce Liu <xiangceliu@redhat.com>
2022-10-28 16:49:28 +0800

    fix: remove duplicated containers from running_rhel_containers (#3571)
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 1ad0bd6b6a50c73cf1df1acd2142078a87a69928)

--- insights/specs/datasources/container/__init__.py
+++ insights/specs/datasources/container/__init__.py
@@ -14,12 +14,21 @@ def running_rhel_containers(broker):
     """
     Returns a list of tuple of (image, <podman|docker>, container_id) of the running
     containers.
+
+    From RHEL 8, the "docker" command is from the package "podman-docker". In
+    general when using command "docker" to access a container, the following
+    message will be reported on the top::
+
+        Emulate Docker CLI using podman. Create /etc/containers/nodocker to quiet msg.
+
+    But sometimes, this line won't be outputted as expected, in this case, it's
+    necessary to remove the duplicated containers from the output of "docker".
     """
     def _is_rhel_image(ctx, c_info):
         """Only collect the containers based from RHEL images"""
         try:
             ret = ctx.shell_out("/usr/bin/%s exec %s cat /etc/redhat-release" % c_info, timeout=DEFAULT_SHELL_TIMEOUT)
-            if ret and "red hat enterprise linux" in ret[0].lower():
+            if ret and len(ret) == 1 and "red hat enterprise linux" in ret[0].lower():
                 return True
         except Exception:
             # return False when there is no such file "/etc/redhat-releas"
@@ -27,16 +36,28 @@ def running_rhel_containers(broker):
         return False
 
     cs = []
+    podman_container = set()
     if (PodmanListContainers in broker):
         podman_c = broker[PodmanListContainers]
         for name in podman_c.running_containers:
-            c_info = ('podman', podman_c.containers[name]['CONTAINER ID'][:12])
-            cs.append((podman_c.containers[name]['IMAGE'],) + c_info) if _is_rhel_image(broker[HostContext], c_info) else None
+            container_id = podman_c.containers[name]['CONTAINER ID']
+            podman_container.add(container_id)
+            c_info = ('podman', container_id[:12])
+            if not _is_rhel_image(broker[HostContext], c_info):
+                # skip containers from non-rhel image
+                continue
+            cs.append((podman_c.containers[name]['IMAGE'],) + c_info)
     if (DockerListContainers in broker):
         docker_c = broker[DockerListContainers]
         for name in docker_c.running_containers:
-            c_info = ('docker', docker_c.containers[name]['CONTAINER ID'][:12])
-            cs.append((docker_c.containers[name]['IMAGE'],) + c_info) if _is_rhel_image(broker[HostContext], c_info) else None
+            container_id = docker_c.containers[name]['CONTAINER ID']
+            c_info = ('docker', container_id[:12])
+            if (container_id in podman_container or
+                    not _is_rhel_image(broker[HostContext], c_info)):
+                # skip containers from non-rhel image and
+                # skip duplicated containers managed by "podman"
+                continue
+            cs.append((docker_c.containers[name]['IMAGE'],) + c_info)
     if cs:
         # Return list of tuple:
         # - (image, <podman|docker>, container_id)
