80169290917d88e023ebfca4bded28fa65aaec41
jiazhang <jiazhang@redhat.com>
2022-11-01 10:50:58 +0800

    Update docstring
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/containers_inspect.py
+++ insights/parsers/containers_inspect.py
@@ -1,6 +1,6 @@
 """
-Containers inspect
-==================
+ContainersInspect - commands ``docker|podman inspect``
+======================================================
 
 This parser reads the output of commands: "/usr/bin/docker|podman inspect <containers ID>"
 which are used to show the metadata information of containers.
