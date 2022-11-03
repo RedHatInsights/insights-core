92b65e940770e98277cbec7b96b581417d5cfedf
Link Dupont <subpop@users.noreply.github.com>
2022-11-03 12:42:06 -0400

    feat(client): add --manifest argument (#3547)
    Add a top-level --manifest argument. This argument expects a path to an
    insights collection manifest file. If the argument is not None, the
    manifest is used instead of the default manifest to collect data.
    
    Signed-off-by: Link Dupont <link@redhat.com>

--- insights/client/config.py
+++ insights/client/config.py
@@ -127,6 +127,14 @@ DEFAULT_OPTS = {
         'group': 'actions',
         'dest': 'app'
     },
+    'manifest': {
+        'default': None,
+        'opt': ['--manifest'],
+        'help': 'Collect using the provided manifest',
+        'action': 'store',
+        'group': 'actions',
+        'dest': 'manifest'
+    },
     'compliance': {
         'default': False,
         'opt': ['--compliance'],
@@ -468,6 +476,7 @@ class InsightsConfig(object):
     '''
     Insights client configuration
     '''
+
     def __init__(self, *args, **kwargs):
         # this is only used to print configuration errors upon initial load
         self._print_errors = False
