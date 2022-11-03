a3a052a49a3b102f50b73cc36aded69b70ac5c22
Xiangce Liu <xiangceliu@redhat.com>
2022-11-03 18:31:04 +0800

    test tracebacks in meta_data file
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/test_add_exception.py
+++ insights/tests/test_add_exception.py
@@ -1,6 +1,11 @@
-from insights.core.dr import run
+import os
+import json
+from tempfile import mkdtemp
+from insights.core import dr
 from insights.core.plugins import datasource, rule, make_info, ContentException
 from insights.core.spec_factory import RegistryPoint, SpecSet
+from insights.core.serde import Hydration
+from insights.util import fs
 
 
 class Specs(SpecSet):
@@ -19,7 +24,7 @@ def report(dt):
 
 
 def test_add_exception_tracebacks():
-    broker = run(report)
+    broker = dr.run(report)
     assert report in broker
     assert TestSpecs.the_data in broker.exceptions
     all_exs = broker.exceptions[TestSpecs.the_data]
@@ -29,3 +34,25 @@ def test_add_exception_tracebacks():
     assert type(tb) is str
     assert "Traceback" in tb
     assert "Fake Datasource" in tb
+
+
+def test_add_exception_tracebacks_in_meta_data_file():
+    broker = dr.run(report)
+    tmp_path = mkdtemp()
+    spec_the_data = TestSpecs.the_data
+    try:
+        h = Hydration(tmp_path)
+        h.dehydrate(spec_the_data, broker)
+        fn = ".".join([dr.get_name(spec_the_data), h.ser_name])
+        meta_data_json = os.path.join(h.meta_data, fn)
+        assert os.path.exists(meta_data_json)
+        with open(meta_data_json, 'r') as fp:
+            ret = json.load(fp)
+            exc = next(iter(broker.tracebacks))
+            tb = broker.tracebacks[exc]
+            assert "errors" in ret
+            assert ret["errors"][0] == tb
+            assert "Traceback" in tb
+            assert "Fake Datasource" in tb
+    finally:
+        fs.remove(tmp_path)
