9cbfb8f421fa3dc5f152a56e72bcbc6335d1601e
Xiangce Liu <xiangceliu@redhat.com>
2022-11-08 08:46:27 +0800

    fix: values of broker.tracebacks should be string (#3579)
    * fix: values of broker.tracebacks should be string
    
    - fix: #3578
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * one more test item
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * test tracebacks in meta_data file
    * move the test to test_serde
    * mini update
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 682b6273d4af9652bd8ab9908295f46596a97a5c)

---
+++ insights/tests/test_add_exception.py
@@ -0,0 +1,31 @@
+from insights.core import dr
+from insights.core.plugins import datasource, rule, make_info, ContentException
+from insights.core.spec_factory import RegistryPoint, SpecSet
+
+
+class Specs(SpecSet):
+    the_data = RegistryPoint()
+
+
+class TestSpecs(Specs):
+    @datasource()
+    def the_data(broker):
+        raise ContentException('Fake Datasource')
+
+
+@rule(Specs.the_data)
+def report(dt):
+    return make_info('INFO_1')
+
+
+def test_broker_add_exception():
+    broker = dr.run(report)
+    assert report in broker
+    assert TestSpecs.the_data in broker.exceptions
+    spec_exs = broker.exceptions[TestSpecs.the_data]
+    exs = [ex for ex in spec_exs if isinstance(ex, ContentException) and str(ex) == "Fake Datasource"]
+    assert len(exs) == 1
+    tb = broker.tracebacks[exs[0]]
+    assert type(tb) is str
+    assert "Traceback" in tb
+    assert "Fake Datasource" in tb
