ce50be2acf6943f45872bef9fae2c6a2b52546ba
xintli <986222045@qq.com>
2022-11-09 17:59:23 +0800

    Feat: New Parser for 'dotnet --version' Command for Containers (#3581)
    * Feat: New Parser for 'dotnet --version' Command
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update spec
    * Fix Parser Name
    * Update Doc string
    * Update doc title
    * Add doctest
    
    Signed-off-by: Xinting Li <xintli@redhat.com>

--- insights/tests/parsers/test_dotnet.py
+++ insights/tests/parsers/test_dotnet.py
@@ -3,7 +3,7 @@ import pytest
 from insights.parsers import dotnet
 from insights.core.plugins import ContentException
 from insights.parsers import SkipException, ParseException
-from insights.parsers.dotnet import DotNetVersion
+from insights.parsers.dotnet import DotNetVersion, ContainerDotNetVersion
 from insights.tests import context_wrap
 
 dotnet_version_1 = "3.1.108"
@@ -45,9 +45,47 @@ def test_dotnet_version_ab():
         assert ret is None
 
 
+def test_container_dotnet_version():
+    ret = ContainerDotNetVersion(
+        context_wrap(
+            dotnet_version_1,
+            container_id='cc2883a1a369',
+            image='quay.io/rhel8',
+            engine='podman'
+        )
+    )
+    assert ret.major == 3
+    assert ret.minor == 1
+    assert ret.raw == dotnet_version_1
+    assert ret.image == "quay.io/rhel8"
+    assert ret.engine == "podman"
+    assert ret.container_id == "cc2883a1a369"
+
+
+def test_container_dotnet_version_error():
+    with pytest.raises(ContentException):
+        ret = ContainerDotNetVersion(
+            context_wrap(
+                dotnet_version_3,
+                container_id='cc2883a1a369',
+                image='quay.io/rhel8',
+                engine='podman'
+            )
+        )
+        assert ret is None
+
+
 def test_doc_examples():
     env = {
-        'dotnet_ver': DotNetVersion(context_wrap(dotnet_version_1))
+        'dotnet_ver': DotNetVersion(context_wrap(dotnet_version_1)),
+        'con_dotnet_ver': ContainerDotNetVersion(
+            context_wrap(
+                dotnet_version_1,
+                container_id='cc2883a1a369',
+                image='quay.io/rhel8',
+                engine='podman'
+            )
+        )
     }
     failed, total = doctest.testmod(dotnet, globs=env)
     assert failed == 0
