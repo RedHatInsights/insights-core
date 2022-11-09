af3016f8bf12608a41454b4ff28b4e41700ba2af
xintli <986222045@qq.com>
2022-11-09 17:59:23 +0800

    Feat: New Parser for 'dotnet --version' Command for Containers (#3581)
    * Feat: New Parser for 'dotnet --version' Command
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    
    * Update spec
    * Fix Parser Name
    * Update Doc string
    * Update doc title
    * Add doctest
    
    Signed-off-by: Xinting Li <xintli@redhat.com>
    (cherry picked from commit 978f80c33e7f0a3cda87858e3c6ab2fb90b9a543)

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -791,6 +791,7 @@ class Specs(SpecSet):
     zipl_conf = RegistryPoint()
 
     # container_specs
+    container_dotnet_version = RegistryPoint(multi_output=True)
     container_redhat_release = RegistryPoint(multi_output=True)
     container_nginx_conf = RegistryPoint(multi_output=True)
     container_installed_rpms = RegistryPoint(multi_output=True)
