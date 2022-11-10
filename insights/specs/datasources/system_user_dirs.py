a7f76dbfcd971c6f788455442cecd410da2b09cf
Jitka Obselkova <41325380+jobselko@users.noreply.github.com>
2022-11-10 01:47:33 +0100

    Update system_user_dirs datasource (#3586)
    * Optimize the code
    
    Signed-off-by: Jitka Obselkova <jobselko@redhat.com>
    
    * Update code logic
    
    Signed-off-by: Jitka Obselkova <jobselko@redhat.com>
    
    * Update code as recommended
    
    Signed-off-by: Jitka Obselkova <jobselko@redhat.com>
    (cherry picked from commit 90a9a41a3f581a84e0cd6f672e14565ce2b93e92)

--- insights/specs/datasources/system_user_dirs.py
+++ insights/specs/datasources/system_user_dirs.py
@@ -15,10 +15,10 @@ from insights.specs import Specs
 
 class LocalSpecs(Specs):
     """
-    Local spec used only by the system_user_dirs datasource
+    Local spec used only by the system_user_dirs datasource.
     """
     rpm_args = simple_command(
-        'rpm -qa --qf="[%{=NAME}; %{FILEMODES:perms}; %{FILEUSERNAME}; %{FILEGROUPNAME}\n]"',
+        'rpm -qa --nosignature --qf="[%{=NAME}; %{FILENAMES}; %{FILEMODES:perms}; %{FILEUSERNAME}; %{FILEGROUPNAME}\n]"',
         signum=signal.SIGTERM
     )
 
@@ -28,7 +28,7 @@ def get_shells():
     Returns all full pathnames of valid login shells without nologins.
     """
     with open("/etc/shells") as file:
-        return [line.strip() for line in file if "nologin" not in line]
+        return set(line.strip() for line in file if "nologin" not in line)
 
 
 def get_users():
@@ -36,7 +36,7 @@ def get_users():
     Returns all users with shell specified in get_shells() except for root.
     """
     shells = get_shells()
-    users = []
+    users = set()
 
     for user in pwd.getpwall():
         name = user[0]
@@ -44,7 +44,7 @@ def get_users():
 
         if name == "root" or (shell not in shells):
             continue
-        users.append(name)
+        users.add(name)
     return users
 
 
@@ -53,17 +53,19 @@ def get_groups(users):
     Returns all groups for users specified in get_users().
     Every user has at least one group with its name.
     """
-    groups = []
+    groups = set()
 
     for group in grp.getgrall():
         group_name = group[0]
         user_list = group[3]
 
         if group_name in users:
-            groups.append(group_name)  # group for an user of interest
-        for user in user_list:
-            if user.strip() in users:
-                groups.append(group_name)  # add groups containing a user
+            groups.add(group_name)
+        else:
+            for user in user_list:
+                if user.strip() in users:
+                    groups.add(group_name)
+                    break
     return groups
 
 
@@ -72,9 +74,11 @@ def system_user_dirs(broker):
     r"""
     Custom datasource for CVE-2021-35937, CVE-2021-35938, and CVE-2021-35939.
 
-    It collects package names from the ``rpm -qa --qf="[%{=NAME}; %{FILEMODES:perms};
-    %{FILEUSERNAME}; %{FILEGROUPNAME}\n]" command``, if the package has
-    at least one directory which is writable by a specific user/group or the others.
+    It collects package names from the ``rpm -qa --nosignature --qf="[%{=NAME}; %{FILENAMES};
+    %{FILEMODES:perms}; %{FILEUSERNAME}; %{FILEGROUPNAME}\n]" command``.
+
+    The output is a sorted list of all packages, which have at least one directory with files
+    inside, and this directory is writable by a specific user/group or the others.
 
     Raises:
         SkipComponent: Raised if no data is available
@@ -82,23 +86,32 @@ def system_user_dirs(broker):
     Returns:
         List[str]: Sorted list of package names
     """
-    users = get_users()
-    groups = get_groups(users)
-    packages = set()
     content = broker[LocalSpecs.rpm_args].content
 
     if not content or "command not found" in content[0]:
         raise SkipComponent
 
+    users = get_users()
+    groups = get_groups(users)
+
+    dir_package = {}
+    dirs = set()
+
     for line in content:
-        name, perms, user, group = line.split("; ")
+        pkg_name, path_name, perms, user, group = line.split("; ")
         if perms[0] == "d":
             user_w = user in users and perms[2] == "w"
             group_w = group in groups and perms[5] == "w"
             others_w = perms[8] == "w"
-
             if user_w or group_w or others_w:
-                packages.add(name)
+                # Stores a writeable directory with its package
+                dir_package[path_name] = pkg_name
+        else:
+            # Stores a file directory for all files
+            dirs.add(path_name.rsplit('/', 1)[0])
+
+    # Stores a package if its associated file is in a writable directory
+    packages = set(dir_package[dir] for dir in dirs if dir in dir_package)
 
     if packages:
         return DatasourceProvider(
