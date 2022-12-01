5c6ffd3a0ba29fbc1d391b054e10f38247e81b8b
Mohit Kumar <mohikuma@REDHAT.COM>
2022-12-02 01:31:04 +0530

    [Enhancement] enhancing parser accoring to the requirement
    Signed-off-by: Mohit Kumar <mohikuma@REDHAT.COM>

--- insights/parsers/ls_var_lib_pcp.py
+++ insights/parsers/ls_var_lib_pcp.py
@@ -1,8 +1,8 @@
 """
-LsVarLibPcp - command ``ls -la /var/lib/pcp``
-=============================================
+LsVarLibPcp - command ``ls -la /var/lib/pcp/pmdas/openmetrics/config.d``
+========================================================================
 
-The ``ls -la /var/lib/pcp`` command provides information for the listing of the ``/var/lib/pcp`` directory.
+The ``ls -la /var/lib/pcp/pmdas/openmetrics/config.d`` command provides information for the listing of the ``/var/lib/pcp/pmdas/openmetrics/config.d`` directory.
 
 The parsers class in this module uses base parser class
 ``CommandParser`` & ``FileListing`` to list files & directories.
@@ -12,18 +12,14 @@ Sample output of this command is::
     total 16
     drwxr-xr-x.  4 root root  33 Dec 15 08:12 .
     drwxr-xr-x. 20 root root 278 Dec 15 08:12 ..
-    drwxr-xr-x.  2 root root   6 Oct  3 09:37 pmcd
-    drwxr-xr-x.  2 root root   6 Oct  3 09:37 pmie
-    drwxrwxr-x.  4 root root 128 Aug 22 17:55 pmdas
+    -rwxr-xr-x.  2 root root   6 Oct  3 09:37 grafana.url
 
 Examples:
 
-    >>> "pmdas" in ls_var_lib_pcp
+    >>> "grafana.url" in ls_var_lib_pcp
     False
-    >>> "/var/lib/pcp" in ls_var_lib_pcp
-    True
-    >>> ls_var_lib_pcp.dir_entry('/var/lib/pcp', 'pmdas')['type']
-    'd'
+    >>> "/var/lib/pcp/pmdas/openmetrics/config.d" in ls_var_lib_pcp
+    False
 """
 
 
@@ -33,6 +29,6 @@ from insights.core import FileListing
 
 
 @parser(Specs.ls_var_lib_pcp)
-class LsVarLibPcp(CommandParser, FileListing):
-    """Parses output of ``ls -la /var/lib/pcp`` command."""
+class LsVarLibPcpOpenmetricsConfigd(CommandParser, FileListing):
+    """Parses output of ``ls -la /var/lib/pcp/pmdas/openmetrics/config.d`` command."""
     pass
