0e263a5ec62ba30da3fc4cf368530c8692dc2c94
Alba Hita <93577878+ahitacat@users.noreply.github.com>
2022-12-05 18:49:53 +0100

    Feat register no machine-id (#3554)
    * Registration mechanism looks for machine-id file and if is not registered insights-client fails executing
    * Test to check this new logic
    
    Signed-off-by: ahitacat <ahitacat@redhat.com>

--- insights/tests/client/phase/test_post_update.py
+++ insights/tests/client/phase/test_post_update.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
 
+from contextlib import contextmanager
+import os
+from shutil import rmtree
 from insights.client.phase.v1 import post_update
 from mock.mock import patch, MagicMock
 from pytest import raises
@@ -29,9 +32,10 @@ def patch_insights_config(old_function):
 # DRY this at some point... for the love of god
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_no_options_registered(insights_config, insights_client):
+def test_post_update_no_options_registered(insights_config, insights_client, _isfile):
     """
     Client run with no options.
         If registered, exit with 0 exit code (don't kill parent)
@@ -46,9 +50,10 @@ def test_post_update_no_options_registered(insights_config, insights_client):
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_no_options_unregistered(insights_config, insights_client):
+def test_post_update_no_options_unregistered(insights_config, insights_client, _isfile):
     """
     Client run with no options.
         If unregistered, exit with 101 exit code (kill parent)
@@ -57,15 +62,14 @@ def test_post_update_no_options_unregistered(insights_config, insights_client):
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 101
-    insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_no_options_err_reg_check(insights_config, insights_client):
+def test_post_update_no_options_err_reg_check(insights_config, insights_client, _isfile):
     """
     Client run with no options.
         If registration check fails, exit with 101 exit code
@@ -74,15 +78,15 @@ def test_post_update_no_options_err_reg_check(insights_config, insights_client):
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 101
-    insights_client.return_value.get_machine_id.assert_called_once()
     insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_check_status_registered(insights_config, insights_client):
+def test_post_update_check_status_registered(insights_config, insights_client, _isfile):
     """
     Just check status.
         If registered, exit with 100 exit code (kill parent)
@@ -92,15 +96,15 @@ def test_post_update_check_status_registered(insights_config, insights_client):
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 100
-    insights_client.return_value.get_machine_id.assert_called_once()
     insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_check_status_unregistered(insights_config, insights_client):
+def test_post_update_check_status_unregistered(insights_config, insights_client, _isfile):
     """
     Just check status.
         If unregistered, exit with 100 exit code (kill parent)
@@ -110,20 +114,18 @@ def test_post_update_check_status_unregistered(insights_config, insights_client)
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 100
-    insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_register_registered(insights_config, insights_client, get_scheduler):
+def test_post_update_register_registered(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client run with --register.
-        If registered, exit with 0 exit code (don't kill parent)
-        Also enable scheduling.
+        If registered, exit with 0 exit code
     """
     insights_config.return_value.load_all.return_value.register = True
     insights_client.return_value.get_registration_status = MagicMock(return_value=True)
@@ -137,6 +139,59 @@ def test_post_update_register_registered(insights_config, insights_client, get_s
     get_scheduler.return_value.schedule.assert_called_once()
 
 
+TEMP_TEST_REG_DIR = "/tmp/insights-client-registration"
+
+
+@contextmanager
+def _mock_no_register_files_machineid_present():
+    # mock a directory with the fresh install files
+    if not os.path.exists(TEMP_TEST_REG_DIR):
+        os.mkdir(TEMP_TEST_REG_DIR)
+    try:
+        machine_id_path = os.path.join(TEMP_TEST_REG_DIR, "machine-id")
+        with open(machine_id_path, "w") as machine_id_file:
+            machine_id_file.write("id")
+        yield
+    finally:
+        rmtree(TEMP_TEST_REG_DIR)
+
+
+@patch('insights.client.utilities.constants.machine_id_file',
+       TEMP_TEST_REG_DIR + '/machine-id')
+@patch("insights.client.phase.v1.get_scheduler")
+@patch("insights.client.phase.v1.InsightsClient")
+@patch_insights_config
+def test_post_update_register_machineid(insights_config, insights_client, get_scheduler):
+    """
+    Client run with --register.
+        If machine-id found, exit with 0 exit code (don't kill parent)
+        Also enable scheduling.
+    """
+    insights_config.return_value.load_all.return_value.register = True
+    insights_client.return_value.get_registration_status = MagicMock(return_value=False)
+    with _mock_no_register_files_machineid_present():
+        with raises(SystemExit) as exc_info:
+            post_update()
+    assert exc_info.value.code == 101
+    insights_client.return_value.get_registration_status.assert_called_once()
+    insights_client.return_value.clear_local_registration.assert_not_called()
+    insights_client.return_value.set_display_name.assert_not_called()
+    get_scheduler.return_value.schedule.assert_not_called()
+
+
+@contextmanager
+def _mock_no_machineid_present():
+    # mock a directory with the fresh install files
+    if not os.path.exists(TEMP_TEST_REG_DIR):
+        os.mkdir(TEMP_TEST_REG_DIR)
+    try:
+        yield
+    finally:
+        rmtree(TEMP_TEST_REG_DIR)
+
+
+@patch('insights.client.utilities.constants.machine_id_file',
+       TEMP_TEST_REG_DIR + '/machine-id')
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
@@ -148,20 +203,22 @@ def test_post_update_register_unregistered(insights_config, insights_client, get
     """
     insights_config.return_value.load_all.return_value.register = True
     insights_client.return_value.get_registration_status = MagicMock(return_value=False)
-    with raises(SystemExit) as exc_info:
-        post_update()
+    with _mock_no_machineid_present():
+        with raises(SystemExit) as exc_info:
+            post_update()
     assert exc_info.value.code == 0
     insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
+    insights_client.return_value.get_registration_status.assert_not_called()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.schedule.assert_called_once()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_unregister_registered(insights_config, insights_client, get_scheduler):
+def test_post_update_unregister_registered(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client run with --unregister.
         If registered, exit with 100 exit code
@@ -172,17 +229,17 @@ def test_post_update_unregister_registered(insights_config, insights_client, get
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 100
-    insights_client.return_value.get_machine_id.assert_called_once()
     insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.remove_scheduling.assert_called_once()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_unregister_unregistered(insights_config, insights_client, get_scheduler):
+def test_post_update_unregister_unregistered(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client run with --unregister.
         If unregistered, exit with 101 exit code
@@ -192,17 +249,16 @@ def test_post_update_unregister_unregistered(insights_config, insights_client, g
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 101
-    insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_not_called()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.remove_scheduling.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True, False])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_force_register_registered(insights_config, insights_client, get_scheduler):
+def test_post_update_force_register_registered(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client run with --force-reregister.
         If registered, exit with 0 exit code (don't kill parent)
@@ -221,10 +277,11 @@ def test_post_update_force_register_registered(insights_config, insights_client,
     get_scheduler.return_value.schedule.assert_called_once()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False, False])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_force_register_unregistered(insights_config, insights_client, get_scheduler):
+def test_post_update_force_register_unregistered(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client run with --force-reregister.
         If registered, exit with 0 exit code (don't kill parent)
@@ -237,16 +294,16 @@ def test_post_update_force_register_unregistered(insights_config, insights_clien
         post_update()
     assert exc_info.value.code == 0
     insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_called_once()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.schedule.assert_called_once()  # ASK
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_set_display_name_cli_no_register_unreg(insights_config, insights_client, get_scheduler):
+def test_post_update_set_display_name_cli_no_register_unreg(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client is unregistered, and run with --display-name but not --register
         Should exit with code 101 after registration check
@@ -257,16 +314,15 @@ def test_post_update_set_display_name_cli_no_register_unreg(insights_config, ins
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 101
-    insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.schedule.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_set_display_name_cli_no_register_reg(insights_config, insights_client, get_scheduler):
+def test_post_update_set_display_name_cli_no_register_reg(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client is registered, and run with --display-name but not --register
         Should exit with code 100 after setting display name
@@ -277,16 +333,16 @@ def test_post_update_set_display_name_cli_no_register_reg(insights_config, insig
     with raises(SystemExit) as exc_info:
         post_update()
     assert exc_info.value.code == 100
-    insights_client.return_value.get_machine_id.assert_called_once()
     insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.set_display_name.assert_called_once()
     get_scheduler.return_value.schedule.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[False, False])
 @patch("insights.client.phase.v1.get_scheduler")
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
-def test_post_update_set_display_name_cli_register(insights_config, insights_client, get_scheduler):
+def test_post_update_set_display_name_cli_register(insights_config, insights_client, get_scheduler, _isfile):
     """
     Client is and run with --display-name and --register
         Should set schedule and exit with code 0
@@ -299,7 +355,6 @@ def test_post_update_set_display_name_cli_register(insights_config, insights_cli
         post_update()
     assert exc_info.value.code == 0
     insights_client.return_value.get_machine_id.assert_called_once()
-    insights_client.return_value.get_registration_status.assert_called_once()
     insights_client.return_value.clear_local_registration.assert_called_once()
     insights_client.return_value.set_display_name.assert_not_called()
     get_scheduler.return_value.schedule.assert_called_once()
@@ -339,10 +394,11 @@ def test_post_update_no_upload(insights_config, insights_client):
     insights_client.return_value.set_display_name.assert_not_called()
 
 
+@patch("insights.client.phase.v1.isfile", side_effect=[True])
 @patch("insights.client.phase.v1.InsightsClient")
 @patch_insights_config
 # @patch("insights.client.phase.v1.InsightsClient")
-def test_exit_ok(insights_config, insights_client):
+def test_exit_ok(insights_config, insights_client, _isfile):
     """
     Support collection replaces the normal client run.
     """
