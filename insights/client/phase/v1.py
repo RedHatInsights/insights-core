1857a429eb81285358f5ed26c939da8090cb979f
Alba Hita <93577878+ahitacat@users.noreply.github.com>
2022-12-05 18:49:53 +0100

    Feat register no machine-id (#3554)
    * Registration mechanism looks for machine-id file and if is not registered insights-client fails executing
    * Test to check this new logic
    
    Signed-off-by: ahitacat <ahitacat@redhat.com>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import functools
+from os.path import isfile
 import json
 import logging
 import os
@@ -135,9 +136,7 @@ def update(client, config):
 
 @phase
 def post_update(client, config):
-    # create a machine id first thing. we'll need it for all uploads
-    logger.debug('Machine ID: %s', client.get_machine_id())
-    logger.debug("CONFIG: %s", config)
+
     print_egg_versions()
 
     if config.list_specs:
@@ -179,12 +178,14 @@ def post_update(client, config):
             else:
                 sys.exit(constants.sig_kill_bad)
 
-        if config.offline:
-            logger.debug('Running client in offline mode. Bypassing registration.')
-            return
-
-        if config.no_upload:
-            logger.debug("Running client without uploading. Bypassing registration.")
+        if config.offline or config.no_upload:
+            # create a machine id first thing. we'll need it for all uploads
+            logger.debug('Machine ID: %s', client.get_machine_id())
+            logger.debug("CONFIG: %s", config)
+            if config.offline:
+                logger.debug('Running client in offline mode. Bypassing registration.')
+            else:
+                logger.debug("Running client without uploading. Bypassing registration.")
             return
 
         if config.display_name and not config.register:
@@ -212,23 +213,25 @@ def post_update(client, config):
         return
     # -------delete everything above this line-------
 
-    if config.offline:
-        logger.debug('Running client in offline mode. Bypassing registration.')
-        return
-
-    if config.no_upload:
-        logger.debug("Running client without uploading. Bypassing registration.")
-        return
-
-    # --payload short circuits registration check
-    if config.payload:
-        logger.debug('Uploading a specified archive. Bypassing registration.')
+    if config.offline or config.no_upload or config.payload:
+        # create a machine id first thing. we'll need it for all uploads
+        logger.debug('Machine ID: %s', client.get_machine_id())
+        logger.debug("CONFIG: %s", config)
+        if config.offline:
+            logger.debug('Running client in offline mode. Bypassing registration.')
+        elif config.no_upload:
+            logger.debug("Running client without uploading. Bypassing registration.")
+        else:
+            logger.debug('Uploading a specified archive. Bypassing registration.')
         return
 
     # check registration status before anything else
-    reg_check = client.get_registration_status()
-    if reg_check is None:
-        sys.exit(constants.sig_kill_bad)
+    if isfile(constants.machine_id_file):
+        reg_check = client.get_registration_status()
+        if reg_check is None:
+            sys.exit(constants.sig_kill_bad)
+    else:
+        reg_check = False
 
     # --status
     if config.status:
@@ -266,6 +269,11 @@ def post_update(client, config):
     if config.register:
         # don't actually need to make a call to register() since
         #   system creation and upload are a single event on the platform
+        if reg_check is False and isfile(constants.machine_id_file):
+            # Do not register if a machine_id file is found
+            logger.info("Machine-id found, insights-client can not be registered."
+                        " Please, unregister insights-client first: `insights-client --unregister`")
+            sys.exit(constants.sig_kill_bad)
         if reg_check:
             logger.info('This host has already been registered.')
         if not config.disable_schedule:
@@ -289,6 +297,10 @@ def post_update(client, config):
             sys.exit(constants.sig_kill_ok)
         else:
             sys.exit(constants.sig_kill_bad)
+
+    # create a machine id first thing. we'll need it for all uploads
+    logger.debug('Machine ID: %s', client.get_machine_id())
+    logger.debug("CONFIG: %s", config)
 
 
 @phase
