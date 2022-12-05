891bbb3a23b3d98521bbc40c31bcacda58dfe169
jiazhang <jiazhang@redhat.com>
2022-12-05 17:18:44 +0800

    Update the length of title
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/cpu_online.py
+++ insights/parsers/cpu_online.py
@@ -1,6 +1,6 @@
 """
 cpu_online - File ``/sys/devices/system/cpu/online``
-=======================================================
+====================================================
 
 This parser reads the content of ``/sys/devices/system/cpu/online``.
 This file shows the number of online cpu. The format of the content
