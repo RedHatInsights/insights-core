9696c99bfa338109f256dd71b19ddd533993489e
Chen lizhong <lichen@redhat.com>
2022-12-21 16:34:17 +0800

    feat: add JbossRuntimeVersions parser
    Signed-off-by: Chen lizhong <lichen@redhat.com>

--- insights/parsers/jboss_version.py
+++ insights/parsers/jboss_version.py
@@ -1,45 +1,60 @@
 """
-JBoss version - File ``$JBOSS_HOME/version.txt``
-================================================
-
-This module provides plugins access to file ``$JBOSS_HOME/version.txt``
-
-Typical content of file ``$JBOSS_HOME/version.txt`` is::
-
-    Red Hat JBoss Enterprise Application Platform - Version 6.4.3.GA
-
-This module parses the file content and stores data in the dict ``self.parsed``.
-The version info can also be got via ``obj.major`` and ``obj.minor``, etc.
-
-Examples:
-    >>> jboss_version.file_path
-    '/home/test/jboss/jboss-eap-6.4/version.txt'
-    >>> jboss_version.raw
-    'Red Hat JBoss Enterprise Application Platform - Version 6.4.3.GA'
-    >>> jboss_version.major
-    6
-    >>> jboss_version.minor
-    4
-    >>> jboss_version.release
-    3
-    >>> jboss_version.version
-    '6.4.3'
-    >>> jboss_version.code_name
-    'GA'
+JBoss version
+=============
+Provide information about the versions of all running Jboss on a system.
 """
 from .. import Parser, parser
 from ..specs import Specs
+import json
+from insights.tests import context_wrap
 
 
 @parser(Specs.jboss_version)
 class JbossVersion(Parser):
-    """Parses the content of file ``$JBOSS_HOME/version.txt``."""
+    """
+    JBoss version - File ``$JBOSS_HOME/version.txt``
+    ================================================
+
+    This class is to access to file ``$JBOSS_HOME/version.txt``
+
+    Typical content of file ``$JBOSS_HOME/version.txt`` is::
+
+        Red Hat JBoss Enterprise Application Platform - Version 6.4.3.GA
+
+    This class parses the file content and stores data in the dict ``self.parsed``.
+    The version info can also be got via ``obj.major`` and ``obj.minor``, etc.
+
+    Examples:
+        >>> jboss_version.file_path
+        '/home/test/jboss/jboss-eap-6.4/version.txt'
+        >>> jboss_version.raw
+        'Red Hat JBoss Enterprise Application Platform - Version 6.4.3.GA'
+        >>> jboss_version.major
+        6
+        >>> jboss_version.minor
+        4
+        >>> jboss_version.release
+        3
+        >>> jboss_version.version
+        '6.4.3'
+        >>> jboss_version.code_name
+        'GA'
+    """
 
     def parse_content(self, content):
         self.raw = content[0]
         product, _, version_name = [v.strip() for v in content[0].partition("- Version")]
-        version, code_name = version_name.strip().rsplit(".", 1)
-        major, minor, release = version.split(".")
+        if " GA" in version_name:
+            # handle Red Hat JBoss Web Server - Version 5.6 GA
+            version = version_name.split(' GA')[0]
+            code_name = "GA"
+            updated_version = version + ".0"
+            major, minor, release = updated_version.split('.')[0:3]
+        else:
+            # add empty code name for Red Hat Data Grid - Version 7.3.0
+            version_name = version_name.strip() + "."
+            major, minor, release, code_name = version_name.split(".")[0:4]
+            version = '.'.join([major, minor, release])
         self.parsed = {
             "product": product,
             "version": version,
@@ -73,3 +88,35 @@ class JbossVersion(Parser):
     def code_name(self):
         """string: code name of this running JBoss progress."""
         return self.parsed["code_name"]
+
+
+@parser(Specs.jboss_runtime_versions)
+class JbossRuntimeVersions(Parser):
+    """
+     Versions of multiple running JBoss - File ``data/insights_commands/jboss_versions``
+     ===================================================================================
+
+     This class is to access to file ``data/insights_commands/jboss_versions``
+
+     Typical content of file ``data/insights_commands/jboss_versions`` is::
+
+         {"/opt/jboss-datagrid-7.3.0-server": "Red Hat Data Grid - Version 7.3.0"}
+
+     This class parses the file content and stores data in the dict ``self.versions``.
+
+     Examples:
+         >>> len(all_jboss_versions.versions)
+         1
+         >>> all_jboss_versions.versions[0].major
+         7
+         >>> all_jboss_versions.versions[0].minor
+         3
+         >>> all_jboss_versions.versions[0].release
+         0
+     """
+
+    def parse_content(self, content):
+        self.versions = []
+        jboss_version_dict = json.loads(' '.join(content))
+        for j_path, version_content in jboss_version_dict.items():
+            self.versions.append(JbossVersion(context_wrap(version_content, path=j_path)))
