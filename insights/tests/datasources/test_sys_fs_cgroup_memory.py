750d36ef8cdf8db0082cd9523baecc5be18d2f38
Ping Qin <30404410+qinpingli@users.noreply.github.com>
2023-01-18 15:58:56 +0800

    Fix: write all getting data to sys_fs_cgroup_uniq_memory_swappiness (#3658)
    Signed-off-by: Qin Ping <piqin@redhat.com>
    (cherry picked from commit 865a37c080cc4150b1ce7d25f0d50b4a7c99a05a)

--- insights/tests/datasources/test_sys_fs_cgroup_memory.py
+++ insights/tests/datasources/test_sys_fs_cgroup_memory.py
@@ -5,12 +5,16 @@ from insights.core.spec_factory import DatasourceProvider
 from insights.specs.datasources.sys_fs_cgroup_memory import sys_fs_cgroup_uniq_memory_swappiness
 
 RELATIVE_PATH = 'insights_commands/sys_fs_cgroup_uniq_memory_swappiness'
-EXPECTED_RESULT = "60  3"
+EXPECTED_RESULT_1 = "60  3"
+EXPECTED_RESULT_2 = """
+60   1
+10   2
+""".strip()
 
 
 @patch('insights.specs.datasources.sys_fs_cgroup_memory.open', new_callable=mock_open, read_data='60')
 @patch('insights.specs.datasources.sys_fs_cgroup_memory.os.walk')
-def test_sys_fs_cgroup_uniq_memory_swappiness(mock_walk, mock_ds_open):
+def test_sys_fs_cgroup_uniq_memory_swappiness_single_line(mock_walk, mock_ds_open):
     mock_walk.return_value = [
         ("/sys/fs/cgroup/memory", (), ("memory.swappiness", "test",)),
         ("/sys/fs/cgroup/memory/test_1", (), ("memory.swappiness", "test",)),
@@ -21,9 +25,30 @@ def test_sys_fs_cgroup_uniq_memory_swappiness(mock_walk, mock_ds_open):
     result = sys_fs_cgroup_uniq_memory_swappiness(broker)
     assert result is not None
     assert isinstance(result, DatasourceProvider)
-    expected = DatasourceProvider(content=EXPECTED_RESULT, relative_path=RELATIVE_PATH)
+    expected = DatasourceProvider(content=EXPECTED_RESULT_1, relative_path=RELATIVE_PATH)
     assert len(result.content) == len(expected.content) == 1
     assert result.content[0].split() == expected.content[0].split()
+    assert result.relative_path == expected.relative_path
+
+
+@patch('insights.specs.datasources.sys_fs_cgroup_memory.open', new_callable=mock_open, read_data='60')
+@patch('insights.specs.datasources.sys_fs_cgroup_memory.os.walk')
+def test_sys_fs_cgroup_uniq_memory_swappiness_multi_lines(mock_walk, mock_ds_open):
+    mock_walk.return_value = [
+        ("/sys/fs/cgroup/memory", (), ("memory.swappiness", "test",)),
+        ("/sys/fs/cgroup/memory/test_1", (), ("memory.swappiness", "test",)),
+        ("/sys/fs/cgroup/memory/test_2", (), ("memory.swappiness", "test",)),
+    ]
+
+    mock_ds_open.side_effect = (mock_ds_open.return_value, mock_open(read_data="10").return_value, mock_open(read_data="10").return_value,)
+
+    broker = {}
+    result = sys_fs_cgroup_uniq_memory_swappiness(broker)
+    assert result is not None
+    assert isinstance(result, DatasourceProvider)
+    expected = DatasourceProvider(content=EXPECTED_RESULT_2, relative_path=RELATIVE_PATH)
+    assert len(result.content) == len(expected.content) == 2
+    assert result.content[1].split() == expected.content[1].split()
     assert result.relative_path == expected.relative_path
 
 
