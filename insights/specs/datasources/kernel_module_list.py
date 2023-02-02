9c4ad4f29fceea28051dd8c773b7fe2d054a40ee
wushiqinlou <jiazhang@redhat.com>
2023-02-02 15:59:00 +0800

    fix: Enhance datasource kernel_module_filters to check the loaded modules (#3670)
    * Enhance datasource kernel_module_filters to check the loaded modules
    * Update the datasource test accordingly
    * Add lsmod into insights.collect.default_manifest module, raise a SkipComponent when no modules are loaded.
    * Update the preload parser name
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit ce140ff7d1e87d192aefc2374c4c55c07fbd665f)

--- insights/specs/datasources/kernel_module_list.py
+++ insights/specs/datasources/kernel_module_list.py
@@ -5,10 +5,11 @@ from insights.core.context import HostContext
 from insights.core.exceptions import SkipComponent
 from insights.core.filters import get_filters
 from insights.core.plugins import datasource
+from insights.parsers.lsmod import LsMod
 from insights.specs import Specs
 
 
-@datasource(HostContext)
+@datasource(LsMod, HostContext)
 def kernel_module_filters(broker):
     """
     Return a string of a list of modules from the spec filter,
@@ -16,5 +17,11 @@ def kernel_module_filters(broker):
     """
     filters = sorted((get_filters(Specs.modinfo_modules)))
     if filters:
-        return ' '.join(filters)
+        loaded_modules = []
+        for item in filters:
+            if item in broker[LsMod]:
+                loaded_modules.append(item)
+        if loaded_modules:
+            return ' '.join(loaded_modules)
+        raise SkipComponent
     raise SkipComponent
