f5a6f32f464a39b0e4165125c82addc6d775e3cf
wushiqinlou <jiazhang@redhat.com>
2023-02-02 15:59:00 +0800

    fix: Enhance datasource kernel_module_filters to check the loaded modules (#3670)
    * Enhance datasource kernel_module_filters to check the loaded modules
    * Update the datasource test accordingly
    * Add lsmod into insights.collect.default_manifest module, raise a SkipComponent when no modules are loaded.
    * Update the preload parser name
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/tests/datasources/test_kernel_module_list.py
+++ insights/tests/datasources/test_kernel_module_list.py
@@ -2,8 +2,28 @@ import pytest
 
 from insights.core import filters
 from insights.core.exceptions import SkipComponent
+from insights.parsers.lsmod import LsMod
 from insights.specs import Specs
 from insights.specs.datasources.kernel_module_list import kernel_module_filters
+from insights.tests import context_wrap
+
+LSMOD = """
+Module                  Size  Used by
+igb                   249856  0
+i2c_algo_bit           16384  1 igb
+dca                    16384  1 igb
+binfmt_misc            20480  1
+tcp_diag               16384  0
+udp_diag               16384  0
+inet_diag              24576  2 tcp_diag,udp_diag
+binfmt_misc            69632  0
+udp_diag               18632  0
+""".strip()
+
+LSMOD_NO_LOADED = """
+Module                  Size  Used by
+tcp_diag               16384  0
+""".strip()
 
 
 def setup_function(func):
@@ -13,18 +33,29 @@ def setup_function(func):
         del filters.FILTERS[Specs.modinfo_modules]
 
     if func is test_module_filters:
-        filters.add_filter(Specs.modinfo_modules, ["udp_diag", "binfmt_misc"])
+        filters.add_filter(Specs.modinfo_modules, ["udp_diag", "binfmt_misc", "wireguard"])
+    if func is test_module_no_loaded_modules:
+        filters.add_filter(Specs.modinfo_modules, ["udp_diag", "binfmt_misc", "wireguard"])
     if func is test_module_filters_empty:
         filters.add_filter(Specs.modinfo_modules, [])
 
 
 def test_module_filters():
-    broker = {}
+    lsmod_info = LsMod(context_wrap(LSMOD))
+    broker = {LsMod: lsmod_info}
     result = kernel_module_filters(broker)
     assert 'binfmt_misc udp_diag' == result
 
 
 def test_module_filters_empty():
-    broker = {}
+    lsmod_info = LsMod(context_wrap(LSMOD))
+    broker = {LsMod: lsmod_info}
+    with pytest.raises(SkipComponent):
+        kernel_module_filters(broker)
+
+
+def test_module_no_loaded_modules():
+    lsmod_info = LsMod(context_wrap(LSMOD_NO_LOADED))
+    broker = {LsMod: lsmod_info}
     with pytest.raises(SkipComponent):
         kernel_module_filters(broker)
