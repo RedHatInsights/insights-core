b3dc269ad67667090c31122d5b79750cab8966c7
Xiangce Liu <xiangceliu@redhat.com>
2023-02-16 15:22:02 +0800

    fix: The provider of GCP should be 'gcp' but not 'google' (#3678)
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/combiners/cloud_instance.py
+++ insights/combiners/cloud_instance.py
@@ -22,13 +22,6 @@ from insights.parsers.gcp_instance_type import GCPInstanceType
 from insights.parsers.subscription_manager import SubscriptionManagerFacts
 
 
-# "google" is used in class:`insights.combiners.cloud_provider.CloudProvider`.
-# but 'gcp' is outputted in "subscription-manager facts"
-rhsm_type_maps = {
-    "google": 'gcp'
-}
-
-
 @combiner(
     CloudProvider,
     [
@@ -84,8 +77,7 @@ class CloudInstance(object):
             self.type = gcp.raw
         # 2. Check the "subscription-manager facts"
         if self.id is None and facts:
-            cp_name = rhsm_type_maps.get(self.provider, self.provider)
-            key = "{0}_instance_id".format(cp_name)
+            key = "{0}_instance_id".format(self.provider)
             if key not in facts:
                 raise ContentException("Unmatched/unsupported types!")
             self.id = facts[key]
