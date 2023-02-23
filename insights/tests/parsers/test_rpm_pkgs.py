c107907cb13fb7b5be88a270138f83df6d81b3b0
Jitka Obselkova <41325380+jobselko@users.noreply.github.com>
2023-02-23 11:23:35 +0100

    Update rpm_pkgs datasource and create new parser RpmPkgsWritable. (#3684)
    Mark the original RpmPkgs parser as deprecated.
    
    Signed-off-by: Jitka Obselkova <jobselko@redhat.com>
    (cherry picked from commit 32193f5c9f8028bc24d434c3066d01ebd40420e5)

--- insights/tests/parsers/test_rpm_pkgs.py
+++ insights/tests/parsers/test_rpm_pkgs.py
@@ -1,20 +1,30 @@
 import doctest
+import pytest
 
 from insights.parsers import rpm_pkgs
-from insights.parsers.rpm_pkgs import RpmPkgs
+from insights.parsers.rpm_pkgs import RpmPkgs, RpmPkgsWritable
 from insights.tests import context_wrap
 
-PACKAGES = ["httpd-core"]
+
+DATASOURCE_OUTPUT = [("httpd-core", "httpd-core-2.4.53-7.el9.x86_64", "Red Hat, Inc.")]
+
+RPM_PKGS_OUTPUT = ["httpd-core"]
+RPM_PKGS_WRITABLE_OUTPUT = [("httpd-core", "httpd-core-2.4.53-7.el9.x86_64", "Red Hat, Inc.")]
 
 
-def test_system_user_dirs():
-    test = RpmPkgs(context_wrap(PACKAGES))
-    assert test.packages == PACKAGES
+@pytest.mark.parametrize("parser, output", [
+    (RpmPkgs, RPM_PKGS_OUTPUT),
+    (RpmPkgsWritable, RPM_PKGS_WRITABLE_OUTPUT)
+])
+def test_rpm_pkgs(parser, output):
+    test = parser(context_wrap(DATASOURCE_OUTPUT))
+    assert test.packages == output
 
 
 def test_doc_examples():
     env = {
-        "rpm_pkgs": RpmPkgs(context_wrap(PACKAGES))
+        "rpm_pkgs": RpmPkgs(context_wrap(DATASOURCE_OUTPUT)),
+        "rpm_pkgs_writable": RpmPkgsWritable(context_wrap(DATASOURCE_OUTPUT))
     }
     failed, total = doctest.testmod(rpm_pkgs, globs=env)
     assert failed == 0
