6d02665e8e5879f4204fb565657dfbb276bbae36
Xiaoxue Wang <xiaoxwan@redhat.com>
2023-03-02 13:36:09 +0800

    Use index [-3] instead of [3] for device getting
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/parsers/sys_block.py
+++ insights/parsers/sys_block.py
@@ -38,7 +38,7 @@ class StableWrites(Parser):
             raise ParseException('Error: {0}'.format(content if content else 'empty file'))
         try:
             self._stable_writes = int(content[0].strip())
-            self._device = self.file_path.split('/')[3]
+            self._device = self.file_path.split('/')[-3]
         except ValueError:
             raise ParseException("Error: unparsable content: {0}".format(content[0]))
 
