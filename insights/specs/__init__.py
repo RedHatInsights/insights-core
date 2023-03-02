060678c6779b403c196d80afb6bb3103c67c3e2e
Xiaoxue Wang <xiaoxwan@redhat.com>
2023-03-02 14:58:54 +0800

    feat: New spec and parser for sys_block_queue_stable_writes (#3688)
    * feat: New spec and parser for sys_block_queue_stable_writes
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>
    
    * Extract 'device' from file path
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>
    
    * Use index [-3] instead of [3] for device getting
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>
    
    ---------
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -663,6 +663,7 @@ class Specs(SpecSet):
     sshd_config_perms = RegistryPoint()
     sssd_config = RegistryPoint()
     sssd_logs = RegistryPoint(multi_output=True, filterable=True)
+    sys_block_queue_stable_writes = RegistryPoint(multi_output=True)
     subscription_manager_facts = RegistryPoint(filterable=True)
     subscription_manager_id = RegistryPoint()
     subscription_manager_installed_product_ids = RegistryPoint(filterable=True)
