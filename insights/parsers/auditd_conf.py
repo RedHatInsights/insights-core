9ddec9d729eb24ee23b090f9d390298060de6cae
Xiaoxue Wang <xiaoxwan@redhat.com>
2023-04-13 13:24:54 +0800

    Fix docstrings errors
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/parsers/auditd_conf.py
+++ insights/parsers/auditd_conf.py
@@ -8,17 +8,17 @@ by using the dictionary `contains` functionality.
 
 The audispd.conf file has the same format and usage with auditd.conf.
 
-Note:
+.. note::
     For Red Hat Enterprise Linux 7 and older, auditd and audispd are separate
     processes.
     Starting with Red Hat Enterprise Linux 8 the functionality of audispd has
     been migrated to auditd.
 
 AuditdConf - file ``/etc/audit/auditd.conf``
--------------------------------------------------
+--------------------------------------------
 
 AudispdConf - file ``/etc/audisp/audispd.conf``
--------------------------------------------------
+-----------------------------------------------
 
 Example:
 
