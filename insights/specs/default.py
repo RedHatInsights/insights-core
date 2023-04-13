5156e8e0a7184d0f6cec4546fd00e3f196e6a8dc
wushiqinlou <jiazhang@redhat.com>
2023-04-13 09:27:00 +0800

    fix: Add new package findutils into spec rpm_V_packages (#3733)
    * Add new package findutils into spec rpm_V_packages
    * Update insights_archive file
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit 0ed91f2a8e9229f6ff2542443ebd9021558873c2)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -524,7 +524,7 @@ class DefaultSpecs(Specs):
     rhsm_releasever = simple_file('/var/lib/rhsm/cache/releasever.json')
     rndc_status = simple_command("/usr/sbin/rndc status")
     ros_config = simple_file("/var/lib/pcp/config/pmlogger/config.ros")
-    rpm_V_packages = simple_command("/bin/rpm -V coreutils procps procps-ng shadow-utils passwd sudo chrony", keep_rc=True, signum=signal.SIGTERM)
+    rpm_V_packages = simple_command("/bin/rpm -V coreutils procps procps-ng shadow-utils passwd sudo chrony findutils", keep_rc=True, signum=signal.SIGTERM)
     rpm_ostree_status = simple_command("/usr/bin/rpm-ostree status --json", signum=signal.SIGTERM)
     rpm_pkgs = rpm_pkgs.pkgs_with_writable_dirs
     rsyslog_conf = glob_file(["/etc/rsyslog.conf", "/etc/rsyslog.d/*.conf"])
