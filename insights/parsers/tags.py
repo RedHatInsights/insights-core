5e2c1ea6a9abdb4cba61b0b5433cfae5967eb92b
Xiangce Liu <xiangceliu@redhat.com>
2023-04-28 19:17:46 +0800

    feat: new client_metadata module to hold parsers for all client generated files
    - make new parsers for ansible_host, display_name, blacklisted_specs
      tags and version_info in the new client_metadata.py
    - and mark the original BlacklistedSpecs, Tags and VersionInfo
      deprecated after 3.3.25
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/tags.py
+++ insights/parsers/tags.py
@@ -1,9 +1,21 @@
 from insights.core import JSONParser
 from insights.core.plugins import parser
 from insights.specs import Specs
+from insights.util import deprecated
 
 
 @parser(Specs.tags)
 class Tags(JSONParser):
-    """ Class for parsing the content of ``tags.json``."""
-    pass
+    """
+    .. warning::
+        This parser is deprecated, please use
+        :py:class:`insights.parsers.client_metadata.Tags` instead.
+
+    Class for parsing the content of ``tags.json``."""
+    def __init__(self, *args, **kwargs):
+        deprecated(
+            Tags,
+            "Please use insights.parsers.client_metadata.Tags instead.",
+            "3.3.25"
+        )
+        super(Tags, self).__init__(*args, **kwargs)
