e45a9a6af47c1189d37ce97d233b06f018ebd764
Xiangce Liu <xiangceliu@redhat.com>
2023-05-04 16:30:35 +0800

    feat: new client_metadata module to hold parsers for all client generated files (#3775)
    - make new parsers for ansible_host, display_name, blacklisted_specs
      machine_id, tags and version_info in the new client_metadata.py
    - and mark the original BlacklistedSpecs, Tags, and VersionInfo
      deprecated after 3.3.25
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit e7e478460ae4bc562be99ca8899c559535a0640f)

--- insights/parsers/branch_info.py
+++ insights/parsers/branch_info.py
@@ -1,9 +1,22 @@
 from insights.core import YAMLParser
 from insights.core.plugins import parser
 from insights.specs import Specs
+from insights.util import deprecated
 
 
 @parser(Specs.branch_info)
 class BranchInfo(YAMLParser):
-    """ Class for parsing the content of ``branch_info``."""
-    pass
+    """
+    .. warning::
+        This parser is deprecated, please use
+        :py:class:`insights.parsers.client_metadata.BranchInfo` instead.
+
+    Class for parsing the content of ``branch_info``.
+    """
+    def __init__(self, *args, **kwargs):
+        deprecated(
+            BranchInfo,
+            "Please use insights.parsers.client_metadata.BranchInfo instead.",
+            "3.3.25"
+        )
+        super(BranchInfo, self).__init__(*args, **kwargs)
