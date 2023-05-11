10693c0fb76db51f22679eaa6713cdeb11583f05
xintli <986222045@qq.com>
2023-05-11 11:16:55 +0800

    Feat: Add new parser CupsdConf (#3781)
    Signed-off-by: Xinting Li <xintli@redhat.com>

---
+++ insights/tests/parsers/test_cups_confs.py
@@ -0,0 +1,130 @@
+import doctest
+import pytest
+
+from insights.core.exceptions import ParseException
+from insights.parsers import cups_confs
+from insights.parsers.cups_confs import CupsdConf
+from insights.parsr.query import first, last
+from insights.tests import context_wrap
+
+CUPSD_CONF = """
+MaxLogSize 0
+LogLevel warn
+
+# Only listen for connections from the local machine.
+Listen localhost:631
+Listen /var/run/cups/cups.sock
+Browsing On
+BrowseLocalProtocols dnssd
+DefaultAuthType Basic
+WebInterface Yes
+IdleExitTimeout 0
+
+<Location />
+  Order allow,deny
+</Location>
+
+<Location /admin>
+  Order allow,deny
+</Location>
+
+<Location /admin/conf>
+  AuthType Default
+  Require user @SYSTEM
+  Order allow,deny
+</Location>
+
+<Location /admin/log>
+  AuthType Default
+  Require user @SYSTEM
+  Order allow,deny
+</Location>
+
+<Policy default>
+  JobPrivateAccess default
+  JobPrivateValues default
+  SubscriptionPrivateAccess default
+  SubscriptionPrivateValues default
+
+  <Limit Create-Job Print-Job Print-URI Validate-Job>
+    Order deny,allow
+  </Limit>
+
+  <Limit Pause-Printer Resume-Printer Enable-Printer Disable-Printer Pause-Printer-After-Current-Job Hold-New-Jobs Release-Held-New-Jobs Deactivate-Printer Activate-Printer Restart-Printer Shutdown-Printer Startup-Printer Promote-Job Schedule-Job-After Cancel-Jobs CUPS-Accept-Jobs CUPS-Reject-Jobs>
+    AuthType Default
+    Require user @SYSTEM
+    Order deny,allow
+  </Limit>
+
+  <Limit Cancel-Job CUPS-Authenticate-Job>
+    Require user @OWNER @SYSTEM
+    Order deny,allow
+  </Limit>
+
+  <Limit All>
+    Order deny,allow
+  </Limit>
+</Policy>
+""".strip()
+
+CUPSD_CONF_EXAMPLE = """
+MaxLogSize 0
+LogLevel warn
+Listen localhost:631
+Listen /var/run/cups/cups.sock
+Browsing On
+BrowseLocalProtocols dnssd
+
+<Location />
+Order allow,deny
+</Location>
+
+<Policy default>
+JobPrivateAccess default
+JobPrivateValues default
+SubscriptionPrivateAccess default
+SubscriptionPrivateValues default
+
+<Limit Send-Document Send-URI Hold-Job Release-Job Restart-Job Purge-Jobs Set-Job-Attributes Create-Job-Subscription Renew-Subscription Cancel-Subscription Get-Notifications Reprocess-Job Cancel-Current-Job Suspend-Current-Job Resume-Job Cancel-My-Jobs Close-Job CUPS-Move-Job CUPS-Get-Document>
+Require user @OWNER @SYSTEM
+Order deny,allow
+</Limit>
+
+<Limit All>
+Order deny,allow
+</Limit>
+</Policy>
+""".strip()
+
+CUPSD_CONF_ERROR = """
+</Limit>
+
+<Limit All>
+Order deny,allow
+</Limit>
+</Policy>
+""".strip()
+
+
+def test_cupsd_conf():
+    context = context_wrap(CUPSD_CONF)
+    result = CupsdConf(context)
+    assert result['Listen'][first].value == 'localhost:631'
+    assert result['Listen'][last].value == '/var/run/cups/cups.sock'
+    assert result[('Policy', 'default')][('Limit', 'Cancel-Job')]['Require'][first].value == "user @OWNER @SYSTEM"
+    assert result[('Policy', 'default')][('Limit', 'CUPS-Authenticate-Job')]['Require'][first].value == "user @OWNER @SYSTEM"
+    assert result[('Location', '/admin')]['Order'][last].value == 'allow,deny'
+
+
+def test_cupsd_conf_error():
+    with pytest.raises(ParseException) as exc:
+        CupsdConf(context_wrap(CUPSD_CONF_ERROR))
+    assert str(exc.value) == "There was an exception when parsing the cupsd.conf file"
+
+
+def test_doc():
+    env = {
+            'cupsd_conf': CupsdConf(context_wrap(CUPSD_CONF_EXAMPLE)),
+          }
+    failed, total = doctest.testmod(cups_confs, globs=env)
+    assert failed == 0
