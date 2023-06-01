9db2573832a767e8abb9f8b94dd5a1bfc1a27657
xintli <986222045@qq.com>
2023-06-01 16:29:40 +0800

    FEAT: Add new parser EtcMachineId (#3805)
    Signed-off-by: Xinting Li <xintli@redhat.com>
    (cherry picked from commit 9562a758c885c0b972e1172aae466645201737de)

---
+++ insights/parsers/etc_machine_id.py
@@ -0,0 +1,33 @@
+"""
+EtcMachineId - file ``/etc/machine-id``
+=======================================
+"""
+
+from insights.core import Parser
+from insights.core.exceptions import SkipComponent
+from insights.core.plugins import parser
+from insights.parsers import get_active_lines
+from insights.specs import Specs
+
+
+@parser(Specs.etc_machine_id)
+class EtcMachineId(Parser):
+    """
+    Class for parsing the file ``/etc/machine-id``
+
+    Sample file content::
+
+        4f3fbfda59ff4aea969255a73342d439
+
+    Examples:
+        >>> type(machine_id)
+        <class 'insights.parsers.etc_machine_id.EtcMachineId'>
+        >>> machine_id.machine_id
+        '4f3fbfda59ff4aea969255a73342d439'
+    """
+
+    def parse_content(self, content):
+        content = get_active_lines(content)
+        if not content or len(content) != 1:
+            raise SkipComponent('Invalid Content')
+        self.machine_id = content[0]
