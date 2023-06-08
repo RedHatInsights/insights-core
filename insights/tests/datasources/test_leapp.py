52d665ab7a09f173b4ab9966e3d5819c5bda59ae
Xiangce Liu <xiangceliu@redhat.com>
2023-06-08 14:57:02 +0800

    fix: support inhibitor entries with missing remediations (#3809)
    * fix: support inhibitor entries with missing remediations
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update per feedback
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/datasources/test_leapp.py
+++ insights/tests/datasources/test_leapp.py
@@ -1,51 +1,67 @@
 import json
 import pytest
 
-from mock.mock import Mock
+try:
+    from unittest.mock import patch
+    builtin_open = "builtins.open"
+except Exception:
+    from mock import patch
+    builtin_open = "__builtin__.open"
+
 from os import path
 
-from insights.core.exceptions import SkipComponent
-from insights.specs.datasources.leapp import leapp_report, LocalSpecs
+from insights.core.exceptions import SkipComponent, ContentException
+from insights.specs.datasources.leapp import leapp_report
 
 
 with open(path.join(path.dirname(__file__), 'leapp-report.json'), 'r') as fp:
-    INPUT_OK = fp.readlines()
+    INPUT_OK = json.load(fp)
 
 with open(path.join(path.dirname(__file__), 'leapp-report.ret'), 'r') as fp:
     RESULT = fp.readlines()
 
-INPUT_NG_1 = """
+INPUT_NG_1 = json.loads("""
 { "entries":[
   {
-    "a": "B",
+    "a": "B"
   }
  ]
 }
-""".strip()
+""".strip())
 
-INPUT_NG_2 = ""
+INPUT_NG_2 = json.loads("{}")
 
 
-def test_leapp_report():
-    report_file = Mock()
-    report_file.content = INPUT_OK
-    broker = {LocalSpecs.leapp_report_raw: report_file}
-
-    result = leapp_report(broker)
-
+@patch("json.load", return_value=INPUT_OK)
+@patch("os.path.isfile", return_value=True)
+@patch(builtin_open)
+def test_leapp_report_ok(m_open, m_isfile, m_load):
+    result = leapp_report({})
     result_json = json.loads(''.join(result.content).strip())
     expected_json = json.loads(''.join(RESULT).strip())
     for ret in result_json:
         assert ret in expected_json
 
-    report_file = Mock()
-    report_file.content = INPUT_NG_1
-    broker = {LocalSpecs.leapp_report_raw: report_file}
-    with pytest.raises(SkipComponent):
-        leapp_report(broker)
 
-    report_file = Mock()
-    report_file.content = INPUT_NG_2
-    broker = {LocalSpecs.leapp_report_raw: report_file}
+@patch("json.load", return_value=INPUT_NG_1)
+@patch("os.path.isfile", return_value=True)
+@patch(builtin_open)
+def test_leapp_report_nothing(m_open, m_isfile, m_load):
+    with pytest.raises(SkipComponent) as ce:
+        leapp_report({})
+    assert "Nothing" in str(ce)
+
+
+@patch("json.load", return_value=INPUT_NG_2)
+@patch("os.path.isfile", return_value=True)
+@patch(builtin_open)
+def test_leapp_report_ng_2(m_open, m_isfile, m_load):
+    with pytest.raises(ContentException) as ce:
+        leapp_report({})
+    assert "Nothing" in str(ce)
+
+
+@patch("os.path.isfile", return_value=False)
+def test_leapp_report_no_file(isfile):
     with pytest.raises(SkipComponent):
-        leapp_report(broker)
+        leapp_report({})
--- insights/tests/parsers/test_leapp.py
+++ insights/tests/parsers/test_leapp.py
@@ -8,13 +8,17 @@ from insights.tests.datasources.test_leapp import RESULT
 
 def test_leapp():
     ret = LeappReport(context_wrap(RESULT, path='insights_commands/leapp_report'))
-    assert len(ret) == 2
+    assert len(ret) == 4
     assert ret[0]['title'] == "Use of NFS detected. Upgrade can't proceed"
     assert 'summary' in ret[0]
     assert ret[1]['title'] == "Possible problems with remote login using root account"
     assert 'summary' in ret[1]
     assert ret[0]['remediations'][0]['context'] == "Disable NFS temporarily for the upgrade if possible."
     assert 'If you depend on remote' in ret[1]['remediations'][0]['context']
+    assert 'remediations' not in ret[2]
+    assert 'external' in ret[2]
+    assert 'remediations' not in ret[3]
+    assert 'related_resources' in ret[3]
 
 
 def test_doc_examples():
