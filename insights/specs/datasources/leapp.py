c4569a3859d327f2485d0676162b02d693b95a0d
Xiangce Liu <xiangceliu@redhat.com>
2023-06-08 14:57:02 +0800

    fix: support inhibitor entries with missing remediations (#3809)
    * fix: support inhibitor entries with missing remediations
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update per feedback
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 386f4a78191b70bc5b48c2a2aef93869129c29d9)

--- insights/specs/datasources/leapp.py
+++ insights/specs/datasources/leapp.py
@@ -2,20 +2,15 @@
 Custom datasources for leapp
 """
 import json
+import os
 
 from insights.core.context import HostContext
-from insights.core.exceptions import SkipComponent
+from insights.core.exceptions import ContentException, SkipComponent
 from insights.core.plugins import datasource
-from insights.core.spec_factory import DatasourceProvider, simple_file
-from insights.specs import Specs
+from insights.core.spec_factory import DatasourceProvider
 
 
-class LocalSpecs(Specs):
-    leapp_report_raw = simple_file("/var/log/leapp/leapp-report.json")
-    """ Returns the contents of the file ``/var/log/leapp/leapp-report.json`` """
-
-
-@datasource(LocalSpecs.leapp_report_raw, HostContext)
+@datasource(HostContext)
 def leapp_report(broker):
     """
     This datasource get useful information from ``leapp-report.json``.
@@ -28,35 +23,32 @@ def leapp_report(broker):
         str: JSON string after get the required data.
 
     Raises:
-        SkipComponent: When the path does not exist, nothing is collected,
-                       or any exception occurs.
+        SkipComponent: When the `leapp_report.json` does not exist or nothing need to collect
+        ContentException: When any exception occurs.
     """
-    relative_path = 'insights_commands/leapp_report'
-    valid_keys = {
-        'inhibitor': ['title', 'summary', ('detail', 'remediations')]
-    }
+    leapp_report_path = "/var/log/leapp/leapp-report.json"
+    if not os.path.isfile(leapp_report_path):
+        raise SkipComponent("No such file")
     try:
-        content = broker[LocalSpecs.leapp_report_raw].content
-        report = json.loads('\n'.join(content))
-        if isinstance(report, dict):
-            results = []
-            for entry in report.get('entries', []):
-                groups = entry.get('groups', entry.get('flags', []))
-                for flag, keys in valid_keys.items():
-                    if flag in groups:
-                        ret = dict(groups=groups)
-                        for key in keys:
-                            if isinstance(key, tuple):
-                                tmp = entry
-                                for k in key:
-                                    tmp = tmp[k]
-                                ret[key[-1]] = tmp
-                            else:
-                                ret[key] = entry[key]
-                        results.append(ret)
-            if results:
-                return DatasourceProvider(content=json.dumps(results), relative_path=relative_path)
-        raise SkipComponent("Invalid format")
+        relative_path = 'insights_commands/leapp_report'
+        valid_keys = {
+            'inhibitor': ['title', 'summary', 'detail']
+        }
+        with open(leapp_report_path, 'r') as fp:
+            json_report = json.load(fp)
+            if isinstance(json_report, dict):
+                results = []
+                for entry in json_report.get('entries', []):
+                    groups = entry.get('groups', entry.get('flags', []))
+                    for flag, keys in valid_keys.items():
+                        if flag in groups:
+                            ret = dict(groups=groups)
+                            for key in keys:
+                                detail = entry.get(key)
+                                ret.update(detail if isinstance(detail, dict) else {key: detail}) if detail else None
+                            results.append(ret)
+                if results:
+                    return DatasourceProvider(content=json.dumps(results), relative_path=relative_path)
+                raise SkipComponent("Nothing")
     except Exception as e:
-        raise SkipComponent(e)
-    raise SkipComponent
+        raise ContentException(e)
