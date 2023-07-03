9eecaf461cf9ef8b2e3a6a637f20e1076c4b2c99
Xiangce Liu <xiangceliu@redhat.com>
2023-07-03 09:28:33 +0800

    fix: refactor the SubscriptionManageID parser (#3834)
    - and Move it to the insights.parsers
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/subscription_manager.py
+++ insights/parsers/subscription_manager.py
@@ -4,16 +4,68 @@ subscription-manager commands
 
 Parsers for parsing output of the ``subscription-manager`` commands.
 
+SubscriptionManagerID - command ``subscription-manager identity``
+-----------------------------------------------------------------
+
 SubscriptionManagerFacts - command ``subscription-manager facts``
 -----------------------------------------------------------------
 """
 from insights.core import CommandParser
-from insights.core.exceptions import ParseException, SkipComponent
+from insights.core.exceptions import SkipComponent
 from insights.core.filters import add_filter
 from insights.core.plugins import parser
 from insights.specs import Specs
 
 add_filter(Specs.subscription_manager_facts, ['instance_id'])
+
+
+def _local_kv_split(lines):
+    ret = dict()
+    for line in lines:
+        if ': ' in line:
+            key, val = [_l.strip() for _l in line.split(': ', 1)]
+            ret[key] = val
+    if ret:
+        return ret
+    raise SkipComponent
+
+
+@parser(Specs.subscription_manager_id)
+class SubscriptionManagerID(CommandParser, dict):
+    """
+    Reads the output of subscription-manager identity and retrieves the UUID
+
+    Example output::
+
+        system identity: 6655c27c-f561-4c99-a23f-f53e5a1ef311
+        name: rhel7.localdomain
+        org name: 1234567
+        org ID: 1234567
+
+    Examples::
+        >>> type(subman_id)
+        <class 'insights.parsers.subscription_manager.SubscriptionManagerID'>
+        >>> subman_id.identity == '6655c27c-f561-4c99-a23f-f53e5a1ef311'
+        True
+        >>> subman_id.get('org ID') == '1234567'
+        True
+    """
+    def parse_content(self, content):
+        self.update(_local_kv_split(content))
+
+    @property
+    def data(self):
+        """
+        .. deprecated:: 3.2.3
+
+            Will be removed from 3.4.0. Please use :attr:`identity` instead.
+        """
+        return self.get('system identity')
+
+    @property
+    def identity(self):
+        """Returns the value of 'system identity'."""
+        return self.get('system identity')
 
 
 @parser(Specs.subscription_manager_facts)
@@ -37,11 +89,4 @@ class SubscriptionManagerFacts(CommandParser, dict):
         '567890567890'
     """
     def parse_content(self, content):
-        for line in content:
-            if ': ' not in line:
-                raise ParseException('Incorrect line: {0}'.format(line))
-            key, val = [_l.strip() for _l in line.split(': ', 1)]
-            self[key] = val
-
-        if len(self) == 0:
-            raise SkipComponent
+        self.update(_local_kv_split(content))
