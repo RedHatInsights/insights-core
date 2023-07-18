c3279cad05e85dfcb56c8996526333bb06bc628d
Xiangce Liu <xiangceliu@redhat.com>
2023-07-18 09:25:45 +0800

    chore: discard the deprecation of LsBoot, LsDev and LsSysFirmware (#3849)
    - They are used by gss rules and other apps
      See the latest comment to #3833
    - use the new ls.FileListing instead of core.FileListing
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/ls_dev.py
+++ insights/parsers/ls_dev.py
@@ -5,64 +5,58 @@ LsDev - Command ``ls -lanR /dev``
 The ``ls -lanR /dev`` command provides information for the listing of the
 ``/dev`` directory.
 
-Sample input is shown in the Examples. See ``FileListing`` class for
-additional information.
+See :class:`insights.parsers.ls.FileListing` for more information.
 
-Examples:
-    >>> LS_DEV = '''
-    ... /dev:
-    ... total 3
-    ... brw-rw----.  1 0  6 253,   0 Aug  4 16:56 dm-0
-    ... brw-rw----.  1 0  6 253,   1 Aug  4 16:56 dm-1
-    ... brw-rw----.  1 0  6 253,  10 Aug  4 16:56 dm-10
-    ... crw-rw-rw-.  1 0  5   5,   2 Aug  5  2016 ptmx
-    ... drwxr-xr-x.  2 0  0        0 Aug  4 16:56 pts
-    ... lrwxrwxrwx.  1 0  0       25 Oct 25 14:48 initctl -> /run/systemd/initctl/fifo
-    ...
-    ... /dev/rhel:
-    ... total 0
-    ... drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .
-    ... drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..
-    ... lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2
-    ... lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 root -> ../dm-0
-    ... lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 swap -> ../dm-1
-    ...'''
-    >>> ls_dev = LsDev(context_wrap(LS_DEV))
-    >>> ls_dev
-    <insights.parsers.ls_dev.LsDev object at 0x7f287406a1d0>
-    >>> "/dev/rhel" in ls_dev
-    True
-    >>> ls_dev.files_of("/dev/rhel")
-    ['home', 'root', 'swap']
-    >>> ls_dev.dirs_of("/dev/rhel")
-    ['.', '..']
-    >>> ls_dev.specials_of("/dev/rhel")
-    []
-    >>> ls_dev.listing_of("/dev/rhel").keys()
-    ['home', 'root', 'swap', '..', '.']
-    >>> ls_dev.dir_entry("/dev/rhel", "home")
-    {'group': '0', 'name': 'home', 'links': 1, 'perms': 'rwxrwxrwx.',
-    'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2', 'owner': '0',
-    'link': '../dm-2', 'date': 'Jul 25 10:00', 'type': 'l', 'size': 7}
-    >>> ls_dev.listing_of('/dev/rhel')['.']['type'] == 'd'
-    True
-    >>> ls_dev.listing_of('/dev/rhel')['home']['link']
-    '../dm-2'
 """
-from .. import parser, FileListing, CommandParser
+from insights import CommandParser, FileListing, parser
 from insights.specs import Specs
-from insights.util import deprecated
 
 
 @parser(Specs.ls_dev)
 class LsDev(CommandParser, FileListing):
     """
-    .. warning::
-        This class is deprecated and will be removed from 3.5.0.
-        Please use the :class:`insights.parsers.ls.LSlanR` instead.
-
     Parses output of ``ls -lanR /dev`` command.
+
+    .. warning::
+
+        For Insights Advisor Rules, it's recommended to use the
+        :class:`insights.parsers.ls.LSlanR` and add the ``"/dev"`` to
+        the filter list of `Specs.ls_lanR_dirs` instead.
+
+    Sample directory listing::
+        /dev:
+        total 3
+        brw-rw----.  1 0  6 253,   0 Aug  4 16:56 dm-0
+        brw-rw----.  1 0  6 253,   1 Aug  4 16:56 dm-1
+        brw-rw----.  1 0  6 253,  10 Aug  4 16:56 dm-10
+        crw-rw-rw-.  1 0  5   5,   2 Aug  5  2016 ptmx
+        drwxr-xr-x.  2 0  0        0 Aug  4 16:56 pts
+        lrwxrwxrwx.  1 0  0       25 Oct 25 14:48 initctl -> /run/systemd/initctl/fifo
+
+        /dev/rhel:
+        total 0
+        drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .
+        drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..
+        lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2
+        lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 root -> ../dm-0
+        lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 swap -> ../dm-1
+
+    Examples:
+        >>> type(ls_dev)
+        <class 'insights.parsers.ls_dev.LsDev'>
+        >>> "/dev/rhel" in ls_dev
+        True
+        >>> ls_dev.files_of("/dev/rhel")
+        ['home', 'root', 'swap']
+        >>> ls_dev.dirs_of("/dev/rhel")
+        ['.', '..']
+        >>> ls_dev.specials_of("/dev/rhel")
+        []
+        >>> sorted(ls_dev.listing_of("/dev/rhel").keys())
+        ['.', '..', 'home', 'root', 'swap']
+        >>> ls_dev.listing_of('/dev/rhel')['.']['type'] == 'd'
+        True
+        >>> ls_dev.listing_of('/dev/rhel')['home']['link']
+        '../dm-2'
     """
-    def __init__(self, *args, **kwargs):
-        deprecated(LsDev, "Please use the :class:`insights.parsers.ls.LSlanR` instead.", "3.5.0")
-        super(LsDev, self).__init__(*args, **kwargs)
+    __root_path = '/dev'
--- insights/parsers/ls_sys_firmware.py
+++ insights/parsers/ls_sys_firmware.py
@@ -5,53 +5,54 @@ LsSysFirmware - command ``ls /sys/firmware``
 The ``ls -lanR /sys/firmware`` command provides information for the listing of
 the ``/sys/firmware`` directory.
 
-Sample input is shown in the Examples. See ``FileListing`` class for
-additional information.
+See :class:`insights.parsers.ls.FileListing` for more information.
 
-Examples:
-    >>> LS_SYS_FIRMWARE = '''
-    ... /sys/firmware:
-    ... total 0
-    ... drwxr-xr-x.  5 0 0 0 Dec 22 17:56 .
-    ... dr-xr-xr-x. 13 0 0 0 Dec 22 17:56 ..
-    ... drwxr-xr-x.  5 0 0 0 Dec 22 17:56 acpi
-    ... drwxr-xr-x.  3 0 0 0 Dec 22 17:57 dmi
-    ... drwxr-xr-x. 10 0 0 0 Dec 22 17:57 memmap
-    ...
-    ... /sys/firmware/acpi:
-    ... total 0
-    ... drwxr-xr-x. 5 0 0    0 Dec 22 17:56 .
-    ... drwxr-xr-x. 5 0 0    0 Dec 22 17:56 ..
-    ... drwxr-xr-x. 6 0 0    0 Feb 10 15:54 hotplug
-    ... drwxr-xr-x. 2 0 0    0 Feb 10 15:54 interrupts
-    ... -r--r--r--. 1 0 0 4096 Feb 10 15:54 pm_profile
-    ... drwxr-xr-x. 3 0 0    0 Dec 22 17:56 tables
-    ... '''
-    >>> ls_sys_firmware = LsSysFirmware(context_wrap(LS_SYS_FIRMWARE))
-    >>> "acpi" in ls_sys_firmware
-    False
-    >>> "/sys/firmware/acpi" in ls_sys_firmware
-    True
-    >>> ls_sys_firmware.dirs_of("/sys/firmware")
-    ['.', '..', 'acpi', 'dmi', 'memmap']
-    >>> ls_sys_firmware.files_of("/sys/firmware/acpi")
-    ['pm_profile']
 """
-from .. import parser
-from .. import FileListing, CommandParser
+
+from insights import CommandParser, FileListing, parser
 from insights.specs import Specs
-from insights.util import deprecated
 
 
 @parser(Specs.ls_sys_firmware)
 class LsSysFirmware(CommandParser, FileListing):
     """
-    .. warning::
-        This class is deprecated and will be removed from 3.5.0.
-        Please use the :class:`insights.parsers.ls.LSlanR` instead.
-
     Parses output of ``ls -lanR /sys/firmware`` command.
+
+    .. warning::
+
+        For Insights Advisor Rules, it's recommended to use the
+        :class:`insights.parsers.ls.LSlanR` and add the ``"/sys/firmware"`` to
+        the filter list of `Specs.ls_lanR_dirs` instead.
+
+    Sample directory listing::
+
+        /sys/firmware:
+        total 0
+        drwxr-xr-x.  5 0 0 0 Dec 22 17:56 .
+        dr-xr-xr-x. 13 0 0 0 Dec 22 17:56 ..
+        drwxr-xr-x.  5 0 0 0 Dec 22 17:56 acpi
+        drwxr-xr-x.  3 0 0 0 Dec 22 17:57 dmi
+        drwxr-xr-x. 10 0 0 0 Dec 22 17:57 memmap
+
+        /sys/firmware/acpi:
+        total 0
+        drwxr-xr-x. 5 0 0    0 Dec 22 17:56 .
+        drwxr-xr-x. 5 0 0    0 Dec 22 17:56 ..
+        drwxr-xr-x. 6 0 0    0 Feb 10 15:54 hotplug
+        drwxr-xr-x. 2 0 0    0 Feb 10 15:54 interrupts
+        -r--r--r--. 1 0 0 4096 Feb 10 15:54 pm_profile
+        drwxr-xr-x. 3 0 0    0 Dec 22 17:56 tables
+
+    Examples:
+        >>> type(ls_sys_firmware)
+        <class 'insights.parsers.ls_sys_firmware.LsSysFirmware'>
+        >>> "acpi" in ls_sys_firmware
+        False
+        >>> "/sys/firmware/acpi" in ls_sys_firmware
+        True
+        >>> ls_sys_firmware.dirs_of("/sys/firmware")
+        ['.', '..', 'acpi', 'dmi', 'memmap']
+        >>> ls_sys_firmware.files_of("/sys/firmware/acpi")
+        ['pm_profile']
     """
-    def __init__(self, *args, **kwargs):
-        deprecated(LsSysFirmware, "Please use the :class:`insights.parsers.ls.LSlanR` instead.", "3.5.0")
-        super(LsSysFirmware, self).__init__(*args, **kwargs)
+    __root_path = '/sys/firmware'
