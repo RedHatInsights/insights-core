85566697f729b5385d14b02818664e7cfa5fd854
Xiangce Liu <xiangceliu@redhat.com>
2023-07-18 09:25:45 +0800

    chore: discard the deprecation of LsBoot, LsDev and LsSysFirmware (#3849)
    - They are used by gss rules and other apps
      See the latest comment to #3833
    - use the new ls.FileListing instead of core.FileListing
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 520c4caba7af09106dc1d46d8030628bd420f7f5)

--- insights/parsers/ls_boot.py
+++ insights/parsers/ls_boot.py
@@ -5,53 +5,53 @@ LsBoot - command ``ls -lanR /boot``
 The ``ls -lanR /boot`` command provides information for the listing of the
 ``/boot`` directory.
 
-See the ``FileListing`` class for a more complete description of the
-available features of the class.
+See :class:`insights.parsers.ls.FileListing` for more information.
 
-Sample directory listing::
-
-    /boot:
-    total 187380
-    dr-xr-xr-x.  3 0 0     4096 Mar  4 16:19 .
-    dr-xr-xr-x. 19 0 0     4096 Jul 14 09:10 ..
-    -rw-r--r--.  1 0 0   123891 Aug 25  2015 config-3.10.0-229.14.1.el7.x86_64
-
-    /boot/grub2:
-    total 36
-    drwxr-xr-x. 6 0 0  104 Mar  4 16:16 .
-    dr-xr-xr-x. 3 0 0 4096 Mar  4 16:19 ..
-    lrwxrwxrwx. 1 0 0     11 Aug  4  2014 menu.lst -> ./grub.conf
-    -rw-r--r--. 1 0 0   64 Sep 18  2015 device.map
-
-Examples:
-
-    >>> bootdir = shared[LsBoot]
-    >>> '/boot' in bootdir
-    True
-    >>> '/boot/grub' in bootdir
-    False
-    >>> bootdir.files_of('/boot')
-    ['config-3.10.0-229.14.1.el7.x86_64']
-    >>> bootdir.dirs_of('/boot')
-    ['.', '..', 'grub2']
-    >>> bootdir.dir_contains('/boot/grub2', 'menu.lst')
-    True
 """
 
-from .. import FileListing, parser, CommandParser
+from insights import CommandParser, parser
+from insights.parsers.ls import FileListing
 from insights.specs import Specs
-from insights.util import deprecated
 
 
 @parser(Specs.ls_boot)
 class LsBoot(CommandParser, FileListing):
     """
-    .. warning::
-        This class is deprecated and will be removed from 3.5.0.
-        Please use the :class:`insights.parsers.ls.LSlanR` instead.
-
     Parse the /boot directory listing using a standard FileListing parser.
+
+    .. warning::
+
+        For Insights Advisor Rules, it's recommended to use the
+        :class:`insights.parsers.ls.LSlanR` and add the ``"/boot"`` to
+        the filter list of `Specs.ls_lanR_dirs` instead.
+
+    Sample directory listing::
+
+        /boot:
+        total 187380
+        dr-xr-xr-x.  3 0 0     4096 Mar  4 16:19 .
+        dr-xr-xr-x. 19 0 0     4096 Jul 14 09:10 ..
+        -rw-r--r--.  1 0 0   123891 Aug 25  2015 config-3.10.0-229.14.1.el7.x86_64
+
+        /boot/grub2:
+        total 36
+        drwxr-xr-x. 6 0 0  104 Mar  4 16:16 .
+        dr-xr-xr-x. 3 0 0 4096 Mar  4 16:19 ..
+        lrwxrwxrwx. 1 0 0     11 Aug  4  2014 menu.lst -> ./grub.conf
+        -rw-r--r--. 1 0 0   64 Sep 18  2015 device.map
+
+    Examples:
+        >>> type(bootdir)
+        <class 'insights.parsers.ls_boot.LsBoot'>
+        >>> '/boot' in bootdir
+        True
+        >>> '/boot/grub' in bootdir
+        False
+        >>> bootdir.files_of('/boot')
+        ['config-3.10.0-229.14.1.el7.x86_64']
+        >>> bootdir.dirs_of('/boot')
+        ['.', '..', 'grub2']
+        >>> bootdir.dir_contains('/boot/grub2', 'menu.lst')
+        True
     """
-    def __init__(self, *args, **kwargs):
-        deprecated(LsBoot, "Please use the :class:`insights.parsers.ls.LSlanR` instead.", "3.5.0")
-        super(LsBoot, self).__init__(*args, **kwargs)
+    __root_path = '/boot'
