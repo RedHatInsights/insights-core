1be38abc4ff6c900b288a124b84aaeafc963a640
wushiqinlou <jiazhang@redhat.com>
2023-08-03 12:21:50 +0800

    feat: Add parser intersystems_configuration_log (#3861)
    * Add parser intersystems_configuration_log
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    * Add datasource test
    * Update datasource to use parsers
    * Update filter spec
    * Fix flake8 issue
    * Update datasources name and update doc
    * Update underline length
    * Update docstring
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    (cherry picked from commit 2334392a0037ed532efa8b805b1be9ef9268cacd)

--- insights/tests/parsers/test_iris.py
+++ insights/tests/parsers/test_iris.py
@@ -32,6 +32,68 @@ Configuration 'IRIS'   (default)
 IRIS_NO_CONTENT = """"""
 
 
+IRIS_CPF = """
+[ConfigFile]
+Product=IRIS
+Version=2023.1
+
+[Databases]
+IRISSYS=/intersystems/mgr/
+IRISLIB=/intersystems/mgr/irislib/
+IRISTEMP=/intersystems/mgr/iristemp/
+IRISLOCALDATA=/intersystems/mgr/irislocaldata/
+IRISAUDIT=/intersystems/mgr/irisaudit/
+ENSLIB=/intersystems/mgr/enslib/
+USER=/intersystems/mgr/user/
+
+[Namespaces]
+%SYS=IRISSYS
+USER=USER
+
+[SqlSysDatatypes]
+BIGINT=%Library.BigInt
+BIGINT(%1)=%Library.BigInt
+BINARY=%Library.Binary(MAXLEN=1)
+BINARY VARYING=%Library.Binary(MAXLEN=1)
+CLOB=%Stream.GlobalCharacter
+DATE=%Library.Date
+DATETIME=%Library.DateTime
+DATETIME2=%Library.DateTime
+DEC=%Library.Numeric(MAXVAL=999999999999999,MINVAL=-999999999999999,SCALE=0)
+DEC(%1)=%Library.Numeric(MAXVAL=<|'$$maxval^%apiSQL(%1,0)'|>,MINVAL=<|'$$minval^%apiSQL(%1,0)'|>,SCALE=0)
+DEC(%1,%2)=%Library.Numeric(MAXVAL=<|'$$maxval^%apiSQL(%1,%2)'|>,MINVAL=<|'$$minval^%apiSQL(%1,%2)'|>,SCALE=%2)
+DECIMAL=%Library.Numeric(MAXVAL=999999999999999,MINVAL=-999999999999999,SCALE=0)
+DECIMAL(%1)=%Library.Numeric(MAXVAL=<|'$$maxval^%apiSQL(%1,0)'|>,MINVAL=<|'$$minval^%apiSQL(%1,0)'|>,SCALE=0)
+DECIMAL(%1,%2)=%Library.Numeric(MAXVAL=<|'$$maxval^%apiSQL(%1,%2)'|>,MINVAL=<|'$$minval^%apiSQL(%1,%2)'|>,SCALE=%2)
+DOUBLE=%Library.Double
+DOUBLE PRECISION=%Library.Double
+""".strip()
+
+IRIS_MESSAGES_LOG = """
+06/26/23-08:02:17:828 (144145) 0 [Generic.Event] Allocated 495MB shared memory
+06/26/23-08:02:17:828 (144145) 0 [Generic.Event] 32MB global buffers, 80MB routine buffers, 64MB journal buffers, 4MB buffer descriptors, 300MB heap, 5MB ECP, 9MB miscellaneous
+06/26/23-08:02:17:831 (144145) 0 [Crypto.IntelSandyBridgeAESNI] Intel Sandy Bridge AES-NI instructions detected.
+06/26/23-08:02:17:831 (144145) 0 [SIMD] SIMD optimization level: DEFAULT X86
+06/26/23-08:02:17:903 (144145) 0 [WriteDaemon.UsingWIJFile] Using WIJ file: /intersystems/mgr/IRIS.WIJ
+06/26/23-08:02:17:903 (144145) 0 [Generic.Event] No journaling info from prior system
+06/26/23-08:02:17:903 (144145) 0 [WriteDaemon.CreatingNewWIJ] Creating a new WIJ file
+06/26/23-08:02:18:104 (144145) 0 [WriteDaemon.CreatedNewWIJ] New WIJ file created
+06/26/23-08:02:18:110 (144145) 0 [Generic.Event]
+Startup of InterSystems IRIS [IRIS for UNIX (Red Hat Enterprise Linux 8 for x86-64) 2023.1 (Build 235.1) Fri Jun 2 2023 13:26:47 EDT]
+        in ../bin/
+        with mgr: /intersystems/mgr
+        with wij: /intersystems/mgr/IRIS.WIJ
+        from: /intersystems/mgr/
+  OS=[Linux], version=[#1 SMP Fri Sep 30 11:45:06 EDT 2022], release=[4.18.0-425.3.1.el8.x86_64], machine=[x86_64]
+  nodename=[rhel8].
+  numasyncwijbuf: 2, wdwrite_asyncio_max: 64, wijdirectio: on, synctype: 3
+  System Initialized.
+06/26/23-08:02:18:136 (144182) 0 [WriteDaemon.Started] Write daemon started.
+06/26/23-08:02:18:281 (144200) 0 [Database.MountedRW] Mounted database /intersystems/mgr/ (SFN 0) read-write.
+06/26/23-08:02:18:317 (144207) 0 [Utility.Event] Starting ^INSTALL procedure
+""".strip()
+
+
 def test_iris_list():
     iris_running = iris.IrisList(context_wrap(IRIS_RUNNING))
     assert iris_running['name'] == "IRIS"
@@ -52,10 +114,24 @@ def test_fail():
     assert "The result is empty" in str(e)
 
 
+def test_iris_cpf():
+    iris_cpf = iris.IrisCpf(context_wrap(IRIS_CPF))
+    assert "Databases" in iris_cpf
+    assert iris_cpf.get("Databases", "IRISSYS") == "/intersystems/mgr/"
+    assert iris_cpf.get("SqlSysDatatypes", "DEC") == "%Library.Numeric(MAXVAL=999999999999999,MINVAL=-999999999999999,SCALE=0)"
+
+
+def test_iris_messages_log():
+    log = iris.IrisMessages(context_wrap(IRIS_MESSAGES_LOG))
+    assert len(log.get('Generic.Event')) == 4
+    assert len(log.get('shared memory')) == 1
+
+
 def test_doc_examples():
     env = {
-        'iris_info': iris.IrisList(
-            context_wrap(IRIS_RUNNING)),
+        'iris_info': iris.IrisList(context_wrap(IRIS_RUNNING)),
+        'iris_cpf': iris.IrisCpf(context_wrap(IRIS_CPF)),
+        'iris_log': iris.IrisMessages(context_wrap(IRIS_MESSAGES_LOG)),
     }
     failed, total = doctest.testmod(iris, globs=env)
     assert failed == 0
