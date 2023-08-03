cd60357f0bb7b934158a58cb8ab7ec6a2109ac7f
Xiangce Liu <xiangceliu@redhat.com>
2023-08-03 12:22:18 +0800

    [INSPEC-414] collect .exp.sed for analysis (#3869)
    - see INSPEC-414
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 9d7bc9a4bb2868098b631857ccea20fa45c09349)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -292,6 +292,7 @@ class DefaultSpecs(Specs):
     init_process_cgroup = simple_file("/proc/1/cgroup")
     initctl_lst = simple_command("/sbin/initctl --system list")
     insights_client_conf = simple_file('/etc/insights-client/insights-client.conf')
+    insights_client_exp_sed = simple_file('/etc/insights-client/.exp.sed')  # INSPEC-414
     installed_rpms = simple_command("/bin/rpm -qa --qf '%s'" % _rpm_format, context=HostContext, signum=signal.SIGTERM)
     interrupts = simple_file("/proc/interrupts")
     ip6tables = simple_command("/sbin/ip6tables-save")
