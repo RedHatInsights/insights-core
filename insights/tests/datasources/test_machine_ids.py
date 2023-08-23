0fd49a1dcdb6863666d5c3100736211aa1a8e3ee
Xiangce Liu <xiangceliu@redhat.com>
2023-08-23 12:41:50 +0800

    fix: don't initial InsighsConfig in dup_machine_id_info
    - load it from broker instead
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/datasources/test_machine_ids.py
+++ insights/tests/datasources/test_machine_ids.py
@@ -1,24 +1,15 @@
 import json
-from mock.mock import patch
 import pytest
 
-from insights.tests import context_wrap
-from insights.core.spec_factory import DatasourceProvider
-from insights.specs.datasources.machine_ids import dup_machine_id_info
+from mock.mock import patch
+
+from insights.client.config import InsightsConfig
 from insights.core import filters
 from insights.core.exceptions import SkipComponent
+from insights.core.spec_factory import DatasourceProvider
 from insights.specs import Specs
-
-
-class MockInsightsConfig(object):
-    def __init__(self):
-        self.legacy_upload = False
-
-    def _load_config_file(self):
-        pass
-
-    def _load_env(self):
-        pass
+from insights.specs.datasources.machine_ids import dup_machine_id_info
+from insights.tests import context_wrap
 
 
 class MockInsightsConnection(object):
@@ -104,15 +95,21 @@ def setup_function(func):
         filters.add_filter(Specs.duplicate_machine_id, [])
 
 
-def mock_try_autoconfig(config):
-    return
-
-
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_duplicate():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
+    result = dup_machine_id_info(broker)
+    expected = DatasourceProvider(content=["dc194312-8cdd-4e75-8cf1-2094bfsfsdeff hostname1.compute.internal,hostname2.compute.internal"], relative_path='insights_commands/duplicate_machine_id_info')
+    assert expected.content == result.content
+    assert expected.relative_path == result.relative_path
+
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff']),
+        'client_config': InsightsConfig(legacy_upload=True)
+    }
     result = dup_machine_id_info(broker)
     expected = DatasourceProvider(content=["dc194312-8cdd-4e75-8cf1-2094bfsfsdeff hostname1.compute.internal,hostname2.compute.internal"], relative_path='insights_commands/duplicate_machine_id_info')
     assert expected.content == result.content
@@ -120,45 +117,50 @@ def test_duplicate():
 
 
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_non_duplicate():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bf45678'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bf45678']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
     with pytest.raises(SkipComponent):
         dup_machine_id_info(broker)
 
 
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_module_filters_empty():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
     with pytest.raises(SkipComponent):
         dup_machine_id_info(broker)
 
 
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_wrong_machine_id_content():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff', 'dc194312-8cdd-4e75-8cf1-2094bfsf45678'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsdeff', 'dc194312-8cdd-4e75-8cf1-2094bfsf45678']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
     with pytest.raises(SkipComponent):
         dup_machine_id_info(broker)
 
 
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_machine_id_not_in_filters():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsabc'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfsfsabc']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
     with pytest.raises(SkipComponent):
         dup_machine_id_info(broker)
 
 
 @patch('insights.specs.datasources.machine_ids.InsightsConnection', MockInsightsConnection)
-@patch('insights.specs.datasources.machine_ids.try_auto_configuration', mock_try_autoconfig)
-@patch('insights.specs.datasources.machine_ids.InsightsConfig', MockInsightsConfig)
 def test_api_result_not_in_json_format():
-    broker = {Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfwrong'])}
+    broker = {
+        Specs.machine_id: context_wrap(lines=['dc194312-8cdd-4e75-8cf1-2094bfwrong']),
+        'client_config': InsightsConfig(legacy_upload=False)
+    }
     with pytest.raises(SkipComponent):
         dup_machine_id_info(broker)
