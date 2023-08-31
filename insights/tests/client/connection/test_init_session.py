d0863d2840b61c53b628f25672fffad53b8a9636
Stephen <stephen.adams@redhat.com>
2023-08-31 01:59:47 -0400

    refactor(client): remove hacky proxy code (#3730)
    Requests and urllib3 now support proxy authentication natively so we
    don't need this proxy hack stuff anymore. Instead we can just directly
    read in the proxy authentication.
    
    The auth split from the url is left in so that debu messages can still
    work without printing the authentication to the console.
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>
    (cherry picked from commit 30b889adcf5332581c0352cf0661b683fef373a0)

--- insights/tests/client/connection/test_init_session.py
+++ insights/tests/client/connection/test_init_session.py
@@ -3,35 +3,6 @@ from mock.mock import Mock, patch
 from os import environ as os_environ
 
 
-@patch("insights.client.connection.requests.Session")
-@patch("insights.client.connection.InsightsConnection.__init__", return_value=None)
-def test_proxy_request(init, session):
-    """
-    If proxy authentication is enabled, the failing hacky request is issued with correct timeout set.
-    """
-    config = Mock()
-
-    # The constructor is bypassed, because it itself runs the _init_session method.
-    connection = InsightsConnection(None)
-    connection.config = config
-    connection.systemid = None
-    connection.authmethod = "BASIC"
-    connection.username = "some username"
-    connection.password = "some password"
-    connection.cert_verify = None
-    connection.proxies = {"https": "some proxy"}
-    connection.proxy_auth = True
-    connection.base_url = 'https://cert-api.access.redhat.com/r/insights'
-
-    connection._init_session()
-
-    session.return_value.request.assert_called_once_with(
-        "GET",
-        "https://cert-api.access.redhat.com/r/insights",
-        timeout=config.http_timeout
-    )
-
-
 def test_get_proxy_env():
     """
     Test geting proxy configuration through environment, with and without no_proxy setting.
