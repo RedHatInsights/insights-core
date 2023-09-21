0ed7d46f9efe47d4f800b462458ebdf527afe2b1
Xiaoxue Wang <xiaoxwan@redhat.com>
2023-09-21 17:13:58 +0800

    do the table header seeking along with parse_fixed_table for efficiency
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/parsers/yum.py
+++ insights/parsers/yum.py
@@ -12,7 +12,7 @@ YumRepoList - command ``yum -C --noplugins repolist``
 from insights.core import CommandParser
 from insights.core.exceptions import ParseException, SkipComponent
 from insights.core.plugins import parser
-from insights.parsers import parse_fixed_table, calc_offset
+from insights.parsers import parse_fixed_table
 from insights.specs import Specs
 
 eus = [
@@ -152,27 +152,28 @@ class YumRepoList(CommandParser):
         self.data = []
         self.repos = {}
 
-        # Tell if the table header is in other language, default to english.
-        header_repo_id, header_repo_name = self._repo_headers[0]
-        for repo_id, repo_name in self._repo_headers:
+        # Header table language defaults to english: self._repo_headers[0]
+        # Give it a try for table header in other languages when the default one is not found
+        error_raised_for_stander_header_repo_id = None
+        for header_pair_index, (header_repo_id, header_repo_name) in enumerate(self._repo_headers):
             try:
-                first_line = calc_offset(content, [repo_id])
-                header_repo_id, header_repo_name = repo_id, repo_name
+                self.data = parse_fixed_table(
+                        content,
+                        heading_ignore=[header_repo_id],
+                        header_substitute=[(header_repo_id, 'id' + ' ' * (len(header_repo_id) - 2)),
+                                            (header_repo_name, 'name' + ' ' * (len(header_repo_name) - 4))],
+                        trailing_ignore=trailing_line_prefix,
+                        empty_exception=True)
                 break
             except ValueError as e:
-                print("meet e:" , e)
-
-        try:
-            self.data = parse_fixed_table(
-                    content,
-                    heading_ignore=[header_repo_id],
-                    header_substitute=[(header_repo_id, 'id' + ' '*(len(header_repo_id) - 2)),
-                                        (header_repo_name, 'name' + ' '*(len(header_repo_name) - 4))],
-                    trailing_ignore=trailing_line_prefix,
-                    empty_exception=True)
-        except ValueError as e:
-            # ValueError raised by parse_fixed_table
-            raise ParseException('Failed to parser yum repolist: {0}'.format(str(e)))
+                # ValueError raised by parse_fixed_table
+                # Likely be a unrecognized header_repo_id raised by calc_offset
+                if header_pair_index == 0:
+                    error_raised_for_stander_header_repo_id = e
+                # When the last pair of table header trying fails, end the parsing by raising a error
+                if header_pair_index == len(self._repo_headers) - 1:
+                    raise ParseException('Failed to parser yum repolist: {0}'.format(
+                                    str(error_raised_for_stander_header_repo_id or e)))
 
         if not self.data:
             raise SkipComponent('No repolist.')
