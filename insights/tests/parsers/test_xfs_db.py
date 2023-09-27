53889ad8fb928fee4404fef38505fa5b22335aea
Ping Qin <piqin@redhat.com>
2023-09-27 17:33:14 +0900

    feat: New spec and parser for xfs_db -r -c freesp command
    Signed-off-by: Ping Qin <piqin@redhat.com>

--- insights/tests/parsers/test_xfs_db.py
+++ insights/tests/parsers/test_xfs_db.py
@@ -1,6 +1,7 @@
 from insights.core.exceptions import SkipComponent
 from insights.parsers import xfs_db
 from insights.parsers.xfs_db import XFSDbFrag
+from insights.parsers.xfs_db import XFSDbFreesp
 from insights.tests import context_wrap
 import doctest
 import pytest
@@ -27,6 +28,24 @@ invalid numrecs (4716) in bmapbtd block
 invalid numrecs (58978) in bmapbtd block
 """.strip()
 
+XFS_DB_FREESP_1 = """
+   from      to extents  blocks    pct
+      1       1      16      16   0.01
+      2       3       1       2   0.00
+     64     127       1     103   0.04
+  32768   65536       4  231135  99.95
+""".strip()
+
+XFS_DB_FREESP_2 = """
+   from      to extents  blocks    pct
+""".strip()
+
+XFS_DB_FREESP_ERR = """
+   from      to extents  blocks
+      1       1      16      16
+      2       3       1       2
+""".strip()
+
 
 def test_xfs_db_frag():
     result = XFSDbFrag(context_wrap(XFS_DB_FRAG_1))
@@ -36,7 +55,7 @@ def test_xfs_db_frag():
     assert result.fragmentation_factor == "98.93%"
 
 
-def test_xfs_db_err():
+def test_xfs_db_frag_err():
     with pytest.raises(SkipComponent) as e:
         XFSDbFrag(context_wrap(XFS_DB_FRAG_ERR_1))
     assert "Invalid output of xfs fragmentation" in str(e)
@@ -46,9 +65,28 @@ def test_xfs_db_err():
     assert "Invalid output of xfs fragmentation" in str(e)
 
 
-def test_xfs_db_frag_doc_examples():
+def test_xfs_db_freesp():
+    result = XFSDbFreesp(context_wrap(XFS_DB_FREESP_1))
+    assert len(result.free_stat) == 4
+    assert result.free_stat[-1]['to'] == '65536'
+    search_result = result.search(pct="0.00")
+    assert len(search_result) == 1
+    assert search_result[0]['from'] == '2'
+
+    result = XFSDbFreesp(context_wrap(XFS_DB_FREESP_2))
+    assert result.free_stat == []
+
+
+def test_xfs_db_freesp_err():
+    with pytest.raises(SkipComponent) as e:
+        XFSDbFreesp(context_wrap(XFS_DB_FREESP_ERR))
+    assert "Invalid output of xfs free space" in str(e)
+
+
+def test_xfs_db_doc_examples():
     env = {
-        'xfs_db_frag': XFSDbFrag(context_wrap(XFS_DB_FRAG_1))
+        'xfs_db_frag': XFSDbFrag(context_wrap(XFS_DB_FRAG_1)),
+        'xfs_db_freesp': XFSDbFreesp(context_wrap(XFS_DB_FREESP_1))
     }
     failed, total = doctest.testmod(xfs_db, globs=env)
     assert failed == 0
