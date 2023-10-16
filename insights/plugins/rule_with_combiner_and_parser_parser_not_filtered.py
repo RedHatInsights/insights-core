26fff0056f8960925fe6af326fdee1392d390bbf
Xiangce Liu <xiangceliu@redhat.com>
2023-10-16 16:30:39 +0800

    add more details in docstring of test rules
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/plugins/rule_with_combiner_and_parser_parser_not_filtered.py
+++ insights/plugins/rule_with_combiner_and_parser_parser_not_filtered.py
@@ -2,6 +2,8 @@
 1. The rule uses one Combiner that depends upon a filterable Parser.
 2. The rule does also depend upon (import) the filterable Parser.
 3. The filterable Parser is NOT filtered in the rule.
+
+=> Exception is expected
 """
 from insights import rule, make_pass
 from insights.combiners.os_release import OSRelease
--- insights/plugins/rule_with_combiner_only.py
+++ insights/plugins/rule_with_combiner_only.py
@@ -1,7 +1,9 @@
 """
 1. The rule uses one Combiner that depends upon a filterable Parser.
 2. The rule does NOT depend upon (import) the filterable Parser directly.
-3. The filterable Parser is NOT filter in the rule as well.
+3. The filterable Parser is NOT filtered in the rule as well.
+
+=> Exception is NOT expected
 """
 from insights import rule, make_pass
 from insights.combiners.os_release import OSRelease
