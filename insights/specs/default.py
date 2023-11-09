3f86a9e9d5c0533b3660238c06093c887c8de22b
Xiangce Liu <xiangceliu@redhat.com>
2023-11-09 11:06:27 +0800

    remove more specs from RegistryPoints as no Parsers/Combiners/Rules use them
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -176,7 +176,7 @@ class DefaultSpecs(Specs):
     dmsetup_status = simple_command("/usr/sbin/dmsetup status")
     dnf_conf = simple_file("/etc/dnf/dnf.conf")
     dnf_modules = glob_file("/etc/dnf/modules.d/*.module")  # used by puptoo
-    dnf_module_list = simple_command("/usr/bin/dnf -C --noplugins module list")  # used by puptoo
+    dnf_module_list = simple_command("/usr/bin/dnf -C --noplugins module list", signum=signal.SIGTERM)  # used by puptoo
     docker_info = simple_command("/usr/bin/docker info")
     docker_list_containers = simple_command("/usr/bin/docker ps --all --no-trunc")
     docker_list_images = simple_command("/usr/bin/docker images --all --no-trunc --digests")
--- insights/specs/insights_archive.py
+++ insights/specs/insights_archive.py
@@ -75,6 +75,7 @@ class InsightsArchiveSpecs(Specs):
     fcoeadm_i = simple_file("insights_commands/fcoeadm_-i")
     findmnt_lo_propagation = simple_file("insights_commands/findmnt_-lo_PROPAGATION")
     firewall_cmd_list_all_zones = simple_file("insights_commands/firewall-cmd_--list-all-zones")
+    fw_devices = simple_file("insights_commands/fwupdagent_get-devices")
     fw_security = simple_file("insights_commands/fwupdagent_security_--force")
     gcp_instance_type = simple_file("insights_commands/python_-m_insights.tools.cat_--no-header_gcp_instance_type")
     gcp_license_codes = simple_file("insights_commands/python_-m_insights.tools.cat_--no-header_gcp_license_codes")
