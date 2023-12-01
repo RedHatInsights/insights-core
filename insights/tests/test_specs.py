231e92b7d73aeb3923f715fc314fee79328c2487
Xiangce Liu <xiangceliu@redhat.com>
2023-12-01 02:14:26 -0600

    fix: do not collect filterable specs when no filters (#3959)
    - add and refine tests
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/test_specs.py
+++ insights/tests/test_specs.py
@@ -4,6 +4,7 @@ import pytest
 import tempfile
 
 from insights.core import Parser, dr
+from insights.core import filters
 from insights.core.context import HostContext
 from insights.core.exceptions import ContentException
 from insights.core.filters import add_filter
@@ -43,7 +44,8 @@ class Specs(SpecSet):
     no_such_file = RegistryPoint()
     smpl_cmd = RegistryPoint()
     smpl_cmd_list_of_lists = RegistryPoint(filterable=True)
-    smpl_file = RegistryPoint(filterable=True)
+    smpl_file = RegistryPoint()
+    smpl_file_w_filter = RegistryPoint(filterable=True)
 
 
 class Stuff(Specs):
@@ -53,6 +55,7 @@ class Stuff(Specs):
     smpl_cmd = simple_command("/usr/bin/uptime")
     smpl_cmd_list_of_lists = simple_command("echo -n ' hello '")
     smpl_file = simple_file(this_file)
+    smpl_file_w_filter = simple_file(this_file)
 
 
 class stage(dr.ComponentType):
@@ -64,11 +67,19 @@ class stage(dr.ComponentType):
 #
 
 
-@stage(Stuff.smpl_file, Stuff.many, Stuff.smpl_cmd,
+def teardown_function(func):
+    filters._CACHE = {}
+
+    if func in [test_spec_factory, test_line_terminators]:
+        del filters.FILTERS[Stuff.smpl_file_w_filter]
+
+
+@stage(Stuff.smpl_file, Stuff.smpl_file_w_filter, Stuff.many, Stuff.smpl_cmd,
        Stuff.smpl_cmd_list_of_lists,
        optional=[Stuff.no_such_cmd, Stuff.no_such_file])
 def dostuff(broker):
     assert Stuff.smpl_file in broker
+    assert Stuff.smpl_file_w_filter in broker
     assert Stuff.many in broker
     assert Stuff.smpl_cmd in broker
     assert Stuff.smpl_cmd_list_of_lists in broker
@@ -78,6 +89,7 @@ def dostuff(broker):
 
 def test_spec_factory():
     add_filter(Stuff.smpl_cmd_list_of_lists, " hello ")
+    add_filter(Stuff.smpl_file_w_filter, "def test")
     hn = HostContext()
     broker = dr.Broker()
     broker[HostContext] = hn
@@ -97,13 +109,13 @@ def test_spec_factory():
 
 
 def test_line_terminators():
-    add_filter(Stuff.smpl_file, "def test")
+    add_filter(Stuff.smpl_file_w_filter, "def test")
     hn = HostContext()
     broker = dr.Broker()
     broker[HostContext] = hn
     broker = dr.run(dr.get_dependency_graph(dostuff), broker)
 
-    content = broker[Stuff.smpl_file].content
+    content = broker[Stuff.smpl_file_w_filter].content
     assert all("def test" in l for l in content), content
     assert not any(l.endswith("\n") for l in content)
 
@@ -128,3 +140,14 @@ def test_datasource_provider():
     p = MyParser(ds)
     assert p.content == data.splitlines()
     assert list(ds.stream()) == data.splitlines()
+
+
+def test_exp_no_filters():
+    hn = HostContext()
+    broker = dr.Broker()
+    broker[HostContext] = hn
+    broker = dr.run(dr.get_dependency_graph(dostuff), broker)
+    assert dostuff not in broker
+    for exp in broker.exceptions:
+        if "due to" in str(exp):
+            assert "smpl_file_w_filter" in str(exp)
