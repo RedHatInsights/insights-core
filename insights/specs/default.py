3483fa323d386ffeb77c1e5b9fcb2208d1c5591c
Xiangce Liu <xiangceliu@redhat.com>
2023-12-13 20:21:15 -0600

    INSPEC-432: collect convert2rhel.facts (#3972)
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit f5479075d961d45cea25638cb001cb83aa073da3)

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -139,6 +139,7 @@ class DefaultSpecs(Specs):
     cluster_conf = simple_file("/etc/cluster/cluster.conf")
     cmdline = simple_file("/proc/cmdline")
     cni_podman_bridge_conf = simple_file("/etc/cni/net.d/87-podman-bridge.conflist")
+    convert2rhel_facts = simple_file("/etc/rhsm/facts/convert2rhel.facts")
     corosync = simple_file("/etc/sysconfig/corosync")
     corosync_cmapctl = foreach_execute(corosync_ds.corosync_cmapctl_cmds, "%s")
     corosync_conf = simple_file("/etc/corosync/corosync.conf")
