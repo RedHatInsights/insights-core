99eef94165b89ceca0419980920b60bbc979786c
Xiangce Liu <xiangceliu@redhat.com>
2023-12-19 21:55:57 -0600

    chore: remove planned deprecations - 3.3.0 (#3978)
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/rpm_pkgs.py
+++ insights/parsers/rpm_pkgs.py
@@ -8,43 +8,6 @@ For more information, see the ``rpm_pkgs`` datasource.
 
 from insights import Parser, parser
 from insights.specs import Specs
-from insights.util import deprecated
-
-
-@parser(Specs.rpm_pkgs)
-class RpmPkgs(Parser):
-    """
-    .. warning::
-        This class is deprecated, please use
-        :py:class:`insights.parsers.rpm_pkgs.RpmPkgsWritable` instead.
-
-    Class for enabling the data from the ``rpm_pkgs`` datasource.
-
-    Sample input of this parser is::
-
-        ["httpd-core|httpd-core-2.4.53-7.el9.x86_64|Red Hat, Inc."]
-
-    Sample output of this parser is::
-
-        ["httpd-core"]
-
-    Examples:
-
-        >>> type(rpm_pkgs)
-        <class 'insights.parsers.rpm_pkgs.RpmPkgs'>
-        >>> rpm_pkgs.packages
-        ['httpd-core']
-    """
-    def __init__(self, *args, **kwargs):
-        deprecated(
-            RpmPkgs,
-            "Please use the `RpmPkgsWritable` instead.",
-            "3.3.0"
-        )
-        super(RpmPkgs, self).__init__(*args, **kwargs)
-
-    def parse_content(self, content):
-        self.packages = [pkg.split('|')[0] for pkg in content]
 
 
 @parser(Specs.rpm_pkgs)
--- insights/tests/parsers/test_multipath_conf.py
+++ insights/tests/parsers/test_multipath_conf.py
@@ -56,46 +56,6 @@ blacklist {
 """.strip()
 
 
-MULTIPATH_CONF_INFO3 = """
-defaults {
-       udev_dir                /dev
-       path_selector           "round-robin 0"
-       user_friendly_names     yes
-}
-
-multipaths {
-       multipath {
-               alias                   yellow
-               path_grouping_policy    multibus
-       }
-       multipath {
-               wwid                    1DEC_____321816758474
-               alias                   red
-       }
-}
-
-devices {
-       device {
-               path_selector           "round-robin 0"
-               no_path_retry           queue
-       }
-       device {
-               vendor                  "COMPAQ  "
-               path_grouping_policy    multibus
-       }
-       device {
-               vendor                  "COMPAQ  "
-               path_grouping_policy    multibus
-       }
-}
-
-blacklist {
-       wwid 26353900f02796769
-       devnode "^hd[a-z]"
-}
-
-""".strip()
-
 MULTIPATH_CONF_CASE_1 = """
 defaults {
     enable_foreign "" # line added by Leapp
@@ -135,24 +95,6 @@ Usage: lsinitrd [options] -k <kernel version>
 -v, --verbose               unpack verbosely.
 -k, --kver <kernel version> inspect the initramfs of <kernel version>.
 """
-
-
-def test_multipath_conf():
-    multipath_conf_info = multipath_conf.MultipathConf(context_wrap(MULTIPATH_CONF_INFO))
-    assert multipath_conf_info.get('defaults').get('udev_dir') == '/dev'
-    assert multipath_conf_info.get('defaults').get('path_selector') == 'round-robin 0'
-    assert multipath_conf_info.get('multipaths')[1].get('alias') == 'red'
-    assert multipath_conf_info.get('devices')[0].get('no_path_retry') == 'queue'
-    assert multipath_conf_info.get('blacklist').get('devnode') == '^hd[a-z]'
-
-
-def test_multipath_conf_initramfs():
-    multipath_conf_initramfs = multipath_conf.MultipathConfInitramfs(context_wrap(MULTIPATH_CONF_INFO))
-    assert multipath_conf_initramfs.get('defaults').get('udev_dir') == '/dev'
-    assert multipath_conf_initramfs.get('defaults').get('path_selector') == 'round-robin 0'
-    assert multipath_conf_initramfs.get('multipaths')[1].get('alias') == 'red'
-    assert multipath_conf_initramfs.get('devices')[0].get('no_path_retry') == 'queue'
-    assert multipath_conf_initramfs.get('blacklist').get('devnode') == '^hd[a-z]'
 
 
 def test_multipath_conf_tree():
@@ -214,12 +156,6 @@ def test_empty_multipath_conf_tree():
 
     with pytest.raises(SkipComponent) as e_info:
         multipath_conf.MultipathConfTreeInitramfs(context_wrap(INPUT_EMPTY))
-    assert "Empty content." in str(e_info.value)
-
-
-def test_empty_multipath_conf():
-    with pytest.raises(SkipComponent) as e_info:
-        multipath_conf.MultipathConfParser(context_wrap(INPUT_EMPTY))
     assert "Empty content." in str(e_info.value)
 
 
