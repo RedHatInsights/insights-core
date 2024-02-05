f0547fc22828068c3d221ec87229eaae19248533
Xiaoxue Wang <xiaoxwan@redhat.com>
2024-02-05 21:33:52 +0800

    Use foreach_execute based on insights-collect test
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/specs/datasources/mdadm.py
+++ insights/specs/datasources/mdadm.py
@@ -5,19 +5,19 @@ from insights.core.context import HostContext
 from insights.core.exceptions import SkipComponent
 from insights.core.plugins import datasource
 from insights.core.spec_factory import simple_command
+# from insights.parsers.mdstat import Mdstat
 from insights.specs import Specs
 
 
 class LocalSpecs(Specs):
     """ Local specs used only by raid_devices datasource. """
-    ls_dev_md = simple_command("ls /dev/md*")
+    ls_dev_md = simple_command("ls /dev/")
 
 
 @datasource(LocalSpecs.ls_dev_md, HostContext)
 def raid_devices(broker):
 
     content = broker[LocalSpecs.ls_dev_md].content
-    print(content)
     if content:
         if "No such file or directory" in content[0]:
             raise SkipComponent
@@ -25,10 +25,31 @@ def raid_devices(broker):
         devices = []
         for line in content:
             for dev in line.split():
-                if dev and dev.startswith("/dev/md"):
-                    devices.append(dev)
+                if dev and dev.startswith("md") and dev != "md":
+                    devices.append("/dev/" + dev)
 
         if devices:
-            return ' '.join(devices)
+            return devices
 
     raise SkipComponent
+
+
+# @datasource(Mdstat, HostContext)
+# def md_raid_arrays(broker):
+#     """
+#     Return the md RAID Array device names.
+
+#     Sample data returned::
+
+#         ["md1", "md2", "md3"]
+
+#     Returns:
+#         List[str]: Sorted list of md RAID Array device names.
+
+#     Raises:
+#         SkipComponent: Raised if no RAID Array name is available
+#     """
+#     md_raid_arrays = broker[Mdstat].mds.keys()
+#     if md_raid_arrays:
+#         return sorted(md_raid_arrays)
+#     raise SkipComponent
