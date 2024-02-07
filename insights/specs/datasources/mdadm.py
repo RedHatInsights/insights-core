bbfc1374843a9f3145405171c7d95d974d4bdc5a
Xiaoxue Wang <xiaoxwan@redhat.com>
2024-02-07 17:02:43 +0800

    Update parser MDAdmDetail accordingly
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/specs/datasources/mdadm.py
+++ insights/specs/datasources/mdadm.py
@@ -12,6 +12,7 @@ from insights.core.plugins import datasource
 
 @datasource(HostContext)
 def raid_devices(broker):
+    """ Glob extent "/dev/md*" to pass to ``mdadm_D`` Spec. """
     try:
         extended = glob.glob("/dev/md*")
         devices = [x for x in extended if stat.S_ISBLK(os.stat(x).st_mode)]
