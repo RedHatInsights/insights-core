bfd6b8e338b1098206387ac1060e6deacbb68a08
Xiangce Liu <xiangceliu@redhat.com>
2024-02-07 19:06:02 -0600

    feat: collect PCP RAW data per 'ros_collect' set in insights-client.conf (#3979)
    * feat: collect PCP RAW data per 'ros_collect' set in insights-client.conf
    
    - see INSGHTCORE-276
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * update per feedback
    
    - handle multiple hostname cases
    - return the pm_files that are complete
      and have the latest 'yesterday.meta.xz'
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/specs/datasources/pcp.py
+++ insights/specs/datasources/pcp.py
@@ -2,14 +2,16 @@
 Custom datasource related PCP (Performance Co-Pilot)
 """
 import datetime
+import glob
 import logging
 import os
 
 from insights.combiners.ps import Ps
 from insights.combiners.services import Services
 from insights.core.context import HostContext
-from insights.core.exceptions import SkipComponent
+from insights.core.exceptions import SkipComponent, ContentException
 from insights.core.plugins import datasource
+from insights.parsers.hostname import Hostname, HostnameDefault
 from insights.parsers.ros_config import RosConfig
 
 logger = logging.getLogger(__name__)
@@ -27,6 +29,21 @@ def pcp_enabled(broker):
     if not broker[Services].is_on("pmproxy"):
         raise SkipComponent("pmproxy not enabled")
     return True
+
+
+@datasource(HostContext)
+def ros_collect(broker):
+    """
+    Returns:
+        bool: True if "ros_collect=True" is set in "insights-client.conf"
+
+    Raises:
+        SkipComponent: When "ros_collect=True" is not set
+    """
+    insights_config = broker.get('client_config')
+    if insights_config and insights_config.ros_collect is True:
+        return True
+    raise SkipComponent
 
 
 @datasource(Ps, RosConfig, HostContext)
@@ -74,3 +91,48 @@ def pmlog_summary_args(broker):
         raise SkipComponent("Failed to get pmlogger metrics: {0}".format(str(e)))
 
     return "{0} {1}".format(pm_file, ' '.join(sorted(metrics)))
+
+
+@datasource(ros_collect, [Hostname, HostnameDefault], HostContext)
+def pcp_raw_files(broker):
+    """
+    Return all existing paths of PCP RAW data files of yesterday.
+
+    .. note::
+        The "hostname" in the file paths will be removed by the "save_as"
+        in the `pcp_raw_data` spec.  All the collected PCP RAW data files
+        will be stored in "var/log/pcp/pmlogger" directory in the archive.
+
+    Returns:
+        list(str): list of file path
+
+    Raises:
+        ContentException: Raises when no such directory or number of PCP RAW
+                          files less than 3.
+    """
+    hostnames = set()
+    hnd = broker.get(HostnameDefault)
+    hostnames.add(hnd.raw) if hnd else None
+    hn = broker.get(Hostname)
+    hostnames.update([hn.hostname, hn.fqdn]) if hn else None
+
+    pm_cand = []
+    yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+    for hostname in sorted(hostnames):
+        pm_root = os.path.join("/var/log/pcp/pmlogger/", hostname)
+        if os.path.exists(pm_root):
+            pm_files = glob.glob(os.path.join(pm_root, "{0}.*.xz".format(yesterday)))
+            pm_files.append(os.path.join(pm_root, "{0}.index".format(yesterday)))
+            pm_files = sorted(set(filter(lambda x: os.path.isfile(x), pm_files)))
+            if len([pf for pf in pm_files if pf.endswith(('.index', '.meta.xz', '.0.xz'))]) >= 3:
+                pm_cand.append((pm_files, os.path.getmtime(pm_files[-2])))  # timestamp of '.index'
+
+    if not pm_cand:
+        raise ContentException("No PCP data directory OR incomplete PCP RAW data")
+
+    # return the "pm_files" which:
+    # 1. has latest yesterday ".index"
+    # 2. has complete data (>=3)
+    # 3. has much more data than others
+    #    - a rare case: when multiple pm_files satisfy with both 1 and 2
+    return sorted(pm_cand, key=lambda x: (x[1], len(x[0])))[-1][0]
