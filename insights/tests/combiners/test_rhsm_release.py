7eeb078be7f6117c54dcbde90aa58902fc0f045e
Huanhuan Li <huali@redhat.com>
2024-02-20 11:20:30 +0800

    fixes: Update combiner "RhsmRelease"
    * Only set minor release when the parser has minor release set
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/tests/combiners/test_rhsm_release.py
+++ insights/tests/combiners/test_rhsm_release.py
@@ -11,14 +11,22 @@ RHSM_RELEASE = """
 {"releaseVer": "7.6"}
 """.strip()
 
+RHSM_RELEASE_EMPTY = """
+{"releaseVer": null}
+""".strip()
+
 SUBSCRIPTION_MANAGER_RELEASE = """
 Release: 7.2
 """.strip()
+
+EMPTY_SUBSCRIPTION_MANAGER_RELEASE = """Release not set"""
 
 RHUI_RELEASE = """8.6"""
 
 SM_PARSER = SubscriptionManagerReleaseShow(context_wrap(SUBSCRIPTION_MANAGER_RELEASE))
 RHSM_PARSER = RhsmReleaseVer(context_wrap(RHSM_RELEASE))
+RHSM_EMPTY_PARSER = RhsmReleaseVer(context_wrap(RHSM_RELEASE_EMPTY))
+SM_EMPTY_PARSER = SubscriptionManagerReleaseShow(context_wrap(EMPTY_SUBSCRIPTION_MANAGER_RELEASE))
 RHUI_PARSER = RHUISetRelease(context_wrap(RHUI_RELEASE))
 
 
@@ -54,6 +62,13 @@ def test_with_three():
     rhsm_release = RhsmRelease(RHSM_PARSER, SM_PARSER, RHUI_PARSER)
     assert rhsm_release.set == '7.6'
     assert rhsm_release.major == 7
+    assert rhsm_release.minor == 6
+
+
+def test_with_three_but_rhsm_is_empty():
+    rhsm_release = RhsmRelease(RHSM_EMPTY_PARSER, SM_EMPTY_PARSER, RHUI_PARSER)
+    assert rhsm_release.set == '8.6'
+    assert rhsm_release.major == 8
     assert rhsm_release.minor == 6
 
 
