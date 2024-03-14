59d2cd6499c8a89d636595d64a96710678f67f05
Andrea Waltlová <andywaltlova@gmail.com>
2024-03-14 03:04:19 +0100

    feat: collect LEAPP_* and CONVERT2RHEL_ env vars in migration-results (#4037)
    Add test with c2r migration results example
    
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- insights/tests/datasources/test_leapp.py
+++ insights/tests/datasources/test_leapp.py
@@ -31,7 +31,7 @@ LEAPP_REPORT_NG_1 = json.loads("""
 
 LEAPP_REPORT_NG_2 = MIGRATION_RESULTS_NG_2 = json.loads("{}")
 
-MIGRATION_RESULTS_OK = json.loads("""
+MIGRATION_RESULTS_OK_1 = json.loads("""
 {
    "activities": [
      {
@@ -133,17 +133,54 @@ MIGRATION_RESULTS_OK = json.loads("""
   ]
 }""".strip())
 
-MIGRATION_RESULTS_RET = """
+MIGRATION_RESULTS_OK_2 = json.loads("""
+{
+  "activities": [
+    {
+      "executed": "/usr/bin/convert2rhel analyze -y",
+      "run_id": "null",
+      "packages": [
+        {
+          "nevra": "0:convert2rhel-1.6.1-1.el7.noarch",
+          "signature": "RSA/SHA256, Wed Dec 13 19:27:28 2023, Key ID 199e2f91fd431d51"
+        }
+      ],
+      "target_os": "null",
+      "success": true,
+      "activity_ended": "2024-03-01T07:44:15.814899Z",
+      "version": "1",
+      "env": {},
+      "activity": "analysis",
+      "source_os": {
+        "version": "7.9",
+        "id": "Core",
+        "name": "CentOS Linux"
+      },
+      "activity_started": "2024-03-01T07:37:39.278478Z"
+    }
+  ]
+}""".strip())
+
+MIGRATION_RESULTS_RET_1 = """
 [
   {
     "activity": "preupgrade",
     "activity_ended": "2023-08-22T08:56:26.971009Z",
     "activity_started": "2023-08-22T08:55:47.798008Z",
     "env": {
-      "LEAPP_CURRENT_PHASE": "FactsCollection",
-      "LEAPP_EXPERIMENTAL": "0",
-      "LEAPP_NO_RHSM": "0",
-      "LEAPP_UNSUPPORTED": "0"
+         "LEAPP_COMMON_FILES": ":/etc/leapp/repos.d/system_upgrade/common/files:/etc/leapp/repos.d/system_upgrade/el8toel9/files",
+         "LEAPP_COMMON_TOOLS": ":/etc/leapp/repos.d/system_upgrade/common/tools:/etc/leapp/repos.d/system_upgrade/el8toel9/tools",
+         "LEAPP_CURRENT_PHASE": "FactsCollection",
+         "LEAPP_DEBUG": "0",
+         "LEAPP_EXECUTION_ID": "1edff870-626d-41ba-854c-8f9dc8f20dc3",
+         "LEAPP_EXPERIMENTAL": "0",
+         "LEAPP_HOSTNAME": "kvm-04-guest17.lab.eng.rdu2.redhat.com",
+         "LEAPP_IPU_IN_PROGRESS": "8to9",
+         "LEAPP_NO_RHSM": "0",
+         "LEAPP_UNSUPPORTED": "0",
+         "LEAPP_UPGRADE_PATH_FLAVOUR": "default",
+         "LEAPP_UPGRADE_PATH_TARGET_RELEASE": "9.0",
+         "LEAPP_VERBOSE": "0"
     },
     "run_id": "1edff870-626d-41ba-854c-8f9dc8f20dc3",
     "source_os": "Red Hat Enterprise Linux 8.6",
@@ -156,10 +193,19 @@ MIGRATION_RESULTS_RET = """
     "activity_ended": "2023-08-22T09:21:40.483395Z",
     "activity_started": "2023-08-22T09:13:50.876865Z",
     "env": {
-      "LEAPP_CURRENT_PHASE": "InterimPreparation",
-      "LEAPP_EXPERIMENTAL": "0",
-      "LEAPP_NO_RHSM": "0",
-      "LEAPP_UNSUPPORTED": "0"
+         "LEAPP_COMMON_FILES": ":/etc/leapp/repos.d/system_upgrade/common/files:/etc/leapp/repos.d/system_upgrade/el8toel9/files",
+         "LEAPP_COMMON_TOOLS": ":/etc/leapp/repos.d/system_upgrade/common/tools:/etc/leapp/repos.d/system_upgrade/el8toel9/tools",
+         "LEAPP_CURRENT_PHASE": "InterimPreparation",
+         "LEAPP_DEBUG": "0",
+         "LEAPP_EXECUTION_ID": "b3baa3f4-de86-4c4b-a7f3-bbe271207efd",
+         "LEAPP_EXPERIMENTAL": "0",
+         "LEAPP_HOSTNAME": "kvm-04-guest17.lab.eng.rdu2.redhat.com",
+         "LEAPP_IPU_IN_PROGRESS": "8to9",
+         "LEAPP_NO_RHSM": "0",
+         "LEAPP_UNSUPPORTED": "0",
+         "LEAPP_UPGRADE_PATH_FLAVOUR": "default",
+         "LEAPP_UPGRADE_PATH_TARGET_RELEASE": "9.0",
+         "LEAPP_VERBOSE": "0"
     },
     "run_id": "b3baa3f4-de86-4c4b-a7f3-bbe271207efd",
     "source_os": "Red Hat Enterprise Linux 8.6",
@@ -169,6 +215,27 @@ MIGRATION_RESULTS_RET = """
   }
 ]
 """.strip()
+
+MIGRATION_RESULTS_RET_2 = """
+[
+  {
+      "activity": "analysis",
+      "activity_ended": "2024-03-01T07:44:15.814899Z",
+      "activity_started": "2024-03-01T07:37:39.278478Z",
+      "env": {},
+      "run_id": "null",
+      "source_os": {
+          "version": "7.9",
+          "id": "Core",
+          "name": "CentOS Linux"
+      },
+      "success": true,
+      "target_os": "null",
+      "version": "1"
+  }
+]
+""".strip()
+
 
 MIGRATION_RESULTS_NG_1 = json.loads("""
 { "activities":[
@@ -215,13 +282,24 @@ def test_leapp_report_no_file(isfile):
         leapp_report({})
 
 
-@patch("json.load", return_value=MIGRATION_RESULTS_OK)
+@patch("json.load", return_value=MIGRATION_RESULTS_OK_1)
 @patch("os.path.isfile", return_value=True)
 @patch(builtin_open)
 def test_leapp_migration_results_ok(m_open, m_isfile, m_load):
     result = migration_results({})
     result_json = json.loads(''.join(result.content).strip())
-    expected_json = json.loads(''.join(MIGRATION_RESULTS_RET).strip())
+    expected_json = json.loads(''.join(MIGRATION_RESULTS_RET_1).strip())
+    for ret in result_json:
+        assert ret in expected_json
+
+
+@patch("json.load", return_value=MIGRATION_RESULTS_OK_2)
+@patch("os.path.isfile", return_value=True)
+@patch(builtin_open)
+def test_c2r_migration_results_ok(m_open, m_isfile, m_load):
+    result = migration_results({})
+    result_json = json.loads(''.join(result.content).strip())
+    expected_json = json.loads(''.join(MIGRATION_RESULTS_RET_2).strip())
     for ret in result_json:
         assert ret in expected_json
 
