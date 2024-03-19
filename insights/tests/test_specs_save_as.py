cb101213449bc27dd8e554f4d986ef85138e40a9
Xiangce Liu <xiangceliu@redhat.com>
2024-03-19 15:58:10 +0800

    refactor: avoid passing duplicated mainfest to insights.collect()
    - the "client_config" (InsightsConfig) already contains the
      manifest read from the client tool, this change removes it
      from the argument list of insights.collect() and simplify
      the code
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/test_specs_save_as.py
+++ insights/tests/test_specs_save_as.py
@@ -171,11 +171,12 @@ def test_specs_save_as_collect(obfuscate):
     for pkg in manifest.get("plugins", {}).get("packages", []):
         dr.load_components(pkg, exclude=None)
 
-    conf = InsightsConfig(obfuscate=obfuscate, obfuscate_hostname=obfuscate)
+    conf = InsightsConfig(
+            obfuscate=obfuscate, obfuscate_hostname=obfuscate,
+            manifest=specs_save_as_manifest)
     arch = InsightsArchive(conf)
     arch.create_archive_dir()
     output_path, errors = collect.collect(
-            manifest=specs_save_as_manifest,
             tmp_path=arch.tmp_dir,
             archive_name=arch.archive_name,
             client_config=conf)
