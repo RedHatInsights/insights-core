812ed2f53f2ccef29514ea5560c3be8dcd8c3112
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2024-04-05 11:26:06 -0500

    feat: Enhance luksmeta parser
    * It was not clear how to determine the number of key slots since the dictionary contained other keys. This PR adds new attributes to enable accessing the device uuid and a list of the key slot IDs.
    * The existing dictionary key for device_uuid was left in place for backward compatibility.
    * Testing was enhanced for new attributes
    * Some minor cleanup
    * Related to TYCHE-1036
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>

--- insights/tests/parsers/test_luksmeta.py
+++ insights/tests/parsers/test_luksmeta.py
@@ -5,7 +5,7 @@ from insights import SkipComponent
 from insights.parsers import luksmeta
 from insights.tests import context_wrap
 
-
+LUKSMETA_FILE_PATH = "/insights_commands/luksmeta_show_-d_.dev.disk.by-uuid.d62357eb-ea88-4b13-b756-a24e91fbfe9a"
 LUKSMETA_OUTPUT = """0   active empty
 1   active cb6e8904-81ff-40da-a84a-07ab9ab5715e
 2   active empty
@@ -22,7 +22,7 @@ LUKSMETA_BAD_DEVICE = "./luks2 (LUKS2) is not a LUKSv1 device"
 
 
 def test_luksmeta():
-    luksmeta_parsed = luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT, path="/insights_commands/cryptsetup_luksDump_--disable-external-tokens_.dev.disk.by-uuid.d62357eb-ea88-4b13-b756-a24e91fbfe9a"))
+    luksmeta_parsed = luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT, path=LUKSMETA_FILE_PATH))
 
     with pytest.raises(SkipComponent):
         luksmeta.LuksMeta(context_wrap(LUKSMETA_NOT_FOUND))
@@ -35,8 +35,10 @@ def test_luksmeta():
 
     # 8 keyslots and 1  device UUID
     assert len(luksmeta_parsed) == 9
+    assert luksmeta_parsed.key_slot_ids == [0, 1, 2, 3, 4, 5, 6, 7]
     assert "device_uuid" in luksmeta_parsed
     assert luksmeta_parsed["device_uuid"] == "d62357eb-ea88-4b13-b756-a24e91fbfe9a"
+    assert luksmeta_parsed.device_uuid == "d62357eb-ea88-4b13-b756-a24e91fbfe9a"
 
     for i in range(8):
         assert luksmeta_parsed[i].index == i
@@ -53,11 +55,13 @@ def test_luksmeta():
 
     luksmeta_parsed = luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT, path="/insights_commands/cryptsetup_luksDump_--disable-external-tokens_.dev.loop0"))
     assert "device_uuid" not in luksmeta_parsed
+    assert luksmeta_parsed.device_uuid is None
+    assert luksmeta_parsed.key_slot_ids == [0, 1, 2, 3, 4, 5, 6, 7]
 
 
 def test_doc_examples():
     env = {
-            'parsed_result': luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT)),
-          }
+        'parsed_result': luksmeta.LuksMeta(context_wrap(LUKSMETA_OUTPUT, path=LUKSMETA_FILE_PATH)),
+    }
     failed, total = doctest.testmod(luksmeta, globs=env)
     assert failed == 0
