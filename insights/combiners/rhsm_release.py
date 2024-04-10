56ac36c63ebfc74e50a52012c8ec77ff713c0334
Xiaoxue Wang <xiaoxwan@redhat.com>
2024-04-10 16:14:38 +0800

    fix: deprecated parser usage in combiner.rhsm_release
      Parser SubscriptionManagerReleaseShow was deprecated,
      remove it from combiner.rhsm_release accordingly.
    
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>

--- insights/combiners/rhsm_release.py
+++ insights/combiners/rhsm_release.py
@@ -4,19 +4,17 @@ Red Hat Subscription Manager Release
 
 Combiner provides the Red Hat Subscription Manager release information from
 the parsers :class:`insights.parsers.rhsm_releasever.RhsmReleaseVer`
-and :class:`insights.parsers.subscription_manager_release.SubscriptionManagerReleaseShow`
 and :class:`insights.parsers.rhui_release.RHUIReleaseVer`.
 """
 from insights.core.plugins import combiner
 from insights.parsers.rhsm_releasever import RhsmReleaseVer
-from insights.parsers.subscription_manager_release import SubscriptionManagerReleaseShow
 from insights.parsers.rhui_release import RHUIReleaseVer
 
 
-@combiner([RhsmReleaseVer, SubscriptionManagerReleaseShow, RHUIReleaseVer])
+@combiner([RhsmReleaseVer, RHUIReleaseVer])
 class RhsmRelease(object):
     """
-    Combiner for parsers RhsmReleaseVer and SubscriptionManagerReleaseShow and RHUIReleaseVer.
+    Combiner for parsers RhsmReleaseVer and RHUIReleaseVer.
 
     Examples:
         >>> type(rhsm_release)
@@ -28,7 +26,7 @@ class RhsmRelease(object):
         >>> rhsm_release.minor
         6
     """
-    def __init__(self, rhsm_release, sm_release, rhui_release):
+    def __init__(self, rhsm_release, rhui_release):
         self.set = None
         """ str: Release version string returned from the parsers """
 
@@ -37,15 +35,12 @@ class RhsmRelease(object):
 
         self.minor = None
         """ int: Minor version of the release """
+
         if rhsm_release is not None and rhsm_release.set:
             self.set = rhsm_release.set
             self.major = rhsm_release.major
             self.minor = rhsm_release.minor
 
-        elif sm_release is not None and sm_release.set:
-            self.set = sm_release.set
-            self.major = sm_release.major
-            self.minor = sm_release.minor
         elif rhui_release is not None and rhui_release.set:
             self.set = rhui_release.set
             self.major = rhui_release.major
