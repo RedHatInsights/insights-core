7668ff6afdd91b420f13cbc2626735b4cf7b561a
Xinting Li <xintli@Xintings-MacBook-Pro.local>
2024-04-24 09:31:19 +0800

    Update doc string
    Signed-off-by: Xinting Li <xintli@Xintings-MacBook-Pro.local>

--- insights/combiners/sudoers.py
+++ insights/combiners/sudoers.py
@@ -19,6 +19,9 @@ class Sudoers(SudoersBase):
             ``/etc/sudoers.d/*`` files. The order of lines keeps their original
             order them in files. And the files are read with `"filename`"
             alphabetical order.
+        data(dict): The dict of RAW lines of ``/etc/sudoers`` and each file under
+            ``/etc/sudoers.d/``, The keys are the filename and values are RAW lines
+             of each file.
 
     .. note::
         1. If there is not `"#includedir /etc/sudoers.d"` line in the entry
