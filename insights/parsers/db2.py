791591b81ccdbb01e51d1538e54327d16be91644
jiazhang <jiazhang@redhat.com>
2024-05-06 17:36:14 +0800

    Update format
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/parsers/db2.py
+++ insights/parsers/db2.py
@@ -4,13 +4,13 @@ db2 - db2 database information
 Parsers included in this module are:
 
 Db2ls - command ``db2ls -a -c``
-===============================
+-------------------------------
 
 Db2DatabaseConfiguration - Command ``/usr/sbin/runuser -l  <user name>  -c 'db2 get database configuration for %s'``
-====================================================================================================================
+--------------------------------------------------------------------------------------------------------------------
 
 Db2DatabaseManager - Command ``/usr/sbin/runuser -l  <user name>  -c 'db2 get dbm cfg'``
-========================================================================================
+----------------------------------------------------------------------------------------
 """
 
 from insights.core import CommandParser
