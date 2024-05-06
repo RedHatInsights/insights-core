efa46bd30728ca43c8e31514811cb4dff2bd6e4a
Xiangce Liu <xiangceliu@redhat.com>
2024-05-06 10:09:25 +0800

    chore: uname supports RHEL 9.4 (#4093)
    - https://access.redhat.com/articles/3078
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 27ec86dca7319fd877f981e43b69e765884bd057)

--- insights/parsers/uname.py
+++ insights/parsers/uname.py
@@ -121,6 +121,7 @@ rhel_release_map = {
     "5.14.0-162": "9.1",
     "5.14.0-284": "9.2",
     "5.14.0-362": "9.3",
+    "5.14.0-427": "9.4",
 }
 
 release_to_kernel_map = dict((v, k) for k, v in rhel_release_map.items())
