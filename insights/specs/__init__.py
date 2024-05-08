17f2248e1472707e5f443a8468673cf928a3a5a9
wushiqinlou <jiazhang@redhat.com>
2024-05-08 16:36:08 +0800

    feat: Add parser db2 commands (#4089)
    - Db2DatabaseConfiguration
    - Db2DatabaseManager
    
    Signed-off-by: jiazhang <jiazhang@redhat.com>

--- insights/specs/__init__.py
+++ insights/specs/__init__.py
@@ -126,6 +126,8 @@ class Specs(SpecSet):
     current_clocksource = RegistryPoint(no_obfuscate=['hostname', 'ip'])
     date = RegistryPoint(no_obfuscate=['hostname', 'ip'])
     date_utc = RegistryPoint(no_obfuscate=['hostname', 'ip'])
+    db2_database_configuration = RegistryPoint(multi_output=True, no_obfuscate=['hostname', 'ip'])
+    db2_database_manager = RegistryPoint(multi_output=True, no_obfuscate=['hostname', 'ip'])
     db2ls_a_c = RegistryPoint(no_obfuscate=['hostname', 'ip'])
     dcbtool_gc_dcb = RegistryPoint(multi_output=True)
     designate_conf = RegistryPoint(filterable=True)
