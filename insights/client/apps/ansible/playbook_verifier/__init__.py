e0423fc6b777ebe46ae5b02bf1e11abc74ac3d70
mhorky <mhorky@redhat.com>
2024-06-05 12:50:51 +0200

    chore: playbook verifier: Simplify the logic and structure
    - Imports were grouped into logical sections.
    - Functions were renamed to more descriptive names.
    - normalize_snippet was moved up to the top of the file.
    - Exception descriptions were altered to better describe the error.
    - Inline type annotations were added.
    - The deepcopy of snippet is performed when needed in
      exclude_dynamic_elements, not in verify_playbook_snippet.
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    (cherry picked from commit 459585b0370ee7a9421906b7089c05c61c7c54b4)

--- insights/client/apps/ansible/playbook_verifier/__init__.py
+++ insights/client/apps/ansible/playbook_verifier/__init__.py
@@ -1,25 +1,22 @@
-import os
-import six
-import copy
 import base64
-import tempfile
-import pkgutil
+import copy
 import hashlib
+import logging
+import os
+import pkgutil
+import tempfile
+
+import six
+
 import insights.client.apps.ansible
-from logging import getLogger
 from insights.client.apps.ansible.playbook_verifier.contrib import gnupg
 from insights.client.apps.ansible.playbook_verifier.contrib.ruamel_yaml.ruamel import yaml
 from insights.client.apps.ansible.playbook_verifier.contrib.ruamel_yaml.ruamel.yaml.comments import CommentedMap, CommentedSeq
 from insights.client.apps.ansible.playbook_verifier.contrib.ruamel_yaml.ruamel.yaml.scalarint import ScalarInt
 from insights.client.constants import InsightsConstants as constants
 
+
 __all__ = ("load_playbook_yaml", "verify", "PlaybookVerificationError")
-
-SIGKEY = 'insights_signature'
-PUBLIC_KEY_PATH = pkgutil.get_data(insights.client.apps.ansible.__name__, 'playbook_verifier/public.gpg')    # Update this when we have the key generated
-EXCLUDABLE_VARIABLES = ['hosts', 'vars']
-
-logger = getLogger(__name__)
 
 yaml = yaml.YAML(typ='rt')
 yaml.indent(mapping=2, sequence=4, offset=2)
@@ -27,17 +24,17 @@ yaml.default_flow_style = False
 yaml.preserve_quotes = True
 yaml.width = 200
 
+PLAYBOOK_SIGNATURE_LABEL = 'insights_signature'
+PLAYBOOK_DYNAMIC_LABELS = ['hosts', 'vars']
+
+PUBLIC_KEY_PATH = pkgutil.get_data(insights.client.apps.ansible.__name__, 'playbook_verifier/public.gpg')
+
+logger = logging.getLogger(__name__)
+
 
 class PlaybookVerificationError(Exception):
-    """
-    Exception raised when playbook verification fails
-
-    Attributes:
-        playbook -- stringified playbook yaml from stdin
-        message -- explanation of why verification failed
-    """
-
-    def __init__(self, message="PLAYBOOK VALIDATION FAILED"):
+    """Exception raised when playbook verification fails."""
+    def __init__(self, message="Could not verify Ansible playbook"):
         self.message = message
         super(PlaybookVerificationError, self).__init__(self.message)
 
@@ -45,207 +42,24 @@ class PlaybookVerificationError(Exception):
         return self.message
 
 
-def decodeSignature(encodedSignature):
-    try:
-        decodedSignature = base64.b64decode(encodedSignature)
-        return decodedSignature
-    except:
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Error Decoding Signature')
-
-
-def hash_playbook_snippet(snippet):
-    """Create SHA256 hash of a playbook snippet.
-
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
-    :returns: Hashed snippet.
-    :rtype: bytes
-    """
-    sha = hashlib.sha256()
-    if six.PY2:
-        serialized = str(normalize_snippet(snippet)).encode("utf-8")
-    else:
-        serialized = str(snippet).encode("utf-8")
-
-    sha.update(serialized)
-    return sha.digest()
-
-
-def get_public_key(gpg):
-    """Import public GPG key.
-
-    :param gpg: Path to the GPG key.
-    :type gpg: gnupg.GPG
-    :returns: Import result
-    :rtype: ...
-    """
-    if not PUBLIC_KEY_PATH:
-        raise PlaybookVerificationError(message="PUBLIC KEY IMPORT ERROR: Public key file not found")
-
-    public_key = PUBLIC_KEY_PATH
-    import_results = gpg.import_keys(public_key)
-    if import_results.count < 1:
-        raise PlaybookVerificationError(message="PUBLIC KEY NOT IMPORTED: Public key import failed")
-
-    return import_results
-
-
-def exclude_dynamic_elements(snippet):
-    """Remove dynamic elements from the Ansible playbook snippet.
-
-    Some parts of the Ansible playbook are dynamic (e.g. host systems) and cannot be signed,
-    because the signature would have to be unique for every playbook that is generated.
-
-    This function removes these variable sections.
-
-    :returns: New playbook dictionary without the unwanted values.
-    :rtype: dict
-    """
-    if 'insights_signature_exclude' not in snippet['vars']:
-        raise PlaybookVerificationError(message='EXCLUDE MISSING: the insights_signature_exclude var does not exist.')
-
-    exclusions = snippet['vars']['insights_signature_exclude'].split(',')
-
-    for element in exclusions:
-        element = element.split('/')
-
-        # remove empty strings
-        element = [string for string in element if string != '']
-
-        if len(element) == 1 and element[0] in EXCLUDABLE_VARIABLES and element[0] in snippet.keys():
-            del snippet[element[0]]
-        elif len(element) == 2 and element[0] in EXCLUDABLE_VARIABLES:
-            try:
-                del snippet[element[0]][element[1]]
-            except:
-                raise PlaybookVerificationError(message='INVALID FIELD: the variable {0} defined in insights_signature_exclude does not exist.'.format(element))
-        else:
-            raise PlaybookVerificationError(message='INVALID EXCLUSION: the variable {0} is not a valid exclusion.'.format(element))
-
-    return snippet
-
-
-def execute_verification(snippet, encoded_signature):
-    """Use GPG to verify the snippet.
-
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
-    :param encoded_signature: The base64-encoded signature.
-    :type encoded_signature: str
-
-    :returns: Result of the GPG verification and a hash of the snippet.
-    :rtype: Tuple[..., bytes]
-    """
-    gpg = gnupg.GPG(gnupghome=constants.insights_core_lib_dir)
-    snippet_hash = hash_playbook_snippet(snippet)
-    decoded_signature = decodeSignature(encoded_signature)
-
-    # load public key
-    get_public_key(gpg)
-
-    fd, fn = tempfile.mkstemp()
-    os.write(fd, decoded_signature)
-    os.close(fd)
-
-    result = gpg.verify_data(fn, snippet_hash)
-    os.unlink(fn)
-
-    return result, snippet_hash
-
-
-def verify_playbook_snippet(snippet):
-    """Verify the signature in a playbook.
-
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
-
-    :returns: Result of the GPG verification and a hash of the snippet.
-    :rtype: Tuple[crypto.GPGCommandResult, bytes]
-    """
-    if 'vars' not in snippet.keys():
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Vars field not found')
-    elif snippet['vars'] is None:
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Empty vars field')
-    elif SIGKEY not in snippet['vars']:
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Signature not found')
-
-    encoded_signature = snippet['vars'][SIGKEY]
-    snippet_copy = copy.deepcopy(snippet)
-
-    snippet_copy = exclude_dynamic_elements(snippet_copy)
-
-    return execute_verification(snippet_copy, encoded_signature)
-
-
-def get_playbook_snippet_revocation_list():
-    """
-    Load the list of revoked playbook snippet hashes from the egg.
-
-    :returns: Revocation entries in a form of `name:hash`.
-    :rtype: dict[str, str]
-    """
-    try:
-        # Import revoked list yaml.  The yaml is structured as a list of lists, so we can reuse the playbook signing and
-        # verification code.  There will only ever be one list, so we just grab the first element...
-        revoked_playbooks = yaml.load(pkgutil.get_data('insights', 'revoked_playbooks.yaml'))[0]
-
-    except Exception:
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Error loading revocation list')
-
-    # verify the list signature!
-    verified, snippet_hash = verify_playbook_snippet(revoked_playbooks)
-
-    if not verified:
-        raise PlaybookVerificationError(message='VERIFICATION FAILED: Revocation list signature invalid')
-
-    revocation_list = revoked_playbooks.get('revoked_playbooks', [])
-    return revocation_list
-
-
-def verify(playbook, skip_verify=False):
-    """
-    Verify the signed playbook.
-
-    :param playbook: Unverified playbook.
-    :type playbook: list[dict]
-    :returns: "verified" playbook
-    :rtype: dict
-    :raises PlaybookVerificationError:
-    """
-    logger.info('Playbook Verification has started')
-
-    if not skip_verify:
-        if not playbook:
-            raise PlaybookVerificationError(message="PLAYBOOK VERIFICATION FAILURE: Playbook is empty")
-
-        revocation_list = get_playbook_snippet_revocation_list()
-
-        for snippet in playbook:
-            verified, snippet_hash = verify_playbook_snippet(snippet)
-
-            if not verified:
-                name = snippet.get('name', 'NAME UNAVAILABLE')
-                raise PlaybookVerificationError(message="SIGNATURE NOT VALID: Template [name: {0}] has invalid signature".format(name))
-
-            # check if snippet_hash is on the revoked list
-            for revokedItem in revocation_list:
-                if snippet_hash == bytearray.fromhex(revokedItem['hash']):
-                    raise PlaybookVerificationError(message="REVOKED PLAYBOOK: Template is on the revoked list [name: {0}]".format(revokedItem['name']))
-
-    logger.info('All templates successfully validated')
-    return playbook
-
-
 def load_playbook_yaml(playbook):
+    """Parse a string into a dictionary.
+
+    :param playbook: The Ansible playbook
+    :type playbook: str
+
+    :returns: The playbook dictionary
+    :rtype: list[dict]
+
+    :raises PlaybookVerificationError: The input is empty or corrupted.
     """
-    Load playbook yaml using current yaml library implementation
-        output: playbook yaml
-    """
+    if not playbook:
+        raise PlaybookVerificationError("Cannot load a playbook from an empty input.")
+
     try:
-        playbook_yaml = yaml.load(playbook)
-        return playbook_yaml
+        return yaml.load(playbook)
     except Exception:
-        raise PlaybookVerificationError(message="PLAYBOOK VERIFICATION FAILURE: Failed to load playbook yaml because yaml is not valid")
+        raise PlaybookVerificationError("Could not load the playbook.")
 
 
 def normalize_snippet(snippet):
@@ -288,3 +102,211 @@ def normalize_snippet(snippet):
         result[key] = value
 
     return result
+
+
+def serialize_playbook_snippet(snippet):
+    """Convert the snippet object to bytes.
+
+    :param snippet: The (part of) Ansible playbook.
+    :type snippet: dict
+    :returns: Serialized snippet.
+    :rtype: bytes
+    """
+    if six.PY2:
+        return str(normalize_snippet(snippet)).encode("utf-8")
+    return str(snippet).encode("utf-8")
+
+
+def hash_playbook_snippet(serialized_snippet):
+    """Create SHA256 hash of a playbook snippet.
+
+    :param serialized_snippet: The (part of) Ansible playbook.
+    :type serialized_snippet: bytes
+    :returns: Hashed snippet.
+    :rtype: bytes
+    """
+    sha = hashlib.sha256()
+    sha.update(serialized_snippet)
+    return sha.digest()
+
+
+def get_public_key(gpg):
+    """Import public GPG key.
+
+    :param gpg: Path to the GPG key.
+    :type gpg: gnupg.GPG
+    :returns: Import result
+    :rtype: ...
+    """
+    if not PUBLIC_KEY_PATH:
+        raise PlaybookVerificationError(message="Public key file not found.")
+
+    public_key = PUBLIC_KEY_PATH
+    import_results = gpg.import_keys(public_key)
+    if import_results.count < 1:
+        raise PlaybookVerificationError(message="Public key import failed.")
+
+    return import_results
+
+
+def exclude_dynamic_elements(snippet):
+    """Remove dynamic elements from the Ansible playbook snippet.
+
+    Some parts of the Ansible playbook are dynamic (e.g. host systems) and cannot be signed,
+    because the signature would have to be unique for every playbook that is generated.
+
+    This function removes these variable sections.
+
+    :returns: New playbook dictionary without the unwanted values.
+    :rtype: dict
+
+    :raises PlaybookVerificationError:
+    """
+    if 'insights_signature_exclude' not in snippet.get('vars', {}):
+        raise PlaybookVerificationError(
+            "The playbook snippet does not have the key 'vars/insights_signature_exclude', "
+            "dynamic exclusion cannot be performed."
+        )
+
+    exclusions = snippet['vars']['insights_signature_exclude'].split(',')  # type: list[str]
+    result = copy.deepcopy(snippet)  # type: dict[str, ...]
+
+    for element in exclusions:
+        elements = [string for string in element.split("/") if string != '']  # type: list[str]
+
+        if len(elements) == 1 and elements[0] in PLAYBOOK_DYNAMIC_LABELS:
+            try:
+                del result[elements[0]]
+            except Exception:
+                raise PlaybookVerificationError(
+                    "Dynamic key '{key}' does not exist and cannot be excluded.".format(key=element)
+                )
+            continue
+
+        if len(elements) == 2 and elements[0] in PLAYBOOK_DYNAMIC_LABELS:
+            try:
+                del result[elements[0]][elements[1]]
+            except Exception:
+                raise PlaybookVerificationError(
+                    "Dynamic key '{key}' does not exist and cannot be excluded.".format(key=element)
+                )
+            continue
+
+        raise PlaybookVerificationError(
+            "Dynamic key '{key}' cannot be excluded from verification.".format(key=element)
+        )
+
+    return result
+
+
+def execute_verification(snippet, encoded_signature):
+    """Use GPG to verify the snippet.
+
+    :param snippet: The (part of) Ansible playbook.
+    :type snippet: dict
+    :param encoded_signature: The base64-encoded signature.
+    :type encoded_signature: str
+
+    :returns: Result of the GPG verification and a hash of the snippet.
+    :rtype: Tuple[..., bytes]
+    """
+    gpg = gnupg.GPG(gnupghome=constants.insights_core_lib_dir)
+    serialized_snippet = serialize_playbook_snippet(snippet)
+    snippet_hash = hash_playbook_snippet(serialized_snippet)
+    decoded_signature = base64.b64decode(encoded_signature)
+
+    # load public key
+    get_public_key(gpg)
+
+    fd, fn = tempfile.mkstemp()
+    os.write(fd, decoded_signature)
+    os.close(fd)
+
+    result = gpg.verify_data(fn, snippet_hash)
+    os.unlink(fn)
+
+    return result, snippet_hash
+
+
+def verify_playbook_snippet(snippet):
+    """Verify the signature in a playbook.
+
+    :param snippet: The (part of) Ansible playbook.
+    :type snippet: dict
+
+    :returns: Result of the GPG verification and a hash of the snippet.
+    :rtype: Tuple[..., bytes]
+    """
+    if not isinstance(snippet.get("vars", None), dict):
+        raise PlaybookVerificationError("The playbook snippet doesn't have a section 'vars'.")
+    if snippet.get("vars", {}).get(PLAYBOOK_SIGNATURE_LABEL, None) is None:
+        raise PlaybookVerificationError("The playbook snippet doesn't contain the Insights signature.")
+
+    cleaned_snippet = exclude_dynamic_elements(snippet)  # type: dict
+    encoded_signature = snippet["vars"][PLAYBOOK_SIGNATURE_LABEL]  # type: str
+
+    return execute_verification(cleaned_snippet, encoded_signature)
+
+
+def get_playbook_snippet_revocation_list():
+    """
+    Load the list of revoked playbook snippet hashes from the egg.
+
+    :returns: Revocation entries in a form of `name:hash`.
+    :rtype: dict[str, str]
+    """
+    try:
+        # We know the structure of the YAML will stay like this:
+        # > - name: revocation list
+        # >   timestamp: ...
+        # >   vars:
+        # >     insights_signature_exclude: ...
+        # >     insights_signature: !!binary | ...
+        # >   revoked_playbooks:
+        # >     - name: ...
+        # >       hash: ...
+        # >     - name: ...
+        # >       hash: ...
+        # There will never be more than one top-level object, we can safely do [0].
+        revoked_playbooks_yaml = pkgutil.get_data('insights', 'revoked_playbooks.yaml')  # type: bytes
+        revoked_playbooks = yaml.load(revoked_playbooks_yaml)[0]  # type: dict
+    except Exception:
+        raise PlaybookVerificationError("Could not load snippet revocation list.")
+
+    verified, snippet_hash = verify_playbook_snippet(revoked_playbooks)
+
+    if not verified:
+        raise PlaybookVerificationError("List of revocation signatures is invalid.")
+
+    revocation_list = revoked_playbooks.get("revoked_playbooks", [])  # type: list[str]
+    return revocation_list
+
+
+def verify(playbook):
+    """Verify the GPG-signed Ansible playbook.
+
+    :param playbook: Unverified playbook.
+    :type playbook: dict
+    :returns: Verified Ansible playbook.
+    :rtype: dict
+    :raises PlaybookVerificationError: An error occurred when trying to verify the playbook.
+    """
+    logger.info("Verifying Ansible playbook")
+
+    if not playbook:
+        raise PlaybookVerificationError("Empty playbooks cannot be verified.")
+
+    revocation_list = get_playbook_snippet_revocation_list()  # type: dict[str, str]
+
+    name = playbook.get("name", "NAME UNAVAILABLE")
+    verified, playbook_hash = verify_playbook_snippet(playbook)  # type: ..., str
+
+    if not verified:
+        raise PlaybookVerificationError(message="Template '{0}' has invalid signature".format(name))
+
+    for revoked_item in revocation_list:
+        if playbook_hash == bytearray.fromhex(revoked_item['hash']):
+            raise PlaybookVerificationError(message="Template '{0}' is on the revoked list.".format(name))
+
+    logger.info("All templates in the Ansible playbook were verified.")
+    return playbook
