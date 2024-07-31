6f68ef32aab3cc82411de1a96b1e05714f2184ec
mhorky <mhorky@redhat.com>
2024-07-31 10:07:14 +0200

    fix: Properly serialize playbook strings containing quote marks (#4175)
    * feat: Simplify Ansible playbook serialization
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    * test: Improve playbook verifier unit tests
    
    - Ensure playbook serialization tests pass in Python 2.7
    
      Dictionary insertion order didn't exist until Python 3.6. Since our
      code relies on it, we have to ensure the unit tests don't fail for no
      good reason.
    
    - Do not run the tests on Python 2.6.
    
    - Use public `serialize()` method to run the test, instead of private
      methods.
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    * fix: Properly serialize playbook strings containing quote marks
    
    * Card ID: CCT-647
    * Card ID: RHINENG-11692
    
    The string serialization should reflect what Python does.
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    ---------
    
    Signed-off-by: mhorky <mhorky@redhat.com>

--- insights/client/apps/ansible/playbook_verifier/serializer.py
+++ insights/client/apps/ansible/playbook_verifier/serializer.py
@@ -1,70 +1,85 @@
+import logging
+
+import six
 from insights.client.apps.ansible.playbook_verifier.contrib.ruamel_yaml.ruamel.yaml.comments import (
     CommentedMap,
     CommentedSeq,
 )
 
+logger = logging.getLogger(__name__)
+
 
 class PlaybookSerializer:
     @classmethod
-    def serialize(cls, source):
-        """Serialize snippet into string.
+    def serialize(cls, value):
+        """Serialize a playbook into a string.
 
-        :param source: Parsed playbook.
-        :type source: dict | yaml.comments.CommentedMap
         :returns: Serialized playbook.
         :rtype: str
         """
-        return cls._dict(source)
+        return cls._obj(value)
 
     @classmethod
-    def _dict(cls, source):
+    def _obj(cls, value):
         """
-        :type source: dict | yaml.comments.CommentedMap
+        :type value: any
         :rtype: str
         """
-        result_map = {}  # type: dict[str, str]
+        if isinstance(value, dict) or isinstance(value, CommentedMap):
+            return cls._dict(value)
+        if isinstance(value, list) or isinstance(value, CommentedSeq):
+            return cls._list(value)
+        if isinstance(value, int) or isinstance(value, float):
+            return str(value)
+        if isinstance(value, six.string_types):
+            return cls._str(value)
 
-        for key, value in source.items():
-            if isinstance(value, dict) or isinstance(value, CommentedMap):
-                result_map[key] = cls._dict(value)
-                continue
-            if isinstance(value, list) or isinstance(value, CommentedSeq):
-                result_map[key] = cls._list(value)
-                continue
-            if isinstance(value, int) or isinstance(value, float):
-                result_map[key] = str(value)
-                continue
-            result_map[key] = "'{value}'".format(value=value)
+        logger.debug("Value type not recognized, it may misbehave: {value} ({typ})".format(
+            value=value, typ=type(value).__name__)
+        )
+        return "'" + str(value) + "'"
 
+    @classmethod
+    def _str(cls, value):
+        """
+        :type value: str
+        :rtype: str
+        """
+        # no quote      'no quote'
+        # single'quote  "single'quote"
+        # double"quote  'double"quote'
+        # both"'quotes  'both"\'quotes'
+
+        quote = "'"
+        if "'" in value:
+            if '"' not in value:
+                quote = '"'
+            else:
+                value = value.replace("'", "\\'")
+
+        return quote + value + quote
+
+    @classmethod
+    def _dict(cls, value):
+        """
+        :type value: dict | yaml.comments.CommentedMap
+        :rtype: str
+        """
         result = "ordereddict(["
         result += ", ".join(
-            "('{key}', {value})".format(key=key, value=value)
-            for key, value in result_map.items()
+            "('{key}', {value})".format(key=k, value=cls._obj(v))
+            for k, v in value.items()
         )
         result += "])"
         return result
 
     @classmethod
-    def _list(cls, source):
+    def _list(cls, value):
         """
-        :type source: list | yaml.comments.CommentedSeq
+        :type value: list | yaml.comments.CommentedSeq
         :rtype: str
         """
-        result_list = []  # type: list[str]
-
-        for value in source:
-            if isinstance(value, list) or isinstance(value, CommentedSeq):
-                result_list.append(cls._list(value))
-                continue
-            if isinstance(value, dict) or isinstance(value, CommentedMap):
-                result_list.append(cls._dict(value))
-                continue
-            if isinstance(value, int) or isinstance(value, float):
-                result_list.append(str(value))
-                continue
-            result_list.append("'{value}'".format(value=value))
-
         result = "["
-        result += ", ".join(value for value in result_list)
+        result += ", ".join(cls._obj(v) for v in value)
         result += "]"
         return result
