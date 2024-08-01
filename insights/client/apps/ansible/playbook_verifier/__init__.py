838578c137f49ac59254affb7a5ca6e4be0a08aa
mhorky <mhorky@redhat.com>
2024-08-01 05:07:09 +0200

    feat: Iterate over all plays in a playbook (#4169)
    * chore: Rename "playbook snippet" to "play"
    
    Playbook contains plays, not snippets. We should be consistent with
    Ansible naming.
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    * fix: Correct type hints of playbook verifier
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    * fix: Verify all plays of a playbook
    
    * Card ID: CCT-643
    
    Ensure all plays of a playbook have passed GPG validation.
    
    Signed-off-by: mhorky <mhorky@redhat.com>
    
    ---------
    
    Signed-off-by: mhorky <mhorky@redhat.com>

--- insights/client/apps/ansible/playbook_verifier/__init__.py
+++ insights/client/apps/ansible/playbook_verifier/__init__.py
@@ -64,19 +64,19 @@ def load_playbook_yaml(playbook):
         raise PlaybookVerificationError("Could not load the playbook.")
 
 
-def normalize_snippet(snippet):
+def normalize_play_py2(play):
     """In Python 2, get rid of any default unicode values.
 
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict | yaml.comments.CommentedMap
+    :param play: The Ansible play.
+    :type play: dict | yaml.comments.CommentedMap
 
-    :returns: Snippet with any Unicode text converted to ASCII.
+    :returns: Play with Unicode characters removed.
     :rtype: CommentedMap
     """
     result = CommentedMap()
-    for key, value in snippet.iteritems():
+    for key, value in play.iteritems():
         if isinstance(value, CommentedMap):
-            result[key] = CommentedMap(normalize_snippet(value))
+            result[key] = CommentedMap(normalize_play_py2(value))
             continue
 
         if isinstance(value, CommentedSeq):
@@ -86,7 +86,7 @@ def normalize_snippet(snippet):
                     new_sequence.append(item.encode('ascii', 'ignore'))
                     continue
                 if isinstance(item, CommentedMap):
-                    new_sequence.append(normalize_snippet(item))
+                    new_sequence.append(normalize_play_py2(item))
                     continue
                 new_sequence.append(item)
 
@@ -106,31 +106,31 @@ def normalize_snippet(snippet):
     return result
 
 
-def serialize_playbook_snippet(snippet):
-    """Convert the snippet object to bytes.
+def serialize_play(play):
+    """Convert the play object to bytes.
 
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
-    :returns: Serialized snippet.
+    :param play: The Ansible play.
+    :type play: dict
+    :returns: Serialized play.
     :rtype: bytes
     """
     if six.PY2:
-        return str(normalize_snippet(snippet)).encode("utf-8")
+        return str(normalize_play_py2(play)).encode("utf-8")
     if sys.version_info < (3, 12):
-        return str(snippet).encode("utf-8")
-    return PlaybookSerializer.serialize(snippet).encode("utf-8")
+        return str(play).encode("utf-8")
+    return PlaybookSerializer.serialize(play).encode("utf-8")
 
 
-def hash_playbook_snippet(serialized_snippet):
-    """Create SHA256 hash of a playbook snippet.
+def hash_play(serialized_play):
+    """Create SHA256 hash of a play.
 
-    :param serialized_snippet: The (part of) Ansible playbook.
-    :type serialized_snippet: bytes
-    :returns: Hashed snippet.
+    :param serialized_play: The Ansible play.
+    :type serialized_play: bytes
+    :returns: Hashed play.
     :rtype: bytes
     """
     sha = hashlib.sha256()
-    sha.update(serialized_snippet)
+    sha.update(serialized_play)
     return sha.digest()
 
 
@@ -153,27 +153,27 @@ def get_public_key(gpg):
     return import_results
 
 
-def exclude_dynamic_elements(snippet):
-    """Remove dynamic elements from the Ansible playbook snippet.
+def exclude_dynamic_elements(play):
+    """Remove dynamic elements from the Ansible play.
 
-    Some parts of the Ansible playbook are dynamic (e.g. host systems) and cannot be signed,
-    because the signature would have to be unique for every playbook that is generated.
+    Some parts of the Ansible play are dynamic (e.g. host systems) and cannot be signed,
+    because the signature would have to be unique for every play that is generated.
 
     This function removes these variable sections.
 
-    :returns: New playbook dictionary without the unwanted values.
+    :returns: New play dictionary without the unwanted values.
     :rtype: dict
 
     :raises PlaybookVerificationError:
     """
-    if 'insights_signature_exclude' not in snippet.get('vars', {}):
+    if 'insights_signature_exclude' not in play.get('vars', {}):
         raise PlaybookVerificationError(
-            "The playbook snippet does not have the key 'vars/insights_signature_exclude', "
+            "Play does not have the key 'vars/insights_signature_exclude', "
             "dynamic exclusion cannot be performed."
         )
 
-    exclusions = snippet['vars']['insights_signature_exclude'].split(',')  # type: list[str]
-    result = copy.deepcopy(snippet)  # type: dict[str, ...]
+    exclusions = play['vars']['insights_signature_exclude'].split(',')  # type: list[str]
+    result = copy.deepcopy(play)  # type: dict[str, ...]
 
     for element in exclusions:
         elements = [string for string in element.split("/") if string != '']  # type: list[str]
@@ -203,20 +203,20 @@ def exclude_dynamic_elements(snippet):
     return result
 
 
-def execute_verification(snippet, encoded_signature):
-    """Use GPG to verify the snippet.
+def execute_verification(play, encoded_signature):
+    """Use GPG to verify the play.
 
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
+    :param play: The Ansible play.
+    :type play: dict
     :param encoded_signature: The base64-encoded signature.
     :type encoded_signature: str
 
-    :returns: Result of the GPG verification and a hash of the snippet.
+    :returns: Result of the GPG verification and a hash of the play.
     :rtype: Tuple[..., bytes]
     """
     gpg = gnupg.GPG(gnupghome=constants.insights_core_lib_dir)
-    serialized_snippet = serialize_playbook_snippet(snippet)
-    snippet_hash = hash_playbook_snippet(serialized_snippet)
+    serialized_play = serialize_play(play)
+    play_hash = hash_play(serialized_play)
     decoded_signature = base64.b64decode(encoded_signature)
 
     # load public key
@@ -226,40 +226,40 @@ def execute_verification(snippet, encoded_signature):
     os.write(fd, decoded_signature)
     os.close(fd)
 
-    result = gpg.verify_data(fn, snippet_hash)
+    result = gpg.verify_data(fn, play_hash)
     os.unlink(fn)
 
-    return result, snippet_hash
+    return result, play_hash
 
 
-def verify_playbook_snippet(snippet):
-    """Verify the signature in a playbook.
+def verify_play(play):
+    """Verify the signature in a play.
 
-    :param snippet: The (part of) Ansible playbook.
-    :type snippet: dict
+    :param play: The Ansible play.
+    :type play: dict
 
-    :returns: Result of the GPG verification and a hash of the snippet.
+    :returns: Result of the GPG verification and a hash of the play.
     :rtype: Tuple[..., bytes]
     """
-    if not isinstance(snippet.get("vars", None), dict):
-        raise PlaybookVerificationError("The playbook snippet doesn't have a section 'vars'.")
-    if snippet.get("vars", {}).get(PLAYBOOK_SIGNATURE_LABEL, None) is None:
-        raise PlaybookVerificationError("The playbook snippet doesn't contain the Insights signature.")
+    if not isinstance(play.get("vars", None), dict):
+        raise PlaybookVerificationError("Play doesn't have a section 'vars'.")
+    if play.get("vars", {}).get(PLAYBOOK_SIGNATURE_LABEL, None) is None:
+        raise PlaybookVerificationError("Play doesn't contain the Insights signature.")
 
-    cleaned_snippet = exclude_dynamic_elements(snippet)  # type: dict
-    encoded_signature = snippet["vars"][PLAYBOOK_SIGNATURE_LABEL]  # type: str
+    cleaned_play = exclude_dynamic_elements(play)  # type: dict
+    encoded_signature = play["vars"][PLAYBOOK_SIGNATURE_LABEL]  # type: str
 
-    return execute_verification(cleaned_snippet, encoded_signature)
+    return execute_verification(cleaned_play, encoded_signature)
 
 
-def get_playbook_snippet_revocation_list(revoked_playbooks_yaml):
+def get_play_revocation_list(revoked_plays_yaml):
     """
-    Load the list of revoked playbook snippet hashes from the egg.
+    Load the list of revoked play hashes from the egg.
 
-    :param revoked_playbooks_yaml: The YAML containing revoked playbooks.
-    :type revoked_playbooks_yaml: bytes
+    :param revoked_plays_yaml: The YAML containing hashes of revoked plays.
+    :type revoked_plays_yaml: bytes
     :returns: Revocation entries in a form of `name:hash`.
-    :rtype: dict[str, str]
+    :rtype: list[dict[str, str]]
     """
     try:
         # We know the structure of the YAML will stay like this:
@@ -274,45 +274,45 @@ def get_playbook_snippet_revocation_list(revoked_playbooks_yaml):
         # >     - name: ...
         # >       hash: ...
         # There will never be more than one top-level object, we can safely do [0].
-        revoked_playbooks = yaml.load(revoked_playbooks_yaml)[0]  # type: dict
+        revoked_plays = yaml.load(revoked_plays_yaml)[0]  # type: dict
     except Exception:
-        raise PlaybookVerificationError("Could not load snippet revocation list.")
+        raise PlaybookVerificationError("Could not load play revocation list.")
 
-    verified, snippet_hash = verify_playbook_snippet(revoked_playbooks)
+    verified, _ = verify_play(revoked_plays)
 
     if not verified:
         raise PlaybookVerificationError("List of revocation signatures is invalid.")
 
-    revocation_list = revoked_playbooks.get("revoked_playbooks", [])  # type: list[str]
+    revocation_list = revoked_plays.get("revoked_playbooks", [])  # type: list[str]
     return revocation_list
 
 
-def verify(playbook):
-    """Verify the GPG-signed Ansible playbook.
+def verify(play):
+    """Verify the GPG-signed Ansible play.
 
-    :param playbook: Unverified playbook.
-    :type playbook: dict
-    :returns: Verified Ansible playbook.
+    :param play: Unverified Ansible play.
+    :type play: dict
+    :returns: Verified Ansible play.
     :rtype: dict
-    :raises PlaybookVerificationError: An error occurred when trying to verify the playbook.
+    :raises PlaybookVerificationError: An error occurred when trying to verify the play.
     """
-    logger.info("Verifying Ansible playbook")
+    play_name = play.get("name", "unnamed")  # type: str
+    logger.info("Play '{name}' is being verified.".format(name=play_name))
 
-    if not playbook:
-        raise PlaybookVerificationError("Empty playbooks cannot be verified.")
+    if not play:
+        raise PlaybookVerificationError("Empty plays cannot be verified.")
 
     revocation_list_file_content = pkgutil.get_data('insights', 'revoked_playbooks.yaml')  # type: bytes
-    revocation_list = get_playbook_snippet_revocation_list(revocation_list_file_content)  # type: dict[str, str]
+    revocation_list = get_play_revocation_list(revocation_list_file_content)  # type: list[dict[str, str]]
 
-    name = playbook.get("name", "NAME UNAVAILABLE")
-    verified, playbook_hash = verify_playbook_snippet(playbook)  # type: ..., str
+    verified, play_hash = verify_play(play)  # type: ..., str
 
     if not verified:
-        raise PlaybookVerificationError(message="Template '{0}' has invalid signature".format(name))
+        raise PlaybookVerificationError(message="Play '{0}' has invalid signature".format(play_name))
 
     for revoked_item in revocation_list:
-        if playbook_hash == bytearray.fromhex(revoked_item['hash']):
-            raise PlaybookVerificationError(message="Template '{0}' is on the revoked list.".format(name))
+        if play_hash == bytearray.fromhex(revoked_item['hash']):
+            raise PlaybookVerificationError(message="Play '{0}' is on the revoked list.".format(play_name))
 
-    logger.info("All templates in the Ansible playbook were verified.")
-    return playbook
+    logger.info("Play '{name}' passed verification.".format(name=play_name))
+    return play
