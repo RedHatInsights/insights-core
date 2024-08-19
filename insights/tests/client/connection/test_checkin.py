aefa1e2841cc4d8168f001521cab6d29ef85dada
mhorky <mhorky@redhat.com>
2024-08-19 11:05:38 +0200

    chore: Improve and simplify logging
    * Card ID: CCT-705
    
    This patch will help us with identifying what is happening at given
    times.
    
    - Include phase, version and arguments on each phase load.
      Some of the information may not be necessary, as newer builds of
      insights-client contain logs that carry the information about phase
      and version. However, older builds do not, and these bits are valuable
      when debugging.
    
    - Include payloads, headers and file attachments in the log message, if
      present. This should make it easier to debug PATCH calls.
      BASIC authentication credentials are not included in these headers,
      the library adds them later when constructing the request.
    
    - Disable logging of HTTP responses for checkin. It is verbose and do
      not need to be logged in a file every time.
    
    Signed-off-by: mhorky <mhorky@redhat.com>

--- insights/tests/client/connection/test_checkin.py
+++ insights/tests/client/connection/test_checkin.py
@@ -47,7 +47,7 @@ def test_canonical_facts_request(get_proxies, post, get_canonical_facts, rm_conf
     expected_headers = {"Content-Type": "application/json"}
     expected_data = get_canonical_facts.return_value
     post.assert_called_once_with(
-        expected_url, headers=expected_headers, data=dumps(expected_data)
+        expected_url, headers=expected_headers, data=dumps(expected_data), log_response_text=False
     )
 
 
@@ -78,7 +78,7 @@ def test_canonical_facts_request_cleaned(get_proxies, post, get_canonical_facts,
     expected_data = get_canonical_facts.return_value
     expected_data = connection._clean_facts(expected_data)
     post.assert_called_once_with(
-        expected_url, headers=expected_headers, data=dumps(expected_data)
+        expected_url, headers=expected_headers, data=dumps(expected_data), log_response_text=False
     )
 
 
@@ -106,7 +106,7 @@ def test_insights_id_request(get_proxies, post, get_canonical_facts, generate_ma
     expected_headers = {"Content-Type": "application/json"}
     expected_data = {"insights_id": generate_machine_id.return_value}
     post.assert_called_once_with(
-        expected_url, headers=expected_headers, data=dumps(expected_data)
+        expected_url, headers=expected_headers, data=dumps(expected_data), log_response_text=False
     )
 
 
