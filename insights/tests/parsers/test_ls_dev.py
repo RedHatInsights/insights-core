ad7b2d114d495aea39dbdbf401f16f9451b2e62a
Rahul Sharma <rahushar@redhat.com>
2024-10-01 11:25:44 +0530

    fix: update ls specs, parser definition & test files to support new options
    Signed-off-by: Rahul Sharma <rahushar@redhat.com>

--- insights/tests/parsers/test_ls_dev.py
+++ insights/tests/parsers/test_ls_dev.py
@@ -33,37 +33,132 @@ lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 rhel-root -> ../dm-0
 lrwxrwxrwx.  1 0 0       7 Jul 25 10:00 rhel-swap -> ../dm-1
 """
 
+LS_ALZR_DEV = """
+/dev:
+total 0
+drwxr-xr-x. 19 root     root    system_u:object_r:device_t:s0                  3300 Sep 30 16:58 .
+dr-xr-xr-x. 18 root     root    system_u:object_r:root_t:s0                     235 Apr 21  2023 ..
+crw-r--r--.  1 root     root    system_u:object_r:autofs_device_t:s0        10, 235 Sep 30 16:58 autofs
+drwxr-xr-x.  2 root     root    system_u:object_r:device_t:s0                   180 Sep 30 16:58 block
+lrwxrwxrwx.  1 root     root    system_u:object_r:device_t:s0                    12 Sep 30 16:58 initctl -> /run/initctl
+brw-rw----.  1 root     disk    system_u:object_r:fixed_disk_device_t:s0   252,   0 Sep 30 16:58 vda
+brw-rw----.  1 root     disk    system_u:object_r:fixed_disk_device_t:s0   252,   1 Sep 30 16:58 vda1
+brw-rw----.  1 root     disk    system_u:object_r:fixed_disk_device_t:s0   252,   2 Sep 30 16:58 vda2
+
+/dev/mapper:
+total 0
+drwxr-xr-x.  2 root root system_u:object_r:device_t:s0          100 Sep 30 16:58 .
+drwxr-xr-x. 19 root root system_u:object_r:device_t:s0         3300 Sep 30 16:58 ..
+crw-------.  1 root root system_u:object_r:lvm_control_t:s0 10, 236 Sep 30 16:58 control
+lrwxrwxrwx.  1 root root system_u:object_r:device_t:s0            7 Sep 30 16:58 rhel-root -> ../dm-0
+lrwxrwxrwx.  1 root root system_u:object_r:device_t:s0            7 Sep 30 16:58 rhel-swap -> ../dm-1
+
+/dev/rhel:
+total 0
+drwxr-xr-x.  2 root root system_u:object_r:device_t:s0   80 Sep 30 16:58 .
+drwxr-xr-x. 19 root root system_u:object_r:device_t:s0 3300 Sep 30 16:58 ..
+lrwxrwxrwx.  1 root root system_u:object_r:device_t:s0    7 Sep 30 16:58 root -> ../dm-0
+lrwxrwxrwx.  1 root root system_u:object_r:device_t:s0    7 Sep 30 16:58 swap -> ../dm-1
+"""
+
 
 def test_ls_dev():
     lsdev = LsDev(context_wrap(LS_DEV))
     assert lsdev.listing_of("/dev/rhel") == {
-        'home': {'group': '0', 'name': 'home', 'links': 1, 'perms': 'rwxrwxrwx.',
-                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2',
-                 'owner': '0', 'link': '../dm-2', 'date': 'Jul 25 10:00',
-                 'type': 'l', 'size': 7, 'dir': '/dev/rhel'},
-        'root': {'group': '0', 'name': 'root', 'links': 1, 'perms': 'rwxrwxrwx.',
-                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 root -> ../dm-0',
-                 'owner': '0', 'link': '../dm-0', 'date': 'Jul 25 10:00',
-                 'type': 'l', 'size': 7, 'dir': '/dev/rhel'},
-        'swap': {'group': '0', 'name': 'swap', 'links': 1, 'perms': 'rwxrwxrwx.',
-                 'raw_entry': 'lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 swap -> ../dm-1',
-                 'owner': '0', 'link': '../dm-1', 'date': 'Jul 25 10:00',
-                 'type': 'l', 'size': 7, 'dir': '/dev/rhel'},
-        '..': {'group': '0', 'name': '..', 'links': 23, 'perms': 'rwxr-xr-x.',
-               'raw_entry': 'drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..',
-               'owner': '0', 'date': 'Jul 25 12:43', 'type': 'd', 'size': 3720,
-               'dir': '/dev/rhel'},
-        '.': {'group': '0', 'name': '.', 'links': 2, 'perms': 'rwxr-xr-x.',
-              'raw_entry': 'drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .', 'owner': '0',
-              'date': 'Jul 25 10:00', 'type': 'd', 'size': 100, 'dir': '/dev/rhel'}}
-    expected = ['docker-253:0-1443032-pool', 'rhel-home', 'rhel-root', 'rhel-swap']
-    actual = lsdev.listings.get("/dev/mapper")['files']
+        "home": {
+            "group": "0",
+            "name": "home",
+            "links": 1,
+            "perms": "rwxrwxrwx.",
+            "raw_entry": "lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 home -> ../dm-2",
+            "owner": "0",
+            "link": "../dm-2",
+            "date": "Jul 25 10:00",
+            "type": "l",
+            "size": 7,
+            "dir": "/dev/rhel",
+        },
+        "root": {
+            "group": "0",
+            "name": "root",
+            "links": 1,
+            "perms": "rwxrwxrwx.",
+            "raw_entry": "lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 root -> ../dm-0",
+            "owner": "0",
+            "link": "../dm-0",
+            "date": "Jul 25 10:00",
+            "type": "l",
+            "size": 7,
+            "dir": "/dev/rhel",
+        },
+        "swap": {
+            "group": "0",
+            "name": "swap",
+            "links": 1,
+            "perms": "rwxrwxrwx.",
+            "raw_entry": "lrwxrwxrwx.  1 0 0    7 Jul 25 10:00 swap -> ../dm-1",
+            "owner": "0",
+            "link": "../dm-1",
+            "date": "Jul 25 10:00",
+            "type": "l",
+            "size": 7,
+            "dir": "/dev/rhel",
+        },
+        "..": {
+            "group": "0",
+            "name": "..",
+            "links": 23,
+            "perms": "rwxr-xr-x.",
+            "raw_entry": "drwxr-xr-x. 23 0 0 3720 Jul 25 12:43 ..",
+            "owner": "0",
+            "date": "Jul 25 12:43",
+            "type": "d",
+            "size": 3720,
+            "dir": "/dev/rhel",
+        },
+        ".": {
+            "group": "0",
+            "name": ".",
+            "links": 2,
+            "perms": "rwxr-xr-x.",
+            "raw_entry": "drwxr-xr-x.  2 0 0  100 Jul 25 10:00 .",
+            "owner": "0",
+            "date": "Jul 25 10:00",
+            "type": "d",
+            "size": 100,
+            "dir": "/dev/rhel",
+        },
+    }
+    expected = ["docker-253:0-1443032-pool", "rhel-home", "rhel-root", "rhel-swap"]
+    actual = lsdev.files_of("/dev/mapper")
     assert actual == expected
 
-    assert lsdev.listings.get("/dev/mapper")['entries']['rhel-home']['link'] == "../dm-2"
+    assert lsdev.get("/dev/mapper")["entries"]["rhel-home"]["link"] == "../dm-2"
+
+    ls_alzr_dev = LsDev(context_wrap(LS_ALZR_DEV))
+    assert "/dev/rhel" in ls_alzr_dev
+    assert ls_alzr_dev.files_of("/dev") == ["initctl"]
+    assert ls_alzr_dev.get("/dev")["specials"] == ["autofs", "vda", "vda1", "vda2"]
+    assert ls_alzr_dev.get("/dev/rhel")["entries"].get("root") == {
+        "type": "l",
+        "perms": "rwxrwxrwx.",
+        "links": 1,
+        "owner": "root",
+        "group": "root",
+        "size": 7,
+        "se_user": "system_u",
+        "se_role": "object_r",
+        "se_type": "device_t",
+        "se_mls": "s0",
+        "name": "root",
+        "date": "Sep 30 16:58",
+        "link": "../dm-0",
+        "raw_entry": "lrwxrwxrwx.  1 root root system_u:object_r:device_t:s0    7 Sep 30 16:58 root -> ../dm-0",
+        "dir": "/dev/rhel",
+    }
 
 
 def test_doc_examples():
-    env = {'ls_dev': LsDev(context_wrap(LS_DEV))}
+    env = {"ls_dev": LsDev(context_wrap(LS_DEV))}
     failed, total = doctest.testmod(ls_dev, globs=env)
     assert failed == 0
--- insights/tests/parsers/test_ls_sys_firmware.py
+++ insights/tests/parsers/test_ls_sys_firmware.py
@@ -24,16 +24,57 @@ drwxr-xr-x. 2 0 0    0 Feb 10 15:54 interrupts
 drwxr-xr-x. 3 0 0    0 Dec 22 17:56 tables
 """
 
+LS_ALZR_SYS_FIRMWARE = """
+/sys/firmware:
+total 0
+drwxr-xr-x.  7 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 .
+dr-xr-xr-x. 12 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 ..
+drwxr-xr-x.  5 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 acpi
+drwxr-xr-x.  2 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 devicetree
+drwxr-xr-x.  4 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 dmi
+drwxr-xr-x.  3 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 efi
+-r--------.  1 root root system_u:object_r:sysfs_t:s0 657 Oct  1 10:17 fdt
+
+/sys/firmware/acpi:
+total 0
+drwxr-xr-x. 5 root root system_u:object_r:sysfs_t:s0    0 Sep 30 16:58 .
+drwxr-xr-x. 7 root root system_u:object_r:sysfs_t:s0    0 Sep 30 16:58 ..
+drwxr-xr-x. 2 root root system_u:object_r:sysfs_t:s0    0 Oct  1 10:17 bgrt
+drwxr-xr-x. 5 root root system_u:object_r:sysfs_t:s0    0 Oct  1 10:17 hotplug
+-r--r--r--. 1 root root system_u:object_r:sysfs_t:s0 4096 Oct  1 10:17 pm_profile
+drwxr-xr-x. 4 root root system_u:object_r:sysfs_t:s0    0 Sep 30 16:58 tables
+"""
+
 
 def test_ls_sys_firmware():
     ls = LsSysFirmware(context_wrap(LS_SYS_FIRMWARE))
     assert "acpi" not in ls
     assert "/sys/firmware/acpi" in ls
-    assert ls.dirs_of("/sys/firmware") == ['.', '..', 'acpi', 'dmi', 'memmap']
-    assert ls.files_of("/sys/firmware/acpi") == ['pm_profile']
+    assert ls.dirs_of("/sys/firmware") == [".", "..", "acpi", "dmi", "memmap"]
+    assert ls.files_of("/sys/firmware/acpi") == ["pm_profile"]
+
+    ls_alzr_sys_firmware = LsSysFirmware(context_wrap(LS_ALZR_SYS_FIRMWARE))
+    assert "/sys/firmware/acpi" in ls_alzr_sys_firmware
+    assert "/sys/firmware/dmi" not in ls_alzr_sys_firmware
+    assert ls_alzr_sys_firmware.get("/sys/firmware")["entries"].get("efi") == {
+        "type": "d",
+        "perms": "rwxr-xr-x.",
+        "links": 3,
+        "owner": "root",
+        "group": "root",
+        "size": 0,
+        "se_user": "system_u",
+        "se_role": "object_r",
+        "se_type": "sysfs_t",
+        "se_mls": "s0",
+        "name": "efi",
+        "date": "Sep 30 16:58",
+        "raw_entry": "drwxr-xr-x.  3 root root system_u:object_r:sysfs_t:s0   0 Sep 30 16:58 efi",
+        "dir": "/sys/firmware",
+    }
 
 
 def test_doc_examples():
-    env = {'ls_sys_firmware': LsSysFirmware(context_wrap(LS_SYS_FIRMWARE))}
+    env = {"ls_sys_firmware": LsSysFirmware(context_wrap(LS_SYS_FIRMWARE))}
     failed, total = doctest.testmod(ls_sys_firmware, globs=env)
     assert failed == 0
