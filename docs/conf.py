fb9433931761f44021a9f86e846d1a63490d4394
Xiangce Liu <xiangceliu@redhat.com>
2024-10-08 15:50:36 +0800

    doc(conf): remove definiation of html_theme_path (#4238)
    - as per the latest sphinx-rtd-theme, the definiation can be removed
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    rh-pre-commit.version: 2.3.1
    rh-pre-commit.check-secrets: ENABLED
    
    (cherry picked from commit 4fb9df2cb5ebca03fbfd0a62dfeac6c22e1f12ae)

--- docs/conf.py
+++ docs/conf.py
@@ -113,8 +113,6 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
