4f5ed9bfe012c3e8eedcb042ebc57a2080173923
Huanhuan Li <huali@redhat.com>
2024-10-15 10:14:43 +0800

    Refactor the code to make use of "insights.parsr.query" module
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- insights/parsers/nftables.py
+++ insights/parsers/nftables.py
@@ -8,13 +8,12 @@ NftListRuleSet - command ``nft -j list ruleset``
 ------------------------------------------------
 """
 
-from collections import defaultdict
-import json
-
 from insights.core import JSONParser
 from insights.core.dr import SkipComponent
 from insights.core.plugins import parser
 from insights.specs import Specs
+from insights.parsr import query
+from insights.parsr.query import make_child_query as q
 
 
 @parser(Specs.nft_list_ruleset)
@@ -71,98 +70,49 @@ class NftListRuleSet(JSONParser):
         }
 
     The json output is dict with a single "nftables" key, the value is a list of all data in nftables.
-    To make it easier to use, here it extracts the address_family/table/chain/rule structures and
-    save it to main_data attribute.
-
-    The main_data attribute sample::
-
-        {
-            'ip': {
-                'table1': {
-                    'chains': {
-                        'chain1': {
-                            'attrs': {
-                                "family": "ip", "table": "table1", "name": "chain1",
-                                "type": "filter", "hook": "input", "prio": 0, "policy": "accept"
-                            },
-                            'rules': [
-                                {
-                                    "family": "ip", "table": "table1", "chain": "chain1",
-                                    "expr": [
-                                        {"vmap": {"key": {"payload":
-                                        {"protocol": "ip", "field": "saddr"}},
-                                        "data": "@example_map"}}]
-                                },
-                                {
-                                    "family": "ip", "table": "table1", "chain": "chain1",
-                                    "expr": [
-                                        {"match": {"op": "==",
-                                        "left": {"payload": {"protocol": "tcp", "field": "dport"}},
-                                        "right": 22}}]
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        }
+    To make it easier to use, here it makes use of ``insights.parsr.query.Entry`` and transfer the
+    data to an Entry object and set it back to the data attribute.
 
     Attributes:
-        main_data (dict): A dict contains the address_family/table/chain/rule data.
+        data (object): An ``insights.parsr.query.Entry`` object containing all of the data as its
+            children.
 
     Raises:
-        SkipComponent: No available nftables.
+        SkipComponent: No tables available.
 
     Examples:
         >>> type(nft_obj)
         <class 'insights.parsers.nftables.NftListRuleSet'>
-        >>> [str(key) for key in nft_obj.main_data.keys()]  # to be compatible with python2.6/2.7, transform unicode to string
-        ['ip']
-        >>> [str(key) for key in nft_obj.main_data['ip'].keys()] # to be compatible with python2.6/2.7, transform unicode to string
+        >>> [str(item) for item in nft_obj.tables('ip')]   # change unicode to string to be compatible with python2.6/2.7
         ['table1']
-        >>> 'chain1' in nft_obj.chains('ip', 'table1')
-        True
-        >>> len(nft_obj.rules('ip', 'table1', 'chain1'))
+        >>> chains = nft_obj.chains('ip', 'table1')
+        >>> len(chains)
+        1
+        >>> str(chains[0]['name'].value)  # change unicode to string to be compatible with python2.6/2.7
+        'chain1'
+        >>> str(chains[0]['type'].value)  # change unicode to string to be compatible with python2.6/2.7
+        'filter'
+        >>> rules = nft_obj.rules('ip', 'table1', 'chain1')
+        >>> len(rules)
         2
-
+        >>> rules[1]['expr']['match']['right'].value
+        22
     """
 
     def parse_content(self, content):
-        """
-        There are a lot of other types in nftables besides "chain/rule", like "map", "set", "quota".
-        But they are not extracted now since there are no rules dependent on them. If needed,
-        we can extract them later.
-        """
         super(NftListRuleSet, self).parse_content(content)
-        self.main_data = defaultdict(dict)
-        for item in self.data['nftables']:
-            for key, value in item.items():
-                address_name = value.get('family')
-                table_name = value.get('table')
-                if key == 'table':
-                    table_name = value['name']
-                    self.main_data[address_name][table_name] = defaultdict(dict)
-                elif key == 'chain':
-                    chain_name = value['name']
-                    self.main_data[address_name][table_name]['chains'][chain_name] = {}
-                    self.main_data[address_name][table_name]['chains'][chain_name]['attrs'] = value
-                    self.main_data[address_name][table_name]['chains'][chain_name]['rules'] = []
-                elif key == 'rule':
-                    chain_name = value['chain']
-                    self.main_data[address_name][table_name]['chains'][chain_name]['rules'].append(value)
-        # transform to normal dict incase non-existing key are added when accessing it
-        self.main_data = json.loads(json.dumps(self.main_data))
-        if not self.main_data:
+        self.data = query.from_dict(self.data)
+        if not self.data['nftables']['table']:
             raise SkipComponent
 
     def tables(self, address_family):
-        """dict: Return the tables in some address family."""
-        return self.main_data.get(address_family)
+        """list: Return the table names in some address family."""
+        return [item['name'].value for item in self.data['nftables']['table'].where('family', address_family)]
 
     def chains(self, address_family, table_name):
-        """dict: Returns the chains in some table."""
-        return self.main_data.get(address_family, {}).get(table_name, {}).get('chains')
+        """list: Returns the chains in some table."""
+        return self.data['nftables']['chain'].where(q('family', address_family) & q('table', table_name))
 
     def rules(self, address_family, table_name, chain_name):
         """list: Returns the rules of some chain in some table."""
-        return self.main_data.get(address_family, {}).get(table_name, {}).get('chains', {}).get(chain_name, {}).get('rules')
+        return self.data['nftables']['rule'].where(q('family', address_family) & q('table', table_name) & q('chain', chain_name))
--- insights/tests/parsers/test_nftables.py
+++ insights/tests/parsers/test_nftables.py
@@ -36,8 +36,6 @@ def test_module_documentation():
 
 def test_nft_list_ruleset():
     nft_obj = NftListRuleSet(context_wrap(NTT_LIST_RULESET_OUTPU1))
-    assert len(nft_obj.main_data) == 1
-    assert 'ip' in nft_obj.main_data
 
     tables = nft_obj.tables('ip')
     assert len(tables) == 2
@@ -46,84 +44,85 @@ def test_nft_list_ruleset():
 
     chains = nft_obj.chains('ip', 'example_table')
     assert len(chains) == 1
-    assert 'example_chain' in chains
+    assert chains[0]['name'].value == 'example_chain'
 
-    assert chains['example_chain']['attrs']['type'] == 'filter'
-    assert chains['example_chain']['attrs']['hook'] == 'input'
-    assert chains['example_chain']['attrs']['prio'] == 0
-    assert chains['example_chain']['attrs']['policy'] == 'accept'
+    assert chains[0]['type'].value == 'filter'
+    assert chains[0]['hook'].value == 'input'
+    assert chains[0]['prio'].value == 0
+    assert chains[0]['policy'].value == 'accept'
 
     example_chain_rules = nft_obj.rules('ip', 'example_table', 'example_chain')
     assert len(example_chain_rules) == 1
     # {"rule": {"family": "ip", "table": "example_table", "chain": "example_chain", "handle": 3, "expr": [{"vmap": {"key": {"payload": {"protocol": "ip", "field": "saddr"}}, "data": "@example_map"}}]}}
     assert 'vmap' in example_chain_rules[0]['expr'][0]
-    assert example_chain_rules[0]['expr'][0]['vmap']['key']['payload']['protocol'] == 'ip'
-    assert example_chain_rules[0]['expr'][0]['vmap']['key']['payload']['field'] == 'saddr'
-    assert example_chain_rules[0]['expr'][0]['vmap']['data'] == '@example_map'
+    assert example_chain_rules[0]['expr'][0]['vmap']['key']['payload']['protocol'].value == 'ip'
+    assert example_chain_rules[0]['expr'][0]['vmap']['key']['payload']['field'].value == 'saddr'
+    assert example_chain_rules[0]['expr'][0]['vmap']['data'].value == '@example_map'
 
     svc_chains = nft_obj.chains('ip', 'nftables_svc')
     assert len(svc_chains) == 1
-    assert 'INPUT' in svc_chains
+    assert 'INPUT' == svc_chains[0]['name'].value
 
     nftables_svc_input_rules = nft_obj.rules('ip', 'nftables_svc', 'INPUT')
     assert len(nftables_svc_input_rules) == 3
     # {"rule": {"family": "ip", "table": "nftables_svc", "chain": "INPUT", "handle": 2, "expr": [{"match": {"op": "==", "left": {"payload": {"protocol": "tcp", "field": "dport"}}, "right": 22}}, {"accept": null}]}}
     first_rule = nftables_svc_input_rules[0]
-    assert first_rule['expr'][0]['match']['left']['payload']['protocol'] == 'tcp'
-    assert first_rule['expr'][0]['match']['left']['payload']['field'] == 'dport'
-    assert first_rule['expr'][0]['match']['op'] == '=='
-    assert first_rule['expr'][0]['match']['right'] == 22
-    assert first_rule['expr'][1]['accept'] is None
+    assert first_rule['expr'][0]['match']['left']['payload']['protocol'].value == 'tcp'
+    assert first_rule['expr'][0]['match']['left']['payload']['field'].value == 'dport'
+    assert first_rule['expr'][0]['match']['op'].value == '=='
+    assert first_rule['expr'][0]['match']['right'].value == 22
+    assert first_rule['expr'][1]['accept'].value is None
     # {"rule": {"family": "ip", "table": "nftables_svc", "chain": "INPUT", "handle": 3, "expr": [{"match": {"op": "==", "left": {"payload": {"protocol": "tcp", "field": "dport"}}, "right": 443}}, {"accept": null}]}}
     second_rule = nftables_svc_input_rules[1]
-    assert second_rule['expr'][0]['match']['left']['payload']['protocol'] == 'tcp'
-    assert second_rule['expr'][0]['match']['left']['payload']['field'] == 'dport'
-    assert second_rule['expr'][0]['match']['op'] == '=='
-    assert second_rule['expr'][0]['match']['right'] == 443
-    assert second_rule['expr'][1]['accept'] is None
+    assert second_rule['expr'][0]['match']['left']['payload']['protocol'].value == 'tcp'
+    assert second_rule['expr'][0]['match']['left']['payload']['field'].value == 'dport'
+    assert second_rule['expr'][0]['match']['op'].value == '=='
+    assert second_rule['expr'][0]['match']['right'].value == 443
+    assert second_rule['expr'][1]['accept'].value is None
     # {"rule": {"family": "ip", "table": "nftables_svc", "chain": "INPUT", "handle": 4, "expr": [{"reject": null}]}}
     third_rule = nftables_svc_input_rules[2]
-    assert third_rule['expr'][0]['reject'] is None
+    assert third_rule['expr'][0]['reject'].value is None
 
     # test map info in data
     # {"map": {"family": "ip", "name": "example_map", "table": "example_table", "type": "ipv4_addr", "handle": 2, "map": "verdict", "elem": [["192.0.2.1", {"accept": null}], ["192.0.2.2", {"drop": null}], ["192.0.2.3", {"accept": null}]]}}
     assert 'map' in nft_obj.data['nftables'][2]
     map_info = nft_obj.data['nftables'][2]['map']
-    assert map_info['name'] == 'example_map'
-    assert map_info['table'] == 'example_table'
-    assert map_info['type'] == 'ipv4_addr'
-    assert map_info['map'] == 'verdict'
-    assert len(map_info['elem']) == 3
-    map_eles = map_info['elem']
-    assert '192.0.2.1' in map_eles[0]
-    assert '192.0.2.2' in map_eles[1]
-    assert '192.0.2.3' in map_eles[2]
+    assert map_info['name'].value == 'example_map'
+    assert map_info['table'].value == 'example_table'
+    assert map_info['type'].value == 'ipv4_addr'
+    assert map_info['map'].value == 'verdict'
+    assert len(map_info['elem']) == 1
+    map_ele = map_info['elem'][0]
+    # "['192.0.2.1', {'accept': None}] ['192.0.2.2', {'drop': None}] ['192.0.2.3', {'accept': None}]"
+    assert '192.0.2.1' in map_ele.value
+    assert '192.0.2.2' in map_ele.value
+    assert '192.0.2.3' in map_ele.value
 
     # test set info in data
     # {"set": {"family": "ip", "name": "example_set", "table": "example_table", "type": "ipv4_addr", "handle": 4}}
     assert 'set' in nft_obj.data['nftables'][3]
     set_info = nft_obj.data['nftables'][3]['set']
-    assert set_info['name'] == 'example_set'
-    assert set_info['table'] == 'example_table'
-    assert set_info['type'] == 'ipv4_addr'
+    assert set_info['name'].value == 'example_set'
+    assert set_info['table'].value == 'example_table'
+    assert set_info['type'].value == 'ipv4_addr'
 
     nft2_obj = NftListRuleSet(context_wrap(NTT_LIST_RULESET_TWO_CHAINS))
     chains = nft2_obj.chains('ip', 'example_table')
     assert len(chains) == 2
-    assert 'postrouting' in chains
+    assert 'postrouting' in [item['name'].value for item in chains]
     rules = nft2_obj.rules('ip', 'example_table', 'postrouting')
     assert len(rules) == 1
     first_rule = rules[0]
     # {"rule": {"family": "ip", "table": "example_table", "chain": "postrouting", "handle": 9, "expr": [{"match": {"op": "==", "left": {"payload": {"protocol": "tcp", "field": "dport"}}, "right": 22}}, {"accept": null}]}}
-    assert first_rule['expr'][0]['match']['left']['payload']['protocol'] == 'tcp'
-    assert first_rule['expr'][0]['match']['left']['payload']['field'] == 'dport'
-    assert first_rule['expr'][0]['match']['op'] == '=='
-    assert first_rule['expr'][0]['match']['right'] == 22
-    assert first_rule['expr'][1]['accept'] is None
+    assert first_rule['expr'][0]['match']['left']['payload']['protocol'].value == 'tcp'
+    assert first_rule['expr'][0]['match']['left']['payload']['field'].value == 'dport'
+    assert first_rule['expr'][0]['match']['op'].value == '=='
+    assert first_rule['expr'][0]['match']['right'].value == 22
+    assert first_rule['expr'][1]['accept'].value is None
 
-    assert nft2_obj.tables('non_exist_ip') is None
-    assert nft2_obj.chains('ip', 'non_exist_table') is None
-    assert nft2_obj.rules('ip', 'example_table', 'non_exist_chain') is None
+    assert not nft2_obj.tables('non_exist_ip')
+    assert not nft2_obj.chains('ip', 'non_exist_table')
+    assert not nft2_obj.rules('ip', 'example_table', 'non_exist_chain')
 
 
 def test_except_in_nft_list_ruleset():
