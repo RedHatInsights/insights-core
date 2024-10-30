0fba6e7ecfa260dd85b227585e06831c2a65b29b
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-10-30 10:39:35 +0100

    parsers: add more docs to image_builder_facts
    Signed-off-by: Sanne Raymaekers <sanne.raymaekers@gmail.com>

--- insights/parsers/image_builder_facts.py
+++ insights/parsers/image_builder_facts.py
@@ -1,18 +1,10 @@
 """
-ImageBuilderFacts - files ``/etc/rhsm/facts/osbuild.facts``
-=========================================================
+ImageBuilderFacts - file ``/etc/rhsm/facts/osbuild.facts``
+==========================================================
 
 This module provides parsing for the ``/etc/rhsm/facts/osbuild.facts`` file.
 The ``ImageBuilderFacts`` class is based on a shared class which processes the JSON
 information into a dictionary.
-
-Sample input data looks like::
-
-    {
-      "image-builder.insights.compliance-policy-id": "61812cce-c884-4d52-bf05-15b41370db23",
-      "image-builder.insights.compliance-profile-id": "xccdf_org.ssgproject.content_profile_cis",
-      "image-builder.osbuild-composer.api-type": "cloudapi-v2"
-    }
 """
 
 from insights.core import JSONParser
@@ -22,4 +14,30 @@ from insights.specs import Specs
 
 @parser(Specs.image_builder_facts)
 class ImageBuilderFacts(JSONParser):
+    """
+    Parses the ``/etc/rhsm/facts/osbuild.facts`` file.
+
+    Sample input::
+
+        {
+          "image-builder.insights.compliance-policy-id": "61812cce-c884-4d52-bf05-15b41370db23",
+          "image-builder.insights.compliance-profile-id": "xccdf_org.ssgproject.content_profile_cis",
+          "image-builder.osbuild-composer.api-type": "cloudapi-v2"
+        }
+
+    Output will be a python object with the same structure as the JSON::
+
+        {
+          "image-builder.insights.compliance-policy-id": "61812cce-c884-4d52-bf05-15b41370db23",
+          "image-builder.insights.compliance-profile-id": "xccdf_org.ssgproject.content_profile_cis",
+          "image-builder.osbuild-composer.api-type": "cloudapi-v2"
+        }
+
+    Example:
+
+        >>> type(image_builder_facts)
+        <class 'insights.parsers.image_builder_facts.ImageBuilderFacts'>
+        >>> image_builder_facts["image-builder.insights.compliance-policy-id"] == "61812cce-c884-4d52-bf05-15b41370db23"
+        True
+    """
     pass
