c62eae1c85353d151378ef62781f174f0b234f6e
Xiangce Liu <xiangceliu@redhat.com>
2024-11-29 15:51:30 +0800

    fix unittest
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/datasources/compliance/test_compliance.py
+++ insights/tests/datasources/compliance/test_compliance.py
@@ -18,9 +18,13 @@ PATH = '/usr/share/xml/scap/ref_id.xml'
 def test_get_system_policies(config):
     compliance_client = ComplianceClient(config=config)
     compliance_client._inventory_id = '068040f1-08c8-43e4-949f-7d6470e9111c'
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': ['test']})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(status_code=200, json=Mock(return_value={'data': ['test']}))
+    )
     assert compliance_client.get_system_policies() == ['test']
-    url = "https://localhost/app/compliance/v2/systems/{0}/policies".format(compliance_client.inventory_id)
+    url = "https://localhost/app/compliance/v2/systems/{0}/policies".format(
+        compliance_client.inventory_id
+    )
     compliance_client.conn.session.get.assert_called_with(url)
 
 
@@ -30,7 +34,9 @@ def test_get_system_policies_error(config):
     compliance_client._inventory_id = '068040f1-08c8-43e4-949f-7d6470e9111c'
     compliance_client.conn.session.get = Mock(return_value=Mock(status_code=500))
     assert compliance_client.get_system_policies() == []
-    url = "https://localhost/app/compliance/v2/systems/{0}/policies".format(compliance_client.inventory_id)
+    url = "https://localhost/app/compliance/v2/systems/{0}/policies".format(
+        compliance_client.inventory_id
+    )
     compliance_client.conn.session.get.assert_called_with(url)
 
 
@@ -54,10 +60,12 @@ def test_find_scap_policy_with_one_datastream_file(config, tmpdir):
     compliance_client = ComplianceClient(config=config)
     dir1 = tmpdir.mkdir('scap')
     file = dir1.join('test_file.xml')
-    file.write("""
+    file.write(
+        """
     <xccdf-1.2:Profile id="xccdf_org.ssgproject.content_profile_anssi_bp28_high">
     </xccdf-1.2:Profile>
-        """)
+        """
+    )
     compliance_client.profile_files = lambda: [str(file)]
     with patch("insights.specs.datasources.compliance.SCAP_DATASTREAMS_PATH", str(dir1) + "/"):
         assert compliance_client.find_scap_policy('content_profile_anssi_bp28_high') == file
@@ -68,21 +76,28 @@ def test_find_scap_policy_with_two_datastream_file(config, tmpdir):
     compliance_client = ComplianceClient(config=config)
     dir1 = tmpdir.mkdir('scap')
     file1 = dir1.join('test_file1.xml')
-    file1.write("""
+    file1.write(
+        """
     <xccdf-1.2:Profile id="xccdf_org.ssgproject.content_profile_anssi_bp28_high">
     </xccdf-1.2:Profile>
-        """)
+        """
+    )
     file2 = dir1.join('test_file2.xml')
-    file2.write("""
+    file2.write(
+        """
     <xccdf-1.2:Profile id="xccdf_org.ssgproject.content_profile_anssi_bp28_high">
     </xccdf-1.2:Profile>
-        """)
+        """
+    )
     compliance_client.profile_files = lambda: [str(file1), str(file2)]
     with patch("insights.specs.datasources.compliance.SCAP_DATASTREAMS_PATH", str(dir1) + "/"):
         assert compliance_client.find_scap_policy('content_profile_anssi_bp28_high') == file1
 
 
-@patch("insights.specs.datasources.compliance.call", return_value=(1, 'bad things happened'.encode('utf-8')))
+@patch(
+    "insights.specs.datasources.compliance.call",
+    return_value=(1, 'bad things happened'.encode('utf-8')),
+)
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_find_scap_policy_not_found(config, call):
     compliance_client = ComplianceClient(config=config)
@@ -99,12 +114,25 @@ def test_run_scan(config, call):
     env.update({'TZ': 'UTC'})
     compliance_client.run_scan('ref_id', '/nonexistent', output_path)
     if six.PY3:
-        call.assert_called_with(("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'), keep_rc=True, env=env)
+        call.assert_called_with(
+            ("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'),
+            keep_rc=True,
+            env=env,
+        )
     else:
-        call.assert_called_with(("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent').encode(), keep_rc=True, env=env)
+        call.assert_called_with(
+            (
+                "oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'
+            ).encode(),
+            keep_rc=True,
+            env=env,
+        )
 
 
-@patch("insights.specs.datasources.compliance.call", return_value=(1, 'bad things happened'.encode('utf-8')))
+@patch(
+    "insights.specs.datasources.compliance.call",
+    return_value=(1, 'bad things happened'.encode('utf-8')),
+)
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_run_scan_fail(config, call):
     compliance_client = ComplianceClient(config=config)
@@ -114,9 +142,19 @@ def test_run_scan_fail(config, call):
     with raises(SystemExit):
         compliance_client.run_scan('ref_id', '/nonexistent', output_path)
     if six.PY3:
-        call.assert_called_with(("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'), keep_rc=True, env=env)
+        call.assert_called_with(
+            ("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'),
+            keep_rc=True,
+            env=env,
+        )
     else:
-        call.assert_called_with(("oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent').encode(), keep_rc=True, env=env)
+        call.assert_called_with(
+            (
+                "oscap xccdf eval --profile ref_id --results " + output_path + ' /nonexistent'
+            ).encode(),
+            keep_rc=True,
+            env=env,
+        )
 
 
 @patch("insights.specs.datasources.compliance.call", return_value=(0, ''.encode('utf-8')))
@@ -140,29 +178,68 @@ def test_tailored_file_is_not_downloaded_if_not_needed(config):
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_tailored_file_is_not_downloaded_if_os_minor_version_mismatches(config):
     compliance_client = ComplianceClient(os_version=['6', '5'], config=config)
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, headers={"Content-Type": "application/xml"}, json=Mock(return_value={'data': []})))
-    assert compliance_client.download_tailoring_file({'id': 'foo', 'ref_id': 'aaaaa', 'os_minor_version': '2'}) is None
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=200,
+            headers={"Content-Type": "application/xml"},
+            json=Mock(return_value={'data': []}),
+        )
+    )
+    assert (
+        compliance_client.download_tailoring_file(
+            {'id': 'foo', 'ref_id': 'aaaaa', 'os_minor_version': '2'}
+        )
+        is None
+    )
 
 
 @patch("insights.specs.datasources.compliance.open", new_callable=mock_open)
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_tailored_file_is_downloaded_if_needed(config, call):
     compliance_client = ComplianceClient(os_version=['6', '5'], config=config)
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, headers={"Content-Type": "application/xml"}, json=Mock(return_value={'data': []})))
-    assert 'oscap_tailoring_file-aaaaa' in compliance_client.download_tailoring_file({'id': 'foo', 'ref_id': 'aaaaa', 'os_minor_version': '5'})
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=200,
+            headers={"Content-Type": "application/xml"},
+            json=Mock(return_value={'data': []}),
+        )
+    )
+    assert 'oscap_tailoring_file-aaaaa' in compliance_client.download_tailoring_file(
+        {'id': 'foo', 'ref_id': 'aaaaa', 'os_minor_version': '5'}
+    )
 
 
 @patch("insights.specs.datasources.compliance.open", new_callable=mock_open)
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_tailored_file_fails_to_download(config, call):
     compliance_client = ComplianceClient(config=config)
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=403, ok=False, headers={"Content-Type": "application/xml"}, json=Mock(return_value={'data': []})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=403,
+            ok=False,
+            headers={"Content-Type": "application/xml"},
+            json=Mock(return_value={'data': []}),
+        )
+    )
     assert compliance_client.download_tailoring_file({'id': 'foo', 'ref_id': 'aaaaa'}) is None
 
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, headers={"Content-Type": "application/json"}, json=Mock(return_value={'data': []})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=200,
+            headers={"Content-Type": "application/json"},
+            json=Mock(return_value={'data': []}),
+        )
+    )
     assert compliance_client.download_tailoring_file({'id': 'foo', 'ref_id': 'aaaaa'}) is None
 
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, content=None, headers={"Content-Type": "application/xml"}, json=Mock(return_value={'data': []})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=200,
+            content=None,
+            headers={"Content-Type": "application/xml"},
+            json=Mock(return_value={'data': []}),
+        )
+    )
     assert compliance_client.download_tailoring_file({'id': 'foo', 'ref_id': 'aaaaa'}) is None
 
 
@@ -170,14 +247,18 @@ def test_tailored_file_fails_to_download(config, call):
 def test_build_oscap_command_does_not_append_tailoring_path(config):
     compliance_client = ComplianceClient(config=config)
     expected_command = 'oscap xccdf eval --profile aaaaa --results output_path xml_sample'
-    assert expected_command == compliance_client.build_oscap_command('aaaaa', 'xml_sample', 'output_path', None)
+    assert expected_command == compliance_client.build_oscap_command(
+        'aaaaa', 'xml_sample', 'output_path', None
+    )
 
 
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
 def test_build_oscap_command_append_tailoring_path(config):
     compliance_client = ComplianceClient(config=config)
     expected_command = 'oscap xccdf eval --profile aaaaa --tailoring-file tailoring_path --results output_path xml_sample'
-    assert expected_command == compliance_client.build_oscap_command('aaaaa', 'xml_sample', 'output_path', 'tailoring_path')
+    assert expected_command == compliance_client.build_oscap_command(
+        'aaaaa', 'xml_sample', 'output_path', 'tailoring_path'
+    )
 
 
 @patch("insights.client.config.InsightsConfig", base_url='localhost.com/app')
@@ -198,14 +279,20 @@ def test__inventory_id(config):
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
 def test_assignable_policies(config):
     compliance_client = ComplianceClient(config=config)
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': ['test']})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(status_code=200, json=Mock(return_value={'data': ['test']}))
+    )
     compliance_client._inventory_id = '068040f1-08c8-43e4-949f-7d6470e9111c'
     compliance_client.get_system_policies = lambda: []
     compliance_client.os_major = 9
     compliance_client.os_minor = 3
-    compliance_client.conn.session.get = Mock(return_value=Mock(status_code=200, json=Mock(return_value={'data': [{"id": 123456, "title": "foo"}]})))
+    compliance_client.conn.session.get = Mock(
+        return_value=Mock(
+            status_code=200, json=Mock(return_value={'data': [{"id": 123456, "title": "foo"}]})
+        )
+    )
     assert compliance_client.assignable_policies() == 0
-    url = "https://localhost/app/compliance/v2/policies?filter=(os_major_version = 9 and os_minor_version = 3)"
+    url = "https://localhost/app/compliance/v2/policies?filter=(os_major_version=9 and os_minor_version=3)"
     compliance_client.conn.session.get.assert_called_with(url)
 
 
@@ -214,18 +301,26 @@ def test_policy_link_assign(config):
     compliance_client = ComplianceClient(config=config)
     compliance_client._inventory_id = '068040f1-08c8-43e4-949f-7d6470e9111c'
     policy_id = "d83ddbac-ab56-420b-9e71-878795375af5"
-    compliance_client.conn.session.patch = Mock(return_value=Mock(status_code=202, json=Mock(return_value={})))
+    compliance_client.conn.session.patch = Mock(
+        return_value=Mock(status_code=202, json=Mock(return_value={}))
+    )
     assert compliance_client.policy_link(policy_id, 'patch') == 0
-    url = "https://localhost/app/compliance/v2/policies/{0}/systems/{1}".format(policy_id, compliance_client.inventory_id)
+    url = "https://localhost/app/compliance/v2/policies/{0}/systems/{1}".format(
+        policy_id, compliance_client.inventory_id
+    )
     compliance_client.conn.session.patch.assert_called_with(url)
 
 
 @patch("insights.client.config.InsightsConfig", base_url='localhost/app', systemid='', proxy=None)
 def test_policy_link_unassign(config):
     compliance_client = ComplianceClient(config=config)
-    compliance_client.conn.session.delete = Mock(return_value=Mock(status_code=202, json=Mock(return_value={})))
+    compliance_client.conn.session.delete = Mock(
+        return_value=Mock(status_code=202, json=Mock(return_value={}))
+    )
     compliance_client._inventory_id = '068040f1-08c8-43e4-949f-7d6470e9111c'
     policy_id = "d83ddbac-ab56-420b-9e71-878795375af5"
     assert compliance_client.policy_link(policy_id, 'delete') == 0
-    url = "https://localhost/app/compliance/v2/policies/{0}/systems/{1}".format(policy_id, compliance_client.inventory_id)
+    url = "https://localhost/app/compliance/v2/policies/{0}/systems/{1}".format(
+        policy_id, compliance_client.inventory_id
+    )
     compliance_client.conn.session.delete.assert_called_with(url)
