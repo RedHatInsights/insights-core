139d8793262f93bba81bc8b7cb95e1c23ccbbe08
Glutexo <stomsa@redhat.com>
2024-12-20 01:47:04 +0100

    chore: Use caplog to verify log calls (#4315)
    Logger asserts are error-prone, because they assert calls, not actual messages. The caplog fixture is Pytest-native way to test logs. This makes integration-level unit tests easier to add.
    
    For the caplog fixture to work on Python 2.6 (RHEL 6), pytest_catchlog needs to be installed. On more recent version of python, the fixture is included in Pytest.
    
    pytest_catchlog caused pidfile write tests to fail. The fixture itself calls os.getpid, interfering with the patch. Every log message makes a getpid call that is recorded in the patch. assert_not_called patches then stopped working.
    
    Fixed by removing the os.getpid patch. The original function is safe to use. test_write_pidfile_not_called verifies several calls among which write_to_disk is the important one, not getpid.
    
    Card IDs:
    
    * CCT-774
    * CCT-963
    * CCT-1084
    
    Signed-off-by: Štěpán Tomsa <stomsa@redhat.com>
    (cherry picked from commit 644d314cd574da23dd3d52d24f78bb8b4b84ddcd)
    (cherry picked from commit b22745eef28326c26bb53711fd0a5f15c7840ad5)

--- insights/tests/client/init/test_write_pidfile.py
+++ insights/tests/client/init/test_write_pidfile.py
@@ -1,3 +1,5 @@
+import os
+
 from insights.client import InsightsClient
 from insights.client.constants import InsightsConstants
 from mock.mock import call
@@ -6,17 +8,15 @@ from mock.mock import patch
 
 @patch("insights.client.atexit.register")
 @patch("insights.client.write_to_disk")
-@patch("insights.client.os.getpid")
 @patch("insights.client.get_parent_process")
-def test_write_pidfile(get_parent_process, getpid, write_to_disk, register):
+def test_write_pidfile(get_parent_process, write_to_disk, register):
     '''
     Test writing of the pidfile when InsightsClient
     is called initially (when from_phase=False)
     '''
     InsightsClient(from_phase=False)
-    getpid.assert_called_once()
     write_to_disk.assert_has_calls((
-        call(InsightsConstants.pidfile, content=str(getpid.return_value)),
+        call(InsightsConstants.pidfile, content=str(os.getpid())),
         call(InsightsConstants.ppidfile, content=get_parent_process.return_value)
     ))
 
@@ -37,15 +37,13 @@ def test_atexit_delete_pidfile(write_to_disk, register):
 
 @patch("insights.client.write_to_disk")
 @patch("insights.client.get_parent_process")
-@patch("insights.client.os.getpid")
-def test_write_pidfile_not_called(getpid, get_parent_process, write_to_disk):
+def test_write_pidfile_not_called(get_parent_process, write_to_disk):
     '''
     Test that the pidfile is not written when
     called from a phase (from-phase=True)
     '''
     InsightsClient(from_phase=True)
     get_parent_process.assert_not_called()
-    getpid.assert_not_called()
     write_to_disk.assert_not_called()
 
 
