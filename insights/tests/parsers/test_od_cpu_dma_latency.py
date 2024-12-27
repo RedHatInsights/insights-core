22be42e6dafe8e6de48c5a44699c8102569845ca
wushiqinlou <jiazhang@redhat.com>
2024-12-27 09:08:19 +0800

    feat: Enhance od_cpu_dma_latency miss strip (#4321)
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    rh-pre-commit.version: 2.3.1
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: max <mclinton@redhat.com>

--- insights/tests/parsers/test_od_cpu_dma_latency.py
+++ insights/tests/parsers/test_od_cpu_dma_latency.py
@@ -6,22 +6,20 @@ from insights.parsers import od_cpu_dma_latency
 from insights.parsers.od_cpu_dma_latency import OdCpuDmaLatency
 from insights.tests import context_wrap
 
-CONTENT_OD_CPU_DMA_LATENCY = """
-  2000000000
-"""
+CONTENT_OD_CPU_DMA_LATENCY = """  2000000000"""
 
 CONTENT_OD_CPU_DMA_LATENCY_EMPTY = ""
 
 
 def test_doc_examples():
-    env = {'cpu_dma_latency': OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY))}
+    env = {'cpu_dma_latency': OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY, strip=False))}
     failed, total = doctest.testmod(od_cpu_dma_latency, globs=env)
     assert failed == 0
 
 
 def test_OdCpuDmaLatency():
-    d = OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY))
+    d = OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY, strip=False))
     assert d.force_latency == 2000000000
 
     with pytest.raises(SkipComponent):
-        OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY_EMPTY))
+        OdCpuDmaLatency(context_wrap(CONTENT_OD_CPU_DMA_LATENCY_EMPTY, strip=False))
