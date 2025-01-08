799c95db00c4e6b9333ca06278a822538f7142ac
Xiangce Liu <xiangceliu@redhat.com>
2025-01-08 12:58:27 +0800

    chore: sort out exceptions (#4327)
    - and remove the contrib.ConfigParser which only hold exceptions
    - N/A to "rhel_6" branch
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    (cherry picked from commit 4f596dddb3422492c2b15a55a250a445e255bba0)

--- insights/tests/test_config_parser.py
+++ insights/tests/test_config_parser.py
@@ -1,8 +1,7 @@
 import pytest
 
-from insights.contrib.ConfigParser import NoOptionError
 from insights.core import ConfigParser, IniConfigFile
-from insights.core.exceptions import SkipComponent
+from insights.core.exceptions import NoOptionError, SkipComponent
 from insights.tests import context_wrap
 
 # An example config file with a few tricks and traps for the parser
@@ -41,19 +40,19 @@ def test_ini_config_file_parser():
     ini = IniConfigFile(context_wrap(CONFIG_FILE))
 
     # sections() tests
-    assert list(ini.sections()) == \
-        ['global', 'comment tricks', 'value overwriting', 'value checks']
+    assert list(ini.sections()) == ['global', 'comment tricks', 'value overwriting', 'value checks']
 
     # items() tests - here we return a dictionary
-    assert dict(ini.items('global')) == \
-        {'keynospace': 'valuenospaces',
-         'key with spaces': 'value with spaces',
-         'key with continued value': "value1 value2"}
-    assert dict(ini.items('comment tricks')) == \
-        {'comment # in key': 'value still found',
-         'comment in value': 'value includes'}
-    assert dict(ini.items('value overwriting')) == \
-        {'key': 'this one should be picked'}
+    assert dict(ini.items('global')) == {
+        'keynospace': 'valuenospaces',
+        'key with spaces': 'value with spaces',
+        'key with continued value': "value1 value2",
+    }
+    assert dict(ini.items('comment tricks')) == {
+        'comment # in key': 'value still found',
+        'comment in value': 'value includes',
+    }
+    assert dict(ini.items('value overwriting')) == {'key': 'this one should be picked'}
 
     # get() tests on global section
     assert ini.get('global', 'keynospace') == 'valuenospaces'
