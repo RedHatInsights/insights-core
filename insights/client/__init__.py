caf1f38c0b6f60242a53e860c25805be424508fa
mhorky <mhorky@redhat.com>
2025-01-29 14:01:58 +0100

    feat(client): Support Fedora for egg update process
    * Card ID: CCT-1148
    
    When developing or testing on Fedora, previous code would try to fetch
    insights-client.el41.egg. Since Fedora 40-45 should match RHEL 10, this
    code implements mapping between these two distributions.
    
    For RHEL and CentOS, this patch contains no behavioral change.
    
    Signed-off-by: mhorky <mhorky@redhat.com>

--- insights/client/__init__.py
+++ insights/client/__init__.py
@@ -19,7 +19,7 @@ from .utilities import (write_data_to_file,
                         get_tags,
                         write_tags,
                         migrate_tags,
-                        os_release_info,
+                        get_rhel_version,
                         get_parent_process)
 
 NETWORK = constants.custom_network_log_level
@@ -131,11 +131,12 @@ class InsightsClient(object):
         self.tmpdir = tempfile.mkdtemp()
         atexit.register(self.delete_tmpdir)
         try:
-            _, os_release = os_release_info()
-            rhel_major = os_release.split('.')[0]
+            rhel_major = get_rhel_version()
             # set egg name as 'insights-core.el#.egg' per RHEL #
             egg_name = 'insights-core.el{0}.egg'.format(rhel_major)
-        except Exception:
+            logger.debug("Using versioned egg: {}".format(rhel_major))
+        except ValueError as exc:
+            logger.debug("Using non-versioned egg: {}".format(exc))
             # set default egg as 'insights-core.egg'
             egg_name = 'insights-core.egg'
 
