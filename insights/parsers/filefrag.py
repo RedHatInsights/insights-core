a8f1c27408996fb249d0fa83cd651a05b2043a39
xintli <xintli@redhat.com>
2025-02-19 15:34:30 +0800

    Update
    Signed-off-by: xintli <xintli@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- insights/parsers/filefrag.py
+++ insights/parsers/filefrag.py
@@ -1,6 +1,6 @@
 """
-FilefragGrubenv - Command ``/sbin/filefrag /boot/grub2/grubenv``
-================================================================
+Filefrag - Command ``/sbin/filefrag /boot/grub2/grubenv``
+=========================================================
 
 Parser for parsing the output of command ``/sbin/filefrag /boot/grub2/grubenv``.
 """
@@ -10,23 +10,20 @@ from insights.core.plugins import parser
 from insights.specs import Specs
 
 
-@parser(Specs.filefrag_grubenv)
-class FilefragGrubenv(Parser):
+@parser(Specs.filefrag)
+class Filefrag(Parser, dict):
     """
-    Provides the extent counts of ``/boot/grub2/grubenv`` by parsing the
-    output of command ``filefrag /boot/grub2/grubenv``.
-
-    Attributes:
-        extents(int): the extent counts of file ``/boot/grub2/grubenv``
+    Provides the extent counts of the specified file by parsing the
+    output of command ``filefrag <specified file>``.
 
     Typical content looks like::
 
         /boot/grub2/grubenv: 1 extent found
 
     Examples:
-        >>> type(grubenv)
-        <class 'insights.parsers.filefrag.FilefragGrubenv'>
-        >>> grubenv.extents
+        >>> type(filefrag)
+        <class 'insights.parsers.filefrag.Filefrag'>
+        >>> filefrag['/boot/grub2/grubenv']
         1
 
     Raises:
@@ -34,9 +31,10 @@ class FilefragGrubenv(Parser):
     """
 
     def parse_content(self, content):
-        if len(content) == 0 or 'No such file or directory' in content[0]:
+        if len(content) == 0 or (len(content) == 1 and 'No such file or directory' in content[0]):
             raise SkipComponent("Error: ", content[0] if content else 'empty file')
-        extents = content[0].split()[1]
-        if not extents.isdigit():
-            raise SkipComponent("Error: unparsable output", content[0])
-        self.extents = int(extents)
+        for line in content:
+            file = line.split()[0].strip(':')
+            extents = line.split()[1]
+            if extents.isdigit():
+                self[file] = int(extents)
