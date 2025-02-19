81680d0704b33e6ee225b9364d0756682ecdb246
xintli <xintli@redhat.com>
2025-02-19 17:33:33 +0800

    Update
    Signed-off-by: xintli <xintli@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- insights/parsers/filefrag.py
+++ insights/parsers/filefrag.py
@@ -25,18 +25,25 @@ class Filefrag(Parser, dict):
         <class 'insights.parsers.filefrag.Filefrag'>
         >>> filefrag['/boot/grub2/grubenv']
         1
+        >>> filefrag.unparsed_lines
+        ['open: No such file or directory', '/boot/grub2/grubenv: unknow extent found']
 
     Raises:
         SkipComponent: if the command output is empty or missing file
     """
 
     def parse_content(self, content):
-        if len(content) == 0 or (len(content) == 1 and 'No such file or directory' in content[0]):
+        self.unparsed_lines = []
+        if len(content) == 0:
             raise SkipComponent("Error: ", content[0] if content else 'empty file')
         for line in content:
             split_list = line.split()
-            if (not len(split_list) < 2) or 'No such file or directory' in line:
-                file = split_list[0].strip(':')
-                extents = split_list[1]
-                if extents.isdigit():
-                    self[file] = int(extents)
+            if len(split_list) < 2 or 'No such file or directory' in line:
+                self.unparsed_lines.append(line)
+                continue
+            file = split_list[0].strip(':')
+            extents = split_list[1]
+            if not extents.isdigit():
+                self.unparsed_lines.append(line)
+                continue
+            self[file] = int(extents)
--- insights/tests/parsers/test_filefrag.py
+++ insights/tests/parsers/test_filefrag.py
@@ -12,27 +12,29 @@ open: No such file or directory
 /boot/grub2/grubenv: unknow extent found
 """.strip()
 
-FILEFRAG_CONTENT_ERROR = """
+FILEFRAG_CONTENT_NO_FILE = """
 open: No such file or directory
 """.strip()
 
-FILEFRAG_CONTENT_ERROR2 = """
+FILEFRAG_CONTENT_ERROR = """
 """.strip()
 
 
-def test_filefrag():
+def test_filefrag_1():
     results = Filefrag(context_wrap(FILEFRAG_CONTENT))
     assert results['/boot/grub2/grubenv'] == 1
+    assert results.unparsed_lines == ['open: No such file or directory', '/boot/grub2/grubenv: unknow extent found']
+
+
+def test_filefrag_2():
+    results = Filefrag(context_wrap(FILEFRAG_CONTENT_NO_FILE))
+    assert results.unparsed_lines == ['open: No such file or directory']
 
 
 def test_filefrag_errors():
     with pytest.raises(SkipComponent) as ex:
         Filefrag(context_wrap(FILEFRAG_CONTENT_ERROR))
         assert FILEFRAG_CONTENT_ERROR in str(ex)
-
-    with pytest.raises(SkipComponent) as ex:
-        Filefrag(context_wrap(FILEFRAG_CONTENT_ERROR2))
-        assert FILEFRAG_CONTENT_ERROR2 in str(ex)
 
 
 def test_doc_examples():
