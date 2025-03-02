dd527c52e01c279a7446ba3d9fe038abffc8b2c9
jiazhang <jiazhang@redhat.com>
2025-03-02 10:07:07 +0800

    Add test instance for coverage
    Signed-off-by: jiazhang <jiazhang@redhat.com>
    
    rh-pre-commit.version: 2.3.1
    rh-pre-commit.check-secrets: ENABLED

--- insights/tests/datasources/test_files_number_of.py
+++ insights/tests/datasources/test_files_number_of.py
@@ -9,14 +9,15 @@ from insights.specs import Specs
 from insights.specs.datasources.files_number_of import files_number_dir
 
 
-TEST_DIR = "/tmp/test_files_number/"
-CREATE_TEST_FILES = "touch " + TEST_DIR + "file1" + " " + TEST_DIR + "file2"
-REMOVE_DIR = "rm -rf " + TEST_DIR
+TEST_DIR1 = "/tmp/test_files_number1/"
+TEST_DIR2 = "/tmp/test_files_number2"
+CREATE_TEST_FILES = "touch " + TEST_DIR1 + "file1" + " " + TEST_DIR1 + "file2"
+REMOVE_DIR = "rm -rf " + TEST_DIR1 + " " + TEST_DIR2
 
 
 def setup_function(func):
     if func is test_module_filters:
-        filters.add_filter(Specs.files_number_filter, [TEST_DIR])
+        filters.add_filter(Specs.files_number_filter, [TEST_DIR1, TEST_DIR2])
     if func is test_module_filters_empty:
         filters.add_filter(Specs.files_number_filter, [])
 
@@ -28,11 +29,12 @@ def teardown_function(func):
 
 def test_module_filters():
     broker = dr.Broker()
-    os.makedirs(TEST_DIR)
+    os.makedirs(TEST_DIR1)
+    os.makedirs(TEST_DIR2)
     os.system(CREATE_TEST_FILES)
     result = files_number_dir(broker)
     os.system(REMOVE_DIR)
-    assert result.content == ['{"/tmp/test_files_number/": 2}']
+    assert result.content == ['{"/tmp/test_files_number1/": 2, "/tmp/test_files_number2/": 0}']
 
 
 def test_module_filters_empty():
