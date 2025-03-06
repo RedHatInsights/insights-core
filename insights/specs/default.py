3fc583f5dce979daec53c80991454b2ee7198346
Xiaoxue Wang <xiaoxwan@redhat.com>
2025-03-06 16:12:25 +0800

    feat: add spec nvidia_smi_query_gpu (#4375)
    Signed-off-by: Xiaoxue Wang <xiaoxwan@redhat.com>
    Signed-off-by: max <mclinton@redhat.com>

--- insights/specs/default.py
+++ insights/specs/default.py
@@ -640,6 +640,9 @@ class DefaultSpecs(Specs):
         "/usr/bin/nvidia-smi --query-gpu=name,clocks_event_reasons.active --format=csv,noheader"
     )
     nvidia_smi_l = simple_command("/usr/bin/nvidia-smi -L")
+    nvidia_smi_query_gpu = simple_command(
+        "/usr/bin/nvidia-smi --query-gpu=index,name,uuid,memory.total,clocks_event_reasons.active --format=csv,noheader"
+    )
     nvme_core_io_timeout = simple_file("/sys/module/nvme_core/parameters/io_timeout")
     od_cpu_dma_latency = simple_command("/usr/bin/od -An -t d /dev/cpu_dma_latency")
     odbc_ini = simple_file("/etc/odbc.ini")
