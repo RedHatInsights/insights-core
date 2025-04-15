0c88695f1dc7243b9bc0f619b714eab9ea1de78d
Xiangce Liu <xiangceliu@redhat.com>
2025-04-15 18:32:15 +0800

    fix: add more candidates to CloudProvider combiner
    - add 'cloud init query' and 'subman facts' as optional candidates,
      and check them parsers firstly to identify the CP more accurately.
    - do not check RPM for Azure, as the 'walinuxagent' package can be
      installed on other cloud instance
    - refine the "_select_provider" method to use the checking results
    - Jira: RHINENG-13705
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/tests/components/test_cloud_provider.py
+++ insights/tests/components/test_cloud_provider.py
@@ -3,25 +3,28 @@ from insights.parsers.dmidecode import DMIDecode
 from insights.combiners.cloud_provider import CloudProvider
 from insights.components.cloud_provider import IsAWS, IsAzure, IsGCP
 from insights.tests.combiners.test_cloud_provider import (
-        DMIDECODE_AWS, DMIDECODE_GOOGLE, DMIDECODE_AZURE_ASSET_TAG)
+    DMIDECODE_AWS,
+    DMIDECODE_GOOGLE,
+    DMIDECODE_AZURE_ASSET_TAG,
+)
 
 
 def test_is_aws():
     dmi = DMIDecode(context_wrap(DMIDECODE_AWS))
-    cp = CloudProvider(None, dmi, None, None)
+    cp = CloudProvider(None, dmi, None, None, None, None)
     result = IsAWS(cp)
     assert isinstance(result, IsAWS)
 
 
 def test_is_azure():
     dmi = DMIDecode(context_wrap(DMIDECODE_AZURE_ASSET_TAG))
-    cp = CloudProvider(None, dmi, None, None)
+    cp = CloudProvider(None, dmi, None, None, None, None)
     result = IsAzure(cp)
     assert isinstance(result, IsAzure)
 
 
 def test_is_gcp():
     dmi = DMIDecode(context_wrap(DMIDECODE_GOOGLE))
-    cp = CloudProvider(None, dmi, None, None)
+    cp = CloudProvider(None, dmi, None, None, None, None)
     result = IsGCP(cp)
     assert isinstance(result, IsGCP)
