e964b92444547ea9f845bfb5b6cdb3d538435b6b
Xiangce Liu <xiangceliu@redhat.com>
2025-05-29 22:21:42 +0800

    Revert "chore(client): Simplify automatic configuration logic (#4376)" (#4465)
    This reverts commit 5dcf6892
    
    Signed-off-by: max <mclinton@redhat.com>

--- insights/tests/client/connection/test_init.py
+++ insights/tests/client/connection/test_init.py
@@ -1,6 +1,4 @@
-import configparser
-
-from insights.client.auto_config import autoconfigure_network
+from insights.client.auto_config import try_auto_configuration
 from insights.client.config import InsightsConfig
 from insights.client.connection import InsightsConnection
 from mock.mock import Mock
@@ -19,35 +17,22 @@ def test_inventory_url_from_base_url(get_proxies, init_session):
     assert connection.inventory_url == "https://www.example.com/inventory/v1"
 
 
-@mark.parametrize(
-    "input",
-    [{"check_results": True}, {"checkin": True}],
-    ids=["--check-results", "--checkin"]
-)
-@patch("insights.client.auto_config.rhsmCertificate.existsAndValid", return_value=True)
+@mark.parametrize(("config_kwargs",), (({"check_results": True},), ({"checkin": True},)))
 @patch("insights.client.connection.InsightsConnection._init_session")
 @patch("insights.client.connection.InsightsConnection.get_proxies")
+@patch("insights.client.auto_config._try_satellite6_configuration")
 @patch('insights.client.config.argparse.ArgumentParser.parse_args')
 def test_inventory_url_from_phase(
-    _parse_args,
-    _get_proxies,
-    _init_session,
-    _rhsm_is_registered,
-    input,
+    _parse_args, try_satellite6_configuration, get_proxies, init_session, config_kwargs
 ):
-    """Inventory URL is composed correctly from the default configuration."""
-    rhsm_config = configparser.ConfigParser()
-    rhsm_config.read_string(
-        u"[server]\n"
-        u"hostname=subscription.rhsm.redhat.com\nport=443\n"
-        u"proxy_scheme=http\nproxy_hostname=\nproxy_port=\n"
-        u"proxy_user=\nproxy_password=\n"
-        u"no_proxy=\n"
-    )
-
-    config = InsightsConfig(**input)
-    with patch("insights.client.auto_config.get_rhsm_config", return_value=rhsm_config):
-        with patch("insights.client.auto_config.utilities.get_rhel_version", return_value=9):
-            autoconfigure_network(config)
+    """
+    Inventory URL is composed correctly from the default configuration.
+    """
+    config = InsightsConfig(**config_kwargs)
+    config.load_all()  # Disables legacy upload.
+    try_auto_configuration(config)  # Updates base_url if legacy upload is disabled.
     connection = InsightsConnection(config)
-    assert connection.inventory_url == "https://cert.cloud.redhat.com/api/inventory/v1"
+    assert (
+        connection.inventory_url
+        == "https://cert-api.access.redhat.com/r/insights/platform/inventory/v1"
+    )
