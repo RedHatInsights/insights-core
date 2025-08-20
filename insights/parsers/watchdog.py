aa5db02f69f45758f22549d0247b301c2fe978bf
Huanhuan Li <huali@redhat.com>
2025-08-20 14:41:26 +0800

    Move parser "WatchDogLog" to "watchdog.py" file
    * Move all the parsers related with watchdog to one file to eliminate files
      count
    
    Signed-off-by: Huanhuan Li <huali@redhat.com>

--- insights/parsers/watchdog.py
+++ insights/parsers/watchdog.py
@@ -4,9 +4,11 @@ Parsers for watchdog configuration
 
 WatchDogConf - file ``/etc/watchdog.conf``
 ------------------------------------------
+WatchDogLog - file ``/var/log/watchdog/*.std*``
+-----------------------------------------------
 """
 
-from insights import parser, Parser, SkipComponent
+from insights import parser, Parser, SkipComponent, LogFileOutput
 from insights.specs import Specs
 from insights.parsers import split_kv_pairs
 
@@ -42,3 +44,30 @@ class WatchDogConf(Parser, dict):
         self.update(split_kv_pairs(content))
         if not self:
             raise SkipComponent('Not available data')
+
+
+@parser(Specs.watchdog_logs)
+class WatchDogLog(LogFileOutput):
+    """
+    Class for parsing ``/var/log/watchdog/*.std*`` files.
+
+    Sample Input::
+
+        DEBUG:root:0
+
+        INFO:root:Executing: /usr/bin/sg_persist -n -i -k -d /dev/mapper/mpathb
+
+        DEBUG:root:0   PR generation=0xc2, 4 registered reservation keys follow:
+            0xfdfe0001
+            0xfdfe0001
+            0xfdfe0000
+            0xfdfe0000
+
+
+        DEBUG:root:key fdfe0001 registered with device /dev/mapper/mpathb
+        INFO:root:Executing: /usr/bin/sg_turs /dev/mapper/mpathb
+
+        DEBUG:root:0
+    """
+
+    time_format = None
