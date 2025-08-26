31cd08d5d7b249703069a36ced90df6d955581e3
Xiangce Liu <xiangceliu@redhat.com>
2025-08-26 14:29:33 +0800

    fix: reduce the size of CalledProcessError exception (#4528)
    * fix: reduce the output to CalledProcessError exception
    
    - In some edge cases, some command outputs are significant large
      even when the command failed. To hold the whole output in the
      Traceback is not reasonable, as it will cause container being
      killed due to OOM when loading the meta_data Spec JSON.
    - In fact the Traceback + "head" (1024 chars) of the output are
      good enough for devs to debug and troubleshoot it.
    - Jira: RHINENG-20300
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    
    * Update insights/util/subproc.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    (cherry picked from commit ff082f180f45564e199e2a32b3c26e353974ab90)
    (cherry picked from commit eaf642c71972a75f411a34e7394328f20845508f)

--- insights/tests/test_subproc.py
+++ insights/tests/test_subproc.py
@@ -1,6 +1,8 @@
+import os
 import pytest
 import shlex
 import sys
+import stat
 
 from insights.core.exceptions import CalledProcessError
 from insights.util import subproc
@@ -23,3 +25,31 @@ def test_call_timeout():
     if sys.platform != "darwin":
         with pytest.raises(CalledProcessError):
             subproc.call('sleep 3', timeout=1)
+
+
+SCRIPT_CONTENT = """
+#!/bin/bash
+echo '0123456789'
+exit 1
+""".strip()
+
+
+@pytest.fixture(scope="module")
+def tmp_script(tmpdir_factory):
+    root = str(tmpdir_factory.mktemp("test_subproc"))
+    file_path = os.path.join(root, 'tmp.sh')
+    with open(file_path, 'w') as fd:
+        for line in SCRIPT_CONTENT.splitlines():
+            fd.write(line + '\n')
+    st = os.stat(file_path)
+    os.chmod(file_path, st.st_mode | stat.S_IEXEC)
+    return file_path
+
+
+def test_call_error_output(tmp_script):
+    env = os.environ
+    env.update(MAX_FAILURE_OUTPUT="6")
+    with pytest.raises(CalledProcessError) as cpe:
+        subproc.call(tmp_script, env=env)
+    assert "012345" in str(cpe)
+    assert "0123456" not in str(cpe)
