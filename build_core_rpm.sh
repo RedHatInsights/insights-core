c8ecf8420760539a14bd3107cf236b0b0a02164e
Xiangce Liu <xiangceliu@redhat.com>
2025-09-04 16:25:03 +0800

    chore: update RPM building scripts and spec file (#4533)
    - Add a `testing` entry to build RPM for PR check
    - Update spec file to decide selinux version per RHEL
      By doing this, it's not required to specify the version by hand
      when building RPM.
    - Rename Source0 to use the `modulename` per the latest setuptools
    - Remove the URL link from Sources of spec
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>
    Signed-off-by: Jiajun Zhang <jiazhang@redhat.com>

--- build_core_rpm.sh
+++ build_core_rpm.sh
@@ -20,19 +20,27 @@ fi
 
 # Check build target
 if [ "$TARGET" == "internal" ]; then
+    # backforward compatible with internal RPM building
     echo "Building insights-core RPM for internal usage."
     BUILDTARGET="for_internal 1"
     MANIFEST="MANIFEST.in.core"
-elif [ "$TARGET" == "client" ]; then
-    echo "Building insights-core RPM for insights-client."
-    BUILDTARGET="with_selinux 1"
+elif [ "$TARGET" == "client" ] || [ "$TARGET" == "testing" ]; then
+    if [ "$TARGET" == "client" ]; then
+        # for RPM release
+        echo "Building insights-core RPM for insights-client."
+        BUILDTARGET="with_selinux 1"
+    else
+        # for RPM testing (PR Check)
+        echo "Building insights-core RPM for testing with insights-client."
+        BUILDTARGET="with_selinux 0"
+    fi
     MANIFEST="MANIFEST.in.client"
     # - remove depedencies for data processing
     sed -i -e '/cachecontrol/d' -e '/defusedxml/d' -e '/jinja2/d' -e '/lockfile/d' -e '/redis/d' -e '/setuptools;/d' pyproject.toml setup.py
     # - remove entrypoints for data processing
     sed -i -e '/insights =/d' -e '/insights-dupkey/d' -e '/insights-run/d' -e '/insights-inspect/d' -e '/mangle =/d' pyproject.toml setup.py
 else
-    echo "Error: invalid build target: '$TARGET'. Use 'internal' or 'client'"
+    echo "Error: invalid build target: '$TARGET'. Use 'internal', 'client', or 'testing'"
     exit 1
 fi
 
@@ -40,7 +48,25 @@ rm -rf BUILD BUILDROOT RPMS SRPMS insights_core.egg-info
 # Prepare the tarbal
 git rev-parse --short HEAD > insights/COMMIT
 cp $MANIFEST MANIFEST.in
-$PYTHON setup.py sdist
+
+$PYTHON -m pip install build
+$PYTHON -m build --sdist
+
+if [ "$TARGET" == "client" ]; then
+    # keep compatible with old&new setuptools
+    VERSION=`cat insights/VERSION`
+    pushd dist
+    if [ -f insights_core-${VERSION}.tar.gz ]; then
+        tar xf insights_core-${VERSION}.tar.gz
+        rename _ - insights_core-${VERSION}
+        tar zcf insights-core-${VERSION}.tar.gz insights-core-${VERSION}
+    fi
+    popd
+    if [ ! -f dist/insights-core-${VERSION}.tar.gz ]; then
+        echo "Error: Source tarbal archive 'insights-core-${VERSION}.tar.gz' doesn't exist."
+        exit 1
+    fi
+fi
 # Build the RPM/SRPM
 rpmbuild -D "${BUILDTARGET}" -D "_topdir $PWD" -D "_sourcedir $PWD/dist" -ba insights-core.spec
 
