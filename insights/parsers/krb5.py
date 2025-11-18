9a613e99373d46e6417977a61df6901aafb3aa6d
Xiangce Liu <xiangceliu@redhat.com>
2025-11-18 12:04:33 +0800

    spec: new spec and parser for krb5_localauth_plugin (#4629)
    - Refactor the existing Krb5Configuration to a base Parser
      Refactor the existing AllKrb5Conf combiner accordingly
    - Remove the using of LegacyItemAccess
    - Refine insights.util.parse_bool to support more keys
    - Collect new spec for krb5_localauth_plugin and add its parser Krb5LocalauthPlugin
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/krb5.py
+++ insights/parsers/krb5.py
@@ -1,63 +1,21 @@
 """
+Configuration Parsers for Krb5
+==============================
+
+Below parsers are included:
+
 Krb5Configuration - files ``/etc/krb5.conf`` and ``/etc/krb5.conf.d/*``
-=======================================================================
+-----------------------------------------------------------------------
 
-krb5 Configuration are ``/etc/krb5.conf`` and ``/etc/krb5.conf.d/*``,
-and the content format is similar to ``INI config``, but they include
-values that span multiple lines. Multi-line values start with a '{'
-and end with a '}', and we join them together by setting the `is_squ`
-variable to True while in a multi-line value.
-
-Example:
-    >>> krb5_content = '''
-    [realms]
-     dns_lookup_realm = false
-     ticket_lifetime = 24h
-     default_ccache_name = KEYRING:persistent:%{uid}
-     EXAMPLE.COM = {
-      kdc = kerberos.example.com
-      admin_server = kerberos.example.com
-     }
-     pam = {
-      debug = false
-      krb4_convert = false
-      ticket_lifetime = 36000
-     }
-     [libdefaults]
-      dns_lookup_realm = false
-      ticket_lifetime = 24h
-      EXAMPLE.COM = {
-       kdc = kerberos2.example.com
-       admin_server = kerberos2.example.com
-     }
-    # renew_lifetime = 7d
-    # forwardable = true
-    # rdns = false
-    '''.strip()
-
-    >>> from insights.tests import context_wrap
-    >>> shared = {Krb5Configuration: Krb5Configuration(context_wrap(krb5_content))}
-    >>> krb5_info = shared[Krb5Configuration]
-    >>> krb5_info["libdefaults"]["dnsdsd"]
-    "false"
-    >>> krb5_info["realms"]["EXAMPLE.COM"]["kdc"]
-    "kerberos.example.com"
-    >>> krb5_info.sections()
-    ["libdefaults","realms"]
-    >>> krb5_info.has_section("realms")
-    True
-    >>> krb5_info.has_option("realms", "nosuchoption")
-    False
-    >>> krb5_info.options("libdefaults")
-    ["dns_lookup_realm","ticket_lifetime","EXAMPLE.COM"]
+Krb5LocalauthPlugin - file ``/var/lib/sss/pubconf/krb5.include.d/localauth_plugin``
+-----------------------------------------------------------------------------------
 """
 
-
-from .. import parser, Parser, get_active_lines, LegacyItemAccess
+from insights.core import Parser
+from insights.core.exceptions import SkipComponent
+from insights.core.plugins import parser
 from insights.specs import Specs
-
-
-PREFIX_FOR_LIST = ("includedir", "include", "module")
+from insights.util import parse_bool
 
 
 def _handle_key_value(t_dict, key, value):
@@ -73,58 +31,43 @@ def _handle_key_value(t_dict, key, value):
     return value
 
 
-def _handle_krb5_bool(value):
+class Krb5ConfBase(Parser, dict):
     """
-    Convert krb5.conf boolean
+    Base Class to process the Kerberos relevant configurations.
+
+    The Kerberos Configuration are generally in .ini format. it is like an
+    ordinary .ini file except that values can include a multiple line key-value
+    pair 'relation' that starts with a '{' and end with a '}' on a trailing line.
+    So we track whether we're in curly braces by setting `is_squ` when we enter
+    a relation, and clearing it when we leave.  Please fill in the remainder of
+    the logic here.
+
     """
-    # see lib/krb5/krb/libdef_parse.c _krb5_conf_boolean()
-    if value in set(["y", "yes", "true", "t", "1", "on"]):
-        return True
-    elif value in set(["n", "no", "false", "nil", "0", "off"]):
-        return False
-    else:
-        # _krb5_conf_boolean() treats any other value as "False". Return
-        # "None" so caller can identify this case.
-        return None
 
-
-@parser(Specs.krb5)
-class Krb5Configuration(Parser, LegacyItemAccess):
-    """
-    Class for ``krb5.conf`` and ``krb5.conf.d`` configuration files.
-
-    The Kerberos .ini format is like an ordinary .ini file except that values
-    can include a multiple line key-value pair 'relation' that starts with a
-    '{' and end with a '}' on a trailing line.  So we track whether we're in
-    curly braces by setting `is_squ` when we enter a relation, and clearing
-    it when we leave.  Please fill in the remainder of the logic here.
-
-    Attributes:
-        includedir (list): The directory list that `krb5.conf` includes via
-            `includedir` directive
-        include (list): The configuration file list that `krb5.conf` includes
-            via `include` directive
-        module (list): The module list that `krb5.conf` specifed via `module`
-            directive
-    """
+    RESERVED_ATTRS = None
 
     def parse_content(self, content):
+        if self.RESERVED_ATTRS is None:
+            raise RuntimeError('Not applied when "RESERVED_ATTRS" does not exist')
+        for key in self.RESERVED_ATTRS:
+            setattr(self, key, [])
         dict_all = {}
         is_squ = False
         section_name = ""
         squ_value = {}
         squ_section_name = ""
         section_value = {}
-        self.includedir = []
-        self.include = []
-        self.module = []
         unchangeable_tags = []
-        for line in get_active_lines(content):
+        for line in content:
             line = line.strip()
-            if line.startswith(PREFIX_FOR_LIST):
-                key, value = [i.strip() for i in line.split(None, 1)]
-                getattr(self, key).append(value) if key in PREFIX_FOR_LIST else None
+            if not line or line.startswith('#'):
+                # skip empty and commented lines
                 continue
+            if line.startswith(self.RESERVED_ATTRS):
+                key, value = [i.strip() for i in line.split(None, 1)]
+                if key in self.RESERVED_ATTRS:
+                    getattr(self, key).append(value)
+                    continue
             if is_squ:
                 # If in {} sub_section, get the key_value pair
                 if "=" in line:
@@ -165,40 +108,135 @@ class Krb5Configuration(Parser, LegacyItemAccess):
                 else:
                     is_squ = True
                     squ_section_name = line.split("=")[0].strip()
-        self.data = dict_all
+        if dict_all:
+            self.update(dict_all)
+        elif not any(getattr(self, key) for key in self.RESERVED_ATTRS):
+            raise SkipComponent()
+
+    @property
+    def data(self):
+        """
+        Keep backward compatibility. The "data" atrribute is deprecated,
+        the parser itself is dictionary.
+
+        .. warning::
+            This will be removed from 3.8.0.
+        """
+        return self
 
     def sections(self):
         """
         Return a list of section names.
         """
-        return self.data.keys()
+        return sorted(self.keys())
 
     def has_section(self, section):
         """
         Indicate whether the named section is present in the configuration.
         Return True if the given section is present, and False if not present.
         """
-        return section in self.data
+        return section in self
 
     def options(self, section):
         """
         Return a list of option names for the given section name.
         """
-        return self.data[section].keys() if self.has_section(section) else []
+        return sorted(self[section].keys()) if self.has_section(section) else []
 
     def has_option(self, section, option):
         """
         Check for the existence of a given option in a given section.
         Return True if the given option is present, and False if not present.
         """
-        if section not in self.data:
-            return False
-        return option in self.data[section]
+        return False if section not in self else option in self[section]
 
     def getboolean(self, section, option):
         """Parse option as bool
 
         Returns None is not a krb5.conf boolean string.
         """
-        value = self.data[section][option]
-        return _handle_krb5_bool(value)
+        return parse_bool(self[section][option], default=None)
+
+
+@parser(Specs.krb5)
+class Krb5Configuration(Krb5ConfBase):
+    """
+    Krb5 Configuration are ``/etc/krb5.conf`` and ``/etc/krb5.conf.d/*``.
+
+    See :class:`Krb5ConfBase` for details.
+
+    Attributes:
+        includedir (list): The directory list that `krb5.conf` includes via
+            `includedir` directive
+        include (list): The configuration file list that `krb5.conf` includes
+            via `include` directive
+        module (list): The module list that `krb5.conf` specifed via `module`
+            directive
+
+    Sample content::
+
+        include /etc/krb5test.conf
+        [realms]
+          dns_lookup_realm = false
+          ticket_lifetime = 24h
+          default_ccache_name = KEYRING:persistent:%{uid}
+          EXAMPLE.COM = {
+           kdc = kerberos.example.com
+           admin_server = kerberos.example.com
+          }
+          pam = {
+           debug = false
+           krb4_convert = false
+           ticket_lifetime = 36000
+          }
+        [libdefaults]
+          dns_lookup_realm = false
+          dnsdsd = false
+          ticket_lifetime = 24h
+          EXAMPLE.COM = {
+           kdc = kerberos2.example.com
+           admin_server = kerberos2.example.com
+         }
+
+    Example:
+        >>> type(krb5_conf)
+        <class 'insights.parsers.krb5.Krb5Configuration'>
+        >>> krb5_conf["libdefaults"]["dnsdsd"]
+        'false'
+        >>> krb5_conf["realms"]["EXAMPLE.COM"]["kdc"]
+        'kerberos.example.com'
+        >>> krb5_conf.sections()
+        ['libdefaults', 'realms']
+        >>> krb5_conf.has_section("realms")
+        True
+        >>> krb5_conf.has_option("realms", "nosuchoption")
+        False
+        >>> krb5_conf.options("libdefaults")
+        ['EXAMPLE.COM', 'dns_lookup_realm', 'dnsdsd', 'ticket_lifetime']
+        >>> krb5_conf.include
+        ['/etc/krb5test.conf']
+    """
+
+    RESERVED_ATTRS = ("includedir", "include", "module")
+
+
+@parser(Specs.krb5_localauth_plugin)
+class Krb5LocalauthPlugin(Krb5ConfBase):
+    """
+    Krb5 Configuration parser for `/var/lib/sss/pubconf/krb5.include.d/localauth_plugin`
+
+    Sample input::
+
+        [plugins]
+         localauth = {
+          module = sssd:/usr/lib64/sssd/modules/sssd_krb5_localauth_plugin.so
+         }
+
+    Examples:
+        >>> type(krb5_LP)
+        <class 'insights.parsers.krb5.Krb5LocalauthPlugin'>
+        >>> krb5_LP['plugins']['localauth']['module']
+        'sssd:/usr/lib64/sssd/modules/sssd_krb5_localauth_plugin.so'
+    """
+
+    RESERVED_ATTRS = ()
