38038cca70d43f69f61776f1d977302572c20d98
Xiangce Liu <xiangceliu@redhat.com>
2026-01-30 14:42:37 +0800

    chore(parser): refine the SEStatus parser (#4706)
    - discard the use of LegacyItemAccess
    - update the logic (keep backword comptibility)
    - Jira RHINENG-23655
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/parsers/sestatus.py
+++ insights/parsers/sestatus.py
@@ -1,31 +1,25 @@
-from .. import parser, LegacyItemAccess, CommandParser
+"""
+SEStatus - command ``sestatus -b``
+==================================
+"""
+
+from insights.core import CommandParser
+from insights.core.plugins import parser
 from insights.specs import Specs
 
 
 @parser(Specs.sestatus)
-class SEStatus(LegacyItemAccess, CommandParser):
-    """Class to parse the ``sestatus -b`` command
+class SEStatus(CommandParser, dict):
+    """
+    Class to parse the output of ``sestatus -b`` command into a Python dictionary
+    with names mangled as below:
+    - All characters are converted to lowercase
+    - All blank spaces are converted to underline
+    - All policy-booleans items are put as the value of "Policy booleans"
 
-    Attributes:
-        data (dict): A dict likes
-        {
-            "loaded_policy_name": "targeted",
-            "policy_booleans": {
-                "antivirus_use_jit": False,
-                "abrt_anon_write": False,
-                "abrt_upload_watch_anon_write": True,
-                "antivirus_can_scan_system": False,
-                "abrt_handle_event": False,
-                "auditadm_exec_content": True,
-            },
-            "mode_from_config_file": "enforcing",
-            "current_mode": "enforcing",
-            "policy_mls_status": "enabled",
-            "policy_deny_unknown_status": "allowed",
-            "max_kernel_policy_version": "30"
-        }
+    Sample output of ``sestatus -b``::
 
-    ---Sample---
+        SELinux status:                 enabled
         Loaded policy name:             targeted
         Current mode:                   enforcing
         Mode from config file:          enforcing
@@ -41,37 +35,47 @@ class SEStatus(LegacyItemAccess, CommandParser):
         antivirus_use_jit                           off
         auditadm_exec_content                       on
         ...
+
+    Example:
+        >>> type(sestatus)
+        <class 'insights.parsers.sestatus.SEStatus'>
+        >>> sestatus['selinux_status']
+        'enabled'
+        >>> sestatus['policy_booleans']['abrt_anon_write']
+        False
+
     """
 
     def parse_content(self, content):
-        # Default to disabled if not found
-        sestatus_info = {
-            'loaded_policy_name': None,
-            'current_mode': 'disabled',
-            'mode_from_config_file': 'disabled',
-            'policy_mls_status': 'disabled',
-            'policy_deny_unknown_status': 'disabled',
-            'max_kernel_policy_version': None,
-            'policy_booleans': {},
-        }
+        # Backward compatible
+        # - Default to disabled if not found
+        self.update(
+            loaded_policy_name=None,
+            current_mode='disabled',
+            mode_from_config_file='disabled',
+            policy_mls_status='disabled',
+            policy_deny_unknown_status='disabled',
+            max_kernel_policy_version=None,
+            policy_booleans={},
+        )
 
         for line in content:
+            line = line.strip()
             if ":" in line:
                 if 'Policy booleans' in line:
-                    pass
+                    self['policy_booleans'] = {}
                 else:
                     key, val = [s.strip() for s in line.split(":", 1)]
-                    sestatus_info[key.lower().replace(" ", "_")] = val
-            else:
-                if line.strip():
-                    key, val = line.split()
-                    #  convert 'on' and 'off' strings to actual boolean values
-                    sestatus_info['policy_booleans'][key] = val == 'on'
+                    self[key.lower().replace(" ", "_")] = val
+            elif line:
+                key, val = line.split()
+                #  convert 'on' and 'off' strings to actual boolean values
+                self['policy_booleans'][key] = val == 'on'
 
-        # When SELinux is disabled, sestatus has simply 'SELinux status: disabled'
-        # in its output.  But 'SELinux status' is not included in the output
-        # when SELinux is enabled.  So we include it as a nicety.
-        if sestatus_info['current_mode'] != 'disabled' and 'selinux_status' not in sestatus_info:
-            sestatus_info['selinux_status'] = sestatus_info['current_mode']
+        # Backward compatible
+        # - "SELinux status" now are always printed
+        if 'selinux_status' not in self and 'current_mode' in self:
+            self['selinux_status'] = self['current_mode']
 
-        self.data = sestatus_info
+    # Backward compatible
+    data = property(lambda self: self)
--- insights/tests/parsers/test_sestatus.py
+++ insights/tests/parsers/test_sestatus.py
@@ -1,8 +1,12 @@
+import doctest
+
+from insights.parsers import sestatus
 from insights.parsers.sestatus import SEStatus
 from insights.tests import context_wrap
 
 
 SESTATUS_ENFORCING = '''
+SELinux status:                 enabled
 Loaded policy name:             targeted
 Current mode:                   enforcing
 Mode from config file:          enforcing
@@ -17,6 +21,11 @@ abrt_upload_watch_anon_write                on
 antivirus_can_scan_system                   off
 antivirus_use_jit                           off
 auditadm_exec_content                       on
+'''
+
+SESTATUS_ENFORCING_LEGACY = '''
+Loaded policy name:             targeted
+Current mode:                   enforcing
 '''
 
 SESTATUS_PERMISSIVE = '''
@@ -49,7 +58,7 @@ SELinux status:                 disabled
 
 
 def test_sestatus():
-    sestatus_info = SEStatus(context_wrap(SESTATUS_ENFORCING)).data
+    sestatus_info = SEStatus(context_wrap(SESTATUS_ENFORCING))
 
     assert sestatus_info['loaded_policy_name'] == 'targeted'
     assert sestatus_info['current_mode'] == 'enforcing'
@@ -57,33 +66,42 @@ def test_sestatus():
     assert sestatus_info['policy_mls_status'] == 'enabled'
     assert sestatus_info['policy_deny_unknown_status'] == 'allowed'
     assert sestatus_info['max_kernel_policy_version'] == '30'
-    assert sestatus_info['selinux_status'] == 'enforcing'
-    assert sestatus_info['policy_booleans'] == {'abrt_anon_write': False,
-                                                'abrt_handle_event': False,
-                                                'abrt_upload_watch_anon_write': True,
-                                                'antivirus_can_scan_system': False,
-                                                'antivirus_use_jit': False,
-                                                'auditadm_exec_content': True}
-    assert sorted(sestatus_info) == sorted([
-        'loaded_policy_name', 'current_mode', 'mode_from_config_file',
-        'policy_mls_status', 'policy_deny_unknown_status',
-        'max_kernel_policy_version', 'policy_booleans', 'selinux_status'
-    ])
+    assert sestatus_info['selinux_status'] == 'enabled'
+    assert sestatus_info['policy_booleans'] == {
+        'abrt_anon_write': False,
+        'abrt_handle_event': False,
+        'abrt_upload_watch_anon_write': True,
+        'antivirus_can_scan_system': False,
+        'antivirus_use_jit': False,
+        'auditadm_exec_content': True,
+    }
+    assert sorted(sestatus_info) == sorted(
+        [
+            'loaded_policy_name',
+            'current_mode',
+            'mode_from_config_file',
+            'policy_mls_status',
+            'policy_deny_unknown_status',
+            'max_kernel_policy_version',
+            'policy_booleans',
+            'selinux_status',
+        ]
+    )
 
     perm_info = SEStatus(context_wrap(SESTATUS_PERMISSIVE))
     assert perm_info is not None
-    assert perm_info.data
-    assert 'selinux_status' in perm_info.data
-    assert perm_info.data['selinux_status'] == 'enabled'
-    assert perm_info.data['selinuxfs_mount'] == '/sys/fs/selinux'
-    assert perm_info.data['selinux_root_directory'] == '/etc/selinux'
-    assert perm_info.data['loaded_policy_name'] == 'targeted'
-    assert perm_info.data['current_mode'] == 'permissive'
-    assert perm_info.data['mode_from_config_file'] == 'permissive'
-    assert perm_info.data['policy_mls_status'] == 'enabled'
-    assert perm_info.data['policy_deny_unknown_status'] == 'allowed'
-    assert perm_info.data['max_kernel_policy_version'] == '28'
-    assert perm_info.data['policy_booleans'] == {
+    assert perm_info
+    assert 'selinux_status' in perm_info
+    assert perm_info['selinux_status'] == 'enabled'
+    assert perm_info['selinuxfs_mount'] == '/sys/fs/selinux'
+    assert perm_info['selinux_root_directory'] == '/etc/selinux'
+    assert perm_info['loaded_policy_name'] == 'targeted'
+    assert perm_info['current_mode'] == 'permissive'
+    assert perm_info['mode_from_config_file'] == 'permissive'
+    assert perm_info['policy_mls_status'] == 'enabled'
+    assert perm_info['policy_deny_unknown_status'] == 'allowed'
+    assert perm_info['max_kernel_policy_version'] == '28'
+    assert perm_info['policy_booleans'] == {
         'abrt_anon_write': False,
         'abrt_handle_event': False,
         'abrt_upload_watch_anon_write': True,
@@ -98,13 +116,27 @@ def test_sestatus():
 
     disabled_info = SEStatus(context_wrap(SESTATUS_DISABLED))
     assert disabled_info is not None
-    assert disabled_info.data
-    assert disabled_info.data['loaded_policy_name'] is None
-    assert disabled_info.data['current_mode'] == 'disabled'
-    assert disabled_info.data['mode_from_config_file'] == 'disabled'
-    assert disabled_info.data['policy_mls_status'] == 'disabled'
-    assert disabled_info.data['policy_deny_unknown_status'] == 'disabled'
-    assert disabled_info.data['max_kernel_policy_version'] is None
-    assert 'selinux_status' in disabled_info.data
-    assert disabled_info.data['selinux_status'] == 'disabled'
-    assert disabled_info.data['policy_booleans'] == {}
+    assert disabled_info
+    assert disabled_info['loaded_policy_name'] is None
+    assert disabled_info['current_mode'] == 'disabled'
+    assert disabled_info['mode_from_config_file'] == 'disabled'
+    assert disabled_info['policy_mls_status'] == 'disabled'
+    assert disabled_info['policy_deny_unknown_status'] == 'disabled'
+    assert disabled_info['max_kernel_policy_version'] is None
+    assert 'selinux_status' in disabled_info
+    assert disabled_info['selinux_status'] == 'disabled'
+    assert disabled_info['policy_booleans'] == {}
+
+
+def test_sestatus_legacy():
+    sestatus_info = SEStatus(context_wrap(SESTATUS_ENFORCING_LEGACY))
+    assert sestatus_info['current_mode'] == 'enforcing'
+    assert sestatus_info['selinux_status'] == 'enforcing'
+    assert sestatus_info['policy_booleans'] == {}
+
+
+def test_doc_examples():
+    failed_count, tests = doctest.testmod(
+        sestatus, globs={'sestatus': SEStatus(context_wrap(SESTATUS_ENFORCING))}
+    )
+    assert failed_count == 0
