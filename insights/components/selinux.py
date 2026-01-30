47f3fd0f0110f041a2929b0e357b456f88402147
Xiangce Liu <xiangceliu@redhat.com>
2026-01-30 18:56:59 +0800

    chore(component): update and add new selinux components (#4710)
    - Update SELinuxEnabled to use SEStatus first, SelinuxConfig second
    - Add new SELinuxDisabled and SELinuxNotEnforcing components
    - Jira: RHINENG-23654
    
    
    (cherry picked from commit a48ed8de2232af23f04d97d52a7825f959403484)
    
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- insights/components/selinux.py
+++ insights/components/selinux.py
@@ -1,23 +1,57 @@
 """
-SELinuxEnabled
-==============
+SELinux related Components
+==========================
 
-The ``SELinuxEnabled`` component uses ``SelinuxConfig`` parser to determine if
-SELinux is enabled or not.  The ``SkipComponent`` will be raised only when
-"SELINUX=disabled" is set.
+This module provides components which check the SELinux status.
+
+SELinuxEnabled - If SELinux is enabled
+--------------------------------------
+
+SELinuxDisabled - If SELinux is disabled
+----------------------------------------
 """
+
 from insights.core.exceptions import SkipComponent
 from insights.core.plugins import component
 from insights.parsers.selinux_config import SelinuxConfig
+from insights.parsers.sestatus import SEStatus
 
 
-@component(SelinuxConfig)
+@component([SEStatus, SelinuxConfig])
 class SELinuxEnabled(object):
     """
+    This ``SELinuxEnabled`` component will be skipped when SELinux is disabled,
+    which means the SELinux is enabled in "enforcing" or "permissive" mode.
+
     Raises:
-        SkipComponent: When ``SELINUX=disabled`` is set.
+        SkipComponent: When SELinux is disabled
     """
-    def __init__(self, seconf):
-        self.selinux = seconf.get('SELINUX')
+
+    def __init__(self, ses, seconf):
+        if ses:
+            self.selinux = ses.get('selinux_status')
+        else:
+            self.selinux = seconf.get('SELINUX', 'disabled')
+
         if self.selinux == 'disabled':
-            raise SkipComponent("SELinux not enabled")
+            raise SkipComponent('SELinux not enabled')
+
+
+@component([SEStatus, SelinuxConfig])
+class SELinuxDisabled(object):
+    """
+    This ``SELinuxDisabled`` component will be skipped when SELinux is enabled
+    in "enforcing" or "permissive" mode, which means the SELinux is disabled.
+
+    Raises:
+        SkipComponent: When SELinux is "enforcing" or "permissive".
+    """
+
+    def __init__(self, ses, seconf):
+        if ses:
+            self.selinux = ses.get('selinux_status')
+        else:
+            self.selinux = seconf.get('SELINUX', 'disabled')
+
+        if self.selinux != 'disabled':
+            raise SkipComponent('SELinux is enabled')
